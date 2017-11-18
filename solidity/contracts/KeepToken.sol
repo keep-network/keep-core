dc3e402c07c5c1c6d15128fe34c22f58dad68de3
Nik G <subzeromedia@gmail.com>
2017-11-17 16:32:33 -0800

    Add staking to KEEP token

--- solidity/contracts/KeepToken.sol
+++ solidity/contracts/KeepToken.sol
@@ -6,4 +6,45 @@ contract KeepToken is MintableToken {
   string public name = "KEEP TOKEN";
   string public symbol = "KEEP";
   uint256 public decimals = 18;
+
+  mapping(address => uint256) stakeBalances;
+
+  event StakeIn(address indexed from, uint256 value);
+  event StakeOut(address indexed from, uint256 value);
+
+  /**
+  * @dev Stake in tokens
+  * @param _value The amount to be staked in
+  */
+  function stakeIn(uint256 _value) public returns (bool) {
+    require(_value <= balances[msg.sender]);
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    stakeBalances[msg.sender] = stakeBalances[msg.sender].add(_value);
+    StakeIn(msg.sender, _value);
+    return true;
+  }
+
+  /**
+  * @dev Stake out tokens
+  * @param _value The amount to be staked out
+  */
+  function stakeOut(uint256 _value) public returns (bool) {
+    require(_value <= stakeBalances[msg.sender]);
+
+    stakeBalances[msg.sender] = stakeBalances[msg.sender].sub(_value);
+    balances[msg.sender] = balances[msg.sender].add(_value);
+    StakeOut(msg.sender, _value);
+    return true;
+  }
+
+
+  /**
+  * @dev Gets the stake balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function stakeBalanceOf(address _owner) public constant returns (uint256 stakeBalance) {
+    return stakeBalances[_owner];
+  }
 }
