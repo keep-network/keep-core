c0c341a95d9cab5305f4705e3254a808eaf80868
Nik G <subzeromedia@gmail.com>
2017-11-17 17:17:23 -0800

    Minor update

--- solidity/contracts/KeepToken.sol
+++ solidity/contracts/KeepToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 
 contract KeepToken is MintableToken {
-  string public name = "KEEP TOKEN";
+  string public name = "KEEP Token";
   string public symbol = "KEEP";
   uint256 public decimals = 18;
 
