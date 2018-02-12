20e89f2b91654a43e5677b4f5694a7362ccae64b
Antonio Salazar Cardozo <antonio@keep.network>
2018-02-12 15:13:43 -0500

    Transition keep-network imports to github.com/keep-network/keep-core.
    For now it's /keep-core/go/..., but we'll see if we come up with a different
    strategy.

--- go/main.go
+++ go/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/dfinity/go-dfinity-crypto/rand"
+	"github.com/keep-network/go-dfinity-crypto/bls"
+	"github.com/keep-network/keep-core/go/beacon/broadcast"
+	"github.com/keep-network/keep-core/go/beacon/chain"
+	"github.com/keep-network/keep-core/go/beacon/dkg"
 )
 
 func main() {
