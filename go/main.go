796d7ba2f14374967c9133b2a1a56b558457a81a
Antonio Salazar Cardozo <antonio@keep.network>
2018-02-12 18:03:34 -0500

    Set up main to call DKG with the configured group size and threshold
    We use a local broadcast channel and a channel for reporting the complete
    members as they wrap up. No group signing yet though!

--- go/main.go
+++ go/main.go
@@ -2,15 +2,56 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/dfinity/go-dfinity-crypto/rand"
 	"github.com/keep-network/go-dfinity-crypto/bls"
 	"github.com/keep-network/keep-core/go/beacon/broadcast"
 	"github.com/keep-network/keep-core/go/beacon/chain"
 	"github.com/keep-network/keep-core/go/beacon/dkg"
+	"github.com/keep-network/keep-core/go/thresholdgroup"
 )
 
 func main() {
-	r := rand.NewRand()
-	fmt.Printf("%v\n", r)
+	bls.Init(bls.CurveFp382_1)
+
+	beaconConfig := chain.GetBeaconConfig()
+
+	channel := broadcast.LocalChannel("test")
+	chainCounter := chain.LocalBlockCounter()
+
+	members := make([]*thresholdgroup.Member, 0, beaconConfig.GroupSize)
+	memberChannel := make(chan *thresholdgroup.Member)
+	for i := 0; i < beaconConfig.GroupSize; i++ {
+		go func() {
+			member, err := dkg.Execute(chainCounter, channel, beaconConfig.GroupSize, beaconConfig.Threshold)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to run DKG for member %v: [%s].", i, err)
+				memberChannel <- nil
+				return
+			}
+
+			memberChannel <- member
+		}()
+	}
+
+	seenMembers := 0
+	for member := range memberChannel {
+		seenMembers++
+		if member != nil {
+			members = append(members, member)
+			if len(members) == beaconConfig.GroupSize {
+				break
+			}
+		}
+
+		if seenMembers == beaconConfig.GroupSize {
+			break
+		}
+	}
+
+	if len(members) < beaconConfig.GroupSize {
+		panic("Failed to reach group size during DKG, aborting.")
+	}
+
+	fmt.Printf("Members! %v\n", members)
 }
