4847b802f7837ee87f25bc2be0969488a11dc85f
Philip Schlump <pschlump@gmail.com>
2018-02-13 13:06:37 -0700

    Moved checksum generation into own directory

--- go/contract/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var Storage = artifacts.require("./Storage.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Storage);
-};
