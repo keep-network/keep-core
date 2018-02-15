6d20a1bb47265bb76c4e0a10886ffded01999ca1
l3x <lex.sheehan@gmail.com>
2018-02-15 10:45:16 -0500

    Add --force flag for build script and mention including vendor dir
    This build proces assumes that the vendor directory has been checked
    into github.  Otherwise, the build server must run dep ensure.

--- build-keep-client-docker-img.sh
+++ build-keep-client-docker-img.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# Build vendor directory from latest source
+# Build vendor directory from latest source (assumes vendor directory is checked into github)
+# Usage: build-keep-client-docker-img.sh [--force]
+# Note:  Only use the --force flag locally when testing.  Your last test before pushing to github
+#        should be run w/o the --force flag to ensure your latest updates will work in production.
+FORCE=$1
 if [ "$(basename $(pwd))" != "keep-core" ] || [ ! -d ./go ]; then
 	echo "You should run $(basename $0) from the github.com/keep-net/keep-core directory"
-	exit 2
+	if [ "$FORCE" != "--force" ]; then exit 2;fi
 fi
 
 if [ $(git status | grep "On branch master" | wc | awk '{print $1}') == "0" ]; then
@@ -11,11 +15,11 @@ if [ $(git status | grep "On branch master" | wc | awk '{print $1}') == "0" ]; t
 fi
 if [ $(git status | grep "Your branch is up to date with 'origin/master'." | wc | awk '{print $1}') == "0" ]; then
 	echo "Your branch (local file system) is NOT up-to-date with the master branch."
-	exit 2
+	if [ "$FORCE" != "--force" ]; then exit 2;fi
 fi
 if [ $(git status | grep "nothing to commit, working tree clean" | wc | awk '{print $1}') == "0" ]; then
 	echo "Have you committed all of your changes?"
-	exit 2
+	if [ "$FORCE" != "--force" ]; then exit 2;fi
 fi
 
 # Go to source directory
--- run-keep-client.sh
+++ run-keep-client.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Run both the build and the test script like this:  
+#   ./build-keep-client-docker-img.sh && ./run-keep-client.sh
+
 IMG=keep-client
 DOCKERFILE=Dockerfile
 CMD=keep-client
