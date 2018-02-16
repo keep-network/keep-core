c9121756c0171e40b3ee2888c425d4e2c631dd27
Nik G <subzeromedia@gmail.com>
2018-02-16 13:24:42 +0000

    Split tests to avoid 'out of gas' issue

--- solidity/test/TestTokenGrant.sol
+++ solidity/test/TestTokenGrant.sol
@@ -45,32 +45,4 @@ contract TestTokenGrant {
     Assert.equal(_start, start, "Grant should have start time.");
     Assert.equal(_cliff, start+cliff, "Grant should have vesting schedule cliff duration.");
   }
-
-  function testCannotReleaseGrantedAmmount() {
-    Assert.equal(c.unreleasedAmount(id), 0, "Unreleased token grant amount should be 0.");
-
-    // http://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
-    ThrowProxy throwProxy = new ThrowProxy(address(c));
-
-    // Prime the proxy
-    TokenGrant(address(throwProxy)).release(id);
-
-    // Execute the call that is supposed to throw.
-    // r will be false if it threw and true if it didn't.
-    bool r = throwProxy.execute.gas(200000)();
-    Assert.isFalse(r, "Should throw when trying to release token grant.");
-
-    Assert.equal(t.balanceOf(beneficiary), 0, "Released balance should not be added to beneficiary main balance.");
-  }
-
-  // TOken grant with 0 duration, release available immediately.
-  function testCanReleaseGrantedAmount() {
-    
-    t.approve(address(c), 100);
-    id = c.grant(100, beneficiary, 0, now, 0, false);
-
-    c.release(id);
-    Assert.equal(t.balanceOf(beneficiary), 100, "Released balance should be added to beneficiary main balance.");
-    Assert.equal(c.unreleasedAmount(id), 0, "Unreleased granted amount should be 0 after release.");
-  }
 }
