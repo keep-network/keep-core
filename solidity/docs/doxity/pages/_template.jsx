62c893c7bfdfa5909c2522d8b9252971d533d328
Nik G <subzeromedia@gmail.com>
2018-03-09 15:59:01 +0000

    Add navbar + minor fixes

--- solidity/docs/doxity/pages/_template.jsx
+++ solidity/docs/doxity/pages/_template.jsx
@@ -6,6 +6,7 @@ import { config } from 'config'
 import { Menu, Container, Label, Segment, Grid, Icon } from 'semantic-ui-react'
 import '../css/style.scss'
 import sortBy from 'sort-by'
+import * as Icons from '../components/icons';
 
 export default class Index extends Component {
   constructor(props) {
@@ -23,38 +24,55 @@ export default class Index extends Component {
     const childRoutes = this.props.route && this.props.route.childRoutes
     const docsPath = childRoutes && childRoutes[0] && childRoutes[0].path
     const menuItems = childRoutes.map((child) => {
-      const isActive = prefixLink(child.path) === this.props.location.pathname
-      return (
-        <Menu text vertical key={child.page.data.name}>
-          <Menu.Item header onClick={this.handleItemClick}>
-            {isActive ? <strong>{child.page.data.name}</strong> : child.page.data.name}
-          </Menu.Item>
-          {child.page.data.abiDocs.sort(sortBy('type', 'name')).map(method => {
-            if (method.name) return (
-              <Menu.Item name={method.name} key={`${child.page.data.name}${method.name}`}>
-                <Link
-                  to={`${child.page.data.name}${method.name}`}
-                  isDynamic={false}
-                  duration={500}
-                  smooth={true}
-                  containerId="mainscroll">
-                  {method.name}
-                </Link>
-              </Menu.Item>
-            )
-          })}
-        </Menu>
-      )
+      if (prefixLink(child.path) === this.props.location.pathname) {
+        return (
+          <Menu text vertical key={child.page.data.name}>
+            {child.page.data.abiDocs.sort(sortBy('type', 'name')).map(method => {
+              if (method.name) return (
+                <Menu.Item name={method.name} key={`${child.page.data.name}${method.name}`}>
+                  <Link
+                    to={`${child.page.data.name}${method.name}`}
+                    isDynamic={false}
+                    duration={500}
+                    smooth={true}
+                    containerId="mainscroll">
+                    {method.name}
+                  </Link>
+                </Menu.Item>
+              )
+            })}
+          </Menu>
+        )
+      }
     })
     return (
-      <div style={{ paddingTop: '60px' }} className="pusher">
+      <div style={{ paddingTop: '120px' }} className="pusher">
+        <Menu borderless fixed="top" className="navbar">
+          <Container>
+            <Menu.Item header as={Link} to={prefixLink('/')}>
+              <div>
+                <Icons.Keep width="160px" height="auto" />
+                <p>Contracts Documentation</p>
+              </div>
+            </Menu.Item>
+            <Menu.Menu position="right">
+              {childRoutes.map((child) => {
+                return (
+                  <Menu.Item as={'a'} href={prefixLink(child.path)}>
+                    {child.page.data.name}
+                  </Menu.Item>
+                )
+              })}
+            </Menu.Menu>
+          </Container>
+        </Menu>
         <Container>
           <Grid>
             <Grid.Row>
-              <Grid.Column width={4}>
+              <Grid.Column width={3}>
                 <div className="scrollable">{menuItems}</div>
               </Grid.Column>
-              <Grid.Column width={12}>
+              <Grid.Column width={13}>
                 <div className="scrollable" id="mainscroll">{this.props.children}</div>
               </Grid.Column>
             </Grid.Row>
--- solidity/scripts/docs.sh
+++ solidity/scripts/docs.sh
@@ -4,4 +4,4 @@ flatten contracts/KeepToken.sol --output docs/contracts/KeepToken.sol
 flatten contracts/TokenStaking.sol --output docs/contracts/TokenStaking.sol
 flatten contracts/TokenGrant.sol --output docs/contracts/TokenGrant.sol
 
-cd docs && node ../../doxity/lib/bin/doxity.js build
+cd docs && node ../node_modules/@digix/doxity/lib/bin/doxity.js build
