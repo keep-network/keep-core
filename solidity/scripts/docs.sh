349a72a11c199fcf78f9d949c8dc63357faf648a
Nik G <subzeromedia@gmail.com>
2018-03-16 15:58:28 +0000

    Fix docs temp folders

--- solidity/scripts/docs.sh
+++ solidity/scripts/docs.sh
@@ -5,4 +5,5 @@ flatten contracts/KeepToken.sol --output docs/contracts/KeepToken.sol
 flatten contracts/TokenStaking.sol --output docs/contracts/TokenStaking.sol
 flatten contracts/TokenGrant.sol --output docs/contracts/TokenGrant.sol
 
-cd docs && node ../node_modules/@digix/doxity/lib/bin/doxity.js build
+cd docs && mkdir -p output doxity/pages/docs
+node ../node_modules/@digix/doxity/lib/bin/doxity.js build
