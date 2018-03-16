da468912334d711762d19ca5ff633072f14e946a
Nik G <subzeromedia@gmail.com>
2018-03-16 17:29:18 +0000

    Make alias work in docs script. Cleanup

--- solidity/scripts/docs.sh
+++ solidity/scripts/docs.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+shopt -s expand_aliases
 alias flatten="solidity_flattener --solc-paths zeppelin-solidity=$(pwd)/node_modules/zeppelin-solidity"
+
+mkdir -p docs/contracts docs/output docs/doxity/pages/docs
 
 flatten contracts/KeepToken.sol --output docs/contracts/KeepToken.sol
 flatten contracts/TokenStaking.sol --output docs/contracts/TokenStaking.sol
 flatten contracts/TokenGrant.sol --output docs/contracts/TokenGrant.sol
 
-cd docs && mkdir -p output doxity/pages/docs
-node ../node_modules/@digix/doxity/lib/bin/doxity.js build
+cd docs && node ../node_modules/@digix/doxity/lib/bin/doxity.js build
