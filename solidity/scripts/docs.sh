2eb51a9c0dbc00bf5c6ada95b5a53337e885a409
Nik G <subzeromedia@gmail.com>
2018-03-16 18:24:52 +0000

    Add python env. Add solidity-flattener as dependancy

--- solidity/scripts/docs.sh
+++ solidity/scripts/docs.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 shopt -s expand_aliases
+
 alias flatten="solidity_flattener --solc-paths zeppelin-solidity=$(pwd)/node_modules/zeppelin-solidity"
 
 mkdir -p docs/contracts docs/output docs/doxity/pages/docs
 
+source ~/venv/bin/activate
 flatten contracts/KeepToken.sol --output docs/contracts/KeepToken.sol
 flatten contracts/TokenStaking.sol --output docs/contracts/TokenStaking.sol
 flatten contracts/TokenGrant.sol --output docs/contracts/TokenGrant.sol
