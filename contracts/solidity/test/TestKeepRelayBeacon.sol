ee3f18ed154b7c50fce3e11e0fb76bf3dd82d713
Philip Schlump <pschlump@gmail.com>
2018-03-23 12:12:15 -0600

    Remove tabs

--- contracts/solidity/test/TestKeepRelayBeacon.sol
+++ contracts/solidity/test/TestKeepRelayBeacon.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.4.18;
 import "truffle/Assert.sol";
 import "../contracts/KeepRelayBeacon.sol";
 
-contract TestKeepRelayBeacon {	
+contract TestKeepRelayBeacon {  
 
-	address stakingAddress = address(0x51fcf39b2050c04a14152a5dd69884e811968128);
-	KeepRelayBeacon ks = new KeepRelayBeacon( stakingAddress, 1 );
+  address stakingAddress = address(0x51fcf39b2050c04a14152a5dd69884e811968128);
+  KeepRelayBeacon ks = new KeepRelayBeacon( stakingAddress, 1 );
 
-	function testRequestRelay() public {
-		uint256 rid = ks.requestRelay(5,1212121);
-		Assert.equal(rid, rid, "should have generated a unique request id, did not.");
-	}
+  function testRequestRelay() public {
+    uint256 rid = ks.requestRelay(5,1212121);
+    Assert.equal(rid, rid, "should have generated a unique request id, did not.");
+  }
 
 }
--- contracts/solidity/test/testRequestRelayEvent.js
+++ contracts/solidity/test/testRequestRelayEvent.js
@@ -2,15 +2,15 @@
 var KeepRelayBeacon = artifacts.require("KeepRelayBeacon");
 
 contract('KeepRelayBeacon', function(accounts) {
-	it("KeepRelayBeacon.requestRelay must return an event:T-00001: ", function() {
-		return KeepRelayBeacon.deployed().then(function(instance) {
-			return instance.requestRelay(8,12111);
-		}).then(function(events) {
-			assert.equal(events.logs.length, 1);
-			assert.equal(events.logs[0].event, "RelayEntryRequested");
-			//
-			// TODO: should check some other stuff at this point too!
-			//
-		});
-	});
+  it("KeepRelayBeacon.requestRelay must return an event:T-00001: ", function() {
+    return KeepRelayBeacon.deployed().then(function(instance) {
+      return instance.requestRelay(8,12111);
+    }).then(function(events) {
+      assert.equal(events.logs.length, 1);
+      assert.equal(events.logs[0].event, "RelayEntryRequested");
+      //
+      // TODO: should check some other stuff at this point too!
+      //
+    });
+  });
 });
