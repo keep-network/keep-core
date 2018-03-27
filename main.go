db45218f7023c8dd024acc48622a8885cd7fc080
Antonio Salazar Cardozo <antonio@keep.network>
2018-03-27 12:35:01 -0400

    Pull net/gen/gossip into the compile
    This ensures CI will tell us if the generation failed or whatever.

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 	"github.com/keep-network/keep-core/pkg/beacon/broadcast"
 	"github.com/keep-network/keep-core/pkg/beacon/chain"
 	"github.com/keep-network/keep-core/pkg/beacon/relay"
+	"github.com/keep-network/keep-core/pkg/net/gen/gossip"
 	"github.com/keep-network/keep-core/pkg/thresholdgroup"
 )
 
 func main() {
 	bls.Init(bls.CurveFp382_1)
+
+	_ = gossip.GossipMessage{}
 
 	beaconConfig := chain.GetBeaconConfig()
 
