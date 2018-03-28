9d9fc5cdb9c1fc3beb04f76df5d9cd047e2e6a89
Nik G <subzeromedia@gmail.com>
2018-03-28 12:19:11 +0100

    Add staking proxy contract
    This is a simple contract with basic events and methods to
    allow upgradable staking functionality. Compatible contracts are
    added(authorised) to this contract by this contract owner.

---
+++ solidity/contracts/StakingProxy.sol
@@ -0,0 +1,101 @@
+pragma solidity ^0.4.18;
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+interface authorizedStakingContract {
+    function stakeBalanceOf(address addr) public returns (uint256);
+}
+
+
+/**
+ * @title Staking Proxy Contract
+ * @dev An ownable staking proxy contract to provide upgradable staking.
+ * Upgraded contracts are added to authorizedContracts list.
+ * The staking contracts must call emit events on this contract.
+ */
+contract StakingProxy is Ownable {
+
+    /**
+     * @dev Only authorized contracts can invoke functions with this modifier.
+     */
+    modifier onlyAuthorized {
+        require(isAuthorized(msg.sender));
+        _;
+    }
+
+    address[] public authorizedContracts;
+
+    event Staked(address indexed user, uint256 amount);
+    event Unstaked(address indexed user, uint256 amount);
+
+    /**
+     * @dev Gets the sum of all staking balances of the specified staker address.
+     * @param _staker The address to query the balance of.
+     * @return balance An uint256 representing the amount staked by the passed address.
+     */
+    function balanceOf(address _staker)
+        public
+        constant
+        returns (uint256 balance)
+    {
+        uint256 balance = 0;
+        for (uint i = 0; i < authorizedContracts.length; i++) {
+            balance = balance + authorizedStakingContract(authorizedContracts[i]).stakeBalanceOf(_staker);
+        }
+        return balance;
+    }
+
+    /**
+     * @dev Authorize contract.
+     * @param _contract The address of a staking contract.
+     */
+    function authorizeContract(address _contract) 
+        public
+        onlyOwner
+    {
+        require(_contract != address(0));
+        require(!isAuthorized(_contract));
+        authorizedContracts.push(_contract);
+    }
+
+    /**
+     * @dev Emit staked event.
+     * @param _staker The address of the staker.
+     * @param _amount The staked amount.
+     */
+    function emitStakedEvent(address _staker, uint256 _amount)
+        public
+        onlyAuthorized
+    {
+        Staked(_staker, _amount);
+    }
+
+    /**
+     * @dev Emit unstaked event.
+     * @param _staker The address of the staker.
+     * @param _amount The unstaked amount.
+     */
+    function emitUnstakedEvent(address _staker, uint256 _amount)
+        public
+        onlyAuthorized
+    {
+        Unstaked(_staker, _amount);
+    }
+
+    /**
+     * @dev Check if a staking contract is authorized to work with this contract.
+     * @param _address The address of a staking contract.
+     * @return A bool wether it's authorized.
+     */
+    function isAuthorized(address _address) 
+        public
+        returns (bool) 
+    {
+        for (uint i = 0; i < contracts.length; i++) {
+            if (contracts[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
