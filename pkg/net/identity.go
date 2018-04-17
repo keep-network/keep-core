f810111f14bfeb3ae718d00ef09e33fba1d8e7d7
Raghav Gulati <rargulati@users.noreply.github.com>
2018-04-17 14:32:36 -0400

    Refactor Identity.
    First tune names to be more sensible. Also ensure parameters make sense
    for each function.
    
    Second, enable adding an identity to the store; unexport private key.

--- pkg/net/identity.go
+++ pkg/net/identity.go
@@ -5,21 +5,22 @@ import (
 	"io"
 
 	ci "github.com/libp2p/go-libp2p-crypto"
+	pstore "github.com/libp2p/go-libp2p-peerstore"
 	peer "github.com/rargulati/go-libp2p-peer"
 )
 
 type Identity interface {
 	ID() peer.ID
-	PubKey() ci.PubKey
+	PubKey() (ci.PubKey, error)
 	PubKey(peer.ID) (ci.PubKey, error)
 }
 
 type PeerIdentity struct {
-	PrivKey ci.PrivKey
+	privKey ci.PrivKey
 }
 
 func (pi *PeerIdentity) ID() peer.ID {
-	return pubKeyToID(pi.PrivKey.GetPublic())
+	return pubKeyToID(pi.privKey.GetPublic())
 }
 
 func pubKeyToID(pk ci.PubKey) peer.ID {
@@ -30,35 +31,64 @@ func pubKeyToID(pk ci.PubKey) peer.ID {
 	}
 }
 
-func (pi *PeerIdentity) PubKey() ci.PubKey {
-	return pi.PrivKey.GetPublic()
+func (pi *PeerIdentity) KeyPair() (ci.PrivKey, ci.PubKey) {
+	return pi.privkey, pi.privKey.GetPublic()
+}
+
+func (pi *PeerIdentity) AddIdentityToStore() (*pstore.Peerstore, error) {
+	ps := pstore.NewPeerstore()
+	// HACK: see github.com/rargulati/go-libp2p-crypto for fix
+	if err := ps.AddPrivKey(pi.ID(), pi.privKey); err != nil {
+		return nil, fmt.Errorf("failed to add PrivateKey with error %s", err)
+	}
+	if err := ps.AddPubKey(pi.ID(), pi.PubKey()); err != nil {
+		return nil, fmt.Errorf("failed to add PubKey with error %s", err)
+	}
+	return ps, nil
+}
+
+func (pi *PeerIdentity) PubKey(peer.ID) (ci.PubKey, error) {
+	return pi.privKey.GetPublic()
 }
 
 func (pi *PeerIdentity) PubKey(peer.ID) (ci.PubKey, error) {
 	return pi.ID().ExtractEd25519PublicKey()
 }
 
-func LoadOrGeneratePKI(filePath string) (*PeerIdentity, error) {
+func LoadOrGenerateIdentity(randseed int64, filePath string) (*PeerIdentity, error) {
 	if filePath != "" {
 		// TODO: unmarshal and build out PKI
+		// TODO: ensure this is associated with some staking address
 	}
-	return generatePKI(0, "") // TODO: ensure this is associated with some staking address
+	if randseed != 0 {
+		return generateDeterministicKeypair(randseed)
+	}
+	return generateKeypair()
 }
 
-// generatePKI generates a public/private-key pair
+// generateIdentity generates a public/private-key pair
 // (using the libp2p/crypto wrapper for golang/crypto) provided a reader.
 // Use randseed for deterministic IDs, otherwise we'll use cryptographically secure psuedorandomness.
-func generatePKI(randseed int64, keyType string) (ci.PrivKey, ci.PubKey, error) {
+func generateIdentity() (*PeerIdentity, error) {
 	var r io.Reader
-	if randseed == 0 {
-		r = crand.Reader
-	} else {
-		r = mrand.New(mrand.NewSource(randseed))
-	}
-	// TODO: explore if we use PublicKeyToCurve25519 (converts an Ed25519 public key into the curve25519)
-	priv, pub, err := ci.GenerateKeyPairWithReader(ci.Ed25519, 2048, r)
+	r = crand.Reader
+
+	priv, _, err := ci.GenerateKeyPairWithReader(ci.Ed25519, 2048, r)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	return priv, pub, nil
+
+	return &PeerIdentity{privKey: priv}, nil
+}
+
+func generateDeterministicIdentity(randseed int64) (*PeerIdentity, error) {
+	var r io.Reader
+	r = mrand.New(mrand.NewSource(randseed))
+
+	priv, _, err := ci.GenerateKeyPairWithReader(ci.Ed25519, 2048, r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PeerIdentity{privKey: priv}, nil
 }
