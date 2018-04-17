ba49420881c3aa75551b70e7b560a1dc6a0d6c73
Raghav Gulati <rargulati@users.noreply.github.com>
2018-04-17 14:45:06 -0700

    Introduce mechanism for Peers to authenticate.
    Peers must Authenticate messages through signing and verifying the
    individual messages that they pass through libp2p. We introduce an
    Authenticator interface that specifies Signing and Verifying methods.

--- pkg/net/peer.go
+++ pkg/net/peer.go
@@ -5,7 +5,14 @@ import (
 
 	host "github.com/libp2p/go-libp2p-host"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
+	peer "github.com/rargulati/go-libp2p-peer"
 )
+
+type Authenticator interface {
+	Sign(data []byte) ([]byte, error)
+	Sign(data interface{}) ([]byte, error)
+	Verify(data []byte, sig []byte, peerID peer.ID, pubKey []byte) bool
+}
 
 type Peer struct {
 	ID    Identity
@@ -25,3 +32,7 @@ func NewPeer(randseed int64, filepath string) *Peer {
 
 	return &Peer{ID: pi, Store: ps}
 }
+
+func (p *Peer) Sign(data []byte) ([]byte, error)
+func (p *Peer) Sign(data interface{}) ([]byte, error)
+func (p *Peer) Verify(data []byte, sig []byte, peerID peer.ID, pubkey []byte) bool
