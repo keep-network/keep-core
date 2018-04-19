3a801cfa75885474a344e9bb590e1e4ad40e255a
Nik G <subzeromedia@gmail.com>
2018-04-19 17:52:35 +0100

    Add example of upgraded implementation contract and tests

--- contracts/solidity/test/TestKeepRandomBeaconViaProxy.js
+++ contracts/solidity/test/TestKeepRandomBeaconViaProxy.js
@@ -6,11 +6,12 @@ const StakingProxy = artifacts.require('./StakingProxy.sol');
 const TokenStaking = artifacts.require('./TokenStaking.sol');
 const Proxy = artifacts.require('./KeepRandomBeacon.sol');
 const KeepRandomBeaconImplV1 = artifacts.require('./KeepRandomBeaconImplV1.sol');
+const Upgrade = artifacts.require('./examples/KeepRandomBeaconUpgradeExample.sol');
 
 
 contract('TestKeepRandomBeaconViaProxy', function(accounts) {
 
-  let token, stakingProxy, stakingContract, implV1, proxy, implViaProxy,
+  let token, stakingProxy, stakingContract, implV1, implV2, proxy, implViaProxy, impl2ViaProxy,
     account_one = accounts[0],
     account_two = accounts[1];
 
@@ -27,6 +28,21 @@ contract('TestKeepRandomBeaconViaProxy', function(accounts) {
   it("should be able to check if the implementation contract was initialized", async function() {
     let result = await implViaProxy.initialized();
     assert.equal(result, true, "Implementation contract should be initialized.");
+  });
+
+  it("should be able to upgrade implementation and initialize it with new data", async function() {
+
+    implV2 = await Upgrade.new();
+    await proxy.upgradeTo('v2', implV2.address);
+    
+    impl2ViaProxy = await Upgrade.at(proxy.address);
+    await impl2ViaProxy.initialize(stakingProxy.address, 100, 200);
+
+    let result = await impl2ViaProxy.initialized();
+    assert.equal(result, true, "Implementation contract should be initialized.");
+
+    let newVar = await impl2ViaProxy.getNewVar();
+    assert.equal(newVar, 1234, "Should be able to get new data from upgraded contract.");
   });
 
 });
