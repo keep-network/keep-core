768712a646ce6ddf1ccbc2b90081a3c90757f280
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2018-04-19 14:35:43 -0400

    Add pkg/chain and rearrange interfaces
    The new package contains abstract chain-interaction interfaces. For now, the
    main handle to a chain provides access to an implementor of the beacon's
    expected chain interface. The idea is to use this mechanism to provide, from
    one chain handle, access to multiple underlying contract interfaces, with the
    base functionality being specified by the consumer and the chain packages
    bridging the consumer's API with the chain API.
    
    This may end up needing further adjustment.

---
+++ pkg/chain/chain.go
@@ -0,0 +1,26 @@
+package chain
+
+import (
+	"github.com/keep-network/keep-core/pkg/beacon"
+)
+
+// BlockCounter is an interface that provides the ability to wait for a certain
+// number of abstract blocks. It provides for two ways to wait, one blocking and
+// one chan-based. Block height is expected to increase monotonically, though
+// the time between blocks will depend on the underlying implementation. See
+// LocalBlockCounter() for a local implementation.
+type BlockCounter interface {
+	// WaitForBlocks blocks at the caller until numBlocks new blocks have been
+	// seen.
+	WaitForBlocks(numBlocks int)
+	// BlockWaiter returns a channel that will emit the current block height
+	// after the given number of blocks has elapsed and then immediately close.
+	BlockWaiter(numBlocks int) <-chan int
+}
+
+// Handle represents a handle to a blockchain that provides access to the core
+// functionality needed for Keep network interactions.
+type Handle interface {
+	BlockCounter() BlockCounter
+	RandomBeacon() beacon.ChainInterface
+}
