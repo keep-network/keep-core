629a32df002d3f96b1f2c6b4bf46ebb32d09b206
Philip Schlump <philip.schlump@keep.network>
2018-04-24 13:53:20 -0600

    Partial cleanup of testing code

--- pkg/chain/ethereum/block_test.go
+++ pkg/chain/ethereum/block_test.go
@@ -4,44 +4,31 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/pschlump/MiscLib"
-	"github.com/pschlump/godebug"
-
 	"github.com/keep-network/keep-core/pkg/chain/ethereum"
 )
 
 type CfgType struct {
-	Server string
+	GethConnectionString string
 }
 
-var gCfg CfgType
-var initTest bool
-var client *rpc.Client
+var (
+	gCfg   CfgType
+	client *rpc.Client
+)
 
-func Test_BlockTest01(t *testing.T) {
+func Test_BlockTest(t *testing.T) {
 
 	// ----------------------------------------------------------------------------------------------------
 	// Test code that needs to do a timeout!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	// ----------------------------------------------------------------------------------------------------
 
-	// get connection info - cfg.json
-	err := InitTest(t)
-	if err != nil {
-		t.Fatalf("Initialization Failed, error: %s", err)
-		return
-	}
-
-	godebug.Printf(db02, "AT: %s\n", godebug.LF())
-
 	// func BlockCounter(client *rpc.Client) chain.BlockCounter {
 	countWait := ethereum.BlockCounter(client)
-	if db01 {
-		fmt.Printf("Before Wait\n")
-	}
 	start := time.Now()
 	countWait.WaitForBlocks(1)
 	tm := time.Now()
@@ -49,11 +36,7 @@ func Test_BlockTest01(t *testing.T) {
 	if elapsed < 1000000000 {
 		t.Errorf("Did not wait\n")
 	}
-	if db01 {
-		fmt.Printf("After Wait, %d\n", elapsed)
-	}
 
-	godebug.Printf(db02, "AT: %s\n", godebug.LF())
 	start = time.Now()
 	countWait.WaitForBlocks(2)
 	tm = time.Now()
@@ -62,10 +45,6 @@ func Test_BlockTest01(t *testing.T) {
 		t.Errorf("Did not wait\n")
 	}
 
-	godebug.Printf(db02, "AT: %s\n", godebug.LF())
-	if db01 {
-		fmt.Printf("Before test #3 , %d\n", elapsed)
-	}
 	start = time.Now()
 	countWait.WaitForBlocks(0)
 	tm = time.Now()
@@ -73,44 +52,31 @@ func Test_BlockTest01(t *testing.T) {
 	if elapsed < 1000 {
 		t.Errorf("Did not wait\n")
 	}
-	godebug.Printf(db02, "AT: %s\n", godebug.LF())
 }
 
-func InitTest(t *testing.T) (err error) {
-	if !initTest {
-		initTest = true
+func TestMain(m *testing.M) {
 
-		var buf []byte
+	var buf []byte
+	fn := "cfg.json"
 
-		godebug.Printf(db02, "AT: %s\n", godebug.LF())
-
-		// get connection info - cfg.json
-		buf, err = ioutil.ReadFile("cfg.json")
-		if err != nil {
-			t.Fatalf("Setup incorrect - unable to read cfg.json: error: %s", err)
-			return
-		}
-
-		err = json.Unmarshal(buf, &gCfg)
-		if err != nil {
-			t.Fatalf("Setup incorrect - Unable to parse cfg.json, error: %s", err)
-			return
-		}
-
-		godebug.Printf(db02, "AT: %s\n", godebug.LF())
-
-		// use that to setup connection to Geth
-		client, err = rpc.Dial(gCfg.Server)
-		if err != nil {
-			fmt.Printf("Error Connecting to Server: %s server %s\n", err, gCfg.Server)
-			t.Fatalf("Setup incorrect - failed to connect to server")
-			return
-		}
-
-		godebug.Printf(db02, "%sSuccessfully connected to Geth: AT: %s%s\n", MiscLib.ColorGreen, godebug.LF(), MiscLib.ColorReset)
+	buf, err = ioutil.ReadFile(fn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "FAIL: Setup incorrect - unable to read %s: Error: %s", fn, err)
+		return
 	}
-	return
-}
 
-const db01 = false
-const db02 = true
+	err = json.Unmarshal(buf, &gCfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "FAIL: Setup incorrect - Unable to parse %s: data -->>%s<<-- Error: %s", fn, buf, err)
+		return
+	}
+
+	// use that to setup connection to Geth
+	client, err = rpc.Dial(gCfg.GethConnectionString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "FAIL: Error Connecting to Geth Server: %s server %s\n", err, gCfg.GethConnectionString)
+		return
+	}
+
+	os.Exit(m.Run())
+}
