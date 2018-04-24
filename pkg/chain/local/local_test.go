77c7666dc046d0c2fc5e714fed17a288698d756a
Philip Schlump <philip.schlump@keep.network>
2018-04-24 14:17:08 -0600

    Tests in parallel with timeout to catch indefinit wait

--- pkg/chain/local/local_test.go
+++ pkg/chain/local/local_test.go
@@ -5,34 +5,64 @@ import (
 	"time"
 )
 
-func Test_Chain01(t *testing.T) {
+func TestLocalBlockWaiter(t *testing.T) {
+
+	t.Parallel()
+
+	var tests = map[string]struct {
+		wait         int
+		want         time.Duration
+		errorMessage string
+	}{
+		"does wait for a block": {
+			wait:         1,
+			want:         time.Duration(100000000),
+			errorMessage: "failed to wait for a single block",
+		},
+		"waited for a longer time": {
+			wait:         2,
+			want:         time.Duration(200000000),
+			errorMessage: "failed to wait for 2 blocks",
+		},
+		"doesn't wait if 0 blocks": {
+			wait:         0,
+			want:         time.Duration(1000),
+			errorMessage: "some errror message here...not understanding this case",
+		},
+		"invalid value": {
+			wait:         -1,
+			want:         time.Duration(0),
+			errorMessage: "Waiting for a time when it should have errored",
+		},
+	}
+
+	var e chan interface{}
+
+	tim := 15 // Force test to fail if not completed in 15 seconds.
+	tick := time.NewTimer(time.Duration(tim) * time.Second)
+
+	go func() {
+		select {
+		case e <- tick:
+			t.Fatal("Test ran too long - it failed")
+		}
+	}()
+
 	countWait := BlockCounter()
 
-	t.Log("Before Wait")
-	start := time.Now()
-	countWait.WaitForBlocks(3)
-	tm := time.Now()
-	elapsed := tm.Sub(start)
-	if elapsed < 1400000000 {
-		t.Fatalf("Did not wait\n")
-	}
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
 
-	t.Logf("After Wait, %d\n", elapsed)
+			start := time.Now().UTC()
+			countWait.WaitForBlocks(test.wait)
+			end := time.Now().UTC()
 
-	start = time.Now()
-	countWait.WaitForBlocks(5)
-	tm = time.Now()
-	elapsed = tm.Sub(start)
-	if elapsed < 2400000000 {
-		t.Fatalf("Did not wait\n")
+			elapsed := end.Sub(start)
+			if elapsed < test.want {
+				t.Error(test.errorMessage)
+			}
+		})
 	}
+	tick.Stop()
 
-	t.Logf("Before test #3 , %d\n", elapsed)
-	start = time.Now()
-	countWait.WaitForBlocks(0)
-	tm = time.Now()
-	elapsed = tm.Sub(start)
-	if elapsed < 10 {
-		t.Fatalf("Did not wait\n")
-	}
 }
