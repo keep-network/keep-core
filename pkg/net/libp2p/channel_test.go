90371c4b73cb0e3fe8ab2ac7dcd6ccbe2dadc7f9
Raghav Gulati <raghav.gulati@keep.network>
2018-05-07 13:24:29 -0700

    Add test cases for RegisterIdentifier.
    First round of tests, this grouping focused on the register
    identifier functionality.

--- pkg/net/libp2p/channel_test.go
+++ pkg/net/libp2p/channel_test.go
@@ -2,11 +2,13 @@ package libp2p
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/keep-network/keep-core/pkg/net"
+	peer "github.com/libp2p/go-libp2p-peer"
 )
 
 func TestMain(m *testing.M) {
@@ -23,25 +25,71 @@ func newTestContext() (context.Context, context.CancelFunc) {
 func TestRegisterIdentifier(t *testing.T) {
 	t.Parallel()
 
-	ctx, cancel := newTestContext()
+	_, cancel := newTestContext()
 	defer cancel()
 
+	var (
+		ch        = &channel{}
+		peerId    = peerIdentifier(peer.ID(""))
+		testProto = &testProtocolIdentifier{}
+	)
+
 	tests := map[string]struct {
-		tIdentifier net.TransportIdentifier
-		pIdentifier net.ProtocolIdentifier
-		errorString string
+		transportIdentifier net.TransportIdentifier
+		protocolIdentifier  net.ProtocolIdentifier
+		tMap                map[net.TransportIdentifier]net.ProtocolIdentifier
+		pMap                map[net.ProtocolIdentifier]net.TransportIdentifier
+		errorString         string
 	}{
-		"invalid transport identifier":        {},
-		"transport identifier already exists": {},
-		"protocol identifier already exists":  {},
-		"new, valid transport identifier":     {},
+		"invalid transport identifier": {
+			transportIdentifier: &testTransportIdentifier{},
+			protocolIdentifier:  nil,
+			tMap:                nil,
+			pMap:                nil,
+			errorString:         fmt.Sprintf("incorrect type for transportIdentifier: [%v]", &testTransportIdentifier{}),
+		},
+		"protocol identifier already exists": {
+			transportIdentifier: peerId,
+			protocolIdentifier:  testProto,
+			tMap: map[net.TransportIdentifier]net.ProtocolIdentifier{
+				&testTransportIdentifier{}: testProto,
+			},
+			pMap:        nil,
+			errorString: fmt.Sprintf("already have a protocol identifier in channel [%v] associated with [%v]", ch, peerId),
+		},
+		"transport identifier already exists": {
+			transportIdentifier: peerId,
+			protocolIdentifier:  testProto,
+			tMap:                nil,
+			pMap: map[net.ProtocolIdentifier]net.TransportIdentifier{
+				testProto: &testTransportIdentifier{},
+			},
+			errorString: fmt.Sprintf("already have a transport identifier in channel [%v] associated with [%v]", ch, testProto),
+		},
 	}
 
 	for name, tt := range tests {
+		if tt.tMap != nil {
+			ch.transportToProtoIdentifiers = tt.tMap
+		} else {
+			ch.transportToProtoIdentifiers = make(map[net.TransportIdentifier]net.ProtocolIdentifier)
+		}
+		if tt.pMap != nil {
+			ch.protoToTransportIdentifiers = tt.pMap
+		} else {
+			ch.protoToTransportIdentifiers = make(map[net.ProtocolIdentifier]net.TransportIdentifier)
+		}
+		err := ch.RegisterIdentifier(tt.transportIdentifier, tt.protocolIdentifier)
+		if err != nil && tt.errorString != err.Error() {
+			t.Errorf("test case: %s\n unexpected error: %v\n expected: %v", name, err, tt.errorString)
+		}
 	}
 }
 
-type testProtocolIdentifier interface{}
-type testTransportIdentifier interface{}
+type testProtocolIdentifier struct{}
+type testTransportIdentifier struct{}
 
-func (t testIdentifier) ProviderName() string
+var _ net.TransportIdentifier = (*testTransportIdentifier)(nil)
+var _ net.ProtocolIdentifier = (*testProtocolIdentifier)(nil)
+
+func (t *testTransportIdentifier) ProviderName() string { return "" }
