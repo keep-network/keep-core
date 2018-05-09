1a3aa89e16c280b2d6d5a7c8e1237661b3cd1cd1
Antonio Salazar Cardozo <antonio@keep.network>
2018-05-09 15:07:05 -0400

    Report expected/elapsed times in test failure messages

--- pkg/chain/local/local_test.go
+++ pkg/chain/local/local_test.go
@@ -16,22 +16,22 @@ func TestLocalBlockWaiter(t *testing.T) {
 		"does wait for a block": {
 			blockWait:    1,
 			expectation:  time.Duration(600) * time.Millisecond,
-			errorMessage: "Failed to wait for a single block",
+			errorMessage: "Failed to wait for a single block; expected %s but took %s.",
 		},
 		"waited for a longer time": {
 			blockWait:    2,
 			expectation:  time.Duration(600*2) * time.Millisecond,
-			errorMessage: "Failed to wait for 2 blocks",
+			errorMessage: "Failed to wait for 2 blocks; expected %s but took %s.",
 		},
 		"doesn't wait if 0 blocks": {
 			blockWait:    0,
 			expectation:  time.Duration(10) * time.Microsecond,
-			errorMessage: "Failed for a 0 block wait",
+			errorMessage: "Failed for a 0 block wait; expected %s but took %s.",
 		},
 		"invalid value": {
 			blockWait:    -1,
 			expectation:  time.Duration(10) * time.Microsecond,
-			errorMessage: "Waiting for a time when it should have errored",
+			errorMessage: "Waiting for a time when it should have errored; expected %s but took %s.",
 		},
 	}
 
@@ -45,7 +45,7 @@ func TestLocalBlockWaiter(t *testing.T) {
 
 			elapsed := end.Sub(start)
 			if elapsed < test.expectation {
-				t.Error(test.errorMessage)
+				t.Errorf(test.errorMessage, test.expectation, elapsed)
 			}
 		})
 	}
