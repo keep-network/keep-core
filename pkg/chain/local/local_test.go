dbf710dfdf36babd8bca0e57de91f6e6ae66d9ac
Philip Schlump <philip.schlump@keep.network>
2018-05-09 09:01:07 -0600

    Improved names of variables
    Cleanup improving names of variables.

--- pkg/chain/local/local_test.go
+++ pkg/chain/local/local_test.go
@@ -9,28 +9,28 @@ func TestLocalBlockWaiter(t *testing.T) {
 	t.Parallel()
 
 	var tests = map[string]struct {
-		wait         int
-		want         time.Duration
+		blockWait    int
+		expectation  time.Duration
 		errorMessage string
 	}{
 		"does wait for a block": {
-			wait:         1,
-			want:         time.Duration(100000000),
+			blockWait:    1,
+			expectation:  time.Duration(100000000),
 			errorMessage: "Failed to wait for a single block",
 		},
 		"waited for a longer time": {
-			wait:         2,
-			want:         time.Duration(200000000),
+			blockWait:    2,
+			expectation:  time.Duration(200000000),
 			errorMessage: "Failed to wait for 2 blocks",
 		},
 		"doesn't wait if 0 blocks": {
-			wait:         0,
-			want:         time.Duration(1000),
+			blockWait:    0,
+			expectation:  time.Duration(1000),
 			errorMessage: "Failed for a 0 block wait",
 		},
 		"invalid value": {
-			wait:         -1,
-			want:         time.Duration(0),
+			blockWait:    -1,
+			expectation:  time.Duration(0),
 			errorMessage: "Waiting for a time when it should have errored",
 		},
 	}
@@ -52,11 +52,11 @@ func TestLocalBlockWaiter(t *testing.T) {
 	for testName, test := range tests {
 		t.Run(testName, func(t *testing.T) {
 			start := time.Now().UTC()
-			countWait.WaitForBlocks(test.wait)
+			countWait.WaitForBlocks(test.blockWait)
 			end := time.Now().UTC()
 
 			elapsed := end.Sub(start)
-			if elapsed < test.want {
+			if elapsed < test.expectation {
 				t.Error(test.errorMessage)
 			}
 		})
