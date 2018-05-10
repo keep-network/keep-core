a55e00600cb5b4412f147d5695475c4971c01c8c
Raghav Gulati <raghav.gulati@keep.network>
2018-05-10 08:44:17 -0700

    Introduce identity focused functions
    Move peer identifier out of channel, make it a struct, and give it
    supporting functions (to go between the identifier and other libp2p
    methods).

--- pkg/net/libp2p/channel.go
+++ pkg/net/libp2p/channel.go
@@ -6,15 +6,7 @@ import (
 
 	"github.com/keep-network/keep-core/pkg/net"
 	floodsub "github.com/libp2p/go-floodsub"
-	peer "github.com/libp2p/go-libp2p-peer"
 )
-
-// TODO: if it's absolutely necessary to have a pk/sk, make this a struct
-type peerIdentifier peer.ID
-
-func (n peerIdentifier) ProviderName() string {
-	return "libp2p"
-}
 
 type channel struct {
 	name string
@@ -41,7 +33,6 @@ func (c *channel) SendTo(
 	recipientIdentifier interface{},
 	message net.TaggedMarshaler,
 ) error {
-	// TODO: do a check between the registered protocol identifier and the network ident
 	return nil
 }
 
