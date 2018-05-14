bd21e69abbfd8ed3de9e8be3e1aa2de9232816aa
Philip Schlump <philip.schlump@keep.network>
2018-05-14 08:53:17 -0600

    Better use of variable
    In an `if a := funcCall(); a {` construct in go you can reuse the
    variable `a` in an if-else.   This change takes advantage of
    the go capability.

--- config_test.go
+++ config_test.go
@@ -12,9 +12,9 @@ func TestReadConfig(t *testing.T) {
 		t.Errorf("Error: Did not correctly read in ./test/config.toml, Expected [%s] Got [%s]\n", expectedURL, cfg.Ethereum.URL)
 	}
 	expectedAddress := "0x639deb0dd975af8e4cc91fe9053a37e4faf37649"
-	if _, ok := cfg.Ethereum.ContractAddresses["KeepRandomBeacon"]; !ok {
+	if vv, ok := cfg.Ethereum.ContractAddresses["KeepRandomBeacon"]; !ok {
 		t.Errorf("Error: Did not correctly read in ./test/config.toml, expecte to have a key in the map for [KeepRandomBeacon].  Did not find one.\n")
-	} else if vv, ok2 := cfg.Ethereum.ContractAddresses["KeepRandomBeacon"]; ok2 && vv != expectedAddress {
+	} else if vv != expectedAddress {
 		t.Errorf("Error: Did not correctly read in ./test/config.toml, for KeepRandomBeacon expecte to have an address of [%s] got [%s]\n", expectedAddress, vv)
 	}
 }
