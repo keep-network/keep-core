c96ffce1dbe838b4e0caa750de3d166508aa7952
l3x <l3x@users.noreply.github.com>
2018-05-21 12:44:10 -0400

    Move client configuration into main

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/dfinity/go-dfinity-crypto/bls"
 	"github.com/keep-network/keep-core/cmd/keep"
+	"github.com/urfave/cli"
 )
 
 var (
@@ -13,8 +18,31 @@ var (
 	Revision string
 )
 
+//TODO: Remove init when build process is ready to populate Version and Revision
+func init() {
+	Version = "0.0.1"
+	Revision = "deadbeef"
+}
+
 func main() {
 	bls.Init(bls.CurveSNARK1)
 
-	keep.ClientApp(Version, Revision)
+	app := cli.NewApp()
+	app.Name = "keep-client"
+	app.Version = fmt.Sprintf("%s (revision %s)", Version, Revision)
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Keep Authors",
+			Email: "noreply@example.com",
+		},
+	}
+	app.Copyright = "(c) 2018 Thesis, Inc."
+	app.HelpName = "keep-client"
+	app.Usage = "The Keep Client Application"
+	app.Commands = keep.KeepCommands
+	app.Action = func(c *cli.Context) error {
+		return nil
+	}
+	app.Run(os.Args)
 }
