23f47f71d807e9d1bdb97e81ccb09eb49e2f4e1b
Matt Luongo <mhluongo@gmail.com>
2018-05-26 14:18:16 -0700

    Test g1HashToPoint and fix a missing mod.

--- contracts/solidity/migrations/2_deploy_contracts.js
+++ contracts/solidity/migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const KeepToken = artifacts.require("./KeepToken.sol");
 const ModUtils = artifacts.require("./utils/ModUtils.sol");
+const AltBn128 = artifacts.require("./AltBn128.sol");
 const StakingProxy = artifacts.require("./StakingProxy.sol");
 const TokenStaking = artifacts.require("./TokenStaking.sol");
 const TokenGrant = artifacts.require("./TokenGrant.sol");
@@ -10,11 +11,12 @@ const minPayment = 1;
 const minStake = 1;
 
 module.exports = function(deployer) {
+  deployer.deploy(ModUtils);
+  deployer.link(ModUtils, AltBn128);
+  deployer.deploy(AltBn128);
   deployer.deploy(KeepToken)
     .then(function() {
       return deployer.deploy(StakingProxy);
-    }).then(function() {
-      return deployer.deploy(ModUtils);
     }).then(function() {
       return deployer.deploy(TokenStaking, KeepToken.address, StakingProxy.address, withdrawalDelay);
     }).then(function() {
