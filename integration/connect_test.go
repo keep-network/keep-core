ee62998ecd9d0421cf31bd6388685096ee680bc8
Philip Schlump <philip.schlump@keep.network>
2018-05-29 16:23:46 -0600

    Move code around to support integration test
    Code that was formerly part of the main is now
    in its own package, "cofnig" so that it can be
    reused in integration testing.   Test cases
    have been split into 2 parts and an integration
    test case has been added.

---
+++ integration/connect_test.go
@@ -0,0 +1,46 @@
+package integration
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/keep-network/keep-core/pkg/chain/ethereum"
+	"github.com/keep-network/keep-core/pkg/config"
+)
+
+var client *ethereum.Provider
+
+func TestConnectToServer(t *testing.T) {
+	// do some testing using the client connection
+	// xyzzy - use JSON rpc to pull back version
+}
+
+var (
+	integration = flag.Bool("integration", false, "run Geth integration tests")
+	configFile  = flag.String("config", "test/config.toml", "Config file to specify connection to ethereum")
+)
+
+func TestMain(m *testing.M) {
+
+	flag.Parse()
+
+	os.Setenv("KEEP_ETHEREUM_PASSWORD", "not-my-password")
+
+	Config, err := config.ReadConfig(*configFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "FAIL: Error reading configuration: %s\n", err)
+		os.Exit(1)
+	}
+
+	if *integration {
+		client, err = ethereum.Connect(Config.Ethereum)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "FAIL: Failed to connect to Ethereum: %s\n", err)
+			os.Exit(1)
+		}
+
+		os.Exit(m.Run())
+	}
+}
--- pkg/chain/ethereum/connect.go
+++ pkg/chain/ethereum/connect.go
@@ -3,26 +3,36 @@ package ethereum
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-type provider struct {
-	Config Config
-	Client *rpc.Client
+// Provider contains the connections to the ethereum server.
+type Provider struct {
+	Config    Config
+	Client    *ethclient.Client // connection with ws:// or .ipc for event filering calls
+	ClientRPC *rpc.Client       // JSON RPC connection
 }
 
 // Connect makes the network connection to the Ethereum network.  Note: for other things
 // to work correctly the configuration will need to reference a websocket, "ws://", or
 // local IPC connection.
-func Connect(cfg Config) (*provider, error) {
-	client, err := rpc.Dial(cfg.URL)
+func Connect(cfg Config) (*Provider, error) {
+	client, err := ethclient.Dial(cfg.URL)
 	if err != nil {
 		return nil, fmt.Errorf("error Connecting to Geth Server: %s server %s",
 			err, cfg.URL)
 	}
 
-	return &provider{
-		Config: cfg,
-		Client: client,
+	clientrpc, err := rpc.Dial(cfg.URLRPC)
+	if err != nil {
+		return nil, fmt.Errorf("error Connecting to Geth Server: %s server %s",
+			err, cfg.URL)
+	}
+
+	return &Provider{
+		Config:    cfg,
+		Client:    client,
+		ClientRPC: clientrpc,
 	}, nil
 }
