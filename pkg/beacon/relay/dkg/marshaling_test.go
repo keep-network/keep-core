711500d378d74d9f333f6c1fd7eeec1b69acb274
Antonio Salazar Cardozo <antonio@keep.network>
2018-05-31 15:14:06 -0400

    Adjust justifications marshaling test for clarity

--- pkg/beacon/relay/dkg/marshaling_test.go
+++ pkg/beacon/relay/dkg/marshaling_test.go
@@ -99,15 +99,14 @@ func TestAccusationsMessageRoundTrip(t *testing.T) {
 }
 
 func TestJustificationsMessageRoundTrip(t *testing.T) {
-	id := generateBlsID()
 	justifications := make(map[bls.ID]*bls.SecretKey)
 	for i := 0; i < 10; i++ {
-		id := generateBlsID()
+		justificationID := generateBlsID()
 		sk := generateBlsSecretKey()
-		justifications[*id] = sk
+		justifications[*justificationID] = sk
 	}
 
-	msg := &JustificationsMessage{id, justifications}
+	msg := &JustificationsMessage{generateBlsID(), justifications}
 	unmarshaled := &JustificationsMessage{}
 	err := roundTrip(msg, unmarshaled)
 	if err != nil {
