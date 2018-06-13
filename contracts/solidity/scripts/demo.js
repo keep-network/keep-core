9ddac39aca495e98f6e7ed2a4d42ed4acd505b70
Nik G <subzeromedia@gmail.com>
2018-06-13 13:37:44 -0400

    Add a few more demo data items

--- contracts/solidity/scripts/demo.js
+++ contracts/solidity/scripts/demo.js
@@ -3,6 +3,10 @@ const StakingProxy = artifacts.require("./StakingProxy.sol");
 const TokenStaking = artifacts.require("./TokenStaking.sol");
 const TokenGrant = artifacts.require("./TokenGrant.sol");
 
+function formatAmount(amount, decimals) {
+  return amount * (10 ** decimals)
+}
+
 function getAccounts() {
   return new Promise((resolve, reject) => {
       web3.eth.getAccounts((error, accounts) => {
@@ -28,14 +32,25 @@ module.exports = async function() {
   }
 
   // Stake tokens
-  await token.approveAndCall(TokenStaking.address, 1000000, "");
+  await token.approveAndCall(tokenStaking.address, formatAmount(1000000, 18), "");
 
-  // Grant tokens to a different account
-  let amount = 1000000000;
+  // Send tokens to the second account
+  await token.transfer(accounts[1], formatAmount(1000000, 18));
+
+  // Stake tokens as a second account
+  await token.approveAndCall(tokenStaking.address, formatAmount(70000, 18), "", {from: accounts[1]});
+
+  // Grant tokens to the second account
+  let amount = formatAmount(70000, 18);
   let vestingDuration = 86400*60;
   let start = web3.eth.getBlock('latest').timestamp;
   let cliff = 86400*10;
   let revocable = true;
   await token.approve(tokenGrant.address, amount);
   await tokenGrant.grant(amount, accounts[1], vestingDuration, start, cliff, revocable);
+
+  // Grant tokens from the second account
+  amount = formatAmount(1000, 18);
+  await token.approve(tokenGrant.address, amount, {from: accounts[1]});
+  await tokenGrant.grant(amount, accounts[0], vestingDuration, start, cliff, revocable, {from: accounts[1]});
 };
