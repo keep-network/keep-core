537173261c7ea88613f06ec1f170b0c702ae73e0
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-06-13 17:20:08 -0400

    Added initial tests for Promises

--- pkg/callback/promise_test.go
+++ pkg/callback/promise_test.go
@@ -2,28 +2,67 @@ package promise
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
-func TestPromise2(t *testing.T) {
-	done := make(chan bool)
+func TestPromiseOnSuccessFulfill(t *testing.T) {
+	done := make(chan interface{})
+
+	expectedResult := "batman"
 
 	promise := newPromise()
 
 	promise.onSuccess(func(in interface{}) {
-		fmt.Printf("Got %v\n", in)
-		done <- true
+		done <- in
 	})
 
-	promise.onFailure(func(err error) {
-		fmt.Printf("Got error %v\n", err)
-		done <- true
-	})
+	err := promise.fulfill(expectedResult)
 
-	err := promise.fulfill("dupa")
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	result := <-done
+	if result != expectedResult {
+		t.Errorf("Unexpected value passed to callback\nExpected: %v\nActual:%v\n", expectedResult, result)
+	}
+}
+
+func TestPromiseAlreadyCompleted(t *testing.T) {
+	done := make(chan bool)
+
+	var tests = map[string]struct {
+		function      func() error
+		expectedError error
+	}{
+		"Fulfill with result `promise already completed`": {
+			function: func() error {
+				promise := newPromise().onSuccess(func(in interface{}) { done <- true })
+				promise.fulfill(nil)
+				return promise.fulfill(nil)
+			},
+			expectedError: fmt.Errorf("promise already completed"),
+		},
+		"Fail with result `promise already completed`": {
+			function: func() error {
+				promise := newPromise().onFailure(func(error) { done <- true })
+				promise.fail(nil)
+				return promise.fail(nil)
+			},
+			expectedError: fmt.Errorf("promise already completed"),
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+
+			error := test.function()
+			if !reflect.DeepEqual(test.expectedError, error) {
+				t.Fatalf("Errors don't match\nExpected: %v\nActual: %v\n", test.expectedError, error)
+			}
+
+		})
+	}
 	<-done
 }
