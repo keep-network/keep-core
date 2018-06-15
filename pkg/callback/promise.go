23c385c62ed0f87c935db688993eac41e43e1546
Piotr Dyraga <piotr.dyraga@akena.engineering>
2018-06-15 15:02:02 -0400

    Call the installed callback for an already evaluated Promise
    If the promise has been already fulfilled or failed, we call the
    callback being installed on the Promise immediatelly. Previously,
    we had to install the callback before calling `Fulfill` or `Fail`.
    Otherwise, callback was not getting executed. Now the order does not
    matter.

--- pkg/callback/promise.go
+++ pkg/callback/promise.go
@@ -15,29 +15,43 @@ type Promise struct {
 	completeFn func(interface{}, error)
 
 	isComplete bool
+	value      interface{}
+	err        error
 }
 
 // OnSuccess registers a function to be called when the Promise
 // has been fulfilled. In case of a failed Promise, function is not
 // called at all. OnSuccess is a non-blocking operation. Only one on success
-// function can be registered for a Promise.
+// function can be registered for a Promise. If the Promise has been already
+// fulfilled, the function is called immediatelly.
 func (p *Promise) OnSuccess(onSuccess func(interface{})) *Promise {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
 	p.successFn = onSuccess
+
+	if p.isComplete {
+		p.callSuccessFn()
+	}
+
 	return p
 }
 
 // OnFailure registers a function to be called when the Promise
 // execution failed. In case of a fulfilled Promise, function is not
 // called at all. OnFailure is a non-blocking operation. Only one on failure
-// function can be registered for a Promise.
+// function can be registered for a Promise. If the Promise has already failed,
+// the function is called immediatelly.
 func (p *Promise) OnFailure(onFailure func(error)) *Promise {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
 	p.failureFn = onFailure
+
+	if p.isComplete {
+		p.callFailureFn()
+	}
+
 	return p
 }
 
@@ -47,12 +61,18 @@ func (p *Promise) OnFailure(onFailure func(error)) *Promise {
 // function is nil. In case of a failed execution, there is no
 // value evaluated so the value parameter is nil. OnComplete is
 // a non-blocking operation. Only one on complete function can be
-// registered for a Promise.
+// registered for a Promise. If the Promise has already completed,
+// the function is called immediatelly.
 func (p *Promise) OnComplete(onComplete func(interface{}, error)) *Promise {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
 	p.completeFn = onComplete
+
+	if p.isComplete {
+		p.callCompleteFn()
+	}
+
 	return p
 }
 
@@ -69,16 +89,10 @@ func (p *Promise) Fulfill(value interface{}) error {
 	}
 
 	p.isComplete = true
-	if p.successFn != nil {
-		go func() {
-			p.successFn(value)
-		}()
-	}
-	if p.completeFn != nil {
-		go func() {
-			p.completeFn(value, nil)
-		}()
-	}
+	p.value = value
+
+	p.callSuccessFn()
+	p.callCompleteFn()
 
 	return nil
 }
@@ -96,16 +110,34 @@ func (p *Promise) Fail(err error) error {
 	}
 
 	p.isComplete = true
-	if p.failureFn != nil {
-		go func() {
-			p.failureFn(err)
-		}()
-	}
-	if p.completeFn != nil {
-		go func() {
-			p.completeFn(nil, err)
-		}()
-	}
+	p.err = err
+
+	p.callFailureFn()
+	p.callCompleteFn()
 
 	return nil
+}
+
+func (p *Promise) callCompleteFn() {
+	if p.completeFn != nil {
+		go func() {
+			p.completeFn(p.value, p.err)
+		}()
+	}
+}
+
+func (p *Promise) callSuccessFn() {
+	if p.successFn != nil {
+		go func() {
+			p.successFn(p.value)
+		}()
+	}
+}
+
+func (p *Promise) callFailureFn() {
+	if p.failureFn != nil {
+		go func() {
+			p.failureFn(p.err)
+		}()
+	}
 }
