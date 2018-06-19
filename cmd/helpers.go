00fc11b820409bfa26d9c3a785dd7d8c2448a21e
l3x <l3x@users.noreply.github.com>
2018-06-19 14:17:08 -0400

    Remove libp2pMessage and PeerNetParams types from helpers

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -6,10 +6,6 @@ import (
 	"log"
 	"net"
 	"strings"
-
-	ci "github.com/libp2p/go-libp2p-crypto"
-	"github.com/libp2p/go-libp2p-peer"
-	ma "github.com/multiformats/go-multiaddr"
 )
 
 const HomeIPv4Address = "127.0.0.1"
@@ -72,33 +68,6 @@ func header(header string) {
 	fmt.Printf("\n%s\n%s\n%s\n", dashes, header, dashes)
 }
 
-type libp2pMessage struct {
-	//ID      *libp2p.Identity
-	Payload string
-}
-
-// Type of this message
-func (m *libp2pMessage) Type() string {
-	return "test/unmarshaler"
-}
-
-// Marshal this message
-func (m *libp2pMessage) Marshal() ([]byte, error) {
-	return json.Marshal(m)
-}
-
-// Unmarshal this message
-func (m *libp2pMessage) Unmarshal(bytes []byte) error {
-	var message libp2pMessage
-	if err := json.Unmarshal(bytes, &message); err != nil {
-		fmt.Println("hit this error")
-		return err
-	}
-	m.Payload = message.Payload
-
-	return nil
-}
-
 type testMessage struct {
 	Payload string
 }
@@ -123,15 +92,6 @@ func (m *testMessage) Unmarshal(bytes []byte) error {
 	m.Payload = message.Payload
 
 	return nil
-}
-
-// PeerNetParams is a struct to bundle together the four things
-// you need to run a connection with a peer: id, 2keys, and addr.
-type PeerNetParams struct {
-	ID      peer.ID
-	PrivKey ci.PrivKey
-	PubKey  ci.PubKey
-	Addr    ma.Multiaddr
 }
 
 // Closable wraps Close() method
