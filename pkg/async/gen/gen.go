121370289b289e87a8446d6edb71fa1d738d65e8
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-06-20 11:10:54 +0200

    Imports processing for generated Promise code
    Imports in generated Promise code are now resolved and organized.
    If package import is missing for any type in the code, it will be added
    automatically.

--- pkg/async/gen/gen.go
+++ pkg/async/gen/gen.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"html/template"
 	"log"
 	"os"
 	"path"
+
+	"golang.org/x/tools/imports"
 )
 
 // Promises code config.
@@ -24,6 +27,7 @@ type config struct {
 	// Type which Promise will handle
 	Type string
 	// Package of the `Type`
+	// Need to be provided only if package cannot be resolved by `imports.Process` function
 	PackagedType string
 	// Prefix for naming the promises
 	Prefix string
@@ -36,12 +40,12 @@ func main() {
 		// Promise for `string` type. There is a test for this promise named `string_promise_test.go`.
 		// We need a test to validate correctness of generated promises.
 		{
-			Type:         "string",
-			PackagedType: "",
-			Prefix:       "String",
-			outputFile:   "string_promise.go",
+			Type:       "string",
+			Prefix:     "String",
+			outputFile: "string_promise.go",
 		},
-		// Example for types from packages which need to be imported:
+		// Example for types from packages which need to be imported.
+		// Provide `PackagedType` only if package cannot be resolved by the `imports.Process`
 		// {
 		// 	Type:         "ethereum.KeepRandomBeacon",
 		// 	PackagedType: "github.com/keep-network/keep-core/pkg/chain/ethereum",
@@ -51,24 +55,42 @@ func main() {
 	}
 
 	for _, c := range configs {
-		// Read template
+		// Read a Promise's Template
 		t, err := template.New(promiseTemplate).ParseFiles(promiseTemplate)
 		if err != nil {
 			log.Fatalf("template creation failed [%v]", err)
 		}
 
-		// Create output file
-		outputPath := path.Join(outDir, c.outputFile)
-		f, err := os.Create(outputPath)
+		// Generate a Promise
+		var buf bytes.Buffer
+		err = t.Execute(&buf, c)
+		if err != nil {
+			log.Fatalf("generation failed [%v]", err)
+		}
+
+		organizeImports(&buf)
+
+		// Store the Promise in a file
+		f, err := os.Create(path.Join(outDir, c.outputFile))
 		defer f.Close()
 		if err != nil {
 			log.Fatalf("output file creation failed [%v]", err)
 		}
+		buf.WriteTo(f)
+	}
+}
 
-		// Generate files
-		err = t.Execute(f, c)
-		if err != nil {
-			log.Fatalf("generation failed [%v]", err)
-		}
+// Resolves imports in a code stored in a Buffer
+func organizeImports(buf *bytes.Buffer) {
+	code, err := imports.Process(outDir, buf.Bytes(), nil)
+	if err != nil {
+		log.Fatalf("failed to find/resove imports [%v]\nCode: %s", err, code)
+	}
+
+	// Write organized code to the buffer
+	buf.Reset()
+	length, err := buf.Write(code)
+	if err != nil {
+		log.Fatalf("cannot write code to buffer [%v]\nCode length: %d\nCode: %s", err, length, code)
 	}
 }
