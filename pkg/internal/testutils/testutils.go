53ed7f2c804feada7fc0c0d11bd316652102bd13
Antonio Salazar Cardozo <antonio@keep.network>
2018-06-27 10:36:36 -0400

    Fix and refactor testutils.AssertBytesEqual
    Specifically, AssertBytesEqual now calls an unexported helper, and tests
    are introduced on that unexported helper to verify the variations on the
    slice equality check. There was a mixup on length comparisons for the
    expected and actual bytes that resulted in weird behavior.

--- pkg/internal/testutils/testutils.go
+++ pkg/internal/testutils/testutils.go
@@ -2,32 +2,45 @@
 // consistency in output style.
 package testutils
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // AssertBytesEqual takes a testing.T and two byte slices and reports an error
 // if the two bytes are not equal.
 func AssertBytesEqual(t *testing.T, expectedBytes []byte, actualBytes []byte) {
-	maxLen := len(expectedBytes)
+	err := testBytesEqual(expectedBytes, actualBytes)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testBytesEqual(expectedBytes []byte, actualBytes []byte) error {
+	minLen := len(expectedBytes)
 	diffCount := 0
-	if actualLen := len(actualBytes); actualLen > maxLen {
-		diffCount = maxLen - actualLen
-		maxLen = actualLen
+	if actualLen := len(actualBytes); actualLen < minLen {
+		diffCount = minLen - actualLen
+		minLen = actualLen
 	} else {
-		diffCount = actualLen - maxLen
+		diffCount = actualLen - minLen
 	}
 
-	for i := 0; i < maxLen; i++ {
+	for i := 0; i < minLen; i++ {
 		if expectedBytes[i] != actualBytes[i] {
 			diffCount++
 		}
 	}
 
 	if diffCount != 0 {
-		t.Errorf(
+		return fmt.Errorf(
 			"Byte slices differ in %v places\nexpected: [%v]\nactual:   [%v]",
 			diffCount,
 			expectedBytes,
 			actualBytes,
 		)
 	}
+
+	return nil
 }
