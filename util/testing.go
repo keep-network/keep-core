75ce67b72b7d167fc030c0c7bcf082f445d4417d
l3x <l3x@users.noreply.github.com>
2018-06-26 23:06:23 -0400

    Update testing util to use wrapped errors

--- util/testing.go
+++ util/testing.go
@@ -15,21 +15,25 @@ func Assert(tb testing.TB, condition bool, message string, got ...interface{}) {
 }
 
 // Ok fails when err is not nil
-func Ok(tb testing.TB, err error) {
+func Ok(tb testing.TB, errWrap ErrWrap) {
 	tb.Helper()
-	if err != nil {
-		tb.Fatalf(redMsg("unexpected error: %v"), err)
+	if errWrap.Err != nil {
+		tb.Fatalf(redMsg("unexpected error: %v"), errWrap)
 	}
 }
 
 // NotOk fails when err is nil
-func NotOk(tb testing.TB, err error, message string, got ...interface{}) {
+// util.NotOk(t, err, "input=%v", test.input)
+func NotOk(tb testing.TB, errWrap ErrWrap, errType int, msgFormat string, msgArgs ...interface{}) {
 	tb.Helper()
-	if err == nil {
-		if len(got) != 0 {
-			tb.Fatalf(redMsg("expected error: "+message+", got none"), got...)
+
+	if errWrap.Err == nil {
+		if len(msgArgs) != 0 {
+			tb.Fatalf(redMsg("expected error where: "+msgFormat+", got none"), msgArgs...)
 		}
 		tb.Fatalf(redMsg("expected error, got none"))
+	} else if errWrap.ErrNo != errType {
+		tb.Fatalf(redMsg(fmt.Sprintf("expected error (%d) got (%d): %v", errType, errWrap.ErrorNumber(), errWrap.Err.Error())))
 	}
 }
 
