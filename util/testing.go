5ab6f435dbef465887cb1378cdf6aafd04d281da
l3x <l3x@users.noreply.github.com>
2018-07-01 21:02:17 -0400

    Remove function that printed (red) ansi characters

--- util/testing.go
+++ util/testing.go
@@ -4,40 +4,46 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strings"
 	"syscall"
 	"testing"
 )
 
-// Ok fails when err is not nil
+// Ok fails when err is not nil.
 func Ok(tb testing.TB, err error) {
 	tb.Helper()
 	if err != nil {
-		tb.Fatalf(redMsg("unexpected error: %v"), err)
+		if strings.Count(err.Error(), "\n") > 0 {
+			tb.Fatalf("unexpected errors:\n%v", err)
+
+		} else {
+			tb.Fatalf("unexpected error: %v", err)
+		}
 	}
 }
 
-// NotOk fails when err is nil
+// NotOk fails when err is nil.
 func NotOk(tb testing.TB, err error, msgFormat string, msgArgs ...interface{}) {
 	tb.Helper()
 
 	if err == nil {
 		if len(msgArgs) != 0 {
-			tb.Fatalf(redMsg("expected error where: "+msgFormat+", got none"), msgArgs...)
+			tb.Fatalf("expected error where: "+msgFormat+", got none", msgArgs...)
 		} else {
-			tb.Fatalf(redMsg("expected error, got none"))
+			tb.Fatalf("expected error, got none")
 		}
 	}
 }
 
-// NotOkRead fails when err is nil or when the error is an runtime error due to file not found
+// NotOkRead fails when err is nil or when the error is an runtime error due to file not found.
 func NotOkRead(tb testing.TB, err error, msgFormat string, msgArgs ...interface{}) {
 	tb.Helper()
 
 	if err == nil {
 		if len(msgArgs) != 0 {
-			tb.Fatalf(redMsg("expected error where: "+msgFormat+", got none"), msgArgs...)
+			tb.Fatalf("expected error where: "+msgFormat+", got none", msgArgs...)
 		} else {
-			tb.Fatalf(redMsg("expected error, got none"))
+			tb.Fatalf("expected error, got none")
 		}
 	} else {
 		// Report unexpected error if caused by failure to read a file
@@ -48,30 +54,26 @@ func NotOkRead(tb testing.TB, err error, msgFormat string, msgArgs ...interface{
 				if err, ok := err.(*os.PathError); ok {
 					errMsg = fmt.Sprintf("file at path (%s) failed to open", err.Path)
 				}
-				tb.Fatalf(redMsg(errMsg))
+				tb.Fatalf(errMsg)
 			default:
-				tb.Fatalf(redMsg(fmt.Sprintf("got unknown error: %v", err)))
+				tb.Fatalf(fmt.Sprintf("got unknown error: %v", err))
 			}
 		}
 	}
 }
 
-// Assert fails when condition is false
+// Assert fails when condition is false.
 func Assert(tb testing.TB, condition bool, message string, got ...interface{}) {
 	tb.Helper()
 	if !condition {
-		tb.Fatalf(redMsg(message), got...)
+		tb.Fatalf(message, got...)
 	}
 }
 
-// Equals fails when expected is not equal to got
+// Equals fails when expected value is not equal to the got value.
 func Equals(tb testing.TB, expected, got interface{}) {
 	tb.Helper()
 	if !reflect.DeepEqual(expected, got) {
-		tb.Fatalf(redMsg("expected: %#v\n\ngot: %#v"), expected, got)
+		tb.Fatalf("expected: %#v\n\ngot: %#v", expected, got)
 	}
-}
-
-func redMsg(msg string) string {
-	return fmt.Sprintf("\033[31m%s\033[39m\n", msg)
 }
