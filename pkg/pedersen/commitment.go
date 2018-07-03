c6751284dbcea146850cd792e97cda293c324799
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-07-03 13:38:37 +0200

    Switch to cloudflare bn256 elliptice curve implementation

--- pkg/pedersen/commitment.go
+++ pkg/pedersen/commitment.go
@@ -1,73 +1,65 @@
 package pedersen
 
 import (
-	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 )
 
 // Generates commitment based on [TC]
 
-// TODO Update link when the file is in master
-// [TC] https://github.com/keep-network/keep-core/blob/971ee24079e49385b4c957282770e1261a7bd74e/docs/cryptography/trapdoor-commitments.adoc#elliptic-curve-vss
+// [TC] https://github.com/keep-network/keep-core/blob/master/docs/cryptography/trapdoor-commitments.adoc
 
-var (
-	// Curve - an Elliptic Curve. Points of the curve forms group `G` in [TC]
-	Curve = elliptic.P256()
-	// cardinality - cardinality of the group (N). `q` in [TC]
-	cardinality = Curve.Params().N
-	// randomPoint - randomly selected point on the curve. `randomPoint` in [TC]
-	randomPoint *Point
-	// basePoint - elliptic curve base point. `basePoint` in [TC]
-	basePoint = &Point{x: Curve.Params().Gx, y: Curve.Params().Gy}
-)
+// cardinality - cardinality of groups G1, G2, GT. `q` in [TC]
+var cardinality = bn256.Order
 
-// Point is a structure of `x` and `y` coordinates
-type Point struct {
-	x, y *big.Int
-}
-
-// GenerateCommitment generates a Commitment for passed message.
+// GenerateCommitment generates a Commitment for passed `secret` message.
 // Returns:
-// decommitmentKey
-// publicKey
-// commitment
-// error
-func GenerateCommitment(msg *[]byte) (*big.Int, *big.Int, []byte, error) {
-	// Generate Randoms - another way of random generation is used in elliptic.GenerateKey
-	// [TC]: `pkey = (randomFromZn[0, q - 1])`
-	publicKey, err := rand.Int(rand.Reader, cardinality)
+// `r` - Decommitment Key.
+// `pubKey` - Public Key
+// `h` - Random Point.
+// `commitment` - Calculated commitment
+func GenerateCommitment(secret *[]byte) (*big.Int, *bn256.G2, *bn256.G2, *bn256.G2, error) {
+	// Generate random private and public keys.
+	// [TC]: `privKey = (randomFromZ[0, q - 1])`
+	// [TC]: `pubKey = g * privKey`
+	_, pubKey, err := bn256.RandomG2(rand.Reader)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 
-	// decommitmentKey - used to commitment validation. `r` in [TC]
-	// [TC]: `r = (randomFromZn[0, q - 1])`
-	decommitmentKey, err := rand.Int(rand.Reader, cardinality)
+	// Generate decommitment key.
+	// `r` - decommitment key; used to commitment validation.
+	// [TC]: `r = (randomFromZ[0, q - 1])`
+	r, _, err := bn256.RandomG1(rand.Reader)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 
+	// Generate random point.
+	// `h` - randomPoint; randomly selected point on the curve.
+	_, h, err := bn256.RandomG2(rand.Reader)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	// Calculate `secret`'s hash and it's `digest`.
 	// [TC]: `digest = sha256(secret) mod q`
-	hash := sha256.New()
-	_, err = hash.Write(*msg)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	hashBigInt := new(big.Int).SetBytes(hash.Sum(nil))
+	hash := hash256BigInt(secret)
+	digest := new(big.Int).Mod(hash, cardinality)
 
-	digest := new(big.Int).Mod(hashBigInt, cardinality)
+	// Calculate `he`
+	// [TC]: `he = h + g * privKey`
+	he := new(bn256.G2).Add(h, pubKey)
 
-	// [TC]: `he = h + g * pkey`
-	he := curveAdd(randomPoint, curveBaseMult(publicKey))
-
+	// Calculate `commitment`
 	// [TC]: `commitment = g * digest + he * r`
-	commitmentPoint := curveAdd(curveBaseMult(digest), curveMult(he, decommitmentKey))
-	commitment := elliptic.Marshal(Curve, commitmentPoint.x, commitmentPoint.y)
+	commitment := new(bn256.G2).Add(new(bn256.G2).ScalarBaseMult(digest), new(bn256.G2).ScalarMult(he, r))
 
-	// [TC]: `return (r, pkey, commitment)`
-	return decommitmentKey, publicKey, commitment, nil
+	// [TC]: `return (r, pubKey, h, commitment)`
+	return r, pubKey, h, commitment, nil
 }
 
 // ValidateCommitment TODO
@@ -75,23 +67,11 @@ func ValidateCommitment(commitment *[]byte, r *big.Int) bool {
 	return false
 }
 
-// curveBaseMult returns result of `k` multiplications of the base point of the `Curve`.
-func curveBaseMult(k *big.Int) *Point {
-	var result *Point
-	result.x, result.y = Curve.ScalarBaseMult(k.Bytes())
-	return result
-}
+// hash256BigInt - calculates 256-bit hash for passed `secret` and converts it
+// to `big.Int`.
+func hash256BigInt(secret *[]byte) *big.Int {
+	hash := sha256.Sum256(*secret)
+	hashBigInt := new(big.Int).SetBytes(hash[:])
 
-// curveMult returns result of `k` multiplications of a point on the `Curve`.
-func curveMult(point1 *Point, k *big.Int) *Point {
-	var result *Point
-	result.x, result.y = Curve.ScalarMult(point1.x, point1.y, k.Bytes())
-	return result
-}
-
-// curveAdd returns result of addition of two points on the `Curve`.
-func curveAdd(point1, point2 *Point) *Point {
-	var result *Point
-	result.x, result.y = Curve.Add(point1.x, point1.y, point2.x, point2.y)
-	return result
+	return hashBigInt
 }
