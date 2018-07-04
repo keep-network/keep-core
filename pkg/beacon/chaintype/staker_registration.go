0bc4867858125ad917f17ae8757df748c0b2194b
Antonio Salazar Cardozo <antonio@keep.network>
2018-07-03 22:53:57 -0400

    Nuke chaintype package that sneaked back in

--- pkg/beacon/chaintype/staker_registration.go
+++
@@ -1,8 +0,0 @@
-package chaintype
-
-// StakerRegistration is the data for the OnStakerAdded event.  This type may
-// only be needed in Milestone 1 - it may change at Milestone 2.
-type StakerRegistration struct {
-	Index         int
-	GroupMemberID string
-}
