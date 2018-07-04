dbe4b6e95ddba2c5709c5c662aa28842453c1be2
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-07-04 14:37:40 +0200

    Reorganize commitments test map

--- pkg/tecdsa/commitment_test.go
+++ pkg/tecdsa/commitment_test.go
@@ -1,8 +1,6 @@
 package tecdsa
 
 import (
-	"encoding/base64"
-	"fmt"
 	"math/big"
 	"testing"
 
@@ -10,19 +8,74 @@ import (
 )
 
 func TestGenerateAndValidateCommitment(t *testing.T) {
-	msg := []byte("top secret message")
-
-	// Generate Commitment
-	commitment, secret, err := GenerateCommitment(&msg)
-	if err != nil {
-		t.Fatalf("generation error [%v]", err)
+	var tests = map[string]struct {
+		modifySecret     func(secret *Secret)
+		modifyCommitment func(commitment *Commitment)
+		expectedResult   bool
+	}{
+		"positive validation - pass values used for generation": {
+			modifySecret:     func(secret *Secret) {},
+			modifyCommitment: func(commitment *Commitment) {},
+			expectedResult:   true,
+		},
+		"negative validation - incorrect `secret`": {
+			modifySecret: func(secret *Secret) {
+				msg := []byte("top secret message2")
+				secret.secret = &msg
+			},
+			modifyCommitment: func(commitment *Commitment) {},
+			expectedResult:   false,
+		},
+		"negative validation - incorrect `r`": {
+			modifySecret: func(secret *Secret) {
+				secret.r = big.NewInt(3)
+			},
+			modifyCommitment: func(commitment *Commitment) {},
+			expectedResult:   false,
+		},
+		"negative validation - incorrect `commitment`": {
+			modifySecret: func(secret *Secret) {},
+			modifyCommitment: func(commitment *Commitment) {
+				commitment.commitment = new(bn256.G2).ScalarBaseMult(big.NewInt(3))
+			},
+			expectedResult: false,
+		},
+		"negative validation - incorrect `pubKey`": {
+			modifySecret:     func(secret *Secret) {},
+			modifyCommitment: func(commitment *Commitment) { commitment.pubKey = big.NewInt(3) },
+			expectedResult:   false,
+		},
+		"negative validation - incorrect `h`": {
+			modifySecret: func(secret *Secret) {},
+			modifyCommitment: func(commitment *Commitment) {
+				commitment.h = new(bn256.G2).ScalarBaseMult(big.NewInt(6))
+			},
+			expectedResult: false,
+		},
 	}
 
-	// Validate Commitment
-	result := ValidateCommitment(commitment, secret)
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			msg := []byte("top secret message")
 
-	if !result {
-		t.Fatalf("validation result: [%v]", result)
+			// Generate Commitment
+			commitment, secret, err := GenerateCommitment(&msg)
+			if err != nil {
+				t.Fatalf("generation error [%v]", err)
+			}
+
+			// Invoke modification functions defined in test
+			test.modifyCommitment(commitment)
+			test.modifySecret(secret)
+
+			// Validate Commitment
+			result := ValidateCommitment(commitment, secret)
+
+			// Check result
+			if result != test.expectedResult {
+				t.Fatalf("\nexpected: %v\nactual:   %v", test.expectedResult, result)
+			}
+		})
 	}
 }
 
@@ -60,118 +113,4 @@ func TestGenerateTwoCommitmentsCheckUniqueResults(t *testing.T) {
 	if commitment1.commitment.String() == commitment2.commitment.String() {
 		t.Fatal("both commitments are equal")
 	}
-}
-
-func TestValidate(t *testing.T) {
-	msg := []byte("top secret message")
-	h := new(bn256.G2).ScalarBaseMult(big.NewInt(5))
-	r := big.NewInt(2)
-	pubKey := big.NewInt(4)
-	commitment := convertBase64StringToCommitment("DZv5bcjEHmVfO+ukZUq7uLUKQhXTbE8mBkG4CTpt6bAiKnOXf2v6E3reMnW9qV0B5hjT9NPxaux6OQW5/vnw3iHJ2Gu8/icq3T7sKvoD+CXFDi8uYZv7pln7zyAI32OiGhjPPvBtZ/jtaIx0WTF58f1HVrQj1vWeQO43zFJCi38=")
-
-	msgInvalid := []byte("top secret message2")
-
-	var tests = map[string]struct {
-		secret         *Secret
-		commitment     *Commitment
-		expectedResult bool
-		expectedError  error
-	}{
-		"positive validation - pass values used for generation": {
-			secret: &Secret{
-				secret: &msg,
-				r:      r,
-			},
-			commitment: &Commitment{
-				commitment: commitment,
-				pubKey:     pubKey,
-				h:          h,
-			},
-			expectedResult: true,
-		},
-		"negative validation - incorrect `secret`": {
-			secret: &Secret{
-				secret: &msgInvalid,
-				r:      r,
-			},
-			commitment: &Commitment{
-				commitment: commitment,
-				pubKey:     pubKey,
-				h:          new(bn256.G2).ScalarBaseMult(big.NewInt(5)),
-			},
-			expectedResult: false,
-		},
-		"negative validation - incorrect `r`": {
-			secret: &Secret{
-
-				secret: &msg,
-				r:      big.NewInt(3),
-			},
-			commitment: &Commitment{
-				commitment: commitment,
-				pubKey:     pubKey,
-				h:          new(bn256.G2).ScalarBaseMult(big.NewInt(5)),
-			},
-			expectedResult: false,
-		},
-		"negative validation - incorrect `commitment`": {
-			secret: &Secret{
-
-				secret: &msg,
-				r:      r,
-			},
-			commitment: &Commitment{
-				commitment: new(bn256.G2).ScalarBaseMult(big.NewInt(3)),
-				pubKey:     pubKey,
-				h:          h,
-			},
-			expectedResult: false,
-		},
-		"negative validation - incorrect `pubKey`": {
-			secret: &Secret{
-
-				secret: &msg,
-				r:      r,
-			},
-			commitment: &Commitment{
-				commitment: commitment,
-				pubKey:     big.NewInt(3),
-				h:          h,
-			},
-			expectedResult: false,
-		},
-		"negative validation - incorrect `h`": {
-			secret: &Secret{
-
-				secret: &msg,
-				r:      r,
-			},
-			commitment: &Commitment{
-				commitment: commitment,
-				pubKey:     pubKey,
-				h:          new(bn256.G2).ScalarBaseMult(big.NewInt(6)),
-			},
-			expectedResult: false,
-			expectedError:  fmt.Errorf("pairings doesn't match"),
-		},
-	}
-
-	for testName, test := range tests {
-		t.Run(testName, func(t *testing.T) {
-			result := ValidateCommitment(test.commitment, test.secret)
-
-			if result != test.expectedResult {
-				t.Fatalf("\nexpected: %v\nactual:   %v", test.expectedResult, result)
-			}
-		})
-	}
-}
-
-func convertBase64StringToCommitment(str string) *bn256.G2 {
-	commitmentBytes, _ := base64.StdEncoding.DecodeString(str)
-
-	commitment := new(bn256.G2)
-	commitment.Unmarshal(commitmentBytes)
-
-	return commitment
 }
