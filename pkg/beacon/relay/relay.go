b09fbc05223d29e4164d59fdcc98519af57ba5af
Raghav Gulati <raghav.gulati@keep.network>
2018-07-05 09:51:31 -0700

    Allow group members to GenerateRelayEntryIfEligible
    Given a new request, we get the thresholdmember and group for the
    request. If the current node is a member of the group and has the
    associated broadcast channel, the node fires off a goroutine to generate
    a new threshold signature AND submit it to the chain.

--- pkg/beacon/relay/relay.go
+++ pkg/beacon/relay/relay.go
@@ -1,9 +1,16 @@
 package relay
 
 import (
+	"fmt"
+	"os"
+
+	relaychain "github.com/keep-network/keep-core/pkg/beacon/relay/chain"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/config"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/event"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/thresholdsignature"
 	"github.com/keep-network/keep-core/pkg/chain"
 	"github.com/keep-network/keep-core/pkg/net"
+	"github.com/keep-network/keep-core/pkg/thresholdgroup"
 )
 
 // NewNode returns an empty Node with no group, zero group count, and a nil last
@@ -20,4 +27,49 @@ func NewNode(
 		blockCounter: blockCounter,
 		chainConfig:  chainConfig,
 	}
+}
+
+func (n *Node) GenerateRelayEntryIfEligible(
+	req event.Request,
+	relayChain relaychain.Interface,
+) {
+	combinedEntryToSign := make([]byte, 0)
+	combinedEntryToSign = append(combinedEntryToSign, req.PreviousEntry()...)
+	combinedEntryToSign = append(combinedEntryToSign, req.Seed.Bytes()...)
+
+	thresholdMember, groupChannel, err := n.memberAndGroupForRequest(req)
+	if err != nil {
+		fmt.Fprintf(
+			os.Stderr,
+			"Error joining group channel for request group [%s]: [%v]",
+			req.RequestID.String(),
+			err,
+		)
+	}
+	if thresholdMember != nil {
+		go func() {
+			signature, err := thresholdsignature.Execute(
+				combinedEntryToSign,
+				n.blockCounter,
+				groupChannel,
+				thresholdMember,
+			)
+			if err != nil {
+				fmt.Printf(
+					"error creating threshold signature: [%v]",
+					err,
+				)
+				return
+			}
+		}()
+	}
+}
+
+func (n *Node) memberAndGroupForRequest(
+	req event.Request,
+) (*thresholdgroup.Member, net.BroadcastChannel, error) {
+	// Use request to choose group.
+	// See if we're in the group.
+	// If we are, look up and return our member entry and our broadcast channel entry.
+	return nil, nil, nil
 }
