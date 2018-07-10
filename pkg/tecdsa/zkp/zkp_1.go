efbb95277ed8254066e78aac7f2fe672a0153011
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-07-10 13:47:05 +0200

    Update ZKP PI1 commitment calculation

--- pkg/tecdsa/zkp/zkp_1.go
+++ pkg/tecdsa/zkp/zkp_1.go
@@ -22,82 +22,88 @@ type PI1 struct {
 	s3 *big.Int
 }
 
-// Commit to c1, c2, c3
-func (zkp *PI1) Commit(c1, c2, c3, eta, r *big.Int, params *ZKPPublicParameters) error {
-	g := new(big.Int).Add(params.N, big.NewInt(1)) // Γ - capital gamma
-
+// Commit to the Proof PI_1,i, which states that there exists
+// η (eta) ∈ [−q3, q3] such that:
+//   D(c1) = η*D(c2)
+//   D(c3) = η
+//
+// We assume the Prover knows the value r ∈ Z_N∗ used to encrypt η (eta)
+// such that c3 = (Γ^η)*(r^N) mod N2.
+//
+// First the prover chooses uniformly at random four values:
+// * α(alpha) ∈ Z_q^3,
+// * β(beta) ∈ Z_N∗,
+// * ρ(rho) ∈ Z_q^N ̃,
+// * γ(gamma) ∈ Z_((q^3)*N ̃).
+//
+// Then the prover computes u1, u2, z, v, e, s1, s2,s3. This values will be sent
+// by the prover to the verifier.
+func (zkp *PI1) Commit(eta, r, c1, c2, c3 *big.Int, params *ZKPPublicParameters) error {
 	q3 := new(big.Int).Exp(params.q, big.NewInt(3), nil) // q^3
-	qNTilde := new(big.Int).Mul(params.q, params.NTilde) // q * NTilde ̃
-	q3NTilde := new(big.Int).Mul(q3, params.NTilde)      // q^3 * nTilde
 
-	// α ∈ Z_q^3
 	alpha, err := rand.Int(rand.Reader, q3)
 	if err != nil {
-		return fmt.Errorf("could not construct ZKPi [%v]", err)
+		return fmt.Errorf("could not construct ZKP1i [%v]", err)
 	}
 
-	// β ∈ Z_N∗
-	// TODO Check if it's correct random
-	beta, err := rand.Int(rand.Reader, params.N)
+	beta, err := randomFromMultiplicativeGroup(rand.Reader, params.N)
 	if err != nil {
-		return fmt.Errorf("could not construct ZKPi [%v]", err)
+		return fmt.Errorf("could not construct ZKP1i [%v]", err)
 	}
 
-	// ρ ∈ Z_q^N ̃
-	rho, err := rand.Int(rand.Reader, qNTilde)
+	rho, err := rand.Int(rand.Reader, new(big.Int).Mul(params.q, params.NTilde))
 	if err != nil {
-		return fmt.Errorf("could not construct ZKPi [%v]", err)
+		return fmt.Errorf("could not construct ZKP1i [%v]", err)
 	}
 
-	// γ ∈ Z_((q^3)*N ̃)
-	gamma, err := rand.Int(rand.Reader, q3NTilde)
+	gamma, err := rand.Int(rand.Reader, new(big.Int).Mul(q3, params.NTilde))
 	if err != nil {
-		return fmt.Errorf("could not construct ZKPi [%v]", err)
+		return fmt.Errorf("could not construct ZKP1i [%v]", err)
 	}
 
 	// u_1 = ((h1)^η)*((h2)^ρ) mod N ̃
-	u1 := new(big.Int).Mod(
+	zkp.u1 = new(big.Int).Mod(
 		new(big.Int).Mul(
 			new(big.Int).Exp(params.h1, eta, params.NTilde),
 			new(big.Int).Exp(params.h2, rho, params.NTilde),
 		),
-		params.NTilde)
+		params.NTilde,
+	)
 
 	// u_2 = ((h1)^α)*((h2)^γ) mod N ̃
-	u2 := new(big.Int).Mod(
+	zkp.u2 = new(big.Int).Mod(
 		new(big.Int).Mul(
 			new(big.Int).Exp(params.h1, alpha, params.NTilde),
 			new(big.Int).Exp(params.h2, gamma, params.NTilde),
 		),
-		params.NTilde)
+		params.NTilde,
+	)
 
-	NPow2 := new(big.Int).Exp(params.N, big.NewInt(2), nil)
-	// z = ((Γ)^α)*((β)^N) mod N2
-	z := new(big.Int).Mod(
+	// z = ((Γ)^α)*((β)^N) mod N^2
+	zkp.z = new(big.Int).Mod(
 		new(big.Int).Mul(
-			new(big.Int).Exp(g, alpha, NPow2),
-			new(big.Int).Exp(beta, params.N, NPow2),
+			new(big.Int).Exp(params.G, alpha, params.N2),
+			new(big.Int).Exp(beta, params.N, params.N2),
 		),
-		NPow2,
+		params.N2,
 	)
 
 	// v = (c2)^α mod N^2
-	v := new(big.Int).Exp(c2, alpha, NPow2)
+	zkp.v = new(big.Int).Exp(c2, alpha, params.N2)
 
 	// e = hash(c1, c2, c3, z, u1, u2, v)
 	digest := sum256(
 		c1.Bytes(),
 		c2.Bytes(),
 		c3.Bytes(),
-		z.Bytes(),
-		u1.Bytes(),
-		u2.Bytes(),
-		v.Bytes(),
+		zkp.z.Bytes(),
+		zkp.u1.Bytes(),
+		zkp.u2.Bytes(),
+		zkp.v.Bytes(),
 	)
+	zkp.e = new(big.Int).Abs(new(big.Int).SetBytes(digest[:]))
 
-	zkp.e = new(big.Int).SetBytes(digest[:])
-
-	// s1 = eη+α
+	// s1 = e*η+α
 	zkp.s1 = new(big.Int).Add(
 		new(big.Int).Mul(zkp.e, eta),
 		alpha,
@@ -112,7 +118,7 @@ func (zkp *PI1) Commit(c1, c2, c3, eta, r *big.Int, params *ZKPPublicParameters)
 		params.N,
 	)
 
-	// s3 =eρ+γ
+	// s3 = e*ρ+γ
 	zkp.s3 = new(big.Int).Add(
 		new(big.Int).Mul(zkp.e, rho),
 		gamma,
