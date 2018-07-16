9f15e5223d904f614c71561b091249816eef0c84
Piotr Dyraga <piotr.dyraga@akena.engineering>
2018-07-16 12:33:41 +0200

    Keep secret value separately from the decommitment key
    Although the previous approach with storing secret value along with a
    decommitment key in one structure was correct, it was quite unwieldy in
    a real world. Because how our workflow is structured, lack of
    generics in go and difficult deserialization of elliptic curve points
    from bytes, it's better for us to keep the secret value separately and
    pass it later as a function argument for verification along with the
    decommitment key.

--- pkg/tecdsa/commitment/trapdoor_commitment.go
+++ pkg/tecdsa/commitment/trapdoor_commitment.go
@@ -1,5 +1,6 @@
 // Package commitment generates and validates trapdoor commitments using
-// bn256 elliptic curve pairings as described in docs/cryptography/trapdoor-commitments.adoc
+// bn256 elliptic curve pairings as described in
+// docs/cryptography/trapdoor-commitments.adoc
 package commitment
 
 import (
@@ -10,14 +11,9 @@ import (
 	"github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 )
 
-// Secret is what we have committed to. Secret is usually kept private until
-// the receiving party receives the message and wants to validate the commitment.
-// Along with the message we have committed to, Secret contains
-// a decommitment key r needed for the commitment verification.
-type Secret struct {
-	// Secret message
-	message []byte
-	// Decommitment key; used to commitment validation.
+// DecommitmentKey allows to open a commitment and verify if the value is what
+// we have really committed to.
+type DecommitmentKey struct {
 	r *big.Int
 }
 
@@ -27,11 +23,12 @@ type Secret struct {
 // is really what that party has committed to.
 // However, the Commitment itself is not enough for a verification.
 // In order to perform verification, the interested party must receive
-// the Secret too.
+// the DecommitmentKey too.
+//
 // Usually the process happens in two phases:
-// first Commitment is evaluated and sent to receiver and then, after some time,
-// secret is revealed and the receiver can check the secret message against
-// the Commitment received earlier.
+// first, Commitment is evaluated and sent to receiver and then, after some time,
+// secret value along with a DecommitmentKey is revealed and the receiver can
+// check the secret value against the Commitment received earlier.
 type TrapdoorCommitment struct {
 	// Public key for a specific trapdoor commitment.
 	pubKey *big.Int
@@ -41,8 +38,8 @@ type TrapdoorCommitment struct {
 	commitment *bn256.G2
 }
 
-// GenerateCommitment generates a Commitment for passed `secret` message.
-func GenerateCommitment(secret []byte) (*TrapdoorCommitment, *Secret, error) {
+// GenerateCommitment generates a Commitment for the passed `secret` message.
+func GenerateCommitment(secret []byte) (*TrapdoorCommitment, *DecommitmentKey, error) {
 	// Generate a random public key.
 	pubKey, _, err := bn256.RandomG2(rand.Reader)
 	if err != nil {
@@ -75,20 +72,22 @@ func GenerateCommitment(secret []byte) (*TrapdoorCommitment, *Secret, error) {
 			h:          h,
 			commitment: commitment,
 		},
-		&Secret{
-			message: secret,
-			r:       r,
+		&DecommitmentKey{
+			r: r,
 		},
 		nil
 }
 
-// ValidateCommitment validates received commitment against revealed secret.
-func (tc *TrapdoorCommitment) ValidateCommitment(secret *Secret) bool {
-	hash := sha256Sum(secret.message)
+// ValidateCommitment validates received commitment against the revealed secret.
+func (tc *TrapdoorCommitment) ValidateCommitment(
+	secret []byte,
+	decommitmentKey *DecommitmentKey,
+) bool {
+	hash := sha256Sum(secret)
 	digest := new(big.Int).Mod(hash, bn256.Order)
 
 	// a = g * r
-	a := new(bn256.G1).ScalarBaseMult(secret.r)
+	a := new(bn256.G1).ScalarBaseMult(decommitmentKey.r)
 
 	// b = h + g * pubKey
 	b := new(bn256.G2).Add(tc.h, new(bn256.G2).ScalarBaseMult(tc.pubKey))
