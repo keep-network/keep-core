9065c597d1b0e9ad58abf1cc21e9ab9f44212bb0
Philip Schlump <philip.schlump@keep.network>
2018-07-19 18:12:44 -0600

    Removed local test system from config.
    Remove local test stystem from truffle.js.

--- contracts/solidity/truffle.js
+++ contracts/solidity/truffle.js
@@ -22,19 +22,5 @@ module.exports = {
       network_id: "*",
       gas: 4712388
     }
-    , test2: {
-      host: "192.168.0.157",
-      port: 8545,
-      network_id: "*",
-      gas: 4712388,
-      from: "0x6ffba2d0f4c8fd7961f516af43c55fe2d56f6044"
-    }
-    , local: {
-      host: "192.168.0.158",
-      port: 8545,
-      network_id: "91204",
-      gas: 4712388,
-      from: "0xc2a56884538778bacd91aa5bf343bf882c5fb18b"
-    }
   }
 };
