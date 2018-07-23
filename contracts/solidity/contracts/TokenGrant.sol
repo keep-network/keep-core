a6dbddba389820828c0a1c230ec9caf57e367f3c
Nik G <subzeromedia@gmail.com>
2018-07-23 12:36:09 -0500

    Update pragma solidity version

--- contracts/solidity/contracts/TokenGrant.sol
+++ contracts/solidity/contracts/TokenGrant.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.24;
 
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "zeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
