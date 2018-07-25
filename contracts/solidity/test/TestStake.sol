acc1808b1738d905484f64a656e580aea4ac7d40
Philip Schlump <philip.schlump@keep.network>
2018-07-24 21:08:38 -0600

    Changed indentation of tests to match with linter
    Fixed indentation to 4 chars - blanks - no tabs.

--- contracts/solidity/test/TestStake.sol
+++ contracts/solidity/test/TestStake.sol
@@ -7,70 +7,70 @@ import "../contracts/TokenStaking.sol";
 import "./helpers/ThrowProxy.sol";
 
 contract TestStake {  
-  
-  // Create KEEP token
-  KeepToken token = new KeepToken();
-
-  // Create staking contract with 30 days withdrawal delay
-  TokenStaking stakingContract = new TokenStaking(token, 0, 30 days);
-
-  uint withdrawalId;
-
-  function testTotalSupply() public {
-    uint expected = token.INITIAL_SUPPLY();
-    Assert.equal(token.balanceOf(address(this)), expected, "Owner should have all tokens initially.");
-  }
-
-  // Token holder should be able to stake it's tokens
-  function testCanStake() public {
-    uint balance = token.balanceOf(address(this));
-
-    token.approveAndCall(address(stakingContract), 100, "");
     
-    Assert.equal(token.balanceOf(address(this)), balance - 100, "Stake amount should be taken out from token holder's main balance.");
-    Assert.equal(stakingContract.stakeBalanceOf(address(this)), 100, "Stake amount should be added to token holder's stake balance.");
-  }
+    // Create KEEP token
+    KeepToken token = new KeepToken();
 
-  // Token holder should be able to initiate unstake of it's tokens
-  function testCanInitiateUnstake() public {
-    uint balance = token.balanceOf(address(this));
+    // Create staking contract with 30 days withdrawal delay
+    TokenStaking stakingContract = new TokenStaking(token, 0, 30 days);
 
-    withdrawalId = stakingContract.initiateUnstake(100);
+    uint withdrawalId;
 
-    // Inspect created withdrawal request
-    address owner;
-    uint amount;
-    uint start;
-    (owner, amount, start) = stakingContract.withdrawals(withdrawalId);
-    Assert.equal(owner, address(this), "Withdrawal request should maintain a record of the owner.");
-    Assert.equal(amount, 100, "Withdrawal request should maintain a record of the amount.");
-    Assert.equal(start, now, "Withdrawal request should maintain a record of when it was initiated.");
+    function testTotalSupply() public {
+        uint expected = token.INITIAL_SUPPLY();
+        Assert.equal(token.balanceOf(address(this)), expected, "Owner should have all tokens initially.");
+    }
 
-    Assert.equal(stakingContract.stakeBalanceOf(address(this)), 0, "Unstake amount should be taken out from token holder's stake balance."); 
-    Assert.equal(token.balanceOf(address(this)), balance, "Unstake amount should not be added to token holder main balance.");
-  }
+    // Token holder should be able to stake it's tokens
+    function testCanStake() public {
+        uint balance = token.balanceOf(address(this));
 
-  // Should not be able to finish unstake when withdrawal delay is not over
-  function testCannotFinishUnstake() public {
+        token.approveAndCall(address(stakingContract), 100, "");
+        
+        Assert.equal(token.balanceOf(address(this)), balance - 100, "Stake amount should be taken out from token holder's main balance.");
+        Assert.equal(stakingContract.stakeBalanceOf(address(this)), 100, "Stake amount should be added to token holder's stake balance.");
+    }
 
-    // http://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
-    ThrowProxy throwProxy = new ThrowProxy(address(stakingContract));
+    // Token holder should be able to initiate unstake of it's tokens
+    function testCanInitiateUnstake() public {
+        uint balance = token.balanceOf(address(this));
 
-    // Prime the proxy
-    TokenStaking(address(throwProxy)).finishUnstake(withdrawalId);
+        withdrawalId = stakingContract.initiateUnstake(100);
 
-    // Execute the call that is supposed to throw.
-    // r will be false if it threw and true if it didn't.
-    bool r = throwProxy.execute.gas(200000)();
-    Assert.isFalse(r, "Should throw when trying to unstake when withdrawal delay is not over.");
-    Assert.equal(stakingContract.stakeBalanceOf(address(this)), 0, "Stake balance should stay unchanged.");
-  }
+        // Inspect created withdrawal request
+        address owner;
+        uint amount;
+        uint start;
+        (owner, amount, start) = stakingContract.withdrawals(withdrawalId);
+        Assert.equal(owner, address(this), "Withdrawal request should maintain a record of the owner.");
+        Assert.equal(amount, 100, "Withdrawal request should maintain a record of the amount.");
+        Assert.equal(start, now, "Withdrawal request should maintain a record of when it was initiated.");
 
-  // Token holder should not be able to stake without providing correct stakingContract address.
-  function testCanNotStakeWithWrongRecipient() public {
+        Assert.equal(stakingContract.stakeBalanceOf(address(this)), 0, "Unstake amount should be taken out from token holder's stake balance."); 
+        Assert.equal(token.balanceOf(address(this)), balance, "Unstake amount should not be added to token holder main balance.");
+    }
 
-    bytes4 methodId = bytes4(keccak256("approveAndCall(address,uint256,bytes)"));
-    Assert.isFalse(address(token).call(
-      methodId, address(0), 100, 0x1234), "Should throw when trying to stake.");
-  }
+    // Should not be able to finish unstake when withdrawal delay is not over
+    function testCannotFinishUnstake() public {
+
+        // http://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
+        ThrowProxy throwProxy = new ThrowProxy(address(stakingContract));
+
+        // Prime the proxy
+        TokenStaking(address(throwProxy)).finishUnstake(withdrawalId);
+
+        // Execute the call that is supposed to throw.
+        // r will be false if it threw and true if it didn't.
+        bool r = throwProxy.execute.gas(200000)();
+        Assert.isFalse(r, "Should throw when trying to unstake when withdrawal delay is not over.");
+        Assert.equal(stakingContract.stakeBalanceOf(address(this)), 0, "Stake balance should stay unchanged.");
+    }
+
+    // Token holder should not be able to stake without providing correct stakingContract address.
+    function testCanNotStakeWithWrongRecipient() public {
+
+        bytes4 methodId = bytes4(keccak256("approveAndCall(address,uint256,bytes)"));
+        Assert.isFalse(address(token).call(
+            methodId, address(0), 100, 0x1234), "Should throw when trying to stake.");
+    }
 }
