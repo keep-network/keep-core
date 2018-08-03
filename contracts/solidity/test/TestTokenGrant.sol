186890002845acc0911ccd919d8678f9fb581cdc
Nik G <subzeromedia@gmail.com>
2018-08-02 20:30:28 -0500

    Split token grant tests

--- contracts/solidity/test/TestTokenGrant.sol
+++ contracts/solidity/test/TestTokenGrant.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.4.21;
 
 import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
 import "../contracts/KeepToken.sol";
 import "../contracts/TokenGrant.sol";
-import "./helpers/ThrowProxy.sol";
 
-contract TestTokenGrant {  
-    
+
+contract TestTokenGrant {
+
     // Create KEEP token.
     KeepToken t = new KeepToken();
 
@@ -28,29 +27,5 @@ contract TestTokenGrant {
         id = c.grant(100, beneficiary, duration, start, cliff, false);
         Assert.equal(t.balanceOf(address(this)), balance - 100, "Amount should be taken out from grant creator main balance.");
         Assert.equal(c.balanceOf(beneficiary), 100, "Amount should be added to beneficiary's granted balance.");
-    }
-
-    function testCanGetGrantByID() public {
-        uint _amount;
-        uint _released;
-        bool _locked;
-        bool _revoked;
-        (_amount, _released, _locked, _revoked) = c.getGrant(id);
-        Assert.equal(_amount, 100, "Grant should maintain a record of the granted amount.");
-        Assert.equal(_released, 0, "Grant should have 0 amount released initially.");
-        Assert.equal(_locked, false, "Grant should initially be unlocked.");
-        Assert.equal(_revoked, false, "Grant should not be marked as revoked initially.");
-    }
-
-    function testCanGetGrantVestingScheduleByGrantID() public {
-        address _owner;
-        uint _duration;
-        uint _start;
-        uint _cliff;
-        (_owner, _duration, _start, _cliff) = c.getGrantVestingSchedule(id);
-        Assert.equal(_owner, address(this), "Grant should maintain a record of the creator.");
-        Assert.equal(_duration, duration, "Grant should have vesting schedule duration.");
-        Assert.equal(_start, start, "Grant should have start time.");
-        Assert.equal(_cliff, start+cliff, "Grant should have vesting schedule cliff duration.");
     }
 }
