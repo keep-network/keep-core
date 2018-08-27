dabb486c12b7f247249d7dd15c9e9ac01299cfa9
Nik G <subzeromedia@gmail.com>
2018-08-27 12:49:45 +0100

    Add footer

--- contracts/solidity/dashboard/src/components/Footer.jsx
+++ contracts/solidity/dashboard/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Footer = () => (
-  <p>Footer</p>
-)
+const Footer = () => {
+  const d = new Date();
+  const year = d.getFullYear();
+  return (
+    <footer class="footer">
+      <div class="container text-center">
+        <span class="text-muted"><small>© {year} Keep. All Rights Reserved.</small></span>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer;
