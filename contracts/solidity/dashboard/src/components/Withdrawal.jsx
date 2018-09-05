316868c63d94bc1184ac88e1472f30be89075353
Nik G <subzeromedia@gmail.com>
2018-09-05 12:45:05 +0300

    Move withdrawal component into a separate file

---
+++ contracts/solidity/dashboard/src/components/Withdrawal.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
+import { getTokenStaking } from '../contracts'
+import WithWeb3Context from './WithWeb3Context'
+
+class Withdrawal extends Component {
+
+  async finishUnstake(withdrawalId) {
+    const { web3 } = this.props
+    const stakingContract = await getTokenStaking(process.env.REACT_APP_STAKING_ADDRESS)
+    stakingContract.finishUnstake(withdrawalId, {from: web3.yourAddress, gas: 110000})
+  }
+
+  render() {
+    const { withdrawal } = this.props
+    let action = 'N/A'
+    if (withdrawal.available) {
+      action = <Button bsSize="small" bsStyle="primary" onClick={()=>this.finishUnstake(withdrawal.id)}>Finish Unstake</Button>
+    }
+
+    return (
+      <tr>
+        <td>{withdrawal.amount}</td>
+        <td className="text-mute">{withdrawal.availableAt}</td>
+        <td>{action}</td>
+      </tr>
+    )
+  }
+}
+
+export default WithWeb3Context(Withdrawal)
--- contracts/solidity/dashboard/src/components/WithdrawalsTable.jsx
+++ contracts/solidity/dashboard/src/components/WithdrawalsTable.jsx
@@ -1,32 +1,6 @@
 import React, { Component } from 'react'
-import { Table, Button } from 'react-bootstrap'
-import { getTokenStaking } from '../contracts'
-import Network from '../network'
-
-class Withdrawal extends Component {
-
-  async finishUnstake(withdrawalId) {
-    const accounts = await Network.getAccounts()
-    const stakingContract = await getTokenStaking(process.env.REACT_APP_STAKING_ADDRESS)
-    stakingContract.finishUnstake(withdrawalId, {from: accounts[0], gas: 110000})
-  }
-
-  render() {
-
-    let action = 'N/A'
-    if (this.props.withdrawal.available) {
-      action = <Button bsSize="small" bsStyle="primary" onClick={()=>this.finishUnstake(this.props.withdrawal.id)}>Finish Unstake</Button>
-    }
-
-    return (
-      <tr>
-        <td>{this.props.withdrawal.amount}</td>
-        <td className="text-mute">{this.props.withdrawal.availableAt}</td>
-        <td>{action}</td>
-      </tr>
-    )
-  }
-}
+import { Table } from 'react-bootstrap'
+import Withdrawal from './Withdrawal'
 
 class WithdrawalsTable extends Component {
 
