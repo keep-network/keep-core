20c7338f8fb13d94afd32239a62bd2dd274b8c48
Nik G <subzeromedia@gmail.com>
2018-10-02 19:45:11 +0300

    Add more comments

--- pkg/bls/altbn128.go
+++ pkg/bls/altbn128.go
@@ -32,6 +32,7 @@ func modSqrt(i, m *big.Int) *big.Int {
 	return new(big.Int).ModSqrt(i, m)
 }
 
+// Returns only one of the two possible Y (even/odd) for provided X
 func yFromX(x *big.Int) *big.Int {
 	return modSqrt(sum(product(x, x, x), big.NewInt(3)), bn256.P)
 }
@@ -71,11 +72,16 @@ func G1HashToPoint(m []byte) *bn256.G1 {
 	}
 }
 
+// Calculates whether the provided Y coordinate is an even or odd number.
+// Returns 0x01 if Y is an even number and 0x00 if it's odd.
 func ySign(y *big.Int) byte {
 	arr := y.Bytes()
 	return arr[len(arr)-1] & 1
 }
 
+// Compress point by using X value and the sign of Y (even/odd) encoded
+// into the first byte. This is achieved by using bitwise `OR` operator.
+// Original byte and the sign can be later extracted with bitswise `AND`.
 func Compress(g *bn256.G1) []byte {
 
 	rt := make([]byte, 32)
@@ -88,22 +94,35 @@ func Compress(g *bn256.G1) []byte {
 
 	y := new(big.Int).SetBytes(marshalled[32:])
 
+	// Prepare bytes mask with (even/odd) sign
 	mask := ySign(y) << 7
 
+	// Use `OR` operator to save the sign
 	rt[0] |= mask
 
 	return rt
 }
 
+// Decompress byte slice into G1 point by extracting Y sign from the first
+// byte, extracting X value and calculating original Y value based on the
+// extracted Y sign. Extracting sign and original byte is achieved by
+// using `AND` bitwise operator:
+// ... & 0x7F => to get the original byte
+// ... & 0x80 => to get the sign
 func Decompress(m []byte) (*bn256.G1, error) {
 
+	// Get the original X
 	x := new(big.Int).SetBytes(append([]byte{m[0] & 0x7F}, m[1:]...))
+
+	// Get one of the two possible Y
 	y := yFromX(x)
 
 	if y == nil {
 		return nil, errors.New("Failed to decompress G1.")
 	}
 
+	// Compare calculated Y sign with the original Y sign and if it doesn't match
+	// get the right Y by extracting the calculated one from the bn256.P
 	if m[0] & 0x80 >> 7 != ySign(y) {
 		y = new(big.Int).Add(bn256.P, new(big.Int).Neg(y))
 	}
