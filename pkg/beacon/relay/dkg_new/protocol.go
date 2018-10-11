ae13c53a055cd42d57c9ebacd63b1ee2723fa6ab
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-10-11 17:08:04 +0200

    Function to calculate share for member

--- pkg/beacon/relay/dkg_new/protocol.go
+++ pkg/beacon/relay/dkg_new/protocol.go
@@ -9,3 +9,30 @@
 //         Lecture Notes in Computer Science, vol 1592. Springer, Berlin, Heidelberg
 //         http://groups.csail.mit.edu/cis/pubs/stasio/vss.ps.gz
 package dkg
+
+import (
+	"math/big"
+)
+
+// calculateShare calculates a share for given memberID.
+//
+// It calculates `Σ a_j * z^j mod q`for j in [0..T], where:
+// - `a_j` is j coefficient
+// - `z` is memberID
+// - `T` is threshold
+func calculateShare(memberID *big.Int, coefficients []*big.Int, mod *big.Int) *big.Int {
+	result := big.NewInt(0)
+	for j, a := range coefficients {
+		result = new(big.Int).Mod(
+			new(big.Int).Add(
+				result,
+				new(big.Int).Mul(
+					a,
+					new(big.Int).Exp(memberID, big.NewInt(int64(j)), mod),
+				),
+			),
+			mod,
+		)
+	}
+	return result
+}
