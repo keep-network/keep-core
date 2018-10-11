f8c096abd565286538c8ac13d0c2574e27be2de0
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-10-11 16:59:57 +0200

    DKG member group structure

---
+++ pkg/beacon/relay/dkg_new/group.go
@@ -0,0 +1,15 @@
+package dkg
+
+import "math/big"
+
+// Group is protocol's members group.
+type Group struct {
+	// The number of members in the complete group.
+	groupSize int
+	// The maximum number of group members who could be dishonest in order for the
+	// generated key to be uncompromised.
+	dishonestThreshold int
+	// IDs of all members of the group. Contains local member's ID.
+	// Initially empty, populated as each other member announces its presence.
+	memberIDs []*big.Int
+}
