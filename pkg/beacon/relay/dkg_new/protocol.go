faaa8a703844cdd300be4b997b11382ee0839c2f
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-10-11 17:05:53 +0200

    DKG protocol file

---
+++ pkg/beacon/relay/dkg_new/protocol.go
@@ -0,0 +1,11 @@
+// Package dkg conatins code that implements Distributed Key Generation protocol
+// described in [GJKR 99].
+//
+// See http://docs.keep.network/cryptography/beacon_dkg.html#_protocol
+//
+//     [GJKR 99]: Gennaro R., Jarecki S., Krawczyk H., Rabin T. (1999) Secure
+//         Distributed Key Generation for Discrete-Log Based Cryptosystems. In:
+//         Stern J. (eds) Advances in Cryptology — EUROCRYPT ’99. EUROCRYPT 1999.
+//         Lecture Notes in Computer Science, vol 1592. Springer, Berlin, Heidelberg
+//         http://groups.csail.mit.edu/cis/pubs/stasio/vss.ps.gz
+package dkg
