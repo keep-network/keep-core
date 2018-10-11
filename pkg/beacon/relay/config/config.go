d5581c0167c1ab65af92fc98bdebf4e04c9ee5f5
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-10-11 16:54:10 +0200

    Add DKG protocol parameters to config

--- pkg/beacon/relay/config/config.go
+++ pkg/beacon/relay/config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"math/big"
+)
+
 // Chain contains the config data needed for the relay to operate.
 type Chain struct {
 	// GroupSize is the size of a group in the threshold relay.
@@ -7,4 +11,10 @@ type Chain struct {
 	// Threshold is the minimum number of interacting group members needed to
 	// produce a relay entry.
 	Threshold int
+}
+
+// DKG contains the configuration data needed for the DKG protocol execution.
+type DKG struct {
+	// P, Q are big primes, such that `p = 2q + 1`
+	P, Q *big.Int
 }
