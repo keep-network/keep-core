65f7d530b990aeb447fce447b7f1028777aacca3
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-10-11 17:03:24 +0200

    CommittingMember for Phases 3 and 4

--- pkg/beacon/relay/dkg_new/member.go
+++ pkg/beacon/relay/dkg_new/member.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	"github.com/keep-network/keep-core/pkg/beacon/relay/config"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/pedersen"
 )
 
 type memberCore struct {
@@ -17,4 +18,19 @@ type memberCore struct {
 
 func (m *memberCore) ProtocolConfig() *config.DKG {
 	return m.protocolConfig
+}
+
+// CommittingMember represents one member in a threshold key sharing group, after
+// it has a full list of `memberIDs` that belong to its threshold group. A
+// member in this state has two maps of `memberShares` for each member of the
+// group.
+type CommittingMember struct {
+	*memberCore
+
+	// Pedersen VSS scheme used to calculate commitments.
+	vss *pedersen.VSS
+	// Member's share of the shared secret key.
+	secretKeyShare *big.Int
+	// memberShares calculated for each group member.
+	memberSharesS, memberSharesT map[*big.Int]*big.Int
 }
