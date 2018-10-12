2de763a1cfc0fbb01fd74d8d740b65061319ba28
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-10-12 16:29:57 +0200

    Separate messages for shares and commitments broadcasting
    It is expected that shares will be encrypted and delivered in such
    manner that only receiving member will be able to open them.
    Commitments will be broadcasted and opened by all members.

--- pkg/beacon/relay/dkg_new/message.go
+++ pkg/beacon/relay/dkg_new/message.go
@@ -3,15 +3,24 @@ package dkg
 import "math/big"
 
 // MemberCommitmentsMessage is a message payload that carries the sender's
-// shares calculates for each group member and commitments during distributed
-// key generation.
-// It is expected to be encrypted before broadcast.
+// commitments to polynomial coefficients during distributed key generation.
+// It is expected to be broadcast.
 type MemberCommitmentsMessage struct {
 	senderID *big.Int
 
-	sharesS     map[*big.Int]*big.Int
-	sharesT     map[*big.Int]*big.Int
 	commitments []*big.Int
+}
+
+// PeerSharesMessage is a message payload that carries the sender's secret and
+// random shares for the recipient during distributed key generation.
+// It is expected to be communicated in encrypted fashion to the recipient over
+// a broadcast channel.
+type PeerSharesMessage struct {
+	senderID   *big.Int
+	receiverID *big.Int
+
+	secretShare *big.Int
+	randomShare *big.Int
 }
 
 // FirstAccusationsMessage is a message payload that carries all of the sender's
