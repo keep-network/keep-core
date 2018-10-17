4620ed5c227ff200debddc89116d2e5e7747909c
Philip Schlump <philip.schlump@keep.network>
2018-10-17 05:06:50 -0600

    Removed local testnet configuration
    Removed my local testnet config from the truffle.js.

--- contracts/solidity/truffle.js
+++ contracts/solidity/truffle.js
@@ -14,18 +14,5 @@ module.exports = {
       network_id: "*",
       gas: 4712388
     }
-    , test3: {
-      host: "192.168.0.199",
-      port: 8545,
-      network_id: "11011"
-      , gas: 4712388
-      , from: "0x023e291a99d21c944a871adcc44561a58f99bdbc"
-    }
-    , oldTest3: {
-      host: "192.168.0.199",
-      port: 8545,
-      network_id: "*"
-      , from: "0x6ffba2d0f4c8fd7961f516af43c55fe2d56f6044"
-    }
   }
 };
