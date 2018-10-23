cd7805c37e679806af71b0e98d3e9c521b581a9c
Raghav Gulati <raghav.gulati@keep.network>
2018-10-23 10:09:06 -0400

    Default security for libp2p
    Libp2p allows for customization of security transports. Our handshake
    protocol will implement the necessary pieces to satisfy a security
    transport. When this runs, peers will authenticate each other.

---
+++ pkg/net/security/security.go
@@ -0,0 +1,11 @@
+package security
+
+import (
+	"github.com/keep-network/keep-core/pkg/net/security/handshake"
+	libp2p "github.com/libp2p/go-libp2p"
+)
+
+// DefaultSecurity is the default security option.
+//
+// Precedes every connection
+var DefaultSecurity = libp2p.Security(handshake.ID, handshake.New)
