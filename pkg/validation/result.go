5dc40967c623eddd845d94e67fc10d7cffb7a596
Philip Schlump <pschlump@gmail.com>
2018-10-25 08:39:27 -0600

    Better information on how results are captured
    Better layout information on what constitutes results.

--- pkg/validation/result.go
+++ pkg/validation/result.go
@@ -1,18 +1,29 @@
 package validaiton
 
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+)
+
 /* Haskel Data Type
 
 data Result = NoFaultFailure
             | FailureDQ { disqualified :: Array Bool }
-            | PerfectSuccess { pubkey :: BeaconPubkey }
-            | SuccessIA { pubkey :: BeaconPubkey
+            | PerfectSuccess { pubkey :: BlsPubkey }
+            | SuccessIA { pubkey :: BlsPubkey
                         , inactive :: Array Bool }
-            | SuccessDQ { pubkey :: BeaconPubkey
+            | SuccessDQ { pubkey :: BlsPubkey
                         , disqualified :: Array Bool }
-            | MixedSuccess { pubkey :: BeaconPubkey
+            | MixedSuccess { pubkey :: BlsPubkey
                            , inactive :: Array Bool
                            , disqualified :: Array Bool }
 
+class Result(NamedTuple):
+    groupPubkey:  Optional[BlsPubkey]
+    disqualified: Optional[BitArray]
+    inactive:     Optional[BitArray]
 */
 
 type ResultType int
@@ -26,12 +37,56 @@ const (
 	MixedSuccess
 )
 
-type BeaconPubkey struct {
+// BlsPubkey is refered to as BeaconPubkey in the paper/deisng specification.
+// This is actually A BLS key.  In the code it is called BlsPubkey.
+type BlsPubkey struct {
 }
 
 type Result struct {
 	TypeIs       ResultType
 	Disqualified []bool
-	Pubkey       BeaconPubkey
+	Pubkey       BlsPubkey
 	Inactive     []bool
+}
+
+func SearilizeResultForHash(rs Result) []byte {
+	var buf bytes.Buffer
+
+	binary.Write(&buf, binary.BigEndian, int64(rs.TypeIs))
+
+	binary.Write(&buf, binary.BigEndian, int64(len(rs.Disqualified)))
+	for _, b := range rs.Disqualified {
+		if b {
+			buf.Write([]byte{'1'})
+		} else {
+			buf.Write([]byte{'0'})
+		}
+	}
+
+	// FIXME - TODO - put in Pubkey
+
+	binary.Write(&buf, binary.BigEndian, int64(len(rs.Inactive)))
+	for _, b := range rs.Inactive {
+		if b {
+			buf.Write([]byte{'1'})
+		} else {
+			buf.Write([]byte{'0'})
+		}
+	}
+
+	return buf.Bytes()
+}
+
+func Hash(data ...[]byte) []byte {
+	// this probably isn't the correct hash - but will work for testing at the moment.
+	return Keccak256(data...)
+}
+
+// Keccak256 use the Ethereum Keccak hasing fucntions to return a hash from a list of values.
+func Keccak256(data ...[]byte) []byte {
+	d := sha3.NewKeccak256()
+	for _, b := range data {
+		d.Write(b)
+	}
+	return d.Sum(nil)
 }
