3d4622725a6bde0a071b0cf060b8c601d758a063
Raghav Gulati <raghav.gulati@keep.network>
2018-10-26 11:37:02 -0400

    No longer importing ID and new func
    We moved the creation of the transport and libp2p specific parts into
    the libp2p package; no longer necessary to export the package.

--- pkg/net/libp2p/libp2p.go
+++ pkg/net/libp2p/libp2p.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/keep-network/keep-core/pkg/net"
 	"github.com/keep-network/keep-core/pkg/net/key"
-	"github.com/keep-network/keep-core/pkg/net/security/handshake"
 
 	dstore "github.com/ipfs/go-datastore"
 	dssync "github.com/ipfs/go-datastore/sync"
@@ -139,7 +138,7 @@ func discoverAndListen(
 	return libp2p.New(ctx,
 		libp2p.ListenAddrs(addrs...),
 		libp2p.Identity(identity.privKey),
-		libp2p.Security(handshake.ID, handshake.New),
+		libp2p.Security(handshakeID, newAuthenticatedTransport),
 	)
 }
 
