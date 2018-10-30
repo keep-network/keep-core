cb28be77914bfbd4c75923d8d5dd9fc3d0a7740f
Jakub Nowakowski <jakub.nowakowski@akena.co>
2018-10-30 16:27:56 +0100

    Test for private key shares reconstruction

---
+++ pkg/beacon/relay/gjkr/protocol_reconstructions_test.go
@@ -0,0 +1,63 @@
+package gjkr
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestReconstructPrivateKeyShares(t *testing.T) {
+	threshold := 2
+	groupSize := 5
+
+	group, _ := initializeSharingMembersGroup(threshold, groupSize)
+
+	disqualifiedIDs := []int{3, 5}
+
+	expectedPrivateKeyShare1 := group[2].secretCoefficients[0] // for ID = 3
+	expectedPrivateKeyShare2 := group[4].secretCoefficients[0] // for ID = 5
+
+	// Prepare disqualified shares map for test run
+	allDisqualifiedShares := make(map[int]map[int]*big.Int, groupSize-len(disqualifiedIDs))
+	for _, disqualifiedID := range disqualifiedIDs {
+		disqualifiedShares := make(map[int]*big.Int, groupSize-len(disqualifiedIDs))
+		for _, m := range group {
+			if !contains(disqualifiedIDs, m.ID) {
+				for peerID, share := range m.receivedSharesS {
+					if peerID == disqualifiedID {
+						disqualifiedShares[m.ID] = share
+					}
+				}
+			}
+		}
+		allDisqualifiedShares[disqualifiedID] = disqualifiedShares
+	}
+
+	for _, sm := range group {
+		if !contains(disqualifiedIDs, sm.ID) {
+			privateKeyShares := sm.ReconstructPrivateKeyShares(allDisqualifiedShares)
+
+			if privateKeyShares[disqualifiedIDs[0]].Cmp(expectedPrivateKeyShare1) != 0 {
+				t.Fatalf("\nexpected: %s\nactual:   %s\n",
+					expectedPrivateKeyShare1,
+					privateKeyShares[disqualifiedIDs[0]],
+				)
+			}
+
+			if privateKeyShares[disqualifiedIDs[1]].Cmp(expectedPrivateKeyShare2) != 0 {
+				t.Fatalf("\nexpected: %s\nactual:   %s\n",
+					expectedPrivateKeyShare1,
+					privateKeyShares[disqualifiedIDs[1]],
+				)
+			}
+		}
+	}
+}
+
+func contains(slice []int, value int) bool {
+	for _, i := range slice {
+		if i == value {
+			return true
+		}
+	}
+	return false
+}
