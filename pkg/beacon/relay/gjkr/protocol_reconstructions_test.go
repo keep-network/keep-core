e734a1840f26fb97538c853c7bac0c0fca227e93
Jakub Nowakowski <jakub.nowakowski@akena.co>
2018-10-31 12:10:44 +0100

    Test function to init reconstructing group

--- pkg/beacon/relay/gjkr/protocol_reconstructions_test.go
+++ pkg/beacon/relay/gjkr/protocol_reconstructions_test.go
@@ -8,29 +8,13 @@ import (
 func TestReconstructPrivateKeyShares(t *testing.T) {
 	threshold := 2
 	groupSize := 5
-
-	group, _ := initializeSharingMembersGroup(threshold, groupSize)
-
 	disqualifiedIDs := []int{3, 5}
+
+	group, allDisqualifiedShares, _ := initializeReconstructingMembersGroup(threshold, groupSize, disqualifiedIDs)
 
 	expectedPrivateKeyShare1 := group[2].secretCoefficients[0] // for ID = 3
 	expectedPrivateKeyShare2 := group[4].secretCoefficients[0] // for ID = 5
 
-	// Prepare disqualified shares map for test run
-	allDisqualifiedShares := make(map[int]map[int]*big.Int, groupSize-len(disqualifiedIDs))
-	for _, disqualifiedID := range disqualifiedIDs {
-		disqualifiedShares := make(map[int]*big.Int, groupSize-len(disqualifiedIDs))
-		for _, m := range group {
-			if !contains(disqualifiedIDs, m.ID) {
-				for peerID, share := range m.receivedSharesS {
-					if peerID == disqualifiedID {
-						disqualifiedShares[m.ID] = share
-					}
-				}
-			}
-		}
-		allDisqualifiedShares[disqualifiedID] = disqualifiedShares
-	}
 	for _, rm := range group {
 		if !contains(disqualifiedIDs, rm.ID) {
 			rm.ReconstructPrivateKeyShares(allDisqualifiedShares)
@@ -59,4 +43,39 @@ func contains(slice []int, value int) bool {
 		}
 	}
 	return false
+}
+
+func initializeReconstructingMembersGroup(
+	threshold, groupSize int,
+	disqualifiedIDs []int,
+) ([]*ReconstructingMember, map[int]map[int]*big.Int, error) {
+	sharingMembers, _ := initializeSharingMembersGroup(threshold, groupSize)
+
+	var reconstructingMembers []*ReconstructingMember
+	for _, sm := range sharingMembers {
+		reconstructingMembers = append(reconstructingMembers,
+			&ReconstructingMember{
+				SharingMember: sm,
+			},
+		)
+	}
+
+	// Disqualified shares map for test run:
+	// <disqualifiedID, <peerID, shareS>>
+	allDisqualifiedShares := make(map[int]map[int]*big.Int, groupSize-len(disqualifiedIDs))
+	for _, disqualifiedID := range disqualifiedIDs {
+		disqualifiedShares := make(map[int]*big.Int, groupSize-len(disqualifiedIDs))
+		for _, m := range sharingMembers {
+			if !contains(disqualifiedIDs, m.ID) {
+				for peerID, share := range m.receivedSharesS {
+					if peerID == disqualifiedID {
+						disqualifiedShares[m.ID] = share
+					}
+				}
+			}
+		}
+		allDisqualifiedShares[disqualifiedID] = disqualifiedShares
+	}
+
+	return reconstructingMembers, allDisqualifiedShares, nil
 }
