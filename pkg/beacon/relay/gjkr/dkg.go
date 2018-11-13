d395c7c57e42a419fa0461b2a6a34e429d265ea9
Jakub Nowakowski <jakub.nowakowski@akena.co>
2018-11-13 17:02:31 +0100

    Chain configuration in DKG struct

--- pkg/beacon/relay/gjkr/dkg.go
+++ pkg/beacon/relay/gjkr/dkg.go
@@ -4,12 +4,32 @@ import (
 	crand "crypto/rand"
 	"fmt"
 	"math/big"
+
+	"github.com/keep-network/keep-core/pkg/chain"
 )
 
 // DKG contains the configuration data needed for the DKG protocol execution.
 type DKG struct {
 	// P, Q are big primes, such that `p = 2q + 1`
 	P, Q *big.Int
+
+	chain *Chain
+}
+
+// Chain contains handle to interact with blockchain along with parameters specific
+// for block height tracking.
+type Chain struct {
+	handle chain.Handle
+
+	// Block height when the protocol execution started. Value needs to be set
+	// at the begining of the protocol execution.
+	initialBlockHeight int // t_init
+	// Predefined expected duration of the protocol execution. Relates to DKG
+	// Phase 13.
+	expectedProtocolDuration int // t_dkg
+	// Predefined step for each publishing window. The value is used to determine
+	// eligible publishing member. Relates to DKG Phase 13.
+	blockStep int // t_step
 }
 
 // RandomQ generates a random `big.Int` in range (0, q).
@@ -23,4 +43,18 @@ func (d *DKG) RandomQ() (*big.Int, error) {
 			return x, nil
 		}
 	}
+}
+
+// ChainHandle returns blockchain handle that provides access to chain interactions.
+func (d *DKG) ChainHandle() chain.Handle {
+	return d.chain.handle
+}
+
+// CurrentBlock returns current block height on a chain.
+func (d *Chain) CurrentBlock() (int, error) {
+	blockCounter, err := d.handle.BlockCounter()
+	if err != nil {
+		return 0, err
+	}
+	return blockCounter.CurrentBlock()
 }
