faf30820c2d9eb7967c99207f5172ae4e0cf1514
lispmeister <markus.fix@keep.network>
2018-11-13 15:36:34 +0400

    change account generation to runtime and simplify Dockerfile
    We now create the two necessary Ethereum accounts at runtime when
    the docker container starts. We patch the genesis.json at runtime
    to issue tokens to account1. We allocate mining rewards to account0.
    Loggin output to STDOUT is a bit more structured now to clarify
    boot phases of the container.

--- infrastructure/geth-node/docker-entrypoint.sh
+++ infrastructure/geth-node/docker-entrypoint.sh
@@ -1,11 +1,60 @@
 #!/bin/sh
 set -e
 
-export GETH_ETH_ACCOUNT=`cat /root/account1`
-echo "GETH_ETH_ACCOUNT: $GETH_ETH_ACCOUNT"
-
+# generate a random node id
 export RANDOM_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo "RANDOM_ID: $RANDOM_ID"
+echo "-- RANDOM_ID: $RANDOM_ID"
+echo ""
+
+# create new account for this Keep client
+/geth account new  --password /root/passphrase | \
+    cut -d "{" -f2 | cut -d "}" -f1 > /root/account0
+export GETH_ETH_ACCOUNT0=`cat /root/account0`
+echo "-- GETH_ETH_ACCOUNT0: $GETH_ETH_ACCOUNT0"
+echo ""
+
+# create new account for Keep peers
+/geth account new  --password /root/passphrase | \
+    cut -d "{" -f2 | cut -d "}" -f1 > /root/account1
+export GETH_ETH_ACCOUNT1=`cat /root/account1`
+echo "-- GETH_ETH_ACCOUNT1: $GETH_ETH_ACCOUNT1"
+echo ""
+
+# Generate genesis.json and issue tokens to Keep peers account1
+cat <<EOF >> /root/genesis.json
+{
+  "config": {
+    "chainId": 1101,
+    "homesteadBlock": 0,
+    "eip155Block": 0,
+    "eip158Block": 0
+  },
+  "difficulty" : "0x20000",
+  "gasLimit"   : "0x493E00",
+  "alloc": {
+EOF
+
+echo "   \"0x$GETH_ETH_ACCOUNT1\": {" >> /root/genesis.json
+echo "    \"balance\": \"1000000000000000000000\"" >> /root/genesis.json
+cat <<EOF >> /root/genesis.json
+   }
+  }
+}
+EOF
+
+# dump genesis file
+echo "-- Dump genesis.json:"
+cat /root/genesis.json
+echo ""
+
+# initialize chain with our genesis.json parameters
+echo "-- Initialize geth"
+/geth init /root/genesis.json
+echo ""
+
+# start miner and allocate rewards to account0
+echo "-- Start geth mining for account0: $GETH_ETH_ACCOUNT0"
+echo ""
 
 exec "/geth" --port 30303 --networkid 1101 \
     --ws --wsaddr "0.0.0.0" --wsport 8546 --wsorigins "*" \
@@ -14,4 +63,4 @@ exec "/geth" --port 30303 --networkid 1101 \
     --syncmode "fast" \
     --mine --miner.threads=1 \
     --identity $RANDOM_ID \
-    --miner.etherbase=$GETH_ETH_ACCOUNT
+    --miner.etherbase=$GETH_ETH_ACCOUNT0
