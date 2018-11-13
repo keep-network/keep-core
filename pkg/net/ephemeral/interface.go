da0db3787ba1d47aa76874268cc64e9a624c2056
Piotr Dyraga <piotr.dyraga@akena.engineering>
2018-11-13 09:05:53 +0100

    Ephemeral key interface moved to a separate package

---
+++ pkg/net/ephemeral/interface.go
@@ -0,0 +1,22 @@
+package ephemeral
+
+// SymmetricKey is a session-scoped ECDH symmetric key.
+type SymmetricKey interface {
+	Encrypt([]byte) ([]byte, error)
+}
+
+// PrivateKey is a session-scoped private elliptic curve key.
+type PrivateKey interface {
+	Decrypt([]byte) ([]byte, error)
+
+	Ecdh(PublicKey) SymmetricKey
+
+	Marshal([]byte, error)
+	Unmarshal([]byte) error
+}
+
+// PublicKey is a session-scoped public elliptic curve key.
+type PublicKey interface {
+	Marshal([]byte, error)
+	Unmarshal([]byte) error
+}
