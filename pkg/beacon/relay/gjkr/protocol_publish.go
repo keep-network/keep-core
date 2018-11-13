12f0ad6999704f99a0b893f44ec0872fc36b1653
Jakub Nowakowski <jakub.nowakowski@akena.co>
2018-11-13 16:57:33 +0100

    Determine publishing members IDs
    Implement DKG Phase 13 part where IDs of the members eligible to publish
    the result to the chain are determined.
    ```
     T_now = getCurrentBlockHeight()
    
      # using T_init from phase 1
      T_elapsed = T_now - T_init
    
      # determine highest index j eligible to submit
      if T_elapsed <= T_dkg:
        j = 1
      else:
        T_over = T_elapsed - T_dkg
        j = 1 + ceiling(T_over / T_step)
    ```

--- pkg/beacon/relay/gjkr/protocol_publish.go
+++ pkg/beacon/relay/gjkr/protocol_publish.go
@@ -1,6 +1,8 @@
 package gjkr
 
 import (
+	"fmt"
+	"math"
 	"math/big"
 
 	"github.com/keep-network/keep-core/pkg/beacon/relay/result"
@@ -34,4 +36,49 @@ func (pm *PublishingMember) PrepareResult() {
 			Inactive:       inactiveMembers,
 		}
 	}
+}
+
+// determinePublishersIDs determines IDs of members eligable to submit the result
+// to the blockchain. It takes into a consideration the number of blocks which has
+// passed during the protocol execution. If protocol execution time did not
+// exceed expected protocol duration, then first group member is eligable to
+// publish the result. If expected protocol duration is exceeded, then next members
+// are added to the eligable publishers. Subsequent members are added to the group
+// as the blocks defined by `blockStep` pass.
+func (pm *PublishingMember) determinePublishersIDs() ([]int, error) {
+	expectedProtocolDuration := pm.protocolConfig.chain.expectedProtocolDuration // t_dkg
+
+	// Current block height.
+	currentBlock, err := pm.protocolConfig.chain.CurrentBlock() // t_now
+	if err != nil {
+		return nil, fmt.Errorf("getting current block height failed [%v]", err)
+	}
+
+	// Time elapsed from protocol execution initialization.
+	// `T_elapsed = T_now - T_init`
+	elapsedBlocks := currentBlock - pm.protocolConfig.chain.initialBlockHeight
+
+	// Determine highest member index eligible to publish the result.
+	var highestMemberIndex int // j
+	// If elapsed time is less than expected protocol execution duration.
+	if elapsedBlocks <= expectedProtocolDuration { // if T_elapsed <= T_dkg
+		highestMemberIndex = 0 // in protocol spec first player is denoted as `j=1`
+	} else {
+		// Current execution time exceeded expected protocol execution duration.
+		surpassBlocks := elapsedBlocks - expectedProtocolDuration // T_over = T_elapsed - T_dkg
+		// j = 1 + ceiling(T_over / T_step)
+		highestMemberIndex = int(math.Ceil(float64(surpassBlocks / pm.protocolConfig.chain.blockStep)))
+	}
+
+	// Select group members with index less or equal the highest member index.
+	var publishersIDs []int
+	for index, groupMemberID := range pm.group.MemberIDs() {
+		if index <= highestMemberIndex { // if j >= i
+			publishersIDs = append(publishersIDs, groupMemberID)
+		} else {
+			break
+		}
+	}
+
+	return publishersIDs, nil
 }
