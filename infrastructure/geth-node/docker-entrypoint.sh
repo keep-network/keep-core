7ffe5acc477eafe1d88db2cec0944eb079579706
lispmeister <markus.fix@keep.network>
2018-11-13 09:17:53 +0400

    fix Dockerfile geth startup parameters
    The resulting container now boots up correctly and starts mining.

--- infrastructure/geth-node/docker-entrypoint.sh
+++ infrastructure/geth-node/docker-entrypoint.sh
@@ -7,15 +7,11 @@ echo "GETH_ETH_ACCOUNT: $GETH_ETH_ACCOUNT"
 export RANDOM_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo "RANDOM_ID: $RANDOM_ID"
 
-export GETH_PARAMETERS='--port 3000 --networkid 1101 \
-    --ws --wsport "8546" --wsorigins "*" \
-    --rpc --rpcport "8545" --rpccorsdomain "" \
+exec "/geth" --port 30303 --networkid 1101 \
+    --ws --wsaddr "0.0.0.0" --wsport 8546 --wsorigins "*" \
+    --rpc --rpcport 8545 --rpcaddr 0.0.0.0 --rpccorsdomain "" \
     --rpcapi "db,ssh,miner,admin,eth,net,web3,personal" \
     --syncmode "fast" \
-    --mine --miner.threads=1'
-export GETH_PARAMETERS="$GETH_PARAMETERS \\
-    --identity $RANDOM_ID \\
-    --miner.etherbase=$GETH_ETH_ACCOUNT"
-echo "GETH_PARAMETERS: $GETH_PARAMETERS"
-
-exec "/geth" $GETH_STATIC
+    --mine --miner.threads=1 \
+    --identity $RANDOM_ID \
+    --miner.etherbase=$GETH_ETH_ACCOUNT
