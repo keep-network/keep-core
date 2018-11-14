e97cc5c1d7aacad8084fa43a6443cee89dc46ac0
Philip Schlump <philip.schlump@keep.network>
2018-11-14 05:30:47 -0700

    Add placeholder IsResultPublished method
    Placeholder so that stuff will compile added.

---
+++ pkg/chain/ethereum/isResultPublished.go
@@ -0,0 +1,18 @@
+package ethereum
+
+import "github.com/keep-network/keep-core/pkg/beacon/relay/result"
+
+func (c *localChain) IsResultPublished(result *result.Result) bool {
+	resultHash := result.Hash()
+
+	// Placeholder FIXME.
+	/*
+		for _, r := range c.submittedResults {
+			if reflect.DeepEqual(r, resultHash) {
+				return true
+			}
+		}
+	*/
+
+	return false
+}
