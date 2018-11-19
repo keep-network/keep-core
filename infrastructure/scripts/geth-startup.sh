77f4ba365f5d19aca9f3ace872616d43231ce9b4
lispmeister <markus.fix@keep.network>
2018-11-19 17:33:44 +0400

    add first draft of geth cluster with mining-node, tx-node and dashboard
    Mining and TX nodes come up correctly. Dashboard still needs work.

--- infrastructure/scripts/geth-startup.sh
+++ infrastructure/scripts/geth-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-kubectl create -f infrastructure/kube/dashboard.yaml
-kubectl create -f infrastructure/kube/miner-nodes.yaml
-kubectl create -f infrastructure/kube/tx-nodes.yaml
+kubectl create -f infrastructure/kube/lcl/dashboard.yaml
+kubectl create -f infrastructure/kube/lcl/miner-nodes.yaml
+kubectl create -f infrastructure/kube/lcl/tx-nodes.yaml
