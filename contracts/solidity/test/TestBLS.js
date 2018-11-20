e458b424eb90c5a76866a4fd8b0fdbbd8cae37ab
Nik G <subzeromedia@gmail.com>
2018-11-20 12:26:28 -0500

    Minor comment improvements

--- contracts/solidity/test/TestBLS.js
+++ contracts/solidity/test/TestBLS.js
@@ -9,7 +9,7 @@ contract('TestBLS', function() {
 
   it("should be able to verify threshold BLS recovered/reconstructed signature", async function() {
 
-    // Data generated using 'keep-core/pkg/bls' with master secret key 123 and message "Hello!"
+    // Data generated using client Go code with master secret key 123 and message "Hello!"
     let result = await bls.verify(
       "0x1f1954b33144db2b5c90da089e8bde287ec7089d5d6433f3b6becaefdb678b1b2a9de38d14bef2cf9afc3c698a4211fa7ada7b4f036a2dfef0dc122b423259d0",
       "0x48656c6c6f21",
@@ -20,7 +20,7 @@ contract('TestBLS', function() {
 
   it("should be able to verify BLS aggregated signature", async function() {
 
-    // Data generated using 'keep-core/pkg/bls' with multiple random signers signing the same message "Hello!"
+    // Data generated using client Go code with multiple random signers signing the same message "Hello!"
     let result = await bls.verify(
       "0x05c188c72f44373a42008c55499dfb4eb8944d89a62b10cc395c6cf11acff7c71f96fdd34a73284dec3126d19db9dfa2fb07752709818ce58a5a539c62ff09ae",
       "0x48656c6c6f21",
