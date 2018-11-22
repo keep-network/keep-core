b613e9c15f24ec18f754b0d50fbdec41c9ff42a4
Raghav Gulati <raghav.gulati@keep.network>
2018-11-21 16:09:29 -0800

    Minor improvements to message buffer docs

--- pkg/beacon/relay/gjkr/message_buffer.go
+++ pkg/beacon/relay/gjkr/message_buffer.go
@@ -2,10 +2,10 @@ package gjkr
 
 import "github.com/keep-network/keep-core/pkg/net/ephemeral"
 
-// For the sake of complaint resolution, group members need to have an access
-// to messages exchanged between the accuser and the accused party. There are
-// two situations in DKG protocol when group member generates values
-// individually for each other group member:
+// For complaint resolution, group members need to have access to messages
+// exchanged between the accuser and the accused party. There are two situations
+// in the DKG protocol where group members generate values for every other group
+// member:
 //
 // - Ephemeral key generation (phase 1) - each group member generates an
 // ephemeral keypair for each other group member and broadcast all the ephemeral
@@ -13,19 +13,17 @@ import "github.com/keep-network/keep-core/pkg/net/ephemeral"
 // resolution need to validate private ephemeral key revealed by the accuser.
 // To perform the validation, members need to compare public ephemeral key
 // published by the accuser in phase 1 with the private ephemeral key published
-// by the accuser with the complaint.
+// by the accuser in the complaint.
 //
 // - Polynomial generation (phase 3) - each group member generates two sharing
-// polynomials and calculates shares as points on these polynomials individually
-// for each other group member. Shares are publicly broadcast, encrypted with
-// a symmetric key established between the sender and receiver. In case of an
-// accusation, members performing compliant resolution need to look at the
-// shares sent by the accused party. To do that, they read the round 3 message
-// from the buffer passing the symmetric key used between the accuser and
-// accused party so that round 3 message from the accused party can be
-// decrypted.
+// polynomials, and calculates shares as points on these polynomials for each
+// other group member. Shares are publicly broadcast, encrypted with a symmetric
+// key established between the sender and receiver. In the case of an accusation,
+// members performing compliant resolution need to look at the shares sent by the
+// accused party. To do this, they read the round 3 message from the buffer,
+// passing the symmetric key used between the accuser and accused so that the
+// round 3 message from the accused party can be decrypted.
 type messageBuffer interface {
-
 	// ephemeralPublicKeyMessage returns the `EphemeralPublicKeyMessage`
 	// broadcast in the first protocol round by the given sender for the
 	// given receiver.
