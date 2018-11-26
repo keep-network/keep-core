fc1bb6175c331afe3377fd71b4268071627b276e
Raghav Gulati <raghav.gulati@keep.network>
2018-11-26 09:37:27 -0800

    Update messageBuffer docs
    Clarify that this is implemented for phase 2 of DKG as that's where the
    receive occurs, though what we are receving is Phase 1 specific
    messages.

--- pkg/beacon/relay/gjkr/message_buffer.go
+++ pkg/beacon/relay/gjkr/message_buffer.go
@@ -7,13 +7,14 @@ import "github.com/keep-network/keep-core/pkg/net/ephemeral"
 // in the DKG protocol where group members generate values for every other group
 // member:
 //
-// - Ephemeral key generation (phase 1) - each group member generates an
-// ephemeral keypair for each other group member and broadcast all the ephemeral
-// public keys publicly. In case of an accusation, members performing compliant
-// resolution need to validate private ephemeral key revealed by the accuser.
-// To perform the validation, members need to compare public ephemeral key
-// published by the accuser in phase 1 with the private ephemeral key published
-// by the accuser in the complaint.
+// - Ephemeral ECDH (phase 2) - after each group member generates an ephemeral
+// keypair for each other group member and broadcasts those ephemeral public keys
+// in the clear (phase 1), group members must ECDH those public keys with the
+// ephemeral private key for that group member to derive a symmetric key.
+// In the case of an accusation, members performing compliant resolution need to
+// validate the private ephemeral key revealed by the accuser. To perform the
+// validation, members need to compare public ephemeral key published by the
+// accuser in phase 1 with the private ephemeral key published by the accuser.
 //
 // - Polynomial generation (phase 3) - each group member generates two sharing
 // polynomials, and calculates shares as points on these polynomials for each
