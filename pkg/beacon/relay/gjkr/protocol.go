9e29731432616a7446f57bbc4f5ac8faf3e44ace
Raghav Gulati <raghav.gulati@keep.network>
2018-11-27 12:55:06 -0800

    Embed previous state, which contains membercore
    Embed EphemeralKeyGeneratingMember in SymmetricKeyGeneratingMember.
    Update code and tests to work with this change.

--- pkg/beacon/relay/gjkr/protocol.go
+++ pkg/beacon/relay/gjkr/protocol.go
@@ -70,7 +70,7 @@ func (sm *SymmetricKeyGeneratingMember) GenerateSymmetricKeys(
 	for _, ephemeralPubKeyMessage := range ephemeralPubKeyMessages {
 		otherMemberID := ephemeralPubKeyMessage.senderID
 		// Find the ephemeral key pair for this group member.
-		ephemeralKeyPair, ok := sm.ephemeralKeyPairs[otherMemberID]
+		ephemeralKeyPair, ok := sm.ephemeralKeys[otherMember]
 		if !ok {
 			return fmt.Errorf(
 				"ephemeral key pair does not exist for %d",
