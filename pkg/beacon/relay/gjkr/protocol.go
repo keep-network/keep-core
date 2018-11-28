792fe8ed199b640b4902ec5baef886cbef466a48
Raghav Gulati <raghav.gulati@keep.network>
2018-11-28 14:10:10 -0800

    Missed this otherMember -> otherMemberID in rebase

--- pkg/beacon/relay/gjkr/protocol.go
+++ pkg/beacon/relay/gjkr/protocol.go
@@ -70,7 +70,7 @@ func (sm *SymmetricKeyGeneratingMember) GenerateSymmetricKeys(
 	for _, ephemeralPubKeyMessage := range ephemeralPubKeyMessages {
 		otherMemberID := ephemeralPubKeyMessage.senderID
 		// Find the ephemeral key pair for this group member.
-		ephemeralKey, ok := sm.ephemeralKeys[otherMember]
+		ephemeralKey, ok := sm.ephemeralKeys[otherMemberID]
 		if !ok {
 			return fmt.Errorf(
 				"ephemeral key pair does not exist for %d",
