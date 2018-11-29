d7d26abea95cdb8fb77625c2a3d1aa8b217afed4
Raghav Gulati <raghav.gulati@keep.network>
2018-11-28 22:34:12 -0800

    Add test for Phase 1 and 2
    This test takes us from Phase 1 to Phase 2 by:
     * Generating ephemeral key pairs between group members.
     * Constructing messages to broadcast.
     * Validating that we generated the correct number of keys and messages
       to send.
     * Simulating the broadcast and receiving of the messages.
     * Changing the state from EphemeralKeyGeneratingMember to
       SymmetricKeyGeneratingMember (Phase 2).
     * Generating symmetric keys between group members using the received
       messages.
     * Validating that we generated the correct number of symmetric keys.

--- pkg/beacon/relay/gjkr/protocol_ecdh_test.go
+++ pkg/beacon/relay/gjkr/protocol_ecdh_test.go
@@ -2,6 +2,8 @@ package gjkr
 
 import (
 	"fmt"
+	"reflect"
+	"testing"
 
 	"github.com/keep-network/keep-core/pkg/net/ephemeral"
 )
@@ -55,7 +57,6 @@ func initializeSymmetricKeyMembersGroup(
 	for _, member1 := range members {
 		for _, member2 := range members {
 			if member1.ID != member2.ID {
-
 				privKey := member1.ephemeralKeys[member2.ID].PrivateKey
 				pubKey := member2.ephemeralKeys[member1.ID].PublicKey
 				member1.symmetricKeys[member2.ID] = privKey.Ecdh(pubKey)
@@ -64,4 +65,126 @@ func initializeSymmetricKeyMembersGroup(
 	}
 
 	return members, nil
+}
+
+func TestGenerateSymmetricKeyGroup(t *testing.T) {
+	groupSize := 3
+	group := &Group{
+		groupSize: groupSize,
+	}
+
+	// Create a group of 3 members
+	var ephemeralGeneratingMembers []*EphemeralKeyGeneratingMember
+	for i := 1; i <= groupSize; i++ {
+		id := MemberID(i)
+		ephemeralGeneratingMembers = append(ephemeralGeneratingMembers,
+			&EphemeralKeyGeneratingMember{
+				memberCore: &memberCore{
+					ID:             id,
+					group:          group,
+					protocolConfig: nil,
+				},
+				ephemeralKeys: make(map[MemberID]*ephemeral.KeyPair),
+			},
+		)
+		group.RegisterMemberID(id)
+	}
+
+	// generate ephemeral key pairs for each group member; prepare messages
+	broadcastedPubKeyMessages := make(map[MemberID][]*EphemeralPublicKeyMessage)
+	for _, ephemeralGeneratingMember := range ephemeralGeneratingMembers {
+		messages, err := ephemeralGeneratingMember.GenerateEphemeralKeyPair()
+		if err != nil {
+
+		}
+		broadcastedPubKeyMessages[ephemeralGeneratingMember.ID] = messages
+	}
+
+	// We should have groupSize members.
+	members := reflect.ValueOf(broadcastedPubKeyMessages).MapKeys()
+	if len(members) != groupSize {
+		t.Fatalf(
+			"expected messages for %d members, got %d members",
+			groupSize,
+			len(members),
+		)
+	}
+
+	for memberID, ephemeralPubKeyMessages := range broadcastedPubKeyMessages {
+		// We should have groupSize - 1 messages per member.
+		if len(ephemeralPubKeyMessages) != groupSize-1 {
+			t.Fatalf(
+				"expected %d messages, got %d messages",
+				groupSize-1,
+				len(ephemeralPubKeyMessages),
+			)
+		}
+
+		for _, message := range ephemeralPubKeyMessages {
+			// We should always be the sender of our own messages
+			if message.senderID != memberID {
+				t.Fatalf("message from incorrect sender got %v want %v",
+					message.senderID,
+					memberID,
+				)
+			}
+
+			// We should never have a message addressed to ourselves
+			if message.receiverID == memberID {
+				t.Fatal("found message addressed to self")
+			}
+		}
+	}
+
+	// Simulate the each member receiving all messages from the network
+	receivedPubKeyMessages := make(map[MemberID][]*EphemeralPublicKeyMessage)
+	for memberID, ephemeralPubKeyMessages := range broadcastedPubKeyMessages {
+		for _, otherMember := range ephemeralGeneratingMembers {
+			// We would only receive messages from the other members
+			if memberID != otherMember.ID {
+				receivedPubKeyMessages[otherMember.ID] = append(
+					receivedPubKeyMessages[otherMember.ID],
+					ephemeralPubKeyMessages...,
+				)
+			}
+		}
+	}
+
+	// Move to the next phase, using the previous phase as state
+	var symmetricGeneratingMembers []*SymmetricKeyGeneratingMember
+	for _, member := range ephemeralGeneratingMembers {
+		symmetricKeyMap := make(map[MemberID]ephemeral.SymmetricKey)
+		symmetricGeneratingMembers = append(symmetricGeneratingMembers,
+			&SymmetricKeyGeneratingMember{
+				EphemeralKeyGeneratingMember: member,
+				symmetricKeys:                symmetricKeyMap,
+			},
+		)
+	}
+
+	// For each member, attempt to generate a symmetric key
+	for _, symmetricGeneratingMember := range symmetricGeneratingMembers {
+		if err := symmetricGeneratingMember.GenerateSymmetricKeys(
+			receivedPubKeyMessages[symmetricGeneratingMember.ID],
+		); err != nil {
+			t.Fatalf(
+				"failed to generate symmetric key with error %v",
+				err,
+			)
+		}
+	}
+
+	// Ensure that for each member, we generated the correct number of
+	// symmetric keys (groupSize - 1 keys)
+	for _, symmetricGeneratingMember := range symmetricGeneratingMembers {
+		symmetricKeys := symmetricGeneratingMember.symmetricKeys
+		keySlice := reflect.ValueOf(symmetricKeys).MapKeys()
+		if len(keySlice) != groupSize-1 {
+			t.Fatalf(
+				"expected %d keys, got %d keys",
+				groupSize-1,
+				len(keySlice),
+			)
+		}
+	}
 }
