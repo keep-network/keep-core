673f254fbfce4f40e360ddb8aa83aa8d13101cde
Raghav Gulati <raghav.gulati@keep.network>
2018-12-04 16:02:35 -0800

    Implement calculate ticket, group selection phase1
    Calculate ticket uses sha3-256 as the psuedorandom function to hash the
    proof of eligibility. The hash and Proofs go into a ticket which will be
    later submitted on-chain.

---
+++ pkg/beacon/membership/protocol.go
@@ -0,0 +1,49 @@
+// Package membership contains code that implements the Random Beacon Group
+// Selection protocol as described in
+// http://docs.keep.network/the-beaconness-of-keep/random-beacon.pdf
+package membership
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+)
+
+// Ticket is a message containing a pseudorandomly generated value, W_k, which is
+// used to determine whether a given virtual staker is eligible for the group P
+// (the lowest N tickets will be chosen) and a proof of the validity of the value
+type Ticket struct {
+	Value [sha256.Size]byte
+
+	// Proof
+	PreviousBeaconValue []byte
+	StakerValue         []byte // Staker-specific value, Q_j
+	VirtualStakerIndex  uint64
+}
+
+// CalculateTicket generates a Ticket from the previous beacon output, the
+// staker's ECDSA public key, and the virtual staker index. Validation is done
+// outside of this function, and it is intended to be called in a loop, ranging
+// over the list of virtual stakers.
+
+// See Phase 1 of the Group Selection protocol specification.
+func (s *Staker) CalculateTicket(
+	beaconOutput []byte,
+	virtualStakerIndex uint64,
+) *Ticket {
+	var combinedProof []byte
+
+	combinedProof = append(combinedProof, beaconOutput...)
+	combinedProof = append(combinedProof, s.PubKey.SerializeCompressed()...)
+	binary.LittleEndian.PutUint64(
+		combinedProof[len(combinedProof)-1:],
+		virtualStakerIndex,
+	)
+	value := sha256.Sum256(combinedProof[:])
+
+	return &Ticket{
+		Value:               value,
+		PreviousBeaconValue: beaconOutput,
+		StakerValue:         s.PubKey.SerializeCompressed(),
+		VirtualStakerIndex:  virtualStakerIndex,
+	}
+}
