023517e140a502fcaccc87af1c6145a30b0153cb
Philip Schlump <philip.schlump@keep.network>
2018-12-05 06:12:15 -0700

    3 more files match with current phase 13

--- pkg/beacon/relay/gjkr/member.go
+++ pkg/beacon/relay/gjkr/member.go
@@ -3,8 +3,9 @@ package gjkr
 import (
 	"math/big"
 
-	"github.com/keep-network/keep-core/pkg/beacon/relay/pedersen"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/result"
+
+	"github.com/keep-network/keep-core/pkg/beacon/relay/pedersen"
 )
 
 type memberCore struct {
@@ -20,8 +21,6 @@ type memberCore struct {
 // it has a full list of `memberIDs` that belong to its threshold group. A
 // member in this state has two maps of member shares for each member of the
 // group.
-//
-// Executes Phase 3 and Phase 4 of the protocol.
 type CommittingMember struct {
 	*memberCore
 
@@ -38,22 +37,18 @@ type CommittingMember struct {
 	//
 	// These are private values and should not be exposed.
 	selfSecretShareS, selfSecretShareT *big.Int
-	// Shares calculated for the current member by peer group members which passed
-	// the validation.
+	// Shares calculated for the current member by peer group members.
 	//
-	// receivedValidSharesS are defined as `s_ji` and receivedValidSharesT are
+	// receivedSharesS are defined as `s_ji` and receivedSharesT are
 	// defined as `t_ji` across the protocol specification.
-	receivedValidSharesS, receivedValidSharesT map[int]*big.Int
-	// Commitments to coefficients received from peer group members which passed
-	// the validation.
-	receivedValidPeerCommitments map[int][]*big.Int
+	receivedSharesS, receivedSharesT map[int]*big.Int
+	// Commitments to coefficients received from peer group members.
+	receivedCommitments map[int][]*big.Int
 }
 
 // SharesJustifyingMember represents one member in a threshold key sharing group,
 // after it completed secret shares and commitments verification and enters
 // justification phase where it resolves invalid share accusations.
-//
-// Executes Phase 5 of the protocol.
 type SharesJustifyingMember struct {
 	*CommittingMember
 }
@@ -61,8 +56,6 @@ type SharesJustifyingMember struct {
 // QualifiedMember represents one member in a threshold key sharing group, after
 // it completed secret shares justification. The member holds a share of group
 // master private key.
-//
-// Executes Phase 6 of the protocol.
 type QualifiedMember struct {
 	*SharesJustifyingMember
 
@@ -75,42 +68,16 @@ type QualifiedMember struct {
 // SharingMember represents one member in a threshold key sharing group, after it
 // has been qualified to the master private key sharing group. A member shares
 // public values of it's polynomial coefficients with peer members.
-//
-// Executes Phase 7 and Phase 8 of the protocol.
 type SharingMember struct {
 	*QualifiedMember
 
 	// Public values of each polynomial `a` coefficient defined in secretCoefficients
-	// field. It is denoted as `A_ik` in protocol specification. The zeroth
-	// public key share point `A_i0` is a member's public key share.
-	publicKeySharePoints []*big.Int
-	// Public key share points received from peer group members which passed the
-	// validation. Defined as `A_jk` across the protocol documentation.
-	receivedValidPeerPublicKeySharePoints map[int][]*big.Int
+	// field. It is denoted as `A_ik` in protocol specification.
+	publicCoefficients []*big.Int
 }
 
-// PointsJustifyingMember represents one member in a threshold key sharing group,
-// after it completed public key share points verification and enters justification
-// phase where it resolves public key share points accusations.
-//
-// Executes Phase 9 of the protocol.
-type PointsJustifyingMember struct {
-	*SharingMember
-}
-
-// PublishingMember represents one member in a threshold key sharing group,
-// after it completed public key share points justification and proceeds to
-// result publication phase.
-//
-// Executes Phase 13 of the protocol.
 type PublishingMember struct {
-	*PointsJustifyingMember
+	*SharingMember
 
 	result *result.Result
 }
-
-//type PublishingMember struct {
-//	*SharingMember
-//
-//	result *result.Result
-//}
