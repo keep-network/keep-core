05dad4f0c987979477e18b7f2e6644c621043933
Raghav Gulati <raghav.gulati@keep.network>
2018-12-05 20:44:03 -0800

    Use by reference instead of by value
    Otherwise we can't assign correctly to this field!

--- pkg/beacon/membership/staker.go
+++ pkg/beacon/membership/staker.go
@@ -8,14 +8,14 @@ import (
 
 // Staker represents an on-chain identity and staked amount.
 type Staker struct {
-	PubKey btcec.PublicKey // Q_j
+	PubKey *btcec.PublicKey // Q_j
 
 	// A staker's VirtualStakers is how many minimum-stake stakers a given
 	// actual staker could form if they were to blitzpants their stake.
 	VirtualStakers uint64
 }
 
-func NewStaker(pubKey btcec.PublicKey, weight uint64) *Staker {
+func NewStaker(pubKey *btcec.PublicKey, weight uint64) *Staker {
 	return &Staker{
 		PubKey:         pubKey,
 		VirtualStakers: weight,
