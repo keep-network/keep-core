80afb814f4567c477e9984ac3bf0fde542a9ebcf
Raghav Gulati <raghav.gulati@keep.network>
2018-12-07 08:54:41 -0500

    Introduce intial group selection 2a algorithm
    This method is called by the relay on a new relay entry. On a new relay
    entry, we have the chance to submit _virtual_staker_ number of tickets
    for consideration.

--- pkg/beacon/relay/group_selection.go
+++ pkg/beacon/relay/group_selection.go
@@ -1,22 +1,78 @@
 package relay
 
-import "math/big"
+import (
+	"context"
+	"fmt"
+	"math/big"
 
-var naturalThreshold = big.NewInt((2 ^ 256) - 1)
+	"github.com/keep-network/keep-core/pkg/beacon/relay/candidate"
+	relaychain "github.com/keep-network/keep-core/pkg/beacon/relay/chain"
+	"github.com/keep-network/keep-core/pkg/chain"
+)
 
-func (n *Node) SubmitTicketsForGroupSelection(entryValue []byte) error {
+const TicketInitialTimeout = 5
+
+var NaturalThreshold = big.NewInt((2 ^ 256) - 1)
+
+func (n *Node) SubmitTicketsForGroupSelection(
+	entryValue []byte,
+	relayChain relaychain.Interface,
+	blockCounter chain.BlockCounter,
+) error {
 	tickets, err := n.Staker.GenerateTickets(entryValue)
 	if err != nil {
 		return err
 	}
 
-	for _, ticket := range tickets {
-		// submit the tickets that fall under the natural threshold
-		if ticket.Value.Int().Cmp(naturalThreshold) < 0 {
-			// publish the result
+	ctx, cancel := context.WithCancel(context.Background())
+	errCh := make(chan error, len(tickets))
+
+	go func(
+		cancel context.CancelFunc,
+		tickets candidate.Tickets,
+		relayChain relaychain.Interface,
+		blockCounter chain.BlockCounter,
+		errCh chan error,
+	) {
+		initialTimeout, err := blockCounter.BlockWaiter(
+			TicketInitialTimeout,
+		)
+		if err != nil {
+			errCh <- fmt.Errorf(
+				"failed to initialize blockCounter with err %v",
+				err,
+			)
+			return
+		}
+		for _, ticket := range tickets {
+			// submit the tickets that fall under the natural threshold
+			if ticket.Value.Int().Cmp(NaturalThreshold) < 0 {
+				// publish the result
+				relayChain.SubmitTicket(
+					ticket,
+				).OnFailure(func(err error) {
+					errCh <- err
+				})
+			}
+			select {
+			// if TicketInitialTimeout blocks have passed, close the context
+			case <-initialTimeout:
+				cancel()
+				return
+			}
+		}
+	}(cancel, tickets, relayChain, blockCounter, errCh)
+
+	for {
+		select {
+		case err := <-errCh:
+			// TODO: log this error
+			fmt.Println(err)
+		case <-ctx.Done():
+			close(errCh)
+			return nil
 		}
 	}
 
-	// Ensure this state terminates at TICKET_INITAL_TIMEOUT
 	return nil
 }
