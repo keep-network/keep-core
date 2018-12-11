8ddde1f8ef621d46f40dcb8f2900b58f7b787ae7
Philip Schlump <philip.schlump@keep.network>
2018-12-11 10:00:49 -0700

    Implement and test Hash() function
    Instead of returning constant implement the hash function.
    Add test for the serialization and hashing.

---
+++ pkg/beacon/relay/chain/hash_test.go
@@ -0,0 +1,43 @@
+package chain
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+)
+
+func Test_searialize(t *testing.T) {
+
+	r1 := &DKGResult{
+		Success:        true,
+		GroupPublicKey: big.NewInt(100),
+		Disqualified:   []bool{true, false, true, false},
+		Inactive:       []bool{false, false, true, false},
+	}
+
+	actualResult := r1.searialize()
+	expectedResult := "013130300100010000000100"
+
+	if fmt.Sprintf("%x", actualResult) != expectedResult {
+		t.Errorf("\nexpected: %v\nactual:   %v\n", expectedResult, actualResult)
+	}
+
+}
+
+func Test_Hash(t *testing.T) {
+
+	r1 := &DKGResult{
+		Success:        true,
+		GroupPublicKey: big.NewInt(100),
+		Disqualified:   []bool{true, false, true, false},
+		Inactive:       []bool{false, false, true, false},
+	}
+
+	actualResult := r1.Hash()
+	expectedResult := "cc4140787d3c888c67575792bc377020def9ec654c7fc19d8dbfe6c19becbe6d"
+
+	if fmt.Sprintf("%x", actualResult) != expectedResult {
+		t.Errorf("\nexpected: %v\nactual:   %v\n", expectedResult, actualResult)
+	}
+
+}
