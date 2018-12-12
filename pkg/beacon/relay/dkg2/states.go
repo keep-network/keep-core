139d92db5ff18b24c14067dcf36207a3768e7fbc
Piotr Dyraga <piotr.dyraga@akena.engineering>
2018-12-12 11:15:31 +0100

    Ephemeral key generation state for GJKR DKG state machine

--- pkg/beacon/relay/dkg2/states.go
+++ pkg/beacon/relay/dkg2/states.go
@@ -37,10 +37,8 @@ func (is *initializationState) nextState() (keyGenerationState, error) {
 }
 
 // joinState is the state during which a member announces itself to the key
-// generation broadcast channel to initiate the distributed protocol. Join
-// messages from other members are valid in this state, and when the member is
-// ready and activePeriod has elapsed, it proceeds to
-// ephemeralKeyPairGeneratingState.
+// generation broadcast channel to initiate the distributed protocol.
+// `JoinMessage`s from other members are valid in this state.
 type joinState struct {
 	channel net.BroadcastChannel
 	member  *gjkr.EphemeralKeyPairGeneratingMember
@@ -68,5 +66,58 @@ func (js *joinState) receive(msg net.Message) error {
 }
 
 func (js *joinState) nextState() (keyGenerationState, error) {
+	return &ephemeralKeyPairGeneratingState{
+		js.channel,
+		js.member,
+		make([]*gjkr.EphemeralPublicKeyMessage, 0),
+	}, nil
+}
+
+// ephemeralKeyPairGeneratingState is the state during which members broadcast
+// publish ephemeral keys generated for each other member in the group.
+// `EphemeralPublicKeyMessage`s from other members are valid in this state.
+type ephemeralKeyPairGeneratingState struct {
+	channel       net.BroadcastChannel
+	member        *gjkr.EphemeralKeyPairGeneratingMember
+	phaseMessages []*gjkr.EphemeralPublicKeyMessage
+}
+
+func (ekpgs *ephemeralKeyPairGeneratingState) activeBlocks() int { return 1 }
+
+func (ekpgs *ephemeralKeyPairGeneratingState) initiate() error {
+	message, err := ekpgs.member.GenerateEphemeralKeyPair()
+	if err != nil {
+		return fmt.Errorf("ephemeral key generation phase failed [%v]", err)
+	}
+
+	if err := ekpgs.channel.Send(message); err != nil {
+		return fmt.Errorf("ephemeral key generation phase failed [%v]", err)
+	}
+	return nil
+}
+func (ekpgs *ephemeralKeyPairGeneratingState) receive(msg net.Message) error {
+	switch publicKeyMessage := msg.Payload().(type) {
+	case *gjkr.EphemeralPublicKeyMessage:
+		if senderID, ok := msg.ProtocolSenderID().(gjkr.MemberID); ok {
+			if senderID == ekpgs.member.ID {
+				return nil // ignore message from self
+			}
+			ekpgs.phaseMessages = append(ekpgs.phaseMessages, publicKeyMessage)
+			return nil
+		}
+
+		return fmt.Errorf(
+			"unknown protocol sender id type [%T]  [%v]",
+			msg.ProtocolSenderID(),
+			msg.TransportSenderID(),
+		)
+	}
+
+	return fmt.Errorf(
+		"unexpected message for ephemeral key generation state: [%#v]",
+		msg,
+	)
+}
+func (ekpgs *ephemeralKeyPairGeneratingState) nextState() (keyGenerationState, error) {
 	return nil, nil
 }
