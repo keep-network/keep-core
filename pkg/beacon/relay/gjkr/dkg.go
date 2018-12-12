f5bc060aa5b5b6ebeadbb11ee85848eaec62e514
Piotr Dyraga <piotr.dyraga@akena.engineering>
2018-12-12 16:36:43 +0100

    Auxiliary function for generating DKG protocol parameters

--- pkg/beacon/relay/gjkr/dkg.go
+++ pkg/beacon/relay/gjkr/dkg.go
@@ -4,12 +4,38 @@ import (
 	crand "crypto/rand"
 	"fmt"
 	"math/big"
+	"time"
+
+	"github.com/keep-network/paillier"
 )
 
 // DKG contains the configuration data needed for the DKG protocol execution.
 type DKG struct {
 	// P, Q are big primes, such that `p = 2q + 1`
 	P, Q *big.Int
+}
+
+// GenerateDKG generates new DKG protocol configuration using randomly chosen
+// safe prime.
+//
+// TODO: It's just a temporary solution. This function should be removed once
+// we switch to elliptic curves.
+func GenerateDKG() (*DKG, error) {
+	bitLength := 256
+	concurrencyLevel := 4
+	timeout := 120 * time.Second
+
+	p, q, err := paillier.GenerateSafePrime(
+		bitLength,
+		concurrencyLevel,
+		timeout,
+		crand.Reader,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate DKG paramters [%v]", err)
+	}
+
+	return &DKG{p, q}, nil
 }
 
 // RandomQ generates a random `big.Int` in range (0, q).
