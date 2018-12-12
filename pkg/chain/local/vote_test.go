104818474b6e9971896c0e98065831941a3a928c
Philip Schlump <philip.schlump@keep.network>
2018-12-12 05:58:50 -0700

    Improve tests for Vote() and GetDKGSubmissions()
    Cleanup of test.

--- pkg/chain/local/vote_test.go
+++ pkg/chain/local/vote_test.go
@@ -1,24 +1,20 @@
 package local
 
 import (
-	//	"context"
-	//	"math/big"
 	"fmt"
 	"math/big"
 	"testing"
-	//	"time"
-	//	"github.com/keep-network/keep-core/pkg/beacon/relay/event"
+
 	relaychain "github.com/keep-network/keep-core/pkg/beacon/relay/chain"
 	"github.com/pschlump/MiscLib"
-	"github.com/pschlump/godebug"
 )
 
 func TestVote(t *testing.T) {
 
-	// func (c *localChain) Vote(requestID *big.Int, dkgResultHash []byte) {
 	tests := map[string]struct {
 		requestID *big.Int
 		dkgResult *relaychain.DKGResult
+		expected  int
 	}{
 		"test increment of votes when match occures": {
 			requestID: big.NewInt(40000),
@@ -28,6 +24,7 @@ func TestVote(t *testing.T) {
 				Disqualified:   []bool{},
 				Inactive:       []bool{},
 			},
+			expected: 2,
 		},
 	}
 
@@ -35,16 +32,18 @@ func TestVote(t *testing.T) {
 
 	for testName, test := range tests {
 		t.Run(testName, func(t *testing.T) {
-			fmt.Printf("%sTest %+v %s\n", MiscLib.ColorGreen, test, MiscLib.ColorReset)
 			promise := local.SubmitDKGResult(test.requestID, test.dkgResult)
 			_ = promise
-			// requestID *big.Int, resultToPublish *relaychain.DKGResult,
 			local.Vote(test.requestID, test.dkgResult.Hash())
 			subs := local.GetDKGSubmissions(test.requestID)
-			fmt.Printf("%s\tSubmissions: %+v%s\n", MiscLib.ColorCyan, subs, MiscLib.ColorReset)
-			fmt.Printf("%s\tVotes: %+v%s\n", MiscLib.ColorCyan, godebug.SVarI(subs), MiscLib.ColorReset)
-			if subs.Submissions[0].Votes != 2 {
+			actual := subs.Submissions[0].Votes
+			if test.expected != actual {
 				fmt.Printf("%s\tError: %+v%s\n", MiscLib.ColorRed, subs, MiscLib.ColorReset)
+				t.Errorf(
+					"\nexpected: [%v]\nactual:   [%v]",
+					expected,
+					actual,
+				)
 			}
 		})
 	}
