6eeb1979e4b363fa9359da103b214b351079c366
Philip Schlump <philip.schlump@keep.network>
2018-12-13 17:49:10 -0700

    Start of testing before adding new interface
    Beginning of testing - will be adding interface
    items to map and get resultID to GroupPublicKey.

---
+++ pkg/beacon/relay/publish/Phase14_test.go
@@ -0,0 +1,104 @@
+package publish
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+
+	relayChain "github.com/keep-network/keep-core/pkg/beacon/relay/chain"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/gjkr"
+	"github.com/keep-network/keep-core/pkg/chain"
+	"github.com/keep-network/keep-core/pkg/chain/local"
+	"github.com/pschlump/godebug"
+)
+
+func TestPhase14_pt1(t *testing.T) {
+	threshold := 2
+	groupSize := 5
+	blockStep := 2 // T_step
+
+	chainHandle, initialBlock, err := initChainHandle2(threshold, groupSize)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resultToPublish := &relayChain.DKGResult{
+		GroupPublicKey: big.NewInt(12345),
+	}
+	_ = resultToPublish
+
+	var tests = map[string]struct {
+		correctResult   *relayChain.DKGResult
+		publishingIndex int
+		xyzzy           int
+	}{
+		"base call": {
+			correctResult:   resultToPublish,
+			publishingIndex: 0,
+			xyzzy:           0,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			fmt.Printf("Running Test: [%s] %s\n", testName, godebug.SVarI(test))
+
+			publisher := &Publisher{
+				ID:              gjkr.MemberID(test.publishingIndex + 1),
+				RequestID:       big.NewInt(101),
+				publishingIndex: test.publishingIndex,
+				chainHandle:     chainHandle,
+				blockStep:       blockStep,
+			}
+
+			// Reinitialize chain to reset block counter
+			publisher.chainHandle, initialBlock, err = initChainHandle2(threshold, groupSize)
+			if err != nil {
+				t.Fatalf("chain initialization failed [%v]", err)
+			}
+
+			chainRelay := publisher.chainHandle.ThresholdRelay()
+			_ = chainRelay
+
+			blockCounter, err := publisher.chainHandle.BlockCounter()
+			if err != nil {
+				t.Fatalf("unexpected error [%v]", err)
+			}
+			_ = blockCounter
+
+			if false {
+				// func (pm *Publisher) Phase14(correctResult *relayChain.DKGResult) error {
+				publisher.Phase14(test.correctResult)
+			}
+		})
+	}
+}
+
+/*
+TODO:
+1. Figure out setup for local - that includes
+	1. Setup of requestID -> groupPublicKey
+	2. Create machine to test this (clone old one)
+
+Add To Local
+	4. Add a "MapRequestIDToAssociateGroupPubKeyWith" -> Has to be on chain
+	5. Add a "GetGroupPubKeyForRequestID" - pull back data from chain
+*/
+
+func initChainHandle2(threshold, groupSize int) (chainHandle chain.Handle, initialBlock int, err error) {
+	chainHandle = local.Connect(groupSize, threshold)
+	blockCounter, err := chainHandle.BlockCounter() // PJS - save blockCounter?
+	if err != nil {
+		return nil, -1, err
+	}
+	err = blockCounter.WaitForBlocks(1)
+	if err != nil {
+		return nil, -1, err
+	}
+
+	initialBlock, err = blockCounter.CurrentBlock() // PJS - need CurrentBlock to make this work
+	if err != nil {
+		return nil, -1, err
+	}
+	return
+}
---
+++ pkg/beacon/relay/publish/publish_result.go
@@ -0,0 +1,173 @@
+package publish
+
+import (
+	"fmt"
+	"math/big"
+
+	relayChain "github.com/keep-network/keep-core/pkg/beacon/relay/chain"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/event"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/gjkr"
+	"github.com/keep-network/keep-core/pkg/chain"
+)
+
+// Publisher is a member submitting distributed key generation result to a
+// blockchain.
+type Publisher struct {
+	ID gjkr.MemberID
+	// ID of distributed key generation execution.
+	RequestID *big.Int
+	// Handle to interact with a blockchain.
+	chainHandle chain.Handle
+	// Sequential number of the current member in the publishing group.
+	// The value is used to determine eligible publishing member. Relates to DKG
+	// Phase 13.
+	publishingIndex int
+	// Predefined step for each publishing window. The value is used to determine
+	// eligible publishing member. Relates to DKG Phase 13.
+	blockStep int
+
+	// PHASE 14
+	conflictDuration int // T_conflict
+}
+
+// PublishDKGResult sends a result containing i.a. group public key to the blockchain.
+// It checks if the result has already been published to the blockchain with
+// request ID specific for current DKG execution. If not it determines if the
+// current member is eligable to result submission. If allowed it submits the
+// results to the blockchain. The function returns result published
+// to the blockchain containing ID of the member who published it.
+//
+// See Phase 13 of the protocol specification.
+func (pm *Publisher) PublishDKGResult(resultToPublish *relayChain.DKGResult) error {
+	chainRelay := pm.chainHandle.ThresholdRelay()
+
+	onPublishedResultChan := make(chan *event.DKGResultPublication)
+	chainRelay.OnDKGResultPublished(func(publishedResult *event.DKGResultPublication) {
+		onPublishedResultChan <- publishedResult
+	})
+
+	// Check if the result has already been published to the chain.
+	if chainRelay.IsDKGResultPublished(pm.RequestID, resultToPublish) {
+		return nil // TODO What should we return here? Should it be an error?
+	}
+
+	blockCounter, err := pm.chainHandle.BlockCounter()
+	if err != nil {
+		return fmt.Errorf("block counter failure [%v]", err)
+	}
+
+	// Waits until the current member is eligible to submit a result to the
+	// blockchain.
+	eligibleToSubmitWaiter, err := blockCounter.BlockWaiter(
+		pm.publishingIndex * pm.blockStep,
+	)
+	if err != nil {
+		return fmt.Errorf("block waiter failure [%v]", err)
+	}
+
+	for {
+		select {
+		case <-eligibleToSubmitWaiter:
+			errors := make(chan error)
+			chainRelay.SubmitDKGResult(pm.RequestID, resultToPublish).
+				OnComplete(func(resultPublicationEvent *event.DKGResultPublication, err error) {
+					errors <- err
+				})
+			return <-errors
+		case publishedResultEvent := <-onPublishedResultChan:
+			if publishedResultEvent.RequestID.Cmp(pm.RequestID) == 0 {
+				if chainRelay.IsDKGResultPublished(pm.RequestID, resultToPublish) {
+					return nil
+				}
+			}
+		}
+	}
+}
+
+/*
+// Phase14 - PHASE 14
+func (pm *Publisher) BlockCounter() (int, error) {
+	blockCounter, err := pm.chainHandle.BlockCounter()
+	if err != nil {
+		return 0, fmt.Errorf("block counter failure [%v]", err)
+	}
+	return blockCounter, nil
+}
+*/
+
+// Phase14 - PHASE 14
+func (pm *Publisher) Phase14(correctResult *relayChain.DKGResult) error {
+	chainRelay := pm.chainHandle.ThresholdRelay()
+
+	onVoteChan := make(chan *event.DKGResultVote)
+	chainRelay.OnDKGResultVote(func(vote *event.DKGResultVote) {
+		onVoteChan <- vote
+	})
+	onSubmissionChan := make(chan *event.DKGResultPublication)
+	chainRelay.OnDKGResultPublished(func(result *event.DKGResultPublication) {
+		onSubmissionChan <- result
+	})
+
+	submissions := chainRelay.GetDKGSubmissions(pm.RequestID)
+	if !checkVotingThreshold(submissions) {
+		return fmt.Errorf("voting threshold exceeded")
+	}
+	if !submissions.Contains(correctResult) {
+		chainRelay.SubmitDKGResult(pm.RequestID, correctResult)
+		return nil
+	}
+
+	blockCounter, err := pm.chainHandle.BlockCounter()
+	if err != nil {
+		return fmt.Errorf("block counter failure [%v]", err)
+	}
+
+	// TODO We wait for T_conflict blocks but the protocol specification states
+	// that we should wait for block `T_first + T_conflict`. Need clarification.
+	phaseDurationWaiter, err := blockCounter.BlockWaiter(pm.conflictDuration)
+	if err != nil {
+		return fmt.Errorf("block waiter failure [%v]", err)
+	}
+
+	votesAndSubmissions := func(chainRelay relayChain.Interface) (bool, error) {
+		submissions := chainRelay.GetDKGSubmissions(pm.RequestID)
+		if !checkVotingThreshold(submissions) {
+			return true, fmt.Errorf("voting threshold exceeded")
+		}
+
+		if !submissions.Lead().DKGResult.Equals(correctResult) {
+			chainRelay.Vote(pm.RequestID, correctResult.Hash())
+			return true, nil
+		} else if !submissions.Contains(correctResult) {
+			chainRelay.SubmitDKGResult(pm.RequestID, correctResult)
+			return true, nil
+		}
+		return false, nil
+	}
+
+	for {
+		select {
+		case <-phaseDurationWaiter:
+			return nil
+		case vote := <-onVoteChan:
+			if vote.RequestID.Cmp(pm.RequestID) == 0 {
+				if result, err := votesAndSubmissions(chainRelay); result {
+					return err
+				}
+			}
+		case submission := <-onSubmissionChan:
+			if submission.RequestID.Cmp(pm.RequestID) == 0 {
+				if result, err := votesAndSubmissions(chainRelay); result {
+					return err
+				}
+			}
+		}
+	}
+
+}
+
+func checkVotingThreshold(submissions *relayChain.Submissions) bool {
+	var votingThreshold int // M_max
+	// leadResult.votes > M_max
+	return submissions.Lead().Votes <= votingThreshold
+}
