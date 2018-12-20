d97cb6ef0a48736f5ddcebfb3977da7c39da625c
Philip Schlump <philip.schlump@keep.network>
2018-12-19 19:26:34 -0700

    Implemented the block counter function
    Implemented CurrentBlock in this interface.

--- pkg/chain/ethereum/connect.go
+++ pkg/chain/ethereum/connect.go
@@ -19,6 +19,7 @@ type ethereumChain struct {
 	keepGroupContract        *keepGroup
 	keepRandomBeaconContract *KeepRandomBeacon
 	accountKey               *keystore.Key
+	blockCounter             chain.BlockCounter
 }
 
 // Connect makes the network connection to the Ethereum network.  Note: for
@@ -58,6 +59,15 @@ func Connect(cfg Config) (chain.Handle, error) {
 		clientRPC: clientrpc,
 		clientWS:  clientws,
 	}
+
+	blockCounter, err := pv.BlockCounter()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"error setting up BlockCounter: [%v]",
+			err,
+		)
+	}
+	pv.blockCounter = blockCounter
 
 	keepRandomBeaconContract, err := newKeepRandomBeacon(pv)
 	if err != nil {
