2418c53d481708ec12073aeca71124f95c515b35
Piotr Dyraga <piotr.dyraga@akena.engineering>
2018-12-21 15:43:50 +0100

    Test making sure the unsubscribe handler is called only one time

--- pkg/beacon/relay/event/subscription_test.go
+++ pkg/beacon/relay/event/subscription_test.go
@@ -19,3 +19,19 @@ func TestUnsubscribe(t *testing.T) {
 		t.Fatalf("should be unsubscribed at this point")
 	}
 }
+
+func TestDoNotUnsubscribeTwice(t *testing.T) {
+	unsubscribeCount := 0
+
+	subscription := NewSubscription(func() {
+		unsubscribeCount = unsubscribeCount + 1
+	})
+
+	subscription.Unsubscribe()
+	subscription.Unsubscribe()
+	subscription.Unsubscribe()
+
+	if unsubscribeCount != 1 {
+		t.Fatalf("unsubscribe handler should be called only once")
+	}
+}
