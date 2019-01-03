9d19264ce53abb71a413adc13e5d3121aa2b9bfc
Raghav Gulati <raghav.gulati@keep.network>
2019-01-03 16:49:49 -0500

    Add document for exported parameters
    The MinimumStake and NaturalThresold are exported out of the
    groupselection packge for use in the relay.

--- pkg/beacon/relay/groupselection/parameters.go
+++ pkg/beacon/relay/groupselection/parameters.go
@@ -2,6 +2,8 @@ package groupselection
 
 import "math/big"
 
+// MinimumStake is an on-chain value representing the minimum necessary amount a
+// client must lock up to submit a single ticket
 var MinimumStake = big.NewInt(1)
 
 var tokensTotal = big.NewInt(1000000000) // 1 Billion tokens
@@ -15,6 +17,9 @@ func virtualStakersDistribution(n *big.Int) *big.Int {
 	return big.NewInt(1).Mul(n, spaceTickets)
 }
 
+// NaturalThreshold takes the number of virtual stakers a client wishes to submit
+// tickets for and computes the natural threshold value defined as:
+// floor(N * spaceTickets / (tokensTotal / minimumStake))
 func NaturalThreshold(n *big.Int) *big.Int {
 	return virtualStakersDistribution(n).Quo(
 		spaceTickets, quotientTokensTotalOverMinStake(),
