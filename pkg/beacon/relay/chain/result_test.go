5c0c7b3553f0c1d1bd31b0aacebf11cb10a3bd0e
Philip Schlump <philip.schlump@keep.network>
2019-01-06 19:28:12 -0700

    Move test code and removed bytesEqual
    Replaced bytesEqual with bytes.Equal.

--- pkg/beacon/relay/chain/result_test.go
+++ pkg/beacon/relay/chain/result_test.go
@@ -1,6 +1,7 @@
 package chain
 
 import (
+	"bytes"
 	"math/big"
 	"testing"
 )
@@ -84,5 +85,34 @@ func TestDKGResultEquals(t *testing.T) {
 				t.Fatalf("\nexpected: %v\nactual:   %v\n", test.expectedResult, actualResult)
 			}
 		})
+	}
+}
+
+func TestHash(t *testing.T) {
+	r1 := &DKGResult{
+		Success:        true,
+		GroupPublicKey: big.NewInt(100),
+		Disqualified:   []bool{true, false, true, false},
+		Inactive:       []bool{false, false, true, false},
+	}
+	actualResult := r1.Hash()
+	expectedResult := []byte{8, 164, 224, 67, 206, 144, 73, 72, 162, 22, 148, 136, 241,
+		243, 2, 210, 221, 121, 31, 208, 51, 144, 48, 23, 142, 126, 12, 7, 222, 185, 107, 98}
+	if !bytes.Equal(actualResult, expectedResult) {
+		t.Errorf("\nexpected: %v\nactual:   %v\n", expectedResult, actualResult)
+	}
+}
+
+func TestSerialize(t *testing.T) {
+	r1 := &DKGResult{
+		Success:        true,
+		GroupPublicKey: big.NewInt(100),
+		Disqualified:   []bool{true, false, true, false},
+		Inactive:       []bool{false, false, true, false},
+	}
+	actualResult := r1.serialize()
+	expectedResult := []byte{1, 100, 1, 0, 1, 0, 0, 0, 1, 0}
+	if !bytes.Equal(actualResult, expectedResult) {
+		t.Errorf("\nexpected: %v\nactual:   %v\n", expectedResult, actualResult)
 	}
 }
