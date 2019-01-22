912ed12439075ef44042f8320c5efbb3ef31683f
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-01-22 14:15:20 +0100

    Use StakerFor with casting to localStaker

--- pkg/chain/local/stake_monitor.go
+++ pkg/chain/local/stake_monitor.go
@@ -40,42 +40,6 @@ func (lsm *StakeMonitor) StakerFor(address string) (chain.Staker, error) {
 	return newStaker, nil
 }
 
-// HasMinimumStake checks if the provided address staked enough to become
-// a network operator. The minimum stake is an on-chain parameter.
-func (lsm *StakeMonitor) HasMinimumStake(address string) (bool, error) {
-	if !common.IsHexAddress(address) {
-		return false, fmt.Errorf("not a valid ethereum address: %v", address)
-	}
-
-	staker := lsm.findStakerByAddress(address)
-
-	return staker.stake.Cmp(minimumStake) >= 0, nil
-}
-
-// StakeTokens stakes enough tokens for the provided address to be a network
-// operator.
-func (lsm *StakeMonitor) StakeTokens(address string) error {
-	if !common.IsHexAddress(address) {
-		return fmt.Errorf("not a valid ethereum address: %v", address)
-	}
-
-	lsm.findStakerByAddress(address).stake = minimumStake
-
-	return nil
-}
-
-// UnstakeTokens unstakes all tokens from the provided address so it can no
-// longer be a network operator.
-func (lsm *StakeMonitor) UnstakeTokens(address string) error {
-	if !common.IsHexAddress(address) {
-		return fmt.Errorf("not a valid ethereum address: %v", address)
-	}
-
-	lsm.findStakerByAddress(address).stake = big.NewInt(0)
-
-	return nil
-}
-
 func (lsm *StakeMonitor) findStakerByAddress(address string) *localStaker {
 	for _, staker := range lsm.stakers {
 		if staker.address == address {
@@ -85,19 +49,71 @@ func (lsm *StakeMonitor) findStakerByAddress(address string) *localStaker {
 	return nil
 }
 
+// HasMinimumStake checks if the provided address staked enough to become
+// a network operator. The minimum stake is an on-chain parameter.
+func (lsm *StakeMonitor) HasMinimumStake(address string) (bool, error) {
+	staker, err := lsm.StakerFor(address)
+	if err != nil {
+		return false, err
+	}
+
+	stake, err := staker.Stake()
+	if err != nil {
+		return false, err
+	}
+
+	return stake.Cmp(minimumStake) >= 0, nil
+}
+
+// StakeTokens stakes enough tokens for the provided address to be a network
+// operator.
+func (lsm *StakeMonitor) StakeTokens(address string) error {
+	staker, err := lsm.StakerFor(address)
+	if err != nil {
+		return err
+	}
+
+	stakerLocal, ok := staker.(*localStaker)
+	if !ok {
+		return fmt.Errorf("invalid type of staker")
+	}
+
+	stakerLocal.stake = minimumStake
+
+	return nil
+}
+
+// UnstakeTokens unstakes all tokens from the provided address so it can no
+// longer be a network operator.
+func (lsm *StakeMonitor) UnstakeTokens(address string) error {
+	staker, err := lsm.StakerFor(address)
+	if err != nil {
+		return err
+	}
+
+	stakerLocal, ok := staker.(*localStaker)
+	if !ok {
+		return fmt.Errorf("invalid type of staker")
+	}
+
+	stakerLocal.stake = big.NewInt(0)
+
+	return nil
+}
+
 type localStaker struct {
 	address string
 	stake   *big.Int
 }
 
-func (ls *localStaker) ID() string {
+func (ls localStaker) ID() string {
 	return ls.address
 }
 
-func (ls *localStaker) Stake() (*big.Int, error) {
+func (ls localStaker) Stake() (*big.Int, error) {
 	return ls.stake, nil
 }
 
-func (ls *localStaker) OnStakeChanged(func(newStake *big.Int)) {
+func (ls localStaker) OnStakeChanged(func(newStake *big.Int)) {
 	// Do nothing for now.
 }
