c91babe2fa5141ee98dcb3f8eaf568ef6a2b8f4f
Raghav Gulati <raghav.gulati@keep.network>
2019-01-22 23:19:59 -0800

    Remove TransportIdentifier from BasicMessage

--- pkg/net/internal/message.go
+++ pkg/net/internal/message.go
@@ -5,13 +5,11 @@ import "github.com/keep-network/keep-core/pkg/net"
 // BasicMessage returns a struct-based trivial implementation of the net.Message
 // interface for use by packages that don't need any frills.
 func BasicMessage(
-	networkSenderID net.TransportIdentifier,
 	protocolSenderID net.ProtocolIdentifier,
 	payload interface{},
 	messageType string,
 ) net.Message {
 	return &basicMessage{
-		networkSenderID,
 		protocolSenderID,
 		payload,
 		messageType,
@@ -21,14 +19,9 @@ func BasicMessage(
 // basicMessage is a struct-based trivial implementation of the net.Message
 // interface for use by packages that don't need any frills.
 type basicMessage struct {
-	networkSenderID  net.TransportIdentifier
 	protocolSenderID net.ProtocolIdentifier
 	payload          interface{}
 	messageType      string
-}
-
-func (m *basicMessage) TransportSenderID() net.TransportIdentifier {
-	return m.networkSenderID
 }
 
 func (m *basicMessage) ProtocolSenderID() net.ProtocolIdentifier {
