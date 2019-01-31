168c8447d11cb0ecc913f118880b2e084de5d6af
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-01-31 16:28:39 -0500

    Updates to relay entry submitting command
    Command to submit relay entry has been updated to generate an entry with
    random values.
    Additional command `genesis` was added to cover initial entry creation.
    It is expected that by running this command initial group will be created.
    On clean chain it is required to first run `genesis` command and after
    the group is formed `submit` command might be executed.

--- cmd/relay.go
+++ cmd/relay.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"context"
+	crand "crypto/rand"
 	"fmt"
 	"math/big"
 	"os"
 	"sync"
 	"time"
 
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 	"github.com/keep-network/keep-core/config"
 	"github.com/keep-network/keep-core/pkg/beacon/relay"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/event"
@@ -15,9 +17,18 @@ import (
 	"github.com/urfave/cli"
 )
 
+const (
+	defaultRequestID int = 1
+)
+
 // RelayCommand contains the definition of the relay command-line subcommand and
 // its own subcommands.
 var RelayCommand cli.Command
+
+const (
+	requestIDFlag  = "request-id"
+	requestIDShort = "r"
+)
 
 const relayDescription = `The relay command allows access to the two functions
    possible in the Keep threshold relay implementation of a random
@@ -44,9 +55,20 @@ func init() {
 				Action: relayEntry,
 			},
 			{
+				Name:   "genesis",
+				Usage:  "Submits a genesis entry to the relay; only for testing.",
+				Action: submitRelayEntryGenesis,
+			},
+			{
 				Name:   "submit",
 				Usage:  "Submits a new seed entry to the relay; only for testing.",
 				Action: submitRelayEntrySeed,
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:  requestIDFlag + "," + requestIDShort,
+						Value: defaultRequestID,
+					},
+				},
 			},
 		},
 	}
@@ -68,6 +90,8 @@ func relayRequest(c *cli.Context) error {
 
 	requestMutex := sync.Mutex{}
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
 	wait := make(chan struct{})
 	var requestID *big.Int
 	provider.ThresholdRelay().OnRelayEntryRequested(func(request *event.Request) {
@@ -117,18 +141,15 @@ func relayRequest(c *cli.Context) error {
 
 	select {
 	case <-wait:
-		cancel()
 		return nil
 	case <-ctx.Done():
 		err := ctx.Err()
 		if err != nil {
-			return fmt.Errorf("Request errored out: [%v].\n", err)
-		} else {
-			return fmt.Errorf("Request errored for unknown reason.\n")
+			return fmt.Errorf("request errored out [%v]", err)
 		}
-	}
+		return fmt.Errorf("request errored for unknown reason")
 
-	return nil
+	}
 }
 
 // relayEntry requests an entry with a particular id from the threshold relay
@@ -137,10 +158,53 @@ func relayEntry(c *cli.Context) error {
 	return fmt.Errorf("relay entry lookups are currently unimplemented")
 }
 
+// submitRelayEntryGenesis creates a genesis entry for the threshold relay. It
+// should be called to initialize a first group selection. It creates an entry
+// with request ID equal `0`.
+func submitRelayEntryGenesis(c *cli.Context) error {
+	// Seed the network with the first n bits of pi
+	value := relay.GenesisEntryValue()
+
+	entry := &event.Entry{
+		RequestID:     big.NewInt(0),
+		Value:         value,
+		GroupID:       big.NewInt(0),
+		PreviousValue: big.NewInt(0),
+		Timestamp:     time.Now().UTC(),
+	}
+
+	return submitRelayEntry(c, entry)
+}
+
 // submitRelayEntrySeed creates a new seed entry for the threshold relay, kicking
 // off the group selection process, and prints the newly generated value. By
-// default, it uses a random big integer as the value.
+// default, it uses a request ID equal `1` and generates a random value and
+// previous value.
 func submitRelayEntrySeed(c *cli.Context) error {
+	requestID := c.Int(requestIDFlag)
+
+	value, err := crand.Int(crand.Reader, bn256.Order)
+	if err != nil {
+		return err
+	}
+
+	previousValue, err := crand.Int(crand.Reader, bn256.Order)
+	if err != nil {
+		return err
+	}
+
+	entry := &event.Entry{
+		RequestID:     big.NewInt(int64(requestID)),
+		Value:         value,
+		GroupID:       big.NewInt(int64(requestID)),
+		PreviousValue: previousValue,
+		Timestamp:     time.Now().UTC(),
+	}
+
+	return submitRelayEntry(c, entry)
+}
+
+func submitRelayEntry(c *cli.Context, entry *event.Entry) error {
 	cfg, err := config.ReadConfig(c.GlobalString("config"))
 	if err != nil {
 		return fmt.Errorf("error reading config file: [%v]", err)
@@ -155,59 +219,37 @@ func submitRelayEntrySeed(c *cli.Context) error {
 		wait        = make(chan error)
 		ctx, cancel = context.WithCancel(context.Background())
 	)
-
-	// Seed the network with the first n bits of pi
-	value := relay.GenesisEntryValue()
-
-	entry := &event.Entry{
-		RequestID:     big.NewInt(0),
-		Value:         value,
-		GroupID:       big.NewInt(0),
-		PreviousEntry: big.NewInt(0),
-		Timestamp:     time.Now().UTC(),
-	}
+	defer cancel()
 
 	provider.ThresholdRelay().SubmitRelayEntry(
 		entry,
-	).OnSuccess(func(data *event.Entry) {
+	).OnComplete(func(data *event.Entry, err error) {
+		if err != nil {
+			wait <- err
+			return
+		}
 		fmt.Fprintf(
 			os.Stdout,
-			"Submitted relay entry: [%v].\n",
+			"Submitted relay entry: [%+v].\n",
 			data,
 		)
-		wait <- nil
-		return
-	}).OnFailure(func(err error) {
 		wait <- err
-		return
-	})
-
-	provider.ThresholdRelay().OnRelayEntryGenerated(func(entry *event.Entry) {
-		fmt.Fprintf(
-			os.Stderr,
-			"Relay entry received with value: [%v].\n",
-			entry.Value,
-		)
 	})
 
 	select {
 	case err := <-wait:
 		if err != nil {
 			return fmt.Errorf(
-				"Error in submitting relay entry: [%v].\n",
+				"error in submitting relay entry: [%v]",
 				err,
 			)
-		} else {
-			cancel()
 		}
 	case <-ctx.Done():
 		err := ctx.Err()
 		if err != nil {
-			return fmt.Errorf("Request errored out: [%v].\n", err)
-		} else {
-			return fmt.Errorf("Request errored for unknown reason.\n")
+			return fmt.Errorf("request errored out: [%v]", err)
 		}
+		return fmt.Errorf("request errored for unknown reason")
 	}
-
 	return nil
 }
