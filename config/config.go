0e237e5b5435f8841349bd61a7f8f2edc084ce6f
lispmeister <markus.fix@keep.network>
2019-02-05 16:20:48 -0500

    refactor configuration override via environment variables and fix tests
    We now support the following environment variables as overrides
    for parameters defined in the toml config file for the Keep client:
    
    ethPasswordEnvVariable = "KEEP_ETHEREUM_PASSWORD"
    ethAccountEnvVariable = "KEEP_ETHEREUM_ACCOUNT"
    ethKeyfileEnvVariable = "KEEP_ETHEREUM_KEYFILE"
    ethRandomBeaconContractEnvVariable = "KEEP_ETHEREUM_RANDOM_BEACON_CONTRACT"
    ethKeepGroupContractEnvVariable = "KEEP_ETHEREUM_KEEP_GROUP_CONTRACT"
    ethStakingProxyContractEnvVariable = "KEEP_ETHEREUM_STAKING_PROXY_CONTRACT"
    ethURLEnvVariable = "KEEP_ETHEREUM_URL"
    ethURLRPCEnvVariable = "KEEP_ETHEREUM_URL_RPC"
    libp2pPortEnvVariable = "KEEP_LIBP2P_PORT"
    libp2pPeersEnvVariable = "KEEP_LIBP2P_PEERS"
    libp2pSeedEnvVariable = "KEEP_LIBP2P_SEED"
    
    All of the above parameters have tests in config_test.go now.

--- config/config.go
+++ config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -15,6 +17,14 @@ import (
 const ethPasswordEnvVariable = "KEEP_ETHEREUM_PASSWORD"
 const ethAccountEnvVariable = "KEEP_ETHEREUM_ACCOUNT"
 const ethKeyfileEnvVariable = "KEEP_ETHEREUM_KEYFILE"
+const ethRandomBeaconContractEnvVariable = "KEEP_ETHEREUM_RANDOM_BEACON_CONTRACT"
+const ethKeepGroupContractEnvVariable = "KEEP_ETHEREUM_KEEP_GROUP_CONTRACT"
+const ethStakingProxyContractEnvVariable = "KEEP_ETHEREUM_STAKING_PROXY_CONTRACT"
+const ethURLEnvVariable = "KEEP_ETHEREUM_URL"
+const ethURLRPCEnvVariable = "KEEP_ETHEREUM_URL_RPC"
+const libp2pPortEnvVariable = "KEEP_LIBP2P_PORT"
+const libp2pPeersEnvVariable = "KEEP_LIBP2P_PEERS"
+const libp2pSeedEnvVariable = "KEEP_LIBP2P_SEED"
 
 // Config is the top level config structure.
 type Config struct {
@@ -79,6 +89,80 @@ func ReadConfig(filePath string) (*Config, error) {
 	// complain if keyfile is still not set (not in env or config file)
 	if config.Ethereum.Account.KeyFile == "" {
 		return nil, fmt.Errorf("Keyfile is required.  Set " + ethKeyfileEnvVariable + " environment variable to keyfile")
+	}
+
+	// get random beacon contract address from environment if set or fallback to config file
+	if ethRandomBeaconContract, ok := os.LookupEnv(ethRandomBeaconContractEnvVariable); ok {
+		config.Ethereum.ContractAddresses["KeepRandomBeacon"] = ethRandomBeaconContract
+	}
+	// complain if random beacon contract address is still not set (not in env or config file)
+	if config.Ethereum.ContractAddresses["KeepRandomBeacon"] == "" {
+		return nil, fmt.Errorf("Keep Random Beacon contract address is required.  Set " + ethRandomBeaconContractEnvVariable + " environment variable to Keep Random Beacon contract address.")
+	}
+
+	// get Keep group contract address from environment if set or fallback to config file
+	if ethKeepGroupContract, ok := os.LookupEnv(ethKeepGroupContractEnvVariable); ok {
+		config.Ethereum.ContractAddresses["KeepGroup"] = ethKeepGroupContract
+	}
+	// complain if Keep group contract address is still not set (not in env or config file)
+	if config.Ethereum.ContractAddresses["KeepGroup"] == "" {
+		return nil, fmt.Errorf("Keep Group contract address is required.  Set " + ethKeepGroupContractEnvVariable + " environment variable to Keep Group contract address.")
+	}
+
+	// get staking proxy contract address from environment if set or fallback to config file
+	if ethStakingProxyContract, ok := os.LookupEnv(ethStakingProxyContractEnvVariable); ok {
+		config.Ethereum.ContractAddresses["StakingProxy"] = ethStakingProxyContract
+	}
+	// complain if staking proxy contract address is still not set (not in env or config file)
+	if config.Ethereum.ContractAddresses["StakingProxy"] == "" {
+		return nil, fmt.Errorf("Keep Random Beacon contract address is required.  Set " + ethStakingProxyContractEnvVariable + " environment variable to Staking Proxy contract address.")
+	}
+
+	// get Ethereum URL from environment if set or fallback to config file
+	if ethURL, ok := os.LookupEnv(ethURLEnvVariable); ok {
+		config.Ethereum.URL = ethURL
+	}
+	// complain if Ethereum URL is still not set (not in env or config file)
+	if config.Ethereum.URL == "" {
+		return nil, fmt.Errorf("Keep Ethereum URL is required.  Set " + ethURLEnvVariable + " environment variable to Ethereum URL.")
+	}
+
+	// get Ethereum URL RPC from environment if set or fallback to config file
+	if ethURLRPC, ok := os.LookupEnv(ethURLRPCEnvVariable); ok {
+		config.Ethereum.URLRPC = ethURLRPC
+	}
+	// complain if Ethereum URL RPC is still not set (not in env or config file)
+	if config.Ethereum.URLRPC == "" {
+		return nil, fmt.Errorf("Keep Ethereum URL RPC is required.  Set " + ethURLRPCEnvVariable + " environment variable to Ethereum URL RPC.")
+	}
+
+	// get Keep LibP2P port from environment if set or fallback to config file
+	if libp2pPort, ok := os.LookupEnv(libp2pPortEnvVariable); ok {
+		port, err := strconv.Atoi(libp2pPort)
+		if err != nil {
+			return nil, fmt.Errorf("Error parsing %s: %s", libp2pPortEnvVariable, err)
+		}
+		config.LibP2P.Port = port
+	}
+
+	// Get Keep LibP2P peers from environment if set or fallback to config file.
+	// In the toml config file this is defined as a toml serialized string array
+	// and we use a similar JSON representation inside the environment variable.
+	// The environment variable represents the string array using the following
+	// syntax: ["peer0", "peer1", "peer2"]
+	if libp2pPeers, ok := os.LookupEnv(libp2pPeersEnvVariable); ok {
+		var peers []string
+		_ = json.Unmarshal([]byte(libp2pPeers), &peers)
+		config.LibP2P.Peers = peers
+	}
+
+	// get Keep LibP2P peers from environment if set or fallback to config file
+	if libp2pSeed, ok := os.LookupEnv(libp2pSeedEnvVariable); ok {
+		seed, err := strconv.Atoi(libp2pSeed)
+		if err != nil {
+			return nil, fmt.Errorf("Error parsing %s: %s", libp2pSeedEnvVariable, err)
+		}
+		config.LibP2P.Seed = seed
 	}
 
 	if config.LibP2P.Port == 0 {
