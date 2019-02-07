28bd545356c9dec3a736ccce65cea3ed19ff0a46
Raghav Gulati <raghav.gulati@keep.network>
2019-02-07 12:47:55 -0500

    Merge mishap: wrong type for map init
    Seen has a map value type of bool, not struct{}.

--- pkg/beacon/relay/relay.go
+++ pkg/beacon/relay/relay.go
@@ -30,7 +30,7 @@ func NewNode(
 		chainConfig:      chainConfig,
 		stakeIDs:         make([]string, 100),
 		groupPublicKeys:  make([][]byte, 0),
-		seenPublicKeys:   make(map[string]struct{}),
+		seenPublicKeys:   make(map[string]bool),
 		myGroups:         make(map[string]*membership),
 		pendingGroups:    make(map[string]*membership),
 		tickets:          make([]*groupselection.Ticket, 0),
