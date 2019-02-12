4fafbad928ca425bda3e3da694b089779fbd7839
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-02-12 14:58:30 +0100

    SubmitGroupPublicKeyEvent to have bytes groupPublicKey

--- contracts/solidity/contracts/KeepRandomBeaconImplV1.sol
+++ contracts/solidity/contracts/KeepRandomBeaconImplV1.sol
@@ -21,7 +21,7 @@ contract KeepRandomBeaconImplV1 is Ownable {
     event RelayEntryRequested(uint256 requestID, uint256 payment, uint256 blockReward, uint256 seed, uint blockNumber); 
     event RelayEntryGenerated(uint256 requestID, uint256 requestResponse, uint256 requestGroupID, uint256 previousEntry, uint blockNumber, uint256 seed);
     event RelayResetEvent(uint256 lastValidRelayEntry, uint256 lastValidRelayTxHash, uint256 lastValidRelayBlock);
-    event SubmitGroupPublicKeyEvent(byte[] groupPublicKey, uint256 requestID, uint256 activationBlockHeight);
+    event SubmitGroupPublicKeyEvent(bytes groupPublicKey, uint256 requestID, uint256 activationBlockHeight);
 
     uint256 internal _seq;
     uint256 internal _minPayment;
