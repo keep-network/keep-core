8895c73c9412e5774e43f061043dca99f57d17dd
sthompson22 <sloan.thompson@thesis.co>
2019-02-12 15:00:56 -0500

    Remove stand alone helm-release file, will sort proper resourse segmentation later

--- infrastructure/terraform/keep-dev/main.tf
+++ infrastructure/terraform/keep-dev/main.tf
@@ -163,3 +163,21 @@ module "gke_cluster_metrics" {
     version = "${var.prometheus_to_sd["version"]}"
   }
 }
+
+# OpenVPN
+resource "helm_release" "openvpn" {
+  name      = "helm-openvpn"
+  namespace = "default"
+  chart     = "stable/openvpn"
+  version   = "3.10.0"
+
+  set {
+    name  = "openvpn.redirectGateway"
+    value = "false"
+  }
+
+  set {
+    name  = "openvpn.conf"
+    value = "push \"route 172.16.0.0 255.255.255.240\""
+  }
+}
