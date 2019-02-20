3e86d6f1fbc5c340da266094c28e1a90f2e9e818
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-02-20 12:17:39 +0100

    Update result serialization test

--- pkg/beacon/relay/chain/result_test.go
+++ pkg/beacon/relay/chain/result_test.go
@@ -93,30 +93,67 @@ func TestDKGResultEquals(t *testing.T) {
 }
 
 func TestHash(t *testing.T) {
-	r1 := &DKGResult{
+	dkgResult := &DKGResult{
 		Success:        true,
 		GroupPublicKey: []byte{100},
 		Disqualified:   []byte{1, 0, 1, 0},
 		Inactive:       []byte{0, 0, 1, 0},
 	}
-	actualResult := r1.Hash()
-	expectedResult := []byte{253, 157, 252, 65, 254, 175, 165, 131, 87, 84, 98, 128, 37, 49,
+	expectedHash := []byte{253, 157, 252, 65, 254, 175, 165, 131, 87, 84, 98, 128, 37, 49,
 		106, 147, 230, 64, 192, 22, 193, 71, 150, 55, 101, 1, 110, 4, 10, 189, 121, 38}
-	if !bytes.Equal(actualResult, expectedResult) {
-		t.Errorf("\nexpected: %v\nactual:   %v\n", expectedResult, actualResult)
+
+	actualHash := dkgResult.Hash()
+
+	if !bytes.Equal(actualHash, expectedHash) {
+		t.Errorf("\nexpected: %v\nactual:   %v\n", expectedHash, actualHash)
 	}
 }
 
 func TestSerialize(t *testing.T) {
-	r1 := &DKGResult{
-		Success:        true,
-		GroupPublicKey: []byte{100},
-		Disqualified:   []byte{1, 0, 1, 0},
-		Inactive:       []byte{0, 0, 1, 0},
+	var tests = map[string]struct {
+		dkgResult                *DKGResult
+		expectedSerializedResult []byte
+	}{
+		"success with 1 byte group public key": {
+			dkgResult: &DKGResult{
+				Success:        true,
+				GroupPublicKey: []byte{100},
+				Disqualified:   []byte{1, 0, 1, 0},
+				Inactive:       []byte{0, 0, 1, 0},
+			},
+			expectedSerializedResult: []byte{1, 0, 0, 0, 1, 100, 0, 0, 0, 4, 1, 0, 1, 0, 0, 0, 0, 4, 0, 0, 1, 0},
+		},
+		"failure with empty group public key and disqualified and inactive lists": {
+			dkgResult: &DKGResult{
+				Success:        false,
+				GroupPublicKey: []byte{},
+				Disqualified:   []byte{},
+				Inactive:       []byte{},
+			},
+			expectedSerializedResult: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+		"success with 3 bytes group public key": {
+			dkgResult: &DKGResult{
+				Success:        true,
+				GroupPublicKey: []byte{2, 21, 210},
+				Disqualified:   []byte{0, 0, 0, 0},
+				Inactive:       []byte{0, 0, 1, 0},
+			},
+			expectedSerializedResult: []byte{1, 0, 0, 0, 3, 2, 21, 210, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 1, 0},
+		},
 	}
-	actualResult := r1.serialize()
-	expectedResult := []byte{1, 0, 0, 0, 1, 100, 0, 0, 0, 4, 1, 0, 1, 0, 0, 0, 0, 4, 0, 0, 1, 0}
-	if !bytes.Equal(actualResult, expectedResult) {
-		t.Errorf("\nexpected: %v\nactual:   %v\n", expectedResult, actualResult)
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			actualSerializedResult := test.dkgResult.serialize()
+
+			if !bytes.Equal(actualSerializedResult, test.expectedSerializedResult) {
+				t.Errorf(
+					"\nexpected: %v\nactual:   %v\n",
+					test.expectedSerializedResult,
+					actualSerializedResult,
+				)
+			}
+		})
 	}
 }
