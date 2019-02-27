b7211946d0421dd024bc1309c41ce0c405225d62
Raghav Gulati <raghav.gulati@keep.network>
2019-02-26 16:42:05 -0800

    First pass at fixing signing test

--- pkg/beacon/relay/dkg2/signer_test.go
+++ pkg/beacon/relay/dkg2/signer_test.go
@@ -13,24 +13,36 @@ func TestSignAndComplete(t *testing.T) {
 	groupPrivateKey := big.NewInt(34)
 	groupPublicKey := new(bn256.G2).ScalarBaseMult(groupPrivateKey)
 
+	threshold := 6
 	signers := []*ThresholdSigner{
 		&ThresholdSigner{1, groupPublicKey, big.NewInt(2)},
-		&ThresholdSigner{1, groupPublicKey, big.NewInt(12)},
-		&ThresholdSigner{1, groupPublicKey, big.NewInt(10)},
-		&ThresholdSigner{1, groupPublicKey, big.NewInt(5)},
-		&ThresholdSigner{1, groupPublicKey, big.NewInt(4)},
-		&ThresholdSigner{1, groupPublicKey, big.NewInt(1)},
+		&ThresholdSigner{2, groupPublicKey, big.NewInt(12)},
+		&ThresholdSigner{3, groupPublicKey, big.NewInt(10)},
+		&ThresholdSigner{4, groupPublicKey, big.NewInt(5)},
+		&ThresholdSigner{5, groupPublicKey, big.NewInt(4)},
+		&ThresholdSigner{6, groupPublicKey, big.NewInt(1)},
 	} // 2 + 12 + 10 + 5 + 4 + 1 = 34
 
 	message := []byte("hello world")
 
-	shares := make([]*bn256.G1, len(signers))
+	shares := make([]*bls.SignatureShare, 0)
 	for i, signer := range signers {
-		shares[i] = signer.CalculateSignatureShare(message)
+		shares = append(shares,
+			&bls.SignatureShare{
+				I: i + 1, // always 1-indexed
+				V: signer.CalculateSignatureShare(message),
+			},
+		)
 	}
 
-	actual := signers[0].CompleteSignature(shares).Marshal()
+	signature, err := signers[0].CompleteSignature(shares, threshold)
+	if err != nil {
+		t.Fatal(err)
+	}
+	actual := signature.Marshal()
 	expected := bls.Sign(groupPrivateKey, message).Marshal()
 
 	testutils.AssertBytesEqual(t, expected, actual)
 }
+
+// 3 tests - 1 where we have 0 that work, 1 where we have under t, one where we have t
