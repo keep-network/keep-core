34afc442ddb13d1b8cffd9f246470b9766d9467a
Raghav Gulati <raghav.gulati@keep.network>
2019-02-26 16:40:35 -0800

    Implement the new Signature method
    Now that CompleteSignature correctly uses RecoverSignature, it requires
    bls.SignatureShares as a parameter. This means we accumulate a slice of
    bls.SignatureShares to pass in. We also pass in the required system
    paremeter, threshold.

--- pkg/beacon/relay/thresholdsignature/thresholdsignature.go
+++ pkg/beacon/relay/thresholdsignature/thresholdsignature.go
@@ -8,6 +8,7 @@ import (
 	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/dkg2"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/gjkr"
+	"github.com/keep-network/keep-core/pkg/bls"
 
 	"github.com/keep-network/keep-core/pkg/chain"
 	"github.com/keep-network/keep-core/pkg/net"
@@ -115,12 +116,16 @@ func Execute(
 			}
 		case <-blockWaiter:
 			// put all seen shares into a slice and complete the signature
-			seenSharesSlice := make([]*bn256.G1, 0)
-			for _, share := range seenShares {
-				seenSharesSlice = append(seenSharesSlice, share)
+			seenSharesSlice := make([]*bls.SignatureShare, 0)
+			for memberID, share := range seenShares {
+				signatureShare := &bls.SignatureShare{I: int(memberID), V: share}
+				seenSharesSlice = append(seenSharesSlice, signatureShare)
 			}
 
-			signature := signer.CompleteSignature(seenSharesSlice)
+			signature, err := signer.CompleteSignature(seenSharesSlice, threshold)
+			if err != nil {
+				return nil, err
+			}
 
 			return signature.Marshal(), nil
 		}
