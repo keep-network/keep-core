f86f507ee5111a56197f91be80e8d219193d6623
Raghav Gulati <raghav.gulati@keep.network>
2019-02-28 08:55:45 -0800

    Completely rework signing test
    Rather than comparing two signatures (the recovered sig and bls.signing
    a message with a private key), we instead recover a signature, and along
    with a group public key, we ask the question: can the presented signature
    really be the signed message by the group that is presenting this
    signature to me?

--- pkg/beacon/relay/dkg2/signer_test.go
+++ pkg/beacon/relay/dkg2/signer_test.go
@@ -1,27 +1,56 @@
 package dkg2
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 
-	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/gjkr"
 	"github.com/keep-network/keep-core/pkg/bls"
-	"github.com/keep-network/keep-core/pkg/internal/testutils"
 )
 
 func TestSignAndComplete(t *testing.T) {
-	groupPrivateKey := big.NewInt(34)
-	groupPublicKey := new(bn256.G2).ScalarBaseMult(groupPrivateKey)
-
 	threshold := 6
-	signers := []*ThresholdSigner{
-		&ThresholdSigner{1, groupPublicKey, big.NewInt(2)},
-		&ThresholdSigner{2, groupPublicKey, big.NewInt(12)},
-		&ThresholdSigner{3, groupPublicKey, big.NewInt(10)},
-		&ThresholdSigner{4, groupPublicKey, big.NewInt(5)},
-		&ThresholdSigner{5, groupPublicKey, big.NewInt(4)},
-		&ThresholdSigner{6, groupPublicKey, big.NewInt(1)},
-	} // 2 + 12 + 10 + 5 + 4 + 1 = 34
+
+	// Obtained by running `TestFullStateTransitions` and outputting shares.
+
+	// groupPublicKey: bn256.G2((0b9a57387a8e2c5f588ddece242ab9aa005eea7a2edd93c2890d6e6b6a4a379c, 23023e11f05d3c4694f5fb22960fa6e9448cd7d959695f2ca11b8fe737dc03b3), (17c10becb92975a8a9d0fd5bb4d69c964c69cfae94694db45ed4b14d9ba8299b, 1b9cfe419acfb244d95bd71b2c8453859e75437249c6ab085a4a5c8c920f0690))
+
+	privateKeySharesSlice := []string{
+		"11317689322678074320253490110336291778019389544269451057844495958842587881889",
+		"3321938026855708852034359825983988567719999295358659054897559573286783870881",
+		"16637974750087405446600293454316416930247455842507609024774997421245084242640",
+		"8811658855469363004088702363302325849186372736438301169002749756782287688357",
+		"5216737455961088866926156556557357315061484853999540690387272503653832520818",
+		"19745272718386738224116191829677947371012382482669271992696752605818677063650",
+	}
+
+	// First get SecretKeyShares from slice of privateKeyShares
+	var publicKeyShares []*bls.PublicKeyShare
+	for i, privateKeyShareString := range privateKeySharesSlice {
+		privateKeyShare, _ := new(big.Int).SetString(privateKeyShareString, 10)
+		publicKeyShare := (&bls.SecretKeyShare{
+			I: i + 1,
+			V: privateKeyShare,
+		}).PublicKeyShare()
+		publicKeyShares = append(publicKeyShares, publicKeyShare)
+	}
+	// Build up the group public key
+	groupPublicKey, err := bls.RecoverPublicKey(publicKeyShares, threshold)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(groupPublicKey)
+
+	var signers []*ThresholdSigner
+	for i, privateKeyShare := range privateKeySharesSlice {
+		share, _ := new(big.Int).SetString(privateKeyShare, 10)
+		signers = append(signers, &ThresholdSigner{
+			memberID:             gjkr.MemberID(i + 1),
+			groupPublicKey:       groupPublicKey,
+			groupPrivateKeyShare: share,
+		})
+	}
 
 	message := []byte("hello world")
 
@@ -39,10 +68,10 @@ func TestSignAndComplete(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	actual := signature.Marshal()
-	expected := bls.Sign(groupPrivateKey, message).Marshal()
 
-	testutils.AssertBytesEqual(t, expected, actual)
+	if !bls.Verify(groupPublicKey, message, signature) {
+		t.Fatal("Failed to verify recovered signature")
+	}
 }
 
 // 3 tests - 1 where we have 0 that work, 1 where we have under t, one where we have t
