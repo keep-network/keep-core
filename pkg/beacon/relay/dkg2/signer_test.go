b6194d90fe601802afbaa6165a25ffe11858a7f2
Raghav Gulati <raghav.gulati@keep.network>
2019-02-28 23:33:11 -0800

    Remove +1 on the index and fmt
    Removing + 1 on the index gets the test to pass - investigate this.
    Remove fmt as debug is no longer needed.

--- pkg/beacon/relay/dkg2/signer_test.go
+++ pkg/beacon/relay/dkg2/signer_test.go
@@ -1,7 +1,6 @@
 package dkg2
 
 import (
-	"fmt"
 	"math/big"
 	"testing"
 
@@ -13,16 +12,13 @@ func TestSignAndComplete(t *testing.T) {
 	threshold := 6
 
 	// Obtained by running `TestFullStateTransitions` and outputting shares.
-
-	// groupPublicKey: bn256.G2((0b9a57387a8e2c5f588ddece242ab9aa005eea7a2edd93c2890d6e6b6a4a379c, 23023e11f05d3c4694f5fb22960fa6e9448cd7d959695f2ca11b8fe737dc03b3), (17c10becb92975a8a9d0fd5bb4d69c964c69cfae94694db45ed4b14d9ba8299b, 1b9cfe419acfb244d95bd71b2c8453859e75437249c6ab085a4a5c8c920f0690))
-
 	privateKeySharesSlice := []string{
-		"11317689322678074320253490110336291778019389544269451057844495958842587881889",
-		"3321938026855708852034359825983988567719999295358659054897559573286783870881",
-		"16637974750087405446600293454316416930247455842507609024774997421245084242640",
-		"8811658855469363004088702363302325849186372736438301169002749756782287688357",
-		"5216737455961088866926156556557357315061484853999540690387272503653832520818",
-		"19745272718386738224116191829677947371012382482669271992696752605818677063650",
+		"7965280207209549879164292761852524476109477664957641865927295346590476704711",
+		"6106610144639464785158072029008498287824734372346580964957055618768317731307",
+		"1440342545552619026306193227443878557590393893287770611896928291936003327327",
+		"4784636576436291513911847371324649154255552173376284038648549444705786002507",
+		"11155341033982526633651061739311639943498023031242960281134125600620928234282",
+		"476414470139165825449834970450656200990319293028518982997364506493673497757",
 	}
 
 	// First get SecretKeyShares from slice of privateKeyShares
@@ -30,7 +26,7 @@ func TestSignAndComplete(t *testing.T) {
 	for i, privateKeyShareString := range privateKeySharesSlice {
 		privateKeyShare, _ := new(big.Int).SetString(privateKeyShareString, 10)
 		publicKeyShare := (&bls.SecretKeyShare{
-			I: i + 1,
+			I: i,
 			V: privateKeyShare,
 		}).PublicKeyShare()
 		publicKeyShares = append(publicKeyShares, publicKeyShare)
@@ -40,20 +36,18 @@ func TestSignAndComplete(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(groupPublicKey)
 
 	var signers []*ThresholdSigner
 	for i, privateKeyShare := range privateKeySharesSlice {
 		share, _ := new(big.Int).SetString(privateKeyShare, 10)
 		signers = append(signers, &ThresholdSigner{
-			memberID:             gjkr.MemberID(i + 1),
+			memberID:             gjkr.MemberID(i),
 			groupPublicKey:       groupPublicKey,
 			groupPrivateKeyShare: share,
 		})
 	}
 
 	message := []byte("hello world")
-
 	shares := make([]*bls.SignatureShare, 0)
 	for i, signer := range signers {
 		shares = append(shares,
