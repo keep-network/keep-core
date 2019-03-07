5a903c1dfb114b8113ccbca1cf36b86ade6e31a4
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-03-07 08:47:41 +0100

    Move msg marshalling to separate file

--- pkg/beacon/relay/dkg2/message.go
+++ pkg/beacon/relay/dkg2/message.go
@@ -2,7 +2,6 @@ package dkg2
 
 import (
 	"github.com/keep-network/keep-core/pkg/beacon/relay/chain"
-	"github.com/keep-network/keep-core/pkg/beacon/relay/dkg2/gen/pb"
 )
 
 // DKGResultHashSignatureMessage is a message payload that carries a hash of
@@ -14,34 +13,4 @@ type DKGResultHashSignatureMessage struct {
 
 	resultHash chain.DKGResultHash
 	signature  []byte
-}
-
-// Marshal converts this DKGResultHashSignatureMessage to a byte array suitable
-// for network communication.
-func (d *DKGResultHashSignatureMessage) Marshal() ([]byte, error) {
-	return (&pb.DKGResultHashSignature{
-		SenderID:   d.senderIndex,
-		ResultHash: d.resultHash[:],
-		Signature:  d.signature,
-	}).Marshal()
-}
-
-// Unmarshal converts a byte array produced by Marshal to a
-// DKGResultHashSignatureMessage.
-func (d *DKGResultHashSignatureMessage) Unmarshal(bytes []byte) error {
-	pbMsg := pb.DKGResultHashSignature{}
-	if err := pbMsg.Unmarshal(bytes); err != nil {
-		return err
-	}
-	d.senderIndex = pbMsg.SenderID
-
-	resultHash, err := chain.DKGResultHashFromBytes(pbMsg.ResultHash)
-	if err != nil {
-		return err
-	}
-	d.resultHash = resultHash
-
-	d.signature = pbMsg.Signature
-
-	return nil
 }
