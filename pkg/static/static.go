d2e86194d8aba041139be3034dd7e823fd9b7bac
Raghav Gulati <raghav.gulati@keep.network>
2019-03-07 15:22:07 -0800

    First pass at agnostic static key package
    In DKG Phase 13 and 14, we'll need to sign the group public key with the
    operator's static public key as a proxy for voting. Previously, our
    static public key was confined to the network package. This commit
    brings that functionality to our top level, allowing it to be used both
    in net and in dkg.

---
+++ pkg/static/static.go
@@ -0,0 +1,61 @@
+package static
+
+import (
+	"crypto/ecdsa"
+	"io"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+	"github.com/pborman/uuid"
+)
+
+// PrivateKey represents peer's static key associated with an on-chain
+// stake. It is used to authenticate the peer and for attributability (signing).
+type PrivateKey = btcec.PrivateKey
+
+// PublicKey represents peer's static key associated with an on-chain
+// stake. It is used to authenticate the peer and for attributability
+// (verification).
+type PublicKey = btcec.PublicKey
+
+// GenerateStaticKeyPair generates a new, random static key based on
+// secp256k1 ethereum curve.
+func GenerateStaticKeyPair(rand io.Reader) (*PrivateKey, *PublicKey, error) {
+	id := uuid.NewRandom()
+
+	ecdsaKey, err := ecdsa.GenerateKey(secp256k1.S256(), rand)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	key := &keystore.Key{
+		Id:         id,
+		Address:    crypto.PubkeyToAddress(ecdsaKey.PublicKey),
+		PrivateKey: ecdsaKey,
+	}
+
+	privKey, pubKey := EthereumKeyToStaticKey(key)
+	return privKey, pubKey, nil
+}
+
+// EthereumKeyToStaticKey transforms `go-ethereum`-based ECDSA key into the
+// format supported by the network layer. Because all curve parameters of
+// secp256k1 curve defined by `go-ethereum` and all curve parameters of
+// secp256k1 curve defined by `btcsuite` used by `lipb2b` under the hood are
+// identical, we can simply rewrite the private key.
+func EthereumKeyToStaticKey(ethereumKey *keystore.Key) (*PrivateKey, *PublicKey) {
+	privKey, pubKey := btcec.PrivKeyFromBytes(
+		btcec.S256(), ethereumKey.PrivateKey.D.Bytes(),
+	)
+
+	return (*PrivateKey)(privKey), (*PublicKey)(pubKey)
+}
+
+// PubKeyToEthAddress transforms a static PubKey into an Ethereum account
+// address, in a string format.
+func PubKeyToEthAddress(publicKey *PublicKey) string {
+	ecdsaKey := (*btcec.PublicKey)(publicKey).ToECDSA()
+	return crypto.PubkeyToAddress(*ecdsaKey).String()
+}
