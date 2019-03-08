8aeb86d80e37268eafb8a36496b91b2bdc635791
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-03-08 16:46:48 +0100

    Updated protocol implementation

--- pkg/beacon/relay/dkg2/resultsubmission/result_submission.go
+++ pkg/beacon/relay/dkg2/resultsubmission/result_submission.go
@@ -21,7 +21,11 @@ func (fm *ResultSigningMember) SignDKGResult(dkgResult *relayChain.DKGResult) (
 	}
 	fm.dkgResultHash = resultHash
 
-	signature := fm.sign(resultHash)
+	signature, err := fm.sign(resultHash)
+	if err != nil {
+		return nil, fmt.Errorf("dkg result hash signing failed [%v]", err)
+	}
+
 	// Register self signature.
 	fm.validResultSignatures[fm.index] = signature
 
@@ -33,22 +37,56 @@ func (fm *ResultSigningMember) SignDKGResult(dkgResult *relayChain.DKGResult) (
 }
 
 // VerifyDKGResultSignatures // TODO: Write docs
-func (fm *ResultSigningMember) VerifyDKGResultSignatures(messages []*DKGResultHashSignatureMessage) error {
+func (fm *ResultSigningMember) VerifyDKGResultSignatures(
+	messages []*DKGResultHashSignatureMessage,
+) (map[ParticipantIndex][]Signature, error) {
+	alreadyReceivedSignature := make([]ParticipantIndex, 0) // track if member already send signature
+	accusations := make(map[ParticipantIndex][]Signature)
+
+messagesCheck:
 	for _, message := range messages {
-		if !fm.verifySignature(message.senderIndex, message.resultHash, message.signature) {
-			fmt.Fprintf(os.Stderr, "invalid signature in message: [%+v]", message)
+		// Check if sender sent multiple signatures.
+		for _, alreadySignedIndex := range alreadyReceivedSignature {
+			if message.senderIndex == alreadySignedIndex {
+				fmt.Println("message from member who already send a message")
+
+				if signature, ok := fm.validResultSignatures[message.senderIndex]; ok {
+					accusations[message.senderIndex] = append(
+						accusations[message.senderIndex],
+						signature,
+					)
+
+					delete(fm.validResultSignatures, message.senderIndex)
+				}
+
+				accusations[message.senderIndex] = append(
+					accusations[message.senderIndex],
+					message.signature,
+				)
+
+				continue messagesCheck
+			}
+		}
+		alreadyReceivedSignature = append(alreadyReceivedSignature, message.senderIndex)
+
+		// Sender's preferred DKG result hash doesn't match current member's
+		// preferred DKG result hash.
+		if message.resultHash != fm.dkgResultHash {
+			fmt.Println("signature for result different than preferred")
 			continue
 		}
 
-		if message.resultHash != fm.dkgResultHash {
-			fmt.Printf("signature for result different than preferred")
+		// Signature is invalid.
+		if !fm.verifySignature(message.senderIndex, message.resultHash, message.signature) {
+			fmt.Fprintf(os.Stderr, "invalid signature in message: [%+v]", message)
+			// TODO: Should we accuse the member who send invalid signature?
 			continue
 		}
 
 		fm.validResultSignatures[message.senderIndex] = message.signature
 	}
 
-	return nil
+	return accusations, nil
 }
 
 func (fm *ResultSigningMember) sign(resultHash relayChain.DKGResultHash) []byte {
