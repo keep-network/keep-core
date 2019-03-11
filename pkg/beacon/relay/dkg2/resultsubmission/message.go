18b954366c15049c2ef1c4e9d4a3f516f284d895
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-03-11 11:15:46 +0100

    Alias signature type

--- pkg/beacon/relay/dkg2/resultsubmission/message.go
+++ pkg/beacon/relay/dkg2/resultsubmission/message.go
@@ -4,6 +4,10 @@ import (
 	"github.com/keep-network/keep-core/pkg/beacon/relay/chain"
 )
 
+// Signature is a signature of the DKG result hash.
+// TODO: When we have signing implemented update aliased type if needed.
+type Signature []byte
+
 // DKGResultHashSignatureMessage is a message payload that carries a hash of
 // the DKG result and a signature over this hash for a DKG result.
 //
@@ -14,5 +18,5 @@ type DKGResultHashSignatureMessage struct {
 	// Hash of the DKG result preferred by the sender.
 	resultHash chain.DKGResultHash
 	// Signature over the DKG result hash calculated by the sender.
-	signature []byte
+	signature Signature
 }
