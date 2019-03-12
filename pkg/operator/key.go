6f38d338bd3f90b4d6b51ff3c39c3d2a89df4c3f
Raghav Gulati <raghav.gulati@keep.network>
2019-03-12 16:55:00 -0700

    Modify geth Sign for ECDSA signatures
    Modify go-ethereum/crypto.Sign to remove the arbitrary curve check,
    along with working with operator.PrivateKeys. We maintain the addition
    of the go-ethereum specific ECDSA signature format, [R || S || V] with
    the ethereum required V at the end.

--- pkg/operator/key.go
+++ pkg/operator/key.go
@@ -2,8 +2,10 @@ package operator
 
 import (
 	"crypto/ecdsa"
+	"fmt"
 	"io"
 
+	"github.com/btcsuite/btcd/btcec"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
@@ -33,4 +35,27 @@ func GenerateKeyPair(rand io.Reader) (*PrivateKey, *PublicKey, error) {
 func EthereumKeyToOperatorKey(ethereumKey *keystore.Key) (*PrivateKey, *PublicKey) {
 	privKey := ethereumKey.PrivateKey
 	return (*PrivateKey)(privKey), (*PublicKey)(&privKey.PublicKey)
+}
+
+// Sign calculates an ECDSA signature. Modified code from go-ethereum.
+//
+// This function is susceptible to chosen plaintext attacks that can leak
+// information about the private key that is used for signing. Callers must
+// be aware that the given hash cannot be chosen by an adversery. Common
+// solution is to hash any input before calculating the signature.
+//
+// The produced signature is in the [R || S || V] format where V is 0 or 1.
+func Sign(hash []byte, prv *PrivateKey) ([]byte, error) {
+	if len(hash) != 32 {
+		return nil, fmt.Errorf("hash is required to be exactly 32 bytes (%d)", len(hash))
+	}
+	sig, err := btcec.SignCompact(btcec.S256(), (*btcec.PrivateKey)(prv), hash, false)
+	if err != nil {
+		return nil, err
+	}
+	// Convert to Ethereum signature format with 'recovery id' v at the end.
+	v := sig[0] - 27
+	copy(sig, sig[1:])
+	sig[64] = v
+	return sig, nil
 }
