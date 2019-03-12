32e8b7124788e85ea93c185717edfeffe4f5b4e6
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-03-12 13:51:58 +0100

    PrivateKey type alias update

--- pkg/static/static.go
+++ pkg/static/static.go
@@ -13,7 +13,7 @@ import (
 
 // PrivateKey represents peer's static key associated with an on-chain
 // stake. It is used to authenticate the peer and for attributability (signing).
-type PrivateKey = ecdsa.PrivateKey
+type PrivateKey ecdsa.PrivateKey
 
 // PublicKey represents peer's static key associated with an on-chain
 // stake. It is used to authenticate the peer and for attributability
