ed367b37f816bb56b79cf8ae509416ce57b3ab7e
Raghav Gulati <raghav.gulati@keep.network>
2019-03-13 10:26:13 -0700

    Turn single test into table test
    Get the table test format down for the single success case.

--- pkg/operator/key_test.go
+++ pkg/operator/key_test.go
@@ -1,7 +1,6 @@
 package operator
 
 import (
-	"crypto/rand"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -9,19 +8,37 @@ import (
 
 const testMessage = "Safe And Secure"
 
-func TestOperatorKeySignAndverify(t *testing.T) {
-	operatorPrivateKey, operatorPublicKey, err := GenerateKeyPair(rand.Reader)
+func TestOperatorKeySignAndVerify(t *testing.T) {
+	operatorPrivateKey, operatorPublicKey, err := GenerateKeyPair()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	hashedMessage := crypto.Keccak256([]byte(testMessage))
-	sig, err := Sign(hashedMessage, operatorPrivateKey)
-	if err != nil {
-		t.Fatal(err)
+	var tests = map[string]struct {
+		sign              func(hash []byte, priv *PrivateKey) ([]byte, error)
+		signingError      error
+		verificationError error
+	}{
+		"signature is equal to 65 bytes": {
+			sign: func(hash []byte, priv *PrivateKey) ([]byte, error) {
+				return Sign(hash, priv)
+			},
+			signingError:      nil,
+			verificationError: nil,
+		},
 	}
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			hashedMessage := crypto.Keccak256([]byte(testMessage))
+			sig, err := test.sign(hashedMessage, operatorPrivateKey)
+			if err != nil && err != test.signingError {
+				t.Fatal(err)
+			}
 
-	if err := VerifySignature(operatorPublicKey, hashedMessage, sig); err != nil {
-		t.Fatal(err)
+			err = VerifySignature(operatorPublicKey, hashedMessage, sig)
+			if err != nil && err != test.verificationError {
+				t.Fatal(err)
+			}
+		})
 	}
 }
