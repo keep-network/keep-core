ad5abb3a5e0d938b4c732bb731822c23dc42dde7
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-03-19 13:14:39 +0100

    Added explanation about what ID is returned

--- contracts/solidity/contracts/KeepRandomBeaconImplV1.sol
+++ contracts/solidity/contracts/KeepRandomBeaconImplV1.sol
@@ -89,7 +89,7 @@ contract KeepRandomBeaconImplV1 is Ownable {
      * @dev Creates a request to generate a new relay entry, which will include a
      * random number (by signing the previous entry's random number).
      * @param seed Initial seed random value from the client. It should be a cryptographically generated random value.
-     * @return An uint256 representing uniquely generated ID. It is also returned as part of the event.
+     * @return An uint256 representing uniquely generated relay request ID. It is also returned as part of the event.
      */
     function requestRelayEntry(uint256 seed) public payable returns (uint256) {
         require(
