bd7873d6c4b9e647e829f59a8727c7450a0acbf2
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-03-20 17:43:41 +0100

    Update on-chain solidity test

--- contracts/solidity/test/TestPublishDkgResult.js
+++ contracts/solidity/test/TestPublishDkgResult.js
@@ -1,24 +1,24 @@
 const KeepGroupImplV1 = artifacts.require('./KeepGroupImplV1.sol');
 
-contract('TestPublishDkgResult', function(accounts) {
+contract('TestPublishDkgResult', function (accounts) {
   let keepGroupImplV1;
 
   beforeEach(async () => {
     keepGroupImplV1 = await KeepGroupImplV1.new();
   })
 
-  it("should know that result has not been published yet", async function() {
+  it("should know that result has not been published yet", async function () {
     let published = await keepGroupImplV1.isDkgResultSubmitted(1811);
     assert.equal(published, false, "result has not been published yet")
   });
 
-  it("should publish DKG result", async function() {
-    await keepGroupImplV1.submitDkgResult(1812, true, "0x100101011", "0x000000011", "0x000000100");
+  it("should publish DKG result", async function () {
+    await keepGroupImplV1.submitDkgResult(1812, "0x100101011", "0x000000011", "0x000000100");
     assert.equal((await keepGroupImplV1.getPastEvents())[0].event, 'DkgResultPublishedEvent', "DkgResultPublishedEvent should occur");
   });
 
-  it("should now that result has been already published", async function() {
-    await keepGroupImplV1.submitDkgResult(1813, true, "0x100101011", "0x000000011", "0x000000100");
+  it("should now that result has been already published", async function () {
+    await keepGroupImplV1.submitDkgResult(1813, "0x100101011", "0x000000011", "0x000000100");
     let published = await keepGroupImplV1.isDkgResultSubmitted(1813);
     assert.equal(published, true, "result has been already published")
   });
