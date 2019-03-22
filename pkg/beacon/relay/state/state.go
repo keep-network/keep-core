70f776f2a6ce07f01df6d43ab7626fc7e05981f1
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-03-22 14:03:09 +0100

    State interface functions made public
    Functions are exported to be able to implement the interface in other
    packages.

--- pkg/beacon/relay/state/state.go
+++ pkg/beacon/relay/state/state.go
@@ -11,29 +11,29 @@ import (
 
 // State is and interface against which relay states should be implemented.
 type State interface {
-	// activeBlocks returns the number of blocks during which the current state
+	// ActiveBlocks returns the number of blocks during which the current state
 	// is active. Blocks are counted after the initiation process of the
 	// current state has completed.
-	activeBlocks() int
+	ActiveBlocks() int
 
-	// initiate performs all the required calculations and sends out all the
+	// Initiate performs all the required calculations and sends out all the
 	// messages associated with the current state.
-	initiate() error
+	Initiate() error
 
 	// Receive is called each time a new message arrived. receive is expected to
 	// be called for all broadcast channel messages, including the member's own
 	// messages.
-	receive(msg net.Message) error
+	Receive(msg net.Message) error
 
-	// nextNextStateState performs a state transition to the next state of the protocol.
+	// NextState performs a state transition to the next state of the protocol.
 	// If the current state is the last one, nextState returns `nil`.
-	nextState() State
+	NextState() State
 
-	// memberIndex returns the index of member associated with the current state.
-	memberIndex() member.Index
+	// MemberIndex returns the index of member associated with the current state.
+	MemberIndex() member.Index
 
-	// isFinalState returns true when final state is reached.
-	isFinalState() bool
+	// IsFinalState returns true when final state is reached.
+	IsFinalState() bool
 }
 
 // Execute state machine starting with initial state up to finalization.
@@ -76,26 +76,26 @@ func Execute(
 		case msg := <-recvChan:
 			fmt.Printf(
 				"[member:%v, state:%T] Processing message\n",
-				currentState.memberIndex(),
+				currentState.MemberIndex(),
 				currentState,
 			)
 
-			err := currentState.receive(msg)
+			err := currentState.Receive(msg)
 			if err != nil {
 				fmt.Printf(
 					"[member:%v, state: %T] Failed to receive a message [%v]\n",
-					currentState.memberIndex(),
+					currentState.MemberIndex(),
 					currentState,
 					err,
 				)
 			}
 
 		case <-blockWaiter:
-			if currentState.isFinalState() {
+			if currentState.IsFinalState() {
 				return currentState, nil
 			}
 
-			currentState = currentState.nextState()
+			currentState = currentState.NextState()
 			blockWaiter, err = stateTransition(currentState, blockCounter)
 			if err != nil {
 				return nil, err
@@ -112,7 +112,7 @@ func stateTransition(
 ) (<-chan int, error) {
 	fmt.Printf(
 		"[member:%v, state:%T] Transitioning to a new state...\n",
-		currentState.memberIndex(),
+		currentState.MemberIndex(),
 		currentState,
 	)
 
@@ -125,12 +125,12 @@ func stateTransition(
 		)
 	}
 
-	err = currentState.initiate()
+	err = currentState.Initiate()
 	if err != nil {
 		return nil, fmt.Errorf("failed to initiate new state [%v]", err)
 	}
 
-	blockWaiter, err := blockCounter.BlockWaiter(currentState.activeBlocks())
+	blockWaiter, err := blockCounter.BlockWaiter(currentState.ActiveBlocks())
 	if err != nil {
 		return nil, fmt.Errorf(
 			"failed to initialize blockCounter.BlockWaiter state [%T]: [%v]",
@@ -141,7 +141,7 @@ func stateTransition(
 
 	fmt.Printf(
 		"[member:%v, state:%T] Transitioned to new state\n",
-		currentState.memberIndex(),
+		currentState.MemberIndex(),
 		currentState,
 	)
 
