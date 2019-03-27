8e913256dfe839ce5901f31c31323f03a6c996ec
Raghav Gulati <raghav.gulati@keep.network>
2019-03-27 14:24:43 -0700

    Ensure we ignore messages from IA/DQ
    This involves ensuring our state is aware of IA/DQ members (pulled from
    the gjkr.Result).

--- pkg/beacon/relay/dkg2/result/states.go
+++ pkg/beacon/relay/dkg2/result/states.go
@@ -1,6 +1,7 @@
 package result
 
 import (
+	"github.com/keep-network/keep-core/pkg/beacon/relay/gjkr"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/member"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/state"
 	"github.com/keep-network/keep-core/pkg/net"
@@ -18,12 +19,19 @@ type resultSigningState struct {
 	channel net.BroadcastChannel
 	member  *SigningMember
 
+	result                gjkr.Result
+	disqualifiedMemberIDs []gjkr.MemberID
+	inactiveMemberIDs     []gjkr.MemberID
+
 	signedHashResults []*DKGResultHashSignatureMessage
 }
 
 func (rs *resultSigningState) ActiveBlocks() int { return 3 }
 
 func (rs *resultSigningState) Initiate() error {
+	rs.disqualifiedMemberIDs = rs.result.Disqualified
+	rs.inactiveMemberIDs = rs.result.Inactive
+
 	message, err := rs.member.SignDKGResult(nil, nil)
 	if err != nil {
 		return err
@@ -35,14 +43,26 @@ func (rs *resultSigningState) Initiate() error {
 }
 
 func (rs *resultSigningState) Receive(msg net.Message) error {
-	switch phaseMessage := msg.Payload().(type) {
+	switch signedMessage := msg.Payload().(type) {
 	case *DKGResultHashSignatureMessage:
 		// ignore messages from ourselves
-		if phaseMessage.senderIndex == rs.member.index {
+		if signedMessage.senderIndex == rs.member.index {
 			return nil
 		}
 
-		// is message sender accepted?
+		// ignore messages from DQ
+		for _, disqualifiedMember := range rs.disqualifiedMemberIDs {
+			if signedMessage.senderIndex == disqualifiedMember {
+				return nil
+			}
+		}
+
+		// ignore messages from IA
+		for _, inactiveMemeber := range rs.inactiveMemberIDs {
+			if signedMessage.senderIndex == inactiveMemeber {
+				return nil
+			}
+		}
 
 		// then add it to our list
 		rs.signedHashResults = append(rs.signedHashResults)
