473175f57b95e8e365ea382c4e6176fe8cd73d86
Raghav Gulati <raghav.gulati@keep.network>
2019-03-27 10:41:23 -0700

    Outline basic phase 13 state machine
    Introduce the signingState state (to transition into phase 13 and 14) as
    well as the resultSigningState which governs the transitions into, of,
    and out of Phase 13.

---
+++ pkg/beacon/relay/dkg2/result/states.go
@@ -0,0 +1,45 @@
+package result
+
+import (
+	"github.com/keep-network/keep-core/pkg/beacon/relay/member"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/state"
+	"github.com/keep-network/keep-core/pkg/net"
+)
+
+// represents a given state in the state machine for signing dkg results
+type signingState = state.State
+
+// resultSigningState is the state during which group members sign their preferred
+// dkg result (by hashing their dkg result, and then signing the result), and
+// share this over the broadcast channel.
+//
+// State covers phase 13 of the protocol.
+type resultSigningState struct {
+	channel net.BroadcastChannel
+	member  *SigningMember
+}
+
+func (rs *resultSigningState) ActiveBlocks() int { return 3 }
+
+func (rs *resultSigningState) Initiate() error {
+	// sign the dkg result
+	// send it on the broadcast channel
+
+	return nil
+}
+
+func (rs *resultSigningState) Receive(msg net.Message) error {
+	// is message from self?
+	// is message sender accepted?
+	// then add it to our list
+	return nil
+}
+
+func (rs *resultSigningState) Next() signingState {
+	// set up the verification state, phase 14
+	return nil
+}
+
+func (rs *resultSigningState) MemberIndex() member.Index {
+	return rs.member.index
+}
