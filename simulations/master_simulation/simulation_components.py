f2e38f8382023488be3f9a0d119060b69edac5cc
starsitar <prashanth@keep.network>
2019-03-29 09:17:59 +0530

    commented out watching relay process and watching entry process for Node class

--- simulations/master_simulation/simulation_components.py
+++ simulations/master_simulation/simulation_components.py
@@ -25,21 +25,18 @@ class Node:
         self.node_status = "online"
         
     #Connecting to Ethereum
- 
     def Connect_Node(self, env):
         self.node_failure_generator() 
         if self.node_status == "failed": yield env.exit() #checks if the node has failed
-        while True:
-            ethereum_conection_time = np.random.lognormal(3,1,) # assumes a lognormal distribution of connection time
-            if ethereum_conection_time>=30:
-                print (str(self.id) + " ethereum connection Failure" + "cycle="+str(self.cycle_count))
-                self.current_state = " not connected"
-                env.process(self.Connect_Node(env))
-            else:
-                print (str(self.id) + " ethereum connection success" + " cycle="+str(self.cycle_count))
-                env.process(self.Forking_MainLoop(env))               
-                self.current_state = " connected"
-        yield env.exit()
+        ethereum_conection_time = np.random.lognormal(3,1,) # assumes a lognormal distribution of connection time
+        if ethereum_conection_time>=30:
+            print (str(self.id) + " ethereum connection Failure" + "cycle="+str(self.cycle_count))
+            self.current_state = " not connected"
+            env.process(self.Connect_Node(env))
+        else:
+            print (str(self.id) + " ethereum connection success" + "cycle="+str(self.cycle_count))
+            env.process(self.Forking_MainLoop(env))               
+            self.current_state = "connected"
     
     def Forking_MainLoop(self,env):
         self.node_failure_generator()
@@ -47,23 +44,21 @@ class Node:
         print(str(self.id) + " Forking Main Loop" + " cycle="+str(self.cycle_count))
         env.process(self.Watching_RelayRequest(env))
         env.process(self.Watching_RelayEntry(env))  
-        yield env.exit()
     
-    # wait for relay request
+    """ # wait for relay request
     def Watching_RelayRequest(self, env):
         self.node_failure_generator()
         if self.node_status == "failed": yield env.exit()
         print(str(self.id)+" Watching Relay Request" + " cycle="+str(self.cycle_count))
-        self.relay_request_time = np.random.normal(3,1,)
-        yield env.exit()
+        self.relay_request_time = np.random.normal(3,1,) """
     
-    # watching for relay entry
+    """ # watching for relay entry
     def Watching_RelayEntry(self, env):
         self.node_failure_generator()
         if self.node_status == "failed": yield env.exit()
         print(str(self.id)+" Watching Relay Entry" + " cycle="+str(self.cycle_count))
         self.relay_entry_watch_time = np.random.normal(3,1,)
-        yield env.exit()
+        yield env.exit() """
 
     #join group
     def join_group(self,group_object,env):
@@ -155,7 +150,7 @@ class Group:
         self.status = "inactive"
         self.id = identity
         self.member_check = np.zeros(group_size) #tally of how many members are currently connected to the group
-        self.group = np.array(group_distr_matrix[self.id]) > 0
+        self.group = np.array(group_distr_matrix[self.id]) > 0 # the group distribution
         self.signing_events =[]
 
     def connect(self, node_id):
