e2e2dc90868dd4c2f84cfed30edf2c11eaa08076
starsitar <prashanth@keep.network>
2019-04-04 15:06:05 +0530

    created methods for node agent

---
+++ simulations/Mesa Simulations/agent.py
@@ -0,0 +1,91 @@
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+import numpy as np
+
+class Node(Agent):
+    """ Node: One hardware device used to stake tokens on the network. 
+    Each node will create virtual stakers proportional to
+    the number of tokens owned by the node 
+    
+    Attributes:
+    unique_id: unique int
+    token_amount: int value of tokens staked by node
+    node_status: status of node can be - not connected, connected
+    """
+    def __init__(self, unique_id, model, token_amt):
+        super().__init__(unique_id, model)
+        self.id = unique_id
+        self.token_amount = token_amt
+        self.connection_status = "not connected" #change later to event - currently used for node failure process
+        self.mainloop_status = "not forked"
+        self.stake_status = "not staked"
+        self.connection_delay = np.random.randint(0,100) #uniform randomly assigned connection delay step value
+        self.mainloop_fork_delay = np.random.randint(0,100) #uniform randomly assigned connection delay step value
+    
+    def step(self):
+        #connect to chain
+        if self.connection_delay>0:
+            self.connection_delay -=1
+        elif self.connection_status == "not connected":
+            self.connection_status = "connected"
+        else:
+        #once connected fork the main loop
+            if self.mainloop_fork_delay>0:
+                self.mainloop_fork_delay -=1
+            elif self.mainloop_fork_delay == "not forked":
+                self.mainloop_status = "forked"
+            elif self.stake_status == "not staked":
+                self.generate_virtual_stakers()
+        return
+
+    def generate_virtual_stakers(self):
+        #add code here to create virtual staker distribution
+        self.stake_status = "staked"
+
+    def node_disconnect(self):
+        # disconnect the node from the network; causes it to go through the entire reconnection sequence in the next step
+        self.connection_status = "not connected"
+        self.mainloop_status = "not forked"
+        self.stake_status = "not staked"
+
+class Virtial_Stakers(Agent):
+    """ Virtual Stakers aka Tickets 
+    Attributes:
+    unique_id: unique int
+    owner_id = id of the node that owns this virtual staker
+    """
+    def __init__(self, unique_id, model, owner_id):
+        super().__init__(unique_id, model)
+        self.id = unique_id
+        self.owner = owner_id
+        self.
+
+    def step():
+        pass
+
+    # wait for relay request
+    def watching_relay_request(self, env):
+        pass
+
+    #join group
+    def join_group(self, env, group_object):
+        pass
+
+class Group(Agent):
+    """ A Group """
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step():
+        pass
+    
+    def sign():
+        pass
+
+    def expire():
+        pass
+
+class Beacon_Model(Model):
+    """The model"""
+    def __init__(self, N):
+        pass
