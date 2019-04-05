b03d63395c369efd7d62aace115ee120f35ea64e
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-04-05 09:10:44 +0200

    dkg2 package renamed to dkg
    dkg2 was a temporary name when we were developing a new version of DKG
    protocol while having the old dkg version still in place. Since we have
    fully switched to the new version, we can do the rename.

--- pkg/beacon/relay/node.go
+++ pkg/beacon/relay/node.go
@@ -10,7 +10,7 @@ import (
 
 	relaychain "github.com/keep-network/keep-core/pkg/beacon/relay/chain"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/config"
-	"github.com/keep-network/keep-core/pkg/beacon/relay/dkg2"
+	"github.com/keep-network/keep-core/pkg/beacon/relay/dkg"
 	"github.com/keep-network/keep-core/pkg/beacon/relay/groupselection"
 	"github.com/keep-network/keep-core/pkg/chain"
 	"github.com/keep-network/keep-core/pkg/net"
@@ -40,7 +40,7 @@ type Node struct {
 }
 
 type membership struct {
-	member  *dkg2.ThresholdSigner
+	member  *dkg.ThresholdSigner
 	channel net.BroadcastChannel
 	index   int
 }
@@ -93,7 +93,7 @@ func (n *Node) JoinGroupIfEligible(
 			}
 
 			go func() {
-				signer, err := dkg2.ExecuteDKG(
+				signer, err := dkg.ExecuteDKG(
 					entryRequestID,
 					entrySeed,
 					playerIndex,
@@ -195,7 +195,7 @@ func (n *Node) flushPendingGroup(requestID string) {
 // We overwrite our placeholder membership set by initializePendingGroup.
 func (n *Node) registerPendingGroup(
 	requestID string,
-	signer *dkg2.ThresholdSigner,
+	signer *dkg.ThresholdSigner,
 	channel net.BroadcastChannel,
 ) {
 	n.mutex.Lock()
