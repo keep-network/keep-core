e7f55447c75e1c7a2c8ddcf925b5b78ba98243d6
starsitar <prashanth@keep.network>
2019-04-09 14:28:07 +0530

    changed self.ticket_list to not save all generated tickets

--- simulations/Mesa Simulations/agent.py
+++ simulations/Mesa Simulations/agent.py
@@ -21,36 +21,35 @@ class Node(Agent):
         self.connection_status = "not connected" #change later to event - currently used for node failure process
         self.mainloop_status = "not forked"
         self.stake_status = "not staked"
-        self.connection_delay = np.random.randint(0,100) #uniform randomly assigned connection delay step value
-        self.mainloop_fork_delay = np.random.randint(0,100) #uniform randomly assigned connection delay step value
+        self.connection_delay = np.random.randint(0,3) #uniform randomly assigned connection delay step value
+        self.mainloop_fork_delay = np.random.randint(0,3) #uniform randomly assigned connection delay step value
         self.timer = self.model.timer
     
     def step(self):
         #connect to chain
         if self.connection_delay>0:
             self.connection_delay -=1
-        elif self.connection_status == "not connected":
-            self.connection_status = "connected"
         else:
+            self.connection_status = "connected"
         #once connected fork the main loop
             if self.mainloop_fork_delay>0:
                 self.mainloop_fork_delay -=1
-            elif self.mainloop_fork_delay == "not forked":
+            else: 
                 self.mainloop_status = "forked"
-            elif self.stake_status == "not staked":
-                self.generate_tickets()
-
+            
         #simulate node failure
-        
+
 
     def advance(self):
         if self.model.relay_request:
             self.generate_tickets()
+        #print("Mainloop fork delay = " + str(self.mainloop_fork_delay))
+        #print("Mainloop_status = " + self.mainloop_status)
 
     def generate_tickets(self):
         #generates tickets using the uniform distribution
         self.stake_status = "staked"
-        self.ticket_list.append(np.random.random_sample(self.num_tickets))
+        self.ticket_list = np.random.random_sample(self.num_tickets))
         
     def node_disconnect(self):
         # disconnect the node from the network; causes it to go through the entire reconnection sequence in the next step
--- simulations/Mesa Simulations/model.py
+++ simulations/Mesa Simulations/model.py
@@ -11,7 +11,7 @@ class Beacon_Model(Model):
         self.relay_request = False
         self.active_groups = []
         self.active_nodes = []
-        self.active_group_threshold = active_group_threshold
+        self.active_group_threshold = active_group_threshold # number of groups that will always be maintained in an active state
         self.signature_threshold = signature_threshold
         self.group_size = group_size
         self.ticket_distribution = ticket_distribution
@@ -29,8 +29,11 @@ class Beacon_Model(Model):
 
     def step(self):
         '''Advance the model by one step'''
+        print("newest group id" + str(self.newest_group_id))
+        
         #check how many active nodes are available
-        self.refresh_connected_nodes_list
+        self.refresh_connected_nodes_list()
+        print("Number of nodes in the forked state = " + str(len(self.active_nodes)))
 
 
         #bootstrap active groups as nodes become available. Can only happen once enough nodes are online
@@ -42,7 +45,7 @@ class Beacon_Model(Model):
             
 
         #check how many active groups are available
-        self.refresh_active_list()
+        self.refresh_active_group_list()
         print('number of active groups = ' + str(len(self.active_groups)))
         
         #generate relay requests
@@ -68,35 +71,40 @@ class Beacon_Model(Model):
         ticket_list = []
         group_members = []
 
-        # make each node generate tickets and save them to a list
-        max_tickets = int(max(self.ticket_distribution))
-        for node in self.active_nodes:
-            adjusted_ticket_list = []
-            node.generate_tickets()
-            adjusted_ticket_list = np.concatenate([node.ticket_list[self.newest_group_id],np.ones(int(max_tickets)-len(node.ticket_list[self.newest_group_id]))])  #adds 2's the ends of the list so that the 2D array can have equal length rows
-            ticket_list.append(adjusted_ticket_list)
+        if len(self.active_nodes)<self.group_formation_threshold:
+            print("Not enough nodes to register a group")
 
-        #iteratively add group members by lowest value
-        while len(group_members) <= self.group_size:
+        else:
+            # make each node generate tickets and save them to a list
+            max_tickets = int(max(self.ticket_distribution))
+            for node in self.active_nodes:
+                adjusted_ticket_list = []
+                node.generate_tickets()
+                print("node.ticket_list[self.newest_group_id]")
+                print(self.newest_group_id)
+                adjusted_ticket_list = np.concatenate([node.ticket_list[self.newest_group_id],np.ones(int(max_tickets)-len(node.ticket_list[self.newest_group_id]))])  #adds 2's the ends of the list so that the 2D array can have equal length rows
+                ticket_list.append(adjusted_ticket_list)
 
-            min_index = np.where(ticket_list == np.min(ticket_list)) # find the index of the minimum value in the array
-            for i,index in enumerate(min_index[0]): #if there are repeated values, iterate through and add the indexes to the group
-                group_members.append(index)
-                ticket_list[index][min_index[1][i]] = 2 # Set the value of the ticket to a high value so it doesn't get counted again
-        
-        #create a group agent which can track expiry, sign, etc
-        group_object = agent.Group(self.newest_group_id, self, group_members, self.group_expiry, self.signature_threshold)
+            #iteratively add group members by lowest value
+            while len(group_members) <= self.group_size:
 
-        #add group to schedule
-        self.schedule.add(group_object)
+                min_index = np.where(ticket_list == np.min(ticket_list)) # find the index of the minimum value in the array
+                for i,index in enumerate(min_index[0]): #if there are repeated values, iterate through and add the indexes to the group
+                    group_members.append(index)
+                    ticket_list[index][min_index[1][i]] = 2 # Set the value of the ticket to a high value so it doesn't get counted again
+            
+            #create a group agent which can track expiry, sign, etc
+            group_object = agent.Group(self.newest_group_id, self, group_members, self.group_expiry, self.signature_threshold)
 
-        #add group to active group list
-        self.active_groups.append(group_object)
+            #add group to schedule
+            self.schedule.add(group_object)
 
+            #add group to active group list
+            self.active_groups.append(group_object)
+            
+            return group_object
 
-        return group_object
-
-    def refresh_active_list(self):
+    def refresh_active_group_list(self):
         temp_list = []
 
         for group in self.active_groups:
@@ -106,10 +114,13 @@ class Beacon_Model(Model):
         self.active_groups = temp_list
 
     def refresh_connected_nodes_list(self):
+        print("refreshing active nodes list")
         temp_active_node_list = []
         for agent in self.schedule.agents:
+            print("agent id = " + str(agent.id))
             if agent.type == "node":
-                if agent.connection_status == "forked": 
+                if agent.mainloop_status == "forked": 
+                    print("forked agent ID" + str(agent.id))
                     temp_active_node_list.append(agent) #adds the node to the active list only if it is in the forked state
         self.active_nodes = temp_active_node_list
 
