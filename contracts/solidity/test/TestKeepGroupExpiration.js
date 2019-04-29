05c83cfea208873235b0e39c804496aeaa30bf9f
madxor <marcin.pawlowski@akena.co>
2019-04-29 10:58:03 +0200

    Remove unlimited timeout

--- contracts/solidity/test/TestKeepGroupExpiration.js
+++ contracts/solidity/test/TestKeepGroupExpiration.js
@@ -70,7 +70,6 @@ contract('TestKeepGroupExpiration', function(accounts) {
   });
 
   it("should be able to check that groups are marked as expired except the minimal active groups number", async function() {
-    this.timeout(0);
 
     let after = await keepGroupImplViaProxy.numberOfGroups();
 
