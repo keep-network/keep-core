f96492f733cba85100e5a2b818ae9b67045d5783
sthompson22 <sloan.thompson@thesis.co>
2019-05-08 09:15:24 -0400

    Bake InitContainer For now were baking the InitContainer and the required scripts into a Docker image we can deploy.  Two parts to this, it cuts down on the assets I have to fox with during testing and I'm having some permission issues when trying to execute the script when it's mounted as a volume from a ConfigMap.

---
+++ infrastructure/kube/keep-dev/keep-client/initcontainer/unlock-and-stake-eth-account.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+function unlock_eth_account() {
+  geth --exec "personal.unlockAccount(\"$ETHEREUM_ACCOUNT\", \"$ETHEREUM_PASSWORD\", 700)" attach http://eth-tx-node.default.svc.cluster.local:8545
+}
+
+unlock_eth_account
