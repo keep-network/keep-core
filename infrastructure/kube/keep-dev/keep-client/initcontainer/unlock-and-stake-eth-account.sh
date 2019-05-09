4eefc4199a1e10ed7e3a34f61171aedc50e677c1
sthompson22 <sloan.thompson@thesis.co>
2019-05-09 11:16:27 -0400

    Load stake account script

--- infrastructure/kube/keep-dev/keep-client/initcontainer/unlock-and-stake-eth-account.sh
+++ infrastructure/kube/keep-dev/keep-client/initcontainer/unlock-and-stake-eth-account.sh
@@ -4,4 +4,8 @@ function unlock_eth_account() {
   geth --exec "personal.unlockAccount(\"$ETHEREUM_ACCOUNT\", \"$ETHEREUM_PASSWORD\", 700)" attach http://eth-tx-node.default.svc.cluster.local:8545
 }
 
+function stake_eth_account() {
+  geth --exec 'loadScript("./stake-eth-account.js");' attach http://eth-tx-node.default.svc.cluster.local:8545
+}
+
 unlock_eth_account
