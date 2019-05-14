924836741c2f5518aa6e0f82de1f917284c39b3b
Marcin Pawlowski <marcin.pawlowski@akena.co>
2019-05-14 15:43:13 +0200

    Add tests detecting not released operator after unstaking

--- contracts/solidity/test/keeptoken.js
+++ contracts/solidity/test/keeptoken.js
@@ -228,11 +228,100 @@ contract('KeepToken', function(accounts) {
     await grantContract.finishUnstake(stakeWithdrawalId);
     account_two_operator_stake_balance = await grantContract.stakeBalanceOf.call(account_two_operator);
     assert.equal(account_two_operator_stake_balance.isZero(), true, "Stake grant amount should be 0");
+    assert.equal(await grantContract.operatorsOf.call(account_two), 0, "Operator should be released after finishing unstake");
 
     // should be able to release 'releasable' granted amount as it's not locked for staking anymore
     await grantContract.release(id);
     let account_two_ending_balance = await token.balanceOf.call(account_two);
     assert.equal(account_two_ending_balance.gte(amount.div(web3.utils.toBN(2))), true, "Should have some released grant amount");
 
+  });
+
+  it("should be able to delegate stake to the same operator after finishing unstaking", async function() {
+    let amount = web3.utils.toBN(1000000000);
+    let vestingDuration = duration.days(60);
+    let start = await latestTime();
+    let cliff = duration.days(10);
+    let revocable = true;
+
+    // Grant tokens
+    await token.approve(grantContract.address, amount, {from: account_one});
+    let id = await grantContract.grant(amount, account_two, vestingDuration,
+      start, cliff, revocable, {from: account_one}).then((result)=>{
+      // Look for CreatedTokenGrant event in transaction receipt and get grant id
+      for (var i = 0; i < result.logs.length; i++) {
+        var log = result.logs[i];
+        if (log.event == "CreatedTokenGrant") {
+          return log.args.id.toNumber();
+        }
+      }
+    })
+
+    let signature = Buffer.from((await web3.eth.sign(web3.utils.soliditySha3(account_two), account_two_operator)).substr(2), 'hex');
+    let delegation = Buffer.concat([Buffer.from(account_two_magpie.substr(2), 'hex'), signature]);
+
+    // should throw if stake granted tokens called by anyone except grant beneficiary
+    await exceptThrow(grantContract.stake(id, delegation));
+
+    // stake granted tokens can be only called by grant beneficiary
+    await grantContract.stake(id, delegation, {from: account_two});
+    let account_two_operator_stake_balance = await grantContract.stakeBalanceOf.call(account_two_operator);
+    assert.equal(account_two_operator_stake_balance.eq(amount), true, "Should stake grant amount");
+
+    // should throw if initiate unstake called by anyone except grant beneficiary
+    await exceptThrow(grantContract.initiateUnstake(id, account_two_operator));
+
+    // Initiate unstake of granted tokens by grant beneficiary
+    let stakeWithdrawalId = await grantContract.initiateUnstake(id, account_two_operator, {from: account_two}).then((result)=>{
+      // Look for InitiatedTokenGrantUnstake event in transaction receipt and get stake withdrawal id
+      for (var i = 0; i < result.logs.length; i++) {
+        var log = result.logs[i];
+        if (log.event == "InitiatedTokenGrantUnstake") {
+          return log.args.id.toNumber();
+        }
+      }
+    });
+
+    // should not be able to finish unstake before withdrawal delay is over
+    await exceptThrow(grantContract.finishUnstake(stakeWithdrawalId));
+
+    // should not be able to release grant as its still locked for staking
+    await exceptThrow(grantContract.release(id));
+
+    // jump in time over withdrawal delay
+    await increaseTimeTo(await latestTime()+duration.days(30));
+    await grantContract.finishUnstake(stakeWithdrawalId);
+    account_two_operator_stake_balance = await grantContract.stakeBalanceOf.call(account_two_operator);
+    assert.equal(account_two_operator_stake_balance.isZero(), true, "Stake grant amount should be 0");
+    assert.equal(await grantContract.operatorsOf.call(account_two), 0, "Operator should be released after finishing unstake");
+
+    // should be able to release 'releasable' granted amount as it's not locked for staking anymore
+    await grantContract.release(id);
+    let account_two_ending_balance = await token.balanceOf.call(account_two);
+    assert.equal(account_two_ending_balance.gte(amount.div(web3.utils.toBN(2))), true, "Should have some released grant amount");
+
+    // Grant tokens
+    await token.approve(grantContract.address, amount, {from: account_one});
+    id = await grantContract.grant(amount, account_two, vestingDuration,
+      start, cliff, revocable, {from: account_one}).then((result)=>{
+      // Look for CreatedTokenGrant event in transaction receipt and get grant id
+      for (var i = 0; i < result.logs.length; i++) {
+        var log = result.logs[i];
+        if (log.event == "CreatedTokenGrant") {
+          return log.args.id.toNumber();
+        }
+      }
+    })
+
+    signature = Buffer.from((await web3.eth.sign(web3.utils.soliditySha3(account_two), account_two_operator)).substr(2), 'hex');
+    delegation = Buffer.concat([Buffer.from(account_two_magpie.substr(2), 'hex'), signature]);
+
+    // should throw if stake granted tokens called by anyone except grant beneficiary
+    await exceptThrow(grantContract.stake(id, delegation));
+
+    // stake granted tokens can be only called by grant beneficiary
+    await grantContract.stake(id, delegation, {from: account_two});
+    account_two_operator_stake_balance = await grantContract.stakeBalanceOf.call(account_two_operator);
+    assert.equal(account_two_operator_stake_balance.eq(amount), true, "Should stake grant amount");
   });
 });
