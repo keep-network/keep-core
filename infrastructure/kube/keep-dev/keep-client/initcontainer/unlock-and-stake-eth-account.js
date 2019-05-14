818260dfa8a8801a351394455283f98f8b74e644
sthompson22 <sloan.thompson@thesis.co>
2019-05-14 11:26:21 -0400

    Moving hard-coded test values out to configurable ones

--- infrastructure/kube/keep-dev/keep-client/initcontainer/unlock-and-stake-eth-account.js
+++ infrastructure/kube/keep-dev/keep-client/initcontainer/unlock-and-stake-eth-account.js
@@ -1,27 +1,34 @@
 const fs = require('fs');
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('http://eth-tx-node.default.svc.cluster.local:8545'));
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETHEREUM_HOSTNAME + ":" + process.env.ETHEREUM_HOST_PORT));
 
-// Set contract abi file
+// Set compiled contract json
 const stakingProxyContractJsonFile = "/Users/sthompson22/projects/keep-core/contracts/solidity/build/contracts/StakingProxy.json";
 const tokenStakingContractJsonFile = "/Users/sthompson22/projects/keep-core/contracts/solidity/build/contracts/TokenStaking.json";
 const keepTokenContractJsonFile = "/Users/sthompson22/projects/keep-core/contracts/solidity/build/contracts/KeepToken.json";
 
-// Select abi
+// We need to do some parse-fu to get at the values we need
 const stakingProxyContractParsed = JSON.parse(fs.readFileSync(stakingProxyContractJsonFile));
 const tokenStakingContractParsed = JSON.parse(fs.readFileSync(tokenStakingContractJsonFile));
 const keepTokenContractParsed = JSON.parse(fs.readFileSync(keepTokenContractJsonFile));
 
+// .abi is used to set contract functions
 const stakingProxyContractAbi = stakingProxyContractParsed.abi;
 const tokenStakingContractAbi = tokenStakingContractParsed.abi;
 const keepTokenContractAbi = keepTokenContractParsed.abi;
 
-// Set contracts
-const stakingProxyContract = new web3.eth.Contract(stakingProxyContractAbi, "0x9F77E21dB16ef8218640Be6E62472B675366377f");
-const tokenStakingContract = new web3.eth.Contract(tokenStakingContractAbi, "0xeFB960Ca430F982c0b6650C3544fD8244A117407");
-const keepTokenContract = new web3.eth.Contract(keepTokenContractAbi, "0x67a6c635b967fDBDB313eF6C043117b6780f978E");
+// Set the current contract address for the chosen network
+const stakingProxyContractAddress = stakingProxyContractParsed.networks[process.env.ETH_NETWORK_ID].address;
+const tokenStakingContractAddress = tokenStakingContractParsed.networks[process.env.ETH_NETWORK_ID].address;
+const keepTokenContractAddress = keepTokenContractParsed.networks[process.env.ETH_NETWORK_ID].address;
 
-const owner = "0x0F0977c4161a371B5E5eE6a8F43Eb798cD1Ae1DB"
+// Set contracts to be used for staking an Ethereum account
+const stakingProxyContract = new web3.eth.Contract(stakingProxyContractAbi, stakingProxyContractAddress);
+const tokenStakingContract = new web3.eth.Contract(tokenStakingContractAbi, tokenStakingContractAddress);
+const keepTokenContract = new web3.eth.Contract(keepTokenContractAbi, keepTokenContractAddress);
+
+// Ethereum account that contracts were migrated against
+const contract_owner = process.env.CONTRACT_OWNER_ETH_ACCOUNT_ADDRESS
 
 // \heimdall aliens numbers
 function formatAmount(amount, decimals) {
@@ -30,21 +37,21 @@ function formatAmount(amount, decimals) {
 
 // Stake a target eth account
 async function stakeEthAccount() {
-  let magpie = "0x0F0977c4161a371B5E5eE6a8F43Eb798cD1Ae1DB";
-  let operator = "0xa924d3a62b2d515235e5de5d903c405cba7f0e86";
+  let magpie = process.env.CONTRACT_OWNER_ETH_ACCOUNT_ADDRESS;
+  let operator = process.env.KEEP_CLIENT_ETH_ACCOUNT_ADDRESS;
 
-  let signature = Buffer.from((await web3.eth.sign(web3.utils.soliditySha3(owner), operator)).substr(2), 'hex');
+  let signature = Buffer.from((await web3.eth.sign(web3.utils.soliditySha3(contract_owner), operator)).substr(2), 'hex');
   let delegation = '0x' + Buffer.concat([Buffer.from(magpie.substr(2), 'hex'), signature]).toString('hex');
 
   console.log(signature)
   console.log(delegation)
 
   try{
-    if (!await stakingProxyContract.methods.isAuthorized(tokenStakingContract.address).send({from: owner}).then((receipt) => {
+    if (!await stakingProxyContract.methods.isAuthorized(tokenStakingContract.address).send({from: contract_owner}).then((receipt) => {
         console.log("isAuthorized transaction receipt:")
         console.log(receipt)
     })) {
-      await stakingProxyContract.methods.authorizeContract(tokenStakingContract.address).send({from: owner}).then((receipt) => {
+      await stakingProxyContract.methods.authorizeContract(tokenStakingContract.address).send({from: contract_owner}).then((receipt) => {
         console.log("authorizeContract transaction receipt:")
         console.log(receipt)
       })
@@ -59,7 +66,7 @@ async function stakeEthAccount() {
     await keepTokenContract.methods.approveAndCall(
       tokenStakingContract.address,
       formatAmount(1000000, 18),
-      delegation).send({from: owner, gas: 4712388}).then((receipt) => {
+      delegation).send({from: contract_owner, gas: 4712388}).then((receipt) => {
         console.log("Account " + operator + " staked!");
         console.log(receipt);
       });
@@ -72,14 +79,14 @@ async function stakeEthAccount() {
 async function unlockEthAccount() {
 
   try {
-    console.log("Unlocking account: " + owner);
-    await web3.eth.personal.unlockAccount(owner, "doughnut_armenian_parallel_firework_backbite_employer_singlet", 700);
-    console.log("Account: " + owner + " unlocked!");
+    console.log("Unlocking account: " + contract_owner);
+    await web3.eth.personal.unlockAccount(contract_owner, process.env.KEEP_CLIENT_ETH_ACCOUNT_PASSPHRASE, 700);
+    console.log("Account: " + contract_owner + " unlocked!");
   }
   catch(error) {
     console.error(error);
   }
 }
 
-unlockEthAccount();
-stakeEthAccount();
+//unlockEthAccount();
+//stakeEthAccount();
