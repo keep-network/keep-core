c14383b98549533c35ee281edcd653514ed2bdd0
sthompson22 <sloan.thompson@thesis.co>
2019-05-15 16:29:51 -0400

    SCP to target dir were persisting, add some logs

--- contracts/solidity/scripts/circleci-migrate-contracts.sh
+++ contracts/solidity/scripts/circleci-migrate-contracts.sh
@@ -18,21 +18,27 @@ Host utilitybox
   ProxyCommand ssh -W %h:%p $GOOGLE_PROJECT_NAME-jumphost.$GOOGLE_COMPUTE_ZONE_A.$GOOGLE_PROJECT_ID
 EOF
 
-# Copy deployment artifacts over
+# Copy migration artifacts over
 ssh utilitybox rm -rf /tmp/$BUILD_TAG
 ssh utilitybox mkdir /tmp/$BUILD_TAG
 scp -r contracts/solidity utilitybox:/tmp/$BUILD_TAG/
 
-# Run deployment
+# Run migration
 ssh utilitybox << EOF
+  echo "<<<<<<START Download Kube Creds<<<<<<"
   gcloud container clusters get-credentials $GOOGLE_PROJECT_NAME --region $GOOGLE_REGION --internal-ip --project=$GOOGLE_PROJECT_ID
+  echo ">>>>>>FINISH Download Kube Creds>>>>>>"
 
+  echo "<<<<<<START Port Forward eth-tx-node<<<<<<"
   nohup timeout 600 kubectl port-forward svc/eth-tx-node 8545:8545 2>&1 > /dev/null &
-
   sleep 10s
+  echo ">>>>>>FINISH Port Forward eth-tx-node>>>>>>"
 
+  echo "<<<<<<START Unlock Contract Owner ETH Account<<<<<<"
   geth --exec "personal.unlockAccount(\"${CONTRACT_OWNER_ETH_ACCOUNT_ADDRESS}\", \"${CONTRACT_OWNER_ETH_ACCOUNT_PASSWORD}\", 600)" attach http://localhost:8545
+  echo ">>>>>>FINISH Unlock Contract Owner ETH Account>>>>>>"
 
+  echo "<<<<<<START Contract Migration<<<<<<"
   cd /tmp/$BUILD_TAG/solidity
 
   npm init -y
@@ -44,7 +50,14 @@ ssh utilitybox << EOF
 
   cp ./truffle_sample.js ./truffle.js
   ./node_modules/.bin/truffle migrate --reset --network $TRUFFLE_NETWORK
+  echo ">>>>>>FINISH Contract Migration>>>>>>"
 EOF
 
-scp utilitybox:/tmp/$BUILD_TAG/solidity/build/contracts/* /tmp/contracts
-#ssh utilitybox rm -rf /tmp/$BUILD_TAG
+echo "<<<<<<START Contract Copy<<<<<<"
+scp utilitybox:/tmp/$BUILD_TAG/solidity/build/contracts/* /tmp/keep-client/contracts
+echo ">>>>>>FINISH Contract Copy>>>>>>"
+
+echo "<<<<<<START Migration Dir Cleanup<<<<<<"
+ssh utilitybox rm -rf /tmp/$BUILD_TAG
+echo ">>>>>>FINISH Contract Migration>>>>>>"
+
