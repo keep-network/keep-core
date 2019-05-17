7bebc55ecdbb116fb0d40e23af91505a67e579f1
Nik G <subzeromedia@gmail.com>
2019-05-17 07:53:28 -0500

    Refactor staking table as a functional component

--- contracts/solidity/dashboard/src/components/StakingTable.jsx
+++ contracts/solidity/dashboard/src/components/StakingTable.jsx
@@ -1,37 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Table } from 'react-bootstrap'
 import { displayAmount } from '../utils'
 import WithWeb3Context from './WithWeb3Context'
 import UndelegateStakeButton from './UndelegateStakeButton'
 
-class StakingTable extends Component {
-  render() {
-    if (this.props.data) {
-      var rows = this.props.data.map(function(item, i){
-        return (
-          <tr key={"stake-delegate-"+i+"-to-"+item.address}>
-            <td>{displayAmount(item.amount,18, 3)}</td>
-            <td><a href="/">{item.address}</a></td>
-            <td><UndelegateStakeButton key={i} amount={item.amount} operator={item.address}/></td>
-          </tr>
-        )
-      })
-    }
-    return (
-      <Table className="small table-sm" condensed>
-        <thead>
-          <tr>
-            <th><strong>Amount</strong></th>
-            <th><strong>Operator</strong></th>
-            <th><strong>Action</strong></th>
-          </tr>
-        </thead>
-        <tbody>
-          { rows }
-        </tbody>
-      </Table>
-    )
+const StakingTable = (props) => {
+  if (props.data) {
+    var rows = props.data.map(function(item, i){
+      return (
+        <tr key={`stake-delegate-${i}-to-${item.address}`}>
+          <td>{displayAmount(item.amount,18, 3)}</td>
+          <td><a href="/">{item.address}</a></td>
+          <td><UndelegateStakeButton amount={item.amount} operator={item.address}/></td>
+        </tr>
+      )
+    })
   }
+  return (
+    <Table className="small table-sm" condensed>
+      <thead>
+        <tr>
+          <th><strong>Amount</strong></th>
+          <th><strong>Operator</strong></th>
+          <th><strong>Action</strong></th>
+        </tr>
+      </thead>
+      <tbody>
+        { rows }
+      </tbody>
+    </Table>
+  )
 }
 
 export default WithWeb3Context(StakingTable)
