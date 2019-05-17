5957e5584e56f21c5b2c3ed9bbe5e09d4109ae91
Nik G <subzeromedia@gmail.com>
2019-05-17 15:46:01 -0500

    Improve web3 loader

--- contracts/solidity/dashboard/src/utils.js
+++ contracts/solidity/dashboard/src/utils.js
@@ -17,38 +17,38 @@ export function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-export function getWeb3() {
-  return new Promise((resolve, reject) => {
-    // Wait for loading completion to avoid race conditions with web3 injection timing.
-    window.addEventListener("load", async () => {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          // Acccounts now exposed
-          resolve(web3);
-        } catch (error) {
-          reject(error);
-        }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-        const web3 = window.web3;
-        console.log("Injected web3 detected.");
-        resolve(web3);
-      }
-      // Fallback to localhost; use dev console port by default...
-      else {
-        const provider = new Web3.providers.HttpProvider(
-          "http://127.0.0.1:9545"
-        );
-        const web3 = new Web3(provider);
-        console.log("No web3 instance injected, using Local web3.");
-        resolve(web3);
-      }
-    });
-  })
+let web3;
+
+export const getWeb3 = async () => {
+
+  if (web3) return web3
+
+  // Modern dapp browsers...
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum)
+    try {
+      // Request account access if needed
+      await window.ethereum.enable()
+      // Acccounts now exposed
+      return web3;
+    } catch (error) {
+      return error;
+    }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    // Use Mist/MetaMask's provider.
+    web3 = new Web3(window.web3.currentProvider)
+    console.log("Injected web3 detected.")
+    return web3;
+  }
+  // Fallback to localhost; use dev console port by default...
+  else {
+    const provider = new Web3.providers.HttpProvider(
+      "http://127.0.0.1:9545"
+    );
+    web3 = new Web3(provider);
+    console.log("No web3 instance injected, using Local web3.")
+    return web3;
+  }
 }
