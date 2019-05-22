0d3ae274dc2f6a3dc21b27bb5a168797b38f6954
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-05-22 15:17:26 +0200

    Implemented additional tests for addressForContract function
    - test covering unknown contract name scenario
    - test covering invalid contract address scenario

--- pkg/chain/ethereum/connect_test.go
+++ pkg/chain/ethereum/connect_test.go
@@ -1,39 +1,78 @@
 package ethereum
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
 
-func TestResolveContractByValidName(t *testing.T) {
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestResolveContractByName(t *testing.T) {
 	config := Config{
 		ContractAddresses: make(map[string]string),
 	}
 
-	conquerorAddress := "0x1895e4A71d0956553cf80f2ccac69642a2f1bFF4"
 	barbarianAddress := "0x0b185C37E1C9D01437c800a8B60fA0845742c271"
+	conquerorAddress := "0x1895e4A71d0956553cf80f2ccac69642a2f1bFF4"
+	destroyerAddress := "0xYOLO" // invalid HEX address
 
-	config.ContractAddresses["ConanTheConqueror"] = conquerorAddress
 	config.ContractAddresses["ConanTheBarbarian"] = barbarianAddress
+	config.ContractAddresses["ConanTheConqueror"] = conquerorAddress
+	config.ContractAddresses["ConanTheDestroyer"] = destroyerAddress
 
-	address, err := addressForContract(config, "ConanTheConqueror")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if address.Hex() != conquerorAddress {
-		t.Fatalf(
-			"Unexpected contract address\nExpected: [%v]\nActual:   [%v]\n",
-			conquerorAddress,
-			address.Hex(),
-		)
+	toCommonAddress := func(address string) *common.Address {
+		commonAddress := common.HexToAddress(address)
+		return &commonAddress
 	}
 
-	address, err = addressForContract(config, "ConanTheBarbarian")
-	if err != nil {
-		t.Fatal(err)
+	var tests = map[string]struct {
+		contracts               map[string]string
+		queriedContractName     string
+		expectedContractAddress *common.Address
+		expectedError           error
+	}{
+		"known contract name #1": {
+			queriedContractName:     "ConanTheBarbarian",
+			expectedContractAddress: toCommonAddress(barbarianAddress),
+			expectedError:           nil,
+		},
+		"known contract name #2": {
+			queriedContractName:     "ConanTheConqueror",
+			expectedContractAddress: toCommonAddress(conquerorAddress),
+			expectedError:           nil,
+		},
+		"unknown contract name": {
+			queriedContractName:     "ConanTheLegend",
+			expectedContractAddress: nil,
+			expectedError: fmt.Errorf(
+				"no address information for [ConanTheLegend] in configuration",
+			),
+		},
+		"invalid contract address": {
+			queriedContractName:     "ConanTheDestroyer",
+			expectedContractAddress: nil,
+			expectedError: fmt.Errorf(
+				"configured address [0xYOLO] for contract [ConanTheDestroyer] is not valid hex address",
+			),
+		},
 	}
-	if address.Hex() != barbarianAddress {
-		t.Fatalf(
-			"Unexpected contract address\nExpected: [%v]\nActual:   [%v]\n",
-			barbarianAddress,
-			address.Hex(),
-		)
+
+	for _, test := range tests {
+		address, err := addressForContract(config, test.queriedContractName)
+		if !reflect.DeepEqual(test.expectedContractAddress, address) {
+			t.Fatalf(
+				"Unexpected contract address\nExpected: [%v]\nActual:   [%v]\n",
+				test.expectedContractAddress,
+				address,
+			)
+		}
+		if !reflect.DeepEqual(test.expectedError, err) {
+			t.Fatalf(
+				"Unexpected error\nExpected: [%v]\nActual:   [%v]\n",
+				test.expectedError,
+				err,
+			)
+		}
 	}
 }
