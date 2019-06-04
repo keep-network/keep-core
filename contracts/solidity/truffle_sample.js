238f10d724c255d2a3b110a2a21ddb7dc52f3696
sthompson22 <sloan.thompson@thesis.co>
2019-06-04 13:49:31 -0400

    Commenting gas limit to see if we can get migrations to pass

--- contracts/solidity/truffle_sample.js
+++ contracts/solidity/truffle_sample.js
@@ -18,7 +18,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*",
-      gas: 4712388,
+      //gas: 4712388,
       from: "0x0F0977c4161a371B5E5eE6a8F43Eb798cD1Ae1DB"
     }
   },
