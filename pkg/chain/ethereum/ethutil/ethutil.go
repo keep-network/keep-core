921a982e10a3f0c58f731b61189b6bbc058d8f8a
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2019-06-11 11:32:48 -0400

    Add ethutil.AddressFromHex
    Unlike common.HexToAddress, this is a single stop for parsing a hex
    string to an address, and returns an error if the string is invalid for
    parsing. Tests included.

--- pkg/chain/ethereum/ethutil/ethutil.go
+++ pkg/chain/ethereum/ethutil/ethutil.go
@@ -7,7 +7,20 @@ import (
 	"io/ioutil"
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
 )
+
+// AddressFromHex converts the passed string to a common.Address and returns it,
+// unless it is not a valid address, in which case it returns an error. Compare
+// to common.HexToAddress, which assumes the address is valid and does not
+// provide for an error return.
+func AddressFromHex(hex string) (common.Address, error) {
+	if common.IsHexAddress(hex) {
+		return common.HexToAddress(hex), nil
+	}
+
+	return common.Address{}, fmt.Errorf("[%v] is not a valid Ethereum address", hex)
+}
 
 // DecryptKeyFile reads in a key file and uses the password to decrypt it.
 func DecryptKeyFile(keyFile, password string) (*keystore.Key, error) {
