e2d9d76b81b953cdbaad7abc16e384ad3f8731c3
Nik G <subzeromedia@gmail.com>
2019-06-19 14:18:42 -0500

    Minor test improvements

--- contracts/solidity/test/TestKeepRandomBeaconCallback.js
+++ contracts/solidity/test/TestKeepRandomBeaconCallback.js
@@ -24,16 +24,20 @@ contract('TestKeepRandomBeaconCallback', function() {
     let requestId = tx.logs[0].args.requestID;
     await keepRandomBeacon.relayEntry(requestId, bls.groupSignature, bls.groupPubKey, bls.previousEntry, bls.seed);
 
-    let result = await callbackContract.lastEntry();
-    assert.isFalse(result.eq(bls.groupSignature), "Value should not change on the callback contract.");
+    let result = await keepRandomBeacon.previousEntry();
+    assert.isTrue(result.eq(bls.groupSignature), "Value should be updated on beacon contract.");
   });
 
   it("should successfully call method on a callback contract", async function() {
     let tx = await keepRandomBeacon.methods['requestRelayEntry(uint256,address,string)'](bls.seed, callbackContract.address, "callback(uint256)", {value: 10});
     let requestId = tx.logs[0].args.requestID;
-    await keepRandomBeacon.relayEntry(requestId, bls.groupSignature, bls.groupPubKey, bls.previousEntry, bls.seed);
 
     let result = await callbackContract.lastEntry();
+    assert.isFalse(result.eq(bls.groupSignature), "Entry value on the callback contract should not be the same as next relay entry.");
+
+    await keepRandomBeacon.relayEntry(requestId, bls.groupSignature, bls.groupPubKey, bls.previousEntry, bls.seed);
+
+    result = await callbackContract.lastEntry();
     assert.isTrue(result.eq(bls.groupSignature), "Value updated by the callback should be the same as relay entry.");
   });
 
--- contracts/solidity/test/TestKeepRandomBeaconViaProxy.js
+++ contracts/solidity/test/TestKeepRandomBeaconViaProxy.js
@@ -53,7 +53,7 @@ contract('TestKeepRandomBeaconViaProxy', function(accounts) {
 
     await web3.eth.sendTransaction({
       from: account_two, value: 100, gas: 200000, to: proxy.address,
-      data: encodeCall('requestRelayEntry', ['uint256', 'address', 'string'], [0, "0x0000000000000000000000000000000000000000", ""])
+      data: encodeCall('requestRelayEntry', ['uint256'], [0])
     });
 
     assert.equal((await implViaProxy.getPastEvents())[0].event, 'RelayEntryRequested', "RelayEntryRequested event should occur on the proxy contract.");
@@ -70,7 +70,7 @@ contract('TestKeepRandomBeaconViaProxy', function(accounts) {
     let amount = web3.utils.toWei('1', 'ether');
     await web3.eth.sendTransaction({
       from: account_two, value: amount, gas: 200000, to: proxy.address,
-      data: encodeCall('requestRelayEntry', ['uint256', 'address', 'string'], [0, "0x0000000000000000000000000000000000000000", ""])
+      data: encodeCall('requestRelayEntry', ['uint256'], [0])
     });
 
     // should fail to withdraw if not owner
