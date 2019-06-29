76e82cc734bbc54fa1392f1ae81a134ac898bb40
Nik G <subzeromedia@gmail.com>
2019-06-29 10:44:56 -0500

    Fix broken genesis entry script

--- contracts/solidity/scripts/genesis.js
+++ contracts/solidity/scripts/genesis.js
@@ -1,5 +1,4 @@
-const KeepRandomBeaconService = artifacts.require('KeepRandomBeaconService.sol')
-const KeepRandomBeaconServiceImplV1 = artifacts.require("KeepRandomBeaconServiceImplV1.sol")
+const KeepRandomBeaconOperator = artifacts.require('KeepRandomBeaconOperator.sol')
 
 // The data below should match genesis relay request data defined on contract
 // initialization i.e. in 2_deploy_contracts.js. Successful genesis entry will
@@ -19,8 +18,7 @@ const groupSignature = web3.utils.toBN('1092010247678959141494937778210470713041
 
 module.exports = async function () {
 
-  const keepRandomBeaconService = await KeepRandomBeaconService.deployed()
-  let contract = await KeepRandomBeaconServiceImplV1.at(keepRandomBeaconService.address)
+  const contract = await KeepRandomBeaconOperator.deployed()
   try {
     await contract.relayEntry(1, groupSignature, groupPubKey, previousEntry, seed)
     console.log('Genesis entry successfully submitted.')
