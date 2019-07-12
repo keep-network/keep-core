d73fdbae1f44742b35d6ceb7e039826a461020b6
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2019-07-12 15:20:15 -0400

    Move LOG_LEVEL env variable into main
    The logging setup functions are now agnostic to where the configuration
    comes from, and only the main function knows this is sourced from the
    environment. Additionally, command help now lists the LOG_LEVEL
    environment variable.
    
    Lastly, the logging functions are in a separate logging.go file.

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 
 	"fmt"
 	"path"
 	"time"
 
-	logging "github.com/ipfs/go-log"
 	"github.com/keep-network/keep-core/cmd"
 	"github.com/urfave/cli"
 )
@@ -31,7 +29,10 @@ func main() {
 		revision = "unknown"
 	}
 
-	readLoggingSetup()
+	err := setUpLogging(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	app := cli.NewApp()
 	app.Name = path.Base(os.Args[0])
@@ -71,10 +72,12 @@ func main() {
 	cli.AppHelpTemplate = fmt.Sprintf(`%s
 ENVIRONMENT VARIABLES:
    KEEP_ETHEREUM_PASSWORD    keep client password
+   LOG_LEVEL                 space-delimited set of log level directives; set to
+                             "help" for help
 
 `, cli.AppHelpTemplate)
 
-	err := app.Run(os.Args)
+	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -92,91 +95,4 @@ func printInfo(c *cli.Context) {
 		revision,
 		c.GlobalString("config"),
 	)
-}
-
-func readLoggingSetup() {
-	// Single string with space-delimited directives setting log level for each
-	// subsystem, with = separating subsystem from log level. Example:
-	//
-	// "relay=debug bootstrap=info swarm2=error"
-	//
-	// Can also be a single log level for all subsystems:
-	//
-	// "info"
-	//
-	// If blank or unset, subsystems are left in their default initial state.
-	levelDirectiveString := os.Getenv("LOG_LEVEL")
-
-	// Default to info logs for keep.
-	if len(levelDirectiveString) == 0 {
-		levelDirectiveString = "keep*=info"
-	}
-
-	levelDirectives := strings.Split(levelDirectiveString, " ")
-	for _, directive := range levelDirectives {
-		err := evaluateLevelDirective(directive)
-		if err != nil {
-			log.Fatalf(
-				"Failed to parse log level directive [%s]: [%v]\n"+
-					"Directives can be any of:\n"+
-					" - a global log level, e.g. 'debug'\n"+
-					" - a subsystem=level pair, e.g. 'keep-relay=info'\n"+
-					" - a subsystem*=level prefix pair, e.g. 'keep*=warn'\n",
-				directive,
-				err,
-			)
-		}
-	}
-}
-
-// Takes a levelDirective that can have one of three formats:
-//
-//     <log-level> |
-// 	   <subsystem>=<log-level> |
-// 	   <subsystem-prefix>*=<log-level>
-//
-// In the first form, the given log-level is set on all subsystems.
-//
-// In the second form, the given log-level is set on the given subsystem.
-//
-// In the third form, the given log-level is set on any subsystem that starts
-// with the given subsystem-prefix.
-//
-// Supported log levels are as per the ipfs/go-logging library.
-func evaluateLevelDirective(levelDirective string) error {
-	splitLevel := strings.Split(levelDirective, "=")
-
-	switch len(splitLevel) {
-	case 1:
-		level := splitLevel[0]
-
-		err := logging.SetLogLevel("*", level)
-		if err != nil {
-			return err
-		}
-
-	case 2:
-		levelSubsystem := splitLevel[0]
-		level := splitLevel[1]
-
-		if strings.HasSuffix(levelSubsystem, "*") {
-			subsystemPrefix := strings.TrimSuffix(levelSubsystem, "*")
-			// Wildcard suffix, check for matching subsystems.
-			for _, subsystem := range logging.GetSubsystems() {
-				if strings.HasPrefix(subsystem, subsystemPrefix) {
-					err := logging.SetLogLevel(subsystem, level)
-					if err != nil {
-						return err
-					}
-				}
-			}
-		} else {
-			return logging.SetLogLevel(levelSubsystem, level)
-		}
-
-	default:
-		return fmt.Errorf("more than two =-delimited components in directive")
-	}
-
-	return nil
 }
