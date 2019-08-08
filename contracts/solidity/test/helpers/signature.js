ac1eb5ad20747ddd04d839649c20704a4ea72d7f
Nik G <subzeromedia@gmail.com>
2019-08-08 12:38:00 +0100

    Return '0x' prefixed signature and remove duplicate hashing

--- contracts/solidity/test/helpers/signature.js
+++ contracts/solidity/test/helpers/signature.js
@@ -1,5 +1,5 @@
 export const sign = async (dataToSign, address) => {
-    return web3.utils.toBN(
-        await web3.eth.sign(web3.utils.soliditySha3(dataToSign), address)
-    ).add(web3.utils.toBN(27)).toString()
+    return '0x' + web3.utils.toBN(
+        await web3.eth.sign(dataToSign, address)
+    ).add(web3.utils.toBN(27)).toString('hex')
 }
