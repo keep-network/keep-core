61989ecd5ff4a360e42a0014e96ddd7f567af75a
Nik G <subzeromedia@gmail.com>
2019-08-08 13:47:16 +0100

    Fix incorrect signatures length
    Convert BN to buffer rather than string so we don't lose zero paddings.

--- contracts/solidity/test/helpers/signature.js
+++ contracts/solidity/test/helpers/signature.js
@@ -1,5 +1,5 @@
 export const sign = async (dataToSign, address) => {
-    return '0x' + web3.utils.toBN(
+    return '0x' + Buffer.from(web3.utils.toBN(
         await web3.eth.sign(dataToSign, address)
-    ).add(web3.utils.toBN(27)).toString('hex')
+    ).add(web3.utils.toBN(27)).toBuffer()).toString('hex')
 }
