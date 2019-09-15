631f39ac8afd6b27efcb148c925072ee478f941b
Nik G <subzeromedia@gmail.com>
2019-09-15 12:43:23 +0300

    Update genesis.js to include dkg fee

--- contracts/solidity/scripts/genesis.js
+++ contracts/solidity/scripts/genesis.js
@@ -3,8 +3,12 @@ const KeepRandomBeaconOperator = artifacts.require('KeepRandomBeaconOperator.sol
 module.exports = async function () {
 
   const contract = await KeepRandomBeaconOperator.deployed()
+  const dkgGas = await contract.dkgGasEstimate()
+  const gasPrice = web3.utils.toBN(20).mul(web3.utils.toBN(10**9)) // 20 Gwei
+  const dkgFee = dkgGas.mul(gasPrice)
+
   try {
-    await contract.genesis()
+    await contract.genesis({value: dkgFee})
     console.log('Genesis successfully triggered.')
   } catch(error) {
     console.error('Could not trigger genesis', error)
