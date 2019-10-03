07b8a0c21f65ab363266fc91a846287232310963
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-10-03 13:28:58 +0200

    Fixed ticket sort algorithm
    Tickets should be sorted by their value and tickets with the lowest
    value should be submitted first as they have the highest chance of being
    selected to the group.
    
    Our previous implementation of sorting algorithm was buggy - instead of
    sorting by value it was sorting by virtual staker index. Additionally,
    it was replacing just the virtual staker index in the proof instead of
    swapping tickets in slice during the stable sort.

--- pkg/beacon/relay/groupselection/tickets.go
+++ pkg/beacon/relay/groupselection/tickets.go
@@ -3,28 +3,17 @@ package groupselection
 // tickets implements sort.Interface
 type tickets []*ticket
 
-// Len is the sort.Interface requirement for Tickets
+// Len is the sort.Interface requirement for tickets
 func (ts tickets) Len() int {
 	return len(ts)
 }
 
-// Swap is the sort.Interface requirement for Tickets
+// Swap is the sort.Interface requirement for tickets
 func (ts tickets) Swap(i, j int) {
-	ts[i].proof.virtualStakerIndex, ts[j].proof.virtualStakerIndex =
-		ts[j].proof.virtualStakerIndex, ts[i].proof.virtualStakerIndex
+	ts[i], ts[j] = ts[j], ts[i]
 }
 
-// Less is the sort.Interface requirement for Tickets
+// Less is the sort.Interface requirement for tickets
 func (ts tickets) Less(i, j int) bool {
-	iVirtualStakeIndex := ts[i].proof.virtualStakerIndex
-	jVirtualStakerIndex := ts[j].proof.virtualStakerIndex
-
-	switch iVirtualStakeIndex.Cmp(jVirtualStakerIndex) {
-	case -1:
-		return true
-	case 1:
-		return false
-	}
-
-	return true
+	return ts[i].intValue().Cmp(ts[j].intValue()) < 1
 }
---
+++ pkg/beacon/relay/groupselection/tickets_test.go
@@ -0,0 +1,56 @@
+package groupselection
+
+import (
+	"encoding/binary"
+	"math/big"
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func TestSortByValue(t *testing.T) {
+	ticket1 := newTestTicket(5, 1001)
+	ticket2 := newTestTicket(4, 1002)
+	ticket3 := newTestTicket(1, 1003)
+	ticket4 := newTestTicket(3, 1004)
+	ticket5 := newTestTicket(2, 1005)
+
+	tickets := tickets{
+		ticket3,
+		ticket5,
+		ticket4,
+		ticket1,
+		ticket2,
+	}
+
+	sort.Stable(tickets)
+
+	assertTicketAtIndex(t, tickets, 0, ticket1)
+	assertTicketAtIndex(t, tickets, 1, ticket2)
+	assertTicketAtIndex(t, tickets, 2, ticket3)
+	assertTicketAtIndex(t, tickets, 3, ticket4)
+	assertTicketAtIndex(t, tickets, 4, ticket5)
+}
+
+func assertTicketAtIndex(t *testing.T, tickets tickets, index int, ticket *ticket) {
+	if !reflect.DeepEqual(ticket, tickets[index]) {
+		t.Errorf(
+			"unexpected ticket at index [%v]\nexpected: [%+v]\nactual:   [%+v]",
+			index,
+			ticket,
+			tickets[index],
+		)
+	}
+}
+
+func newTestTicket(virtualStakerIndex uint32, value uint32) *ticket {
+	var bytes [32]byte
+	binary.BigEndian.PutUint32(bytes[28:], value)
+
+	return &ticket{
+		value: bytes,
+		proof: &proof{
+			virtualStakerIndex: big.NewInt(int64(virtualStakerIndex)),
+		},
+	}
+}
