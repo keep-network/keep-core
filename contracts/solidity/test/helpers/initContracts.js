3c0d861fc389ff6bc724b42cf7354f4d40fda0e7
Nik G <subzeromedia@gmail.com>
2019-10-09 15:17:06 +0200

    Remove funding DKG for every test
    This is unnecessary and can be done per test if needed.

--- contracts/solidity/test/helpers/initContracts.js
+++ contracts/solidity/test/helpers/initContracts.js
@@ -36,9 +36,6 @@ async function initContracts(KeepToken, TokenStaking, KeepRandomBeaconService,
   let dkgGasEstimate = await operatorContract.dkgGasEstimate();
   let gasPriceWithFluctuationMargin = priceFeedEstimate.add(priceFeedEstimate.mul(web3.utils.toBN(fluctuationMargin)).div(web3.utils.toBN(100)));
 
-  // Add initial funds to the fee pool to trigger group creation without waiting for DKG fee accumulation
-  await serviceContract.fundDkgFeePool({value: dkgGasEstimate.mul(gasPriceWithFluctuationMargin)});
-
   // Genesis should include payment to cover DKG cost to create first group
   await operatorContract.genesis({value: dkgGasEstimate.mul(gasPriceWithFluctuationMargin)});
 
