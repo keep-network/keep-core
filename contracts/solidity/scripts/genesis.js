5af2381aec68f3bd3e0bdc88a0ab4cf210ef6e24
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-10-16 15:43:12 +0200

    Read price feed estimate from operator contract instead of hardcoding it

--- contracts/solidity/scripts/genesis.js
+++ contracts/solidity/scripts/genesis.js
@@ -3,10 +3,13 @@ const KeepRandomBeaconOperator = artifacts.require('KeepRandomBeaconOperator.sol
 module.exports = async function () {
 
   const contract = await KeepRandomBeaconOperator.deployed()
+  
   const dkgGas = await contract.dkgGasEstimate()
   const fluctuationMargin = await contract.fluctuationMargin()
-  const priceFeedEstimate = web3.utils.toBN(20).mul(web3.utils.toBN(10**9)) // 20 Gwei
-  const gasPriceWithFluctuationMargin = priceFeedEstimate.add(priceFeedEstimate.mul(fluctuationMargin).div(web3.utils.toBN(100)));
+  const priceFeedEstimate = await contract.priceFeedEstimate()
+  const gasPriceWithFluctuationMargin = priceFeedEstimate.add(
+    priceFeedEstimate.mul(fluctuationMargin).div(web3.utils.toBN(100))
+  );
   const dkgFee = dkgGas.mul(gasPriceWithFluctuationMargin)
 
   try {
