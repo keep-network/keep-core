e016575454c5aeaf7e4c72ddad93a212d3fe9e3b
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-10-21 13:13:56 +0200

    Wrap the entire genesis script logic in a try-catch

--- contracts/solidity/scripts/genesis.js
+++ contracts/solidity/scripts/genesis.js
@@ -2,19 +2,19 @@ const KeepRandomBeaconOperator = artifacts.require('KeepRandomBeaconOperator.sol
 
 module.exports = async function () {
 
-  const contract = await KeepRandomBeaconOperator.deployed()
-  
-  const dkgGas = await contract.dkgGasEstimate()
-  const fluctuationMargin = await contract.fluctuationMargin()
-  const priceFeedEstimate = await contract.priceFeedEstimate()
-  const gasPriceWithFluctuationMargin = priceFeedEstimate.add(
-    priceFeedEstimate.mul(fluctuationMargin).div(web3.utils.toBN(100))
-  );
-  const dkgFee = dkgGas.mul(gasPriceWithFluctuationMargin)
-
   try {
+    const contract = await KeepRandomBeaconOperator.deployed()
+  
+    const dkgGas = await contract.dkgGasEstimate()
+    const fluctuationMargin = await contract.fluctuationMargin()
+    const priceFeedEstimate = await contract.priceFeedEstimate()
+    const gasPriceWithFluctuationMargin = priceFeedEstimate.add(
+      priceFeedEstimate.mul(fluctuationMargin).div(web3.utils.toBN(100))
+    );
+    const dkgFee = dkgGas.mul(gasPriceWithFluctuationMargin)
+
     await contract.genesis({value: dkgFee})
-    console.log('Genesis successfully triggered.')
+    console.log('Genesis successfully triggered.')    
   } catch(error) {
     console.error('Could not trigger genesis', error)
   }
