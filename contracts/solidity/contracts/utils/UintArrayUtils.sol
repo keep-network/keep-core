4382bdcd4f60801d14a24cbdd553adefe91fb35b
Dmitry <dymitr.paremski@gmail.com>
2019-10-22 18:21:09 +0200

    Changing the current sorting algorithm to the Quick Sort algorithm
    - Replaced an existing sorting algorithm with the 'quick sort'
    implementation. We use this algorithm to sort tickets in ascending
    order. Gas usage for tickets selection went down from 671548 to 154304.
    Algorithm is based on the book Algorithms by R.Sedgewick and K.Wayne

--- contracts/solidity/contracts/utils/UintArrayUtils.sol
+++ contracts/solidity/contracts/utils/UintArrayUtils.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.4;
 
 library UintArrayUtils {
 
-    function removeValue(uint256[] storage self, uint256 _value) 
+    function removeValue(uint256[] storage self, uint256 _value)
         internal
         returns(uint256[] storage)
     {
@@ -21,19 +21,39 @@ library UintArrayUtils {
         return self;
     }
 
-    function sort(uint256[] memory array)
-        internal pure
-        returns (uint256[] memory)
-    {
-        for (uint i = 0; i < array.length; i++) {
-            for (uint j = i+1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    uint256 temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-        return array;
+    function sort(uint256[] memory data) internal pure returns(uint256[] memory) {
+       quickSort(data, int(0), int(data.length - 1));
+       return data;
     }
+
+    // Quicksort is a divide-and-conquer algorithm for sorting. It works by partitioning
+    // an array into two subarrays, then sorting the subarrays independently.
+    function quickSort(uint256[] memory arr, int lo, int hi) internal pure {
+        if (hi <= lo) return;
+        int j = partition(arr, lo, hi);
+        quickSort(arr, lo, j-1);
+        quickSort(arr, j+1, hi);
+    }
+
+    function partition(uint256[] memory arr, int lo, int hi) internal pure returns(int) {
+        int i = lo; // left scan indice
+        int j = hi + 1; // right scan indice
+        uint256 pivot = arr[uint256(lo)]; // partitioning item
+        while(true) {
+            // scan left
+            while (arr[uint256(++i)] < pivot) {
+                if (i == hi) break;
+            }
+            // scan right
+            while (pivot < arr[uint256(--j)]) {
+                if (j == lo) break;
+            }
+            if (i >= j) break;
+            (arr[uint256(i)], arr[uint256(j)]) = (arr[uint256(j)], arr[uint256(i)]);
+        }
+        (arr[uint256(lo)], arr[uint256(j)]) = (arr[uint256(j)], arr[uint256(lo)]);
+
+        return j;
+    }
+
 }
