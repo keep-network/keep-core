72d5ee719b16c7e3eb6e7efaa7d8b7c029e4d8a8
Sloan Thompson <sloan.thompson@thesis.co>
2019-10-23 17:10:48 -0400

    Don't generate new accounts
    Here we read in existing geth accounts instead of create new ones.  We
    need to do this because our environments currently expect these specific
    accounts to always exist.  We'll change that in a future date.
    Configuring existing accounts this way allows us to update the geth
    Docker image without loosing these accounts.

--- infrastructure/geth-node/geth-init.sh
+++ infrastructure/geth-node/geth-init.sh
@@ -2,54 +2,13 @@
 
 DATADIR_DEFAULT=/root/.geth
 
-usage()
-{
-    echo "usage: geth-init.sh <KEEP_ACCOUNTS>"
-}
-
-generate_Keep_account()
-{
-  ACCOUNT=$1
-  # create a new Ethereum account. We are prepending "0x".
-  echo "-- Generating account $ACCOUNT"
-  GETH_ETH_ACCOUNT=`/geth --datadir $DATADIR_DEFAULT account new  \
-    --password /root/passphrase | \
-    cut -d "{" -f2 | cut -d "}" -f1 | \
-    while read line; do echo "0x$line"; done`
-  echo "$GETH_ETH_ACCOUNT" >> /root/keep_accounts
-  echo "   GETH_ETH_ACCOUNT: $GETH_ETH_ACCOUNT"
-}
-
-
-# parse command line parameter
-if [ "$1" != "" ]; then
-  re='^[0-9]+$'
-  if ! [[ $1 =~ $re ]] ; then
-     echo "error: Not a number" >&2; exit 1
-  fi
-  KEEP_ACCOUNTS=$1
-  echo "Will generate $KEEP_ACCOUNTS Keep client accounts."
-else
-    echo "error: KEEP_ACCOUNTS undefined!"
-    usage
-    exit 1
-fi
-
-# create mining account. We are prepending "0x".
-echo "-- Generating mining_account"
-GETH_ETH_MINING_ACCOUNT=`/geth --datadir $DATADIR_DEFAULT account new  \
-    --password /root/passphrase | \
-    cut -d "{" -f2 | cut -d "}" -f1 | \
-    while read line; do echo "0x$line"; done`
-echo "$GETH_ETH_MINING_ACCOUNT"  >> /root/mining_account
-echo "-- GETH_ETH_MINING_ACCOUNT: $GETH_ETH_MINING_ACCOUNT"
-
-# create Keep client accounts
-for i in `seq 1 $KEEP_ACCOUNTS`;
+# feed keep ETH accounts into genesis
+for keyfile in ${DATADIR_DEFAULT}/keystore/*;
 do
-  generate_Keep_account $i
+  ACCOUNT=`cat $keyfile | jq .address | tr -d '"'`
+  echo "0x${ACCOUNT}" >> /root/keep_accounts
 done
-echo "-- Keep client accounts generated:"
+echo "-- Keep client accounts populated:"
 cat /root/keep_accounts
 
 # Generate genesis.json and issue tokens to Keep peers.
@@ -62,12 +21,17 @@ GENESIS_TEMPLATE=/root/genesis-template.json
 RESULT=`cat $GENESIS_TEMPLATE`
 # add Keep client accounts and fund them
 while read -r ACCOUNT; do
-  LINE=".alloc += {\"$ACCOUNT\": {"balance": \"1000000000000000000000\"}}"
+  LINE=".alloc += {\"${ACCOUNT}\": {"balance": \"1000000000000000000000\"}}"
   RESULT=`echo $RESULT | jq "$LINE"`
 done < /root/keep_accounts
 echo $RESULT | jq . > $GENESIS
 
 ## genesis.json generation done -------
+
+## set miner account
+echo "-- Setting miner account:"
+head -1 /root/keep_accounts > /root/mining_account
+cat /root/mining_account
 
 # dump genesis file
 echo "-- Dump genesis.json:"
@@ -76,7 +40,7 @@ echo ""
 
 # List the keystore directory
 echo "-- KEYSTORE directory"
-ls -la $DATADIR_DEFAULT/keystore
+ls -la ${DATADIR_DEFAULT}/keystore
 
 # List the .geth directory
 echo "-- .geth directory"
