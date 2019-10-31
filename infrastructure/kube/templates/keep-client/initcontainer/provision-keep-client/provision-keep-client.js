be7103a2f9fcf3d19f019e084abae69d554f6b60
Sloan Thompson <sloan.thompson@thesis.co>
2019-10-31 13:36:54 -0400

    Set LibP2P.Peers with the value of KEEP_NETWORK_BOOTSTRAP_PEERS

--- infrastructure/kube/templates/keep-client/initcontainer/provision-keep-client/provision-keep-client.js
+++ infrastructure/kube/templates/keep-client/initcontainer/provision-keep-client/provision-keep-client.js
@@ -251,6 +251,7 @@ async function createKeepClientConfig(operator) {
       parsedConfigFile.ethereum.ContractAddresses.KeepRandomBeaconService = keepRandomBeaconServiceContractAddress;
       parsedConfigFile.ethereum.ContractAddresses.TokenStaking = tokenStakingContractAddress;
       parsedConfigFile.LibP2P.Port = 3919;
+      parsedConfigFIle.LibP2P.Peers = process.env.KEEP_NETWORK_BOOTSTRAP_PEERS;
       parsedConfigFile.Storage.DataDir = process.env.KEEP_DATA_DIR;
 
       let formattedConfigFile = tomlify.toToml(parsedConfigFile, {
