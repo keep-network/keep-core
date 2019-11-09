8e810da726081192062fd86ea1bfe999d1be926c
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-11-09 11:29:13 +0100

    Pricing tests divided by files based on the tested contract

--- contracts/solidity/test/TestKeepRandomBeaconOperatorPricingRewards.js
+++ contracts/solidity/test/TestKeepRandomBeaconOperatorPricingRewards.js
@@ -3,7 +3,7 @@ import {createSnapshot, restoreSnapshot} from "./helpers/snapshot";
 import {bls} from './helpers/data';
 import mineBlocks from './helpers/mineBlocks';
 
-contract('Keep random beacon pricing', function(accounts) {
+contract('KeepRandomBeaconOperator', function(accounts) {
     let serviceContract;
     let operatorContract;
 
@@ -29,61 +29,6 @@ contract('Keep random beacon pricing', function(accounts) {
     
     afterEach(async () => {
       await restoreSnapshot()
-    });
-
-    it("should correctly evaluate entry verification fee", async () => {
-        await serviceContract.setPriceFeedEstimate(200);
-        await operatorContract.setEntryVerificationGasEstimate(12);        
-
-        let fees = await serviceContract.entryFeeBreakdown();
-        let entryVerificationFee = fees.entryVerificationFee;
-
-        let expectedEntryVerificationFee = 3600; // 200 * 12 * 150%
-        assert.equal(expectedEntryVerificationFee, entryVerificationFee);
-    });
-
-    it("should correctly evaluate DKG contribution fee", async () => {
-        await serviceContract.setPriceFeedEstimate(123);
-        await operatorContract.setDkgGasEstimate(13);
-
-        let fees = await serviceContract.entryFeeBreakdown();
-        let dkgContributionFee = fees.dkgContributionFee;
-
-        let expectedDkgContributionFee = 159; // 123 * 13 * 10%
-        assert.equal(expectedDkgContributionFee, dkgContributionFee);
-    });
-
-    it("should correctly evaluate callback fee", async function() {
-        await serviceContract.setPriceFeedEstimate(160);
-
-        let callbackGas = 1091;
-
-        let callbackFee = await serviceContract.callbackFee(callbackGas);
-        
-        let expectedCallbackFee = 261840; // 1091 * 160 * 150%
-        assert.equal(expectedCallbackFee, callbackFee);
-    });
-
-    it("should correctly evaluate entry fee estimate", async () => {
-        await serviceContract.setPriceFeedEstimate(200);
-        await operatorContract.setEntryVerificationGasEstimate(12); 
-        await operatorContract.setDkgGasEstimate(14); 
-        await operatorContract.setGroupSize(13);
-        await operatorContract.setGroupMemberBaseReward(3);
-
-        let callbackGas = 7;
-
-        let entryFeeEstimate = await serviceContract.entryFeeEstimate(
-            callbackGas
-        );
-
-        // entry verification fee = 12 * 200 * 150% = 3600
-        // dkg contribution fee = 14 * 200 * 10% = 280
-        // group profit fee = 13 * 3 = 39
-        // callback fee = 7 * 200 * 150% = 2100
-        // entry fee = 3600 + 280 + 39 + 2100 = 6019
-        let expectedEntryFeeEstimate = 6019;
-        assert.equal(expectedEntryFeeEstimate, entryFeeEstimate)
     });
 
     it("should correctly evaluate delay factor right after the request", async () => {
