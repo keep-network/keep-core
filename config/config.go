9c9f83c7cdac9ec31e6cee93430967f26b3748fa
Antonio Salazar Cardozo <antonio@keep.network>
2019-11-13 08:33:24 -0500

    Add config.ReadEthereumConfig helper
    This exported helper enables reading just the Ethereum portion of a
    config directly, without having to worry about the structure of the
    surrounding config. This will be helpful when keep-common-generated code
    needs to read Ethereum configuration and act on it, without being
    interested in which application is using that generated code and what
    config structure that particular application has chosen to use. All that
    matters in this case is that the Ethereum configuration's structure is
    as expected.

--- config/config.go
+++ config/config.go
@@ -74,6 +74,23 @@ func ReadConfig(filePath string) (*Config, error) {
 	return config, nil
 }
 
+// ReadEthereumConfig reads in the configuration file at `filePath` and returns
+// its contained Ethereum config, or an error if something fails while reading
+// the file.
+//
+// This is the same as invoking ReadConfig and reading the Ethereum property
+// from the returned config, but is available for external functions that expect
+// to interact solely with Ethereum and are therefore independent of the rest of
+// the config structure.
+func ReadEthereumConfig(filePath string) (ethereum.Config, error) {
+	config, err := ReadConfig(filePath)
+	if err != nil {
+		return ethereum.Config{}, err
+	}
+
+	return config.Ethereum, nil
+}
+
 // ReadPassword prompts a user to enter a password.   The read password uses
 // the system password reading call that helps to prevent key loggers from
 // capturing the password.
