d9d7c1b36d6016045a0eb7ca8cc94aef1e2a7921
Sloan Thompson <sloan.thompson@thesis.co>
2019-11-13 08:51:01 -0500

    Removing deprecated scripts

--- infrastructure/scripts/keep-client-create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create -f ../kube/keep-dev/keep-client/keep-client-bootstrap-peer-0.yaml
-kubectl create -f ../kube/keep-dev/keep-client/keep-client-peer-0.yaml
-kubectl create -f ../kube/keep-dev/keep-client/keep-client-peer-1.yaml
-kubectl create -f ../kube/keep-dev/keep-client/keep-client-peer-2.yaml
-kubectl create -f ../kube/keep-dev/keep-client/keep-client-peer-3.yaml
