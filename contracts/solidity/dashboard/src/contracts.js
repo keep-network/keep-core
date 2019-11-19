2677109e8029318357dc9e0571a3b2183aeb4381
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-19 14:07:41 +0100

    Created common funcion to valid code
    Chnage imports.

--- contracts/solidity/dashboard/src/contracts.js
+++ contracts/solidity/dashboard/src/contracts.js
@@ -1,31 +1,33 @@
-import * as KeepToken from "./contracts/KeepToken.json"
-import * as TokenStaking from "./contracts/TokenStaking.json"
-import * as TokenGrant from "./contracts/TokenGrant.json"
+import KeepToken from "./contracts/KeepToken.json"
+import TokenStaking from "./contracts/TokenStaking.json"
+import TokenGrant from "./contracts/TokenGrant.json"
 
 export async function getKeepToken(web3) {
-  const address = getContractAddress(KeepToken.default);
-
+  const address = getContractAddress(KeepToken);
   const code = await web3.eth.getCode(address);
   
-  if (!code || code === '0x0' || code === '0x') throw Error('No contract at address');
+  checkCodeIsValid(code);
   return new web3.eth.Contract(KeepToken.abi, address)
 }
 
 export async function getTokenStaking(web3) {
-  const address = getContractAddress(TokenStaking.default);
-  
+  const address = getContractAddress(TokenStaking);
   const code = await web3.eth.getCode(address);
 
-  if (!code || code === '0x0' || code === '0x') throw Error('No contract at address');
+  checkCodeIsValid(code);
   return new web3.eth.Contract(TokenStaking.abi, address)
 }
 
 export async function getTokenGrant(web3) {
-  const address = getContractAddress(TokenGrant.default);
-  
+  const address = getContractAddress(TokenGrant);
   const code = await web3.eth.getCode(address);
-  if (!code || code === '0x0' || code === '0x') throw Error('No contract at address');
+
+  checkCodeIsValid(code);
   return new web3.eth.Contract(TokenGrant.abi, address)
+}
+
+const checkCodeIsValid = (code) => {
+  if (!code || code === '0x0' || code === '0x') throw Error('No contract at address');
 }
 
 const getContractAddress = ({ networks }) => networks[Object.keys(networks)[0]].address;
