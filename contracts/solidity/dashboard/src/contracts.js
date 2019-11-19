87186a5027f0a7141b1aa9b2c3fb294b4192bd07
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-19 13:22:06 +0100

    Created ne funciton to get contract address
    Refactored function to get contract just with web3 instance insted of
    pass address param to function.

--- contracts/solidity/dashboard/src/contracts.js
+++ contracts/solidity/dashboard/src/contracts.js
@@ -1,24 +1,32 @@
-import KeepToken from "contracts/KeepToken.json"
-import TokenStaking from "contracts/TokenStaking.json"
-import TokenGrant from "contracts/TokenGrant.json"
+import * as KeepToken from "./contracts/KeepToken.json"
+import * as TokenStaking from "./contracts/TokenStaking.json"
+import * as TokenGrant from "./contracts/TokenGrant.json"
 
-export async function getKeepToken(web3, address) {
+export async function getKeepToken(web3) {
+  const address = getContractAddress(KeepToken.default);
 
   const code = await web3.eth.getCode(address);
+  
   if (!code || code === '0x0' || code === '0x') throw Error('No contract at address');
   return new web3.eth.Contract(KeepToken.abi, address)
 }
 
-export async function getTokenStaking(web3, address) {
-
+export async function getTokenStaking(web3) {
+  const address = getContractAddress(TokenStaking.default);
+  
   const code = await web3.eth.getCode(address);
+
   if (!code || code === '0x0' || code === '0x') throw Error('No contract at address');
   return new web3.eth.Contract(TokenStaking.abi, address)
 }
 
-export async function getTokenGrant(web3, address) {
-
+export async function getTokenGrant(web3) {
+  const address = getContractAddress(TokenGrant.default);
+  
   const code = await web3.eth.getCode(address);
   if (!code || code === '0x0' || code === '0x') throw Error('No contract at address');
   return new web3.eth.Contract(TokenGrant.abi, address)
 }
+
+const getContractAddress = ({ networks }) => networks[Object.keys(networks)[0]].address;
+
