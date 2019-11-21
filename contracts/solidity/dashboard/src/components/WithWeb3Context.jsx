9e15b5410beb2142ea2c26bcadbff58bd22a6f77
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-21 12:22:06 +0100

    Set default values of web3 context

--- contracts/solidity/dashboard/src/components/WithWeb3Context.jsx
+++ contracts/solidity/dashboard/src/components/WithWeb3Context.jsx
@@ -1,12 +1,24 @@
 
 import React from 'react'
 
-export const Web3Context = React.createContext()
+export const Web3Context = React.createContext({ 
+  yourAddress: '',
+  networkType: '',
+  token: { options: { address: '' } },
+  stakingContract: { options: { address: '' } },
+  grantContract: { options: { address: '' } },
+  utils: {},
+  eth: {},
+  error: '',
+  dataIsReady: false,
+});
 
 const WithWeb3Context = (Component) => {
   return (props) => (
     <Web3Context.Consumer>
-      {web3 =>  <Component {...props} web3={web3} />}
+      {web3 =>   {
+        return <Component {...props} web3={web3} />
+      }}
     </Web3Context.Consumer>
   )
 }
