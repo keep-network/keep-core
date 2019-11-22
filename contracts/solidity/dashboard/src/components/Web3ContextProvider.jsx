53969c9dd45a7c968a08e761168f806675df6ad5
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-22 15:35:29 +0100

    Refactoring
    Simplified `getContracts` function by using Promise.all.
    Removed dataIsReady from state.

--- contracts/solidity/dashboard/src/components/Web3ContextProvider.jsx
+++ contracts/solidity/dashboard/src/components/Web3ContextProvider.jsx
@@ -17,70 +17,50 @@ export default class Web3ContextProvider extends React.Component {
             utils: {},
             eth: {},
             error: '',
-            dataIsReady: false,
         }
     }
 
-    async componentDidMount() {
-        try {
-            await this.initialize();
-        } catch(e) {
-            console.log('onit', e)
-        }
+    componentDidMount() {
+       this.initialize();
     }
 
     initialize = async () => {
+
+        const web3 = await getWeb3();
+        if(!web3) {
+            this.setState({ error: 'No network detected. Do you have Metamask installed?' });
+            return;
+        }
+       
+        window.ethereum.on('accountsChanged', this.accountHasBeenChanged)
+
         try {
-            const web3 = await getWeb3();
-            if (!web3) {
-                this.setState({
-                    error: "No network detected. Do you have MetaMask installed?",
-                })
-                return
-            }
-
-            window.ethereum.on('accountsChanged', this.accountHasBeenChanged)
-            const contracts = await this.getContracts(web3);
-            if (!contracts) {
-                this.setState({
-                    error: "Failed to load contracts. Please check if Metamask is enabled and connected to the correct network.",
-                })
-                return
-            }
-
+            const [token, grantContract, stakingContract] = await this.getContracts(web3);
             this.setState({
-                ...contracts,
+                token,
+                grantContract,
+                stakingContract,
+                defaultContract: stakingContract,
                 yourAddress: (await web3.eth.getAccounts())[0],
                 networkType: await web3.eth.net.getNetworkType(),
-                defaultContract: contracts.stakingContract,
                 utils: web3.utils,
                 eth: web3.eth,
-                dataIsReady: true,
             })
-        } catch(e) {
-            console.log('errrrror', e);
+        } catch(error) {
+            this.setState({
+                error: "Failed to load contracts. Please check if Metamask is enabled and connected to the correct network.",
+            })
+            return;
         }
     }
 
-    getContracts = async (web3) => {
-
-        try {
-          const token = await getKeepToken(web3)
-          const stakingContract = await getTokenStaking(web3)
-          const grantContract = await getTokenGrant(web3)
-          return {
-            token: token,
-            stakingContract: stakingContract,
-            grantContract: grantContract
-          }
-        } catch (e) {
-            console.log('error get contracts', e);
-          return null
-        }
-    }
+    getContracts = async (web3) => await Promise.all([
+        getKeepToken(web3),
+        getTokenGrant(web3),
+        getTokenStaking(web3)
+    ])
 
     accountHasBeenChanged = ([yourAddress]) => {
-        console.log('new address', yourAddress);
         this.setState({ yourAddress })
     }
 
@@ -89,11 +69,9 @@ export default class Web3ContextProvider extends React.Component {
     };
 
       render() {
-          const { dataIsReady } = this.state;
-          console.log('state', this.state);
           return (
               <Web3Context.Provider value={{ ...this.state, changeDefaultContract: this.changeDefaultContract }}>
-                  {!dataIsReady ? 'Loading.....': this.props.children}
+                  {this.props.children}
               </Web3Context.Provider>    
           );
       }
