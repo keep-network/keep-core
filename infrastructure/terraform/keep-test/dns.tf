41f0839c462310350340b5c888dae3c8363c6464
Sloan Thompson <sloan.thompson@thesis.co>
2019-11-22 10:26:35 -0500

    Create test subdomain for tbtc.network
    We're exposing tbtc to the internet from the keep-test environment.  We
    need a subdomain to represent those hosts to the wild.  Here we provide
    the .test subdomain for tbtc.network.

--- infrastructure/terraform/keep-test/dns.tf
+++ infrastructure/terraform/keep-test/dns.tf
@@ -5,3 +5,11 @@ resource "google_dns_managed_zone" "test_keep_network" {
   dns_name    = "test.keep.network."
   labels      = "${local.labels}"
 }
+
+resource "google_dns_managed_zone" "test_tbtc_network" {
+  project     = "${module.project.project_id}"
+  description = "tbtc-test subdomain for hosts who will be accessed from the outside world."
+  name        = "test-tbtc-network"
+  dns_name    = "test.tbtc.network."
+  labels      = "${local.labels}"
+}
