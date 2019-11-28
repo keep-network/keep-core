358fe596619b3c9eabdfb44e760f92756bdc8811
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-28 19:47:06 +0100

    Added new routes and redirections

--- contracts/solidity/dashboard/src/components/Routing.jsx
+++ contracts/solidity/dashboard/src/components/Routing.jsx
@@ -6,10 +6,33 @@ import StakeTab from './StakeTab';
 import { RoutingTabs } from './RoutingTabs'
 import TokenGrantsTab from './TokenGrantsTab';
 import CreateTokenGrantsTab from './CreateTokenGrantsTab';
+import { withContractsDataContext } from './ContractsDataContextProvider';
+import SigningForm from './SigningForm'
+import Siginig from './Signing';
 
-export default class Routing extends React.Component { 
+class Routing extends React.Component { 
+
+    renderContent = () => {
+        const { isOperator, isTokenHolder, contractsDataIsFetching } = this.props;
+
+        if(contractsDataIsFetching)
+            return <><div>Loading...</div></>
+        else if(isOperator)
+            return <Route exact path='/overview' component={OverviewTab} />
+        else if(!isOperator && !isTokenHolder)
+            return <Redirect to='/sign-in' />
+        return (
+            <>
+                <Route exact path='/overview' component={OverviewTab} />
+                <Route exact path='/stake' component={StakeTab} />
+                <Route exact path='/token-grants' component={TokenGrantsTab} />
+                <Route exact path='/create-token-grants' component={CreateTokenGrantsTab} />
+            </>
+        )
+    }
 
     render() {
+        console.log('props', this.props);
         return (
            <Router>
                <Grid>
@@ -17,11 +40,9 @@ export default class Routing extends React.Component {
                         <Col xs={12}>
                         <RoutingTabs>
                             <Switch>
-                                <Route exact path='/overview' component={OverviewTab} />
-                                <Route exact path='/stake' component={StakeTab} />
-                                <Route exact path='/token-grants' component={TokenGrantsTab} />
-                                <Route exact path='/create-token-grants' component={CreateTokenGrantsTab} />
-                                <Redirect from='/' to='/overview'/>
+                                <Route exact path='/sign-in' component={Siginig} />
+                                {this.renderContent()}
+                                <Redirect to='/overview'/>
                             </Switch>
                         </RoutingTabs>
                         </Col>
@@ -31,3 +52,5 @@ export default class Routing extends React.Component {
        );
     }
 }
+
+export default withContractsDataContext(Routing)
