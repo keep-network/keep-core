821f7ef5ff681ee6c212b68f75043f0821c27d3d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-28 17:47:01 +0100

    Moved token grant's part to separated component

--- contracts/solidity/dashboard/src/components/TokenGrantsTab.jsx
+++ contracts/solidity/dashboard/src/components/TokenGrantsTab.jsx
@@ -1,12 +1,72 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import WithWeb3Context from './WithWeb3Context';
+import VestingChart from './VestingChart'
+import VestingDetails from './VestingDetails'
+import TokenGrants from './TokenGrants'
+import StakingDelegateTokenGrantForm from './StakingDelegateTokenGrantForm'
+import FetchTokenGrantsCore from './FetchTokenGrantsCore';
 
-export default class TokenGrantsTab extends React.Component {
+class TokenGrantsTab extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedGrantIndex: 0,
+        }
+    }
+
+    selectTokenGrant = (selectedGrantIndex) => this.setState({ selectedGrantIndex })
+    
+    selectedGrant = () => this.props.data ? this.props.data[this.state.selectedGrantIndex] : {}
 
     render() {
+        const { data } = this.props
+        const grantBalance = '00'
         return (
-            <div>
-                token grants tab here
-            </div>
+                <>  
+                    <h3>Tokens granted to you</h3>
+                    <Row>
+                        <Col xs={12} md={6}>
+                            <VestingDetails
+                                details={this.selectedGrant()}
+                            />
+                        </Col>
+                        <Col xs={12} md={6}>
+                            <VestingChart
+                                details={this.selectedGrant()}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={12}>
+                            <TokenGrants
+                                data={data}
+                                selectTokenGrant={this.selectTokenGrant}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={12}>
+                            <h3>Stake Delegation of Token Grants</h3>
+                            <p>
+                                Keep network does not require token owners to perform the day-to-day operations of staking 
+                                with the private keys holding the tokens. This is achieved by stake delegation, where different
+                                addresses hold different responsibilities and cold storage is supported to the highest extent practicable.
+                            </p>
+                            
+                            <StakingDelegateTokenGrantForm
+                                tokenBalance={grantBalance}
+                            />
+                        </Col>
+                    </Row>
+                </>
         )
     }
 }
+
+export default (props) => (
+    <FetchTokenGrantsCore userGrants >
+        <TokenGrantsTab />
+    </FetchTokenGrantsCore>
+)
