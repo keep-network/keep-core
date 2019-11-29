74d415c7fa93637734acf0c347d4589c0de99a00
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-29 12:14:36 +0100

    Removed unused properties from state
    Connected component with contracts data provider.
    Removed unused imports.
    Fixed indents in file.

--- contracts/solidity/dashboard/src/components/OverviewTab.jsx
+++ contracts/solidity/dashboard/src/components/OverviewTab.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { Pie } from 'react-chartjs-2'
 import { Table, Col, Row } from 'react-bootstrap'
 import moment from 'moment'
@@ -8,40 +8,28 @@ import StakingTable from './StakingTable'
 import WithdrawalsTable from './WithdrawalsTable'
 import TableRow from './TableRow'
 import { colors } from '../colors'
-import WithWeb3Context from './WithWeb3Context';
+import WithWeb3Context from './WithWeb3Context'
+import { withContractsDataContext } from './ContractsDataContextProvider'
 
 class OverviewTab extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            tokenBalance: '',
           operators: [],
-          isTokenHolder: false,
-          isOperator: false,
-          isOperatorOfStakedTokenGrant: false,
-          stakedGrant: '',
-          stakeOwner: '',
           stakeBalance: '',
-          grantBalance: '',
-          grantStakeBalance: '',
+          withdrawals: [],
+          withdrawalsTotal: [],
           chartOptions: {
             legend: {
                 position: 'right'
               }
           },
-          operatorChartData: {},
-          stakeOwnerChartData: {},
-          withdrawals: [],
-          withdrawalsTotal: [],
-          grantedToYou: [],
-          grantedByYou: [],
-          selectedGrantIndex: 1
         }
     }
 
-    async componentDidMount() {
-        await this.getData();
+    componentDidMount() {
+        this.getData();
     }
 
     componentDidUpdate(prevProps) {
@@ -50,35 +38,10 @@ class OverviewTab extends React.Component {
     }
 
     async getData() {
-        const { web3: { token, stakingContract, grantContract, yourAddress, changeDefaultContract, utils } } = this.props;
+        const { web3: { token, stakingContract, grantContract, yourAddress, utils } } = this.props
+        const { isOperator } = this.props
         if(!token.options.address || !stakingContract.options.address || !grantContract.options.address)
           return
-        // to display and to check is token holder  
-        const tokenBalance = new utils.BN(await token.methods.balanceOf(yourAddress).call());
-        //
-
-        // check is operator
-        const stakeOwner = await stakingContract.methods.ownerOf(yourAddress).call();
-        //
-
-        // To display only
-        const grantBalance = await grantContract.methods.balanceOf(yourAddress).call()
-        const grantStakeBalance = displayAmount(await grantContract.methods.stakeBalanceOf(yourAddress).call(), 18, 3)
-        //
-
-        let isTokenHolder = tokenBalance.gt(new utils.BN(0));
-        let isOperator = stakeOwner !== "0x0000000000000000000000000000000000000000" && utils.toChecksumAddress(yourAddress) !== utils.toChecksumAddress(stakeOwner)
-    
-        // Check if your account is an operator for a staked Token Grant.
-        let stakedGrant
-        let isOperatorOfStakedTokenGrant
-        let stakedGrantByOperator = await grantContract.methods.grantStakes(yourAddress).call()
-    
-        if (stakedGrantByOperator.stakingContract === stakingContract.options.address) {
-          isOperatorOfStakedTokenGrant = true
-          stakedGrant = await grantContract.methods.grants(stakedGrantByOperator.grantId.toString()).call()
-          changeDefaultContract(grantContract);
-        }
     
         // Calculate delegated stake balances
         let stakeBalance = new utils.BN(await stakingContract.methods.balanceOf(yourAddress).call());
@@ -98,22 +61,14 @@ class OverviewTab extends React.Component {
         }
     
         // Unstake withdrawals
-        let withdrawalsByOperator = []
-    
-        if (isOperator) {
-          withdrawalsByOperator.push(yourAddress)
-        } else {
-          withdrawalsByOperator = operatorsAddresses;
-        }
+        let withdrawalsByOperator = isOperator ? [yourAddress] : operatorsAddresses
     
         const withdrawalDelay = await stakingContract.methods.stakeWithdrawalDelay().call();
         let withdrawals = []
         let withdrawalsTotal = new utils.BN(0);
     
         for(let i=0; i < withdrawalsByOperator.length; i++) {
-            console.log('withdrawal by operator', withdrawalsByOperator[i])
           const withdrawal = await stakingContract.methods.getWithdrawal(withdrawalsByOperator[i]).call()
-          console.log('with', withdrawal)
           if (withdrawal.amount > 0) {
             withdrawalsTotal = withdrawalsTotal.add(new utils.BN(withdrawal.amount))
   
@@ -134,24 +89,17 @@ class OverviewTab extends React.Component {
           }
         }
     
-        withdrawalsTotal = displayAmount(withdrawalsTotal, 18, 3);
-    
         this.setState({
-          tokenBalance,
           operators,
-          isTokenHolder,
-          isOperator,
-          isOperatorOfStakedTokenGrant,
           stakeBalance,
-          grantBalance,
-          grantStakeBalance,
           withdrawals,
-          withdrawalsTotal,
+          withdrawalsTotal: displayAmount(withdrawalsTotal, 18, 3),
         })
     }
 
     getChartData = () => {
-        const { isOperator, tokenBalance, stakeBalance, withdrawalsTotal, grantBalance } = this.state;
+        const { stakeBalance, withdrawalsTotal } = this.state;
+        const { isOperator, tokenBalance, grantBalance } = this.props;
         return isOperator ? 
         {
             labels: [
@@ -186,25 +134,23 @@ class OverviewTab extends React.Component {
     }
 
     render() {
-        const { tokenBalance, operators, stakeBalance, grantBalance, grantStakeBalance,isOperator, isOperatorOfStakedTokenGrant, chartOptions, withdrawals, withdrawalsTotal } = this.state
-      
-        const { web3 } = this.props;
-        const { error } = web3;
+        const { operators, chartOptions, withdrawals, withdrawalsTotal, stakeBalance } = this.state
+        const { web3, isOperator, isOperatorOfStakedTokenGrant, tokenBalance, grantBalance, grantStakeBalance } = this.props;
         return (
             <>
-                {isOperator ?
+              {isOperator ?
                 <Row className="overview">
-                    <Col xs={12} md={6}>
+                  <Col xs={12} md={6}>
                     <Pie dataKey="name" data={this.getChartData()} options={ chartOptions } />
-                    </Col>
-                    <Col xs={12} md={6}>
+                  </Col>
+                  <Col xs={12} md={6}>
                     <Table className="small table-sm" striped bordered condensed>
-                        <tbody>
+                      <tbody>
                         <TableRow title="Your wallet address">
-                            { web3.yourAddress }
+                          { web3.yourAddress }
                         </TableRow>
                         <TableRow title="Tokens">
-                            { displayAmount(tokenBalance, 18, 3) }
+                          { displayAmount(tokenBalance, 18, 3) }
                         </TableRow>
                         <TableRow title="Staked">
                             { displayAmount(stakeBalance, 18, 3) }
@@ -212,64 +158,63 @@ class OverviewTab extends React.Component {
                         <TableRow title="Pending unstake">
                             { withdrawalsTotal }
                         </TableRow>
-                        </tbody>
+                      </tbody>
                     </Table>
-
-                    {!isOperatorOfStakedTokenGrant ?
-                        <StakingForm btnText="Unstake" action="unstake" />:
+                    {!isOperatorOfStakedTokenGrant 
+                      ? <StakingForm btnText="Unstake" action="unstake" /> 
+                      :
                         <div>
-                        <StakingForm btnText="Unstake" action="unstake" />
-                        <small>You can only unstake full amount. Partial unstake amounts are not yet supported.</small>
+                          <StakingForm btnText="Unstake" action="unstake" />
+                          <small>You can only unstake full amount. Partial unstake amounts are not yet supported.</small>
                         </div>
                     }
-
-                    </Col>
+                  </Col>
                 </Row>:
                 <Row className="overview">
-                    <Col xs={12} md={6}>
+                  <Col xs={12} md={6}>
                     <Pie dataKey="name" data={this.getChartData()} options={ chartOptions } />
-                    </Col>
-                    <Col xs={12} md={6}>
+                  </Col>
+                  <Col xs={12} md={6}>
                     <Table className="small table-sm" striped bordered condensed>
-                        <tbody>
+                      <tbody>
                         <TableRow title="Your wallet address">
-                            { web3.yourAddress }
+                          { web3.yourAddress }
                         </TableRow>
                         <TableRow title="Tokens">
-                            { displayAmount(tokenBalance, 18, 3) }
+                          { displayAmount(tokenBalance, 18, 3) }
                         </TableRow>
                         <TableRow title="Staked">
-                            { displayAmount(stakeBalance, 18, 3) }
+                          { displayAmount(stakeBalance, 18, 3) }
                         </TableRow>
                         <TableRow title="Pending unstake">
-                            { withdrawalsTotal }
+                          { withdrawalsTotal }
                         </TableRow>
                         <TableRow title="Token Grants">
-                            { displayAmount(grantBalance, 18, 3) }
+                          { displayAmount(grantBalance, 18, 3) }
                         </TableRow>
                         <TableRow title="Staked Token Grants">
-                            { grantStakeBalance }
+                          { grantStakeBalance }
                         </TableRow>
-                        </tbody>
+                      </tbody>
                     </Table>
-                    </Col>
+                  </Col>
                 </Row>
-                }
-                <Row>
-                    {!isOperator ?
-                    <Col sm={12}>
+              }
+              <Row>
+                {!isOperator &&
+                  <Col sm={12}>
                     <h4>Delegated Stake</h4>
                     <StakingTable data={operators}/>
-                    </Col>:null
-                    }
-                    <Col sm={12}>
-                    <h4>Pending unstake</h4>
-                    <WithdrawalsTable data={withdrawals}/>
-                    </Col>
-                </Row>
+                  </Col>
+                }
+                <Col sm={12}>
+                  <h4>Pending unstake</h4>
+                  <WithdrawalsTable data={withdrawals}/>
+                </Col>
+              </Row>
             </>
         );
     }
 };
 
-export default WithWeb3Context(OverviewTab)
+export default WithWeb3Context(withContractsDataContext(OverviewTab))
