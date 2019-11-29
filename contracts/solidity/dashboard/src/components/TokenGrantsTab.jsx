b3fe04107f3dbe316c68aa737856f562a02d9df0
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-11-29 12:24:54 +0100

    Connected component with contracts data context
    Fixed indents in file.

--- contracts/solidity/dashboard/src/components/TokenGrantsTab.jsx
+++ contracts/solidity/dashboard/src/components/TokenGrantsTab.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import WithWeb3Context from './WithWeb3Context';
 import VestingChart from './VestingChart'
 import VestingDetails from './VestingDetails'
 import TokenGrants from './TokenGrants'
 import StakingDelegateTokenGrantForm from './StakingDelegateTokenGrantForm'
-import FetchTokenGrantsCore from './FetchTokenGrantsCore';
+import FetchTokenGrantsCore from './FetchTokenGrantsCore'
+import { withContractsDataContext } from './ContractsDataContextProvider'
 
 class TokenGrantsTab extends React.Component {
 
@@ -21,52 +21,52 @@ class TokenGrantsTab extends React.Component {
     selectedGrant = () => this.props.data ? this.props.data[this.state.selectedGrantIndex] : {}
 
     render() {
-        const { data } = this.props
-        const grantBalance = '00'
+        const { data, grantBalance } = this.props
         return (
-                <>  
-                    <h3>Tokens granted to you</h3>
-                    <Row>
-                        <Col xs={12} md={6}>
-                            <VestingDetails
-                                details={this.selectedGrant()}
-                            />
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <VestingChart
-                                details={this.selectedGrant()}
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12} md={12}>
-                            <TokenGrants
-                                data={data}
-                                selectTokenGrant={this.selectTokenGrant}
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12} md={12}>
-                            <h3>Stake Delegation of Token Grants</h3>
-                            <p>
-                                Keep network does not require token owners to perform the day-to-day operations of staking 
-                                with the private keys holding the tokens. This is achieved by stake delegation, where different
-                                addresses hold different responsibilities and cold storage is supported to the highest extent practicable.
-                            </p>
-                            
-                            <StakingDelegateTokenGrantForm
-                                tokenBalance={grantBalance}
-                            />
-                        </Col>
-                    </Row>
-                </>
+            <>  
+                <h3>Tokens granted to you</h3>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <VestingDetails
+                            details={this.selectedGrant()}
+                        />
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <VestingChart
+                            details={this.selectedGrant()}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={12}>
+                        <TokenGrants
+                            data={data}
+                            selectTokenGrant={this.selectTokenGrant}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={12}>
+                        <h3>Stake Delegation of Token Grants</h3>
+                        <p>
+                            Keep network does not require token owners to perform the day-to-day operations of staking 
+                            with the private keys holding the tokens. This is achieved by stake delegation, where different
+                            addresses hold different responsibilities and cold storage is supported to the highest extent practicable.
+                        </p>
+                        
+                        <StakingDelegateTokenGrantForm
+                            tokenBalance={grantBalance}
+                        />
+                    </Col>
+                </Row>
+            </>
         )
     }
 }
+const TokenGrantsTabWithContractDataContext = withContractsDataContext(TokenGrantsTab)
 
 export default (props) => (
     <FetchTokenGrantsCore userGrants >
-        <TokenGrantsTab />
+        <TokenGrantsTabWithContractDataContext />
     </FetchTokenGrantsCore>
 )
