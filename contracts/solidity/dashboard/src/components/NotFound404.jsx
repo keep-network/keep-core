99d3d8c4f904c5b6047347c91b5e6bea4b2bd912
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-12-10 09:49:16 +0100

    Changed to const instead of let

--- contracts/solidity/dashboard/src/components/NotFound404.jsx
+++ contracts/solidity/dashboard/src/components/NotFound404.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 export const NotFound404 = () => {
-    let location = useLocation();
+    const location = useLocation()
   
     return (
       <div>
