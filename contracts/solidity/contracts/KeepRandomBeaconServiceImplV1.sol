e63b5fb320f7a013578c07707495c42c714e084a
Nik G <nik.grinkevich@keep.network>
2019-12-11 11:35:34 +0000

    Fix dkgFeePool bug
    Make sure to reduce dkg pool amount only on successful createGroup() call.

--- contracts/solidity/contracts/KeepRandomBeaconServiceImplV1.sol
+++ contracts/solidity/contracts/KeepRandomBeaconServiceImplV1.sol
@@ -342,10 +342,12 @@ contract KeepRandomBeaconServiceImplV1 is Ownable, DelayedWithdrawal {
             gasPriceWithFluctuationMargin(_priceFeedEstimate)
         );
         if (_dkgFeePool >= dkgFeeEstimate) {
-            _dkgFeePool = _dkgFeePool.sub(dkgFeeEstimate);
             // Disabling ethlint error message. No security implications, we're calling audited and trusted contract here.
             // solium-disable-next-line
             (success, data) = latestOperatorContract.call.value(dkgFeeEstimate)(abi.encodeWithSignature("createGroup(uint256)", entry));
+            if (success) {
+                _dkgFeePool = _dkgFeePool.sub(dkgFeeEstimate);
+            }
         }
     }
 
