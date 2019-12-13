3c32565643101b46c1af9d944a406ff5775e6534
Sloan Thompson <sloan.thompson@thesis.co>
2019-12-12 19:01:16 -0500

    Retrofit bundle script for use against internal or ropsten chains

--- infrastructure/eth-networks/scripts/prepare-participant-bundles.sh
+++ infrastructure/eth-networks/scripts/prepare-participant-bundles.sh
@@ -1,10 +1,40 @@
+set -e
+
+HELP="Usage: ./$(basename $0) -n <ETH_NETWORK> -e <ENVIRONMENT>
+      \n\nAvailable ETH_NETWORK: ropsten, internal
+      \nAvailable ENVIRONMENT: keep-test"
+
+while getopts ":n:e:" opt; do
+  case $opt in
+    n ) ETH_NETWORK=$OPTARG;;
+    e ) ENVIRONMENT=$OPTARG;;
+
+    \?)
+      echo -e $HELP
+      exit 1
+  esac
+done
+
+if [ $# -eq 0 ]
+then
+  echo -e $HELP
+  exit 1
+fi
+
+if [ $ENVIRONMENT != "keep-test" ]
+then
+  echo "Invalid environment: use keep-test for now"
+  exit 1
+fi
+
 BASEDIR=$(dirname "$0")
-EXTERNAL_PARTICIPANT_PATH="$BASEDIR/../participants"
+EXTERNAL_PARTICIPANT_PATH="${BASEDIR}/../${ENVIRONMENT}/${ETH_NETWORK}/participants"
 EXTERNAL_PARTICIPANTS=$(ls $EXTERNAL_PARTICIPANT_PATH)
 
 function clean_deployment_bundles() {
   echo "=====REMOVING OLD BUNDLES====="
-  for participant in $EXTERNAL_PARTICIPANTS; do
+  for participant in $EXTERNAL_PARTICIPANTS
+  do
     PARTICIPANT_PATH=$EXTERNAL_PARTICIPANT_PATH/$participant
     CURRENT_BUNDLE=$(ls $PARTICIPANT_PATH | grep ".tar.gz")
 
@@ -24,17 +54,18 @@ function create_deployment_bundles() {
   echo "=====CREATING DEPLOYMENT BUNDLES====="
   DATE=$(date +%F)
 
-  for participant in $EXTERNAL_PARTICIPANTS; do
+  for participant in $EXTERNAL_PARTICIPANTS
+  do
     PARTICIPANT_PATH=$EXTERNAL_PARTICIPANT_PATH/$participant
 
     echo "$participant"
     tar -zcvf $PARTICIPANT_PATH/$DATE-keep-client-deployment-bundle.tar.gz \
-      -C $BASEDIR/../../docs/ keep-client-quickstart.adoc \
-      -C ../private-testnet/ changelog.adoc \
-        ./eth-account-password.txt  \
-        ./keep-client-snapshot.tar \
+      -C $BASEDIR/../../../docs/ keep-client-quickstart.adoc \
+      -C ../infrastructure/eth-networks/$ENVIRONMENT/$ETH_NETWORK changelog.adoc \
+         ./eth-account-password.txt  \
+         ./keep-client-snapshot.tar \
       -C ./participants/$participant config \
-        ./persistence
+         ./persistence
      echo "==============================\n"
    done
 }
@@ -48,9 +79,10 @@ function fetch_keep_client_image() {
 function save_keep_client_image() {
   echo "=====SAVING LATEST KEEP-CLIENT IMAGE====="
   echo "This will take several seconds..."
-  docker save -o ../$BASEDIR/keep-client-snapshot.tar gcr.io/keep-test-f3e0/keep-client
+  docker save -o ../$ENVIRONMENT/$ETH_NETWORK/keep-client-snapshot.tar gcr.io/keep-test-f3e0/keep-client
   echo "==============================\n"
 }
+
 
 clean_deployment_bundles
 fetch_keep_client_image
