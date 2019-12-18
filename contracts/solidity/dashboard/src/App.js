9eb94bcde3f99616f4dbdfc0990e44cf4cb1ebd6
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-12-18 16:41:24 +0100

    Added the side menu context to main component

--- contracts/solidity/dashboard/src/App.js
+++ contracts/solidity/dashboard/src/App.js
@@ -5,17 +5,27 @@ import Header from './components/Header';
 import Routing from './components/Routing'
 import ContractsDataContextProvider from './components/ContractsDataContextProvider';
 import { Messages } from './components/Message';
+import { SideMenu, SiedMenuProvider } from './components/SideMenu';
+import { BrowserRouter as Router } from 'react-router-dom'
+
 
 const App = () => (
   <Messages>
     <Web3ContextProvider>
-      <div className='main'>
-        <Header />
-        <ContractsDataContextProvider>
-          <Routing />
-        </ContractsDataContextProvider>
-        <Footer />
-      </div>
+      <SiedMenuProvider>
+        <Router>
+          <div className='main'>
+            <Header />
+            <SideMenu />
+            <div className='content'>
+              <ContractsDataContextProvider>
+                <Routing />
+              </ContractsDataContextProvider>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </SiedMenuProvider>
     </Web3ContextProvider>
   </Messages>
 )
