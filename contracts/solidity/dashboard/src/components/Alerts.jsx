b4bbb8e9593d60a64cff7e23172627ceea19d435
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-12-19 15:46:33 +0100

    Removed web3 error from the alerts component

--- contracts/solidity/dashboard/src/components/Alerts.jsx
+++ contracts/solidity/dashboard/src/components/Alerts.jsx
@@ -4,12 +4,9 @@ import { ContractsDataContext } from './ContractsDataContextProvider'
 
 const Alerts = (props) => {
     const { isOperator, isOperatorOfStakedTokenGrant, stakedGrant, stakeOwner } = useContext(ContractsDataContext)
-    const { error } = useContext(Web3Context)
 
     return (
         <>
-        { error && <div className="alert alert-danger m-5" role="alert">{error}</div> }
-
           {isOperator && !isOperatorOfStakedTokenGrant &&
             <div className="alert alert-info m-5" role="alert">You are registered as an operator for {stakeOwner}</div>
           }
