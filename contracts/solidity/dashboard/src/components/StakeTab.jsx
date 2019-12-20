dc57f0f19dd2872d8a3f29bcb1e3d540f44b3887
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-12-20 12:36:24 +0100

    Remove unused imports from the stake tab component

--- contracts/solidity/dashboard/src/components/StakeTab.jsx
+++ contracts/solidity/dashboard/src/components/StakeTab.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
 import StakingForm from './StakingForm'
 import StakingDelegateForm from './StakingDelegateForm'
 import { withContractsDataContext } from './ContractsDataContextProvider';
