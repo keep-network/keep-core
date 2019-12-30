71db83f8f4599210459a321a8a5fb986b0faa5e4
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-12-30 13:25:49 +0100

    Keep group's address in online
    Change the button's label.
    Change the currency of the reward to ETH.

--- contracts/solidity/dashboard/src/components/RewardsGroupItem.jsx
+++ contracts/solidity/dashboard/src/components/RewardsGroupItem.jsx
@@ -8,11 +8,13 @@ export const RewardsGroupItem = (props) => {
       <span>
         Group index: 1
       </span>
-      <h4>Group public key</h4>
-      <AddressShortcut address={'0xcCFe2E36B3F10152D19dD7d14d651F213c9af4b0'}/>
-      <p>300$</p>
+      <h4>
+        Group public key:&nbsp;
+        <AddressShortcut address={'0xcCFe2E36B3F10152D19dD7d14d651F213c9af4b0'} />
+      </h4>
+      <p>300 ETH</p>
       <Button className="btn btn-primary btn-large">
-        Withdrawal reward
+        Withdraw
       </Button>
     </div>
   )
