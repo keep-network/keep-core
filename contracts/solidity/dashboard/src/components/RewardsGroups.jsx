96361b41c239b9378b184e383aecb9a1d3ece8ae
Rafal Czajkowski <rafal.czajkowski@keep.network>
2019-12-30 12:15:54 +0100

    Create a wrapping component for rewards

---
+++ contracts/solidity/dashboard/src/components/RewardsGroups.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { RewardsGroupItem } from './RewardsGroupItem'
+
+export const RewardsGroups = (props) => {
+  return (
+    <div className='group-items'>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+      <RewardsGroupItem/>
+    </div>
+  )
+}
