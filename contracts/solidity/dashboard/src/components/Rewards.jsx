7e108422538acb601cd9aeccedacd15187eed2fb
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-02 11:39:48 +0100

    Fetching groups from the contract

--- contracts/solidity/dashboard/src/components/Rewards.jsx
+++ contracts/solidity/dashboard/src/components/Rewards.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { RewardsGroups } from './RewardsGroups'
+import { Web3Context } from './WithWeb3Context'
 
 export const Rewards = () => {
+  const { keepRandomBeaconOperatorContract, stakingContract, yourAddress, utils } = useContext(Web3Context)
+  const [isFetching, setIsFetching] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    Promise.all([stakingContract.methods.operatorsOfMagpie(yourAddress).call(), keepRandomBeaconOperatorContract.methods.numberOfGroups().call()])
+      .then(async ([operators, numberOfGroups]) => {
+        const groups = []
+        for (let groupIndex=0; groupIndex < numberOfGroups; groupIndex++) {
+          const groupPubKey = await keepRandomBeaconOperatorContract.methods.getGroupPublicKey(groupIndex).call()
+          const indices = await keepRandomBeaconOperatorContract.methods.getGroupMemberIndices(groupPubKey, operators[1]).call()
+          const reward = utils.toBN(await keepRandomBeaconOperatorContract.methods.getGroupMemberRewards(groupPubKey).call()).mul(utils.toBN(indices.length))
+          const isStale = await keepRandomBeaconOperatorContract.methods.isStaleGroup(groupPubKey).call()
+          groups.push({ groupIndex, groupPubKey, indices, reward, isStale })
+        }
+        setIsFetching(false)
+        setData(groups)
+      }).catch((error) => {
+        setIsFetching(false)
+      })
+  }, [])
   return (
-    <RewardsGroups />
+    <RewardsGroups groups={data} />
   )
 }
