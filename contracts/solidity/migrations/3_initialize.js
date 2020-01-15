2e5410386e35217d8de1af7c3ecfdc9dea212b95
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-01-15 18:19:12 +0100

    Rework price setting condition

--- contracts/solidity/migrations/3_initialize.js
+++ contracts/solidity/migrations/3_initialize.js
@@ -4,6 +4,7 @@ const KeepRandomBeaconService = artifacts.require("./KeepRandomBeaconService.sol
 const KeepRandomBeaconServiceImplV1 = artifacts.require("./KeepRandomBeaconServiceImplV1.sol");
 const KeepRandomBeaconOperator = artifacts.require("./KeepRandomBeaconOperator.sol");
 
+let priceFeedEstimate = web3.utils.toBN(20).mul(web3.utils.toBN(10**9)); // (20 Gwei = 20 * 10^9 wei)
 const fluctuationMargin = 50; // 50%
 const dkgContributionMargin = 1; // 1%
 
@@ -11,11 +12,9 @@ module.exports = async function(deployer, network) {
     const keepRandomBeaconService = await KeepRandomBeaconServiceImplV1.at(KeepRandomBeaconService.address);
     const keepRandomBeaconOperator = await KeepRandomBeaconOperator.deployed();
 
-    let priceFeedEstimate;
+    // Set the price feed estimate to 1 Gwei for Ropsten network
     if (network === 'ropsten') {
-        priceFeedEstimate = web3.utils.toBN(1).mul(web3.utils.toBN(10**9)); // (1 Gwei = 1 * 10^9 wei)
-    } else {
-        priceFeedEstimate = web3.utils.toBN(20).mul(web3.utils.toBN(10**9)); // (20 Gwei = 20 * 10^9 wei)
+        priceFeedEstimate = web3.utils.toBN(1).mul(web3.utils.toBN(10 ** 9)); // (1 Gwei = 1 * 10^9 wei)
     }
 
     keepRandomBeaconService.initialize(
