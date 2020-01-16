7ab464f75253d797719d5d762c780b45f7fec06e
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-01-16 12:42:16 +0100

    Change the way prices are converted to wei

--- contracts/solidity/migrations/3_initialize.js
+++ contracts/solidity/migrations/3_initialize.js
@@ -3,7 +3,7 @@ const KeepRandomBeaconServiceImplV1 = artifacts.require("./KeepRandomBeaconServi
 const KeepRandomBeaconOperator = artifacts.require("./KeepRandomBeaconOperator.sol");
 
 const withdrawalDelay = 86400; // 1 day
-let priceFeedEstimate = web3.utils.toBN(20).mul(web3.utils.toBN(10**9)); // (20 Gwei = 20 * 10^9 wei)
+let priceFeedEstimate = web3.utils.toWei(web3.utils.toBN(20), 'Gwei');
 const fluctuationMargin = 50; // 50%
 const dkgContributionMargin = 1; // 1%
 
@@ -13,7 +13,7 @@ module.exports = async function(deployer, network) {
 
     // Set the price feed estimate to 1 Gwei for Ropsten network
     if (network === 'ropsten') {
-        priceFeedEstimate = web3.utils.toBN(1).mul(web3.utils.toBN(10 ** 9)); // (1 Gwei = 1 * 10^9 wei)
+        priceFeedEstimate = web3.utils.toWei( web3.utils.toBN(1), 'Gwei');
     }
 
     keepRandomBeaconService.initialize(
