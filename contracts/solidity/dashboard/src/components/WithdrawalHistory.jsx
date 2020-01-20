3afe0fc138c93f6b3945812a382657e101ebffd7
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-20 17:36:48 +0100

    Added a see all btn to withdrawal history

--- contracts/solidity/dashboard/src/components/WithdrawalHistory.jsx
+++ contracts/solidity/dashboard/src/components/WithdrawalHistory.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState }from 'react'
 import rewardsService from '../services/rewards.service'
 import { useFetchData } from '../hooks/useFetchData'
 import { WithdrawalHistoryItem } from './WithdrawalHistoryItem'
+import { SeeAllButton } from './SeeAllButton'
 
 export const WithdrawalHistory = (props) => {
   const { isFetching, data } = useFetchData(rewardsService.fetchWithdrawalHistory, [])
+  const [showAll, setShowAll] = useState(false)
 
   return (
-    <ul className="withdrawals-history">
-      Withdrawal History
+    <ul className="withdrawal-history tile">
+      <h6>Withdrawal History</h6>
       {data.map((history) => <WithdrawalHistoryItem key={history.groupPubKey} {...history} /> )}
+      <SeeAllButton
+        dataLength={data.length}
+        previewDataCount={3}
+        onClickCallback={() => setShowAll(!showAll)}
+        showAll={showAll}
+      />
     </ul>
   )
 }
--- contracts/solidity/dashboard/src/components/WithdrawalHistoryItem.jsx
+++ contracts/solidity/dashboard/src/components/WithdrawalHistoryItem.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 import AddressShortcut from './AddressShortcut'
 
 export const WithdrawalHistoryItem = ({ date, groupPublicKey, amount }) => (
-  <li className='group-item'>
-    <div>
+  <li className='withdrawal-history-item'>
+    <span className="small-title">
       {date}
-    </div>
-    <div className='group-key'>
-      <AddressShortcut address={groupPublicKey} />
-    </div>
-    <div className>
-      {amount.toString()} ETH
-    </div>
+    </span>
+    <AddressShortcut address={groupPublicKey} />
+    <span className="small-title">{amount.toString()} ETH</span>
   </li>
 )
