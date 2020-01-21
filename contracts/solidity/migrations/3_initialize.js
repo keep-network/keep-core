ce492e1202a6c6efcf77e6d1761e0e590fcee9bd
Nik G <nik.grinkevich@keep.network>
2020-01-21 12:46:03 -0500

    Add missing steps to contracts initialize
    Make sure operator contract is approved and added to the service contract.

--- contracts/solidity/migrations/3_initialize.js
+++ contracts/solidity/migrations/3_initialize.js
@@ -1,6 +1,7 @@
 const KeepRandomBeaconService = artifacts.require("./KeepRandomBeaconService.sol");
 const KeepRandomBeaconServiceImplV1 = artifacts.require("./KeepRandomBeaconServiceImplV1.sol");
 const KeepRandomBeaconOperator = artifacts.require("./KeepRandomBeaconOperator.sol");
+const RegistryKeeper = artifacts.require("./RegistryKeeper.sol");
 
 const withdrawalDelay = 86400; // 1 day
 let priceFeedEstimate = web3.utils.toWei(web3.utils.toBN(20), 'Gwei');
@@ -10,6 +11,7 @@ const dkgContributionMargin = 1; // 1%
 module.exports = async function(deployer, network) {
     const keepRandomBeaconService = await KeepRandomBeaconServiceImplV1.at(KeepRandomBeaconService.address);
     const keepRandomBeaconOperator = await KeepRandomBeaconOperator.deployed();
+    const registryKeeper = await RegistryKeeper.deployed();
 
     // Set the price feed estimate to 1 Gwei for Ropsten network
     if (network === 'ropsten') {
@@ -21,8 +23,12 @@ module.exports = async function(deployer, network) {
         fluctuationMargin,
         dkgContributionMargin,
         withdrawalDelay,
-        keepRandomBeaconOperator.address
+        registryKeeper.address
     );
 
     await keepRandomBeaconOperator.setPriceFeedEstimate(priceFeedEstimate);
+    await registryKeeper.approveOperatorContract(keepRandomBeaconOperator.address);
+    keepRandomBeaconService.addOperatorContract(
+        keepRandomBeaconOperator.address
+    );
 };
