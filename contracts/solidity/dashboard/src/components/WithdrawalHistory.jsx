37bdc3e989a9669e1e4dba2cc83e2243d65e3ee0
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-21 14:43:05 +0100

    Fixed btn in a withdrawal history component

--- contracts/solidity/dashboard/src/components/WithdrawalHistory.jsx
+++ contracts/solidity/dashboard/src/components/WithdrawalHistory.jsx
@@ -1,9 +1,12 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react'
 import rewardsService from '../services/rewards.service'
 import { useFetchData } from '../hooks/useFetchData'
 import { WithdrawalHistoryItem } from './WithdrawalHistoryItem'
 import { SeeAllButton } from './SeeAllButton'
 import { LoadingOverlay } from './Loadable'
+
+const previewDataCount = 3
+
 
 export const WithdrawalHistory = (props) => {
   const { isFetching, data } = useFetchData(rewardsService.fetchWithdrawalHistory, [])
@@ -13,10 +16,10 @@ export const WithdrawalHistory = (props) => {
     <LoadingOverlay isFetching={isFetching} >
       <ul className="withdrawal-history tile">
         <h6>Withdrawal History</h6>
-        {data.map((history) => <WithdrawalHistoryItem key={history.groupPubKey} {...history} /> )}
+        {showAll ? data.map(renderWithdrawalHistoryItem) : data.slice(0, previewDataCount).map(renderWithdrawalHistoryItem)}
         <SeeAllButton
           dataLength={data.length}
-          previewDataCount={3}
+          previewDataCount={previewDataCount}
           onClickCallback={() => setShowAll(!showAll)}
           showAll={showAll}
         />
@@ -24,3 +27,5 @@ export const WithdrawalHistory = (props) => {
     </LoadingOverlay>
   )
 }
+
+const renderWithdrawalHistoryItem = (history, index) => <WithdrawalHistoryItem key={index} {...history} />
