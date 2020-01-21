ae34e9318407fa1afd8839885e16471a9f7eb33e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-21 11:08:57 +0100

    Added a loading overlay component

--- contracts/solidity/dashboard/src/components/RewardsGroups.jsx
+++ contracts/solidity/dashboard/src/components/RewardsGroups.jsx
@@ -3,32 +3,36 @@ import { RewardsGroupItem } from './RewardsGroupItem'
 import { SeeAllButton } from './SeeAllButton'
 import NoData from './NoData'
 import * as Icons from './Icons'
+import { LoadingOverlay } from './Loadable'
 
 const previewDataCount = 3
 
-export const RewardsGroups = ({ groups }) => {
+export const RewardsGroups = ({ groups, isFetching }) => {
   const [showAll, setShowAll] = useState(false)
-
+  console.log('is fetching', isFetching)
   return (
-    <ul className='group-items self-start tile'>
-      { groups.length === 0 ?
-        <NoData
-          title='No rewards yet!'
-          iconComponent={<Icons.Badge width={100} height={100} />}
-          content='You can withdraw any future earned rewards from your delegated stake on this page.'
-        /> :
+    <LoadingOverlay isFetching={isFetching} classNames="self-start">
+      <ul className='group-items self-start tile'>
+        { groups.length === 0 ?
+          <NoData
+            title='No rewards yet!'
+            iconComponent={<Icons.Badge width={100} height={100} />}
+            content='You can withdraw any future earned rewards from your delegated stake on this page.'
+          /> :
           <>
-            <h6>Withdrawal Overview</h6>
-            {showAll ? groups.map(renderGroupItem) : groups.slice(0, previewDataCount).map(renderGroupItem)}
-            <SeeAllButton
-              dataLength={groups.length}
-              previewDataCount={previewDataCount}
-              onClickCallback={() => setShowAll(!showAll)}
-              showAll={showAll}
-            />
-          </>
-      }
-    </ul>
+              <h6>Withdrawal Overview</h6>
+              {showAll ? groups.map(renderGroupItem) : groups.slice(0, previewDataCount).map(renderGroupItem)}
+              <SeeAllButton
+                dataLength={groups.length}
+                previewDataCount={previewDataCount}
+                onClickCallback={() => setShowAll(!showAll)}
+                showAll={showAll}
+              />
+            </>
+        }
+      </ul>
+
+    </LoadingOverlay>
   )
 }
 
--- contracts/solidity/dashboard/src/components/WithdrawalHistory.jsx
+++ contracts/solidity/dashboard/src/components/WithdrawalHistory.jsx
@@ -3,21 +3,24 @@ import rewardsService from '../services/rewards.service'
 import { useFetchData } from '../hooks/useFetchData'
 import { WithdrawalHistoryItem } from './WithdrawalHistoryItem'
 import { SeeAllButton } from './SeeAllButton'
+import { LoadingOverlay } from './Loadable'
 
 export const WithdrawalHistory = (props) => {
   const { isFetching, data } = useFetchData(rewardsService.fetchWithdrawalHistory, [])
   const [showAll, setShowAll] = useState(false)
 
   return (
-    <ul className="withdrawal-history tile">
-      <h6>Withdrawal History</h6>
-      {data.map((history) => <WithdrawalHistoryItem key={history.groupPubKey} {...history} /> )}
-      <SeeAllButton
-        dataLength={data.length}
-        previewDataCount={3}
-        onClickCallback={() => setShowAll(!showAll)}
-        showAll={showAll}
-      />
-    </ul>
+    <LoadingOverlay isFetching={isFetching} >
+      <ul className="withdrawal-history tile">
+        <h6>Withdrawal History</h6>
+        {data.map((history) => <WithdrawalHistoryItem key={history.groupPubKey} {...history} /> )}
+        <SeeAllButton
+          dataLength={data.length}
+          previewDataCount={3}
+          onClickCallback={() => setShowAll(!showAll)}
+          showAll={showAll}
+        />
+      </ul>
+    </LoadingOverlay>
   )
 }
