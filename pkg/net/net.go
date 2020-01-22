3db470bcfd7d2e667035a851ea7175861a790d35
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-01-22 12:33:48 +0100

    Refactoring of net interface
    Refactored net interface by extracting a new
    interface for unicast channel.

--- pkg/net/net.go
+++ pkg/net/net.go
@@ -69,29 +69,20 @@ type TaggedUnmarshaler interface {
 	Type() string
 }
 
-// BroadcastChannelFilter represents a filter which determine if the incoming
-// message should be processed by the receivers. It takes the message author's
-// public key as its argument and returns true if the message should be
-// processed or false otherwise.
-type BroadcastChannelFilter func(*ecdsa.PublicKey) bool
-
-// BroadcastChannel represents a named pubsub channel. It allows group members
-// to broadcast and receive messages. BroadcastChannel implements strategy
-// for the retransmission of broadcast messages and handle duplicates before
-// passing the received message to the client.
-type BroadcastChannel interface {
-	// Name returns the name of this broadcast channel.
-	Name() string
-	// Given a message m that can marshal itself to protobuf, broadcast m to
-	// members of the Group through the BroadcastChannel. Message will be
-	// periodically retransmitted by the channel for the lifetime of the
-	// provided context.
+// Channel is a communication medium between two or more network peers allowing
+// to send and receive messages.
+type Channel interface {
+	// Send function publishes a message m to the channel. Message m needs to
+	// conform to the marshalling interface. If retransmission is supported
+	// by the implementation, message will be periodically retransmitted by
+	// the channel for the lifetime of the provided context.
 	Send(ctx context.Context, m TaggedMarshaler) error
 	// Recv installs a message handler that will receive messages from the
-	// broadcast channel for the entire lifetime of the provided context.
+	// channel for the entire lifetime of the provided context.
 	// When the context is done, handler is automatically unregistered and
-	// receives no more messages. Already received message retransmissions
-	// are filtered out before calling the handler.
+	// receives no more messages. If retransmission is supported by the
+	// implementation, already received message retransmissions are
+	// filtered out before calling the handler.
 	Recv(ctx context.Context, handler func(m Message))
 	// RegisterUnmarshaler registers an unmarshaler that will unmarshal a given
 	// type to a concrete object that can be passed to and understood by any
@@ -102,8 +93,32 @@ type BroadcastChannel interface {
 	// The string type associated with the unmarshaler is the result of calling
 	// Type() on a raw unmarshaler.
 	RegisterUnmarshaler(unmarshaler func() TaggedUnmarshaler) error
+}
+
+// UnicastChannel represents a bidirectional communication channel between two
+// network peers. UnicastChannel guarantees two peers communicating with each
+// other have a direct connection.
+type UnicastChannel interface {
+	Channel
+}
+
+// BroadcastChannel represents a named pubsub channel. It allows group members
+// to broadcast and receive messages. BroadcastChannel implements strategy
+// for the retransmission of broadcast messages and handle duplicates before
+// passing the received message to the client.
+type BroadcastChannel interface {
+	Channel
+
+	// Name returns the name of this broadcast channel.
+	Name() string
 	// SetFilter registers a broadcast channel filter which will be used
 	// to determine if given broadcast channel message should be processed
 	// by the receivers.
 	SetFilter(filter BroadcastChannelFilter) error
 }
+
+// BroadcastChannelFilter represents a filter which determine if the incoming
+// message should be processed by the receivers. It takes the message author's
+// public key as its argument and returns true if the message should be
+// processed or false otherwise.
+type BroadcastChannelFilter func(*ecdsa.PublicKey) bool
