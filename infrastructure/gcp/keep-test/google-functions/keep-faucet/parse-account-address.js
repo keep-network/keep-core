26883ff75c20046c7caf16e8377983f52557e03e
Sloan Thompson <sloan.thompson@thesis.co>
2020-01-23 14:35:55 -0500

    Check address not account, no account hereeee

--- infrastructure/gcp/keep-test/google-functions/keep-faucet/parse-account-address.js
+++ infrastructure/gcp/keep-test/google-functions/keep-faucet/parse-account-address.js
@@ -4,7 +4,7 @@ exports.parseAccountAddress = (request, response) => {
   requestUrl = url.parse(request.url, true);
   if (requestUrl.query.address) {
     return address = requestUrl.query.address
-  } else if (!/^(0x)?[0-9a-f]{40}$/i.test(account)) {
+  } else if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
       // check if it has the basic requirements of an address
       // double thanks to the Ethereum folks for figuring this regex out already
       return response.send(
