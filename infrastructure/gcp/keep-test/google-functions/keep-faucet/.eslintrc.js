e3e344053e2443fb9924de583d827e716dfbbbea
Sloan Thompson <sloan.thompson@thesis.co>
2020-01-23 15:40:31 -0500

    Bare eslint config

---
+++ infrastructure/gcp/keep-test/google-functions/keep-faucet/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"parser": "babel-eslint",
+	'env': {},
+	'extends': [
+		'eslint-config-keep'
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {}
+}
