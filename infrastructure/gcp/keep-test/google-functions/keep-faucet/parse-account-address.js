fb7fd1bb3dd1d50344e50938e06c42e1175f2baa
Sloan Thompson <sloan.thompson@thesis.co>
2020-01-23 15:39:50 -0500

    Testing eslint fix

--- infrastructure/gcp/keep-test/google-functions/keep-faucet/parse-account-address.js
+++ infrastructure/gcp/keep-test/google-functions/keep-faucet/parse-account-address.js
@@ -1,5 +1,5 @@
 // Attempts to read address to be funded from the http request to the faucet
-const url = require("url")
+const url = require('url')
 
 exports.parseAccountAddress = (request, response) => {
   requestUrl = url.parse(request.url, true)
@@ -9,12 +9,12 @@ exports.parseAccountAddress = (request, response) => {
     // check if it has the basic requirements of an address
     // double thanks to the Ethereum folks for figuring this regex out already
     return response.send(
-      "Improperly formatted account address, please try a valid one.",
+      'Improperly formatted account address, please try a valid one.',
     )
   } else {
-    console.log("No address set in HTTP request.")
+    console.log('No address set in HTTP request.')
     return response.send(
-      "No account address set, please set an address with ?address=<accountAddress>",
+      'No account address set, please set an address with ?address=<accountAddress>',
     )
   }
 }
