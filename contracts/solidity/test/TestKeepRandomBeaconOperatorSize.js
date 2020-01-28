818ee519c6746329ea0460599ad61f8d860c9b38
Dmitry <dymitr.paremski@keep.network>
2020-01-28 13:16:16 +0100

    Increasing contract byte size to 23822

--- contracts/solidity/test/TestKeepRandomBeaconOperatorSize.js
+++ contracts/solidity/test/TestKeepRandomBeaconOperatorSize.js
@@ -16,7 +16,7 @@ contract('KeepRandomBeaconOperator', (_) => {
       let bytecodeSize = bytecode.length / 2; // size in bytes
       let deployedBytecodeSize = deployedBytecode.length / 2; // size in bytes
 
-      const maxSafeBytecodeSize = 23758
+      const maxSafeBytecodeSize = 23822
 
       console.log(
         "KeepRandomBeaconOperator size of bytecode in bytes = ", 
