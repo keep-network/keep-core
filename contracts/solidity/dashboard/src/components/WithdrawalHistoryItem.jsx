3536e4fdc74fd33c346e5c3fb95a9064a1bc44ff
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-29 19:35:45 +0100

    Rename css class

--- contracts/solidity/dashboard/src/components/WithdrawalHistoryItem.jsx
+++ contracts/solidity/dashboard/src/components/WithdrawalHistoryItem.jsx
@@ -3,10 +3,10 @@ import AddressShortcut from './AddressShortcut'
 
 export const WithdrawalHistoryItem = ({ date, groupPublicKey, amount }) => (
   <li className='withdrawal-history-item'>
-    <span className="small-title">
+    <span className="text-small">
       {date}
     </span>
     <AddressShortcut address={groupPublicKey} />
-    <span className="small-title">{amount.toString()} ETH</span>
+    <span className="text-small">{amount.toString()} ETH</span>
   </li>
 )
