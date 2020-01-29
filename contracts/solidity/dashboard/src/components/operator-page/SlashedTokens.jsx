19f7b1b605d72f9c0fb13adbcccb7d3817ba1b5e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-29 19:39:05 +0100

    Added styles to the slashed tokens section

--- contracts/solidity/dashboard/src/components/operator-page/SlashedTokens.jsx
+++ contracts/solidity/dashboard/src/components/operator-page/SlashedTokens.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import SlashedTokensList from './SlashedTokensList'
 
 const SlashedTokens = (props) => {
-    return ()
+  return (
+    <section id="slashed-tokens" className="tile">
+      <h5>
+          Slashed Tokens
+        <span className="text-warning">Group misbehavior results in a slash of KEEP tokens.</span>
+      </h5>
+      <SlashedTokensList />
+    </section>
+  )
 }
+
+export default SlashedTokens
