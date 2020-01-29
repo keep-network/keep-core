2de82623042a1f1eaffa3e46db360fac2c3f2af4
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-29 19:41:53 +0100

    Added the SlashedTokens component to the operator

--- contracts/solidity/dashboard/src/components/operator-page/OperatorPage.jsx
+++ contracts/solidity/dashboard/src/components/operator-page/OperatorPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import DelegatedTokens from './DelegatedTokens'
 import PendingUndelegation from './PendingUndelegation'
+import SlashedTokens from './SlashedTokens'
 
 const OperatorPage = (props) => {
+  return (
+    <>
+      <h3>My Token Operations</h3>
+      <DelegatedTokens />
+      <PendingUndelegation />
+      <SlashedTokens />
+    </>
 
-    return (
-        <>
-            <DelegatedTokens />
-            <PendingUndelegation />
-        </>
-        
-    )
+  )
 }
+
+export default OperatorPage
