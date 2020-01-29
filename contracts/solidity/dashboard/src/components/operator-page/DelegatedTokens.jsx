1f800213cd8a83d118e705cf4f37992f5febe05e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-29 19:36:40 +0100

    Added styles to the delegated tokens section

--- contracts/solidity/dashboard/src/components/operator-page/DelegatedTokens.jsx
+++ contracts/solidity/dashboard/src/components/operator-page/DelegatedTokens.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import AddressShortcut from '../AddressShortcut'
+import InlineForm from '../InlineForm'
 
 const DelegatedTokens = (props) => {
-    return()
+  return (
+    <section id="delegated-tokens" className="tile">
+      <h5>Total Delegated Tokens</h5>
+      <div className="flex flex-row">
+        <div className="delegated-tokens-summary flex flex-column" style={{ flex: '1' }} >
+          <h2 className="balance">
+            80,000 K
+          </h2>
+          <div>
+            <h6 className="text-darker-grey">OWNER&nbsp;
+              <AddressShortcut address='0x345CFE101B5860d5F8ed775C21Ad5e55806C6D9B' classNames='text-big text-darker-grey' />
+            </h6>
+            <h6 className="text-darker-grey">BENEFICIARY&nbsp;
+              <AddressShortcut address='0x345CFE101B5860d5F8ed775C21Ad5e55806C6D9B' classNames='text-big text-darker-grey' />
+            </h6>
+          </div>
+        </div>
+        <InlineForm inputProps={{ placeholder: 'Amount' }} classNames="undelegation-form" />
+      </div>
+    </section>
+  )
 }
 
 export default DelegatedTokens
