60a979735b924d57a4bffe59705efe37eab948b6
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-30 14:26:40 +0100

    Event subscription on the operator page

--- contracts/solidity/dashboard/src/components/operator-page/OperatorPage.jsx
+++ contracts/solidity/dashboard/src/components/operator-page/OperatorPage.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import DelegatedTokens from './DelegatedTokens'
 import PendingUndelegation from './PendingUndelegation'
 import SlashedTokens from './SlashedTokens'
+import { useSubscribeToContractEvent } from '../../hooks/useSubscribeToContractEvent'
+import { TOKEN_STAKING_CONTRACT_NAME } from '../../constants/constants'
+import { Web3Context } from '../WithWeb3Context'
 
 const OperatorPage = (props) => {
+  const { yourAddress } = useContext(Web3Context)
+  const { latestEvent } =
+    useSubscribeToContractEvent(TOKEN_STAKING_CONTRACT_NAME, 'InitiatedUnstake', { filter: { operator: yourAddress } })
+
   return (
     <>
       <h3>My Token Operations</h3>
-      <DelegatedTokens />
-      <PendingUndelegation />
+      <DelegatedTokens latestUnstakeEvent={latestEvent} />
+      <PendingUndelegation latestUnstakeEvent={latestEvent} />
       <SlashedTokens />
     </>
 
