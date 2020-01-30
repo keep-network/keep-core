e3de9c1a0889b6e16d29371a3981becd8fbb506a
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-30 13:21:10 +0100

    Fetching data from contract in the pending section

--- contracts/solidity/dashboard/src/components/operator-page/PendingUndelegation.jsx
+++ contracts/solidity/dashboard/src/components/operator-page/PendingUndelegation.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useEffect, useContext, useRef } from 'react'
 import PendingUndelegationList from './PendingUndelegationList'
+import { useFetchData } from '../../hooks/useFetchData'
+import { operatorService } from './service'
+import { formatDate, displayAmount } from '../../utils'
+import { LoadingOverlay } from '../Loadable'
+import { Web3Context } from '../WithWeb3Context'
+
+const initialData = { pendinUndelegations: [] }
 
 const PendingUndelegation = (props) => {
+  const event = useRef(null)
+  const state = useFetchData(operatorService.fetchPendingUndelegation, initialData)
+  const { isFetching, data: { pendingUnstakeBalance, undelegatedOn, stakeWithdrawalDelay, pendinUndelegations } } = state
+  const { stakingContract, yourAddress } = useContext(Web3Context)
+
+  useEffect(() => {
+    event.current = stakingContract.events.InitiatedUnstake({ filter: { operator: yourAddress } }, (error, event) => {
+      console.log('subscribed to event', error, event )
+    })
+
+    return () => {
+      console.log('unmount')
+      event.current.unsubscribe((error, suscces) => console.log('unsub', error, suscces))
+    }
+  }, [])
+
   return (
     <section id="pending-undelegation" className="tile">
-      <h5>Pending Undelegation</h5>
-      <div className="flex pending-undelegation-summary">
-        <h2 className="balance flex flex-1">3,000 K</h2>
-        <div className="flex flex-1 flex-column">
-          <span className="text-label">UNDELEGATED ON</span>
-          <span className="text-big">January 19, 2020</span>
+      <LoadingOverlay isFetching={isFetching}>
+        <h5>Pending Undelegation</h5>
+        <div className="flex pending-undelegation-summary">
+          <h2 className="balance flex flex-1">{pendingUnstakeBalance && `${displayAmount(pendingUnstakeBalance)} K`}</h2>
+          <div className="flex flex-1 flex-column">
+            <span className="text-label">UNDELEGATED ON</span>
+            <span className="text-big">{formatDate(undelegatedOn)}</span>
+          </div>
+          <div className="flex flex-1 flex-column">
+            <span className="text-label">UNDELEGATION PERIOD</span>
+            <span className="text-big">{stakeWithdrawalDelay}</span>
+          </div>
         </div>
-        <div className="flex flex-1 flex-column">
-          <span className="text-label">UNDELEGATION PERIOD</span>
-          <span className="text-big">1 week</span>
+        <div>
+          <PendingUndelegationList pendingUndelegations={pendinUndelegations} />
         </div>
-      </div>
-      <div>
-        <PendingUndelegationList />
-      </div>
+      </LoadingOverlay>
     </section>
   )
 }
