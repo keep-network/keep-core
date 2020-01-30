8191d862c1153665c65dac15e532ac6cf609cab6
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-01-30 16:52:40 +0100

    Moved the operator page to the pages dir

--- contracts/solidity/dashboard/src/pages/OperatorPage.jsx
+++ contracts/solidity/dashboard/src/pages/OperatorPage.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
-import DelegatedTokens from './DelegatedTokens'
-import PendingUndelegation from './PendingUndelegation'
-import SlashedTokens from './SlashedTokens'
-import { useSubscribeToContractEvent } from '../../hooks/useSubscribeToContractEvent'
-import { TOKEN_STAKING_CONTRACT_NAME } from '../../constants/constants'
-import { Web3Context } from '../WithWeb3Context'
+import DelegatedTokens from '../components/DelegatedTokens'
+import PendingUndelegation from '../components/PendingUndelegation'
+import SlashedTokens from '../components/SlashedTokens'
+import { useSubscribeToContractEvent } from '../hooks/useSubscribeToContractEvent'
+import { TOKEN_STAKING_CONTRACT_NAME } from '../constants/constants'
+import { Web3Context } from '../components/WithWeb3Context'
 
 const OperatorPage = (props) => {
   const { yourAddress } = useContext(Web3Context)
