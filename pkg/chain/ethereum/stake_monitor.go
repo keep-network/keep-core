15bd5b950244d1f6af2ed294779c179f6559f2f7
Nik G <nik.grinkevich@keep.network>
2020-01-31 14:33:16 -0500

    Fix missed event rename in keep client

--- pkg/chain/ethereum/stake_monitor.go
+++ pkg/chain/ethereum/stake_monitor.go
@@ -80,7 +80,7 @@ func (es *ethereumStaker) OnStakeChanged(handle func(newStake *big.Int)) {
 
 	if !es.watchingChain {
 		// FIXME Should we do something with this event subscription?
-		_, err := es.ethereum.stakingContract.WatchInitiatedUnstake(
+		_, err := es.ethereum.stakingContract.WatchUndelegated(
 			func(_ common.Address, newStake *big.Int, _ *big.Int, _ uint64) {
 				es.mutex.Lock()
 				allHandlers := make([]func(newStake *big.Int), len(es.stakeChangeHandlers))
