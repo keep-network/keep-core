a447bf26ac9726cf015a7d4202fd8739faad0b55
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-01 16:32:52 +0100

    Update a slashed tokens section

--- contracts/solidity/dashboard/src/components/SlashedTokensList.jsx
+++ contracts/solidity/dashboard/src/components/SlashedTokensList.jsx
@@ -1,37 +1,46 @@
 import React from 'react'
+import moment from 'moment'
+import { formatDate } from '../utils'
 
 const SlashedTokensList = (props) => {
   return (
-    <>
-        <div className="flex flex-1">
-          <span className="text-label flex-2">DETAILS</span>
-          <span className="text-label flex flex-1">SLASH AMOUNT</span>
-          <span className="text-label flex flex-1">MIN STAKE</span>
-        </div>
-        <ul className="slashed-tokens-list flex flex-column flex-1">
-          <li className="flex flex-row flex-1" >
-            <div className="details text-big flex-2">
-              <span className="text-big">January 12, 2020</span>
-              <p className="text-small text-grey">
-                Group 12305162340123 was selected to do work and not enough members participated.
-              </p>
-            </div>
-            <span className="text-big flex-1">1,000 K</span>
-            <span className="text-big flex-1">50 K</span>
-          </li>
-          <li className="flex flex-row flex-1" >
-            <div className="details text-big flex-2">
-              <span className="text-big">January 12, 2020</span>
-              <p className="text-small text-grey">
-                Group 12305162340123 key was leaked. Private key was published outside of the members of the signing group.
-              </p>
-            </div>
-            <span className="text-big flex-1">1,000 K</span>
-            <span className="text-big flex-1">20 K</span>
-          </li>
-        </ul>
-    </>
-
+    <div className="slashed-tokens-list">
+      <div className="flex flex-1">
+        <span className="text-label flex-2">SLASH EXPLANATION</span>
+        <span className="text-label flex flex-1">AMOUNT (KEEP)</span>
+        <span className="text-label flex flex-1">MIN STAKE (KEEP)</span>
+      </div>
+      <ul className="flex flex-column flex-1">
+        <li className="flex flex-row flex-1" >
+          <div className="details text-big flex-2">
+            <p className="text-big">
+              Group 12305162340123 was selected to do work and not enough members participated.
+            </p>
+            <span className="text-small text-grey">{formatDate(moment())}</span>
+          </div>
+          <span className="text-big text-dark-red flex-1">
+            - 25
+          </span>
+          <span className="text-big flex-1">
+            50
+          </span>
+        </li>
+        <li className="flex flex-row flex-1" >
+          <div className="details text-big flex-2">
+            <p className="text-big">
+              Group 12305162340123 key was leaked. Private key was published outside of the members of the signing group.
+            </p>
+            <span className="text-small text-grey">{formatDate(moment())}</span>
+          </div>
+          <span className="text-big text-dark-red flex-1">
+            - 25
+          </span>
+          <span className="text-big flex-1">
+            50
+          </span>
+        </li>
+      </ul>
+    </div>
   )
 }
 
