9b14df3917f55dfcacd23a9ea0fdfb51b3001419
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-05 09:58:17 +0100

    Fix form submission flow

--- contracts/solidity/dashboard/src/hooks/useCustomOnSubmitFormik.js
+++ contracts/solidity/dashboard/src/hooks/useCustomOnSubmitFormik.js
@@ -4,26 +4,26 @@ export const useCustomOnSubmitFormik = (onSubmitAction) => {
   const {
     values,
     setSubmitting,
-    setFormikState,
     setTouched,
     resetForm,
     validateForm,
+    setFormikState,
   } = useFormikContext()
 
   const onSubmit = async (onTransactionHashCallback, openInfoMessage, setIsFetchng) => {
     // Pre-submit
+    const touched = {}
+    Object.keys(values).forEach((name) => {
+      touched[name] = true
+    })
+    setTouched(touched, false)
     setSubmitting(true)
-    setFormikState((prevState) => ({ submitCount: prevState.submitCount + 1 }))
+    setFormikState((prevState) => ({ ...prevState, submitCount: prevState.submitCount + 1 }))
 
     // Validation
     const errors = await validateForm(values)
     console.log('errors', errors)
     if (errors) {
-      const touched = {}
-      Object.keys(errors).forEach((name) => {
-        touched[name] = true
-      })
-      setTouched(touched, false)
       setSubmitting(false)
       return
     }
