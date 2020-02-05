4b80a3d65bd9442480927187278ce079b1c4bd1e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-05 16:50:37 +0100

    Form input styles

--- contracts/solidity/dashboard/src/components/FormInput.jsx
+++ contracts/solidity/dashboard/src/components/FormInput.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import { useField } from 'formik'
 
-const FromInput = ({ label, getFieldProps, getFieldMetaProps, getFieldMeta, ...props }) => {
+const FromInput = ({ label, ...props }) => {
   const [field, meta] = useField(props.name, props.type)
 
-  console.log('field', field, meta)
-
   return (
-      <>
-        <label>
-          {label}
-        </label>
-        <input {...field} {...props} />
-        {meta.touched && meta.error ? (
-          <div className='error'>{meta.error}</div>
-        ) : null}
-      </>
+    <div className="form-input flex flex-column">
+      <label>
+        {label}
+      </label>
+      <input {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className='form-error'>{meta.error}</div>
+      ) : null}
+    </div>
   )
 }
 
