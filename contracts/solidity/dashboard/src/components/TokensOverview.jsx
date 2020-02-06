372d91868eb3242f6e556978898a156ba52ca07e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-06 19:08:55 +0100

    Render the tokens overview tile depending on props

--- contracts/solidity/dashboard/src/components/TokensOverview.jsx
+++ contracts/solidity/dashboard/src/components/TokensOverview.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { displayAmount } from '../utils'
 
-const TokensOverview = () => {
+const TokensOverview = ({ keepBalance, stakingBalance, pendingUndelegationBalance, grantBalance, tokenGrantsStakeBalance }) => {
   return (
     <section id="tokens-overview" className="tile">
       <h5>Totals</h5>
       <section>
         <span className="text-label text-white" style={{ background: '#AC6E16', padding: '.2rem' }}>OWNED</span>
-        <h2 className="balance">200</h2>
+        <h2 className="balance">{displayAmount(keepBalance)}</h2>
         <div className="text-samll">
-          Staked Owned Tokens: 200
+          Staked Owned Tokens: {displayAmount(stakingBalance)}
           <p className="text-smaller text-grey">Tokens you own that are delegated to an operator and doing work on the network.</p>
         </div>
         <div className="text-samll">
-          Pengind Undelegated Tokens: 0
+          Pengind Undelegated Tokens: {displayAmount(pendingUndelegationBalance)}
           <p className="text-smaller text-grey">Stake undelegated from an operator. Estimated X number of blocks until available.</p>
         </div>
         <hr />
       </section>
       <section>
-        <span className="text-label text-white" style={{ background: '#AC6E16', padding:  '.2rem' }}>GRANTED</span>
-        <h2 className="balance">200</h2>
+        <span className="text-label text-white" style={{ background: '#AC6E16', padding: '.2rem' }}>GRANTED</span>
+        <h2 className="balance">{displayAmount(grantBalance)}</h2>
         <div className="text-samll">
-          Staked Tokens: 0
+          Staked Tokens: {displayAmount(tokenGrantsStakeBalance)}
           <p className="text-smaller text-grey">Tokens you were granted that are delegated to an operator and doing work on the network.</p>
         </div>
       </section>
