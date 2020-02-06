824810ea2ef053e269a350fb61f58b2f7722128d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-06 19:13:39 +0100

    Fix the amountInRange validation fn

--- contracts/solidity/dashboard/src/forms/common-validators.js
+++ contracts/solidity/dashboard/src/forms/common-validators.js
@@ -1,4 +1,5 @@
 import web3Utils from 'web3-utils'
+import { displayAmount } from '../utils'
 
 const REGEXP_NOT_BLANK_STRING = /^\s*$/
 const REGEXP_ONLY_NUMBERS = /^\d+$/
@@ -16,18 +17,19 @@ export const validateEthAddress = (address, required = true) => {
 }
 
 export const validateAmountInRange = (value, maxValue, minValue = 0) => {
-  const validateValueInBN = web3Utils.toBN(value || 0)
+  const formatedValue = value ? web3Utils.toBN(value).mul(web3Utils.toBN(10).pow(web3Utils.toBN(18))).toString() : 0
+  const validateValueInBN = web3Utils.toBN(formatedValue)
   const maxValueInBN = web3Utils.toBN(maxValue || 0)
   const minValueInBN = web3Utils.toBN(minValue)
 
   if (isBlankString(value)) {
     return 'Required'
   } else if (!REGEXP_ONLY_NUMBERS.test(value)) {
-    return 'Invalid value'  
-  } else if (validateValueInBN.gte(maxValueInBN)) {
-    return 'You do not have enough KEEP tokens'
-  } else if (minValueInBN.gte(validateValueInBN)) {
-    return `You have to stake more than ${minValueInBN.toString()} KEEP tokens`
+    return 'Invalid value'
+  } else if (validateValueInBN.gt(maxValueInBN)) {
+    return `The value should be less than or equals to ${displayAmount(maxValueInBN).toString()}`
+  } else if (validateValueInBN.lt(minValueInBN)) {
+    return `The value should be greater than or equlas to ${displayAmount(minValueInBN).toString()}`
   }
 }
 
