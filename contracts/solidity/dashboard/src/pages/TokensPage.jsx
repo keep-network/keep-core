73e850950b16005f13f97763551e224561ce5553
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-06 19:16:38 +0100

    Fetching data on the tokens page

--- contracts/solidity/dashboard/src/pages/TokensPage.jsx
+++ contracts/solidity/dashboard/src/pages/TokensPage.jsx
@@ -2,22 +2,44 @@ import React, { useContext } from 'react'
 import DelegateStakeForm from '../components/DelegateStakeForm'
 import TokensOverview from '../components/TokensOverview'
 import Undelegations from '../components/Undelegations'
+import { useFetchData } from '../hooks/useFetchData'
+import { tokensPageService } from '../services/tokens-page.service'
+import DelegatedTokensList from '../components/DelegatedTokensList'
 import { Web3Context } from '../components/WithWeb3Context'
-import { useShowMessage } from '../components/Message'
-import { wait } from '../utils'
+import { useShowMessage, messageType } from '../components/Message'
+
+const initialData = {
+  ownedKeepBalance: '',
+  tokenStakingBalance: '',
+  pendingUndelegationBalance: '',
+  tokenGrantsBalance: '',
+  tokenGrantsStakeBalance: '',
+  minimumStake: '',
+  delegations: [],
+  undelegations: [],
+}
 
 const TokensPage = () => {
-  const { token } = useContext(Web3Context)
+  const web3Context = useContext(Web3Context)
   const showMessage = useShowMessage()
-
+  const [state] = useFetchData(tokensPageService.fetchTokensPageData, initialData)
+  const {
+    ownedKeepBalance,
+    pendingUndelegationBalance,
+    tokenStakingBalance,
+    tokenGrantsBalance,
+    tokenGrantsStakeBalance,
+    minimumStake,
+    delegations,
+    undelegations,
+  } = state.data
 
   const handleSubmit = async (values, onTransactionHashCallback) => {
     try {
-      console.log('values', values, onTransactionHashCallback)
-      await Promise.all([wait(5000)])
-      onTransactionHashCallback('hash heheeh')
+      await tokensPageService.delegateStake(web3Context, values, onTransactionHashCallback)
+      showMessage({ type: messageType.SUCCESS, title: 'Success', content: 'Staking delegate transaction has been successfully completed' })
     } catch (error) {
-      console.log('error', error)
+      showMessage({ type: messageType.ERROR, title: 'Staking delegate action has been failed ', content: error.message })
     }
   }
 
@@ -25,10 +47,23 @@ const TokensPage = () => {
     <React.Fragment>
       <h3>My Tokens</h3>
       <div className="flex flex-1 flex-row-space-between flex-wrap">
-        <DelegateStakeForm onSubmit={handleSubmit} minStake={100} availableTokens={3000}/>
-        <TokensOverview />
+        <DelegateStakeForm
+          onSubmit={handleSubmit}
+          minStake={minimumStake}
+          keepBalance={ownedKeepBalance}
+          grantBalance={tokenGrantsBalance}
+        />
+        <TokensOverview
+          keepBalance={ownedKeepBalance}
+          stakingBalance={tokenStakingBalance}
+          pendingUndelegationBalance={pendingUndelegationBalance}
+          grantBalance={tokenGrantsBalance}
+          tokenGrantsStakeBalance={tokenGrantsStakeBalance}
+        />
       </div>
-      <Undelegations />
+      <Undelegations undelegations={undelegations} />
+      <DelegatedTokensList delegatedTokens={delegations}
+      />
     </React.Fragment>
   )
 }
