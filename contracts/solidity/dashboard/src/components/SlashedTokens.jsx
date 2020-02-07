aa76f08380772133640797a254629a8297c82d2e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-07 12:00:19 +0100

    Update the SlashedTokens style

--- contracts/solidity/dashboard/src/components/SlashedTokens.jsx
+++ contracts/solidity/dashboard/src/components/SlashedTokens.jsx
@@ -14,11 +14,11 @@ const SlashedTokens = (props) => {
         <h5>
             Slashed Tokens
         </h5>
-        <span className="text-small text-darker-grey">
+        <div className="text-small text-warning border">
           A slash is a penalty for signing group misbehavior.
           A slash results in a removal of a portion of your delegated KEEP tokens.
           You can see a record below of all slashes.
-        </span>
+        </div>
         <SlashedTokensList slashedTokens={data} />
       </section>
     </LoadingOverlay>
