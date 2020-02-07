166266e271549d17c64c54fa8d1dba26d55a28e2
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-07 11:38:01 +0100

    Render slashed tokens list

--- contracts/solidity/dashboard/src/components/SlashedTokensList.jsx
+++ contracts/solidity/dashboard/src/components/SlashedTokensList.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import moment from 'moment'
-import { formatDate } from '../utils'
+import { formatDate, displayAmount } from '../utils'
+import AddressShortcut from './AddressShortcut'
 
-const SlashedTokensList = (props) => {
+const SlashedTokensList = ({ slashedTokens }) => {
   return (
     <div className="slashed-tokens-list">
       <div className="flex flex-1">
@@ -11,37 +11,34 @@ const SlashedTokensList = (props) => {
         <span className="text-label flex flex-1">MIN STAKE (KEEP)</span>
       </div>
       <ul className="flex flex-column flex-1">
-        <li className="flex flex-row flex-1" >
-          <div className="details text-big flex-2">
-            <p className="text-big">
-              Group 12305162340123 was selected to do work and not enough members participated.
-            </p>
-            <span className="text-small text-grey">{formatDate(moment())}</span>
-          </div>
-          <span className="text-big text-dark-red flex-1">
-            - 25
-          </span>
-          <span className="text-big flex-1">
-            50
-          </span>
-        </li>
-        <li className="flex flex-row flex-1" >
-          <div className="details text-big flex-2">
-            <p className="text-big">
-              Group 12305162340123 key was leaked. Private key was published outside of the members of the signing group.
-            </p>
-            <span className="text-small text-grey">{formatDate(moment())}</span>
-          </div>
-          <span className="text-big text-dark-red flex-1">
-            - 25
-          </span>
-          <span className="text-big flex-1">
-            50
-          </span>
-        </li>
+        {slashedTokens.map(renderSlashedTokenItem)}
       </ul>
     </div>
   )
 }
+
+const renderSlashedTokenItem = (item) => <SlashedTokeItem key={item.id} {...item} />
+
+const SlashedTokeItem = React.memo(({ amount, date, typeOfPunishment, groupPublicKey }) => (
+  <li className="flex flex-row flex-1" >
+    <div className="details text-big flex-2">
+      <span className="text-big">{formatDate(date)}</span>
+      <p className="text-small text-grey">
+        Group <AddressShortcut address={groupPublicKey}/>&nbsp;
+        {typeOfPunishment === 0 ?
+          'was selected to do work and not enough members participated.' :
+          'key was leaked. Private key was published outside of the members of the signing group.'
+        }
+      </p>
+    </div>
+    <div className="text-big text-dark-red flex-1">
+      - {amount && `${displayAmount(amount)}`}
+    </div>
+    <div className="text-big text-darker-grey flex-1">
+      {amount && `${displayAmount(amount)}`}
+    </div>
+  </li>
+))
+
 
 export default SlashedTokensList
