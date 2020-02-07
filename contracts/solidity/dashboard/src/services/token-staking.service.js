32274e5352263dd6e765d6ae9172f8c6b6953600
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-07 16:12:51 +0100

    Update the token staking service

--- contracts/solidity/dashboard/src/services/token-staking.service.js
+++ contracts/solidity/dashboard/src/services/token-staking.service.js
@@ -1,7 +1,6 @@
 import { contractService } from './contracts.service'
 import { TOKEN_STAKING_CONTRACT_NAME } from '../constants/constants'
 import web3Utils from 'web3-utils'
-import moment from 'moment'
 
 const fetchDelegatedTokensData = async (web3Context) => {
   const { yourAddress } = web3Context
@@ -16,25 +15,20 @@ const fetchDelegatedTokensData = async (web3Context) => {
 
 const fetchPendingUndelegation = async (web3Context) => {
   const { yourAddress } = web3Context
-  const [undelegation, undelegationPeriod, events] = await Promise.all([
+  const [undelegation, undelegationPeriod] = await Promise.all([
     contractService.makeCall(web3Context, TOKEN_STAKING_CONTRACT_NAME, 'getUndelegation', yourAddress),
     contractService.makeCall(web3Context, TOKEN_STAKING_CONTRACT_NAME, 'undelegationPeriod'),
-    contractService.getPastEvents(web3Context, TOKEN_STAKING_CONTRACT_NAME, 'Undelegated', { fromBlock: '0', filter: { operator: yourAddress } }),
   ])
 
   const undelegationAtInBN = web3Utils.toBN(undelegation.undelegatedAt).add(web3Utils.toBN(undelegationPeriod))
-  const pendingUnstakeBalance = undelegation.undelegatedAt === '0' ? 0 : undelegation.balance
-  const undelegationComplete = undelegation.undelegatedAt === '0' ? null : undelegationAtInBN.toString()
-
-  const pendinUndelegations = events
-    .map(({ id, returnValues: { value, createdAt } }) => ({ eventId: id, amount: value, createdAt: moment.unix(createdAt) }))
-    .reverse()
+  const isUndelegation = undelegation.undelegatedAt === '0'
+  const pendingUnstakeBalance = isUndelegation ? 0 : undelegation.amount
+  const undelegationComplete = isUndelegation ? null : undelegationAtInBN.toString()
 
   return {
     pendingUnstakeBalance,
     undelegationComplete,
     undelegationPeriod,
-    pendinUndelegations,
   }
 }
 
