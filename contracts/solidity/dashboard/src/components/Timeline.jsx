2b5c3dee3706baf19a487ae456df53543a8a4252
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-13 13:53:02 +0100

    Timeline component skeleton

---
+++ contracts/solidity/dashboard/src/components/Timeline.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Timeline = ({ title, breakePoints }) => {
+  return (
+    <section className="timeline">
+      <div className="breakpoint">
+        <div className="breakpoint-content">
+          <div>10 VESTED</div>
+          <div>11/02/2019</div>
+        </div>
+      </div>
+      <div className="breakpoint">
+        <div className="breakpoint-content">
+          <div>10 VESTED</div>
+          <div>11/02/2019</div>
+        </div>
+      </div>
+      <div className="breakpoint">
+        <div className="breakpoint-content">
+          <div>10 VESTED</div>
+          <div>11/02/2019</div>
+        </div>
+      </div>
+      <div className="breakpoint">
+        <div className="breakpoint-content">
+          <div>10 VESTED</div>
+          <div>11/02/2019</div>
+        </div>
+      </div>
+      <div className="breakpoint">
+        <div className="breakpoint-content">
+          <div>10 VESTED</div>
+          <div>11/02/2019</div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Timeline
