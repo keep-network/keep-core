e99c89b16c973bfe465c9cf77d22495075a2ff25
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-14 15:41:48 +0100

    Create a modal component

---
+++ contracts/solidity/dashboard/src/components/Modal.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
+
+const modalRoot = document.getElementById('modal-root')
+
+const Modal = React.memo(({ closeModal, ...props }) => {
+  const modalOverlay = useRef(null)
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+
+    return () => {
+      document.body.style.overflow = 'scroll'
+    }
+  }, [])
+
+  const onOverlayClick = (event) => {
+    if (modalOverlay.current === event.target) {
+      closeModal()
+    }
+  }
+
+  return ReactDOM.createPortal(
+    <div ref={modalOverlay} className="modal-overlay" onClick={onOverlayClick}>
+      <div className="modal-content">
+        <div className="modal-close" onClick={closeModal}>close</div>
+        {props.children}
+      </div>
+    </div>,
+    modalRoot
+  )
+})
+
+
+export default Modal
