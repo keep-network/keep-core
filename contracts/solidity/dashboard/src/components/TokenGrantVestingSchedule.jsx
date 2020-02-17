549f36f0153665753051760089fc0f69e5d44f61
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-17 18:07:12 +0100

    Add props to the token grant vesting schedule

--- contracts/solidity/dashboard/src/components/TokenGrantVestingSchedule.jsx
+++ contracts/solidity/dashboard/src/components/TokenGrantVestingSchedule.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Timeline from './Timeline'
 import { useFetchData } from '../hooks/useFetchData'
 import { tokenGrantsService } from '../services/token-grants.service'
+import { displayAmount } from '../utils'
 
-const TokenGrantVestingSchedule = ({ }) => {
+const TokenGrantVestingSchedule = ({ grantId, start, amount }) => {
   const [state] = useFetchData(tokenGrantsService.fetchGrantVestingSchedule, [], 0)
   const { isFetching, data } = state
 
@@ -26,17 +27,19 @@ const TokenGrantVestingSchedule = ({ }) => {
       </div>
       <div className="mt-1">
         <div className="text-title text-darker-grey">total</div>
-        <div>100 KEEP</div>
+        <div>{displayAmount(amount)} KEEP</div>
         <div>progress bar here</div>
       </div>
       <div className="mt-1">
         <Timeline
           title='schedule'
           breakepoints={data}
+          footer={
+            <div className="mb-3 text-smaller text-darker-grey">
+              Vesting will continue until completion on 11/01/2020
+            </div>
+          }
         />
-        <div className="text-smaller text-darker-grey">
-            Vesting will continue until completion on 11/01/2020
-        </div>
       </div>
     </div>
   )
