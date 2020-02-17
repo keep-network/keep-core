f69038d59fbb50a81e004c957cce7176cd5cde72
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-17 18:04:07 +0100

    Fix the vesting schedule calculation

--- contracts/solidity/dashboard/src/services/token-grants.service.js
+++ contracts/solidity/dashboard/src/services/token-grants.service.js
@@ -2,22 +2,27 @@ import { TOKEN_GRANT_CONTRACT_NAME } from '../constants/constants'
 import { contractService } from './contracts.service'
 import moment from 'moment'
 import { displayAmount } from '../utils'
+import web3Utils from 'web3-utils'
 
 const fetchGrantVestingSchedule = async (web3Context, grantId) => {
   const { eth } = web3Context
+  const { cliff, start, duration } = await contractService.makeCall(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'getGrantVestingSchedule', grantId)
+  const { amount } = await contractService.makeCall(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'getGrant', grantId)
 
-  const grantDetails = await contractService.makeCall(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'getGrant', grantId)
-  const { cliff, duration, start } = await contractService.makeCall(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'getGrantVestingSchedule', grantId)
+  const vested = await contractService.makeCall(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'grantedAmount', grantId)
+  const cliffDuration = web3Utils.toBN(cliff).sub(web3Utils.toBN(start))
+  const vestedAmountAfterCliff = web3Utils.toBN(amount).mul(cliffDuration).div(web3Utils.toBN(duration))
 
-  const cliffBreakpoint = { dotColorClassName: 'grey ring', label: 'Cliff', date: moment.unix(start) }
-  const afterCliffBreakpoint = { dotColorClassName: 'grey', label: 'After cliff vested', date: moment.unix(cliff) }
+  const cliffBreakpoint = { dotColorClassName: 'grey ring', label: 'Cliff Start', date: moment.unix(start) }
+  const afterCliffBreakpoint = { dotColorClassName: 'grey', label: `Cliff End Will ${displayAmount(vestedAmountAfterCliff, 18, 2)} Vested`, date: moment.unix(cliff) }
   const breakpoints = [cliffBreakpoint, afterCliffBreakpoint]
   if (moment.unix(cliff).isBefore(moment())) {
-    breakpoints.push({ dotColorClassName: 'grey', label: 'current breakpoint', date: moment() })
+    breakpoints.push({ dotColorClassName: 'grey', label: `${displayAmount(vested)} Vested`, date: moment() })
   }
 
-  const withdrawEvents = await contractService.getPastEvents(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'WithdrawnTokenGrant', { fromBlock: '0', filter: { id: grantId } })
-  const stakedEvents = await contractService.getPastEvents(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'StakedGrant', { fromBlock: '0', filter: { id: grantId } })
+  const filterObj = { fromBlock: '0', filter: { id: grantId } }
+  const withdrawEvents = await contractService.getPastEvents(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'WithdrawnTokenGrant', filterObj)
+  const stakedEvents = await contractService.getPastEvents(web3Context, TOKEN_GRANT_CONTRACT_NAME, 'StakedGrant', filterObj)
 
   for (let i = 0; i < withdrawEvents.length; i++) {
     const { blockNumber, returnValues: { amount } } = withdrawEvents[i]
