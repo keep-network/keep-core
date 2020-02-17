8f7257e29e86e8dce96635251e987ec1e6caa610
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-17 19:05:30 +0100

    Added registry approval in demo.js script
    Operator contract has to be approved in the Registry contract.

--- contracts/solidity/scripts/demo.js
+++ contracts/solidity/scripts/demo.js
@@ -2,6 +2,7 @@ const KeepToken = artifacts.require("./KeepToken.sol");
 const TokenStaking = artifacts.require("./TokenStaking.sol");
 const TokenGrant = artifacts.require("./TokenGrant.sol");
 const KeepRandomBeaconOperator = artifacts.require("./KeepRandomBeaconOperator.sol");
+const Registry = artifacts.require("Registry")
 
 function formatAmount(amount, decimals) {
   return web3.utils.toBN(amount).mul(web3.utils.toBN(10).pow(web3.utils.toBN(decimals)))
@@ -22,6 +23,7 @@ module.exports = async function() {
   const tokenStaking = await TokenStaking.deployed();
   const tokenGrant = await TokenGrant.deployed();
   const operatorContract = await KeepRandomBeaconOperator.deployed();
+    const registry = await Registry.deployed()
 
   let owner = accounts[0]; // The address of an owner of the staked tokens.
   // accounts[1]...[4] Operators for owner delegated stake and receivers of the rewards.
@@ -30,6 +32,9 @@ module.exports = async function() {
   let grantee = accounts[0];
   let grantManager = accounts[5];
   let granteeOperator = accounts[6];
+
+    // Approve operator contract in the Registry.
+    await registry.approveOperatorContract(operatorContract.address, { from: owner })
 
   // Stake delegate tokens for first 5 accounts as operators,
   // including the first account where owner operating for themself.
