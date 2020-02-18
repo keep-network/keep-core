6739fbf3c8401d425aa1b2ac5d2f9e6f4336e1c6
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-18 15:06:41 +0100

    Updated log level selection to choice list
    Log level can be selected from the list but also configured to custom
    value.

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,11 +10,33 @@ CONFIG_FILE_PATH_DEFAULT=$(realpath -m $(dirname $0)/../config.toml)
 read -p "Enter ethereum accounts password [$KEEP_ETHEREUM_PASSWORD_DEFAULT]: " ethereum_password
 KEEP_ETHEREUM_PASSWORD=${ethereum_password:-$KEEP_ETHEREUM_PASSWORD_DEFAULT}
 
-read -p "Enter log level [$LOG_LEVEL_DEFAULT]: " log_level
-LOG_LEVEL=${log_level:-$LOG_LEVEL_DEFAULT}
-
 read -p "Enter path to keep-core client config [$CONFIG_FILE_PATH_DEFAULT]: " config_file_path
 CONFIG_FILE_PATH=${config_file_path:-$CONFIG_FILE_PATH_DEFAULT}
+
+log_level_options=("info" "debug" "custom...")
+while :
+do
+    echo "Select log level [$LOG_LEVEL_DEFAULT]: "
+    i=1
+    for o in  "${log_level_options[@]}"; do
+        echo "$i) $o"
+        let i++
+    done
+
+    read reply
+    case $reply in
+        "1"|"${log_level_options[0]}") LOG_LEVEL=${log_level_options[0]}; break;;
+        "2"|"${log_level_options[1]}") LOG_LEVEL=${log_level_options[1]}; break;;
+        "3"|"${log_level_options[2]}") 
+            read -p "Enter custom log level: [$LOG_LEVEL_DEFAULT]" log_level
+            LOG_LEVEL=${log_level:-$LOG_LEVEL_DEFAULT}
+            break
+            ;;
+        "") LOG_LEVEL=$LOG_LEVEL_DEFAULT; break;;
+        *) echo "Invalid choice. Please choose an existing option number.";;
+    esac
+done
+echo "Log level: \"$LOG_LEVEL\""
 
 # Run script.
 LOG_START='\n\e[1;36m' # new line + bold + color
