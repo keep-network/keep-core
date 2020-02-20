515e66b1bbe0576424881eb586ff33649bcd567a
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-20 13:47:07 +0100

    Add a className prop to the SpeechBubbleInfo

--- contracts/solidity/dashboard/src/components/SpeechBubbleInfo.jsx
+++ contracts/solidity/dashboard/src/components/SpeechBubbleInfo.jsx
@@ -4,15 +4,19 @@ import * as Icons from './Icons'
 const childStyle = { marginLeft: '1rem' }
 const wrapperStyle = { marginTop: '0.8rem' }
 
-export const SpeechBubbleInfo = ({ children }) => {
+export const SpeechBubbleInfo = ({ children, className }) => {
   return (
-    <div className="flex flex-row" style={wrapperStyle}>
+    <div className={`flex flex-row ${className}`} style={wrapperStyle}>
       <Icons.SpeechBubble />
       <div className="text-small text-darker-grey" style={childStyle}>
         {children}
       </div>
     </div>
   )
+}
+
+SpeechBubbleInfo.defaultProps = {
+  className: '',
 }
 
 export default React.memo(SpeechBubbleInfo)
