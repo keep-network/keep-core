6d9e902e32d292d9acec40d0e64683c36f8f63c7
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-21 16:28:57 +0100

    Create recover stake button component

---
+++ contracts/solidity/dashboard/src/components/RecoverStakeButton.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useCallback } from 'react'
+import { SubmitButton } from './Button'
+import { Web3Context } from './WithWeb3Context'
+import { useShowMessage, messageType } from './Message'
+
+const RecoverStakeButton = ({ operatorAddress, ...props }) => {
+  const { yourAddress, stakingContract } = useContext(Web3Context)
+  const showMessage = useShowMessage()
+
+  const recoverStake = useCallback(async (onTransactionHashCallback) => {
+    try {
+      await stakingContract
+        .methods
+        .recoverStake(operatorAddress)
+        .send({ from: yourAddress })
+        .on('transactionHash', onTransactionHashCallback)
+      showMessage({ type: messageType.SUCCESS, title: 'Success', content: 'Recover stake transaction successfully completed' })
+    } catch (error) {
+      showMessage({ type: messageType.ERROR, title: 'Recover stake action has been failed ', content: error.message })
+    }
+  }, [operatorAddress, yourAddress])
+
+  return (
+    <SubmitButton
+      className={props.btnClassName}
+      onSubmitAction={recoverStake}
+      pendingMessageTitle='Recover stake transaction is pending...'
+    >
+      {props.btnText}
+    </SubmitButton>
+  )
+}
+
+RecoverStakeButton.defaultProps = {
+  btnClassName: 'btn btn-sm btn-default',
+  btnText: 'recover',
+}
+
+export default React.memo(RecoverStakeButton)
