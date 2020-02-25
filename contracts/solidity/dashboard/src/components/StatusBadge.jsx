47f0de9268c2fe78bf89ca346561149dbf87b8b9
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-25 11:54:41 +0100

    Add a borderRadius property to the StatusBadge

--- contracts/solidity/dashboard/src/components/StatusBadge.jsx
+++ contracts/solidity/dashboard/src/components/StatusBadge.jsx
@@ -5,11 +5,13 @@ export const BADGE_STATUS = {
   COMPLETED: { textClassName: 'text-success', bgClassName: 'text-bg-success-light' },
 }
 
+const badgeStyle = { padding: '0.1rem 0.5rem', borderRadius: '100px' }
+
 const StatusBadge = ({ status, text, className }) => {
   return (
     <div
       className={`${status.textClassName} ${status.bgClassName} text-label ${className}`}
-      style={{ padding: '0.1rem 0.2rem' }}
+      style={badgeStyle}
     >
       {text}
     </div>
