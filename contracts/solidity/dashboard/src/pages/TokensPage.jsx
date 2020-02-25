7040b32aa0b54a50d48f047a43b647801013dba9
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-25 18:03:24 +0100

    Throw an error in catch statement in the submit fn that is passed to the SubmitBtn component

--- contracts/solidity/dashboard/src/pages/TokensPage.jsx
+++ contracts/solidity/dashboard/src/pages/TokensPage.jsx
@@ -54,6 +54,7 @@ const TokensPage = () => {
       })
     } catch (error) {
       showMessage({ type: messageType.ERROR, title: 'Staking delegate action has been failed ', content: error.message })
+      throw error
     }
   }
 
