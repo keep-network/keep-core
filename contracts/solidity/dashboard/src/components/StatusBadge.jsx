99580cce4ffeef301867514b05148697887f807c
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-26 14:15:10 +0100

    Rename the badge status from completed to complete

--- contracts/solidity/dashboard/src/components/StatusBadge.jsx
+++ contracts/solidity/dashboard/src/components/StatusBadge.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const BADGE_STATUS = {
-  PENDING: { textClassName: 'text-warning text-normal', bgClassName: 'text-bg-pending-light' },
-  COMPLETED: { textClassName: 'text-success', bgClassName: 'text-bg-success-light' },
+  PENDING: { textClassName: 'text-warning', bgClassName: 'text-bg-pending-light' },
+  COMPLETE: { textClassName: 'text-success', bgClassName: 'text-bg-success-light' },
 }
 
 const badgeStyle = { padding: '0.1rem 0.5rem', borderRadius: '100px' }
@@ -10,7 +10,7 @@ const badgeStyle = { padding: '0.1rem 0.5rem', borderRadius: '100px' }
 const StatusBadge = ({ status, text, className }) => {
   return (
     <div
-      className={`${status.textClassName} ${status.bgClassName} text-label ${className}`}
+      className={`${status.textClassName} ${status.bgClassName} text-label text-normal ${className}`}
       style={badgeStyle}
     >
       {text}
