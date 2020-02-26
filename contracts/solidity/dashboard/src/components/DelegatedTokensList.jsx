844686d59d017a433709c663e29e81195dacd585
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-02-26 14:13:32 +0100

    Update the delegaten tokens list
    Add status and authorizer address.

--- contracts/solidity/dashboard/src/components/DelegatedTokensList.jsx
+++ contracts/solidity/dashboard/src/components/DelegatedTokensList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { displayAmount } from '../utils'
 import AddressShortcut from './AddressShortcut'
 import UndelegateStakeButton from './UndelegateStakeButton'
+import StatusBadge, { BADGE_STATUS } from './StatusBadge'
 
 const DelegatedTokensList = ({ delegatedTokens, successDelegationCallback }) => {
   const renderDelegatedTokensItem = (item) =>
@@ -16,16 +17,19 @@ const DelegatedTokensList = ({ delegatedTokens, successDelegationCallback }) => 
       <h5>Delageted Tokens</h5>
       <div className="flex flex-row-center mt-1">
         <div className="flex-1 text-label">
-          CREATED AT
+          amount
         </div>
         <div className="flex-1 text-label">
-          BENEFICIARY ADDRESS
+          delegation status
         </div>
         <div className="flex-1 text-label">
-          OPERATOR ADDRESS
+          beneficiary
         </div>
         <div className="flex-1 text-label">
-          AMOUNT
+          operator
+        </div>
+        <div className="flex-1 text-label">
+          authorizer
         </div>
         <div className="flex-1"/>
       </div>
@@ -37,12 +41,21 @@ const DelegatedTokensList = ({ delegatedTokens, successDelegationCallback }) => 
 }
 
 const DelegatedTokensListItem = React.memo(({ delegation, successDelegationCallback }) => {
+  const delegationStatus = delegation.isInInitializationPeriod ? 'PENDING' : 'COMPLETE'
+
   return (
     <li className="flex flex-row text-darker-grey flex-row-center flex-row-space-between" style={{ marginBottom: `0.5rem` }}>
-      <div className="flex-1">{delegation.createdAt}</div>
+      <div className="flex-1">{displayAmount(delegation.amount)} KEEP</div>
+      <div className="flex flex-1">
+        <StatusBadge
+          status={BADGE_STATUS[delegationStatus]}
+          className="self-start"
+          text={delegationStatus.toLowerCase()}
+        />
+      </div>
       <div className="flex-1"><AddressShortcut address={delegation.beneficiary} /></div>
       <div className="flex-1"><AddressShortcut address={delegation.operatorAddress} /></div>
-      <div className="flex-1">{displayAmount(delegation.amount)} KEEP</div>
+      <div className="flex-1"><AddressShortcut address={delegation.authorizerAddress} /></div>
       <div className="flex-1">
         <UndelegateStakeButton
           isInInitializationPeriod={delegation.isInInitializationPeriod}
