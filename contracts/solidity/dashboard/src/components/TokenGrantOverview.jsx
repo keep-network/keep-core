44f441004ce5ff4de95339051b732eeffedda6bb
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-03-03 16:48:02 +0100

    Add circular bar to the token grant overview

--- contracts/solidity/dashboard/src/components/TokenGrantOverview.jsx
+++ contracts/solidity/dashboard/src/components/TokenGrantOverview.jsx
@@ -1,63 +1,110 @@
-import React, { useState, useEffect } from 'react'
-import { tokenGrantsService } from '../services/token-grants.service'
-import { useFetchData } from '../hooks/useFetchData'
+import React from 'react'
 import { formatDate, displayAmount } from '../utils'
-import Dropdown from './Dropdown'
-import ProgressBar from './ProgressBar'
+import { SubmitButton } from './Button'
 import { colors } from '../constants/colors'
-import SelectedGrantDropdown from './SelectedGrantDropdown'
-import { useModal } from '../hooks/useModal'
-import TokenGrantVestingSchedule from './TokenGrantVestingSchedule'
+import { CircularProgressBars } from './CircularProgressBar'
+import moment from 'moment'
 
-const TokenGrantOverview = (props) => {
-  const [state] = useFetchData(tokenGrantsService.fetchGrants, [])
-  const { data } = state
-  const [selectedGrant, setSelectedGrant] = useState({})
-  const { showModal, ModalComponent } = useModal()
-
-  useEffect(() => {
-    if (selectedGrant && data.length > 0) {
-      setSelectedGrant(data[0])
-    }
-  }, [data])
-
-  const onSelect = (selectedItem) => {
-    setSelectedGrant(selectedItem)
-  }
+const TokenGrantOverview = ({ selectedGrant }) => {
+  const cliffPeriod = moment
+    .unix(selectedGrant.cliff)
+    .from(moment.unix(selectedGrant.start), true)
+  const fullyUnlockedDate = moment
+    .unix(selectedGrant.start)
+    .add(selectedGrant.duration, 'seconds')
 
   return (
-    <div className="token-grant-overview" style={data.length === 0 ? { display: 'none' } : {}}>
-      <ModalComponent title="Vesting Schedule Summary">
-        <TokenGrantVestingSchedule grant={selectedGrant} />
-      </ModalComponent>
-      {
-        data.length > 1 &&
-        <Dropdown
-          onSelect={onSelect}
-          options={data}
-          valuePropertyName='id'
-          labelPropertyName='id'
-          selectedItem={selectedGrant}
-          labelPrefix='Grant ID'
-          noItemSelectedText='Select Grant'
-          label="Choose Grant"
-          selectedItemComponent={<SelectedGrantDropdown grant={selectedGrant} />}
-        />
-      }
-      <div className="flex row center space-between">
-        <h4 className="balance">{displayAmount(selectedGrant.amount)}&nbsp;KEEP</h4>
-        <span className="text-warning text-link" onClick={showModal}>Vesting Schedule</span>
-      </div>
+    <div className="token-grant-overview">
       <div>
-        <ProgressBar
-          total={selectedGrant.amount}
-          items={[
-            { value: selectedGrant.vested, color: colors.grey, label: 'Vested' },
-            { value: selectedGrant.released, color: colors.primary, label: 'Relesed' },
-            { value: selectedGrant.staked, color: colors.brown, label: 'Staked' },
-          ]}
-          withLegend
-        />
+        <h2 className="balance">{displayAmount(selectedGrant.amount)}&nbsp;KEEP</h2>
+        <div className="text-small text-grey-40">
+          Issued: 01/01/2020
+          <span className="text-smaller text-grey-30">&nbsp;Cliff: {cliffPeriod}</span>
+          <br/>
+          Fully Unlocked: {formatDate(fullyUnlockedDate)}
+        </div>
+      </div>
+      <hr/>
+      <div className="flex row full-center">
+        <div>
+          <CircularProgressBars
+            total={selectedGrant.amount}
+            items={[
+              {
+                value: selectedGrant.vested,
+                backgroundStroke: '#D7F6EE',
+                color: colors.primary,
+                label: 'Unlocked',
+              },
+              {
+                value: selectedGrant.released,
+                color: colors.secondary,
+                withBackgroundStroke: false,
+                radius: 48,
+                label: 'Relesed',
+              },
+            ]}
+            withLegend
+          />
+        </div>
+        <div className={`ml-2 mt-1 ${selectedGrant.readyToRealese === '0' ? 'self-start' : '' }`}>
+          <div className="text-label">
+            unlocked
+          </div>
+          <div className="text-label">
+            {displayAmount(selectedGrant.vested)}
+            <div className="text-smaller text-grey-40">
+              of {displayAmount(selectedGrant.amount)} total
+            </div>
+          </div>
+          {
+            selectedGrant.readyToRealese !== '0' &&
+            <div className="mt-2">
+              <div>
+                <span className="text-secondary text-bold text-small">
+                  {displayAmount(selectedGrant.readyToRealese)}
+                &nbsp;
+                </span>
+                <span className="text-smaller text-grey-40">Ready to realese</span>
+              </div>
+              <SubmitButton
+                className="btn btn-sm btn-secondary"
+                onSubmitAction={() => {
+                  console.log('on submit action here')
+                }}
+              >
+              release tokens
+              </SubmitButton>
+            </div>
+          }
+        </div>
+      </div>
+      <div className="flex row full-center mt-1">
+        <div>
+          <CircularProgressBars
+            total={selectedGrant.amount}
+            items={[
+              {
+                value: selectedGrant.staked,
+                backgroundStroke: '#F8E9D3',
+                color: colors.brown,
+                label: 'Staked',
+              },
+            ]}
+            withLegend
+          />
+        </div>
+        <div className="ml-2 mt-1 self-start">
+          <div className="text-label">
+            staked
+          </div>
+          <div className="text-label">
+            {displayAmount(selectedGrant.staked)}
+            <div className="text-smaller text-grey-40">
+              of {displayAmount(selectedGrant.amount)} total
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- contracts/solidity/dashboard/src/components/TokensOverview.jsx
+++ contracts/solidity/dashboard/src/components/TokensOverview.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { displayAmount } from '../utils'
 import TokenGrantOverview from './TokenGrantOverview'
+import Dropdown from './Dropdown'
+import SelectedGrantDropdown from './SelectedGrantDropdown'
+import { useFetchData } from '../hooks/useFetchData'
+import { tokenGrantsService } from '../services/token-grants.service'
 
 const TokensOverview = ({
   undelegationPeriod,
@@ -10,8 +14,44 @@ const TokensOverview = ({
   grantBalance,
   tokenGrantsStakeBalance,
 }) => {
+  const [state] = useFetchData(tokenGrantsService.fetchGrants, [])
+  const { data } = state
+  const [selectedGrant, setSelectedGrant] = useState({})
+
+  useEffect(() => {
+    if (selectedGrant && data.length > 0) {
+      setSelectedGrant(data[0])
+    }
+  }, [data])
+
+  const onSelect = (selectedItem) => {
+    setSelectedGrant(selectedItem)
+  }
+
   return (
     <section id="tokens-overview" className="tile">
+      <section>
+        <h4 className="text-grey-60">Granted Tokens</h4>
+        <h2 className="balance">{displayAmount(grantBalance)}</h2>
+        <div style={data.length === 0 ? { display: 'none' } : {}}>
+          {
+            data.length > 1 &&
+              <Dropdown
+                onSelect={onSelect}
+                options={data}
+                valuePropertyName='id'
+                labelPropertyName='id'
+                selectedItem={selectedGrant}
+                labelPrefix='Grant ID'
+                noItemSelectedText='Select Grant'
+                label="Choose Grant"
+                selectedItemComponent={<SelectedGrantDropdown grant={selectedGrant} />}
+              />
+          }
+          <TokenGrantOverview selectedGrant={selectedGrant} />
+        </div>
+      </section>
+      <hr />
       <section>
         <h4 className="text-grey-60">Owned Tokens</h4>
         <h2 className="balance">{displayAmount(keepBalance)}</h2>
@@ -23,12 +63,6 @@ const TokensOverview = ({
           Pending Undelegated Tokens: {displayAmount(pendingUndelegationBalance)}
           <p className="text-smaller text-grey-30">Stake undelegated from an operator. Estimated {undelegationPeriod} number of blocks until available.</p>
         </div>
-        <hr />
-      </section>
-      <section>
-        <h4 className="text-grey-60">Granted Tokens</h4>
-        <h2 className="balance">{displayAmount(grantBalance)}</h2>
-        <TokenGrantOverview />
       </section>
     </section>
   )
