0adff5e3ac352b09adcbb6d8ab31d7ad9dc3a095
Promethea Raschke <promethea.raschke@keep.network>
2020-03-09 12:29:09 +0000

    Add tests for OperatorParams setters

--- contracts/solidity/test/TestOperatorParams.js
+++ contracts/solidity/test/TestOperatorParams.js
@@ -16,14 +16,68 @@ contract('OperatorParamsStub', (accounts) => {
       opUtils = await OperatorParamsStub.new();
   });
 
-  it("should roundtrip values", async () => {
-    const params = await opUtils.publicPack(allKeepEver, recently, billionYearsFromNow);
-    const amount = await opUtils.publicGetAmount(params);
-    const createdAt = await opUtils.publicGetCreationBlock(params);
-    const undelegatedAt = await opUtils.publicGetUndelegationBlock(params);
+  describe("pack", async () => {
+    it("should roundtrip values", async () => {
+      const params = await opUtils.publicPack(
+        allKeepEver,
+        recently,
+        billionYearsFromNow);
+      const amount = await opUtils.publicGetAmount(params);
+      const createdAt = await opUtils.publicGetCreationBlock(params);
+      const undelegatedAt = await opUtils.publicGetUndelegationBlock(params);
 
-    assert.equal(amount.toJSON(), allKeepEver.toJSON(), "The amount should be the same");
-    assert.equal(createdAt.toJSON(), recently.toJSON(), "The creation block should be the same");
-    assert.equal(undelegatedAt.toJSON(), billionYearsFromNow.toJSON(), "The undelegation block should be the same");
+      assert.equal(
+        amount.toJSON(),
+        allKeepEver.toJSON(),
+        "The amount should be the same");
+      assert.equal(
+        createdAt.toJSON(),
+        recently.toJSON(),
+        "The creation block should be the same");
+      assert.equal(
+        undelegatedAt.toJSON(),
+        billionYearsFromNow.toJSON(),
+        "The undelegation block should be the same");
+    })
+  })
+
+  describe("setAmount", async () => {
+    it("should set the amount", async () => {
+      const params = await opUtils.publicPack(allKeepEver, recently, 0);
+      const newParams = await opUtils.publicSetAmount(params, billion);
+      const amount = await opUtils.publicGetAmount(newParams);
+      assert.equal(
+        amount.toJSON(),
+        billion.toJSON(),
+        "The amount should be the same");
+    })
+  })
+
+  describe("setCreationBlock", async () => {
+    it("should set the creation block", async () => {
+      const params = await opUtils.publicPack(allKeepEver, recently, 0);
+      const newParams = await opUtils.publicSetCreationBlock(
+        params,
+        billionYearsFromNow);
+      const creationBlock = await opUtils.publicGetCreationBlock(newParams);
+      assert.equal(
+        creationBlock.toJSON(),
+        billionYearsFromNow.toJSON(),
+        "The creation block should be the same");
+    })
+  })
+
+  describe("setUndelegationBlock", async () => {
+    it("should set the undelegation block", async () => {
+      const params = await opUtils.publicPack(allKeepEver, recently, 0);
+      const newParams = await opUtils.publicSetUndelegationBlock(
+        params,
+        recently);
+      const undelegationBlock = await opUtils.publicGetUndelegationBlock(newParams);
+      assert.equal(
+        undelegationBlock.toJSON(),
+        recently.toJSON(),
+        "The undelegationBlock should be the same");
+    })
   })
 })
