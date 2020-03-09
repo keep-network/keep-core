8044979929a8bd31aabf4b49a3d78ed1324bc855
Promethea Raschke <promethea.raschke@keep.network>
2020-03-09 11:58:25 +0000

    Add documentation and sanity checks on OperatorParams uint sizes
    Using `require`s instead of just clearing overflow bits
    gives better feedback if we screw up sometime in the future
    (the virtual fields have almost 40 bits of safety margin each,
    so a serious logic error or specification change would be necessary
    for this to ever become an actual problem).

--- contracts/solidity/contracts/utils/OperatorParams.sol
+++ contracts/solidity/contracts/utils/OperatorParams.sol
@@ -1,6 +1,25 @@
 pragma solidity ^0.5.4;
 
 library OperatorParams {
+    // OperatorParams packs values that are commonly used together
+    // into a single uint256 to reduce the cost functions
+    // like querying eligibility.
+    //
+    // An OperatorParams uint256 contains:
+    // - the operator's staked token amount (uint128)
+    // - the operator's creation block (uint64)
+    // - the operator's undelegation block (uint64)
+    //
+    // These are packed as [amount | createdAt | undelegatedAt]
+    //
+    // Staked KEEP is stored in an uint128,
+    // which is sufficient ecause KEEP tokens have 18 decimals (2^60)
+    // and there will be at most 10^9 KEEP in existence (2^30).
+    //
+    // Creation and undelegation times are stored in an uint64 each.
+    // Because blocks are created every 10 to 15 seconds,
+    // there are less than 4 million blocks per year (2^22).
+    // Thus uint64s would be sufficient for a trillion years.
     uint256 constant BLOCKHEIGHT_WIDTH = 64;
     uint256 constant AMOUNT_WIDTH = 128;
 
@@ -15,9 +34,22 @@ library OperatorParams {
         uint256 createdAt,
         uint256 undelegatedAt
     ) internal pure returns (uint256) {
-        uint256 a = (amount & AMOUNT_MAX) << AMOUNT_SHIFT;
-        uint256 c = (createdAt & BLOCKHEIGHT_MAX) << CREATION_SHIFT;
-        uint256 u = undelegatedAt & BLOCKHEIGHT_MAX;
+        // Check for staked amount overflow.
+        // We shouldn't actually ever need this.
+        require(
+            amount <= AMOUNT_MAX,
+            "amount uint128 overflow"
+        );
+        // Bitwise OR the blockheights together.
+        // The resulting number is equal or greater than either,
+        // and tells if we have a bit set outside the 64 available bits.
+        require(
+            (createdAt | undelegatedAt) <= BLOCKHEIGHT_MAX,
+            "blockheight uint64 overflow"
+        );
+        uint256 a = amount << AMOUNT_SHIFT;
+        uint256 c = createdAt << CREATION_SHIFT;
+        uint256 u = undelegatedAt;
         return (a | c | u);
     }
 
