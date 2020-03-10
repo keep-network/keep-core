d2d9fe8e2c2a963848063b5bd30d4fd62928fcd4
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-03-10 16:05:17 +0100

    Set a default etherscane url in env file

--- contracts/solidity/dashboard/src/constants/constants.js
+++ contracts/solidity/dashboard/src/constants/constants.js
@@ -9,7 +9,7 @@ export const TOKEN_STAKING_CONTRACT_NAME_EVENTS = 'eventStakingContract'
 export const TOKEN_GRANT_CONTRACT_NAME_EVENTS = 'eventGrantContract'
 export const OPERATOR_CONTRACT_NAME_EVENTS = 'eventKeepRandomBeaconOperatorContract'
 
-export const ETHERSCAN_DEFAULT_URL = 'https://etherscan.io/address/'
+export const ETHERSCAN_DEFAULT_URL = process.env.REACT_APP_ETHERSCAN_DEFAULT_URL
 
 export const PENDING_STATUS = 'PENDING'
 export const COMPLETE_STATUS = 'COMPLETE'
