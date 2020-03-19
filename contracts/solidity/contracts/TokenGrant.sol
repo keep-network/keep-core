b5dccf19184b813e331361dab011ce184255da98
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-03-19 09:31:51 +0100

    Leave a comment to be sure it is a right condition
    Condition works only because revocable grant cannot be staked. Leave a
    comment just to make sure we dont's skip this line in future.

--- contracts/solidity/contracts/TokenGrant.sol
+++ contracts/solidity/contracts/TokenGrant.sol
@@ -314,6 +314,7 @@ contract TokenGrant {
     function revoke(uint256 _id) public {
         require(grants[_id].grantManager == msg.sender, "Only grant manager can revoke.");
         require(grants[_id].revocable, "Grant must be revocable in the first place.");
+        // This is safe because revocable grant cannot be staked.
         require(grants[_id].revokedAt == 0, "Grant must not be already revoked.");
 
         uint256 amount = withdrawable(_id);
