be870e07e5abd3b63cd7dd40eff00aebb8835ea9
Promethea Raschke <promethea.raschke@keep.network>
2020-03-20 13:41:50 +0000

    Add documentation to UintSet

--- contracts/solidity/contracts/utils/SetUtils.sol
+++ contracts/solidity/contracts/utils/SetUtils.sol
@@ -1,19 +1,24 @@
 pragma solidity ^0.5.4;
 
 library SetUtils {
+    /// @notice The UintSet is like an array of unique `uint256`s,
+    /// but additionally supports O(1) membership tests and removals.
+    /// @dev Because the UintSet relies on a mapping,
+    /// it can only be used in storage, not in memory.
     struct UintSet {
         // members[positions[item] - 1] = item
         uint256[] members;
         mapping(uint256 => uint256) positions;
     }
 
+    /// @notice Check whether the UintSet `self` contains `item`
     function contains(UintSet storage self, uint256 item)
         internal view returns (bool) {
         return (self.positions[item] != 0);
     }
 
-    // Insert item to set
-    // If already present, do nothing
+    /// @notice Insert `item` to UintSet `self`.
+    /// If already present, do nothing.
     function insert(UintSet storage self, uint256 item) internal {
         if (!contains(self, item)) {
             self.members.push(item);
@@ -21,8 +26,8 @@ library SetUtils {
         }
     }
 
-    // Remove item from set
-    // If not already present, do nothing
+    /// @notice Remove `item` from UintSet `self`.
+    /// If not already present, do nothing.
     function remove(UintSet storage self, uint256 item) internal {
         uint256 positionPlusOne = self.positions[item];
         if (positionPlusOne != 0) {
@@ -39,6 +44,7 @@ library SetUtils {
         }
     }
 
+    /// @notice Return the members of the UintSet `self`.
     function enumerate(UintSet storage self)
         internal view returns (uint256[] memory) {
         return self.members;
