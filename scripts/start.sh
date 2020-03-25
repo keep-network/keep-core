10fc49b42dec5a87e54af952fdf2c0da881a6685
Dmitry <dymitr.paremski@keep.network>
2020-03-25 20:07:21 +0100

    Removing realpath from start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,8 @@ set -e
 # Dafault inputs.
 KEEP_ETHEREUM_PASSWORD_DEFAULT="password"
 LOG_LEVEL_DEFAULT="info"
-CONFIG_DIR_PATH_DEFAULT=$(realpath -m $(dirname $0)/../configs)
+KEEP_CORE_PATH=$PWD
+CONFIG_DIR_PATH_DEFAULT="$KEEP_CORE_PATH/configs"
 
 # Read user inputs.
 read -p "Enter ethereum accounts password [$KEEP_ETHEREUM_PASSWORD_DEFAULT]: " ethereum_password
@@ -63,8 +64,7 @@ echo "Log level: \"$LOG_LEVEL\""
 LOG_START='\n\e[1;36m' # new line + bold + color
 LOG_END='\n\e[0m' # new line + reset color
 
-KEEP_CORE_PATH=$(realpath $(dirname $0)/../)
-KEEP_CORE_CONFIG_FILE_PATH=$(realpath $CONFIG_FILE_PATH)
+KEEP_CORE_CONFIG_FILE_PATH=$CONFIG_FILE_PATH
 
 printf "${LOG_START}Starting keep-core client...${LOG_END}"
 cd $KEEP_CORE_PATH
