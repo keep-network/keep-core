61972ff8e4f2019c9f6a1bbe7658c6bf9342eca1
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-03-25 11:38:28 +0100

    Update SlashedTokensList component

--- solidity/dashboard/src/components/SlashedTokensList.jsx
+++ solidity/dashboard/src/components/SlashedTokensList.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { formatDate, displayAmount } from '../utils'
+import { formatDate, displayAmount } from '../utils/general.utils'
 import AddressShortcut from './AddressShortcut'
 
 const SlashedTokensList = ({ slashedTokens }) => {
   return (
     <div className="slashed-tokens-list">
       <div className="flex flex-1">
-        <span className="text-label flex flex-1">AMOUNT</span>
-        <span className="text-label flex-2">SLASH EXPLANATION</span>
+        <span className="text-label flex flex-1">amount</span>
+        <span className="text-label flex-2">details</span>
       </div>
-      <ul className="flex flex-column flex-1">
+      <ul className="flex column flex-1">
         {slashedTokens.map(renderSlashedTokenItem)}
       </ul>
     </div>
@@ -18,20 +18,21 @@ const SlashedTokensList = ({ slashedTokens }) => {
 
 const renderSlashedTokenItem = (item) => <SlashedTokeItem key={item.id} {...item} />
 
-const SlashedTokeItem = React.memo(({ amount, date, typeOfPunishment, groupPublicKey }) => (
-  <li className="flex flex-row flex-1" >
-    <div className="text-big text-dark-red flex-1">
-      - {amount && `${displayAmount(amount)}`} KEEP
+const SlashedTokeItem = React.memo(({ amount, date, event, groupPublicKey }) => (
+  <li className="flex row flex-1" >
+    <div className="text-big flex-1">
+      <span className="text-error">{amount && `-${displayAmount(amount)} `}</span>
+      <span className="text-grey-40">KEEP</span>
     </div>
     <div className="details flex-2">
-      <div className="text-big text-darker-grey">
-        Group <AddressShortcut address={groupPublicKey} classNames="text-big text-darker-grey" />&nbsp;
-        {typeOfPunishment === 0 ?
-          'was selected to do work and not enough members participated.' :
-          'key was leaked. Private key was published outside of the members of the signing group.'
+      <div className="text-big text-grey-70">
+        Group <AddressShortcut address={groupPublicKey} classNames="text-big text-grey-70" />&nbsp;
+        {event === 'UnauthorizedSigningReported' ?
+          'key was leaked. Private key was published outside of the members of the signing group.' :
+          'was selected to do work and not enough members participated.'
         }
       </div>
-      <div className="text-small text-grey">
+      <div className="text-small text-grey-50">
         {formatDate(date)}
       </div>
     </div>
