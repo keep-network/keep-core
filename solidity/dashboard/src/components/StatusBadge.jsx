7ae78ce500f265bdf60d387c0913d914258a1cc2
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-03-27 10:06:44 +0100

    Use a span tag instead of div in the StatusBadge

--- solidity/dashboard/src/components/StatusBadge.jsx
+++ solidity/dashboard/src/components/StatusBadge.jsx
@@ -10,12 +10,12 @@ const badgeStyle = { padding: '0.1rem 0.5rem', borderRadius: '100px' }
 
 const StatusBadge = ({ status, text, className }) => {
   return (
-    <div
+    <span
       className={`${status.textClassName} ${status.bgClassName} text-label text-normal ${className}`}
       style={badgeStyle}
     >
       {text}
-    </div>
+    </span>
   )
 }
 
