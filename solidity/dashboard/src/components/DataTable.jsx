22b98b9d1f71807bd5994d362f320f29fdf6c8b8
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-03-27 10:04:05 +0100

    Create a responsive datatable component

---
+++ solidity/dashboard/src/components/DataTable.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export class DataTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      headers: [],
+      fields: [],
+    }
+  }
+
+  componentDidMount() {
+    this.initializeDataTable()
+  }
+
+  initializeDataTable = () => {
+    const headers = []
+    const fields = []
+    React.Children.forEach(this.props.children, (children) => {
+      headers.push(children.props.header)
+      fields.push( { fieldName: children.props.field, renderContent: children.props.renderContent })
+    })
+    this.setState({ headers, fields })
+  }
+
+  renderItemRow = (item) => {
+    const columns = this.state.fields.map((field, index) => {
+      return (
+        <td key="1">
+          <span className="responsive-header">{this.state.headers[index]}</span>
+          {field.renderContent ?
+            field.renderContent(item) :
+            item[field.fieldName]
+          }
+        </td>
+      )
+    })
+
+    return (
+      <tr key={item.id}>
+        {columns}
+      </tr>
+    )
+  }
+
+  renderHeader = (header) => (
+    <th key={header}>
+      {header}
+    </th>
+  )
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            {this.state.headers.map(this.renderHeader)}
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.data.map(this.renderItemRow)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export const Column = ({ header, field, renderContent }) => null
