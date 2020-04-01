1f1879441919b3ebca9dcf9dcc1f9f1a12978a3d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-01 13:04:04 +0200

    Add subproviders to route all other requests

--- solidity/dashboard/src/connectors/trezor.js
+++ solidity/dashboard/src/connectors/trezor.js
@@ -1,5 +1,7 @@
 import TrezorConnect from 'trezor-connect'
 import Web3ProviderEngine from 'web3-provider-engine'
+import WebsocketSubprovider from 'web3-provider-engine/subproviders/websocket'
+import CacheSubprovider from 'web3-provider-engine/subproviders/cache'
 import { TrezorSubprovider } from '@0x/subproviders/lib/src/subproviders/trezor'
 import { RPCSubprovider } from '@0x/subproviders/lib/src/subproviders/rpc_subprovider'
 
@@ -9,22 +11,25 @@ export class TrezorProvider extends Web3ProviderEngine {
     manifestAppUrl,
   ) {
     super()
-    TrezorConnect.manifest({
-      email: manifestEmail,
-      appUrl: manifestAppUrl,
+    TrezorConnect.init({
+      lazyLoad: true, // this param will prevent iframe injection until TrezorConnect.method will be called
+      manifest: {
+        email: manifestEmail,
+        appUrl: manifestAppUrl,
+      },
+      popup: true,
+      debug: true,
     })
     this.addProvider(
       new TrezorSubprovider({
         trezorConnectClientApi: TrezorConnect,
-        networkId: 1101,
-
+        networkId: 1,
       })
     )
+    this.addProvider(new WebsocketSubprovider({ rpcUrl: 'ws://localhost:8545', debug: true }))
+    this.addProvider(new RPCSubprovider('http://localhost:8545'))
+    this.addProvider(new CacheSubprovider())
 
-    this.addProvider(new RPCSubprovider('localhost:8545'))
-
-    this.start(() => {
-      console.log('callback')
-    })
+    this.start()
   }
 }
