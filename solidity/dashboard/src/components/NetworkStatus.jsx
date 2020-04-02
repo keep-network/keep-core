2728db14bf88bc491cf30d077b36ab027ddf7f51
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-02 17:53:28 +0200

    Fix network status indicator

--- solidity/dashboard/src/components/NetworkStatus.jsx
+++ solidity/dashboard/src/components/NetworkStatus.jsx
@@ -1,13 +1,21 @@
 import React, { useContext } from 'react'
 import { Web3Context } from './WithWeb3Context'
 
-export const NetworkStatus = (props) => {
-  const { networkType, error } = useContext(Web3Context)
+export const NetworkStatus = () => {
+  const {
+    networkType,
+    provider,
+    error,
+  } = useContext(Web3Context)
 
   return (
     <div className="network-status flex row center">
-      <div className={`network-indicator ${!error ? 'connected' : 'error' }`} />
-      <span className="text-label">{error ? 'wrong network' : `connected: ${networkType}`}</span>
+      <div className={`network-indicator ${!error && provider !== null ? 'connected' : 'error' }`} />
+      <span className="text-label">
+        {!error && provider === null && 'not connected' }
+        {!error && provider !== null && `connected: ${networkType}`}
+        {error && provider !== null && `wrong network`}
+      </span>
     </div>
   )
 }
