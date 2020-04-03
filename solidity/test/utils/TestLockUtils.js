298e25d330525325315cf7b96663fcd892ea892b
Promethea Raschke <promethea.raschke@keep.network>
2020-04-03 15:07:04 +0100

    Make LockUtils tests more comprehensive

--- solidity/test/utils/TestLockUtils.js
+++ solidity/test/utils/TestLockUtils.js
@@ -42,10 +42,17 @@ contract('LockUtils', (accounts) => {
   it("overwrites existing locks", async () => {
     await locks.publicSetLock(alice, 2);
     await locks.publicSetLock(bob, 2);
+    let creatorsPre = await locks.publicEnumerateCreators();
+    expect(creatorsPre[0]).to.equal(alice);
+    expect(creatorsPre[1]).to.equal(bob);
     expect(await locks.publicGetLockTime(alice)).to.eq.BN(2);
     expect(await locks.publicGetLockTime(bob)).to.eq.BN(2);
+
     await locks.publicSetLock(alice, 1);
     await locks.publicSetLock(bob, 3);
+    let creatorsPost = await locks.publicEnumerateCreators();
+    expect(creatorsPost[0]).to.equal(alice);
+    expect(creatorsPost[1]).to.equal(bob);
     expect(await locks.publicGetLockTime(alice)).to.eq.BN(1);
     expect(await locks.publicGetLockTime(bob)).to.eq.BN(3);
   })
@@ -55,10 +62,32 @@ contract('LockUtils', (accounts) => {
     await locks.publicSetLock(bob, 2);
     await locks.publicSetLock(carol, 3);
     let creatorsPre = await locks.publicEnumerateCreators();
+    expect(creatorsPre[0]).to.equal(alice);
     expect(creatorsPre[1]).to.equal(bob);
+    expect(creatorsPre[2]).to.equal(carol);
 
     await locks.publicReleaseLock(bob);
     let creatorsPost = await locks.publicEnumerateCreators();
+    expect(creatorsPost[0]).to.equal(alice);
     expect(creatorsPost[1]).to.equal(carol);
+
+    await locks.publicSetLock(bob, 2);
+    let creatorsDoublePost = await locks.publicEnumerateCreators();
+    expect(creatorsDoublePost[0]).to.equal(alice);
+    expect(creatorsDoublePost[1]).to.equal(carol);
+    expect(creatorsDoublePost[2]).to.equal(bob);
+
+    await locks.publicReleaseLock(alice);
+    let creatorsTriplePost = await locks.publicEnumerateCreators();
+    expect(creatorsTriplePost[0]).to.equal(bob);
+    expect(creatorsTriplePost[1]).to.equal(carol);
+
+    await locks.publicReleaseLock(bob);
+    let creatorsQuadPost = await locks.publicEnumerateCreators();
+    expect(creatorsQuadPost[0]).to.equal(carol);
+
+    await locks.publicReleaseLock(carol);
+    let creatorsPentaPost = await locks.publicEnumerateCreators();
+    expect(creatorsPentaPost.length).to.equal(0);
   })
 })
