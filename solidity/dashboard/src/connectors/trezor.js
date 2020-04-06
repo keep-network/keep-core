198bbb3427a224c942206aded3c6a8435cdb0330
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-06 11:36:24 +0200

    TrezorProvider extends from abstract connector

--- solidity/dashboard/src/connectors/trezor.js
+++ solidity/dashboard/src/connectors/trezor.js
@@ -1,11 +1,11 @@
 import TrezorConnect from 'trezor-connect'
-import Web3ProviderEngine from 'web3-provider-engine'
-import WebsocketSubprovider from 'web3-provider-engine/subproviders/websocket'
-import CacheSubprovider from 'web3-provider-engine/subproviders/cache'
 import { TrezorSubprovider } from '@0x/subproviders/lib/src/subproviders/trezor'
-import { RPCSubprovider } from '@0x/subproviders/lib/src/subproviders/rpc_subprovider'
+import { AbstractHardwareWalletConnector } from './abstract'
 
-export class TrezorProvider extends Web3ProviderEngine {
+export class TrezorProvider extends AbstractHardwareWalletConnector {
+  manifestEmail
+  manifestAppUrl
+
   constructor(
     manifestEmail,
     manifestAppUrl,
@@ -18,18 +18,12 @@ export class TrezorProvider extends Web3ProviderEngine {
         appUrl: manifestAppUrl,
       },
       popup: true,
-      debug: true,
+      // debug: true,
     })
-    this.addProvider(
+    this.setProvider(
       new TrezorSubprovider({
         trezorConnectClientApi: TrezorConnect,
         networkId: 1,
-      })
-    )
-    this.addProvider(new WebsocketSubprovider({ rpcUrl: 'ws://localhost:8545', debug: true }))
-    this.addProvider(new RPCSubprovider('http://localhost:8545'))
-    this.addProvider(new CacheSubprovider())
-
-    this.start()
+      }))
   }
 }
