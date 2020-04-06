9dc0681f2ca851c9d61394a21d318b53c763c9a4
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-06 11:37:54 +0200

    Cleanup in  the web3 context provider

--- solidity/dashboard/src/components/Web3ContextProvider.jsx
+++ solidity/dashboard/src/components/Web3ContextProvider.jsx
@@ -11,7 +11,6 @@ export default class Web3ContextProvider extends React.Component {
     constructor(props) {
       super(props)
       this.state = {
-        showModal: true,
         provider: null,
         web3: null,
         isFetching: false,
@@ -26,13 +25,13 @@ export default class Web3ContextProvider extends React.Component {
       }
     }
 
-    showModal = () => this.setState({ showModal: true })
-    closeModal = () => this.setState({ showModal: false })
-
     getWeb3 = (providerName) => {
       switch (providerName) {
         case 'TREZOR': {
-          return new Web3(new TrezorProvider('test@email.com', 'https://keep.network/'))
+          return new Web3(new TrezorProvider(
+            'test@email.com',
+            'https://keep.network/',
+          ))
         }
         case 'METAMASK': {
           if (window.ethereum || window.web3) {
@@ -51,53 +50,39 @@ export default class Web3ContextProvider extends React.Component {
       }
     }
 
-    connectAppWithWallet = (providerName) => {
+    connectAppWithWallet = async (providerName) => {
       let web3
+      let account
+      this.setState({ isFetching: true })
       try {
         web3 = this.getWeb3(providerName)
+        account = (await web3.currentProvider.enable())[0]
       } catch (error) {
-        this.setState({ providerError: error.message })
+        this.setState({ providerError: error.message, isFetching: false })
         this.context.showMessage({ type: messageType.ERROR, title: error.message })
         return
       }
-      this.setState({ web3, provider: providerName, showModal: false }, this.setData)
-    }
-
-    initialize = async () => {
-      const web3 = this.getWeb3()
-      if (!web3) {
-        return
-      }
-      this.setState({ web3 }, this.setData)
+      this.setState({
+        web3,
+        provider: providerName,
+        yourAddress: account,
+      }, this.setData)
     }
 
     setData = async () => {
-      const { web3 } = this.state
-      this.setState({ isFetching: true })
-      const accounts = await web3.eth.getAccounts()
-      console.log('accounts', accounts)
-      this.connectAppWithAccount(!accounts || accounts.length === 0, accounts)
       this.initializeContracts()
       this.state.web3.eth.currentProvider.on('accountsChanged', this.accountHasBeenChanged)
     }
 
-    connectAppWithAccount = async (withInfoMessage = true, accounts) => {
+    connectAppWithAccount = async () => {
       const { web3 } = this.state
       this.setState({ isFetching: true })
-      withInfoMessage && this.context.showMessage({ type: messageType.INFO, title: 'Please check web3 provider' })
-
       try {
-        const [account] = accounts
-        console.log('account', account)
-        this.setState({
-          yourAddress: account,
-          networkType: await web3.eth.net.getNetworkType(),
-          isFetching: false,
-        })
+        const [yourAddress] = await web3.currentProvider.enable()
+        this.setState({ yourAddress, isFetching: false })
       } catch (error) {
-        console.log('error', error)
-        this.context.showMessage({ type: 'error', title: error.message })
-        this.setState({ isFetching: false })
+        this.setState({ providerError: error.message, isFetching: false })
+        this.context.showMessage({ type: messageType.ERROR, title: error.message })
       }
     }
 
@@ -107,13 +92,13 @@ export default class Web3ContextProvider extends React.Component {
         const contracts = await getContracts(web3)
         this.setState({
           ...contracts,
-          defaultContract: contracts.stakingContract,
           utils: web3.utils,
           eth: web3.eth,
+          isFetching: false,
         })
       } catch (error) {
-        console.log('error contracts', error)
         this.setState({
+          isFetching: false,
           error: 'Please select correct network',
         })
       }
@@ -142,12 +127,10 @@ export default class Web3ContextProvider extends React.Component {
             ...restState,
             connectAppWithAccount: this.connectAppWithAccount,
             connectAppWithWallet: this.connectAppWithWallet,
-            showConnectWalletModal: this.showModal,
           }}
         >
           {this.props.children}
         </Web3Context.Provider>
-
       )
     }
 }
