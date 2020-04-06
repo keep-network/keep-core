dd5e3bb355773977aa780da4c7d3aeadd104bb01
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-06 11:36:01 +0200

    Create a abstract hardware wallet connector

---
+++ solidity/dashboard/src/connectors/abstract.js
@@ -0,0 +1,35 @@
+import Web3ProviderEngine from 'web3-provider-engine'
+import WebsocketSubprovider from 'web3-provider-engine/subproviders/websocket'
+import CacheSubprovider from 'web3-provider-engine/subproviders/cache'
+import { RPCSubprovider } from '@0x/subproviders/lib/src/subproviders/rpc_subprovider'
+
+export class AbstractHardwareWalletConnector extends Web3ProviderEngine {
+    provider
+
+    constructor() {
+      super()
+    }
+
+    enable = async () => {
+      this.addProvider(this.provider)
+      this.addProvider(new CacheSubprovider())
+      this.addProvider(new WebsocketSubprovider({ rpcUrl: 'ws://localhost:8545', debug: false }))
+      this.addProvider(new RPCSubprovider('http://localhost:8545'))
+
+      this.start()
+
+      return await this.getAccounts()
+    }
+
+    getAccounts = async () => {
+      return await this.provider.getAccountsAsync()
+    }
+
+    setProvider = (provider) => {
+      this.provider = provider
+    }
+
+    getProvider = () => {
+      return this.provider
+    }
+}
