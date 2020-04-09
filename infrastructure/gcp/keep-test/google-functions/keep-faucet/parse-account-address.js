ae91a2dc778ce92eceb55b648f78147fbd186847
Sloan Thompson <sloan.thompson@thesis.co>
2020-04-09 09:57:49 -0400

    Declare the things
    It's been so long since I've done this I don't remember why this ended
    up this way.  Anywho here we actually declare the variables being used
    in the script.

--- infrastructure/gcp/keep-test/google-functions/keep-faucet/parse-account-address.js
+++ infrastructure/gcp/keep-test/google-functions/keep-faucet/parse-account-address.js
@@ -2,19 +2,20 @@
 const url = require('url')
 
 exports.parseAccountAddress = (request, response) => {
-  requestUrl = url.parse(request.url, true)
+  const requestUrl = url.parse(request.url, true)
+  const account = requestUrl.query.account
   if (requestUrl.query.account) {
-    return (account = requestUrl.query.account)
+    return account
   } else if (!/^(0x)?[0-9a-f]{40}$/i.test(account)) {
     // check if it has the basic requirements of an account
     // double thanks to the Ethereum folks for figuring this regex out already
     return response.send(
-      'Improperly formatted account account, please try a valid one.',
+      'Improperly formatted account account address, please try a valid one.\n',
     )
   } else {
     console.log('No account set in HTTP request.')
     return response.send(
-      'No account account set, please set an account with ?account=<accountAddress>',
+      'No account account address set, please set an account with ?account=<accountAddress>\n',
     )
   }
 }
