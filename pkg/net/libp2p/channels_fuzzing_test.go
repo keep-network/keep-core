58dc208a85d27e50b0032454f3b698b6f7c9f68e
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-04-14 13:38:14 +0200

    Fuzz tests for channels
    Covered broadcast and unicast channels round
    trips with random messages.

---
+++ pkg/net/libp2p/channels_fuzzing_test.go
@@ -0,0 +1,93 @@
+package libp2p
+
+import (
+	"context"
+	"testing"
+
+	fuzz "github.com/google/gofuzz"
+	"github.com/keep-network/keep-core/pkg/net"
+)
+
+func TestBroadcastChannelFuzzing(t *testing.T) {
+	ctx := context.Background()
+
+	withNetwork(ctx, t, func(
+		_ *identity,
+		_ *identity,
+		provider net.Provider,
+		_ net.Provider,
+	) {
+		channel, err := provider.BroadcastChannelFor("test-channel")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = channel.RegisterUnmarshaler(func() net.TaggedUnmarshaler {
+			return &fuzzingMessage{}
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		for i := 0; i < 100000; i++ {
+			var message fuzzingMessage
+
+			f := fuzz.New().NilChance(0.1).NumElements(0, 1024)
+			f.Fuzz(&message)
+
+			if err := channel.Send(ctx, &message); err != nil {
+				t.Fatal(err)
+			}
+		}
+	})
+}
+
+func TestUnicastChannelFuzzing(t *testing.T) {
+	ctx := context.Background()
+
+	withNetwork(ctx, t, func(
+		_ *identity,
+		identity2 *identity,
+		provider1 net.Provider,
+		provider2 net.Provider,
+	) {
+		channel, err := provider1.UnicastChannelWith(identity2.id)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		provider2.OnUnicastChannelOpened(func(channel net.UnicastChannel) {
+			channel.SetUnmarshaler(func() net.TaggedUnmarshaler {
+				return &fuzzingMessage{}
+			})
+		})
+
+		for i := 0; i < 100000; i++ {
+			var message fuzzingMessage
+
+			f := fuzz.New().NilChance(0.1).NumElements(0, 1024)
+			f.Fuzz(&message)
+
+			if err := channel.Send(&message); err != nil {
+				t.Fatal(err)
+			}
+		}
+	})
+}
+
+type fuzzingMessage struct {
+	Bytes []byte
+}
+
+func (m *fuzzingMessage) Type() string {
+	return "test/fuzzing"
+}
+
+func (m *fuzzingMessage) Marshal() ([]byte, error) {
+	return m.Bytes, nil
+}
+
+func (m *fuzzingMessage) Unmarshal(bytes []byte) error {
+	m.Bytes = bytes
+	return nil
+}
