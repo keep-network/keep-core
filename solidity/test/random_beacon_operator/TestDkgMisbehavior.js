6e50c0e5ad002529e9ce92806c68e392790e3ceb
Piotr Dyraga <piotr.dyraga@keep.network>
2020-04-17 02:31:44 +0200

    Reverted changes in TestDkgMisbehavior
    We do not need more than 5 members in a group there.

--- solidity/test/random_beacon_operator/TestDkgMisbehavior.js
+++ solidity/test/random_beacon_operator/TestDkgMisbehavior.js
@@ -1,15 +1,15 @@
 const blsData = require("../helpers/data")
-const {contract, web3, accounts} = require("@openzeppelin/test-environment")
-const {time} = require("@openzeppelin/test-helpers")
+const { contract, web3, accounts } = require("@openzeppelin/test-environment")
+const { time } = require("@openzeppelin/test-helpers")
 var assert = require('chai').assert
 const initContracts = require('../helpers/initContracts')
 const sign = require('../helpers/signature')
 const stakeDelegate = require('../helpers/stakeDelegate')
 const packTicket = require('../helpers/packTicket')
 const generateTickets = require('../helpers/generateTickets')
-const {createSnapshot, restoreSnapshot} = require("../helpers/snapshot.js")
+const { createSnapshot, restoreSnapshot } = require("../helpers/snapshot.js")
 
-describe('KeepRandomBeaconOperator/DkgMisbehavior', function() {
+describe('KeepRandomBeaconOperator/DkgMisbehavior', function () {
   let token, stakingContract, operatorContract,
     owner = accounts[0],
     operator1 = accounts[1],
@@ -26,7 +26,7 @@ describe('KeepRandomBeaconOperator/DkgMisbehavior', function() {
     resultHash = web3.utils.soliditySha3(groupPubKey, misbehaved)
 
   before(async () => {
-    
+
     let contracts = await initContracts(
       contract.fromArtifact('KeepToken'),
       contract.fromArtifact('TokenStaking'),
@@ -38,25 +38,21 @@ describe('KeepRandomBeaconOperator/DkgMisbehavior', function() {
     token = contracts.token
     stakingContract = contracts.stakingContract
     operatorContract = contracts.operatorContract
-    operatorContract.setGroupSize(7, {from: owner})
-    operatorContract.setGroupThreshold(4, {from: owner})
-    
+    operatorContract.setGroupSize(5, { from: owner })
+    operatorContract.setGroupThreshold(3, { from: owner })
+
     let minimumStake = await stakingContract.minimumStake()
-    await stakeDelegate(stakingContract, token, owner, operator1, owner, authorizer, minimumStake, {from: owner})
-    await stakeDelegate(stakingContract, token, owner, operator2, owner, authorizer, minimumStake, {from: owner})
-    await stakeDelegate(stakingContract, token, owner, operator3, owner, authorizer, minimumStake, {from: owner})
-    await stakeDelegate(stakingContract, token, owner, operator4, owner, authorizer, minimumStake, {from: owner})
-    await stakeDelegate(stakingContract, token, owner, operator5, owner, authorizer, minimumStake, {from: owner})
-    await stakeDelegate(stakingContract, token, owner, operator6, owner, authorizer, minimumStake, {from: owner})
-    await stakeDelegate(stakingContract, token, owner, operator7, owner, authorizer, minimumStake, {from: owner})
-    
-    await stakingContract.authorizeOperatorContract(operator1, operatorContract.address, {from: authorizer})
-    await stakingContract.authorizeOperatorContract(operator2, operatorContract.address, {from: authorizer})
-    await stakingContract.authorizeOperatorContract(operator3, operatorContract.address, {from: authorizer})
-    await stakingContract.authorizeOperatorContract(operator4, operatorContract.address, {from: authorizer})
-    await stakingContract.authorizeOperatorContract(operator5, operatorContract.address, {from: authorizer})
-    await stakingContract.authorizeOperatorContract(operator6, operatorContract.address, {from: authorizer})
-    await stakingContract.authorizeOperatorContract(operator7, operatorContract.address, {from: authorizer})
+    await stakeDelegate(stakingContract, token, owner, operator1, owner, authorizer, minimumStake, { from: owner })
+    await stakeDelegate(stakingContract, token, owner, operator2, owner, authorizer, minimumStake, { from: owner })
+    await stakeDelegate(stakingContract, token, owner, operator3, owner, authorizer, minimumStake, { from: owner })
+    await stakeDelegate(stakingContract, token, owner, operator4, owner, authorizer, minimumStake, { from: owner })
+    await stakeDelegate(stakingContract, token, owner, operator5, owner, authorizer, minimumStake, { from: owner })
+
+    await stakingContract.authorizeOperatorContract(operator1, operatorContract.address, { from: authorizer })
+    await stakingContract.authorizeOperatorContract(operator2, operatorContract.address, { from: authorizer })
+    await stakingContract.authorizeOperatorContract(operator3, operatorContract.address, { from: authorizer })
+    await stakingContract.authorizeOperatorContract(operator4, operatorContract.address, { from: authorizer })
+    await stakingContract.authorizeOperatorContract(operator5, operatorContract.address, { from: authorizer })
 
     time.increase((await stakingContract.initializationPeriod()).addn(1));
 
@@ -66,42 +62,30 @@ describe('KeepRandomBeaconOperator/DkgMisbehavior', function() {
     let tickets3 = generateTickets(groupSelectionRelayEntry, operator3, 1)
     let tickets4 = generateTickets(groupSelectionRelayEntry, operator4, 1)
     let tickets5 = generateTickets(groupSelectionRelayEntry, operator5, 1)
-    let tickets6 = generateTickets(groupSelectionRelayEntry, operator6, 1)
-    let tickets7 = generateTickets(groupSelectionRelayEntry, operator7, 1)
 
     await operatorContract.submitTicket(
       packTicket(tickets1[0].valueHex, tickets1[0].virtualStakerIndex, operator1),
-      {from: operator1}
+      { from: operator1 }
     )
 
     await operatorContract.submitTicket(
       packTicket(tickets2[0].valueHex, tickets2[0].virtualStakerIndex, operator2),
-      {from: operator2}
+      { from: operator2 }
     )
 
     await operatorContract.submitTicket(
       packTicket(tickets3[0].valueHex, tickets3[0].virtualStakerIndex, operator3),
-      {from: operator3}
+      { from: operator3 }
     )
 
     await operatorContract.submitTicket(
       packTicket(tickets4[0].valueHex, tickets4[0].virtualStakerIndex, operator4),
-      {from: operator4}
+      { from: operator4 }
     )
 
     await operatorContract.submitTicket(
       packTicket(tickets5[0].valueHex, tickets5[0].virtualStakerIndex, operator5),
-      {from: operator5}
-    )
-
-    await operatorContract.submitTicket(
-      packTicket(tickets6[0].valueHex, tickets6[0].virtualStakerIndex, operator6),
-      {from: operator6}
-    )
-
-    await operatorContract.submitTicket(
-      packTicket(tickets7[0].valueHex, tickets7[0].virtualStakerIndex, operator7),
-      {from: operator7}
+      { from: operator5 }
     )
 
     let ticketSubmissionStartBlock = await operatorContract.getTicketSubmissionStartBlock()
@@ -116,9 +100,9 @@ describe('KeepRandomBeaconOperator/DkgMisbehavior', function() {
     signingMemberIndices = []
     signatures = undefined
 
-    for(let i = 0; i < selectedParticipants.length; i++) {
+    for (let i = 0; i < selectedParticipants.length; i++) {
       let signature = await sign(resultHash, selectedParticipants[i])
-      signingMemberIndices.push(i+1)
+      signingMemberIndices.push(i + 1)
       if (signatures == undefined) signatures = signature
       else signatures += signature.slice(2, signature.length)
     }
@@ -133,14 +117,12 @@ describe('KeepRandomBeaconOperator/DkgMisbehavior', function() {
   })
 
   it("should be able to save group members based on misbehaved data", async () => {
-    await operatorContract.submitDkgResult(1, groupPubKey, misbehaved, signatures, signingMemberIndices, {from: selectedParticipants[0]})
+    await operatorContract.submitDkgResult(1, groupPubKey, misbehaved, signatures, signingMemberIndices, { from: selectedParticipants[0] })
     let registeredMembers = await operatorContract.getGroupMembers(groupPubKey)
     assert.isTrue(registeredMembers.indexOf(selectedParticipants[0]) != -1, "Member should be registered")
     assert.isTrue(registeredMembers.indexOf(selectedParticipants[1]) != -1, "Member should be registered")
     assert.isTrue(registeredMembers.indexOf(selectedParticipants[2]) == -1, "Member should not be registered")
     assert.isTrue(registeredMembers.indexOf(selectedParticipants[3]) != -1, "Member should be registered")
     assert.isTrue(registeredMembers.indexOf(selectedParticipants[4]) == -1, "Member should not be registered")
-    assert.isTrue(registeredMembers.indexOf(selectedParticipants[5]) != -1, "Member should be registered")
-    assert.isTrue(registeredMembers.indexOf(selectedParticipants[6]) != -1, "Member should be registered")
   })
 })
