c33c5373ee71260d4aa4e49a878c3ca56b769d3b
Sloan Thompson <sloan.thompson@thesis.co>
2020-04-21 10:48:54 -0400

    Set max instances
    This option was left out when I moved to deploying from a script.  This
    will increase the time it would take a for a script to drain token
    supply.

--- infrastructure/gcp/keep-test/google-functions/keep-faucet/deploy-faucet.sh
+++ infrastructure/gcp/keep-test/google-functions/keep-faucet/deploy-faucet.sh
@@ -2,4 +2,5 @@ gcloud functions deploy keep-faucet-ropsten \
   --trigger-http \
   --runtime nodejs10 \
   --entry-point issueGrant \
+  --max-instances 5 \
   --set-env-vars KEEP_CONTRACT_OWNER_ADDRESS=$KEEP_CONTRACT_OWNER_ADDRESS,KEEP_CONTRACT_OWNER_PRIVATE_KEY=$KEEP_CONTRACT_OWNER_PRIVATE_KEY,ETHEREUM_HOST=$ETHEREUM_HOST,ETHEREUM_NETWORK_ID=$ETHEREUM_NETWORK_ID
