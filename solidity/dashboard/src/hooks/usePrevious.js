64bc3cec1de90cb55bbace14b5d4e6bb91f1854b
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-22 15:20:54 +0200

    Create a custom usePrevious hook
    A custom hook that uses the useRef hook internally for storing the
    previous value. We can use combine this hook with the useEffect hook and
    use it as componenDidUpdate.

---
+++ solidity/dashboard/src/hooks/usePrevious.js
@@ -0,0 +1,10 @@
+import { useRef, useEffect } from 'react'
+
+export const usePrevious = (value) => {
+    const valueRef = useRef(value)
+    useEffect(() => {
+        valueRef.current = value;
+    }, [value]);
+
+    return valueRef.current;
+}
