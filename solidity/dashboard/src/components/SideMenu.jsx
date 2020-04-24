e2faec7cdde548114b645aed9299fe5726b1c1c8
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-24 12:58:36 +0200

    Update NavLink component
    Added page icon and oarrow right icon.

--- solidity/dashboard/src/components/SideMenu.jsx
+++ solidity/dashboard/src/components/SideMenu.jsx
@@ -5,6 +5,7 @@ import { Web3Context } from './WithWeb3Context'
 import { ContractsDataContext } from './ContractsDataContextProvider'
 import AddressShortcut from './AddressShortcut'
 import { NetworkStatus } from './NetworkStatus'
+import * as Icons from './Icons'
 
 export const SideMenuContext = React.createContext({})
 
@@ -30,11 +31,11 @@ export const SideMenu = (props) => {
   return (
     <nav className={`${ isOpen ? 'active ' : '' }side-menu`}>
       <ul>
-        <NavLink exact to="/tokens" label='tokens'/>
-        <NavLink exact to="/rewards" label='rewards'/>
-        <NavLink exact to="/operations" label='operations'/>
-        <NavLink exact to="/authorizer" label='authorizer'/>
-        { isKeepTokenContractDeployer && <NavLink exact to="/create-token-grants" label='create token grants'/> }
+        <NavLink exact to="/tokens" label='tokens' icon={<Icons.KeepToken />} />
+        <NavLink exact to="/operations" label='operations' icon={<Icons.Operations />}/>
+        <NavLink exact to="/rewards" label='rewards' icon={<Icons.Rewards />}/>
+        <NavLink exact to="/authorizer" label='authorizer' icon={<Icons.Authorizer />}/>
+        { isKeepTokenContractDeployer && <NavLink exact to="/create-token-grants" label='token grants'/> }
         <Web3Status />
         <div className='account-address'>
           <h5 className="text-grey-50">
@@ -48,7 +49,7 @@ export const SideMenu = (props) => {
   )
 }
 
-const NavLink = ({ label, to, exact }) => {
+const NavLink = ({ label, to, exact, icon }) => {
   const match = useRouteMatch({
     path: to,
     exact,
@@ -57,7 +58,9 @@ const NavLink = ({ label, to, exact }) => {
   return (
     <Link to={to}>
       <li className={`text-label ${match ? 'active-page-link' : ''}`}>
-        {label}
+        {icon}
+        <span className="ml-1">{label}</span>
+        <Icons.ArrowRight />
       </li>
     </Link>
   )
