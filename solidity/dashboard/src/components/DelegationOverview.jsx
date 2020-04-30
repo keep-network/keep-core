97728b60298ed8fb826250fabee349a5e8f50686
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-04-30 20:00:02 +0200

    Fix eslint errors

--- solidity/dashboard/src/components/DelegationOverview.jsx
+++ solidity/dashboard/src/components/DelegationOverview.jsx
@@ -1,90 +1,95 @@
-import React, { useMemo } from 'react'
-import Undelegations from '../components/Undelegations'
-import DelegatedTokensTable from '../components/DelegatedTokensTable'
-import StatusBadge, { BADGE_STATUS } from './StatusBadge'
-import { useTokensPageContext } from '../contexts/TokensPageContext'
-import { formatDate } from '../utils/general.utils'
-import moment from 'moment'
+import React, { useMemo } from "react"
+import Undelegations from "../components/Undelegations"
+import DelegatedTokensTable from "../components/DelegatedTokensTable"
+import StatusBadge, { BADGE_STATUS } from "./StatusBadge"
+import { useTokensPageContext } from "../contexts/TokensPageContext"
+import { formatDate } from "../utils/general.utils"
+import moment from "moment"
 
 const DelegationOverview = () => {
-    const { 
-        undelegations,
-        delegations,
-        refreshData,
-        tokensContext,
-        selectedGrant,
-    } = useTokensPageContext()
+  const {
+    undelegations,
+    delegations,
+    refreshData,
+    tokensContext,
+    selectedGrant,
+  } = useTokensPageContext()
 
-    const ownedDelegations = useMemo(() => {
-        return delegations.filter(delegation => !delegation.gratnId)
-    }, [delegations])
+  const ownedDelegations = useMemo(() => {
+    return delegations.filter((delegation) => !delegation.gratnId)
+  }, [delegations])
 
-    const ownedUndelegations = useMemo(() => {
-        return undelegations.filter(undelegation => !undelegation.gratnId)
-    }, undelegations)
+  const ownedUndelegations = useMemo(() => {
+    return undelegations.filter((undelegation) => !undelegation.gratnId)
+  }, undelegations)
 
-    const grantDelegations = useMemo(() => {
-        return delegations.filter(delegation => delegation.gratnId === selectedGrant.id)
-    }, [delegations, selectedGrant])
-
-    const grantUndelegations = useMemo(() => {
-        return undelegations.filter(undelegation => undelegation.gratnId === selectedGrant.id)
-    }, [undelegations, selectedGrant])
-
-    const getDelegations = () => {
-        if (tokensContext === 'granted') {
-            return grantDelegations
-        }
-
-        return ownedDelegations
-    }
-
-    const getUndelegations = () => {
-        if (tokensContext === 'granted') {
-            return grantUndelegations
-        }
-
-        return ownedUndelegations
-    }
- 
-    return (
-        <section>
-            <div className="flex wrap self-center mt-3 mb-2">
-                <h2 className="text-grey-60">{`${tokensContext === 'granted' ? 'Grant ' : ''}Delegation Overview`}</h2>
-                {tokensContext === 'granted' && 
-                    <>
-                        <span className="flex self-center ml-2">
-                            <StatusBadge
-                                className="self-center"
-                                status={BADGE_STATUS.DISABLED}
-                                text="grant id"
-                            />
-                            <span className="self-center h4 text-grey-50 ml-1">
-                                {selectedGrant.id}
-                            </span>
-                        </span>
-                        <span className="flex self-center ml-2">
-                            <StatusBadge
-                                className="self-center"
-                                status={BADGE_STATUS.DISABLED}
-                                text="issued"
-                            />
-                            <span className="h4 text-grey-50 ml-1">
-                                {selectedGrant.start && formatDate(moment.unix(selectedGrant.start))}
-                            </span>
-                        </span>
-                    </>
-                }
-            </div>
-            <DelegatedTokensTable
-                delegatedTokens={getDelegations()}
-                cancelStakeSuccessCallback={refreshData}
-            />
-            <Undelegations
-                undelegations={getUndelegations()}
-            />
-        </section>
+  const grantDelegations = useMemo(() => {
+    return delegations.filter(
+      (delegation) => delegation.gratnId === selectedGrant.id
     )
+  }, [delegations, selectedGrant])
+
+  const grantUndelegations = useMemo(() => {
+    return undelegations.filter(
+      (undelegation) => undelegation.gratnId === selectedGrant.id
+    )
+  }, [undelegations, selectedGrant])
+
+  const getDelegations = () => {
+    if (tokensContext === "granted") {
+      return grantDelegations
+    }
+
+    return ownedDelegations
+  }
+
+  const getUndelegations = () => {
+    if (tokensContext === "granted") {
+      return grantUndelegations
+    }
+
+    return ownedUndelegations
+  }
+
+  return (
+    <section>
+      <div className="flex wrap self-center mt-3 mb-2">
+        <h2 className="text-grey-60">{`${
+          tokensContext === "granted" ? "Grant " : ""
+        }Delegation Overview`}</h2>
+        {tokensContext === "granted" && (
+          <>
+            <span className="flex self-center ml-2">
+              <StatusBadge
+                className="self-center"
+                status={BADGE_STATUS.DISABLED}
+                text="grant id"
+              />
+              <span className="self-center h4 text-grey-50 ml-1">
+                {selectedGrant.id}
+              </span>
+            </span>
+            <span className="flex self-center ml-2">
+              <StatusBadge
+                className="self-center"
+                status={BADGE_STATUS.DISABLED}
+                text="issued"
+              />
+              <span className="h4 text-grey-50 ml-1">
+                {selectedGrant.start &&
+                  formatDate(moment.unix(selectedGrant.start))}
+              </span>
+            </span>
+          </>
+        )}
+      </div>
+      <DelegatedTokensTable
+        delegatedTokens={getDelegations()}
+        cancelStakeSuccessCallback={refreshData}
+      />
+      <Undelegations undelegations={getUndelegations()} />
+    </section>
+  )
 }
 
 export default DelegationOverview
--- solidity/dashboard/src/components/TokensContextSwitcher.jsx
+++ solidity/dashboard/src/components/TokensContextSwitcher.jsx
@@ -1,65 +1,80 @@
-import React, { useCallback } from 'react'
-import * as Icons from './Icons'
-import Dropdown from './Dropdown'
-import SelectedGrantDropdown from './SelectedGrantDropdown'
-import { useTokensPageContext } from '../contexts/TokensPageContext'
-import { SET_TOKENS_CONTEXT, SET_SELECTED_GRANT } from '../reducers/tokens-page.reducer'
-import { displayAmount } from '../utils/general.utils'
+import React, { useCallback } from "react"
+import * as Icons from "./Icons"
+import Dropdown from "./Dropdown"
+import SelectedGrantDropdown from "./SelectedGrantDropdown"
+import { useTokensPageContext } from "../contexts/TokensPageContext"
+import {
+  SET_TOKENS_CONTEXT,
+  SET_SELECTED_GRANT,
+} from "../reducers/tokens-page.reducer"
+import { displayAmount } from "../utils/general.utils"
 
 const TokensContextSwitcher = (props) => {
-    const {
-        dispatch,
-        selectedGrant,  
-        tokensContext,
-        grants,
-        keepTokenBalance
-    } = useTokensPageContext()
+  const {
+    dispatch,
+    selectedGrant,
+    tokensContext,
+    grants,
+    keepTokenBalance,
+  } = useTokensPageContext()
 
-    const setTokensContext = useCallback((contextName) => {
-        dispatch({ type: SET_TOKENS_CONTEXT, payload: contextName })
-    }, [dispatch])
+  const setTokensContext = useCallback(
+    (contextName) => {
+      dispatch({ type: SET_TOKENS_CONTEXT, payload: contextName })
+    },
+    [dispatch]
+  )
 
-    const onSelectGrant = useCallback((grant) => {
-        dispatch({ type: SET_SELECTED_GRANT, payload: grant })
-    },[dispatch])
+  const onSelectGrant = useCallback(
+    (grant) => {
+      dispatch({ type: SET_SELECTED_GRANT, payload: grant })
+    },
+    [dispatch]
+  )
 
-    return (
-        <div className="tokens-context-switcher-wrapper">
-            <div
-                className={`grants ${tokensContext === 'granted' ? 'active' : 'inactive'}`}
-                onClick={() => setTokensContext('granted')}
-            >
-                <div className="flex row">
-                    <Icons.GrantContextIcon />
-                    <div className="ml-1">
-                        <h2 className="text-grey-70">Grants</h2>
-                        <h4 className="balance">{displayAmount(selectedGrant.availableToStake)}</h4>
-                    </div>
-                </div>
-                <div className="grants-dropdown">
-                    <Dropdown
-                        onSelect={onSelectGrant}
-                        options={grants}
-                        valuePropertyName='id'
-                        labelPropertyName='id'
-                        selectedItem={selectedGrant}
-                        labelPrefix='Grant ID'
-                        noItemSelectedText='Select Grant'
-                        label="Choose Grant"
-                        selectedItemComponent={<SelectedGrantDropdown grant={selectedGrant} />}
-                    />
-                </div>
-            </div>
-            <div
-                className={`owned ${tokensContext === 'owned' ? 'active' : 'inactive'}`}
-                onClick={() => setTokensContext('owned')}
-            >
-                <Icons.MoneyWalletOpen />
-                <h2 className="text-grey-70">Owned</h2>
-                <h4 className="balance">{displayAmount(keepTokenBalance)}</h4>
-            </div>
+  return (
+    <div className="tokens-context-switcher-wrapper">
+      <div
+        className={`grants ${
+          tokensContext === "granted" ? "active" : "inactive"
+        }`}
+        onClick={() => setTokensContext("granted")}
+      >
+        <div className="flex row">
+          <Icons.GrantContextIcon />
+          <div className="ml-1">
+            <h2 className="text-grey-70">Grants</h2>
+            <h4 className="balance">
+              {displayAmount(selectedGrant.availableToStake)}
+            </h4>
+          </div>
         </div>
-    )
+        <div className="grants-dropdown">
+          <Dropdown
+            onSelect={onSelectGrant}
+            options={grants}
+            valuePropertyName="id"
+            labelPropertyName="id"
+            selectedItem={selectedGrant}
+            labelPrefix="Grant ID"
+            noItemSelectedText="Select Grant"
+            label="Choose Grant"
+            selectedItemComponent={
+              <SelectedGrantDropdown grant={selectedGrant} />
+            }
+          />
+        </div>
+      </div>
+      <div
+        className={`owned ${tokensContext === "owned" ? "active" : "inactive"}`}
+        onClick={() => setTokensContext("owned")}
+      >
+        <Icons.MoneyWalletOpen />
+        <h2 className="text-grey-70">Owned</h2>
+        <h4 className="balance">{displayAmount(keepTokenBalance)}</h4>
+      </div>
+    </div>
+  )
 }
 
 export default TokensContextSwitcher
