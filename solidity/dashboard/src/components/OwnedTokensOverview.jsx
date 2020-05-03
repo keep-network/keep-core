6239a97ec763e39a396e0d9577aeefc5a5df16ca
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-05-03 13:38:56 +0200

    Display amount with metric suffix
    Use a TokenAmount component and display the amount of token with metric
    suffix. Eg. 10000000 -> 10M

--- solidity/dashboard/src/components/OwnedTokensOverview.jsx
+++ solidity/dashboard/src/components/OwnedTokensOverview.jsx
@@ -2,17 +2,22 @@ import React, { useMemo } from "react"
 import Tile from "./Tile"
 import { CircularProgressBars } from "./CircularProgressBar"
 import { add } from "../utils/arithmetics.utils"
-import { displayAmount } from "../utils/general.utils"
+import {
+  displayAmountWithMetricSuffix,
+  displayAmount,
+} from "../utils/token.utils"
 import { colors } from "../constants/colors"
+import TokenAmount from "./TokenAmount"
 
 const OwnedTokensOverview = ({ keepBalance, stakedBalance }) => {
   const total = useMemo(() => {
     return add(keepBalance, stakedBalance)
   }, [keepBalance, stakedBalance])
+  console.log("total", total.toString())
 
   return (
     <Tile id="tokens-overview" title="Owned Tokens">
-      <h1 className="balance">{displayAmount(keepBalance)}</h1>
+      <TokenAmount amount={keepBalance} />
       <hr />
       <div className="flex">
         <div className="flex-1 self-center">
@@ -33,7 +38,7 @@ const OwnedTokensOverview = ({ keepBalance, stakedBalance }) => {
           <h5 className="text-grey-70">staked</h5>
           <h4 className="text-grey-70">{displayAmount(stakedBalance)}</h4>
           <div className="text-smaller text-grey-40">
-            of {displayAmount(total)} Total
+            of {displayAmountWithMetricSuffix(total)} Total
           </div>
         </div>
       </div>
--- solidity/dashboard/src/components/TokenGrantOverview.jsx
+++ solidity/dashboard/src/components/TokenGrantOverview.jsx
@@ -9,6 +9,8 @@ import Tile from "./Tile"
 import moment from "moment"
 import { gt } from "../utils/arithmetics.utils"
 import { SpeechBubbleTooltip } from "./SpeechBubbleTooltip"
+import TokenAmount from "./TokenAmount"
+import { displayAmountWithMetricSuffix } from "../utils/token.utils"
 
 const TokenGrantOverview = ({ selectedGrant }) => {
   const { yourAddress, grantContract } = useContext(Web3Context)
@@ -59,7 +61,7 @@ const TokenGrantOverview = ({ selectedGrant }) => {
         title: `${cliffPeriod} cliff`,
       }}
     >
-      <h1 className="balance">{displayAmount(selectedGrant.amount)}</h1>
+      <TokenAmount amount={selectedGrant.amount} />
       <h4 className="text-grey-30 mb-1">Grant ID {selectedGrant.id}</h4>
       <h5 className="text-grey-50">
         Issued:{" "}
@@ -101,14 +103,16 @@ const TokenGrantOverview = ({ selectedGrant }) => {
             {displayAmount(selectedGrant.unlocked)}
           </h4>
           <div className="text-smaller text-grey-40">
-            of {displayAmount(selectedGrant.amount)} Total
+            of {displayAmountWithMetricSuffix(selectedGrant.amount)} Total
           </div>
           {gt(selectedGrant.readyToRelease || 0, 0) && (
             <div className="mt-2">
               <div className="text-secondary text-small flex wrap">
-                <span className="mr-1">{`${displayAmount(
-                  selectedGrant.readyToRelease
-                )} Available`}</span>
+                <span className="mr-1">
+                  {`${displayAmountWithMetricSuffix(
+                    selectedGrant.readyToRelease
+                  )} Available`}
+                </span>
                 <SpeechBubbleTooltip text="Releasing tokens allows them to be withdrawn from a grant." />
               </div>
               <SubmitButton
@@ -142,7 +146,7 @@ const TokenGrantOverview = ({ selectedGrant }) => {
             {displayAmount(selectedGrant.staked)}
           </h4>
           <div className="text-smaller text-grey-40">
-            of {displayAmount(selectedGrant.amount)} Total
+            of {displayAmountWithMetricSuffix(selectedGrant.amount)} Total
           </div>
         </div>
       </div>
