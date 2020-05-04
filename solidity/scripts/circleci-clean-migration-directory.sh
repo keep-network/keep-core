c66634166cf0ea48728eb802d2a70b18f1f89239
Sloan Thompson <sloan.thompson@thesis.co>
2020-05-04 14:52:45 -0400

    Cleanup script
    Here we wrap migration dir cleanup in its own script for execution in a
    separate run command on Circle.  This, coupled with the config parameter
    `when: always` will let us clean up the utilitybox migration dir whether
    the migration job passes or fails.  As-is it cleans only when the job
    passes, this results in the utilitybox disk getting filled up when there
    are a few failed builds.

---
+++ solidity/scripts/circleci-clean-migration-directory.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+
+if [[ -z $GOOGLE_PROJECT_NAME || -z $GOOGLE_PROJECT_ID || -z $BUILD_TAG || -z $GOOGLE_REGION || -z $GOOGLE_COMPUTE_ZONE_A ]]; then
+  echo "one or more required variables are undefined"
+  exit 1
+fi
+
+UTILITYBOX_IP=$(gcloud compute instances --project $GOOGLE_PROJECT_ID describe $GOOGLE_PROJECT_NAME-utility-box --zone $GOOGLE_COMPUTE_ZONE_A --format json | jq .networkInterfaces[0].networkIP -r)
+
+# Setup ssh environment
+gcloud compute config-ssh --project $GOOGLE_PROJECT_ID -q
+cat >> ~/.ssh/config << EOF
+Host *
+  StrictHostKeyChecking no
+
+Host utilitybox
+  HostName $UTILITYBOX_IP
+  IdentityFile ~/.ssh/google_compute_engine
+  ProxyCommand ssh -W %h:%p $GOOGLE_PROJECT_NAME-jumphost.$GOOGLE_COMPUTE_ZONE_A.$GOOGLE_PROJECT_ID
+EOF
+
+ssh utilitybox rm -rf /tmp/$BUILD_TAG
