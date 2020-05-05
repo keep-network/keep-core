45c8f7643dabc140f5c5b6e7f67ef8aaba0d8906
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-05-05 17:27:22 +0200

    Replace switch/case with ternary operator
    In the useEtherscanUrl hook the switch/case has been replaced with ternary operator. Currently the dapp supports 2 networks: mainnet/ropsten.

--- solidity/dashboard/src/hooks/useEtherscanUrl.js
+++ solidity/dashboard/src/hooks/useEtherscanUrl.js
@@ -4,12 +4,7 @@ import { Web3Context } from "../components/WithWeb3Context"
 export const useEtherscanUrl = () => {
   const { networkType } = useContext(Web3Context)
 
-  switch (networkType) {
-    case "main":
-      return "https://etherscan.io"
-    case "ropsten":
-      return "https://ropsten.etherscan.io"
-    default:
-      return "https://ropsten.etherscan.io"
-  }
+  return networkType === "main"
+    ? "https://etherscan.io"
+    : "https://ropsten.etherscan.io"
 }
