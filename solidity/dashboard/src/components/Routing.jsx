df9e0399eb2e28381206ba93afff67060fc82d5d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-05-06 15:13:28 +0200

    Update a ChooseWallet component
    Display modal conten depending on the selected provider.

--- solidity/dashboard/src/components/Routing.jsx
+++ solidity/dashboard/src/components/Routing.jsx
@@ -16,10 +16,10 @@ class Routing extends React.Component {
     const {
       isKeepTokenContractDeployer,
       contractsDataIsFetching,
-      web3: { error, provider },
+      web3: { error, provider, yourAddress },
     } = this.props
 
-    if (!provider) {
+    if (!provider || !yourAddress) {
       return <ChooseWallet />
     }
 
