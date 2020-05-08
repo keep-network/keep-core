b3f068b5dedcd58028324eeb9661251ba883448c
Dmitry <dymitr.paremski@keep.network>
2020-05-08 18:56:25 +0200

    Drafting tBTC data fetching for authorizations

--- solidity/dashboard/src/pages/TBTCApplicationPage.jsx
+++ solidity/dashboard/src/pages/TBTCApplicationPage.jsx
@@ -1,9 +1,19 @@
 import React from "react"
 import PageWrapper from "../components/PageWrapper"
 import AuthorizeContracts from "../components/AuthorizeContracts"
+import { tbtcAuthorizationService } from "../services/tbtc-authorization.service"
+import { useFetchData } from "../hooks/useFetchData"
 
 const TBTCApplicationPage = () => {
   // fetch data from service
+  const initialData = {}
+  const [state] = useFetchData(
+    tbtcAuthorizationService.fetchTBTCAuthorizationData,
+    initialData
+  )
+
+  console.log("state:", state.data)
+
   const data = [
     {
       operatorAddress: "address",
