aac2cef790cf15a712cba36dd1b9cdd8da56fb78
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-05-08 11:24:59 +0200

    Render applications on the app overview page

--- solidity/dashboard/src/pages/ApplicationOverviewPage.jsx
+++ solidity/dashboard/src/pages/ApplicationOverviewPage.jsx
@@ -1,13 +1,47 @@
 import React from "react"
 import PageWrapper from "../components/PageWrapper"
 import Tile from "../components/Tile"
+import * as Icons from "../components/Icons"
+import ApplicationBox, {
+  EmptyApplicationBox,
+} from "../components/ApplicationBox"
+
+const applications = [
+  {
+    icon: <Icons.KeepCircle />,
+    name: "tBTC",
+    websiteUrl: "https://tbtc.network/",
+    websiteName: "tBTC Website",
+    description:
+      "Stake KEEP tokens and bond ETH to earn TBTC through ECDSA keeps.",
+    btnLink: "/applications/tbtc",
+  },
+  {
+    icon: <Icons.KeepCircle />,
+    name: "Random Beacon",
+    websiteUrl: "https://keep.network/",
+    websiteName: "Keep Website",
+    description:
+      "Stake KEEP tokens and earn ETH through Random Beacon signing groups.",
+    btnLink: "/applications/random-beacon",
+  },
+]
 
 const ApplicationOverviewPage = () => {
   return (
     <PageWrapper title="Applications">
-      <Tile tile="Select an application below to manage authorization contracts for operators."></Tile>
+      <Tile title="Select an application below to manage authorization contracts for operators.">
+        <div className="flex row space-evenly wrap mt-1">
+          {applications.map(renderApplicationBox)}
+          <EmptyApplicationBox />
+        </div>
+      </Tile>
     </PageWrapper>
   )
 }
+
+const renderApplicationBox = (application) => (
+  <ApplicationBox {...application} />
+)
 
 export default ApplicationOverviewPage
