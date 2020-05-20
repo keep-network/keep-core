2337abe2af476060979e4f0734192ab3afa50e7e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-05-20 17:17:52 +0200

    Count the total amount of the tbtc rewards

--- solidity/dashboard/src/pages/TBTCRewardsPage.jsx
+++ solidity/dashboard/src/pages/TBTCRewardsPage.jsx
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { useMemo } from "react"
 import PageWrapper from "../components/PageWrapper"
 import TBTCRewardsDataTable from "../components/TBTCRewardsDataTable"
 import { tbtcRewardsService } from "../services/tbtc-rewards.service"
 import { useWeb3Context } from "../components/WithWeb3Context"
 import { useFetchData } from "../hooks/useFetchData"
+import { add } from "../utils/arithmetics.utils"
+import { displayAmount } from "../utils/token.utils"
 
 const TBTCRewardsPage = () => {
   const { yourAddress } = useWeb3Context()
@@ -13,12 +15,16 @@ const TBTCRewardsPage = () => {
     yourAddress
   )
 
+  const totalAmount = useMemo(() => {
+    return displayAmount(data.map((reward) => reward.amount).reduce(add, 0))
+  }, [data])
+
   return (
     <PageWrapper title="tBTC Rewards">
       <section className="tile">
         <h2 className="text-grey-70">Total Amount</h2>
         <h1 className="text-primary">
-          4,000&nbsp;<span className="h3">TBTC</span>
+          {totalAmount}&nbsp;<span className="h3">TBTC</span>
         </h1>
         <TBTCRewardsDataTable rewards={data} />
       </section>
