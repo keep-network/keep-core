5024a6ec86c778178e9d69d59a159ebda08bc097
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-05-25 12:22:28 +0200

    Operator page ui updates
    * render the undelegation table info only if the undelegation is in progress,
    * render the component's data depending on props,
    * improve undelegations status messages.

--- solidity/dashboard/src/components/DelegatedTokens.jsx
+++ solidity/dashboard/src/components/DelegatedTokens.jsx
@@ -1,8 +1,5 @@
 import React, { useContext } from "react"
 import AddressShortcut from "./AddressShortcut"
-import { operatorService } from "../services/token-staking.service"
-import { useFetchData } from "../hooks/useFetchData"
-import { LoadingOverlay } from "./Loadable"
 import { Web3Context } from "./WithWeb3Context"
 import UndelegateStakeButton from "./UndelegateStakeButton"
 import Banner, { BANNER_TYPE } from "./Banner"
@@ -10,36 +7,26 @@ import moment from "moment"
 import TokenAmount from "./TokenAmount"
 import { formatDate } from "../utils/general.utils"
 
-const DelegatedTokens = (props) => {
+const DelegatedTokens = ({ data, setData }) => {
   const { yourAddress } = useContext(Web3Context)
-  const [state, setData] = useFetchData(
-    operatorService.fetchDelegatedTokensData,
-    {}
-  )
-  const {
-    isFetching,
-    data: {
-      stakedBalance,
-      ownerAddress,
-      beneficiaryAddress,
-      authorizerAddress,
-      isUndelegationFromGrant,
-      isInInitializationPeriod,
-      undelegationPeriod,
-      isManagedGrant,
-      managedGrantContractInstance,
-      delegationStatus,
-      undelegationCompletedAt,
-    },
-  } = state
 
-  const undelegationSuccessCallback = () => {
-    setData({ ...state.data, delegationStatus: "UNDELEGATED" })
-  }
+  const {
+    stakedBalance,
+    ownerAddress,
+    beneficiaryAddress,
+    authorizerAddress,
+    isUndelegationFromGrant,
+    isInInitializationPeriod,
+    undelegationPeriod,
+    isManagedGrant,
+    managedGrantContractInstance,
+    delegationStatus,
+    undelegationCompletedAt,
+  } = data
 
   const cancelSuccessCallback = () => {
     setData({
-      ...state.data,
+      ...data,
       stakedBalance: "0",
       delegationStatus: "CANCELED",
     })
@@ -50,11 +37,9 @@ const DelegatedTokens = (props) => {
       .add(undelegationPeriod, "seconds")
       .fromNow(true)
     const isUndelegationPeriodOver = moment().isAfter(undelegationCompletedAt)
-    const title = isUndelegationPeriodOver
-      ? `Completed at ${formatDate(undelegationCompletedAt)}`
-      : `Estimated to complete in ${undelegationPeriodRelativeTime}.`
-
-    return `Undelegation is pending. ${title}`
+    return isUndelegationPeriodOver
+      ? `Undelegation period is over at ${formatDate(undelegationCompletedAt)}`
+      : `Undelegation is pending. Estimated to complete in ${undelegationPeriodRelativeTime}.`
   }
 
   const renderUndelegationStatus = () => {
@@ -62,13 +47,12 @@ const DelegatedTokens = (props) => {
       const title =
         delegationStatus === "UNDELEGATED"
           ? getUndelegationBannerTitle()
-          : `Delegation ${delegationStatus}`
-      let bannerType = BANNER_TYPE.PENDING
-      if (delegationStatus === "CANCELED") {
-        bannerType = BANNER_TYPE.DISABLED
-      } else if (delegationStatus === "RECOVERED") {
-        bannerType = BANNER_TYPE.SUCCESS
-      }
+          : "Undelegation completed"
+
+      const bannerType =
+        delegationStatus === "UNDELEGATED"
+          ? BANNER_TYPE.PENDING
+          : BANNER_TYPE.SUCCESS
 
       return (
         <div className="self-start">
@@ -82,9 +66,7 @@ const DelegatedTokens = (props) => {
           btnClassName="btn btn-primary btn-lg self-start"
           operator={yourAddress}
           successCallback={
-            isInInitializationPeriod
-              ? cancelSuccessCallback
-              : undelegationSuccessCallback
+            isInInitializationPeriod ? cancelSuccessCallback : () => {}
           }
           isFromGrant={isUndelegationFromGrant}
           isInInitializationPeriod={isInInitializationPeriod}
@@ -97,31 +79,26 @@ const DelegatedTokens = (props) => {
   }
 
   return (
-    <LoadingOverlay isFetching={isFetching}>
-      <section className="flex row wrap">
-        <section className="tile delegation-overview">
-          <h2 className="text-grey-70">Total Balance</h2>
-          <TokenAmount amount={stakedBalance} />
-          <DelegationAddress address={ownerAddress} label={"owner"} />
-          <DelegationAddress
-            address={beneficiaryAddress}
-            label={"beneficiary"}
-          />
-          <DelegationAddress address={authorizerAddress} label={"authorizer"} />
-        </section>
-        <section className="tile flex column undelegation-section">
-          <h4 className="text-grey-70">Undelegate</h4>
-          <div
-            className="text-small text-grey-70 mt-1"
-            style={{ marginBottom: "auto" }}
-          >
-            Click undelegate below to return all of your delegated KEEP tokens
-            to their original owner address.
-          </div>
-          {renderUndelegationStatus()}
-        </section>
+    <section className="flex row wrap">
+      <section className="tile delegation-overview">
+        <h2 className="text-grey-70">Total Balance</h2>
+        <TokenAmount amount={stakedBalance} />
+        <DelegationAddress address={ownerAddress} label={"owner"} />
+        <DelegationAddress address={beneficiaryAddress} label={"beneficiary"} />
+        <DelegationAddress address={authorizerAddress} label={"authorizer"} />
       </section>
-    </LoadingOverlay>
+      <section className="tile flex column undelegation-section">
+        <h4 className="text-grey-70">Undelegate</h4>
+        <div
+          className="text-small text-grey-70 mt-1"
+          style={{ marginBottom: "auto" }}
+        >
+          Click undelegate below to return all of your delegated KEEP tokens to
+          their original owner address.
+        </div>
+        {renderUndelegationStatus()}
+      </section>
+    </section>
   )
 }
 
--- solidity/dashboard/src/components/PendingUndelegation.jsx
+++ solidity/dashboard/src/components/PendingUndelegation.jsx
@@ -1,31 +1,20 @@
 import React, { useEffect, useContext } from "react"
-import { useFetchData } from "../hooks/useFetchData"
-import { operatorService } from "../services/token-staking.service"
 import {
   isSameEthAddress,
   isEmptyObj,
   formatDate,
 } from "../utils/general.utils"
 import { displayAmount } from "../utils/token.utils"
-import { LoadingOverlay } from "./Loadable"
 import { Web3Context } from "./WithWeb3Context"
 import StatusBadge, { BADGE_STATUS } from "./StatusBadge"
 import { DataTable, Column } from "./DataTable"
-import { PENDING_STATUS } from "../constants/constants"
 import moment from "moment"
 import Tile from "./Tile"
 import { usePrevious } from "../hooks/usePrevious"
 
-const initialData = { undelegation: {} }
-
-const PendingUndelegation = ({ latestUnstakeEvent }) => {
-  const { stakingContract, yourAddress } = useContext(Web3Context)
-  const [state, setData] = useFetchData(
-    operatorService.fetchPendingUndelegation,
-    initialData
-  )
-  const { isFetching, data } = state
-  const { undelegationCompletedAt, undelegationPeriod } = data
+const PendingUndelegation = ({ latestUnstakeEvent, data, setData }) => {
+  const { yourAddress } = useContext(Web3Context)
+  const { undelegationPeriod } = data
   const previousEvent = usePrevious(latestUnstakeEvent)
 
   useEffect(() => {
@@ -36,85 +25,63 @@ const PendingUndelegation = ({ latestUnstakeEvent }) => {
     ) {
       return
     }
+
     const {
       returnValues: { operator, undelegatedAt },
     } = latestUnstakeEvent
     if (!isSameEthAddress(yourAddress, operator)) {
       return
     }
+
     const undelegationCompletedAt = moment
       .unix(undelegatedAt)
       .add(undelegationPeriod, "seconds")
-    stakingContract.methods
-      .getDelegationInfo(operator)
-      .call()
-      .then((data) => {
-        const { amount } = data
-        setData({
-          ...state.data,
-          undelegationCompletedAt,
-          pendingUnstakeBalance: amount,
-          undelegationStatus: "PENDING",
-        })
-      })
+    setData({
+      ...data,
+      undelegationCompletedAt,
+      delegatedStatus: "UNDELEGATED",
+    })
   })
 
-  return (
-    <LoadingOverlay isFetching={isFetching}>
-      <Tile title="Token Undelegation" id="pending-undelegation">
-        <DataTable data={[{ ...data }]} itemFieldId="pendingUnstakeBalance">
-          <Column
-            header="amount"
-            field="pendingUnstakeBalance"
-            renderContent={({ pendingUnstakeBalance }) =>
-              pendingUnstakeBalance && `${displayAmount(pendingUnstakeBalance)}`
-            }
-          />
-          <Column
-            header="status"
-            field="undelegationStatus"
-            renderContent={({ undelegationStatus }) => {
-              const statusText =
-                undelegationStatus === PENDING_STATUS
-                  ? `${undelegationStatus.toLowerCase()}, ${undelegationCompletedAt.fromNow(
-                      true
-                    )}`
-                  : undelegationStatus
-
-              return (
-                undelegationStatus && (
-                  <StatusBadge
-                    className="self-start"
-                    status={BADGE_STATUS[undelegationStatus]}
-                    text={statusText.toLowerCase()}
-                  />
-                )
-              )
-            }}
-          />
-          <Column
-            header="estimate"
-            field="undelegationCompletedAt"
-            renderContent={({ undelegationCompletedAt }) =>
-              undelegationCompletedAt
-                ? formatDate(undelegationCompletedAt)
-                : "-"
-            }
-          />
-          <Column
-            header="undelegation period"
-            field="undelegationPeriod"
-            renderContent={({ undelegationPeriod }) => {
-              const undelegationPeriodRelativeTime = moment()
-                .add(undelegationPeriod, "seconds")
-                .fromNow(true)
-              return undelegationPeriodRelativeTime
-            }}
-          />
-        </DataTable>
-      </Tile>
-    </LoadingOverlay>
-  )
+  return data.delegationStatus === "UNDELEGATED" ? (
+    <Tile title="Pending Undelegations" id="pending-undelegation">
+      <DataTable data={[{ ...data }]} itemFieldId="pendingUnstakeBalance">
+        <Column
+          header="amount"
+          field="stakedBalance"
+          renderContent={({ stakedBalance }) =>
+            stakedBalance && `${displayAmount(stakedBalance)}`
+          }
+        />
+        <Column
+          header="status"
+          field="delegationStatus"
+          renderContent={() => {
+            return (
+              <StatusBadge status={BADGE_STATUS.PENDING} text="processing" />
+            )
+          }}
+        />
+        <Column
+          header="estimate"
+          field="undelegationCompletedAt"
+          renderContent={({ undelegationCompletedAt }) =>
+            undelegationCompletedAt ? formatDate(undelegationCompletedAt) : "-"
+          }
+        />
+        <Column
+          header="undelegation period"
+          field="undelegationPeriod"
+          renderContent={({ undelegationPeriod }) => {
+            const undelegationPeriodRelativeTime = moment()
+              .add(undelegationPeriod, "seconds")
+              .fromNow(true)
+            return undelegationPeriodRelativeTime
+          }}
+        />
+      </DataTable>
+    </Tile>
+  ) : null
 }
 
 export default PendingUndelegation
