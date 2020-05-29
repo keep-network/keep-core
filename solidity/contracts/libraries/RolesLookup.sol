92b1d3f1f559f0b78e0cdef806577f5b894cd77a
Piotr Dyraga <piotr.dyraga@keep.network>
2020-05-29 23:13:11 +0200

    Added documentation to RolesLookup library

--- solidity/contracts/libraries/RolesLookup.sol
+++ solidity/contracts/libraries/RolesLookup.sol
@@ -8,10 +8,16 @@ interface ManagedGrant {
     function grantee() external view returns(address);
 }
 
+/// @title Roles Lookup
+/// @notice Library facilitating lookup of roles in stake delegation setup.
 library RolesLookup {
 
     using AddressArrayUtils for address[];
 
+    /// @notice Returns true if the tokenOwner delegated tokens to operator
+    /// using the provided tokenStaking contract. Othwerwise, returns false.
+    /// This function works only for the case when tokenOwner own those tokens
+    /// and those are not tokens from a grant.
     function isTokenOwnerForOperator(
         address tokenOwner,
         address operator,
@@ -20,6 +26,11 @@ library RolesLookup {
         return tokenStaking.ownerOf(operator) == tokenOwner;
     }
 
+    /// @notice Returns true if the grantee delegated tokens to operator
+    /// with the provided tokenGrant contract. Otherwise, returns false.
+    /// This function works only for the case when tokens were generated from
+    /// a non-managed grant, that is, the grantee is a non-contract address to
+    /// which the delegated tokens were granted.
     function isGranteeForOperator(
         address grantee,
         address operator,
@@ -29,6 +40,13 @@ library RolesLookup {
         return operators.contains(operator);
     }
 
+    /// @notice Returns true if the grantee from the given managed granteeContract
+    /// delegated tokens to operator with the provided tokenGrant contract.
+    /// Otherwise, returns false. In case the grantee declared by the managed
+    /// granteeContract does not match the provided grantee, function reverts.
+    /// This function works only for cases when grantee, from TokenGrant's
+    /// perspective, is a smart contract exposing grantee() function returning
+    /// the final grantee. One possibility is the ManagedGrant contract.
     function isManagedGranteeForOperator(
         address grantee,
         address operator,
