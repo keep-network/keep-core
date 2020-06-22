416a5ac27e26b823bc73653b76aa0c707f35e74d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-06-22 17:24:09 +0200

    Update styles in the DelegatedTokensSkeleton

--- solidity/dashboard/src/components/skeletons/DelegatedTokensSkeleton.jsx
+++ solidity/dashboard/src/components/skeletons/DelegatedTokensSkeleton.jsx
@@ -3,29 +3,32 @@ import React from "react"
 const DelegatedTokensSkeleton = () => {
   return (
     <section className="flex flex-1 row wrap">
-      <section className="tile delegation-overview">
+      <section className="tile mb-0 delegation-overview">
         <div className="skeleton h2 mb-1" />
         <div className="skeleton h1 mb-1" />
         <div
           className="skeleton h6"
-          style={{ width: "70%", marginTop: "0.5rem" }}
+          style={{ width: "70%", marginTop: "2rem" }}
         />
         <div
           className="skeleton h6"
-          style={{ width: "40%", marginTop: "0.5rem" }}
+          style={{ width: "40%", marginTop: "1rem" }}
         />
         <div
           className="skeleton h6"
-          style={{ width: "55%", marginTop: "0.5rem" }}
+          style={{ width: "55%", marginTop: "1rem" }}
         />
       </section>
-      <section className="tile flex column undelegation-section">
+      <section className="tile mb-0 flex column undelegation-section">
         <div className="skeleton h4 mb-1" />
         <div
-          className="skeleton"
+          className="skeleton mt-1"
           style={{ marginBottom: "auto", width: "75%" }}
         />
-        <div className="skeleton" style={{ width: "40%", padding: "2rem" }} />
+        <div
+          className="skeleton self-start"
+          style={{ width: "40%", padding: "2rem 4rem" }}
+        />
       </section>
     </section>
   )
