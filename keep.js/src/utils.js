9276225779f6b939dfee8206d497d46c650bfa91
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-07-03 16:22:54 +0200

    Add web3-utils package
    Added commons functions.

---
+++ keep.js/src/utils.js
@@ -0,0 +1,34 @@
+import web3Utils from "web3-utils"
+
+export const isSameEthAddress = (address1, address2) => {
+  return (
+    web3Utils.toChecksumAddress(address1) ===
+    web3Utils.toChecksumAddress(address2)
+  )
+}
+
+export const add = (a, b) => {
+  return web3Utils.toBN(a).add(web3Utils.toBN(b))
+}
+
+export const sub = (a, b) => {
+  return web3Utils.toBN(a).sub(web3Utils.toBN(b))
+}
+
+export const mul = (a, b) => web3Utils.toBN(a).mul(web3Utils.toBN(b))
+
+export const gt = (a, b) => {
+  return web3Utils.toBN(a).gt(web3Utils.toBN(b))
+}
+
+export const gte = (a, b) => {
+  return web3Utils.toBN(a).gte(web3Utils.toBN(b))
+}
+
+export const lt = (a, b) => {
+  return web3Utils.toBN(a).lt(web3Utils.toBN(b))
+}
+
+export const lte = (a, b) => {
+  return web3Utils.toBN(a).lte(web3Utils.toBN(b))
+}
