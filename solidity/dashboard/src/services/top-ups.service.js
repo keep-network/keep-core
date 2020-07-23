57e13ff6cd255c9b5b536611c5ce5b9ecb6633f4
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-07-23 12:55:34 +0200

    Add a fn that fetches avbl top-ups by operators

--- solidity/dashboard/src/services/top-ups.service.js
+++ solidity/dashboard/src/services/top-ups.service.js
@@ -1,8 +1,75 @@
-import { contracts } from "../contracts"
+import { contracts, CONTRACT_DEPLOY_BLOCK_NUMBER } from "../contracts"
+import { isEmptyArray } from "../utils/array.utils"
+import { add } from "../utils/arithmetics.utils"
 
 export const commitTopUp = async (operator, onTransactionHashCallback) => {
   await contracts.stakingContract.methods
     .commitTopUp(operator)
     .send()
     .on("transactionHash", onTransactionHashCallback)
+}
+
+export const fetchAvailableTopUps = async (operators) => {
+  const availableTopUps = []
+
+  const toupUpsInitiatedByOperator = (
+    await contracts.stakingContract.getPastEvents("TopUpInitiated", {
+      fromBlock: CONTRACT_DEPLOY_BLOCK_NUMBER.stakingContract,
+      filter: { operator: [operators] },
+    })
+  ).reduce(reduceByOperator, {})
+  const toupUpsCompletedByOperator = (
+    await contracts.stakingContract.getPastEvents("TopUpCompleted", {
+      fromBlock: CONTRACT_DEPLOY_BLOCK_NUMBER.stakingContract,
+      filter: { operator: [operators] },
+    })
+  ).reduce(reduceByOperator, {})
+
+  for (const operator of operators) {
+    const topUpInitiated = toupUpsInitiatedByOperator[operator]
+    const topUpCompleted = toupUpsCompletedByOperator[operator]
+
+    const latestTopUpCompletedEvent = !isEmptyArray(topUpCompleted)
+      ? topUpCompleted.pop()
+      : undefined
+
+    if (latestTopUpCompletedEvent && !isEmptyArray(topUpInitiated)) {
+      const availableTopUpAmount = topUpInitiated
+        .filter(filterByAfterLatestCompletedTopUp(latestTopUpCompletedEvent))
+        .reduce(reduceAmount, 0)
+
+      availableTopUps.push({ operatorAddress: operator, availableTopUpAmount })
+    }
+  }
+
+  return availableTopUps
+}
+
+const reduceByOperator = (result, event) => {
+  const {
+    returnValues: { operator },
+  } = event
+
+  result[operator] = (result[operator] || []).push(event)
+
+  return result
+}
+
+const reduceAmount = (result, { returnValues: { topUp } }) => {
+  return add(result, topUp)
+}
+
+const filterByAfterLatestCompletedTopUp = (latestTopUpCompletedEvent) => (
+  initiatedEvent
+) => {
+  const isAfterLatestCompleteedTopUpBlock =
+    initiatedEvent.blockNumber > latestTopUpCompletedEvent.blockNumber
+  const isAfterLatestCompletedTopUpTransactionInBlock =
+    latestTopUpCompletedEvent.blockNumber === initiatedEvent.blockNumber &&
+    initiatedEvent.transactionIndex > latestTopUpCompletedEvent.transactionIndex
+
+  return (
+    isAfterLatestCompleteedTopUpBlock ||
+    isAfterLatestCompletedTopUpTransactionInBlock
+  )
 }
