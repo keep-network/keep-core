06bad6d7ee54a8eec3dd837b2c9c54465f180aa6
Sloan Thompson <sloan.thompson@thesis.co>
2020-07-30 14:11:35 -0400

    Remove NAT VM's
    We've migrated to Google hosted cloud-nat meaning we can get
    rid of self-hosted NAT implementation.

--- infrastructure/terraform/keep-dev/base.tf
+++ infrastructure/terraform/keep-dev/base.tf
@@ -64,58 +64,6 @@ module "vpc" {
   private_subnet_ip_cidr_range = "${var.private_subnet_ip_cidr_range}"
 }
 
-module "nat_gateway_external_ips" {
-  source       = "git@github.com:thesis/infrastructure.git//terraform/modules/gcp_ip"
-  name         = "${var.nat_gateway_ip_name}"
-  count        = "${var.nat_gateway_ip_allocation_count}"
-  project      = "${module.project.project_id}"
-  region       = "${var.region_data["region"]}"
-  address_type = "${var.nat_gateway_ip_address_type}"
-  labels       = "${local.labels}"
-}
-
-module "nat_gateway_zone_a" {
-  source          = "GoogleCloudPlatform/nat-gateway/google"
-  version         = "1.2.2"
-  name            = "${module.vpc.vpc_network_name}-"
-  project         = "${module.project.project_id}"
-  region          = "${var.region_data["region"]}"
-  zone            = "${var.region_data["zone_a"]}"
-  network         = "${module.vpc.vpc_network_name}"
-  subnetwork      = "${module.vpc.vpc_public_subnet_self_link}"
-  ip_address_name = "${module.nat_gateway_external_ips.ip_address_name[0]}" # Here's an example of taking a value from a list.
-  ssh_fw_rule     = false
-  instance_labels = "${local.labels}"
-}
-
-module "nat_gateway_zone_b" {
-  source          = "GoogleCloudPlatform/nat-gateway/google"
-  version         = "1.2.2"
-  name            = "${module.vpc.vpc_network_name}-"
-  project         = "${module.project.project_id}"
-  region          = "${var.region_data["region"]}"
-  zone            = "${var.region_data["zone_b"]}"
-  network         = "${module.vpc.vpc_network_name}"
-  subnetwork      = "${module.vpc.vpc_public_subnet_self_link}"
-  ip_address_name = "${module.nat_gateway_external_ips.ip_address_name[1]}"
-  ssh_fw_rule     = false
-  instance_labels = "${local.labels}"
-}
-
-module "nat_gateway_zone_c" {
-  source          = "GoogleCloudPlatform/nat-gateway/google"
-  version         = "1.2.2"
-  name            = "${module.vpc.vpc_network_name}-"
-  project         = "${module.project.project_id}"
-  region          = "${var.region_data["region"]}"
-  zone            = "${var.region_data["zone_c"]}"
-  network         = "${module.vpc.vpc_network_name}"
-  subnetwork      = "${module.vpc.vpc_public_subnet_self_link}"
-  ip_address_name = "${module.nat_gateway_external_ips.ip_address_name[2]}"
-  ssh_fw_rule     = false
-  instance_labels = "${local.labels}"
-}
-
 resource "google_compute_address" "eth_tx_ropsten_loadbalancer_ip" {
   name         = "${var.eth_tx_ropsten_loadbalancer_name}"
   project      = "${module.project.project_id}"
