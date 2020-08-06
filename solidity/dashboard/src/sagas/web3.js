15cbd1d5b021f9e15a287d39d247f99fecf20a87
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-08-06 17:56:44 +0200

    Sendig tx via eventChannel from redux-saga

---
+++ solidity/dashboard/src/sagas/web3.js
@@ -0,0 +1,94 @@
+import { eventChannel, END, buffers } from "redux-saga"
+import { take, put } from "redux-saga/effects"
+import {
+  Message,
+  showCreatedMessage,
+  showMessage,
+  closeMessage,
+} from "../actions/messages"
+import { messageType } from "../components/Message"
+
+function createTransactionEventChannel(contract, method, args, options) {
+  const infoMessage = Message.create({
+    title: "Waiting for the transaction confirmation...",
+    type: messageType.INFO,
+    sticky: true,
+  })
+
+  const emitter = contract.methods[method](...args).send(options)
+
+  let txHashCache
+
+  return eventChannel((emit) => {
+    emit(showCreatedMessage(infoMessage))
+    emitter
+      .once("transactionHash", (txHash) => {
+        emit(closeMessage(infoMessage.id))
+        txHashCache = txHash
+        emit(
+          showCreatedMessage({
+            id: txHash,
+            title: `Pending tx`,
+            content: txHash,
+            sticky: true,
+            type: messageType.PENDING_ACTION,
+          })
+        )
+      })
+      .once("receipt", (receipt) => {
+        let id
+        if (receipt && receipt.transactionHash) {
+          id = receipt.transactionHash
+        } else {
+          id = txHashCache
+        }
+        emit(closeMessage(infoMessage.id))
+        emit(
+          showMessage({
+            title: "success",
+            content: id,
+            sticky: true,
+            type: messageType.SUCCESS,
+          })
+        )
+        emit(END)
+      })
+      .once("error", (error, receipt) => {
+        emit(closeMessage(infoMessage.id))
+        emit(closeMessage(txHashCache))
+        emit(
+          showMessage({
+            title: "Error",
+            content: error.message,
+            type: messageType.ERROR,
+            sticky: true,
+          })
+        )
+        emit(new Error())
+      })
+
+    return () => {}
+  }, buffers.expanding())
+}
+
+export function* sendTransaction(action) {
+  const { contract, methodName, args, options } = action.payload
+
+  const transactionEventChannel = createTransactionEventChannel(
+    contract,
+    methodName,
+    args,
+    options
+  )
+
+  try {
+    while (true) {
+      const event = yield take(transactionEventChannel)
+      yield put(event)
+    }
+  } catch (error) {
+    throw error
+  } finally {
+    transactionEventChannel.close()
+  }
+}
