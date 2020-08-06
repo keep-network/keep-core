192da17c87d1bf0b01c7d59d25f206081c0cd885
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-08-06 17:58:49 +0200

    Testing sending transaction via redux-saga

--- solidity/dashboard/src/sagas/index.js
+++ solidity/dashboard/src/sagas/index.js
@@ -1,6 +1,11 @@
 import { all, fork } from "redux-saga/effects"
 import * as messagesSaga from "./messages"
+import * as delegateStakeSaga from "./delegate-stake"
 
 export default function* rootSaga() {
-  yield all([...Object.values(messagesSaga)].map(fork))
+  yield all(
+    [...Object.values(messagesSaga), ...Object.values(delegateStakeSaga)].map(
+      fork
+    )
+  )
 }
