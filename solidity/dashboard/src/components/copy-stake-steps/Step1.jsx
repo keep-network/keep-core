496754b38787b888c29fbad933cefb43880109c4
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-08-14 08:37:36 +0200

    Add styles for radio btns based on the KEEP Design

--- solidity/dashboard/src/components/copy-stake-steps/Step1.jsx
+++ solidity/dashboard/src/components/copy-stake-steps/Step1.jsx
@@ -3,12 +3,14 @@ import React from "react"
 const options = [
   {
     id: 1,
+    type: "COPY_TO_NEW_CONTRACT",
     title: "Copy stake balance to an upgraded delegation.",
     subtitle:
       "Avoid waiting the undelegation period and copy over your stake balance to a newly upgraded delegation. Your current stake will undelegate.",
   },
   {
     id: 2,
+    type: "WAIT",
     title:
       "Undelegate and wait the undelegation period to stake to a newly upgraded delegation.",
     subtitle:
@@ -35,11 +37,18 @@ const styles = {
   },
 }
 
-const Option = ({ title, subtitle, isSelected }) => {
+const Option = ({ title, subtitle, isSelected, type, id }) => {
   return (
     <li className="tile" style={styles.optionTile}>
-      <input type="radio" id="option-1" />
-      <label htmlFor="option-1">
+      <input
+        type="radio"
+        name="option"
+        value={type}
+        id={`option-${type}-${id}`}
+        checked={isSelected}
+        onChange={() => console.log("onChnage")}
+      />
+      <label htmlFor={`option-${type}-${id}`}>
         <h3 className="text-grey-70 mb-1">{title}</h3>
         <p className="text-big text-grey-60 mb-0">{subtitle}</p>
       </label>
