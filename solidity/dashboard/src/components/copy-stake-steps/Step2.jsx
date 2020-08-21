18f6d5541e7042d77b71e800ea155bfd9fdf6fca
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-08-21 13:49:05 +0200

    Add a `recomended` badge to the copy stake flow

--- solidity/dashboard/src/components/copy-stake-steps/Step2.jsx
+++ solidity/dashboard/src/components/copy-stake-steps/Step2.jsx
@@ -1,56 +1,99 @@
 import React from "react"
-import TokenAmount from "../TokenAmount"
 import Button from "../Button"
+import Badge from "../Badge"
 
-const styles = {
-  title: {
-    marginBottom: "2.5rem",
+const options = [
+  {
+    id: 1,
+    type: "COPY_STAKE_FLOW",
+    title: "Copy stake balance to an upgraded delegation.",
+    subtitle:
+      "Avoid waiting the undelegation period and copy over your stake balance to a newly upgraded delegation. Your current stake will undelegate.",
+    recomended: true,
   },
-  tileTitle: {
-    textAlign: "justify",
-    marginBottom: "2.5rem",
+  {
+    id: 2,
+    type: "WAIT_FLOW",
+    title:
+      "Undelegate and wait the undelegation period to stake to a newly upgraded delegation.",
+    subtitle:
+      "Undelegate and wait the 60 day undelegation period in order to stake on the upgraded staking contract. You can start new delegations with any tokens not already staked.",
+    recomended: false,
   },
-  addressWrapper: {
-    marginBottom: "0.5rem",
-  },
-}
+]
 
-const CopyStakeStep2 = ({
-  amount,
-  beneficiary,
-  operatorAddress,
-  authorizerAddress,
-}) => {
+const CopyStakeStep2 = ({ incrementStep, selectedStrategy, setStrategy }) => {
+  const onSetStrategy = (event) => {
+    setStrategy(event.target.value)
+  }
+
   return (
     <>
-      <h2 style={styles.title}>Review your stake details below.</h2>
-      <section className="tile">
-        <h3 style={styles.tileTitle}>
-          This stake balance will be copied to a newly upgraded delegation. The
-          undelegation process will be started on the old staking contract.
-        </h3>
-        <TokenAmount
-          amount={amount}
-          currencySymbol={"KEEP"}
-          wrapperClassName="mb-1"
-        />
-        <Address address={authorizerAddress} label="authorizer" />
-        <Address address={operatorAddress} label="operator" />
-        <Address address={beneficiary} label="beneficiary" />
-      </section>
-      <div className="flex row space-between self-end">
-        <Button className="btn btn-transparent btn-lg mr-2">back</Button>
-        <Button className="btn btn-primary btn-lg">copy stake</Button>
-      </div>
+      <h2 className="text-center">
+        Choose how to move your current stake to an upgraded delegation.
+      </h2>
+      <ul className="mt-2">
+        {options.map((option) => (
+          <Option
+            key={option.id}
+            {...option}
+            isSelected={option.type === selectedStrategy}
+            onChange={onSetStrategy}
+          />
+        ))}
+      </ul>
+      <Button
+        className="btn btn-primary btn-lg"
+        onClick={incrementStep}
+        disabled={!selectedStrategy}
+      >
+        review stake
+      </Button>
     </>
   )
 }
 
-const Address = ({ label, address }) => (
-  <div className="flex row center" style={styles.addressWrapper}>
-    <h5 className="text-grey-70 flex-1">{label}</h5>
-    <div className="text-big text-grey-50">{address}</div>
-  </div>
-)
+const styles = {
+  optionTile: {
+    borderRadius: "10px",
+  },
+  recomendeLabel: {
+    marginLeft: "auto",
+    marginTop: "-1.5rem",
+    marginRight: "-2rem",
+  },
+}
+
+const Option = ({
+  title,
+  subtitle,
+  isSelected,
+  type,
+  id,
+  onChange,
+  recomended,
+}) => {
+  return (
+    <li className="tile" style={styles.optionTile}>
+      {recomended && (
+        <div className="flex flex-1">
+          <Badge text="recomended" style={styles.recomendeLabel} />
+        </div>
+      )}
+      <input
+        type="radio"
+        name="option"
+        value={type}
+        id={`option-${type}-${id}`}
+        checked={isSelected}
+        onChange={onChange}
+      />
+      <label htmlFor={`option-${type}-${id}`}>
+        <h3 className="text-grey-70 mb-1">{title}</h3>
+        <p className="text-big text-grey-60 mb-0">{subtitle}</p>
+      </label>
+    </li>
+  )
+}
 
 export default CopyStakeStep2
