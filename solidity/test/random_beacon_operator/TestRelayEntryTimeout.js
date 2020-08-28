f91af67964e90b62e461b6cc7d30c5c5c7e0b482
Piotr Dyraga <piotr.dyraga@keep.network>
2020-08-28 11:20:03 +0200

    Require to report timeout if it happened before requesting new entry
    We were allowing to request a new entry if the previous timed out and
    this fact has not been reported. This could lead do group members
    avoiding slashing by submitting a new entry before someone can report
    the timeout (gas auction).
    
    It is fixed now by requiring to report entry timout before a new entry
    can be requested.

--- solidity/test/random_beacon_operator/TestRelayEntryTimeout.js
+++ solidity/test/random_beacon_operator/TestRelayEntryTimeout.js
@@ -1,14 +1,21 @@
-const blsData = require("../helpers/data.js")
-const {expectRevert, time} = require("@openzeppelin/test-helpers")
-const {initContracts} = require('../helpers/initContracts')
-const assert = require('chai').assert
+
+const {expectRevert, expectEvent, time} = require("@openzeppelin/test-helpers")
 const {createSnapshot, restoreSnapshot} = require("../helpers/snapshot.js")
 const {contract, accounts, web3} = require("@openzeppelin/test-environment")
+const blsData = require("../helpers/data.js")
+const stakeDelegate = require('../helpers/stakeDelegate')
+const {initContracts} = require('../helpers/initContracts')
 
 describe("KeepRandomBeaconOperator/RelayEntryTimeout", function() {
-  let operatorContract, serviceContract, fee;
-  const blocksForward = web3.utils.toBN(20);
-  const requestCounter = 0;
+  const deployer = accounts[0],
+    serviceContractUpgrader = accounts[1]
+    serviceContract = accounts[2],
+    operator1 = accounts[3],
+    operator2 = accounts[4],
+    operator3 = accounts[5],
+    thirdParty = accounts[6]
+
+  let operatorContract, entryFee;
 
   before(async() => {
     let contracts = await initContracts(
@@ -18,20 +25,55 @@ describe("KeepRandomBeaconOperator/RelayEntryTimeout", function() {
       contract.fromArtifact('KeepRandomBeaconOperatorStub')
     ); 
 
-    registryContract = contracts.registry
     operatorContract = contracts.operatorContract;
-    serviceContract = contracts.serviceContract;
-
-    await registryContract.setServiceContractUpgrader(
-      operatorContract.address, accounts[0], {from: accounts[0]}
+  
+    //
+    // register 'serviceContract' account as a new service contract so that
+    // we can hit the operator contract from this account for tests' simplicity
+    //
+    await contracts.registry.setServiceContractUpgrader(
+      operatorContract.address,
+      serviceContractUpgrader,
+      {from: deployer}
     )
-    await operatorContract.addServiceContract(accounts[0], {from: accounts[0]})  
+    await operatorContract.addServiceContract(
+      serviceContract,
+      {from: serviceContractUpgrader}
+    )  
 
-    await operatorContract.registerNewGroup(blsData.groupPubKey);
-    await operatorContract.setGroupMembers(blsData.groupPubKey, [accounts[0]]);
+    //
+    // stake 3 operators, authorize operator contract for all of them,
+    // and wait for the stake initialization period to complete
+    //
+    let token = contracts.token
+    let tokenStaking = contracts.stakingContract
+    const stake = web3.utils.toBN("500000000000000000000000") // 500 000 KEEP
+    await stakeDelegate(tokenStaking, token, deployer, operator1, operator1, operator1, stake)
+    await stakeDelegate(tokenStaking, token, deployer, operator2, operator2, operator2, stake)
+    await stakeDelegate(tokenStaking, token, deployer, operator3, operator3, operator3, stake)
+    await tokenStaking.authorizeOperatorContract(operator1, operatorContract.address, { from: operator1 })
+    await tokenStaking.authorizeOperatorContract(operator2, operatorContract.address, { from: operator2 })
+    await tokenStaking.authorizeOperatorContract(operator3, operatorContract.address, { from: operator3 })
+    await time.increase((await tokenStaking.initializationPeriod()).addn(1))
+    
+    //
+    // register two groups with operators staked in the previous step
+    //
+    await operatorContract.registerNewGroup("0x111")
+    await operatorContract.setGroupMembers("0x111", [operator1, operator2, operator3])
+    await operatorContract.registerNewGroup(blsData.groupPubKey)
+    await operatorContract.setGroupMembers(blsData.groupPubKey, [operator3, operator2, operator1])
 
-    fee = await serviceContract.entryFeeEstimate(0);
+    entryFee = await contracts.serviceContract.entryFeeEstimate(0);
   });
+
+  async function requestRelayEntry() {
+    return operatorContract.sign(
+      0,
+      blsData.previousEntry,
+      {value: entryFee, from: serviceContract}
+    )
+  }
 
   beforeEach(async () => {
     await createSnapshot()
@@ -41,61 +83,59 @@ describe("KeepRandomBeaconOperator/RelayEntryTimeout", function() {
     await restoreSnapshot()
   });
 
-  it("should not throw an error when entry is in progress and " +
-     "block number > relay entry timeout", async () => {
-    await operatorContract.sign(
-      requestCounter, blsData.previousEntry, {value: fee, from: accounts[0]}
-    );
+  describe("request for a new relay entry", async () => {
+    it("should be accepted when no other request is in progress", async () => {
+      let receipt = await requestRelayEntry()
+      await expectEvent(receipt, 'RelayEntryRequested') 
+    })
 
-    await time.advanceBlockTo(blocksForward.addn(await web3.eth.getBlockNumber()))
+    it("should be rejected when another request is in progress", async () => {
+      await requestRelayEntry()
+      await expectRevert(requestRelayEntry(), "Beacon is busy");
+    })
 
-    await operatorContract.sign(
-      requestCounter, blsData.previousEntry, {value: fee, from: accounts[0]}
-    );
+    it("should be rejected when another request is in progress", async () => {
+      await requestRelayEntry()
+      await expectRevert(requestRelayEntry(), "Beacon is busy");
+    })
 
-    assert.equal(
-      (await operatorContract.getPastEvents())[0].event, 
-      "RelayEntryRequested", 
-      "RelayEntryRequested event should occur on operator contract"
-    );
-  });
+    it("should be rejected when another request is in progress even if it timed out", async () => {
+      const timeout = await operatorContract.relayEntryTimeout()      
+      await requestRelayEntry()
+      await time.advanceBlockTo((await time.latestBlock()).add(timeout))    
+      await expectRevert(requestRelayEntry(), "Beacon is busy");
+    })
 
-  it("should throw an error when entry is in progress and " + 
-     "block number <= relay entry timeout", async () => {
-    await operatorContract.sign(
-      requestCounter, blsData.previousEntry, {value: fee, from: accounts[0]}
-    );
+    it("should be retried when another request timed out and it's been reported", async () => {
+      const timeout = await operatorContract.relayEntryTimeout()      
+      await requestRelayEntry()
+      await time.advanceBlockTo((await time.latestBlock()).add(timeout))
+      let receipt = await operatorContract.reportRelayEntryTimeout({from: thirdParty})
+      await expectEvent(receipt, 'RelayEntryRequested') 
+    })
+  })
 
-    await expectRevert(
-      operatorContract.sign(requestCounter, blsData.previousEntry, {value: fee, from: accounts[0]}), 
-      "Beacon is busy"
-    );
-  });
-
-  it("should not throw an error when entry is not in progress and " + 
-     "block number > relay entry timeout", async () => {
-    await operatorContract.sign(
-      requestCounter, blsData.previousEntry, {value: fee, from: accounts[0]}
+  describe("relay entry submission", async () => {
+    it("should be rejected after timeout", async() => {
+      const timeout = await operatorContract.relayEntryTimeout()      
+      await requestRelayEntry()
+      await time.advanceBlockTo((await time.latestBlock()).add(timeout))
+      await expectRevert(
+        operatorContract.relayEntry(blsData.groupSignature), 
+        "Entry timed out"
       );
+    })
 
-    assert.equal(
-      (await operatorContract.getPastEvents())[0].event, 
-      "RelayEntryRequested", 
-      "RelayEntryRequested event should occur on operator contract."
-    );
-  });
+    it("should be accepted when the previous request timed out and it's been reported", async () => {
+      const timeout = await operatorContract.relayEntryTimeout()      
+      await requestRelayEntry()
+      await time.advanceBlockTo((await time.latestBlock()).add(timeout))
+      await operatorContract.reportRelayEntryTimeout({from: thirdParty})
+      // 0x111 group gets terminated and bls.groupPubKey group is now asked
+      // to provide the signature
 
-  it("should not allow to submit relay entry after timeout", async () => {
-    await operatorContract.sign(
-      requestCounter, blsData.previousEntry, {value: fee, from: accounts[0]}
-    );
-
-    const relayEntryTimeout = await operatorContract.relayEntryTimeout()
-    await time.advanceBlockTo(relayEntryTimeout.addn(await web3.eth.getBlockNumber()))
-
-    await expectRevert(
-      operatorContract.relayEntry(blsData.groupSignature), 
-      "Entry timed out"
-    );
-  });
+      await operatorContract.relayEntry(blsData.groupSignature)
+      // ok, no revert
+    })
+  })
 });
