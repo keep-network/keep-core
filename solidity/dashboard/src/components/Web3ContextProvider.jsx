128d4e8c60f7604c5be6048167a658c223edfaff
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-09-18 16:02:20 +0200

    Update functions in the Web3ContextProvider
    Throwing an error in the `catch` block instead a `return` statement.

--- solidity/dashboard/src/components/Web3ContextProvider.jsx
+++ solidity/dashboard/src/components/Web3ContextProvider.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Web3 from "web3"
 import { Web3Context } from "./WithWeb3Context"
-import { MessagesContext, messageType } from "./Message"
+import { MessagesContext } from "./Message"
 import { getContracts, resolveWeb3Deferred } from "../contracts"
 
 export default class Web3ContextProvider extends React.Component {
@@ -39,11 +39,7 @@ export default class Web3ContextProvider extends React.Component {
       resolveWeb3Deferred(web3)
     } catch (error) {
       this.setState({ providerError: error.message, isFetching: false })
-      this.context.showMessage({
-        type: messageType.ERROR,
-        title: error.message,
-      })
-      return
+      throw error
     }
 
     try {
@@ -51,9 +47,9 @@ export default class Web3ContextProvider extends React.Component {
     } catch (error) {
       this.setState({
         isFetching: false,
-        error: "Please select correct network",
+        error: error.message,
       })
-      return
+      throw error
     }
 
     web3.eth.currentProvider.on("accountsChanged", this.onAccountChanged)
