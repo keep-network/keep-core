57077066ccc980ef7942131811af8fe489fb0d2d
Antonio Salazar Cardozo <antonio@thesis.co>
2020-09-27 10:15:46 -0400

    Add callback function for beneficiary
    Tests are not yet updated to handle the fact that beneficiaries are
    expected to always be contracts with an __escrowSentTokens(uint256)
    notifier function.

--- solidity/contracts/PhasedEscrow.sol
+++ solidity/contracts/PhasedEscrow.sol
@@ -4,6 +4,10 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 
+interface IBeneficiaryContract {
+    function __escrowSentTokens(uint256 amount) external;
+}
+
 // @title PhasedEscrow
 // @notice A token holder contract allowing contract owner to set beneficiary of
 //         tokens held by the contract and allowing the owner to withdraw the
@@ -15,7 +19,7 @@ contract PhasedEscrow is Ownable {
     event TokensWithdrawn(address beneficiary, uint256 amount);
 
     IERC20 public token;
-    address public beneficiary;
+    IBeneficiaryContract public beneficiary;
 
     constructor(IERC20 _token) public {
         token = _token;
@@ -24,21 +28,23 @@ contract PhasedEscrow is Ownable {
     // @notice Sets the provided address as a beneficiary allowing it to
     //         withdraw all tokens from escrow. This function can be called only
     //         by escrow owner.
-    function setBeneficiary(address _beneficiary) public onlyOwner {
+    function setBeneficiary(IBeneficiaryContract _beneficiary) external onlyOwner {
         beneficiary = _beneficiary;
-        emit BeneficiaryUpdated(beneficiary);
+        emit BeneficiaryUpdated(address(beneficiary));
     }
 
     // @notice Withdraws the specified number of tokens from escrow to the
     //         beneficiary. If the beneficiary is not set, or there are
     //         insufficient tokens in escrow, the function fails.
-    function withdraw(uint256 amount) public onlyOwner {
-        require(beneficiary != address(0), "Beneficiary not assigned");
+    function withdraw(uint256 amount) external onlyOwner {
+        require(address(beneficiary) != address(0), "Beneficiary not assigned");
 
         uint256 balance = token.balanceOf(address(this));
         require(amount <= balance, "Not enough tokens for withdrawal");
 
-        token.safeTransfer(beneficiary, amount);
-        emit TokensWithdrawn(beneficiary, amount);
+        token.safeTransfer(address(beneficiary), amount);
+        emit TokensWithdrawn(address(beneficiary), amount);
+
+        beneficiary.__escrowSentTokens(amount);
     }
 }
