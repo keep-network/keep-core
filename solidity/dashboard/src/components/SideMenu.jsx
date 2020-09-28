2335023e0e91b6b1058951c2e4b5a47071289594
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-09-28 14:00:57 +0200

    Update left side menu styles
    Updated styles according to the Figma views.

--- solidity/dashboard/src/components/SideMenu.jsx
+++ solidity/dashboard/src/components/SideMenu.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useContext } from "react"
 import { Link, useRouteMatch } from "react-router-dom"
-import { Web3Status } from "./Web3Status"
-import { Web3Context } from "./WithWeb3Context"
 import { ContractsDataContext } from "./ContractsDataContextProvider"
-import AddressShortcut from "./AddressShortcut"
-import { NetworkStatus } from "./NetworkStatus"
+import Footer from "./Footer"
 import * as Icons from "./Icons"
 
 export const SideMenuContext = React.createContext({})
@@ -25,60 +22,23 @@ export const SideMenuProvider = (props) => {
 
 export const SideMenu = (props) => {
   const { isOpen } = useContext(SideMenuContext)
-  const { yourAddress, provider } = useContext(Web3Context)
   const { isKeepTokenContractDeployer } = useContext(ContractsDataContext)
 
-  const isDisabled = !yourAddress || !provider
-
   return (
-    <nav
-      className={`${isOpen ? "active " : ""}side-menu ${
-        isDisabled ? " disabled" : ""
-      }`}
-    >
-      <ul title={isDisabled ? "Please choose a wallet first." : ""}>
-        <NavLink
-          to="/tokens"
-          label="tokens"
-          icon={<Icons.KeepToken />}
-          sublinks={[
-            { to: "/tokens/overview", exact: true, label: "Overview" },
-            { to: "/tokens/delegate", exact: true, label: "Delegate Tokens" },
-            { to: "/tokens/grants", exact: true, label: "Token Grants" },
-          ]}
-        />
+    <nav className={`side-menu${isOpen ? "--active " : ""}`}>
+      <ul className="side-menu__list">
+        <NavLink to="/tokens" label="tokens" icon={<Icons.KeepToken />} />
         <NavLink
           exact
           to="/operations"
           label="operations"
           icon={<Icons.Operations />}
         />
-        <NavLink
-          to="/rewards"
-          label="rewards"
-          icon={<Icons.Rewards />}
-          sublinks={[
-            {
-              to: "/rewards/random-beacon",
-              exact: true,
-              label: "Beacon Rewards",
-            },
-            { to: "/rewards/tbtc", exact: true, label: "tBTC Rewards" },
-          ]}
-        />
+        <NavLink to="/rewards" label="rewards" icon={<Icons.Rewards />} />
         <NavLink
           to="/applications"
           label="applications"
           icon={<Icons.Authorizer />}
-          sublinks={[
-            { to: "/applications/overview", exact: true, label: "Overview" },
-            {
-              to: "/applications/random-beacon",
-              exact: true,
-              label: "Random Beacon",
-            },
-            { to: "/applications/tbtc", exact: true, label: "tBTC" },
-          ]}
         />
         <NavLink
           exact
@@ -90,65 +50,24 @@ export const SideMenu = (props) => {
         {isKeepTokenContractDeployer && (
           <NavLink exact to="/create-token-grants" label="token grants" />
         )}
-        <Web3Status />
-        <div className="account-address">
-          <h5 className="text-grey-50">
-            <span>address:&nbsp;</span>
-            <AddressShortcut classNames="h5" address={yourAddress} />
-          </h5>
-          <NetworkStatus />
-        </div>
       </ul>
+      <Footer />
     </nav>
   )
 }
 
-const NavLink = ({
-  label,
-  to,
-  exact,
-  icon,
-  sublinks,
-  wrapperClassName,
-  activeClassName,
-  withArrowRight,
-}) => {
+const NavLink = ({ label, to, exact, icon }) => {
   const match = useRouteMatch({
     path: to,
     exact,
   })
 
   return (
-    <li className={`${wrapperClassName} ${match ? activeClassName : ""}`}>
-      <Link to={to}>
-        {icon}
-        <span className="ml-1">{label}</span>
-        {withArrowRight && <Icons.ArrowRight />}
+    <li className="side-menu__item-wrapper">
+      <Link to={to} className={`side-menu__item${match ? "--active" : ""}`}>
+        <div className="side-menu__item__icon">{icon}</div>
+        <span className="side-menu__item__title">{label}</span>
       </Link>
-      <SubNavLinks sublinks={sublinks} />
     </li>
-  )
-}
-
-NavLink.defaultProps = {
-  wrapperClassName: "text-label",
-  activeClassName: "active-page-link",
-  withArrowRight: true,
-}
-
-const SubNavLinks = ({ sublinks }) => {
-  if (!sublinks) return null
-
-  return (
-    <ul className="sublinks">
-      {sublinks.map((sublink) => (
-        <NavLink
-          key={sublink.label}
-          {...sublink}
-          wrapperClassName="sublink"
-          withArrowRight={false}
-        />
-      ))}
-    </ul>
   )
 }
