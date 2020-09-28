9f19c2d3ca8d4316421a51198c91b528c0a3b49c
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-09-28 13:59:41 +0200

    Update Footer component
    Updated a footer component according to the Figma views.

--- solidity/dashboard/src/components/Footer.jsx
+++ solidity/dashboard/src/components/Footer.jsx
@@ -1,37 +1,33 @@
 import React from "react"
-import * as Icons from "./Icons"
 
-const socialMedia = [
-  { label: "Twitter", url: "https://twitter.com/keep_project" },
-  { label: "Telegram", url: "https://t.me/KeepNetworkOfficial" },
-  { label: "Reddit", url: "https://www.reddit.com/r/KeepNetwork" },
-]
-
-const aboutUs = [
-  { label: "Whitepaper", url: "https://keep.network/whitepaper" },
-  { label: "Team", url: "https://keep.network/#team" },
-  { label: "Advisors", url: "https://keep.network/#advisors" },
-  { label: "Blog", url: "https://blog.keep.network/" },
+const links = [
+  { label: "Join Discord", url: "https://discordapp.com/invite/wYezN7v" },
+  { label: "About Keep", url: "https://keep.network/" },
+  {
+    label: "User Guide",
+    url: "https://keep-network.gitbook.io/staking-documentation/",
+  },
 ]
 
 const Footer = () => {
   return (
-    <footer>
-      <Icons.KeepCircle color="#F2F2F2" />
-      <ul>{aboutUs.map(renderFooterLinkItem)}</ul>
-      <ul>{socialMedia.map(renderFooterLinkItem)}</ul>
-      <div className="signature text-smaller text-grey-70">
-        <div>A Thesis* Build</div>
-        <div>© 2020 Keep, SEZC. All Rights Reserved.</div>
+    <footer className="footer">
+      <ul className="footer__links">{links.map(renderFooterLinkItem)}</ul>
+      <div className="footer__signature">
+        A Thesis* Build <br />
+        © 2020 Keep, SEZC <br />
+        All Rights Reserved.
       </div>
+      <div className="footer__app-version">{`Version ${process.env.REACT_APP_VERSION}`}</div>
     </footer>
   )
 }
 
 const FooterLinkItem = ({ label, url }) => (
-  <li>
+  <li className="footer__links__item">
+    {/* TODO add arrow link to the external link*/}
     <a href={url} rel="noopener noreferrer" target="_blank">
-      <h5>{label}</h5>
+      {label}
     </a>
   </li>
 )
