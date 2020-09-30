c7eb18fd9e553cd865d6cf3da99ed32b29da9489
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-09-30 14:26:30 +0200

    Update the PageWrapper component
    Simplify the `PageWrapper` component and now this component is
    responsible for setting the header data via `HeaderContext`.

--- solidity/dashboard/src/components/PageWrapper.jsx
+++ solidity/dashboard/src/components/PageWrapper.jsx
@@ -1,39 +1,14 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import * as Icons from "./Icons"
+import React, { useEffect } from "react"
+import { useUpdateHeaderData } from "./Header"
 
-const PageWrapper = ({
-  title,
-  children,
-  nextPageLink,
-  nextPageIcon,
-  nextPageTitle,
-  ...titleProps
-}) => {
-  const IconComponent = nextPageIcon
-  return (
-    <>
-      <header className="flex row wrap center space-between">
-        <h1 className="mb-2" {...titleProps}>
-          {title}
-        </h1>
-        {nextPageLink && nextPageTitle && (
-          <nav style={{ marginLeft: "auto" }}>
-            <Link to={nextPageLink} style={{ textDecoration: "none" }}>
-              <div className="flex center">
-                <IconComponent width={30} height={30} />
-                <span className="text-black text-link-page mr-1 ml-1">
-                  {nextPageTitle}
-                </span>
-                <Icons.ArrowRight className="arrow-right lg black self-center" />
-              </div>
-            </Link>
-          </nav>
-        )}
-      </header>
-      {children}
-    </>
-  )
+const PageWrapper = ({ title, subLinks, children }) => {
+  const updateHeaderData = useUpdateHeaderData()
+
+  useEffect(() => {
+    updateHeaderData(title, subLinks)
+  }, [title, subLinks, updateHeaderData])
+
+  return <>{children}</>
 }
 
 export default PageWrapper
