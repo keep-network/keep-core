fb38b76fbcece26780b060f2d8502d693dc84655
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-10-12 16:07:07 +0200

    Remove unnecessary nav on the Resource page
    Use sub nav on the top instead of additional nav on the right side. Also
    here we removed the `react-scroll` packages because this package was
    only used on the `Resource` page.

--- solidity/dashboard/src/pages/ResourcesPage.jsx
+++ solidity/dashboard/src/pages/ResourcesPage.jsx
@@ -3,7 +3,6 @@ import PageWrapper from "../components/PageWrapper"
 import DocumentationSection from "../components/resources/DocumentationSection"
 import TerminologyDataTable from "../components/resources/TerminologyDataTable"
 import DelegationDiagram from "../components/resources/DelegationDiagram"
-import Navigation from "../components/resources/Navigation"
 
 const ResourcesPage = () => {
   return (
@@ -14,7 +13,6 @@ const ResourcesPage = () => {
           <DelegationDiagram />
           <TerminologyDataTable />
         </div>
-        <Navigation />
       </div>
     </PageWrapper>
   )
