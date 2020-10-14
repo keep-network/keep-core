bb84fad093a5c2c32d5dfec77d6e0276197210f6
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-10-14 15:59:10 +0200

    Extract copy to clipboard feature as a component
    Use a render prop pattern to extract copy to clipboard feature.

--- solidity/dashboard/src/components/AddressShortcut.jsx
+++ solidity/dashboard/src/components/AddressShortcut.jsx
@@ -1,25 +1,26 @@
-import React, { useState } from "react"
+import React, { useMemo } from "react"
 import { shortenAddress } from "../utils/general.utils"
-import copy from "copy-to-clipboard"
+import CopyToClipboard from "./CopyToClipboard"
 
 const AddressShortcut = ({ address, classNames }) => {
-  const [copyStatus, setCopyStatus] = useState("Copy to clipboard")
-
-  const copyToClipboard = () => {
-    copy(address)
-      ? setCopyStatus("Copied!")
-      : setCopyStatus(`Cannot copy value: ${address}!`)
-  }
+  const addr = useMemo(() => shortenAddress(address), [address])
 
   return (
-    <span
-      onClick={copyToClipboard}
-      onMouseOut={() => setCopyStatus("Copy to clipboard")}
-      className={`address-shortcut tooltip address ${classNames}`}
-    >
-      <span className="tooltip-text address bottom">{copyStatus}</span>
-      {shortenAddress(address)}
-    </span>
+    <CopyToClipboard
+      toCopy={address}
+      render={({ reset, copyStatus, copyToClipboard }) => {
+        return (
+          <span
+            onClick={copyToClipboard}
+            onMouseOut={reset}
+            className={`address-shortcut tooltip address ${classNames}`}
+          >
+            <span className="tooltip-text address bottom">{copyStatus}</span>
+            {addr}
+          </span>
+        )
+      }}
+    />
   )
 }
 
