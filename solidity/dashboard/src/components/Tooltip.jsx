c57603a9a3c28b4491a021bb15395ba228c71b67
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-10-19 15:15:29 +0200

    Update the Tooltip component styles
    Created a component that supportss two types of tooltip based on the Figma
    views. The first is a standart tooltip that looks like a speech bubble.
    The second type is more complex case, where contains header and
    content.

---
+++ solidity/dashboard/src/components/Tooltip.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useRef } from "react"
+import * as Icons from "./Icons"
+
+const Tooltip = ({
+  triggerComponent: TriggerComponent,
+  title,
+  content,
+  children,
+  direction = "bottom",
+  icon: IconComponent = Icons.Tooltip,
+  simple = false,
+  delay = 300,
+}) => {
+  const timeout = useRef(null)
+  const [active, setActive] = useState(false)
+
+  const showTooltip = () => {
+    timeout.current = setTimeout(() => {
+      setActive(true)
+    }, delay)
+  }
+
+  const hideTooltip = () => {
+    clearInterval(timeout.current)
+    setActive(false)
+  }
+
+  return (
+    <div className={`tooltip${simple ? "--simple" : ""}--${direction}`}>
+      <div
+        className="tooltip__trigger"
+        onMouseLeave={hideTooltip}
+        onMouseEnter={showTooltip}
+      >
+        <TriggerComponent />
+      </div>
+      {active && (
+        <div className="tooltip__content-wrapper">
+          {children ? (
+            children
+          ) : (
+            <>
+              <Tooltip.Header text={title} icon={IconComponent} />
+              <Tooltip.Divider />
+              <Tooltip.Content>{content}</Tooltip.Content>
+            </>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+Tooltip.Divider = () => <hr className="tooltip__divider" />
+
+Tooltip.Header = ({ icon: IconComponent, text, className = "" }) => (
+  <div className={`tooltip__header ${className}`}>
+    <IconComponent className="tooltip__header__icon" />
+    <div className="tooltip__header__title">{text}</div>
+  </div>
+)
+
+Tooltip.Content = ({ children }) => (
+  <div className="tooltip__content">{children}</div>
+)
+
+export default Tooltip
