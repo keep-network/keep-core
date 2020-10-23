4dfa9d462c566e19f688d4fe6efd0197dacc6216
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-10-23 14:52:25 +0200

    Create a custom Route component
    The `CustomRoute` component is a kind of guard. Based on the page
    component route config `CustomRoute` renders an empty state page for
    when a wallet isn't connected.

--- solidity/dashboard/src/components/Routing.jsx
+++ solidity/dashboard/src/components/Routing.jsx
@@ -1,17 +1,13 @@
 import React from "react"
 import { Route, Switch, Redirect } from "react-router-dom"
-import { withContractsDataContext } from "./ContractsDataContextProvider"
-import Loadable from "./Loadable"
 import { NotFound404 } from "./NotFound404"
-import withWeb3Context from "./WithWeb3Context"
+import { useWeb3Context } from "./WithWeb3Context"
 import OperatorPage from "../pages/OperatorPage"
 import RewardsPageContainer from "../pages/RewardsPageContainer"
-import CreateTokenGrantPage from "../pages/CreateTokenGrantPage"
 import TokensPageContainer from "../pages/TokensPageContainer"
 import ApplicationsPageContainer from "../pages/ApplicationsPageContainer"
 import ResourcesPage from "../pages/ResourcesPage"
 import TokenGrantPreviewPage from "../pages/TokenGrantPreviewPage"
-import Header from "./Header"
 import TokenOverviewPage from "../pages/OverviewPage"
 
 const pages = [
@@ -20,44 +16,19 @@ const pages = [
   OperatorPage,
   RewardsPageContainer,
   ApplicationsPageContainer,
+  ResourcesPage,
 ]
 
-const withoutWalletPages = [ResourcesPage]
-
 class Routing extends React.Component {
-  renderContent() {
-    const {
-      isKeepTokenContractDeployer,
-      contractsDataIsFetching,
-      web3: { error, provider, yourAddress },
-    } = this.props
-
-    if (!provider || !yourAddress) {
-      // Temporary solution. We need to implement the empty states for other
-      // pages.
-      return <Header title="Choose wallet" />
-    }
-
-    if (error) {
-      return null
-    }
-
-    return contractsDataIsFetching ? (
-      <Loadable />
-    ) : (
+  render() {
+    return (
       <Switch>
-        <Route exact path="/overview">
-          {/* Temporary solution. We need to implement the `Overview` page as a separate page. */}
-          <Redirect to="/tokens" />
+        {/* In case that users will have bookmarked the old link. */}
+        <Route exact path="/glossary">
+          <Redirect to="/resources/quick-terminology" />
         </Route>
+        <Route exact path="/grant/:grantId" component={TokenGrantPreviewPage} />
         {pages.map(renderPage)}
-        {isKeepTokenContractDeployer && (
-          <Route
-            exact
-            path="/create-token-grants"
-            component={CreateTokenGrantPage}
-          />
-        )}
         <Route exact path="/">
           <Redirect to="/tokens" />
         </Route>
@@ -67,35 +38,38 @@ class Routing extends React.Component {
       </Switch>
     )
   }
-
-  render() {
-    return (
-      <Switch>
-        {withoutWalletPages.map(renderPage)}
-        {/* In case that users will have bookmarked the old link. */}
-        <Route exact path="/glossary">
-          <Redirect to="/resources/quick-terminology" />
-        </Route>
-        <Route exact path="/grant/:grantId" component={TokenGrantPreviewPage} />
-        <Route path="/">{this.renderContent()}</Route>
-      </Switch>
-    )
-  }
 }
 
 export const renderPage = (PageComponent, index) => {
   return (
-    <Route
+    <CustomRoute
       key={`${PageComponent.route.path}-${index}`}
       path={PageComponent.route.path}
       exact={PageComponent.route.exact}
-    >
-      <PageComponent
-        routes={PageComponent.route.pages}
-        title={PageComponent.route.title}
-      />
-    </Route>
+      component={PageComponent}
+      {...PageComponent.route}
+    />
   )
 }
 
-export default withWeb3Context(withContractsDataContext(Routing))
+const CustomRoute = ({
+  path,
+  exact,
+  component: Component,
+  emptyStateComponent: EmptyStateComponent = null,
+  withConnectWalletGuard = false,
+  ...componentProps
+}) => {
+  const { yourAddress, provider } = useWeb3Context()
+
+  return !withConnectWalletGuard ||
+    (withConnectWalletGuard && yourAddress && provider) ? (
+    <Route path={path} exact={exact}>
+      <Component routes={Component.route.pages} {...componentProps} />
+    </Route>
+  ) : (
+    <EmptyStateComponent />
+  )
+}
+
+export default Routing
