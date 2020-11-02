82e3faf2b67b6e0b873a9d33b46a1fbcbc693019
Dmitry <dymitr.paremski@keep.network>
2020-11-02 14:13:30 +0100

    Fixing typo

--- solidity/contracts/stubs/RewardsStub.sol
+++ solidity/contracts/stubs/RewardsStub.sol
@@ -28,11 +28,11 @@ contract RewardsStub is Rewards {
         receiveReward(bytes32(i));
     }
 
-    function receiveRewards(uint256[] memory identifieres) public {
-        uint256 len = identifieres.length;
+    function receiveRewards(uint256[] memory identifiers) public {
+        uint256 len = identifiers.length;
         bytes32[] memory bytes32identifiers = new bytes32[](len);
-        for (uint256 i = 0; i < identifieres.length; i++) {
-            bytes32identifiers[i] = bytes32(identifieres[i]);
+        for (uint256 i = 0; i < identifiers.length; i++) {
+            bytes32identifiers[i] = bytes32(identifiers[i]);
         }
         receiveRewards(bytes32identifiers);
     }
