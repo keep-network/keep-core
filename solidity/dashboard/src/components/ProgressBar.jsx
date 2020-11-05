37f95b11b7f0d32d6988f94eb504d787396c7824
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-11-05 14:14:10 +0100

    Make ProgressBar component more reusable
    Refactored `ProgressBar` component using compund component pattern and
    hooks.

--- solidity/dashboard/src/components/ProgressBar.jsx
+++ solidity/dashboard/src/components/ProgressBar.jsx
@@ -1,6 +1,7 @@
-import React, { useMemo } from "react"
-import { displayAmountWithMetricSuffix } from "../utils/token.utils"
+import React, { useMemo, useContext } from "react"
 import BigNumber from "bignumber.js"
+import CircularProgressBar from "./CircularProgressBar"
+import { percentageOf, sub } from "../utils/arithmetics.utils"
 
 const defaultValue = 0
 const totalDefaultValue = 1
@@ -12,31 +13,114 @@ const calculateWidth = (value, total) => {
   return valueInBn.multipliedBy(100).div(totalInBn).toFixed(2).toString()
 }
 
-const ProgressBar = ({ total, items, height, withLegend, styles }) => {
-  const bars = useMemo(() => {
-    return items
-      .map((item) => ({ ...item, width: calculateWidth(item.value, total) }))
-      .sort((a, b) => b.width - a.width)
-      .map((item, index) => (
-        <ProgressBarItem
-          key={index}
-          {...item}
-          index={index}
-          wrapperHeight={height}
-        />
-      ))
-  }, [total, items, height])
+const ProgressBarContext = React.createContext({ value: 0, total: 0 })
+
+const useProgressBarContext = () => {
+  const context = useContext(ProgressBarContext)
+
+  if (!context) {
+    throw new Error("ProgressBarContext used outside of ProgressBar component")
+  }
+
+  return context
+}
+
+const ProgressBar = ({ value, total, color, bgColor, children }) => {
+  return (
+    <ProgressBarContext.Provider value={{ value, total, color, bgColor }}>
+      {children}
+    </ProgressBarContext.Provider>
+  )
+}
+
+const ProgressBarInline = ({ height = 10, className = "" }) => {
+  const { value, total, color, bgColor } = useProgressBarContext()
+
+  const barWidth = useMemo(() => calculateWidth(value, total), [value, total])
 
   return (
-    <React.Fragment>
+    <div
+      className={`progress-bar-wrapper ${className}`}
+      style={{
+        height: `${height}px`,
+        backgroundColor: bgColor,
+      }}
+    >
       <div
-        className="progress-bar-wrapper"
-        style={{ height: `${height}px`, ...styles }}
-      >
-        {bars}
+        className="progress-bar"
+        style={{
+          width: `${barWidth}%`,
+          backgroundColor: color,
+        }}
+      />
+    </div>
+  )
+}
+
+const defaultDisplayLegendValuFn = (value) => value.toString()
+const ProgressBarLegendContext = React.createContext({
+  displayLegendValuFn: defaultDisplayLegendValuFn,
+})
+
+const useProgressBarLegendContext = () => {
+  const context = useContext(ProgressBarLegendContext)
+
+  if (!context) {
+    throw new Error(
+      "ProgressBarLegendContext used outside of ProgressBar component"
+    )
+  }
+
+  return context
+}
+
+const ProgressBarLegend = ({
+  valueLabel,
+  leftValueLabel,
+  displayLegendValuFn = defaultDisplayLegendValuFn,
+}) => {
+  const { value, total, color, bgColor } = useProgressBarContext()
+  const leftValue = useMemo(() => sub(total, value).toString(), [value, total])
+
+  return (
+    <ProgressBarLegendContext.Provider value={{ displayLegendValuFn }}>
+      <div className="progress-bar__legend">
+        <ProgressBarLegendItem
+          value={leftValue}
+          label={leftValueLabel}
+          color={bgColor}
+        />
+        <ProgressBarLegendItem value={value} label={valueLabel} color={color} />
       </div>
-      {withLegend && items.map(renderProgressBarLegendItem)}
-    </React.Fragment>
+    </ProgressBarLegendContext.Provider>
+  )
+}
+
+export const ProgressBarLegendItem = React.memo(({ value, label, color }) => {
+  const { displayLegendValuFn } = useProgressBarLegendContext()
+
+  return (
+    <div className="progress-bar-legend__item">
+      <div className="legend__item__dot" style={{ backgroundColor: color }} />
+      <span className="legend__item__value">
+        {displayLegendValuFn(value)}&nbsp;
+      </span>
+      <span className="legend__item__label">{label}</span>
+    </div>
+  )
+})
+
+const ProgressBarCircular = (props) => {
+  const { value, total, color, bgColor } = useProgressBarContext()
+
+  return (
+    <CircularProgressBar
+      {...props}
+      color={color}
+      backgroundStroke={bgColor}
+      total={total}
+      value={value}
+    />
   )
 }
 
@@ -44,33 +128,26 @@ export const renderProgressBarLegendItem = (item, index) => (
   <ProgressBarLegendItem key={index} {...item} />
 )
 
-export const ProgressBarLegendItem = React.memo(({ value, label, color }) => {
-  return (
-    <div className="progress-bar-legend__item">
-      <div className="legend__item__dot" style={{ backgroundColor: color }} />
-      <span className="legend__item__value">
-        {displayAmountWithMetricSuffix(value)}&nbsp;
-      </span>
-      <span className="legend__item__label">{label}</span>
-    </div>
+const PercentageLabel = ({ text, className = "" }) => {
+  const { value, total } = useProgressBarContext()
+  const percentageOfValue = useMemo(
+    () => percentageOf(value, total).toString(),
+    [value, total]
   )
-})
 
-const ProgressBarItem = React.memo(({ width, color, wrapperHeight, index }) => (
-  <div
-    className="progress-bar"
-    style={{
-      width: `${width}%`,
-      zIndex: index + 1,
-      backgroundColor: color,
-      height: `${index === 0 ? wrapperHeight : wrapperHeight - index - 1}px`,
-    }}
-  />
-))
-
-ProgressBar.defaultProps = {
-  height: "10",
-  styles: {},
+  return (
+    <span className={`progress-bar__percentage-label ${className}`}>
+      <span className="progress-bar__percentage-label__value">
+        {percentageOfValue}%
+      </span>
+      <span className="progress-bar__percentage-label__text">{text}</span>
+    </span>
+  )
 }
 
-export default React.memo(ProgressBar)
+ProgressBar.Inline = ProgressBarInline
+ProgressBar.Legend = ProgressBarLegend
+ProgressBar.Circular = ProgressBarCircular
+ProgressBar.PercentageLabel = PercentageLabel
+
+export default ProgressBar
