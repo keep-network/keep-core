1c38d08f86ce71d581ce9a8c059af8d7bb47ab5a
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-11-06 12:57:07 +0100

    Update FormInput component
    Added 3 new props:
    * instructionText- displays the additonal text next to input label,
    * leftIcon- displays the icon centered against only the input container,
    not `label + input + validation error`,
    * inputAddon- displays the additional component at the end of the input
    container.

--- solidity/dashboard/src/components/FormInput.jsx
+++ solidity/dashboard/src/components/FormInput.jsx
@@ -3,13 +3,48 @@ import { useField } from "formik"
 import SpeechBubbleTooltip from "./SpeechBubbleTooltip"
 import { colors } from "../constants/colors"
 
-const FormInput = ({ label, format, normalize, tooltipText, ...props }) => {
+const iconDefaultValues = {
+  width: 32,
+  heigh: 32,
+  marginRight: 1.5,
+}
+
+const FormInput = ({
+  label,
+  format,
+  normalize,
+  tooltipText,
+  instructionText,
+  leftIcon,
+  inputAddon,
+  ...props
+}) => {
   const [field, meta, helpers] = useField(props.name, props.type)
 
+  const leftIconComponent =
+    leftIcon && React.isValidElement(leftIcon)
+      ? React.cloneElement(leftIcon, {
+          width: iconDefaultValues.width,
+          height: iconDefaultValues.height,
+          style: { marginRight: `${iconDefaultValues.marginRight}rem` },
+        })
+      : null
+
+  const alignToInput = leftIconComponent
+    ? {
+        marginLeft: `calc(${iconDefaultValues.width}px + ${iconDefaultValues.marginRight}rem)`,
+      }
+    : {}
   return (
     <div className="form-input flex flex-1 column">
-      <label className="flex center">
-        <span className="mr-1">{label}</span>
+      <label className="input__label" style={alignToInput}>
+        <span className="input__label__text">{label}</span>
+        {instructionText && (
+          <span className="input__label__instruction-text">
+            {instructionText}
+          </span>
+        )}
+        {/* TODO change tolltip when it becomes available. PR is in progress*/}
         {tooltipText && (
           <SpeechBubbleTooltip
             text={tooltipText}
@@ -18,17 +53,23 @@ const FormInput = ({ label, format, normalize, tooltipText, ...props }) => {
           />
         )}
       </label>
-      <input
-        {...field}
-        {...props}
-        onChange={(event) => {
-          const value = event && event.target ? event.target.value : event
-          helpers.setValue(normalize ? normalize(value) : value)
-        }}
-        value={format ? format(field.value) : field.value}
-      />
+      <div className="form-input__wrapper">
+        {leftIconComponent}
+        <input
+          {...field}
+          {...props}
+          onChange={(event) => {
+            const value = event && event.target ? event.target.value : event
+            helpers.setValue(normalize ? normalize(value) : value)
+          }}
+          value={format ? format(field.value) : field.value}
+        />
+        <div className="form-input__addon">{inputAddon}</div>
+      </div>
       {meta.touched && meta.error ? (
-        <div className="form-error">{meta.error}</div>
+        <div className="form-input__error" style={alignToInput}>
+          {meta.error}
+        </div>
       ) : null}
     </div>
   )
