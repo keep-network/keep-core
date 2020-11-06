4e4002ce95c12a1e748efd85dba455f06f3fd79e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-11-06 13:00:47 +0100

    Add onClick prop to the Tag component

--- solidity/dashboard/src/components/Tag.jsx
+++ solidity/dashboard/src/components/Tag.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 
-const Tag = ({ IconComponent, text, type, size }) => {
+const Tag = ({ IconComponent, text, type, size, onClick = () => {} }) => {
   return (
     <div
       className={`tag${type ? `--${type}` : ""} ${size ? `tag--${size}` : ""}`}
+      onClick={onClick}
     >
       <div className="flex row center">
         <div className="tag__icon">
