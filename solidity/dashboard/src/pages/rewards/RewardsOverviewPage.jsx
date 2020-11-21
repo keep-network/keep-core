aa1dcc38b296535e983a5552187cb8f1869ad16b
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-11-21 11:35:16 +0100

    Add tBTC rewards to the rewards overview page

--- solidity/dashboard/src/pages/rewards/RewardsOverviewPage.jsx
+++ solidity/dashboard/src/pages/rewards/RewardsOverviewPage.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useCallback } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import TokenAmount from "../../components/TokenAmount"
-// import { SubmitButton } from "../../components/Button"
-import BeaconRewardsDetails from "../../components/BeaconRewardsDetails"
+import { SubmitButton } from "../../components/Button"
+import {
+  BeaconRewardsDetails,
+  ECDSARewardsDetails,
+} from "../../components/RewardsDetails"
 import StakeDropChart from "../../components/StakeDropChart"
 import { useWeb3Context } from "../../components/WithWeb3Context"
 import { TokenAmountSkeleton } from "../../components/skeletons"
@@ -10,33 +13,73 @@ import { TokenAmountSkeleton } from "../../components/skeletons"
 const RewardsOverviewPage = () => {
   const dispatch = useDispatch()
   const { yourAddress } = useWeb3Context()
-  const { beaconRewardsFetching, becaonRewardsBalance } = useSelector(
-    (state) => state.rewards
-  )
+  const {
+    beaconRewardsFetching,
+    becaonRewardsBalance,
+    ecdsaDistributedBalance,
+    ecdsaAvailableRewardsFetching,
+    ecdsaAvailableRewardsBalance,
+    ecdsaAvailableRewards,
+  } = useSelector((state) => state.rewards)
 
   useEffect(() => {
     dispatch({
       type: "rewards/beacon_fetch_distributed_rewards_request",
       payload: yourAddress,
     })
+    dispatch({
+      type: "rewards/ecdsa_fetch_distributed_rewards_request",
+      payload: yourAddress,
+    })
+    dispatch({
+      type: "rewards/ecdsa_fetch_available_rewards_request",
+      payload: yourAddress,
+    })
   }, [dispatch, yourAddress])
 
+  const onWithdrawECDSARewards = useCallback(
+    (awaitingPromise) => {
+      dispatch({
+        type: "rewards/ecdsa_withdraw",
+        payload: ecdsaAvailableRewards,
+        meta: awaitingPromise,
+      })
+    },
+    [dispatch, ecdsaAvailableRewards]
+  )
+
   return (
-    <section className="rewards-overview--random-beacon">
-      <section className="tile">
-        <Balance
-          title="Random Beacon Rewards"
-          rewardsBalance={becaonRewardsBalance}
-          isBalanceFetching={beaconRewardsFetching}
-        />
-        <section className="mt-2">
-          <BeaconRewardsDetails />
+    <>
+      <section className="rewards-overview--random-beacon">
+        <section className="tile">
+          <Balance
+            title="Random Beacon Rewards"
+            rewardsBalance={becaonRewardsBalance}
+            isBalanceFetching={beaconRewardsFetching}
+          />
+          <section className="mt-2">
+            <BeaconRewardsDetails />
+          </section>
+        </section>
+        <section className="tile">
+          <StakeDropChart />
         </section>
       </section>
-      <section className="tile">
-        <StakeDropChart />
+
+      <section className="rewards-overview--ecdsa">
+        <section className="tile">
+          <Balance
+            title="tBTC Rewards"
+            rewardsBalance={ecdsaAvailableRewardsBalance}
+            isBalanceFetching={ecdsaAvailableRewardsFetching}
+            onWithdrawAll={onWithdrawECDSARewards}
+          />
+        </section>
+        <section className="tile">
+          <ECDSARewardsDetails pastRewards={ecdsaDistributedBalance} />
+        </section>
       </section>
-    </section>
+    </>
   )
 }
 
@@ -54,13 +97,14 @@ const Balance = ({
       ) : (
         <TokenAmount amount={rewardsBalance} currencySymbol="KEEP" />
       )}
-
-      {/* <div className="flex column wrap ">
-        <SubmitButton onClick={onWithdrawAll}>withdraw all</SubmitButton>
-        <span className="text-validation">
-          The beneficiary account receives all withdrawn rewards.
-        </span>
-      </div> */}
+      {onWithdrawAll && (
+        <SubmitButton
+          onSubmitAction={onWithdrawAll}
+          className="btn btn-primary btn-lg mt-2"
+        >
+          withdraw all
+        </SubmitButton>
+      )}
     </>
   )
 }
