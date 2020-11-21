ea43ef1b135850cc3117a17764f23882b99a8243
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-11-21 11:33:51 +0100

    Fetching available and distributed tBTC rewards

--- solidity/dashboard/src/reducers/rewards.js
+++ solidity/dashboard/src/reducers/rewards.js
@@ -1,7 +1,19 @@
 const initialState = {
+  // Beacon distributted rewards
   beaconRewardsFetching: false,
   becaonRewardsBalance: 0,
   error: null,
+
+  // ECDSA distributed rewards
+  ecdsaDistributedRewardsFetching: false,
+  ecdsaDistributedBalance: 0,
+  ecdsaDistributedBalanceError: null,
+
+  // ECDSA available rewards
+  ecdsaAvailableRewardsFetching: false,
+  ecdsaAvailableRewardsBalance: 0,
+  ecdsaAvailableRewards: [],
+  ecdsaAvailableRewardsError: null,
 }
 
 const rewardsReducer = (state = initialState, action) => {
@@ -18,7 +30,44 @@ const rewardsReducer = (state = initialState, action) => {
       return {
         ...state,
         beaconRewardsFetching: false,
-        eror: action.payload.error,
+        error: action.payload.error,
+      }
+    case "rewards/ecdsa_fetch_distributed_rewards_start":
+      return {
+        ...state,
+        ecdsaDistributedRewardsFetching: true,
+        ecdsaDistributedBalanceError: null,
+      }
+    case "rewards/ecdsa_fetch_distributed_rewards_success":
+      return {
+        ...state,
+        ecdsaDistributedRewardsFetching: false,
+        ecdsaDistributedBalance: action.payload,
+      }
+    case "rewards/ecdsa_fetch_distributed_rewards_failure":
+      return {
+        ...state,
+        ecdsaDistributedRewardsFetching: false,
+        ecdsaDistributedBalanceError: action.payload.error,
+      }
+    case "rewards/ecdsa_fetch_available_rewards_start":
+      return {
+        ...state,
+        ecdsaAvailableRewardsFetching: true,
+        ecdsaAvailableRewardsError: null,
+      }
+    case "rewards/ecdsa_fetch_available_rewards_success":
+      return {
+        ...state,
+        ecdsaAvailableRewardsFetching: false,
+        ecdsaAvailableRewardsBalance: action.payload.totalAvailableRewards,
+        ecdsaAvailableRewards: action.payload.toWithdrawn,
+      }
+    case "rewards/ecdsa_fetch_available_rewards_failure":
+      return {
+        ...state,
+        ecdsaAvailableRewardsFetching: false,
+        ecdsaAvailableRewardsError: action.payload.error,
       }
     default:
       return state
