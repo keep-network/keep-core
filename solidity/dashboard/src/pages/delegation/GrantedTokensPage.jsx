905e25922af22b2f5cad242efcc23f97dc5059fb
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-11-27 15:31:19 +0100

    Update page's route
    To keep consistency across other links in the left side menu, we decided
    to changed `Delegation /delegation` to `Delegations /delegations`
    because the rest of the links are in plural.

--- solidity/dashboard/src/pages/delegation/GrantedTokensPage.jsx
+++ solidity/dashboard/src/pages/delegation/GrantedTokensPage.jsx
@@ -180,7 +180,7 @@ const GrantedTokensPage = () => (
 
 GrantedTokensPage.route = {
   title: "Granted Tokens",
-  path: "/delegation/granted",
+  path: "/delegations/granted",
   exact: true,
   withConnectWalletGuard: true,
   emptyStateComponent: EmptyStateComponent,
