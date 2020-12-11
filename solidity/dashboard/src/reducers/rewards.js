ceb8cdcf7c065b15509f6619034451a3e386a23d
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-12-11 14:56:30 +0100

    Update saga that fetches the ecdsa rewards

--- solidity/dashboard/src/reducers/rewards.js
+++ solidity/dashboard/src/reducers/rewards.js
@@ -6,11 +6,6 @@ const initialState = {
   becaonRewardsBalance: 0,
   error: null,
 
-  // ECDSA distributed rewards
-  ecdsaDistributedRewardsFetching: false,
-  ecdsaDistributedBalance: 0,
-  ecdsaDistributedBalanceError: null,
-
   // ECDSA available rewards
   ecdsaAvailableRewardsFetching: false,
   ecdsaAvailableRewardsBalance: 0,
@@ -34,38 +29,20 @@ const rewardsReducer = (state = initialState, action) => {
         beaconRewardsFetching: false,
         error: action.payload.error,
       }
-    case "rewards/ecdsa_fetch_distributed_rewards_start":
-      return {
-        ...state,
-        ecdsaDistributedRewardsFetching: true,
-        ecdsaDistributedBalanceError: null,
-      }
-    case "rewards/ecdsa_fetch_distributed_rewards_success":
-      return {
-        ...state,
-        ecdsaDistributedRewardsFetching: false,
-        ecdsaDistributedBalance: action.payload,
-      }
-    case "rewards/ecdsa_fetch_distributed_rewards_failure":
-      return {
-        ...state,
-        ecdsaDistributedRewardsFetching: false,
-        ecdsaDistributedBalanceError: action.payload.error,
-      }
-    case "rewards/ecdsa_fetch_available_rewards_start":
+    case "rewards/ecdsa_fetch_rewards_data_start":
       return {
         ...state,
         ecdsaAvailableRewardsFetching: true,
         ecdsaAvailableRewardsError: null,
       }
-    case "rewards/ecdsa_fetch_available_rewards_success":
+    case "rewards/ecdsa_fetch_rewards_data_success":
       return {
         ...state,
         ecdsaAvailableRewardsFetching: false,
-        ecdsaAvailableRewardsBalance: action.payload.totalAvailableRewards,
-        ecdsaAvailableRewards: action.payload.toWithdrawn,
+        ecdsaAvailableRewardsBalance: action.payload.totalAvailableAmount,
+        ecdsaAvailableRewards: action.payload.availableRewards,
       }
-    case "rewards/ecdsa_fetch_available_rewards_failure":
+    case "rewards/ecdsa_fetch_rewards_data_failure":
       return {
         ...state,
         ecdsaAvailableRewardsFetching: false,
--- solidity/dashboard/src/sagas/rewards.js
+++ solidity/dashboard/src/sagas/rewards.js
@@ -1,10 +1,21 @@
-import { take, takeLatest, call, put, fork } from "redux-saga/effects"
+import {
+  take,
+  takeLatest,
+  call,
+  put,
+  fork,
+  select,
+  takeEvery,
+} from "redux-saga/effects"
 import { logError, submitButtonHelper, getContractsContext } from "./utils"
 import {
   fetchtTotalDistributedRewards,
   fetchECDSAAvailableRewards,
+  fetchECDSAClaimedRewards,
 } from "../services/rewards"
 import { sendTransaction } from "./web3"
+import { isSameEthAddress } from "../utils/general.utils"
+import { add } from "../utils/arithmetics.utils"
 
 function* fetchBeaconDistributedRewards(address) {
   try {
@@ -28,53 +39,6 @@ export function* watchFetchBeaconDistributedRewards() {
     "rewards/beacon_fetch_distributed_rewards_request"
   )
   yield fork(fetchBeaconDistributedRewards, payload)
-}
-
-function* fetchECDSADistributedRewards(address) {
-  try {
-    yield put({ type: "rewards/ecdsa_fetch_distributed_rewards_start" })
-    const balance = yield call(
-      fetchtTotalDistributedRewards,
-      address,
-      "ECDSARewardsContract"
-    )
-    yield put({
-      type: "rewards/ecdsa_fetch_distributed_rewards_success",
-      payload: balance,
-    })
-  } catch (error) {
-    yield* logError("rewards/ecdsa_fetch_distributed_rewards_failure", error)
-  }
-}
-
-export function* watchFetchECDSADistributedRewards() {
-  const { payload } = yield take(
-    "rewards/ecdsa_fetch_distributed_rewards_request"
-  )
-  yield fork(fetchECDSADistributedRewards, payload)
-}
-
-function* fetchECDSAAvailabledRewards(address) {
-  try {
-    yield put({ type: "rewards/ecdsa_fetch_available_rewards_start" })
-    const { totalAvailableRewards, toWithdrawn } = yield call(
-      fetchECDSAAvailableRewards,
-      address
-    )
-    yield put({
-      type: "rewards/ecdsa_fetch_available_rewards_success",
-      payload: { totalAvailableRewards, toWithdrawn },
-    })
-  } catch (error) {
-    yield* logError("rewards/ecdsa_fetch_available_rewards_failure", error)
-  }
-}
-
-export function* watchFetchECDSAAvailableRewards() {
-  const { payload } = yield take(
-    "rewards/ecdsa_fetch_available_rewards_request"
-  )
-  yield fork(fetchECDSAAvailabledRewards, payload)
 }
 
 function* withdrawECDSARewards(action) {
@@ -107,4 +71,58 @@ export function* watchWithdrawECDSARewards() {
   yield takeLatest("rewards/ecdsa_withdraw", function* (action) {
     yield call(submitButtonHelper, withdrawECDSARewards, action)
   })
+}
+
+export function* fetchECDSARewardsData() {
+  try {
+    yield put({ type: "rewards/ecdsa_fetch_rewards_data_request" })
+    // Fetching delegations for a current loggeed account. Operators are
+    // required to fetch ecdsa available rewards for the given owner.
+    yield put({ type: "staking/fetch_delegations_request" })
+    const operators = yield call(getOperatorsFromStore)
+
+    let availableRewards = yield call(fetchECDSAAvailableRewards, operators)
+    const claimedRewards = yield call(fetchECDSAClaimedRewards, operators)
+
+    availableRewards = availableRewards.filter(
+      ({ operator, merkleRoot }) =>
+        !claimedRewards.find(
+          (lookup) =>
+            isSameEthAddress(operator, lookup) &&
+            merkleRoot === lookup.merkleRoot
+        )
+    )
+
+    const totalAvailableAmount = availableRewards.reduce(
+      (reducer, _) => add(reducer, _.amount),
+      0
+    )
+    yield put({
+      type: "rewards/ecdsa_fetch_rewards_data_success",
+      payload: { totalAvailableAmount, availableRewards },
+    })
+  } catch (error) {
+    yield* logError("rewards/ecdsa_fetch_rewards_data_failure", error)
+  }
+}
+
+function* getOperatorsFromStore() {
+  const getDelegationsFetchingStatus = (state) =>
+    state.staking.delegationsFetchingStatus
+
+  // Waiting for delegation reqest
+  let delegationsFetchingStatus = yield select(getDelegationsFetchingStatus)
+  while (delegationsFetchingStatus !== "completed") {
+    yield take()
+    delegationsFetchingStatus = yield select(getDelegationsFetchingStatus)
+  }
+  const { delegations } = yield select((state) => state.staking)
+  return [...delegations].map(({ operatorAddress }) => operatorAddress)
+}
+
+export function* watchFetchECDSARewards() {
+  yield takeEvery(
+    "rewards/ecdsa_fetch_rewards_data_request",
+    fetchECDSARewardsData
+  )
 }
