2f9cbc9ce668aebae8b8a40cc3d0d388addc17b9
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-12-15 13:13:46 +0100

    Add to the redux store rewards history array
    Rewards history array stores the objects that represent reward. Each
    reawrd object has one of the statuses- `withdrawn` or `available`.

--- solidity/dashboard/src/sagas/rewards.js
+++ solidity/dashboard/src/sagas/rewards.js
@@ -1,11 +1,4 @@
-import {
-  take,
-  takeLatest,
-  call,
-  put,
-  fork,
-  takeEvery,
-} from "redux-saga/effects"
+import { take, takeLatest, call, put, fork } from "redux-saga/effects"
 import { logError, submitButtonHelper, getContractsContext } from "./utils"
 import {
   fetchtTotalDistributedRewards,
@@ -43,28 +36,27 @@ export function* watchFetchBeaconDistributedRewards() {
 
 function* withdrawECDSARewards(action) {
   const { payload: availableRewards } = action
-  const { ECDSARewardsContract } = yield getContractsContext()
-  const unapproved = []
+  const { ECDSARewardsDistributorContract } = yield getContractsContext()
 
-  for (const { operator, interval, withdrawable } of availableRewards) {
+  for (const {
+    merkleRoot,
+    index,
+    operator,
+    amount,
+    proof,
+  } of availableRewards) {
     try {
       yield call(sendTransaction, {
         payload: {
-          contract: ECDSARewardsContract,
-          methodName: "withdrawRewards",
-          args: [interval, operator],
+          contract: ECDSARewardsDistributorContract,
+          methodName: "claim",
+          args: [merkleRoot, index, operator, amount, proof],
         },
       })
     } catch (error) {
-      unapproved.push({ operator, interval, withdrawable })
       continue
     }
   }
-
-  yield put({
-    type: "rewards/ecdsa_update_available_rewards",
-    payload: unapproved,
-  })
 }
 
 export function* watchWithdrawECDSARewards() {
@@ -73,13 +65,14 @@ export function* watchWithdrawECDSARewards() {
   })
 }
 
-function* fetchECDSARewardsData(beneficiary) {
+function* fetchECDSARewardsData(action) {
+  const beneficiary = action.payload
   try {
-    yield put({ type: "rewards/ecdsa_fetch_rewards_data_request" })
+    yield put({ type: "rewards/ecdsa_fetch_rewards_data_start" })
 
-    const opeerators = yield call(getOperatorsOfBeneficiary, beneficiary)
-    let availableRewards = yield call(fetchECDSAAvailableRewards, opeerators)
-    const claimedRewards = yield call(fetchECDSAClaimedRewards, opeerators)
+    const operators = yield call(getOperatorsOfBeneficiary, beneficiary)
+    let availableRewards = yield call(fetchECDSAAvailableRewards, operators)
+    const claimedRewards = yield call(fetchECDSAClaimedRewards, operators)
 
     // Available rewards are fetched from merkle generator's output file. This
     // file doesn't take into account a rewards alredy claimed. So we need to
@@ -88,7 +81,7 @@ function* fetchECDSARewardsData(beneficiary) {
       ({ operator, merkleRoot }) =>
         !claimedRewards.find(
           (lookup) =>
-            isSameEthAddress(operator, lookup) &&
+            isSameEthAddress(operator, lookup.operator) &&
             merkleRoot === lookup.merkleRoot
         )
     )
@@ -98,12 +91,26 @@ function* fetchECDSARewardsData(beneficiary) {
       0
     )
 
+    const rewardsHistory = availableRewards
+      .map((reward) => ({
+        ...reward,
+        status: "AVAILABLE",
+        id: `${reward.operator}-${reward.merkleRoot}`,
+      }))
+      .concat(
+        claimedRewards.map((reward) => ({
+          ...reward,
+          status: "WITHDRAWN",
+          id: `${reward.operator}-${reward.merkleRoot}`,
+        }))
+      )
+
     yield put({
       type: "rewards/ecdsa_fetch_rewards_data_success",
       payload: {
         totalAvailableAmount,
         availableRewards,
-        claimedRewards,
+        rewardsHistory,
       },
     })
   } catch (error) {
@@ -112,7 +119,7 @@ function* fetchECDSARewardsData(beneficiary) {
 }
 
 export function* watchFetchECDSARewards() {
-  yield takeEvery(
+  yield takeLatest(
     "rewards/ecdsa_fetch_rewards_data_request",
     fetchECDSARewardsData
   )
