3f82d6dd1b249b3a8a307513a0d662a4d2c4e8cf
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-12-15 13:19:54 +0100

    Dispatch actions in the TBTCRewardsPage
    Dispatched actions that fetches ecdsa available rewards.

--- solidity/dashboard/src/pages/rewards/TBTCRewardsPage.jsx
+++ solidity/dashboard/src/pages/rewards/TBTCRewardsPage.jsx
@@ -1,51 +1,96 @@
-import React from "react"
+import React, { useEffect, useCallback } from "react"
+import { useDispatch, useSelector } from "react-redux"
+// import { Link } from "react-router-dom"
 import TokenAmount from "../../components/TokenAmount"
 import * as Icons from "../../components/Icons"
 import Tooltip from "../../components/Tooltip"
-import { Link } from "react-router-dom"
 import { SubmitButton } from "../../components/Button"
-import ProgressBar from "../../components/ProgressBar"
+// import ProgressBar from "../../components/ProgressBar"
 import Timer from "../../components/Timer"
 import TBTCRewardsDataTable from "../../components/TBTCRewardsDataTable"
-import { colors } from "../../constants/colors"
-import { ECDSARewardsHelper } from "../../utils/rewardsHelper";
+import { ECDSARewardsHelper } from "../../utils/rewardsHelper"
+import { useWeb3Address } from "../../components/WithWeb3Context"
+import {
+  TokenAmountSkeleton,
+  DataTableSkeleton,
+} from "../../components/skeletons"
+import { gt } from "../../utils/arithmetics.utils"
+import { LoadingOverlay } from "../../components/Loadable"
+// import { colors } from "../../constants/colors"
 
 const TBTCRewardsPage = () => {
+  const dispatch = useDispatch()
+  const yourAddress = useWeb3Address()
   const currentIntervalEndOf = ECDSARewardsHelper.intervalEndOf(
     ECDSARewardsHelper.currentInterval
   ).unix()
 
+  const {
+    ecdsaAvailableRewardsFetching,
+    ecdsaAvailableRewardsBalance,
+    ecdsaAvailableRewards,
+    ecdsaRewardsHistory,
+  } = useSelector((state) => state.rewards)
+
+  useEffect(() => {
+    dispatch({
+      type: "rewards/ecdsa_fetch_rewards_data_request",
+      payload: yourAddress,
+    })
+  }, [dispatch, yourAddress])
+
+  const withdrawRewards = useCallback(
+    (submitButtonPromise) => {
+      dispatch({
+        type: "rewards/ecdsa_withdraw",
+        payload: ecdsaAvailableRewards,
+        meta: submitButtonPromise,
+      })
+    },
+    [dispatch, ecdsaAvailableRewards]
+  )
+
   return (
     <>
-      <RewardsOverview />
+      <RewardsOverview
+        balance={ecdsaAvailableRewardsBalance}
+        isBalanceFetching={ecdsaAvailableRewardsFetching}
+        withdrawRewards={withdrawRewards}
+      />
       <section className="tile rewards-countdown">
         <h2 className="h2--alt">
           Next rewards release:&nbsp;
           <Timer targetInUnix={currentIntervalEndOf} />
         </h2>
       </section>
-      <section className="rewards-calc">
-        <RewardsBoost />
-        <LockedETH />
-        <APY />
-      </section>
-      <section className="rewards-history">
-        <TBTCRewardsDataTable />
-      </section>
+      <LoadingOverlay
+        isFetching={ecdsaAvailableRewardsFetching}
+        skeletonComponent={<DataTableSkeleton columns={4} />}
+      >
+        <section className="tile rewards-history">
+          <TBTCRewardsDataTable data={ecdsaRewardsHistory} />
+        </section>
+      </LoadingOverlay>
     </>
   )
 }
 
-const RewardsOverview = () => {
+const RewardsOverview = ({ balance, isBalanceFetching, withdrawRewards }) => {
   return (
     <section className="tile rewards__overview--tbtc">
       <div className="rewards__overview__balance">
         <h2 className="h2--alt text-grey-70 mb-1">tBTC Rewards</h2>
-        <TokenAmount amount="0" currencySymbol="KEEP" />
+        {isBalanceFetching ? (
+          <TokenAmountSkeleton />
+        ) : (
+          <TokenAmount amount={balance} currencySymbol="KEEP" />
+        )}
       </div>
       <div className="rewards__overview__period">
         <h5 className="text-grey-70">current rewards period</h5>
-        <span className="rewards-period__date">11/15/2020 - 12/15/2020</span>
+        <span className="rewards-period__date">
+          {ECDSARewardsHelper.periodOf(ECDSARewardsHelper.currentInterval)}
+        </span>
         <div className="rewards-period__remaining-periods">
           <Icons.Time width="16" height="16" className="time-icon--grey-30" />
           <span>84 rewards periods remaining&nbsp;</span>
@@ -57,7 +102,8 @@ const RewardsOverview = () => {
       <div className="rewards__overview__withdraw">
         <SubmitButton
           className="btn btn-primary btn-lg"
-          onSubmitAction={() => console.log("submit btn")}
+          onSubmitAction={withdrawRewards}
+          disabled={!gt(balance, 0)}
         >
           withdraw all
         </SubmitButton>
@@ -69,73 +115,74 @@ const RewardsOverview = () => {
   )
 }
 
-const RewardsBoost = () => {
-  return (
-    <section className="tile rewards-calc__boost">
-      <div>
-        <div className="boost__value">
-          <Icons.Plus width={48} height={48} />
-          <div className="ml-1">
-            <h3 className="text-grey-70">2.5x Boost</h3>
-            <h4 className="text-mint-100">500k KEEP</h4>
-          </div>
-        </div>
-        <ProgressBar value={10} total={100} color={colors.primary}>
-          <ProgressBar.Inline className="mb-0" />
-        </ProgressBar>
-        <div className="text-grey-40">500k KEEP</div>
-        <Link to="/delegations" className="btn btn-secondary mt-1 w-100">
-          stake keep
-        </Link>
-      </div>
-      <div>
-        <p>
-          Up to 3M KEEP staked per operator counts toward your reward boost.
-          Your boost multiplies the rewards you receive from bonding ETH.
-        </p>
-        <p>Ahigher boost means more rewards.</p>
-      </div>
-    </section>
-  )
-}
+// const RewardsBoost = () => {
+//   return (
+//     <section className="tile rewards-calc__boost">
+//       <div>
+//         <div className="boost__value">
+//           <Icons.Plus width={48} height={48} />
+//           <div className="ml-1">
+//             <h3 className="text-grey-70">2.5x Boost</h3>
+//             <h4 className="text-mint-100">500k KEEP</h4>
+//           </div>
+//         </div>
+//         <ProgressBar value={10} total={100} color={colors.primary}>
+//           <ProgressBar.Inline className="mb-0" />
+//         </ProgressBar>
+//         <div className="text-grey-40">500k KEEP</div>
+//         <Link to="/delegations" className="btn btn-secondary mt-1 w-100">
+//           stake keep
+//         </Link>
+//       </div>
+//       <div>
+//         <p>
+//           Up to 3M KEEP staked per operator counts toward your reward boost.
+//           Your boost multiplies the rewards you receive from bonding ETH.
+//         </p>
+//         <p>Ahigher boost means more rewards.</p>
+//       </div>
+//     </section>
+//   )
+// }
 
-const LockedETH = () => {
-  return (
-    <section className="tile rewards-calc__eth-locked">
-      <div className="eth-locked__value">
-        <Icons.ETH className="" />
-        <div className="ml-1">
-          <h3 className="text-grey-70">ETH Locked</h3>
-          <h4 className="text-mint-100">1000 ETH</h4>
-        </div>
-      </div>
-      <ProgressBar value={10} total={100} color={colors.primary}>
-        <ProgressBar.Inline className="mb-0" />
-      </ProgressBar>
-      <div className="text-grey-40">1000 ETH Locked</div>
-      <Link to="/applications/tbtc" className="btn btn-secondary mt-1 w-100">
-        lock eth
-      </Link>
-    </section>
-  )
-}
+// const LockedETH = () => {
+//   return (
+//     <section className="tile rewards-calc__eth-locked">
+//       <div className="eth-locked__value">
+//         <Icons.ETH className="" />
+//         <div className="ml-1">
+//           <h3 className="text-grey-70">ETH Locked</h3>
+//           <h4 className="text-mint-100">1000 ETH</h4>
+//         </div>
+//       </div>
+//       <ProgressBar value={10} total={100} color={colors.primary}>
+//         <ProgressBar.Inline className="mb-0" />
+//       </ProgressBar>
+//       <div className="text-grey-40">1000 ETH Locked</div>
+//       <Link to="/applications/tbtc" className="btn btn-secondary mt-1 w-100">
+//         lock eth
+//       </Link>
+//     </section>
+//   )
+// }
 
-const APY = () => {
-  return (
-    <section className="tile rewards-calc__apy">
-      <h2 className="apy__title">Your APY</h2>
-      <h1 className="apy__value">252%</h1>
-    </section>
-  )
-}
+// const APY = () => {
+//   return (
+//     <section className="tile rewards-calc__apy">
+//       <h2 className="apy__title">Your APY</h2>
+//       <h1 className="apy__value">252%</h1>
+//     </section>
+//   )
+// }
 
+const Component = () => <></>
 TBTCRewardsPage.route = {
   title: "tBTC",
   path: "/rewards/tbtc",
   exact: true,
-  withConnectWalletGuard: false,
+  withConnectWalletGuard: true,
   // TODO: empty state page component
-  //   emptyStateComponent: Component,
+  emptyStateComponent: Component,
 }
 
 export default TBTCRewardsPage
