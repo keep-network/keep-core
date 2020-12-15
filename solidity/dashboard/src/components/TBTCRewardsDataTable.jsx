26925b44e01884ae54161dc1e5b8b4f4736255de
Rafal Czajkowski <rafal.czajkowski@keep.network>
2020-12-15 13:01:07 +0100

    Render contents of the cells in the reawrd table

--- solidity/dashboard/src/components/TBTCRewardsDataTable.jsx
+++ solidity/dashboard/src/components/TBTCRewardsDataTable.jsx
@@ -1,15 +1,53 @@
 import React from "react"
 import { DataTable, Column } from "./DataTable"
+import Chip from "./Chip"
+import AddressShortcut from "./AddressShortcut"
+import * as Icons from "./Icons"
+import { displayAmount } from "../utils/token.utils"
 
 const TBTCRewardsDataTable = ({ data = [] }) => {
   return (
     <DataTable data={data} itemFieldId="id" noDataMessage="No rewards history.">
-      <Column header="amount" field="amount" />
-      <Column header="status" field="amount" />
-      <Column header="rewards period" field="period" />
-      <Column header="operator" field="operator" />
+      <Column header="amount" field="amount" renderContent={AmountCell} />
+      <Column header="status" field="status" renderContent={renderStatus} />
+      <Column header="rewards period" field="rewardsPeriod" />
+      <Column
+        header="operator"
+        field="operator"
+        renderContent={renderOperator}
+      />
     </DataTable>
   )
 }
+
+const AmountCell = ({ amount }) => {
+  return (
+    <div className="flex row center">
+      <Icons.KeepOutline
+        className="keep-outline--grey-40 mr-1"
+        width={32}
+        height={32}
+      />
+      <div>
+        <div>{displayAmount(amount)}&nbsp;KEEP</div>
+        <div className="flex row center">
+          <Icons.Rewards width={8} height={8} />
+          <span className="text-small text-grey-40">&nbsp;Reward</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const renderStatus = ({ status }) => {
+  switch (status) {
+    case "AVAILABLE":
+      return <Chip text={status} />
+    case "WITHDRAWN":
+    default:
+      return <Chip text={status} color="disabled" />
+  }
+}
+const renderOperator = ({ operator }) => <AddressShortcut address={operator} />
 
 export default TBTCRewardsDataTable
