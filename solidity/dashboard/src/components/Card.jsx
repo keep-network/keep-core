a33c195be2089108393e8b614df2da07295b630b
michalsmiarowski <michal.smiarowski@thesis.co>
2020-12-22 15:31:35 +0100

    Remove unnecessary scss imports
    Remove unnecessary scss imports in Card and CardContainer components.

--- solidity/dashboard/src/components/Card.jsx
+++ solidity/dashboard/src/components/Card.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import "../css/card.less"
 
 const Card = ({ className, children }) => {
   return (
--- solidity/dashboard/src/components/CardContainer.jsx
+++ solidity/dashboard/src/components/CardContainer.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import "../css/card-container.less"
 
 const CardContainer = ({ className, children }) => {
   return (
