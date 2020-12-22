96b8f2ea2b7e2cd04f22d04a76db6a2fa3b87d76
michalsmiarowski <michal.smiarowski@thesis.co>
2020-12-22 09:51:48 +0100

    Change constant name
    Change constant name. LIQUIDITY_REWARD_PAIR -> LIQUIDITY_REWARD_PAIRS

--- solidity/dashboard/src/pages/liquidity/LiquidityPage.jsx
+++ solidity/dashboard/src/pages/liquidity/LiquidityPage.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import PageWrapper from "../../components/PageWrapper"
 import CardContainer from "../../components/CardContainer"
 import LiquidityRewardCard from "../../components/LiquidityRewardCard"
-import { LIQUIDITY_REWARD_PAIR } from "../../constants/constants"
+import { LIQUIDITY_REWARD_PAIRS } from "../../constants/constants"
 import * as Icons from "../../components/Icons"
 
 const LiquidityPage = ({ title }) => {
@@ -10,22 +10,22 @@ const LiquidityPage = ({ title }) => {
     <PageWrapper title={title}>
       <CardContainer className={"flex wrap"}>
         <LiquidityRewardCard
-          title={LIQUIDITY_REWARD_PAIR.KEEP_ETH.label}
+          title={LIQUIDITY_REWARD_PAIRS.KEEP_ETH.label}
           MainIcon={Icons.KeepBlackGreen}
           SecondaryIcon={Icons.EthToken}
-          viewPoolLink={LIQUIDITY_REWARD_PAIR.KEEP_ETH.viewPoolLink}
+          viewPoolLink={LIQUIDITY_REWARD_PAIRS.KEEP_ETH.viewPoolLink}
         />
         <LiquidityRewardCard
-          title={LIQUIDITY_REWARD_PAIR.KEEP_TBTC.label}
+          title={LIQUIDITY_REWARD_PAIRS.KEEP_TBTC.label}
           MainIcon={Icons.KeepBlackGreen}
           SecondaryIcon={Icons.TBTC}
-          viewPoolLink={LIQUIDITY_REWARD_PAIR.KEEP_ETH.viewPoolLink}
+          viewPoolLink={LIQUIDITY_REWARD_PAIRS.KEEP_ETH.viewPoolLink}
         />
         <LiquidityRewardCard
-          title={LIQUIDITY_REWARD_PAIR.TBTC_ETH.label}
+          title={LIQUIDITY_REWARD_PAIRS.TBTC_ETH.label}
           MainIcon={Icons.TBTC}
           SecondaryIcon={Icons.EthToken}
-          viewPoolLink={LIQUIDITY_REWARD_PAIR.KEEP_ETH.viewPoolLink}
+          viewPoolLink={LIQUIDITY_REWARD_PAIRS.KEEP_ETH.viewPoolLink}
         />
       </CardContainer>
     </PageWrapper>
