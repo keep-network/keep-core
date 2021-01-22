4aaecda0dfdd478e2a28fbc2b3afae7e7a1303f4
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-01-22 18:59:43 +0100

    Defined staking token as IERC20
    Defining the staking token as KeepToken was too narrow as we may want
    the geyser contract to support staking also other tokens (e.g. LP tokens).

--- solidity/contracts/geyser/KeepTokenGeyser.sol
+++ solidity/contracts/geyser/KeepTokenGeyser.sol
@@ -3,11 +3,13 @@ pragma solidity 0.5.17;
 import "./TokenGeyser.sol";
 import "../KeepToken.sol";
 
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
 // TODO: Rename to KeepVault?
 // TODO: Add docs
 contract KeepTokenGeyser is TokenGeyser {
     constructor(
-        KeepToken stakingToken,
+        IERC20 _stakingToken,
         KeepToken distributionToken,
         uint256 maxUnlockSchedules,
         uint256 startBonus_,
@@ -16,7 +18,7 @@ contract KeepTokenGeyser is TokenGeyser {
     )
         public
         TokenGeyser(
-            stakingToken,
+            _stakingToken,
             distributionToken,
             maxUnlockSchedules,
             startBonus_,
