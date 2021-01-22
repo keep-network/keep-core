17a32fba0c787789650294d8c172c908bd6d294d
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-01-22 19:10:54 +0100

    Fund KeepTokenGeyser contract via receiveApproval
    Handled funding TokenGeyser contract (`lockTokens` function) with use of
    `receiveApproval` which is a part of `approveAndCall` pattern we use for
    transferring tokens out of an escrow to rewards contracts.
    
    We expect the transfer to come from a beneficiary contract that should
    be set in `TokenGeyser` contract as a funder.
    
    A function to fund the rewards (distribution) tokens requires an
    additional parameter `durationSec` that we expect to be constatnt in our
    case, but we let the contract owner to reconfigure it via setter.

--- solidity/contracts/geyser/KeepTokenGeyser.sol
+++ solidity/contracts/geyser/KeepTokenGeyser.sol
@@ -2,12 +2,17 @@ pragma solidity 0.5.17;
 
 import "./TokenGeyser.sol";
 import "../KeepToken.sol";
+import {IStakerRewards} from "../PhasedEscrow.sol";
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 // TODO: Rename to KeepVault?
 // TODO: Add docs
-contract KeepTokenGeyser is TokenGeyser {
+contract KeepTokenGeyser is TokenGeyser, IStakerRewards {
+    event DurationSecUpdated(uint256 oldDurationSec, uint256 newDurationSec);
+
+    uint256 private durationSec;
+
     /**
      * @param _stakingToken The token users deposit as a stake.
      * @param _distributionToken The token users are rewarded in and receive it as they unstake.
@@ -16,6 +21,7 @@ contract KeepTokenGeyser is TokenGeyser {
      *                    e.g. 25% means user gets 25% of max distribution tokens.
      * @param _bonusPeriodSec Length of time for bonus to increase linearly to max.
      * @param _initialSharesPerToken Number of shares to mint per staking token on first stake.
+     * @param _durationSec Length of time to linear unlock the rewards tokens.
      */
     constructor(
         IERC20 _stakingToken,
@@ -23,7 +29,8 @@ contract KeepTokenGeyser is TokenGeyser {
         uint256 _maxUnlockSchedules,
         uint256 _startBonus,
         uint256 _bonusPeriodSec,
-        uint256 _initialSharesPerToken
+        uint256 _initialSharesPerToken,
+        uint256 _durationSec
     )
         public
         TokenGeyser(
@@ -34,7 +41,42 @@ contract KeepTokenGeyser is TokenGeyser {
             _bonusPeriodSec,
             _initialSharesPerToken
         )
-    {}
+    {
+        durationSec = _durationSec;
+    }
+
+    /// @notice Receives an approval of token transfer and locks the approved
+    /// amount for rewards.
+    /// @dev Requires the provided token contract to be the same as the distribution
+    /// token supported by this contract.
+    /// @param _from The owner of the tokens who approved them to stake.
+    /// @param _value Approved amount of tokens for the transfer.
+    /// @param _token Token contract address.
+    /// @param _extraData Ignored.
+    function receiveApproval(
+        address _from,
+        uint256 _value,
+        address _token,
+        bytes calldata _extraData
+    ) external {
+        require(
+            KeepToken(_token) == getDistributionToken(),
+            "Token is not supported distribution token"
+        );
+
+        lockTokens(_value, durationSec);
+    }
+
+    function setDurationSec(uint256 _newDurationSec) external onlyOwner {
+        require(
+            _newDurationSec > 0,
+            "New duration has to be greater than zero"
+        );
+
+        emit DurationSecUpdated(durationSec, _newDurationSec);
+
+        durationSec = _newDurationSec;
+    }
 
     // TODO: Add functions that may be required from the Token Dashboard perspective.
 }
