523fd7040172b1d59a66da0e75ab36addf9916a2
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-01-22 19:07:11 +0100

    Updated params naming and added docs for constructor

--- solidity/contracts/geyser/KeepTokenGeyser.sol
+++ solidity/contracts/geyser/KeepTokenGeyser.sol
@@ -8,22 +8,31 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 // TODO: Rename to KeepVault?
 // TODO: Add docs
 contract KeepTokenGeyser is TokenGeyser {
+    /**
+     * @param _stakingToken The token users deposit as a stake.
+     * @param _distributionToken The token users are rewarded in and receive it as they unstake.
+     * @param _maxUnlockSchedules Max number of unlock stages, to guard against hitting gas limit.
+     * @param _startBonus Starting time bonus, BONUS_DECIMALS fixed point.
+     *                    e.g. 25% means user gets 25% of max distribution tokens.
+     * @param _bonusPeriodSec Length of time for bonus to increase linearly to max.
+     * @param _initialSharesPerToken Number of shares to mint per staking token on first stake.
+     */
     constructor(
         IERC20 _stakingToken,
-        KeepToken distributionToken,
-        uint256 maxUnlockSchedules,
-        uint256 startBonus_,
-        uint256 bonusPeriodSec_,
-        uint256 initialSharesPerToken
+        KeepToken _distributionToken,
+        uint256 _maxUnlockSchedules,
+        uint256 _startBonus,
+        uint256 _bonusPeriodSec,
+        uint256 _initialSharesPerToken
     )
         public
         TokenGeyser(
             _stakingToken,
-            distributionToken,
-            maxUnlockSchedules,
-            startBonus_,
-            bonusPeriodSec_,
-            initialSharesPerToken
+            _distributionToken,
+            _maxUnlockSchedules,
+            _startBonus,
+            _bonusPeriodSec,
+            _initialSharesPerToken
         )
     {}
 
