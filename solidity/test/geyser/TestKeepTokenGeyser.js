f8af52440076f4cff186c76e3e5e775a853ce786
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-01-26 17:16:13 +0100

    Added more unit test for Geyser

--- solidity/test/geyser/TestKeepTokenGeyser.js
+++ solidity/test/geyser/TestKeepTokenGeyser.js
@@ -8,8 +8,9 @@ const { createSnapshot, restoreSnapshot } = require("../helpers/snapshot.js")
 
 const { toBN } = require("web3-utils")
 
-const KeepTokenGeyser = contract.fromArtifact("KeepTokenGeyser")
 const KeepToken = contract.fromArtifact("KeepToken")
+const TestToken = contract.fromArtifact("TestToken")
+const KeepTokenGeyser = contract.fromArtifact("KeepTokenGeyser")
 const BatchedPhasedEscrow = contract.fromArtifact("BatchedPhasedEscrow")
 const KeepTokenGeyserRewardsEscrowBeneficiary = contract.fromArtifact(
   "KeepTokenGeyserRewardsEscrowBeneficiary"
@@ -36,20 +37,25 @@ describe("KeepTokenGeyser", async () => {
 
   const tokenDecimalMultiplier = toBN(10e18) // 18-decimal precision
 
-  const stakerInitialBalance = toBN(80e3).mul(tokenDecimalMultiplier) // 80k KEEP with 18-decimal precision
-  const stakeAmount = toBN(20e3).mul(tokenDecimalMultiplier) // 20k KEEP
-  const rewardsAmount = toBN(800e3).mul(tokenDecimalMultiplier) // 800k KEEP
+  const stakerInitialBalance1 = toBN(80e3).mul(tokenDecimalMultiplier) // 80k KEEP
+  const stakeAmount1 = toBN(20e3).mul(tokenDecimalMultiplier) // 20k KEEP
 
-  let token
+  const stakeAmount2 = toBN(40e3).mul(tokenDecimalMultiplier) // 40k KEEP
+  const stakerInitialBalance2 = toBN(50e3).mul(tokenDecimalMultiplier) // 50k KEEP
 
+  const rewardsAmount = toBN(900e3).mul(tokenDecimalMultiplier) // 900k KEEP
+
+  let stakeToken
+  let keepToken
   let tokenGeyser
 
   before(async () => {
-    token = await KeepToken.new({ from: contractOwner })
+    stakeToken = await TestToken.new({ from: contractOwner })
+    keepToken = await KeepToken.new({ from: contractOwner })
 
     tokenGeyser = await KeepTokenGeyser.new(
-      token.address,
-      token.address, // TODO: Use two different contracts
+      stakeToken.address,
+      keepToken.address,
       maxUnlockSchedules,
       startBonus,
       bonusPeriodSec,
@@ -65,9 +71,9 @@ describe("KeepTokenGeyser", async () => {
     })
 
     // Fund accounts with tokens.
-    await token.transfer(staker1, stakerInitialBalance, { from: contractOwner })
-    await token.transfer(staker2, stakerInitialBalance, { from: contractOwner })
-    await token.transfer(rewardDistribution, rewardsAmount, {
+    await stakeToken.mint(staker1, stakerInitialBalance1)
+    await stakeToken.mint(staker2, stakerInitialBalance2)
+    await keepToken.transfer(rewardDistribution, rewardsAmount, {
       from: contractOwner,
     })
   })
@@ -84,13 +90,13 @@ describe("KeepTokenGeyser", async () => {
     describe("called via receiveApproval", async () => {
       it("should update balances", async () => {
         // Deploy Escrow.
-        const escrow = await BatchedPhasedEscrow.new(token.address, {
+        const escrow = await BatchedPhasedEscrow.new(keepToken.address, {
           from: contractOwner,
         })
 
         // Configure escrow beneficiary.
         const escrowBeneficiary = await KeepTokenGeyserRewardsEscrowBeneficiary.new(
-          token.address,
+          keepToken.address,
           tokenGeyser.address,
           {
             from: contractOwner,
@@ -110,12 +116,14 @@ describe("KeepTokenGeyser", async () => {
         })
 
         // Transfer tokens to Escrow.
-        await token.approveAndCall(escrow.address, rewardsAmount, [], {
+        await keepToken.approveAndCall(escrow.address, rewardsAmount, [], {
           from: contractOwner,
         })
 
         // Initiate withdraw.
-        const initialEscrowBalance = await token.balanceOf.call(escrow.address)
+        const initialEscrowBalance = await keepToken.balanceOf.call(
+          escrow.address
+        )
 
         await escrow.batchedWithdraw(
           [escrowBeneficiary.address],
@@ -124,8 +132,8 @@ describe("KeepTokenGeyser", async () => {
         )
 
         expect(
-          await token.balanceOf.call(escrow.address),
-          "invalid staker's token balance"
+          await keepToken.balanceOf.call(escrow.address),
+          "invalid escrow's token balance"
         ).to.eq.BN(initialEscrowBalance.sub(rewardsAmount))
 
         expect(
@@ -144,56 +152,142 @@ describe("KeepTokenGeyser", async () => {
           tokenGeyser.receiveApproval(
             rewardDistribution,
             rewardsAmount,
-            token.address,
+            keepToken.address,
             []
           ),
           "Caller is not the reward distribution"
         )
+      })
+
+      it("reverts for not supported distribution token", async () => {
+        await expectRevert(
+          tokenGeyser.receiveApproval(
+            rewardDistribution,
+            rewardsAmount,
+            thirdParty,
+            []
+          ),
+          "Token is not supported distribution token"
+        )
+      })
+    })
+  })
+
+  describe("setDurationSec", async () => {
+    const newDurationSec = toBN(1230987)
+
+    it("updates durationSec", async () => {
+      await tokenGeyser.setDurationSec(newDurationSec, { from: contractOwner })
+
+      expect(await tokenGeyser.durationSec.call()).to.eq.BN(newDurationSec)
+    })
+
+    it("reverts when called by non-owner", async () => {
+      await expectRevert(
+        tokenGeyser.setDurationSec(newDurationSec),
+        "Ownable: caller is not the owner"
+      )
+    })
+
+    it("emits event", async () => {
+      const oldDurationSec = await tokenGeyser.durationSec.call()
+
+      const receipt = await tokenGeyser.setDurationSec(newDurationSec, {
+        from: contractOwner,
+      })
+
+      expectEvent(receipt, "DurationSecUpdated", {
+        oldDurationSec: oldDurationSec,
+        newDurationSec: newDurationSec,
       })
     })
   })
 
   describe("stake", async () => {
     it("should update balances", async () => {
-      await token.approve(tokenGeyser.address, stakeAmount, {
+      await stakeToken.approve(tokenGeyser.address, stakeAmount1, {
         from: staker1,
       })
-      await tokenGeyser.stake(stakeAmount, [], { from: staker1 })
+      await tokenGeyser.stake(stakeAmount1, [], { from: staker1 })
 
       expect(
-        await token.balanceOf.call(staker1),
+        await stakeToken.balanceOf.call(staker1),
         "invalid staker's token balance"
-      ).to.eq.BN(stakerInitialBalance.sub(stakeAmount))
+      ).to.eq.BN(stakerInitialBalance1.sub(stakeAmount1))
 
       expect(
         await tokenGeyser.totalStakedFor(staker1),
         "invalid staker's staked balance"
-      ).to.eq.BN(stakeAmount)
+      ).to.eq.BN(stakeAmount1)
+    })
+
+    it("should emit event", async () => {
+      await stakeToken.approve(tokenGeyser.address, stakeAmount1, {
+        from: staker1,
+      })
+      const receipt = await tokenGeyser.stake(stakeAmount1, [], {
+        from: staker1,
+      })
+
+      expectEvent(receipt, "Staked", {
+        user: staker1,
+        amount: stakeAmount1,
+        total: stakeAmount1,
+      })
+    })
+
+    it("allows stake top-ups", async () => {
+      await stakeToken.approve(
+        tokenGeyser.address,
+        stakeAmount1.add(stakeAmount2),
+        {
+          from: staker1,
+        }
+      )
+      await tokenGeyser.stake(stakeAmount1, [], { from: staker1 })
+
+      expect(
+        await tokenGeyser.totalStakedFor(staker1),
+        "invalid staker's staked balance"
+      ).to.eq.BN(stakeAmount1)
+
+      const receipt = await tokenGeyser.stake(stakeAmount2, [], {
+        from: staker1,
+      })
+
+      expect(
+        await tokenGeyser.totalStakedFor(staker1),
+        "invalid staker's staked balance"
+      ).to.eq.BN(stakeAmount1.add(stakeAmount2))
+
+      expectEvent(receipt, "Staked", {
+        user: staker1,
+        amount: stakeAmount2,
+        total: stakeAmount1.add(stakeAmount2),
+      })
     })
   })
 
   describe("stakeFor", async () => {
     it("should update balances", async () => {
-      const initialContractOwnerBalance = await token.balanceOf.call(
-        contractOwner
-      )
+      await stakeToken.mint(contractOwner, stakeAmount1)
 
-      await token.approve(tokenGeyser.address, stakeAmount, {
+      await stakeToken.approve(tokenGeyser.address, stakeAmount1, {
         from: contractOwner,
       })
-      await tokenGeyser.stakeFor(beneficiary, stakeAmount, [], {
+      await tokenGeyser.stakeFor(beneficiary, stakeAmount1, [], {
         from: contractOwner,
       })
 
       expect(
-        await token.balanceOf.call(staker1),
+        await stakeToken.balanceOf.call(staker1),
         "invalid staker's token balance"
-      ).to.eq.BN(stakerInitialBalance)
+      ).to.eq.BN(stakerInitialBalance1)
 
       expect(
-        await token.balanceOf.call(contractOwner),
+        await stakeToken.balanceOf.call(contractOwner),
         "invalid contract owner's token balance"
-      ).to.eq.BN(initialContractOwnerBalance.sub(stakeAmount))
+      ).to.eq.BN(0)
 
       expect(
         await tokenGeyser.totalStakedFor(contractOwner),
@@ -203,7 +297,139 @@ describe("KeepTokenGeyser", async () => {
       expect(
         await tokenGeyser.totalStakedFor(beneficiary),
         "invalid staker's staked balance"
-      ).to.eq.BN(stakeAmount)
+      ).to.eq.BN(stakeAmount1)
     })
   })
+
+  describe("unstake", async () => {
+    it("should calculate rewards", async () => {
+      const stakeAmount1 = toBN(20e3).mul(tokenDecimalMultiplier) // 20k KEEP
+      const stakeAmount2 = toBN(40e3).mul(tokenDecimalMultiplier) // 40k KEEP
+      const rewardsAmount = toBN(900e3).mul(tokenDecimalMultiplier) // 900k KEEP
+
+      const expectedRewards1 = toBN(300e3).mul(tokenDecimalMultiplier) // 300k KEEP
+      const expectedRewards2 = toBN(600e3).mul(tokenDecimalMultiplier) // 600k KEEP
+
+      await stakeTokenApprove(staker1, tokenGeyser.address, stakeAmount1)
+      await stakeTokenApprove(staker2, tokenGeyser.address, stakeAmount2)
+      await keepTokenApprove(
+        rewardDistribution,
+        tokenGeyser.address,
+        rewardsAmount
+      )
+
+      await tokenGeyser.stake(stakeAmount1, [], { from: staker1 })
+      await tokenGeyser.stake(stakeAmount2, [], { from: staker2 })
+
+      const lockTokensTX = await tokenGeyser.lockTokens(
+        rewardsAmount,
+        durationSec,
+        {
+          from: rewardDistribution,
+        }
+      )
+
+      const initTimestamp = new BN(
+        (await web3.eth.getBlock(lockTokensTX.blockNumber)).timestamp
+      )
+
+      await time.increase(initTimestamp.add(durationSec))
+
+      const rewards1 = await tokenGeyser.unstakeQuery.call(stakeAmount1, {
+        from: staker1,
+      })
+      const rewards2 = await tokenGeyser.unstakeQuery.call(stakeAmount2, {
+        from: staker2,
+      })
+
+      expectCloseTo(
+        rewards1,
+        expectedRewards1,
+        "invalid calculated staker's 1 rewards"
+      )
+      expectCloseTo(
+        rewards2,
+        expectedRewards2,
+        "invalid calculated staker's 2 rewards"
+      )
+    })
+
+    it("should withdraw stake and rewards", async () => {
+      const rewardsAmount = toBN(900e3).mul(tokenDecimalMultiplier) // 900k KEEP
+
+      const expectedRewards1 = toBN(300e3).mul(tokenDecimalMultiplier) // (20k / (20k + 40k)) * 900k = 300k KEEP
+      const expectedRewards2 = toBN(600e3).mul(tokenDecimalMultiplier) // (40k / (20k + 40k)) * 900k = 600k KEEP
+
+      await stakeTokenApprove(staker1, tokenGeyser.address, stakeAmount1)
+      await stakeTokenApprove(staker2, tokenGeyser.address, stakeAmount2)
+      await keepTokenApprove(
+        rewardDistribution,
+        tokenGeyser.address,
+        rewardsAmount
+      )
+
+      await tokenGeyser.stake(stakeAmount1, [], { from: staker1 })
+      await tokenGeyser.stake(stakeAmount2, [], { from: staker2 })
+
+      const lockTokensTX = await tokenGeyser.lockTokens(
+        rewardsAmount,
+        durationSec,
+        {
+          from: rewardDistribution,
+        }
+      )
+
+      const initTimestamp = new BN(
+        (await web3.eth.getBlock(lockTokensTX.blockNumber)).timestamp
+      )
+
+      await time.increase(initTimestamp.add(durationSec))
+
+      await tokenGeyser.unstake(stakeAmount1, [], {
+        from: staker1,
+      })
+      await tokenGeyser.unstake(stakeAmount2, [], {
+        from: staker2,
+      })
+
+      // Validate stakers' stake token balances.
+      expect(
+        await stakeToken.balanceOf.call(staker1),
+        "invalid staker's 1 token balance"
+      ).to.eq.BN(stakerInitialBalance1)
+      expect(
+        await stakeToken.balanceOf.call(staker2),
+        "invalid staker's 2 token balance"
+      ).to.eq.BN(stakerInitialBalance2)
+
+      // Validate stakers' distribution token balances.
+      expectCloseTo(
+        await keepToken.balanceOf.call(staker1),
+        expectedRewards1,
+        "invalid staker's 1 rewards token balance"
+      )
+      expectCloseTo(
+        await keepToken.balanceOf.call(staker2),
+        expectedRewards2,
+        "invalid staker's 2 rewards token balance"
+      )
+    })
+  })
+
+  async function keepTokenApprove(from, to, amount) {
+    await keepToken.approve(to, amount, { from: from })
+  }
+  async function stakeTokenApprove(from, to, amount) {
+    await stakeToken.approve(to, amount, { from: from })
+  }
+
+  function expectCloseTo(actual, expected, message) {
+    const delta = tokenDecimalMultiplier
+
+    if (actual.lt(expected.sub(delta)) || actual.gt(expected.add(delta))) {
+      expect.fail(
+        `${message}\nexpected : ${expected.toString()}\nactual   : ${actual.toString()}`
+      )
+    }
+  }
 })
