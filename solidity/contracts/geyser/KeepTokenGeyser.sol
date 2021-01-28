43a55ca8c777b2f4d0b97e5a5f9b258d30f38a42
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-01-28 15:18:35 +0100

    Updated docs

--- solidity/contracts/geyser/KeepTokenGeyser.sol
+++ solidity/contracts/geyser/KeepTokenGeyser.sol
@@ -6,22 +6,34 @@ import {IStakerRewards, StakerRewardsBeneficiary} from "../PhasedEscrow.sol";
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
-// TODO: Add docs
+/// @title KEEP Token Geyser
+/// @dev A smart-contract based mechanism to distribute tokens over time, based
+/// on implementation of ampleforth's TokenGeyser contract (see: [token-geyser]).
+///
+/// Token that users stake is any ERC20 token defined on contract deployment.
+/// Users are rewarded in distribution tokens, which in this case will be KEEP.
+///
+/// Account holding Reward Distribution role locks tokens for distribution.
+/// The role can be transferred to any account or contract (e.g. Escrow) by the
+/// contract owner.
+///
+/// [token-geyser]: https://github.com/ampleforth/token-geyser/
 contract KeepTokenGeyser is TokenGeyser, IStakerRewards {
     event DurationSecUpdated(uint256 oldDurationSec, uint256 newDurationSec);
 
     uint256 public durationSec;
 
-    /**
-     * @param _stakingToken The token users deposit as a stake.
-     * @param _distributionToken The token users are rewarded in and receive it as they unstake.
-     * @param _maxUnlockSchedules Max number of unlock stages, to guard against hitting gas limit.
-     * @param _startBonus Starting time bonus, BONUS_DECIMALS fixed point.
-     *                    e.g. 25% means user gets 25% of max distribution tokens.
-     * @param _bonusPeriodSec Length of time for bonus to increase linearly to max.
-     * @param _initialSharesPerToken Number of shares to mint per staking token on first stake.
-     * @param _durationSec Length of time to linear unlock the rewards tokens.
-     */
+    /// @param _stakingToken The token users deposit as a stake.
+    /// @param _distributionToken The token users are rewarded in and receive it
+    /// as they unstake.
+    /// @param _maxUnlockSchedules Max number of unlock stages, to guard against
+    /// hitting gas limit.
+    /// @param _startBonus Starting time bonus, BONUS_DECIMALS fixed point.
+    /// e.g. 25% means user gets 25% of max distribution tokens.
+    /// @param _bonusPeriodSec Length of time for bonus to increase linearly to max.
+    /// @param _initialSharesPerToken Number of shares to mint per staking token
+    /// on first stake.
+    /// @param _durationSec Length of time to linear unlock the rewards tokens.
     constructor(
         IERC20 _stakingToken,
         KeepToken _distributionToken,
@@ -76,8 +88,6 @@ contract KeepTokenGeyser is TokenGeyser, IStakerRewards {
 
         durationSec = _newDurationSec;
     }
-
-    // TODO: Add functions that may be required from the Token Dashboard perspective.
 }
 
 /// @title KeepTokenGeyserRewardsEscrowBeneficiary
--- solidity/contracts/geyser/KeepVault.sol
+++ solidity/contracts/geyser/KeepVault.sol
@@ -3,18 +3,20 @@ pragma solidity 0.5.17;
 import "./KeepTokenGeyser.sol";
 import "../KeepToken.sol";
 
-// TODO: Add docs
+/// @title KEEP Vault
+/// @dev A smart-contract based mechanism to distribute tokens over time.
+/// It accepts KEEP token as stake and distributes KEEP as rewards for staking.
 contract KeepVault is KeepTokenGeyser {
-    /**
-     * @param _keepToken KEEP token contract address. It is a token that is accepted
-     * as user's stake and that will be distributed as rewards.
-     * @param _maxUnlockSchedules Max number of unlock stages, to guard against hitting gas limit.
-     * @param _startBonus Starting time bonus, BONUS_DECIMALS fixed point.
-     *                    e.g. 25% means user gets 25% of max distribution tokens.
-     * @param _bonusPeriodSec Length of time for bonus to increase linearly to max.
-     * @param _initialSharesPerToken Number of shares to mint per staking token on first stake.
-     * @param _durationSec TODO: Update docs
-     */
+    /// @param _keepToken KEEP token contract address. It is a token that is accepted
+    /// as user's stake and that will be distributed as rewards.
+    /// @param _maxUnlockSchedules Max number of unlock stages, to guard against
+    /// hitting gas limit.
+    /// @param _startBonus Starting time bonus, BONUS_DECIMALS fixed point.
+    /// e.g. 25% means user gets 25% of max distribution tokens.
+    /// @param _bonusPeriodSec Length of time for bonus to increase linearly to max.
+    /// @param _initialSharesPerToken Number of shares to mint per staking token
+    /// on first stake.
+    /// @param _durationSec Length of time to linear unlock the rewards tokens.
     constructor(
         KeepToken _keepToken,
         uint256 _maxUnlockSchedules,
