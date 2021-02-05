452ca2a4de0240a78c96e057a32b8026de24e42d
Dmitry <dymitr.paremski@keep.network>
2021-02-05 13:31:25 +0100

    Refactoring install.sh script to handle Ethereum chain only
    Handling multiple chains inside this bash script becomes harder and
    clutters readibility with if statements. Having a dedicated script per
    chain will make it easier to maintain automated scripts. Another script
    will be responsible for Celo chain.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,64 +1,57 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
 LOG_START='\n\e[1;36m' # new line + bold + color
 LOG_END='\n\e[0m' # new line + reset color
 
 # Dafault inputs.
-KEEP_ACCOUNT_PASSWORD_DEFAULT="password"
-CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY=""
-NETWORK_DEFAULT="local"
 KEEP_CORE_PATH=$PWD
+KEEP_CORE_SOL_PATH="$KEEP_CORE_PATH/solidity"
 CONFIG_DIR_PATH_DEFAULT="$KEEP_CORE_PATH/configs"
+NETWORK_DEFAULT="local"
+KEEP_ACCOUNT_PASSWORD=${KEEP_HOST_CHAIN_ACCOUNT_PASSWORD:-"password"}
+ACCOUNT_PRIVATE_KEY=${CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY:-""}
 
 help()
 {
-   echo ""
-   echo "Usage: $0"\
-        "--config-dir <path>"\
-        "--account-password <password>"\
-        "--private-key <private key>"\
-        "--network <network>"
+   echo -e "\nUsage: ENV_VAR(S) $0"\
+           "--config-dir <path>"\
+           "--network <network>"
+   echo -e "\nEnvironment variables:\n"
+   echo -e "\tKEEP_HOST_CHAIN_ACCOUNT_PASSWORD: Unlock an account with a password. Default password is 'password'"
+   echo -e "\tCONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY: Contracts owner private key on Ethereum"
+   echo -e "\nCommand line arguments:\n"
    echo -e "\t--config-dir: Configuration path for keep-core client"
-   echo -e "\t--account-password: Account password"
-   echo -e "\t--private-key: Contract owner's account private key"
-   echo -e "\t--network: Host chain network for keep-core client"
+   echo -e "\t--network: Host chain network for keep-core client\n"
    exit 1 # Exit script after printing help
 }
-
-if [ "$1" == "-help" ]; then
-  help
-fi
 
 # Transform long options to short ones
 for arg in "$@"; do
   shift
   case "$arg" in
-    "--config-dir")        set -- "$@" "-c" ;;
-    "--account-password")  set -- "$@" "-p" ;;
-    "--private-key")       set -- "$@" "-k" ;;
-    "--network")           set -- "$@" "-n" ;;
-    *)                     set -- "$@" "$arg"
+    "--config-dir")  set -- "$@" "-c" ;;
+    "--network")     set -- "$@" "-n" ;;
+    "--help")        set -- "$@" "-h" ;;
+    *)               set -- "$@" "$arg"
   esac
 done
 
 # Parse short options
 OPTIND=1
-while getopts "c:p:k:n:" opt
+while getopts "c:n:h" opt
 do
    case "$opt" in
       c ) config_dir_path="$OPTARG" ;;
-      p ) account_password="$OPTARG" ;;
-      k ) private_key="$OPTARG" ;;
       n ) network="$OPTARG" ;;
+      h ) help ;;
       ? ) help ;; # Print help in case parameter is non-existent
    esac
 done
 shift $(expr $OPTIND - 1) # remove options from positional parameters
 
+# Overwrite default properties
 CONFIG_DIR_PATH=${config_dir_path:-$CONFIG_DIR_PATH_DEFAULT}
-KEEP_ACCOUNT_PASSWORD=${account_password:-$KEEP_ACCOUNT_PASSWORD_DEFAULT}
-ACCOUNT_PRIVATE_KEY=${private_key:-$CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY}
 NETWORK=${network:-$NETWORK_DEFAULT}
 
 # Run script
@@ -67,39 +60,34 @@ printf "${LOG_START}Starting installation...${LOG_END}"
 printf "Config dir path: $CONFIG_DIR_PATH\n"
 printf "Network: $NETWORK"
 
-KEEP_CORE_CONFIG_DIR_PATH=$CONFIG_DIR_PATH
-KEEP_CORE_SOL_PATH="$KEEP_CORE_PATH/solidity"
-
 cd $KEEP_CORE_SOL_PATH
 
 printf "${LOG_START}Installing NPM dependencies...${LOG_END}"
 npm install
 
-if [ "$NETWORK" != "alfajores" ]; then
-    printf "${LOG_START}Unlocking ethereum accounts...${LOG_END}"
-    KEEP_ETHEREUM_PASSWORD=$KEEP_ACCOUNT_PASSWORD \
-    CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
-        npx truffle exec scripts/unlock-eth-accounts.js --network $NETWORK
-fi
+printf "${LOG_START}Unlocking ethereum accounts...${LOG_END}"
+KEEP_ETHEREUM_PASSWORD=$KEEP_ACCOUNT_PASSWORD \
+CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
+    npx truffle exec scripts/unlock-eth-accounts.js --network $NETWORK
 
 printf "${LOG_START}Migrating contracts...${LOG_END}"
 rm -rf build/
 
-CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
+CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
     npx truffle migrate --reset --network $NETWORK
 
 KEEP_CORE_SOL_ARTIFACTS_PATH="$KEEP_CORE_SOL_PATH/build/contracts"
 
 printf "${LOG_START}Initializing contracts...${LOG_END}"
 
-CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
+CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
     npx truffle exec scripts/delegate-tokens.js --network $NETWORK
 
 printf "${LOG_START}Updating keep-core client configs...${LOG_END}"
-for CONFIG_FILE in $KEEP_CORE_CONFIG_DIR_PATH/*.toml
+for CONFIG_FILE in $CONFIG_DIR_PATH/*.toml
 do
     KEEP_CORE_CONFIG_FILE_PATH=$CONFIG_FILE \
-    CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
+    CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$ACCOUNT_PRIVATE_KEY \
         npx truffle exec scripts/lcl-client-config.js --network $NETWORK
 done
 
