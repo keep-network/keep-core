8f4b4322fc20038e67e1a1d88c4057d34c6ccb67
Piotr Dyraga <piotr.dyraga@keep.network>
2021-02-11 17:29:14 +0100

    Reorganized directory structure for Celo docker image
    /data changed to /mnt/data since this is a mounted Docker volume.
    
    /init renamed to /celo-init to be more specific about its purpose.

--- infrastructure/docker/celo/start.sh
+++ infrastructure/docker/celo/start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 mkdir -p /data/keystore
-cp init/keystore/* /data/keystore
-cp init/password.txt /data/password.txt
-[ ! -d "/data/celo" ] && geth --nousb --datadir=/data init init/genesis.json
+cp /celo-init/keystore/* /mnt/data/keystore
+cp /celo-init/password.txt /mnt/data/password.txt
+[ ! -d "/mnt/data/celo" ] && geth --nousb --datadir=/mnt/data init /celo-init/genesis.json
 
  geth --port 3000 --networkid 1101 --identity "somerandomidentity" \
     --ws --wsaddr "0.0.0.0" --wsport "8546" --wsorigins "*" \
     --rpc --rpcport "8545" --rpcaddr "0.0.0.0" --rpccorsdomain "" \
     --rpcapi "db,ssh,miner,admin,eth,net,web3,personal" \
     --wsapi "db,ssh,miner,admin,eth,net,web3,personal" \
-    --datadir /data --syncmode "fast" \
+    --datadir /mnt/data --syncmode "fast" \
     --mine --miner.threads 1 --nousb \
-    --unlock 0x2b2976824682233807a197081119da511af12f7a --password data/password.txt \
+    --unlock 0x2b2976824682233807a197081119da511af12f7a --password /mnt/data/password.txt \
     --allow-insecure-unlock 
