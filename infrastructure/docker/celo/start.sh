9f868b767733966a2de9123a029a25e5e9e0d25e
Piotr Dyraga <piotr.dyraga@keep.network>
2021-02-11 19:07:38 +0100

    Pass CELO_DATA_DIR and CELO_INIT_DIR to start.sh script
    Instead of hardcoding those paths, we now declare them once and pass to
    the script as environment variables.

--- infrastructure/docker/celo/start.sh
+++ infrastructure/docker/celo/start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-mkdir -p /data/keystore
-cp /celo-init/keystore/* /mnt/data/keystore
-cp /celo-init/password.txt /mnt/data/password.txt
-[ ! -d "/mnt/data/celo" ] && geth --nousb --datadir=/mnt/data init /celo-init/genesis.json
+mkdir -p $CELO_DATA_DIR/keystore
+cp $CELO_INIT_DIR/keystore/* $CELO_DATA_DIR/keystore
+cp $CELO_INIT_DIR/password.txt $CELO_DATA_DIR/password.txt
+[ ! -d "$CELO_DATA_DIR/celo" ] && geth --nousb --datadir=$CELO_DATA_DIR init $CELO_INIT_DIR/genesis.json
 
  geth --port 3000 --networkid 1101 --identity "somerandomidentity" \
     --ws --wsaddr "0.0.0.0" --wsport "8546" --wsorigins "*" \
     --rpc --rpcport "8545" --rpcaddr "0.0.0.0" --rpccorsdomain "" \
     --rpcapi "db,ssh,miner,admin,eth,net,web3,personal" \
     --wsapi "db,ssh,miner,admin,eth,net,web3,personal" \
-    --datadir /mnt/data --syncmode "fast" \
+    --datadir $CELO_DATA_DIR --syncmode "fast" \
     --mine --miner.threads 1 --nousb \
-    --unlock 0x2b2976824682233807a197081119da511af12f7a --password /mnt/data/password.txt \
+    --unlock 0x2b2976824682233807a197081119da511af12f7a --password $CELO_DATA_DIR/password.txt \
     --allow-insecure-unlock 
