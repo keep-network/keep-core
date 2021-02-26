96edd063bff5b8a7ef2aa9409b1b970f88d617f0
Dmitry <dymitr.paremski@keep.network>
2021-02-26 15:15:15 +0100

    Calculating KEEP in LP KEEPTBTC pair for stakers

--- token-tracker/src/inspector-lp-token.js
+++ token-tracker/src/inspector-lp-token.js
@@ -8,25 +8,31 @@ import BigNumber from "bignumber.js"
 
 // abi https://etherscan.io/address/0xe6f19dab7d43317344282f803f8e8d240708174a#code
 import KEEPETHTokenJson from "../artifacts/KEEP-ETH-UNI-V2-Token.json"
-import LPRewardsKEEPETHJson from "@keep-network/keep-ecdsa/artifacts/LPRewardsKEEPETH.json"
+// abi https://etherscan.io/address/0x38c8ffee49f286f25d25bad919ff7552e5daf081#code
+import KEEPTBTCTokenJson from "../artifacts/KEEP-TBTC-UNI-V2-Token.json"
 
-const ONE_HUNDRED = new BigNumber(100)
+import LPRewardsKEEPETHJson from "@keep-network/keep-ecdsa/artifacts/LPRewardsKEEPETH.json"
+import LPRewardsKEEPTBTCJson from "@keep-network/keep-ecdsa/artifacts/LPRewardsKEEPTBTC.json"
+
 const LIQUIDITY_POOLS = {
   KEEP_ETH: {
     pair: "0xe6f19dab7d43317344282f803f8e8d240708174a", // https://info.uniswap.org/pair/0xe6f19dab7d43317344282f803f8e8d240708174a
-    lpRewardsKeepEth: "0x47A5f2ffdf66D13ED7e317581F458d09b49d6F44", // https://etherscan.io/address/0x47a5f2ffdf66d13ed7e317581f458d09b49d6f44
+    lpRewardsContractAddress: "0x47A5f2ffdf66D13ED7e317581F458d09b49d6F44", // https://etherscan.io/address/0x47a5f2ffdf66d13ed7e317581f458d09b49d6f44
   },
   KEEP_TBTC: {
-    address: "0x38c8ffee49f286f25d25bad919ff7552e5daf081", // https://info.uniswap.org/pair/0x38c8ffee49f286f25d25bad919ff7552e5daf081
-    lpRewardsKeepTbtc: "0xb3d03a5411261fc2094697c5e969d552ee55cf6b", // https://etherscan.io/address/0xb3d03a5411261fc2094697c5e969d552ee55cf6b
+    pair: "0x38c8ffee49f286f25d25bad919ff7552e5daf081", // https://info.uniswap.org/pair/0x38c8ffee49f286f25d25bad919ff7552e5daf081
+    lpRewardsContractAddress: "0xb3d03A5411261fC2094697C5e969D552eE55cF6B", // https://etherscan.io/address/0xb3d03a5411261fc2094697c5e969d552ee55cf6b
   },
 }
-const LP_KEEPETH_BALANCE_BY_STAKERS_PATH = "./tmp/lp-keepeth-stakers.json"
-const KEEP_AMOUNT_IN_LP_BY_STAKERS_PATH = "./tmp/keep-in-lp-stakers.json"
+const KEEP_AMOUNT_IN_KEEPETH_BY_STAKERS_PATH =
+  "./tmp/keep-in-keepeth-by-stakers.json"
+const KEEP_AMOUNT_IN_KEEPTBTC_BY_STAKERS_PATH =
+  "./tmp/keep-in-keeptbtc-by-stakers.json"
 
 // Block height when stakedrop happens. Change accordingly
-const blockS = "latest"
-const KEEPETHCreationBlock = "10100034" // https://etherscan.io/tx/0xc64ac175846e719bb4f7f9b17a0b04bc365db3dda9d97ef70d7ede8f9c1a265b
+const BLOCK_S = "latest"
+const KEEPETH_CREATION_BLOCK = "10100034" // https://etherscan.io/tx/0xc64ac175846e719bb4f7f9b17a0b04bc365db3dda9d97ef70d7ede8f9c1a265b
+const KEEPTBTC_CREATION_BLOCK = "11452642" // https://etherscan.io/tx/0x1592f9b235c602c87a5b8cc5f896164dc43d16b92664cb9c8b420d28b64ca4a0
 
 async function run() {
   const context = await Context.initialize()
@@ -38,91 +44,124 @@ async function run() {
     LIQUIDITY_POOLS.KEEP_ETH.pair
   )
 
+  const KEEPTBTCPairTokenAbi = JSON.parse(KEEPTBTCTokenJson.result)
+  const KEEPTBTCPairToken = new web3.eth.Contract(
+    KEEPTBTCPairTokenAbi,
+    LIQUIDITY_POOLS.KEEP_TBTC.pair
+  )
+
   const LPRewardsKEEPETHAbi = LPRewardsKEEPETHJson.abi
   const LPRewardsKEEPETH = new web3.eth.Contract(
     LPRewardsKEEPETHAbi,
-    LIQUIDITY_POOLS.KEEP_ETH.lpRewardsKeepEth
+    LIQUIDITY_POOLS.KEEP_ETH.lpRewardsContractAddress
   )
 
-  await calculateKeepForStaker()
+  const LPRewardsKEEPTBTCAbi = LPRewardsKEEPTBTCJson.abi
+  const LPRewardsKEEPTBTC = new web3.eth.Contract(
+    LPRewardsKEEPTBTCAbi,
+    LIQUIDITY_POOLS.KEEP_TBTC.lpRewardsContractAddress
+  )
 
-  async function calculateKeepForStaker() {
+  const keepInKEEPETHByStakers = await calculateKeepForStaker(
+    LIQUIDITY_POOLS.KEEP_ETH.pair
+  )
+  const keepInKEEPTBTCByStakers = await calculateKeepForStaker(
+    LIQUIDITY_POOLS.KEEP_TBTC.pair
+  )
+  writeToJSON(LIQUIDITY_POOLS.KEEP_ETH.pair, keepInKEEPETHByStakers)
+  writeToJSON(LIQUIDITY_POOLS.KEEP_TBTC.pair, keepInKEEPTBTCByStakers)
+
+  async function calculateKeepForStaker(pair) {
     const keepInLpByStakers = {}
-    const stakersBalances = await getBalanceForStakers()
-    const pairData = await getPairData(LIQUIDITY_POOLS.KEEP_ETH.pair)
+
+    let stakersBalances = {}
+    if (pair == LIQUIDITY_POOLS.KEEP_ETH.pair) {
+      stakersBalances = await getLPBalanceForStakers(
+        LIQUIDITY_POOLS.KEEP_ETH.lpRewardsContractAddress
+      )
+    } else {
+      stakersBalances = await getLPBalanceForStakers(
+        LIQUIDITY_POOLS.KEEP_TBTC.lpRewardsContractAddress
+      )
+    }
+
+    // Retrieve current pair data. For historic data, need to pass a block number.
+    const pairData = await getPairData(pair)
+    const decimals = new BigNumber(10).pow(new BigNumber(18))
     for (const [stakerAddress, lpBalance] of Object.entries(stakersBalances)) {
+      const lpBalanceBN = new BigNumber(lpBalance)
       const keepInLPToken = await calcKeepTokenfromLPToken(
-        new BigNumber(lpBalance),
+        lpBalanceBN,
         pairData
       )
       keepInLpByStakers[stakerAddress] = keepInLPToken.toString()
-      console.info(`${stakerAddress}: ${lpBalance}: ${keepInLPToken}`)
+
+      console.info(
+        `Staker: ${stakerAddress} - LP Balance: ${lpBalanceBN.div(
+          decimals
+        )} - KEEP in LP: ${keepInLPToken}`
+      )
     }
 
-    writeFileSync(
-      KEEP_AMOUNT_IN_LP_BY_STAKERS_PATH,
-      JSON.stringify(keepInLpByStakers, null, 2)
-    )
+    return keepInLpByStakers
   }
 
-  function toWei(amount) {
-    return web3.utils.toWei(amount.toString())
+  async function getLPBalanceForStakers(lpRewardContractAddress) {
+    let totalSupply = new BigNumber(0)
+    const stakersBalances = {}
+    const lpStakers = await getLPStakers(lpRewardContractAddress)
+
+    let lpRewardContract = {}
+    if (
+      lpRewardContractAddress ==
+      LIQUIDITY_POOLS.KEEP_ETH.lpRewardsContractAddress
+    ) {
+      lpRewardContract = LPRewardsKEEPETH
+    } else {
+      lpRewardContract = LPRewardsKEEPTBTC
+    }
+
+    for (let i = 0; i < lpStakers.length; i++) {
+      const lpBalance = new BigNumber(
+        await lpRewardContract.methods.balanceOf(lpStakers[i]).call({}, BLOCK_S)
+      )
+      if (!lpBalance.isZero()) {
+        stakersBalances[lpStakers[i]] = lpBalance.toString()
+        totalSupply = totalSupply.plus(lpBalance)
+      }
+    }
+
+    console.info(`Total supply of LP Token: ${totalSupply.toString()}`)
+
+    return stakersBalances
   }
 
-  // Calculation is based on https://uniswap.org/docs/v2/advanced-topics/understanding-returns/
-  async function calcKeepTokenfromLPToken(lpBalance, pairData) {
-    console.info(pairData)
-    const uniswapTotalSupply = toWei(pairData.totalSupply)
-    const keepLiquidityPool = new BigNumber(pairData.reserve0) // KEEP
-    const ethLiquidityPool = new BigNumber(pairData.reserve1) // ETH
-
-    console.info("reserve0 ", keepLiquidityPool.toString())
-    console.info("reserve1 ", ethLiquidityPool.toString())
-
-    const shareOfUniswapPool = ONE_HUNDRED.multipliedBy(lpBalance).div(
-      uniswapTotalSupply
-    )
-    console.info("shareOfUniswapPool:: ", shareOfUniswapPool.toString())
-
-    // const ethPrice =  reserve0.div(reserve1) // price in KEEP for 1 ETH
-    // keep_liquidity_pool * eth_liquidity_pool from Uniswap
-    // const constantProduct = reserve0.multipliedBy(reserve1)
-    // console.info("constantProduct:: ", constantProduct.toString())
-
-    // const ethLiquidityPool = (constantProduct.div(ethPrice)).squareRoot()
-    // console.info("ethLiquidityPool:: ", ethLiquidityPool.toString())
-    // const keepLiquidityPool = (constantProduct.multipliedBy(ethPrice)).squareRoot()
-    // console.info("keepLiquidityPool:: ", keepLiquidityPool.toString())
-    
-    // const ethInLP = shareOfUniswapPool
-    //   .div(ONE_HUNDRED)
-    //   .multipliedBy(ethLiquidityPool)
-    // console.info("ethInLP: ", ethInLP.toString())
-
-    const keepInLP = shareOfUniswapPool
-      .div(ONE_HUNDRED)
-      .multipliedBy(keepLiquidityPool)
-    console.info("keepInLP: ", keepInLP.toString())
-    
-
-    return keepInLP
-  }
-
-  async function getLPStakers(lpRewardAddress) {
-    const events = await getPastEvents(
-      web3,
-      KEEPETHPairToken,
-      "Transfer",
-      KEEPETHCreationBlock,
-      blockS
-    )
+  async function getLPStakers(lpRewardContract) {
+    let events = {}
+    if (lpRewardContract == LIQUIDITY_POOLS.KEEP_ETH.lpRewardsContractAddress) {
+      events = await getPastEvents(
+        web3,
+        KEEPETHPairToken,
+        "Transfer",
+        KEEPETH_CREATION_BLOCK,
+        BLOCK_S
+      )
+    } else {
+      events = await getPastEvents(
+        web3,
+        KEEPTBTCPairToken,
+        "Transfer",
+        KEEPTBTC_CREATION_BLOCK,
+        BLOCK_S
+      )
+    }
 
     console.info(`found ${events.length} token transfer events`)
 
     const lpTokenStakersSet = new Set()
     events.forEach(function (event) {
       // include accounts that staked in LPRewards contracts only
-      if (event.returnValues.to == lpRewardAddress) {
+      if (event.returnValues.to == lpRewardContract) {
         lpTokenStakersSet.add(event.returnValues.from)
       }
     })
@@ -133,37 +172,29 @@ async function run() {
     return lpTokenStakers
   }
 
-  async function getBalanceForStakers() {
-    let totalSupply = new BigNumber(0)
-    const stakersBalances = {}
-    const lpStakers = await getLPStakers(
-      LIQUIDITY_POOLS.KEEP_ETH.lpRewardsKeepEth
-    )
+  // Calculation is based on https://uniswap.org/docs/v2/advanced-topics/understanding-returns/
+  async function calcKeepTokenfromLPToken(lpBalance, pairData) {
+    const uniswapTotalSupply = web3.utils.toWei(pairData.totalSupply.toString())
+    const keepLiquidityPool = new BigNumber(pairData.reserve0) // KEEP
 
-    // for (let i = 0; i < 10; i++) {
-    for (let i = 0; i < lpStakers.length; i++) {
-      const lpBalance = new BigNumber(
-        await LPRewardsKEEPETH.methods.balanceOf(lpStakers[i]).call({}, blockS)
+    return lpBalance.multipliedBy(keepLiquidityPool).div(uniswapTotalSupply)
+  }
+
+  function writeToJSON(pair, data) {
+    let keepInLpPath = ""
+    if (pair == LIQUIDITY_POOLS.KEEP_ETH.pair) {
+      console.info(
+        `writing KEEP amount in LP token ${pair} for stakers to a file: ${KEEP_AMOUNT_IN_KEEPETH_BY_STAKERS_PATH}`
       )
-      if (!lpBalance.isZero()) {
-        stakersBalances[lpStakers[i]] = lpBalance.toString()
-        totalSupply = totalSupply.plus(lpBalance)
-
-        console.info(`Staker: ${lpStakers[i]} - balance: ${lpBalance}`)
-      }
+      keepInLpPath = KEEP_AMOUNT_IN_KEEPETH_BY_STAKERS_PATH
+    } else {
+      console.info(
+        `writing KEEP amount in LP token ${pair} for stakers to a file: ${KEEP_AMOUNT_IN_KEEPTBTC_BY_STAKERS_PATH}`
+      )
+      keepInLpPath = KEEP_AMOUNT_IN_KEEPTBTC_BY_STAKERS_PATH
     }
 
-    console.info(
-      `writing token stakers balance to a file: ${LP_KEEPETH_BALANCE_BY_STAKERS_PATH}`
-    )
-    writeFileSync(
-      LP_KEEPETH_BALANCE_BY_STAKERS_PATH,
-      JSON.stringify(stakersBalances, null, 2)
-    )
-
-    console.info(`Total supply of LP KEEP-ETH: ${totalSupply.toString()}`)
-
-    return stakersBalances
+    writeFileSync(keepInLpPath, JSON.stringify(data, null, 2))
   }
 }
 
