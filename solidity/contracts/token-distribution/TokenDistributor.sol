cbcbe1f1306948f98f7c7f35b84ef93f56169f2b
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-03-04 18:44:49 +0100

    Token Distribution claim function with signature validation
    When receipient gets tokens assigned they can redirect them to another
    address when claiming the tokens. To do that receipient has to provide
    and ethereum signature over the destination address and provide this
    signature to the claim function.
    
    The signature has to be calculated over ethereum specific message format
    that should be obtained in the following way:
    ```
    digest = keccak256(destinationAddress)
    digestToSign = keccak256("\x19Ethereum Signed Message:\n" + len(digest)
    + digest)
    ```
    
    When using web3.eth.sign function the ethereum prefix is added at the
    begining automatically so one has to sign only hash of the destination
    address, e.g.:
    ```
    web3.eth.accounts.sign(web3.utils.sha3(destinationAddress), privateKey)
    ```

---
+++ solidity/contracts/token-distribution/TokenDistributor.sol
@@ -0,0 +1,55 @@
+pragma solidity 0.5.17;
+
+contract TokenDistributor {
+    // In the claim function, you need to provide Ethereum address and a signed
+    // address of token recipient (the signature by Ethereum address from a
+    // merkle tree). We'll validate the signature and see how many tokens should
+    // be claimable by that address based on the information in Merkle tree.
+    // TODO: Add support of Merkle Tree distribution
+    function claim(
+        address recipient,
+        address destination,
+        uint8 v,
+        bytes32 r,
+        bytes32 s // TODO: Add more parameters for claims from merkle tree.
+    ) public {
+        require(recipient != address(0), "recipient address cannot be zero");
+        require(
+            destination != address(0),
+            "destination address cannot be zero"
+        );
+
+        require(
+            recipient ==
+                recoverSignerAddress(abi.encodePacked(destination), v, r, s),
+            "invalid signature of destination address"
+        );
+    }
+
+    function recoverSignerAddress(
+        bytes memory message,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal pure returns (address) {
+        // Validate `s` value for a malleability concern described in EIP-2.
+        // Only signatures with `s` value in the lower half of the secp256k1
+        // curve's order are considered valid.
+        require(
+            uint256(s) <=
+                0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0,
+            "Malleable signature - s should be in the low half of secp256k1 curve's order"
+        );
+
+        bytes32 digest = keccak256(message);
+        bytes32 prefixedDigest =
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", digest)
+            );
+
+        return ecrecover(prefixedDigest, v, r, s);
+    }
+
+    // TODO: Tokens not claimed within a given timeout should go to a treasury
+    // wallet address set on that contract.
+}
