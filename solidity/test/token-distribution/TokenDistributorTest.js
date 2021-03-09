9a6312ffce48831fe4057af16e645273a49939d7
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-03-09 18:07:09 +0100

    Added unit tests

--- solidity/test/token-distribution/TokenDistributorTest.js
+++ solidity/test/token-distribution/TokenDistributorTest.js
@@ -1,15 +1,24 @@
-const { expectRevert, expectEvent } = require("@openzeppelin/test-helpers")
+const {
+  expectRevert,
+  expectEvent,
+  constants,
+  time,
+} = require("@openzeppelin/test-helpers")
+
 const {
   accounts,
   privateKeys,
   contract,
   web3,
 } = require("@openzeppelin/test-environment")
+
 const { createSnapshot, restoreSnapshot } = require("../helpers/snapshot.js")
 const { parseBalanceMap } = require("@keep-network/merkle-distributor-helper")
 
 const TokenDistributor = contract.fromArtifact("TokenDistributor")
 const TestToken = contract.fromArtifact("TestToken")
+
+const { ZERO_ADDRESS } = constants
 
 const { BN, toBN } = web3.utils
 const chai = require("chai")
@@ -27,12 +36,11 @@ describe("TokenDistributor", () => {
   const destination = accounts[destinationIndex]
   const thirdParty = accounts[thirdPartyIndex]
 
-  let testToken
-  let tokenDistributor
+  const unclaimedUnlockDuration = time.duration.weeks(12)
 
   const allocationsMap = {}
-  allocationsMap[recipient] = (100000).toString(16)
-  allocationsMap[thirdParty] = (200000).toString(16)
+  allocationsMap[thirdParty] = (27600092).toString(16)
+  allocationsMap[recipient] = (15800037).toString(16)
 
   const testData = {
     signature: web3.eth.accounts.sign(
@@ -42,7 +50,10 @@ describe("TokenDistributor", () => {
     merkle: parseBalanceMap(allocationsMap),
   }
 
-  before(async () => {
+  let testToken
+  let tokenDistributor
+
+  const freshDeployment = async () => {
     testToken = await TestToken.new({ from: owner })
     tokenDistributor = await TokenDistributor.new(testToken.address, {
       from: owner,
@@ -56,7 +67,9 @@ describe("TokenDistributor", () => {
         from: owner,
       }
     )
-  })
+  }
+
+  before(freshDeployment)
 
   beforeEach(async () => {
     await createSnapshot()
@@ -71,18 +84,21 @@ describe("TokenDistributor", () => {
       await tokenDistributor.allocate(
         testData.merkle.merkleRoot,
         testData.merkle.tokenTotal,
+        unclaimedUnlockDuration,
         { from: owner }
       )
 
-      expect(await testToken.balanceOf(tokenDistributor.address)).to.eq.BN(
-        toBN(testData.merkle.tokenTotal)
-      )
+      expect(
+        await testToken.balanceOf(tokenDistributor.address),
+        "invalid token distributor balance"
+      ).to.eq.BN(toBN(testData.merkle.tokenTotal))
     })
 
     it("sets merkle root", async function () {
       await tokenDistributor.allocate(
         testData.merkle.merkleRoot,
         testData.merkle.tokenTotal,
+        unclaimedUnlockDuration,
         { from: owner }
       )
 
@@ -96,6 +112,7 @@ describe("TokenDistributor", () => {
         await tokenDistributor.allocate(
           testData.merkle.merkleRoot,
           testData.merkle.tokenTotal,
+          unclaimedUnlockDuration,
           { from: owner }
         ),
         "TokensAllocated",
@@ -106,26 +123,69 @@ describe("TokenDistributor", () => {
       )
     })
 
+    it("sets unclaimed tokens unlock timestamp", async function () {
+      const { receipt } = await tokenDistributor.allocate(
+        testData.merkle.merkleRoot,
+        testData.merkle.tokenTotal,
+        unclaimedUnlockDuration,
+        { from: owner }
+      )
+
+      const timestamp = toBN(
+        (await web3.eth.getBlock(receipt.blockNumber)).timestamp
+      )
+
+      expect(
+        await tokenDistributor.unclaimedUnlockTimestamp(),
+        "invalid unclaimed unlock timestamp"
+      ).to.eq.BN(timestamp.add(unclaimedUnlockDuration))
+    })
+
+    it("doesn't set unclaimed tokens unlock timestamp when unclaimed duration is not provided", async function () {
+      await tokenDistributor.allocate(
+        testData.merkle.merkleRoot,
+        testData.merkle.tokenTotal,
+        0,
+        { from: owner }
+      )
+
+      expect(
+        await tokenDistributor.unclaimedUnlockTimestamp(),
+        "invalid unclaimed unlock timestamp"
+      ).to.eq.BN(0)
+    })
+
     it("reverts on merkle root overwrite", async function () {
       await tokenDistributor.allocate(
         testData.merkle.merkleRoot,
         testData.merkle.tokenTotal,
+        unclaimedUnlockDuration,
         { from: owner }
       )
 
       await expectRevert(
-        tokenDistributor.allocate("0x1234567890", testData.merkle.tokenTotal, {
-          from: owner,
-        }),
+        tokenDistributor.allocate(
+          "0x1234567890",
+          testData.merkle.tokenTotal,
+          unclaimedUnlockDuration,
+          {
+            from: owner,
+          }
+        ),
         "tokens were already allocated"
       )
     })
 
     it("reverts on empty merkle root", async function () {
       await expectRevert(
-        tokenDistributor.allocate([], testData.merkle.tokenTotal, {
-          from: owner,
-        }),
+        tokenDistributor.allocate(
+          [],
+          testData.merkle.tokenTotal,
+          unclaimedUnlockDuration,
+          {
+            from: owner,
+          }
+        ),
         "merkle root cannot be empty"
       )
     })
@@ -135,6 +195,7 @@ describe("TokenDistributor", () => {
         tokenDistributor.allocate(
           testData.merkle.merkleRoot,
           testData.merkle.tokenTotal + 1,
+          unclaimedUnlockDuration,
           {
             from: owner,
           }
@@ -142,14 +203,85 @@ describe("TokenDistributor", () => {
         "SafeERC20: low-level call failed"
       )
     })
+
+    it("reverts when called by non-owner", async function () {
+      await expectRevert(
+        tokenDistributor.allocate(
+          testData.merkle.merkleRoot,
+          testData.merkle.tokenTotal,
+          unclaimedUnlockDuration,
+          {
+            from: thirdParty,
+          }
+        ),
+        "Ownable: caller is not the owner"
+      )
+    })
   })
 
   describe("claim", async () => {
-    beforeEach(async () => {
+    before(async () => {
       await tokenDistributor.allocate(
         testData.merkle.merkleRoot,
         testData.merkle.tokenTotal,
+        unclaimedUnlockDuration,
         { from: owner }
+      )
+    })
+
+    after(freshDeployment)
+
+    it("transfers tokens", async function () {
+      const recipientInitialBalance = toBN(await testToken.balanceOf(recipient))
+      const destinationInitialBalance = toBN(
+        await testToken.balanceOf(destination)
+      )
+
+      await tokenDistributor.claim(
+        recipient,
+        destination,
+        testData.signature.v,
+        testData.signature.r,
+        testData.signature.s,
+        testData.merkle.claims[recipient].index,
+        testData.merkle.claims[recipient].amount,
+        testData.merkle.claims[recipient].proof
+      )
+
+      expect(
+        await testToken.balanceOf(recipient),
+        "invalid recipient address balance"
+      ).to.eq.BN(toBN(recipientInitialBalance))
+
+      expect(
+        await testToken.balanceOf(destination),
+        "invalid destination address balance"
+      ).to.eq.BN(
+        destinationInitialBalance.add(
+          toBN(testData.merkle.claims[recipient].amount)
+        )
+      )
+    })
+
+    it("emits event", async function () {
+      expectEvent(
+        await tokenDistributor.claim(
+          recipient,
+          destination,
+          testData.signature.v,
+          testData.signature.r,
+          testData.signature.s,
+          testData.merkle.claims[recipient].index,
+          testData.merkle.claims[recipient].amount,
+          testData.merkle.claims[recipient].proof
+        ),
+        "TokensClaimed",
+        {
+          index: testData.merkle.claims[recipient].index.toString(),
+          recipient: recipient,
+          destination: destination,
+          amount: toBN(testData.merkle.claims[recipient].amount),
+        }
       )
     })
 
@@ -242,7 +374,7 @@ describe("TokenDistributor", () => {
       }
     })
 
-    it("detects malleable signatures", async function () {
+    it("reverts on malleable signatures", async function () {
       const secp256k1N = toBN(
         "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141"
       )
@@ -278,6 +410,163 @@ describe("TokenDistributor", () => {
         "Invalid signature 'v' value"
       )
     })
-    //  TODO: Add more tests
+
+    it("reverts on empty addresses", async function () {
+      await expectRevert(
+        tokenDistributor.claim(
+          ZERO_ADDRESS,
+          destination,
+          testData.signature.v,
+          testData.signature.r,
+          testData.signature.s,
+          testData.merkle.claims[recipient].index,
+          testData.merkle.claims[recipient].amount,
+          testData.merkle.claims[recipient].proof
+        ),
+        "recipient address cannot be zero"
+      )
+
+      await expectRevert(
+        tokenDistributor.claim(
+          recipient,
+          ZERO_ADDRESS,
+          testData.signature.v,
+          testData.signature.r,
+          testData.signature.s,
+          testData.merkle.claims[recipient].index,
+          testData.merkle.claims[recipient].amount,
+          testData.merkle.claims[recipient].proof
+        ),
+        "destination address cannot be zero"
+      )
+    })
+
+    it("reverts if tokens were not allocated", async function () {
+      const tokenDistributor = await TokenDistributor.new(testToken.address, {
+        from: owner,
+      })
+
+      await expectRevert(
+        tokenDistributor.claim(
+          recipient,
+          destination,
+          testData.signature.v,
+          testData.signature.r,
+          testData.signature.s,
+          testData.merkle.claims[recipient].index,
+          testData.merkle.claims[recipient].amount,
+          testData.merkle.claims[recipient].proof
+        ),
+        "tokens were not allocated yet"
+      )
+    })
+
+    it("reverts if tokens were already claimed", async function () {
+      await tokenDistributor.claim(
+        recipient,
+        destination,
+        testData.signature.v,
+        testData.signature.r,
+        testData.signature.s,
+        testData.merkle.claims[recipient].index,
+        testData.merkle.claims[recipient].amount,
+        testData.merkle.claims[recipient].proof
+      )
+
+      await expectRevert(
+        tokenDistributor.claim(
+          recipient,
+          destination,
+          testData.signature.v,
+          testData.signature.r,
+          testData.signature.s,
+          testData.merkle.claims[recipient].index,
+          testData.merkle.claims[recipient].amount,
+          testData.merkle.claims[recipient].proof
+        ),
+        "tokens already claimed"
+      )
+    })
+  })
+
+  describe("recoverUnclaimed", async function () {
+    const allocate = async (unlockDuration) => {
+      const { receipt } = await tokenDistributor.allocate(
+        testData.merkle.merkleRoot,
+        testData.merkle.tokenTotal,
+        unlockDuration,
+        { from: owner }
+      )
+
+      return toBN((await web3.eth.getBlock(receipt.blockNumber)).timestamp)
+    }
+
+    it("transfers tokens to destination address", async function () {
+      const timestamp = await allocate(unclaimedUnlockDuration)
+      await time.increaseTo(timestamp.add(unclaimedUnlockDuration))
+
+      const destinationInitialBalance = toBN(
+        await testToken.balanceOf(destination)
+      )
+
+      await tokenDistributor.recoverUnclaimed(destination, { from: owner })
+
+      expect(
+        await testToken.balanceOf(destination),
+        "invalid recipient address balance"
+      ).to.eq.BN(
+        toBN(destinationInitialBalance.add(toBN(testData.merkle.tokenTotal)))
+      )
+    })
+
+    it("emits event", async function () {
+      const timestamp = await allocate(unclaimedUnlockDuration)
+      await time.increaseTo(timestamp.add(unclaimedUnlockDuration))
+
+      expectEvent(
+        await tokenDistributor.recoverUnclaimed(destination, { from: owner }),
+        "TokensRecovered",
+        {
+          destination: destination,
+          amount: toBN(testData.merkle.tokenTotal),
+        }
+      )
+    })
+
+    it("reverts on empty destination addresse", async function () {
+      await expectRevert(
+        tokenDistributor.recoverUnclaimed(ZERO_ADDRESS, { from: owner }),
+        "destination address cannot be zero"
+      )
+    })
+
+    it("reverts if tokens recovery is not allowed", async function () {
+      await allocate(0)
+
+      await expectRevert(
+        tokenDistributor.recoverUnclaimed(destination, { from: owner }),
+        "token recovery is not allowed"
+      )
+    })
+
+    it("reverts if unlock period has not passed yet", async function () {
+      const timestamp = await allocate(unclaimedUnlockDuration)
+
+      await time.increaseTo(timestamp.add(unclaimedUnlockDuration).subn(1))
+
+      await expectRevert(
+        tokenDistributor.recoverUnclaimed(destination, { from: owner }),
+        "token recovery is not possible yet"
+      )
+    })
+
+    it("reverts when called by non-owner", async function () {
+      await expectRevert(
+        tokenDistributor.recoverUnclaimed(destination, {
+          from: thirdParty,
+        }),
+        "Ownable: caller is not the owner"
+      )
+    })
   })
 })
