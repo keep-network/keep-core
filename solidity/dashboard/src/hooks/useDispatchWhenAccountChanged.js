f9ffd078e4f3f863f0cc4e49025f9c325e0edc7c
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-03-25 13:53:37 +0100

    Update custom hook
    Updated the `useDispatchWhenAccountChanged` hook and added docs.

--- solidity/dashboard/src/hooks/useDispatchWhenAccountChanged.js
+++ solidity/dashboard/src/hooks/useDispatchWhenAccountChanged.js
@@ -1,18 +1,20 @@
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
-import { usePrevious } from "./usePrevious"
+import { useWeb3Address } from "../components/WithWeb3Context"
 
-export const useDispatchWhenAccountChanged = (dispatchFn) => {
+/**
+ * Custom hook that triggers a callback when account changed and the redux store
+ * has been restarted and ready to handle actions.
+ *
+ * @param {Function} callback - A Function to trigger. Note: Make sure the
+ * function is memoized otherwise it will be called every time the component
+ * updates.
+ */
+export const useDispatchWhenAccountChanged = (callback) => {
   const isReady = useSelector((state) => state.app.isReady)
-  const previousIsAppReady = usePrevious(isReady)
+  const address = useWeb3Address()
 
   useEffect(() => {
-    dispatchFn()
-  }, [dispatchFn])
-
-  useEffect(() => {
-    if (!previousIsAppReady && isReady) {
-      dispatchFn()
-    }
-  })
+    callback()
+  }, [callback, address, isReady])
 }
