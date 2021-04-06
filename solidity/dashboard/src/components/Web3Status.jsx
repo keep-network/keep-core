55225b485392d9455620b112e981344148c9f224
michalsmiarowski <michal.smiarowski@thesis.co>
2021-04-06 06:47:49 +0200

    Add an icon of the current connected wallet
    Adds an icon of the current connected wallet on top of the page.
    
    Note: Probably needs different icon for the explorer mode.

--- solidity/dashboard/src/components/Web3Status.jsx
+++ solidity/dashboard/src/components/Web3Status.jsx
@@ -8,9 +8,65 @@ import WalletOptions from "./WalletOptions"
 import CopyToClipboard from "./CopyToClipboard"
 import { displayAmount } from "../utils/token.utils"
 import { WALLETS } from "../constants/constants"
+import Tooltip from "./Tooltip"
 
 export const Web3Status = () => {
   const { yourAddress, isConnected, connector } = useWeb3Context()
+
+  const renderWalletTypeIcon = () => {
+    let tooltipText = ""
+    let iconComponent = <></>
+
+    switch (connector?.name) {
+      case WALLETS.METAMASK.name:
+        tooltipText = "You are connected to MetaMask"
+        iconComponent = <Icons.MetaMask className="flex" />
+        break
+      case WALLETS.LEDGER:
+        tooltipText = "You are connected to Ledger"
+        iconComponent = (
+          <Icons.Ledger className="ledger-logo ledger-logo--black flex" />
+        )
+        break
+      case WALLETS.TREZOR:
+        tooltipText = "You are connected to Trezor"
+        iconComponent = (
+          <Icons.Trezor className="trezor-logo trezor-logo--black flex" />
+        )
+        break
+      case WALLETS.WALLET_CONNECT:
+        tooltipText = "You are connected to WalletConnect"
+        iconComponent = (
+          <Icons.WalletConnect className="wallet-connect-logo wallet-connect-logo--black flex" />
+        )
+        break
+      case WALLETS.EXPLORER_MODE.name:
+        tooltipText = "You are viewing the site in an Explorer Mode"
+        iconComponent = (
+          <Icons.Wallet className="wallet-connect-logo wallet-connect-logo--black flex" />
+        )
+        break
+      default:
+        tooltipText = ""
+        iconComponent = <></>
+    }
+
+    return (
+      <Tooltip
+        simple
+        delay={0}
+        triggerComponent={() => {
+          return iconComponent
+        }}
+        className={"web3-status__wallet-connected-tooltip"}
+        tooltipContentWrapperClassName={
+          "tooltip__content-wrapper--lower-position"
+        }
+      >
+        {tooltipText}
+      </Tooltip>
+    )
+  }
 
   return (
     <div className="web3-status">
@@ -18,6 +74,11 @@ export const Web3Status = () => {
         <div className="web3-status__network-status">
           <NetworkStatus />
         </div>
+        {connector && (
+          <div className={"web3-status__wallet-connected-icon-wrapper"}>
+            {renderWalletTypeIcon()}
+          </div>
+        )}
         <div
           className={`web3-status__wallet ${
             connector?.name === WALLETS.EXPLORER_MODE.name
