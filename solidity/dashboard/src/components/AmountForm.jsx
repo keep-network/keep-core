abe92e067a4d2ab75a05ba8546cab185fe863e77
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-04-23 13:08:50 +0200

    Update a top up form
    Removed unnecessary components based on the Figma views. Also fixed
    validation message.

--- solidity/dashboard/src/components/AmountForm.jsx
+++ solidity/dashboard/src/components/AmountForm.jsx
@@ -8,12 +8,8 @@ import {
   normalizeAmount,
   formatAmount as formatFormAmount,
 } from "../forms/form.utils.js"
-import * as Icons from "./Icons"
-import Tag from "./Tag"
-import ProgressBar from "./ProgressBar"
-import { colors } from "../constants/colors"
-import { displayAmount, fromTokenUnit } from "../utils/token.utils"
-import { add } from "../utils/arithmetics.utils"
+import { displayAmount } from "../utils/token.utils"
+import Divider from "./Divider"
 
 const AmountForm = ({
   onCancel,
@@ -22,34 +18,8 @@ const AmountForm = ({
   currentAmount,
   ...formikProps
 }) => {
-  const { amount: formAmount } = formikProps.values || 0
-  const newAmount =
-    formAmount && displayAmount(add(fromTokenUnit(formAmount), currentAmount))
-
   return (
     <>
-      <div className="flex row center mt-1">
-        <div className="flex-1">
-          <Tag text="Current" IconComponent={Icons.KeepToken} />
-        </div>
-        <h3 className="flex-2 text-primary">
-          {displayAmount(currentAmount)} KEEP
-        </h3>
-      </div>
-      <div className="flex row center mt-1">
-        <div className="flex-1">
-          <Tag text="New" IconComponent={Icons.KeepToken} />
-        </div>
-        {formAmount ? (
-          <h3 className="flex-2 text-primary">
-            {formAmount && `${newAmount} KEEP`}
-          </h3>
-        ) : (
-          <span className="flex-2 text-big text-grey-40">
-            Add an amount below
-          </span>
-        )}
-      </div>
       <form onSubmit={formikProps.handleSubmit} className="mt-1">
         <FormInput
           name="amount"
@@ -59,30 +29,32 @@ const AmountForm = ({
           normalize={normalizeAmount}
           format={formatFormAmount}
         />
-        <ProgressBar
-          styles={styles.progressBar}
-          total={availableAmount}
-          items={[
-            {
-              value: fromTokenUnit(formAmount),
-              color: colors.primary,
-            },
-          ]}
-        />
-        <div className="text-caption text-grey-50">
-          {displayAmount(availableAmount)} KEEP available.
-        </div>
-        <div className="flex row center"></div>
-        <Button
-          className="btn btn-primary mt-1"
-          type="submit"
-          disabled={!(formikProps.isValid && formikProps.dirty)}
+        <div
+          className="text-caption--green-theme flex"
+          style={{ marginTop: "-1rem" }}
         >
-          {submitBtnText}
-        </Button>
-        <span onClick={onCancel} className="mt-1 ml-1 text-link">
-          Cancel
-        </span>
+          <span className="ml-a">
+            {displayAmount(availableAmount)} KEEP available.
+          </span>
+        </div>
+        <Divider
+          style={{
+            margin: "2rem -2rem 0",
+            padding: "2rem 2rem 0",
+          }}
+        />
+        <div className="flex row center mt-2">
+          <Button
+            className="btn btn-lg btn-primary"
+            type="submit"
+            disabled={!(formikProps.isValid && formikProps.dirty)}
+          >
+            {submitBtnText}
+          </Button>
+          <span onClick={onCancel} className="ml-2 text-link text-grey-70">
+            Cancel
+          </span>
+        </div>
       </form>
     </>
   )
@@ -96,7 +68,7 @@ const AmountFormWithFormik = withFormik({
     const errors = {}
 
     if (lte(amount || 0, 0)) {
-      errors.amount = "Insufficient funds"
+      errors.amount = "The value should be greater than zero"
     } else {
       errors.amount = validateAmountInRange(
         amount,
@@ -110,9 +82,5 @@ const AmountFormWithFormik = withFormik({
   handleSubmit: (values, { props }) => props.onBtnClick(values),
   displayName: "KEEPTokenAmountForm",
 })(AmountForm)
-
-const styles = {
-  progressBar: { margin: 0, marginTop: "-0.5rem" },
-}
 
 export default AmountFormWithFormik
