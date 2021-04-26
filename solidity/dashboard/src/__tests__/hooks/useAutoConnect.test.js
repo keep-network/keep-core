f1b735ea18941b10004e540a7d88880a4f3a6206
michalsmiarowski <michal.smiarowski@thesis.co>
2021-04-26 13:01:55 +0200

    Remove a test case from useAutoConnect hook test
    Since we no longer disconnect in useAutoConnect hoook we are removing the test
    case from the useAutoConnect.test.js.

--- solidity/dashboard/src/__tests__/hooks/useAutoConnect.test.js
+++ solidity/dashboard/src/__tests__/hooks/useAutoConnect.test.js
@@ -7,61 +7,34 @@ const mockAddress = "0x857173e7c7d76e051e80d30FCc3EA6A9C2b53756"
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
-  useLocation: jest
-    .fn(() => ({
-      pathname: mockCurrentUrl,
-    }))
-    .mockImplementationOnce(() => ({
-      pathname: `/${mockAddress}${mockCurrentUrl}`,
-    })),
-  useHistory: jest
-    .fn(() => ({
-      pathname: mockCurrentUrl,
-      push: mockHistoryPush,
-    }))
-    .mockImplementationOnce(() => ({
-      pathname: `/${mockAddress}${mockCurrentUrl}`,
-      push: mockHistoryPush,
-    })),
+  useLocation: jest.fn(() => ({
+    pathname: mockCurrentUrl,
+  })),
+  useHistory: jest.fn(() => ({
+    pathname: mockCurrentUrl,
+    push: mockHistoryPush,
+  })),
 }))
 
 jest.mock("../../hooks/useWalletAddressFromUrl", () =>
-  jest
-    .fn(() => {
-      return ""
-    })
-    .mockImplementationOnce(() => {
-      return mockAddress
-    })
+  jest.fn(() => {
+    return ""
+  })
 )
 
 jest.mock("../../components/WithWeb3Context", () => ({
-  useWeb3Context: jest
-    .fn(() => ({
-      yourAddress: mockAddress,
-      connector: {
-        name: "EXPLORER_MODE",
-      },
-      connectAppWithWallet: jest.fn(),
-    }))
-    .mockImplementationOnce(() => ({
-      yourAddress: "",
-      connector: null,
-      connectAppWithWallet: jest.fn(),
-    })),
+  useWeb3Context: jest.fn(() => ({
+    yourAddress: mockAddress,
+    connector: {
+      name: "EXPLORER_MODE",
+    },
+    connectAppWithWallet: jest.fn(),
+  })),
 }))
 
-describe("Current url with wallet address", () => {
-  it("change url when disconnecting from explorer mode", () => {
-    const { result2 } = renderHook(() => useExplorerModeConnect())
-
-    expect(mockHistoryPush).toHaveBeenCalledWith({ pathname: `/dashboard` })
-  })
-})
-
 describe("Current url without wallet address", () => {
-  it("change url when connecting to explorer mode2", () => {
-    const { result } = renderHook(() => useExplorerModeConnect())
+  it("change url when connecting to explorer mode", () => {
+    renderHook(() => useExplorerModeConnect())
 
     expect(mockHistoryPush).toHaveBeenCalledWith({
       pathname: `/${mockAddress}/dashboard`,
