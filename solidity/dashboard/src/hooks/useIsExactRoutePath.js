35d2b7305826b59fea667dab2552ef787233b8d7
michalsmiarowski <michal.smiarowski@thesis.co>
2021-04-29 00:48:29 +0200

    Create useIsExactRoutePath hook
    This hook checks if the path given in the url or the one from useLocation hook
    is correct and match any of the existing routes.

---
+++ solidity/dashboard/src/hooks/useIsExactRoutePath.js
@@ -0,0 +1,27 @@
+import { pages } from "../components/Routing"
+import { useLocation } from "react-router-dom"
+import { removeSubstringBetweenCharacter } from "../utils/general.utils"
+
+const useIsExactRoutePath = (path = null, cutOutWalletAddress = false) => {
+  const location = useLocation()
+  const realPath = path
+    ? path
+    : cutOutWalletAddress
+    ? removeSubstringBetweenCharacter(location.pathname, "/", 1)
+    : location.pathname
+
+  if (!realPath) return false
+
+  return pages.some((page) => {
+    let subpageMatch = false
+    // check if subpages match the path
+    if (page.route.pages?.length > 0) {
+      subpageMatch = page.route.pages.some((subpage) => {
+        return subpage.route.path === realPath
+      })
+    }
+    return subpageMatch ? subpageMatch : page.route.path === realPath
+  })
+}
+
+export default useIsExactRoutePath
