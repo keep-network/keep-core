0af68c7ccb84d0aa4988d53f76bb96280392501c
michalsmiarowski <michal.smiarowski@thesis.co>
2021-04-29 00:50:11 +0200

    Refactor useWalletAddressFromUrl hook
    Refactor useWalletAddressFromUrl hook so it uses the useIsExactRoutePath hook.

--- solidity/dashboard/src/hooks/useWalletAddressFromUrl.js
+++ solidity/dashboard/src/hooks/useWalletAddressFromUrl.js
@@ -1,18 +1,17 @@
 import web3Utils from "web3-utils"
 import { useRouteMatch } from "react-router-dom"
-import { pages } from "../components/Routing"
+import useIsExactRoutePath from "./useIsExactRoutePath"
 
 const useWalletAddressFromUrl = () => {
   const match = useRouteMatch(`/:address/:actualPath`)
+  const isActualPathCorrect = useIsExactRoutePath(
+    `/${match?.params?.actualPath}`,
+    true
+  )
+  if (!match?.params?.address || !match?.params?.actualPath) return false
 
-  if (match) {
-    const isActualPathCorrect = pages.some((page) => {
-      return page.route.path === `/${match.params.actualPath}`
-    })
-
-    if (isActualPathCorrect && web3Utils.isAddress(match.params.address)) {
-      return match.params.address
-    }
+  if (isActualPathCorrect && web3Utils.isAddress(match.params.address)) {
+    return match.params.address
   }
 
   return ""
