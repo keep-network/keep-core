cc63f1e40058c8b6d7bd093bbc4c9689d828cb51
michalsmiarowski <michal.smiarowski@thesis.co>
2021-05-07 14:34:04 +0200

    Move handleWindowSizeChange func into useEffect

--- solidity/dashboard/src/hooks/useCurrentWidthLowerThanThreshold.js
+++ solidity/dashboard/src/hooks/useCurrentWidthLowerThanThreshold.js
@@ -3,11 +3,11 @@ import { useEffect, useState } from "react"
 const useCurrentWidthLowerThanThreshold = (widthThreshold) => {
   const [width, setWidth] = useState(window.innerWidth)
 
-  const handleWindowSizeChange = () => {
-    setWidth(window.innerWidth)
-  }
-
   useEffect(() => {
+    const handleWindowSizeChange = () => {
+      setWidth(window.innerWidth)
+    }
+
     window.addEventListener("resize", handleWindowSizeChange)
     return () => {
       window.removeEventListener("resize", handleWindowSizeChange)
