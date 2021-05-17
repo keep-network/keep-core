1991e91b720e66555bac65fd05bf3a35b9c0357d
michalsmiarowski <michal.smiarowski@thesis.co>
2021-05-17 23:33:46 +0200

    Add decorator to TopUpsDataTable story

--- solidity/dashboard/src/stories/TopUpsTable.stories.js
+++ solidity/dashboard/src/stories/TopUpsTable.stories.js
@@ -1,9 +1,30 @@
 import React from "react"
 import { TopUpsDataTable } from "../components/TopUpsDataTable"
+import { storiesOf } from "@storybook/react"
+import centered from "@storybook/addon-centered/react"
+import Tile from "../components/Tile"
+import DataTableSkeleton from "../components/skeletons/DataTableSkeleton"
+import { LoadingOverlay } from "../components/Loadable"
+
+storiesOf("TopUpsDataTable", module).addDecorator(centered)
 
 export default {
   title: "TopUpsDataTable",
   component: TopUpsDataTable,
+  decorators: [
+    (Story) => (
+      <section>
+        <LoadingOverlay
+          isFetching={false}
+          skeletonComponent={<DataTableSkeleton columns={3} />}
+        >
+          <Tile>
+            <Story />
+          </Tile>
+        </LoadingOverlay>
+      </section>
+    ),
+  ],
 }
 
 const mockedTopUps = [
@@ -28,4 +49,3 @@ Default.args = {
   topUps: mockedTopUps,
   commitTopUp: null,
 }
-
