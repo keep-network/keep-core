a26b3e0d2c4ec9c7ae9b7a4ff1f317313107c8ec
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-06-09 14:19:31 +0200

    Render a CheckList component on the cov pool page

--- solidity/dashboard/src/pages/coverage-pools/CoveragePoolPage.jsx
+++ solidity/dashboard/src/pages/coverage-pools/CoveragePoolPage.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 import PageWrapper from "../../components/PageWrapper"
+import { CheckListBanner } from "../../components/coverage-pools"
 
 const CoveragePoolPage = ({ title, withNewLabel }) => {
-  return <PageWrapper title={title} newPage={withNewLabel}></PageWrapper>
+  return (
+    <PageWrapper title={title} newPage={withNewLabel}>
+      <CheckListBanner />
+    </PageWrapper>
+  )
 }
 
 export default CoveragePoolPage
