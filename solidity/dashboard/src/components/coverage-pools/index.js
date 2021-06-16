0d8ffbbe5b6a36220490b455f09bdfb5c4def7b5
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-06-16 14:21:30 +0200

    Add InitiateDepositModal
    Added a confirmation modal that requires to accept a term. It will be
    displayed as a confirmation modal before executing the
    `AssetPool.deposit` transaction.

--- solidity/dashboard/src/components/coverage-pools/index.js
+++ solidity/dashboard/src/components/coverage-pools/index.js
@@ -1,5 +1,11 @@
 import CheckListBanner from "./CheckListBanner"
 import HowDoesItWorkBanner from "./HowDoesItWorkBanner"
 import DepositForm from "./DepositForm"
+import InitiateDepositModal from "./InitiateDepositModal"
 
-export { CheckListBanner, HowDoesItWorkBanner, DepositForm }
+export {
+  CheckListBanner,
+  HowDoesItWorkBanner,
+  DepositForm,
+  InitiateDepositModal,
+}
