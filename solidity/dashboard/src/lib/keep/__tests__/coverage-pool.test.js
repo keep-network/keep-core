d67d2540a55a892f59d970a5322930b173663c48
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-06-18 14:51:57 +0200

    Add a coverage-pool lib
    Added a lib that provides useful functions for coverage-pools.

---
+++ solidity/dashboard/src/lib/keep/__tests__/coverage-pool.test.js
@@ -0,0 +1,80 @@
+import CoveragePoolV1 from "../coverage-pool"
+
+const createMockedContract = (address) => ({
+  makeCall: jest.fn(),
+  address: address,
+})
+
+describe("Test CoveragePoolV1 lib", () => {
+  let coveragePoolV1
+  beforeEach(() => {
+    const assetPoolContract = createMockedContract("0x9")
+    const rewardPoolContract = createMockedContract("0x8")
+    const covTokenContract = createMockedContract("0x7")
+    const corateralTokenContract = createMockedContract("0x6")
+
+    coveragePoolV1 = new CoveragePoolV1(
+      assetPoolContract,
+      rewardPoolContract,
+      covTokenContract,
+      corateralTokenContract
+    )
+  })
+
+  it("should return the total supply of the cov token", async () => {
+    const mockedResult = "1000"
+    coveragePoolV1.covTokenContract.makeCall.mockResolvedValue(mockedResult)
+
+    const result = await coveragePoolV1.covTotalSupply()
+
+    expect(coveragePoolV1.covTokenContract.makeCall).toHaveBeenCalledWith(
+      "totalSupply"
+    )
+    expect(result).toEqual(mockedResult)
+  })
+
+  it("should return the balance of te cov tokens for the provided address", async () => {
+    const mockedResult = "1000"
+    const mockedAddress = "0x0"
+    coveragePoolV1.covTokenContract.makeCall.mockResolvedValue(mockedResult)
+
+    const result = await coveragePoolV1.covBalanceOf(mockedAddress)
+
+    expect(coveragePoolV1.covTokenContract.makeCall).toHaveBeenCalledWith(
+      "balanceOf",
+      mockedAddress
+    )
+    expect(result).toEqual(mockedResult)
+  })
+
+  it("should return estimated rewards", async () => {
+    const shareOfPool = 0.35
+    coveragePoolV1.corateralTokenContract.makeCall.mockResolvedValue(75)
+    coveragePoolV1.rewardPoolContract.makeCall.mockResolvedValue(25)
+    const epxectedResult = 35
+
+    const result = await coveragePoolV1.estimatedRewards(shareOfPool)
+
+    expect(coveragePoolV1.corateralTokenContract.makeCall).toHaveBeenCalledWith(
+      "balanceOf",
+      coveragePoolV1.assetPoolContract.address
+    )
+    expect(coveragePoolV1.rewardPoolContract.makeCall).toHaveBeenCalledWith(
+      "earned"
+    )
+    expect(result).toEqual(epxectedResult.toString())
+  })
+
+  it("should return the share of pool", async () => {
+    const covBalanceOf = 35
+    const covTotalSupply = 100
+    const expectedResult = covBalanceOf / covTotalSupply
+
+    const result = await coveragePoolV1.shareOfPool(
+      covTotalSupply,
+      covBalanceOf
+    )
+
+    expect(result).toEqual(expectedResult.toString())
+  })
+})
---
+++ solidity/dashboard/src/lib/keep/coverage-pool/index.js
@@ -0,0 +1,53 @@
+import BigNumber from "bignumber.js"
+/** @typedef { import("../../web3").BaseContract} BaseContract */
+
+class CoveragePoolV1 {
+  /**
+   *
+   * @param {BaseContract} _assetPoolContract
+   * @param {BaseContract} _rewardPoolContract
+   * @param {BaseContract} _covTokenContract
+   * @param {BaseContract} _corateralTokenContract
+   */
+  constructor(
+    _assetPoolContract,
+    _rewardPoolContract,
+    _covTokenContract,
+    _corateralTokenContract
+  ) {
+    this.assetPoolContract = _assetPoolContract
+    this.rewardPoolContract = _rewardPoolContract
+    this.covTokenContract = _covTokenContract
+    this.corateralTokenContract = _corateralTokenContract
+  }
+
+  shareOfPool = async (covTotalSupply, covBalanceOf) => {
+    return new BigNumber(covBalanceOf).div(covTotalSupply).toString()
+  }
+
+  covTotalSupply = async () => {
+    return await this.covTokenContract.makeCall("totalSupply")
+  }
+
+  covBalanceOf = async (address) => {
+    return await this.covTokenContract.makeCall("balanceOf", address)
+  }
+
+  estimatedRewards = async (shareOfPool) => {
+    const tokensInPool = await this.corateralTokenContract.makeCall(
+      "balanceOf",
+      this.assetPoolContract.address
+    )
+
+    const earned = await this.rewardPoolContract.makeCall("earned")
+
+    return new BigNumber(tokensInPool)
+      .plus(new BigNumber(earned))
+      .multipliedBy(shareOfPool)
+      .toString()
+  }
+
+  apy = async () => {}
+}
+
+export default CoveragePoolV1
