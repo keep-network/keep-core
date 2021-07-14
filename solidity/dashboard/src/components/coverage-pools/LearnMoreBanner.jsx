f2dadde57d647608841906e26e42757fe46ca224
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-07-14 13:10:12 +0200

    Add new banner about coverage pools
    This banner is indicating that a coverage pools are available in the KEEP
    Token Dashboard. It also has a button that redirects to the resource
    page about coverage pools.

---
+++ solidity/dashboard/src/components/coverage-pools/LearnMoreBanner.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Banner from "../Banner"
+import * as Icons from "../Icons"
+import NavLink from "../NavLink"
+
+const LearnMoreBanner = ({ onClose }) => {
+  return (
+    <Banner className="banner banner--coverage-pools">
+      <Banner.CloseIcon onClick={onClose} />
+      <div className="flex row space-between full-center">
+        <Banner.Icon icon={Icons.CoveragePool} />
+        <Banner.Title className="h3 text-white">
+          Deposit KEEP in the coverage pool to
+          <br />
+          secure the network and earn rewards.
+        </Banner.Title>
+        <NavLink
+          to="coverage-pools/how-it-works"
+          className="btn btn-tertiary btn-lg"
+        >
+          learn more
+        </NavLink>
+      </div>
+    </Banner>
+  )
+}
+
+export default LearnMoreBanner
