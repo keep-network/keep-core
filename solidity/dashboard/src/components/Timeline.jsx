fe2289cef1548532ae5f4b468ff1fa36438c50f1
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-07-19 17:18:31 +0200

    Create a Timline component according to the Figma views
    There are a few things that we can update to make a Timeline component
    more flexible. Eg. this commit will add support for displaying the
    `Timlnie` component on the left side but in a future we may want to
    display events on alternating sides.

--- solidity/dashboard/src/components/Timeline.jsx
+++ solidity/dashboard/src/components/Timeline.jsx
@@ -1,7 +1,51 @@
-import React from "react"
+import React, { useContext } from "react"
 
-const Timeline = ({ children }) => {
-  return <div className="timeline"></div>
+const TimelineContext = React.createContext({
+  // May be useful in the future, eg. if we would like to be able to display the
+  // timeline events on alternating sides.
+  position: "left",
+})
+
+const useTimelineContext = () => {
+  const context = useContext(TimelineContext)
+  if (!context) {
+    throw new Error("TimelineContext used outside of Timeline component")
+  }
+  return context
+}
+
+const Timeline = ({ children, position = "left" }) => {
+  return (
+    <TimelineContext.Provider value={{ position: position }}>
+      <div className="timeline">{children}</div>
+    </TimelineContext.Provider>
+  )
+}
+const TimelineElement = ({ position, children }) => {
+  const { position: timelinePosition } = useTimelineContext()
+  const _position = position || timelinePosition
+  return (
+    <div className={`timeline__element timeline__element--${_position}`}>
+      {children}
+    </div>
+  )
+}
+Timeline.Element = TimelineElement
+
+Timeline.ElementBreakpoint = ({
+  children,
+  className = "",
+  // start, center
+  // TODO add support for a start position.
+  position = "center",
+}) => {
+  return (
+    <div
+      className={`element__breakpoint element__breakpoint--${position} ${className}`}
+    >
+      {children}
+    </div>
+  )
 }
 
 export default Timeline
