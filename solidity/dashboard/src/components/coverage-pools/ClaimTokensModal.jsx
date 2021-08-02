e4e05928f471440d7ffebdbb5df09928fbf22274
michalsmiarowski <michal.smiarowski@thesis.co>
2021-08-02 07:40:36 +0200

    Add timeline to token claim modal

--- solidity/dashboard/src/components/coverage-pools/ClaimTokensModal.jsx
+++ solidity/dashboard/src/components/coverage-pools/ClaimTokensModal.jsx
@@ -5,6 +5,9 @@ import { KEEP } from "../../utils/token.utils"
 import Divider from "../Divider"
 import Button from "../Button"
 import OnlyIf from "../OnlyIf"
+import Timeline from "../Timeline";
+import {colors} from "../../constants/colors";
+import Chip from "../Chip";
 
 const ClaimTokensModal = ({
   amount,
@@ -76,8 +79,61 @@ const ClaimTokensModal = ({
           </OnlyIf>
         </div>
       </div>
-      <div className={"claim-tokens-modal__timeline"}>
-        You are about to claim:
+      <div className={"claim-tokens-modal__timeline-container"}>
+        <h4>Overview</h4>
+        <Timeline className={"claim-tokens-modal__timeline"}>
+          <Timeline.Element>
+            <Timeline.Breakpoint>
+              <Timeline.BreakpointDot>1</Timeline.BreakpointDot>
+              <Timeline.BreakpointLine active />
+            </Timeline.Breakpoint>
+            <Timeline.Content>
+              <Timeline.ElementDefaultCard>
+                <h4 className="text-violet-80">Deposit your tokens</h4>
+              </Timeline.ElementDefaultCard>
+            </Timeline.Content>
+          </Timeline.Element>
+
+          <Timeline.Element>
+            <Timeline.Breakpoint>
+              <Timeline.BreakpointDot>2</Timeline.BreakpointDot>
+              <Timeline.BreakpointLine active />
+            </Timeline.Breakpoint>
+            <Timeline.Content>
+              <Timeline.ElementDefaultCard>
+                <h4 className="text-violet-80">Withdraw deposit</h4>
+              </Timeline.ElementDefaultCard>
+            </Timeline.Content>
+          </Timeline.Element>
+          <Timeline.Element>
+            <Timeline.Breakpoint>
+              <Timeline.BreakpointDot
+                lineBreaker
+                lineBreakerColor="violet-80"
+                style={{ backgroundColor: colors.brandViolet10 }}
+              />
+              <Timeline.BreakpointLine active />
+            </Timeline.Breakpoint>
+            <Timeline.Content>
+              <Chip text="21 day cooldown" color="strong" size="big" />
+            </Timeline.Content>
+          </Timeline.Element>
+
+          <Timeline.Element>
+            <Timeline.Breakpoint>
+              <Timeline.BreakpointDot>3</Timeline.BreakpointDot>
+              <Timeline.BreakpointLine active />
+            </Timeline.Breakpoint>
+            <Timeline.Content>
+              <Timeline.ElementDefaultCard>
+                <h4 className="text-violet-80">Claim tokens</h4>
+                <span className="text-grey-60">
+                  There’s a 2 day claim window to claim your tokens and rewards.
+                </span>
+              </Timeline.ElementDefaultCard>
+            </Timeline.Content>
+          </Timeline.Element>
+        </Timeline>
       </div>
     </div>
   )
