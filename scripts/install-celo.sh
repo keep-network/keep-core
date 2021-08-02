5dbb7e25f4f7aefd317147aaf39eb6a263bdedb6
Tomasz Slabon <tomasz.slabon@keep.network>
2021-08-02 17:07:15 +0200

    Remove artifact folder before copying

--- scripts/install-celo.sh
+++ scripts/install-celo.sh
@@ -89,6 +89,7 @@ CONTRACT_OWNER_CELO_ACCOUNT_PRIVATE_KEY=$CONTRACT_OWNER_CELO_ACCOUNT_PRIVATE_KEY
     npx truffle migrate --reset --network $NETWORK
 
 printf "${LOG_START}Copying contract artifacts...${LOG_END}"
+rm -rf artifacts
 cp -r build/contracts artifacts
 npm link
 
