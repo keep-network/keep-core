4094f5d1f2905bed5850ee84472ebce153a46d9c
Anton Bukov <k06aaa@gmail.com>
2021-08-09 19:16:56 +0300

    Fix negative tests

--- test/CumulativeMerkleDrop.behavior.js
+++ test/CumulativeMerkleDrop.behavior.js
@@ -5,67 +5,70 @@ function claimedEvent (account, amount) {
 }
 
 function shouldBehaveLikeCumulativeMerkleDropFor4WalletsWithBalances1234 (errorPrefix, _, wallets, findSortedIndex, makeFirstDrop, makeSecondDrop) {
-    describe('Double drop for 4 wallets: [1, 2, 3, 4] + [2, 3, 4, 5] = [3, 5, 7, 9]', async function () {
+    describe('First wallet checks', async function () {
         beforeEach(async function () {
             await makeFirstDrop(this);
         });
 
-        describe('First wallet', async function () {
-            it('should success to claim 1 token, second drop and claim 2 tokens', async function () {
-                await expectEvent(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 1, this.root, this.proofs[0]),
-                    'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '1'),
-                );
+        it('should success to claim 1 token, second drop and claim 2 tokens', async function () {
+            await expectEvent(
+                await this.drop.claim(wallets[findSortedIndex(this, 0)], 1, this.root, this.proofs[0]),
+                'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '1'),
+            );
 
-                await makeSecondDrop(this);
+            await makeSecondDrop(this);
 
-                await expectEvent(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
-                    'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '2'),
-                );
-            });
+            await expectEvent(
+                await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
+                'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '2'),
+            );
 
-            it('should success to claim 1 token, second drop and claim 2 tokens twice', async function () {
-                await expectEvent(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 1, this.root, this.proofs[0]),
-                    'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '1'),
-                );
+            await expectRevert(
+                this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
+                `${errorPrefix}: Nothing to claim`,
+            );
+        });
 
-                await makeSecondDrop(this);
+        it('should success to claim 1 token, second drop and claim 2 tokens twice', async function () {
+            await expectEvent(
+                await this.drop.claim(wallets[findSortedIndex(this, 0)], 1, this.root, this.proofs[0]),
+                'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '1'),
+            );
 
-                await expectEvent(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
-                    'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '2'),
-                );
+            await makeSecondDrop(this);
 
-                await expectRevert(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
-                    `${errorPrefix}: Nothing to claim`,
-                );
-            });
+            await expectEvent(
+                await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
+                'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '2'),
+            );
 
-            it('should success to claim all 3 tokens after second drop', async function () {
-                await makeSecondDrop(this);
+            await expectRevert(
+                this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
+                `${errorPrefix}: Nothing to claim`,
+            );
+        });
 
-                await expectEvent(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
-                    'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '3'),
-                );
-            });
+        it('should success to claim all 3 tokens after second drop', async function () {
+            await makeSecondDrop(this);
 
-            it('should fail to claim after succelfful claim of all 3 tokens after second drop', async function () {
-                await makeSecondDrop(this);
+            await expectEvent(
+                await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
+                'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '3'),
+            );
+        });
 
-                await expectEvent(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
-                    'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '3'),
-                );
+        it('should fail to claim after succelfful claim of all 3 tokens after second drop', async function () {
+            await makeSecondDrop(this);
 
-                await expectRevert(
-                    await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
-                    `${errorPrefix}: Nothing to claim`,
-                );
-            });
+            await expectEvent(
+                await this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
+                'Claimed', claimedEvent(wallets[findSortedIndex(this, 0)], '3'),
+            );
+
+            await expectRevert(
+                this.drop.claim(wallets[findSortedIndex(this, 0)], 3, this.root, this.proofs[0]),
+                `${errorPrefix}: Nothing to claim`,
+            );
         });
     });
 }
