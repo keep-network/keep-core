b61d119ea8c091b6b91e94d0295cdd4088a2ff07
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-08-12 10:54:32 +0200

    Add skeletons of the TBTC migration pages

---
+++ solidity/dashboard/src/pages/tbtc-migration/HowItWorksPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const HowItWorksPage = () => {
+  return <>how it works page</>
+}
+
+HowItWorksPage.route = {
+  title: "How it Works",
+  path: "/tbtc-migration/how-it-works",
+  exact: true,
+}
+
+export default HowItWorksPage
---
+++ solidity/dashboard/src/pages/tbtc-migration/TokenUpgradePortalPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const TokenUpgradePortalPage = () => {
+  return <>token upgrade page</>
+}
+
+TokenUpgradePortalPage.route = {
+  title: "Token Upgrade Portal",
+  path: "/tbtc-migration/portal",
+  exact: true,
+}
+
+export default TokenUpgradePortalPage
