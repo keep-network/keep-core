7a1a7ae0d9afb92894056c0e23dd66a182c11e4f
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-08-14 00:00:08 +0200

    Prepare a skeleton of the Token Upgrade Portal page

--- solidity/dashboard/src/pages/tbtc-migration/TokenUpgradePortalPage.jsx
+++ solidity/dashboard/src/pages/tbtc-migration/TokenUpgradePortalPage.jsx
@@ -1,7 +1,43 @@
 import React from "react"
+import List from "../../components/List"
+import TokenAmount from "../../components/TokenAmount"
+import { TBTC } from "../../utils/token.utils"
 
 const TokenUpgradePortalPage = () => {
-  return <>token upgrade page</>
+  const tbtcV1Balance = "0"
+  const tbtcV2Balance = "0"
+  return (
+    <section className="tbtc-migration-portal">
+      <List className="tbtc-migration-portal__tbtc-balances">
+        <List.Title className="h3 text-grey-70">Balance</List.Title>
+        <List.Content className="tbtc-balances">
+          <List.Item className="tbtc-balance tbtc-balance--v1">
+            <TokenAmount
+              token={TBTC}
+              amount={tbtcV1Balance}
+              symbol="tBTC v1"
+              amountClassName="h2 text-white"
+              symbolClassName="h3 text-white"
+              withIcon
+            />
+          </List.Item>
+          <List.Item className="tbtc-balance tbtc-balance--v2">
+            <TokenAmount
+              token={TBTC}
+              amount={tbtcV2Balance}
+              symbol="tBTC v2"
+              amountClassName="h2 text-black"
+              symbolClassName="h3 text-black"
+              withIcon
+            />
+          </List.Item>
+        </List.Content>
+      </List>
+      <section className="tbtc-migration-portal__form-wrapper">
+        {/* TODO: Form content here */}
+      </section>
+    </section>
+  )
 }
 
 TokenUpgradePortalPage.route = {
