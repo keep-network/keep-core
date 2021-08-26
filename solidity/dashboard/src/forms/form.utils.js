f4e29c2502db26e34dc79e7b062a65231d6955b7
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-08-26 09:54:21 +0200

    Add `normalizeFloatingAmount` function
    It will normalize floating numbers in forms:
    * remove chars expect numbeer and point,
    * remove multiple points,
    * remove middle hyphen,
    * remove multiple leeading zeros.

--- solidity/dashboard/src/forms/form.utils.js
+++ solidity/dashboard/src/forms/form.utils.js
@@ -8,3 +8,10 @@ export const formatAmount = (value) => {
   const newValue = value ? value.replace(ONLY_NUMBERS, "") : 0
   return new BigNumber(newValue).toFormat(0)
 }
+
+export const normalizeFloatingAmount = (value) =>
+  value
+    .replace(/[^0-9.]/g, "") // remove chars except number, point.
+    .replace(/(\..*)\./g, "$1") // remove multiple points.
+    .replace(/(?!^)-/g, "") // remove middle hyphen.
+    .replace(/^0+(\d)/gm, "$1") // remove multiple leading zeros.
