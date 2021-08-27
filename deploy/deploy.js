d607ee23ad42bde40d849f222e4a5ee8783feb3a
Mikhail Melnik <by.zumzoom@gmail.com>
2021-08-27 13:32:53 +0800

    add deploy script

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,5 +1,5 @@
 const hre = require('hardhat');
-const { getChainId } = hre;
+const { getChainId, ethers } = hre;
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
     console.log('running deploy script');
@@ -8,15 +8,24 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const example = await deploy('Example', {
+    const args = ['0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'];
+
+    const merkleDrop = await deploy('CumulativeMerkleDrop', {
         from: deployer,
+        args: args,
+        skipIfAlreadyDeployed: true,
     });
 
-    console.log('Example deployed to:', example.address);
+    const CumulativeMerkleDrop = await ethers.getContractFactory('CumulativeMerkleDrop');
+    const cumulativeMerkleDrop = CumulativeMerkleDrop.attach(merkleDrop.address);
+    await cumulativeMerkleDrop.setMerkleRoot('0xd76ea6876293c58ef1fa269fed8274d9784195fd3c2a12dd0bd35b5729d24f76');
+
+    console.log('CumulativeMerkleDrop deployed to:', merkleDrop.address);
 
     if (await getChainId() !== '31337') {
         await hre.run('verify:verify', {
-            address: example.address,
+            address: merkleDrop.address,
+            constructorArguments: args,
         });
     }
 };
