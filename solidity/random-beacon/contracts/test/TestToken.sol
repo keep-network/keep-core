05ce4d75335ffcdedd7e0309b04d830db52e434e
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-09-30 11:34:13 +0200

    Remove examples
    Removed examples of contract and test that are no longer needed, as we
    added proper contracts.

--- solidity/random-beacon/contracts/test/TestToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.6;
-
-// Stub contract used for initial project setup.
-// Should be removed once we have actual contracts.
-contract TestToken {
-    string public name = "Test Token";
-}
