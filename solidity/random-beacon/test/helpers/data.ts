8bb79f16f1ebfe9cf1dab703396ab373bc394c36
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-10-06 00:44:27 +0200

    Add bls test data
    The data were copied from random beacon V1 implementation.

---
+++ solidity/random-beacon/test/helpers/data.ts
@@ -0,0 +1,8 @@
+export const blsData = {
+  // data generated using master secret key 123
+  secretKey: 123,
+
+  // altbn128 public key for secret key 123
+  groupPubKey:
+    "0x1f1954b33144db2b5c90da089e8bde287ec7089d5d6433f3b6becaefdb678b1b2a9de38d14bef2cf9afc3c698a4211fa7ada7b4f036a2dfef0dc122b423259d01659dc18b57722ecf6a4beb4d04dfe780a660c4c3bb2b165ab8486114c464c621bf37ecdba226629c20908c7f475c5b3a7628ce26d696436eab0b0148034dfcd",
+}
