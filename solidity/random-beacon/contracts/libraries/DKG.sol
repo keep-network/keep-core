df736f0ef6f6f82b70a6ee2a24b583165f034118
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-10-05 14:47:56 +0200

    Add dkg result verification function
    The function was copied from V1 implementation: https://github.com/keep-network/keep-core/blob/e1abb9d322843857359f812c916a84e904a6b6a3/solidity-v1/contracts/libraries/operator/DKGResultVerification.sol#L46

--- solidity/random-beacon/contracts/libraries/DKG.sol
+++ solidity/random-beacon/contracts/libraries/DKG.sol
@@ -1,16 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "../utils/BytesLib.sol";
+
 // TODO: Consider which functions can be internal or internal. What are implications
 // to security.
 
 /// @title DKG library
 library DKG {
+    using BytesLib for bytes;
+    using ECDSA for bytes32;
+
     struct Data {
         uint256 seed;
+        // Size of a group in the threshold relay.
         uint256 groupSize;
+        // Time in blocks after which the next group member is eligible
+        // to submit DKG result.
         uint256 dkgSubmissionEligibilityDelay;
+        // Time in blocks at which DKG started.
         uint256 startBlock;
+        // Time in blocks after which DKG result is complete and ready to be
+        // published by clients.
+        uint256 timeDKG;
+        // The minimum number of signatures required to support DKG result.
+        // This number needs to be at least the same as the signing threshold
+        // and it is recommended to make it higher than the signing threshold
+        // to keep a safety margin for misbehaving members.
+        uint256 signatureThreshold;
     }
 
     /// DKG is in an invalid state. Expected in progress: `expectedInProgress`,
@@ -66,6 +84,99 @@ library DKG {
         self.startBlock = block.number;
 
         emit DkgStarted(seed, groupSize, dkgSubmissionEligibilityDelay);
+    }
+
+    /// @notice Verifies the submitted DKG result against supporting member
+    /// signatures and if the submitter is eligible to submit at the current
+    /// block. Every signature supporting the result has to be from a unique
+    /// group member.
+    /// @param submitterMemberIndex Claimed submitter candidate group member index
+    /// @param groupPubKey Generated candidate group public key
+    /// @param misbehaved Bytes array of misbehaved (disqualified or inactive)
+    /// group members indexes; Indexes reflect positions of members in the group,
+    /// as outputted by the group selection protocol.
+    /// @param signatures Concatenation of signatures from members supporting the
+    /// result.
+    /// @param signingMemberIndices Indices of members corresponding to each
+    /// signature. Indices have to be unique.
+    /// @param members Addresses of candidate group members as outputted by the
+    /// group selection protocol.
+    /// @param groupSelectionEndBlock Block height at which the group selection
+    /// protocol ended.
+    function verify(
+        Data storage self,
+        uint256 submitterMemberIndex,
+        bytes memory groupPubKey,
+        bytes memory misbehaved,
+        bytes memory signatures,
+        uint256[] memory signingMemberIndices,
+        address[] memory members,
+        uint256 groupSelectionEndBlock
+    ) public view assertInProgress(self, true) {
+        require(submitterMemberIndex > 0, "Invalid submitter index");
+        require(
+            members[submitterMemberIndex - 1] == msg.sender,
+            "Unexpected submitter index"
+        );
+
+        uint256 T_init = groupSelectionEndBlock + self.timeDKG;
+        require(
+            block.number >=
+                (T_init +
+                    (submitterMemberIndex - 1) *
+                    self.dkgSubmissionEligibilityDelay),
+            "Submitter not eligible"
+        );
+
+        require(groupPubKey.length == 128, "Malformed group public key");
+
+        require(
+            misbehaved.length <= self.groupSize - self.signatureThreshold,
+            "Malformed misbehaved bytes"
+        );
+
+        uint256 signaturesCount = signatures.length / 65;
+        require(signatures.length >= 65, "Too short signatures array");
+        require(signatures.length % 65 == 0, "Malformed signatures array");
+        require(
+            signaturesCount == signingMemberIndices.length,
+            "Unexpected signatures count"
+        );
+        require(
+            signaturesCount >= self.signatureThreshold,
+            "Too few signatures"
+        );
+        require(signaturesCount <= self.groupSize, "Too many signatures");
+
+        bytes32 resultHash = keccak256(
+            abi.encodePacked(groupPubKey, misbehaved)
+        );
+
+        bytes memory current; // Current signature to be checked.
+
+        bool[] memory usedMemberIndices = new bool[](self.groupSize);
+
+        for (uint256 i = 0; i < signaturesCount; i++) {
+            uint256 memberIndex = signingMemberIndices[i];
+            require(memberIndex > 0, "Invalid index");
+            require(memberIndex <= members.length, "Index out of range");
+
+            require(
+                !usedMemberIndices[memberIndex - 1],
+                "Duplicate member index"
+            );
+            usedMemberIndices[memberIndex - 1] = true;
+
+            current = signatures.slice(65 * i, 65);
+            address recoveredAddress = resultHash
+                .toEthSignedMessageHash()
+                .recover(current);
+
+            require(
+                members[memberIndex - 1] == recoveredAddress,
+                "Invalid signature"
+            );
+        }
     }
 
     function notifyTimeout(Data storage self)
