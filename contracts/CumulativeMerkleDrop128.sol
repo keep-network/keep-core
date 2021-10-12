c624121e6c604bddf09d4d8d791f4aa030d47603
Mikhail Melnik <by.zumzoom@gmail.com>
2021-10-12 21:02:39 +0800

    minor

--- contracts/CumulativeMerkleDrop128.sol
+++ contracts/CumulativeMerkleDrop128.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+// import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "./interfaces/ICumulativeMerkleDrop128.sol";
 
 
