42dc9395cff0511d9f94f8b874c0090687713478
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-10-14 15:49:17 +0200

    Update `useModal` hook
    To read the modal data from the redux store. It also remains consistent
    with the old implementation.

--- solidity-v1/dashboard/src/hooks/useModal.js
+++ solidity-v1/dashboard/src/hooks/useModal.js
@@ -1,6 +1,51 @@
-import { useContext } from "react"
-import { ModalContext } from "../components/Modal"
+import { useCallback, useRef } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { modalActions } from "../actions"
 
 export const useModal = () => {
-  return useContext(ModalContext)
+  const modalType = useSelector((state) => state.modal.modalType)
+  const modalProps = useSelector((state) => state.modal.modalProps)
+  const awaitingPromiseRef = useRef()
+
+  const dispatch = useDispatch()
+
+  const openModal = useCallback(
+    (modalType, props) =>
+      dispatch(modalActions.showModal({ modalType, modalProps: props })),
+    [dispatch]
+  )
+
+  const closeModal = useCallback(
+    () => dispatch(modalActions.hideModal()),
+    [dispatch]
+  )
+
+  const onSubmitConfirmationModal = useCallback(
+    (values) => {
+      if (awaitingPromiseRef.current) {
+        awaitingPromiseRef.current.resolve(values)
+      }
+      closeModal()
+    },
+    [closeModal]
+  )
+
+  const openConfirmationModal = useCallback(
+    (modalType, props) => {
+      openModal(modalType, { ...props, onConfirm: onSubmitConfirmationModal })
+
+      return new Promise((resolve, reject) => {
+        awaitingPromiseRef.current = { resolve, reject }
+      })
+    },
+    [openModal, onSubmitConfirmationModal]
+  )
+
+  return {
+    modalType,
+    modalProps,
+    openModal,
+    closeModal,
+    openConfirmationModal,
+  }
 }
