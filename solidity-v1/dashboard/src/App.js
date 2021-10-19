d2aa1385969014e45a86f59ec30c1741d58f976e
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-10-19 14:42:22 +0200

    Cleanup App root component

--- solidity-v1/dashboard/src/App.js
+++ solidity-v1/dashboard/src/App.js
@@ -6,7 +6,6 @@ import Web3ContextProvider from "./components/Web3ContextProvider"
 import Routing from "./components/Routing"
 import { Messages } from "./components/Message"
 import { SideMenu } from "./components/SideMenu"
-// import { ModalContextProvider } from "./components/Modal"
 import * as Icons from "./components/Icons"
 import Footer from "./components/Footer"
 import useSubscribeToConnectorEvents from "./hooks/useSubscribeToConnectorEvents"
@@ -14,19 +13,14 @@ import useAutoConnect from "./hooks/useAutoConnect"
 import useAutoWalletAddressInjectIntoUrl from "./hooks/useAutoWalletAddressInjectIntoUrl"
 import useModalWindowForMobileUsers from "./hooks/useModalWindowForMobileUsers"
 import { ModalRoot } from "./components/modal-component"
-import { useModal } from "./hooks/useModal"
-import { MODAL_TYPES } from "./constants/constants"
-import Button from "./components/Button"
 
 const App = () => (
   <Provider store={store}>
     <Router basename={`${process.env.PUBLIC_URL}`}>
       <Messages>
         <Web3ContextProvider>
-          {/* <ModalContextProvider> */}
           <ModalRoot />
           <AppLayout />
-          {/* </ModalContextProvider> */}
         </Web3ContextProvider>
       </Messages>
     </Router>
@@ -38,18 +32,9 @@ const AppLayout = () => {
   useAutoWalletAddressInjectIntoUrl()
   useSubscribeToConnectorEvents()
   useModalWindowForMobileUsers()
-  const { openConfirmationModal } = useModal()
-
-  const a = async () => {
-    const result = await openConfirmationModal(MODAL_TYPES.Example, {
-      name: "Raf",
-    })
-    console.log("result", result)
-  }
 
   return (
     <>
-      <Button onClick={a}>open confirmation modal</Button>
       <AppHeader />
       <section className="app__content">
         <Routing />
