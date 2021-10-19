c0ec46647b17d109a36fd436c4409011c1fe1f70
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-10-19 16:21:33 +0200

    Expose function to get groups

--- solidity/random-beacon/contracts/test/TestRandomBeacon.sol
+++ solidity/random-beacon/contracts/test/TestRandomBeacon.sol
@@ -2,11 +2,16 @@ pragma solidity ^0.8.6;
 
 import "../RandomBeacon.sol";
 import "../libraries/DKG.sol";
+import "../libraries/Groups.sol";
 
 contract TestRandomBeacon is RandomBeacon {
     constructor(ISortitionPool _sortitionPool) RandomBeacon(_sortitionPool) {}
 
     function getDkgData() external view returns (DKG.Data memory) {
         return dkg;
+    }
+
+    function getGroups() external view returns (Groups.Group[] memory) {
+        return groups.groups;
     }
 }
