4d63ece1e1df7d03658e684aee7cca4f35238787
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-10-20 12:55:38 +0200

    Add missing param to `RandomBeaconStub`

--- solidity/random-beacon/contracts/test/RandomBeaconStub.sol
+++ solidity/random-beacon/contracts/test/RandomBeaconStub.sol
@@ -2,9 +2,12 @@ pragma solidity ^0.8.6;
 
 import "../RandomBeacon.sol";
 import "../libraries/DKG.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RandomBeaconStub is RandomBeacon {
-    constructor(ISortitionPool _sortitionPool) RandomBeacon(_sortitionPool) {}
+    constructor(ISortitionPool _sortitionPool, IERC20 _tToken)
+        RandomBeacon(_sortitionPool, _tToken)
+    {}
 
     function getDkgData() external view returns (DKG.Data memory) {
         return dkg;
