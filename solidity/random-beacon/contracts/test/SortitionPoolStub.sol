af4165050845ab672f5fcdc6ffe7794b6b7ec029
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-10-28 15:25:25 +0200

    Add SortitionTree to SortitionPoolStub

--- solidity/random-beacon/contracts/test/SortitionPoolStub.sol
+++ solidity/random-beacon/contracts/test/SortitionPoolStub.sol
@@ -2,15 +2,27 @@
 
 pragma solidity ^0.8.6;
 
+import "@keep-network/sortition-pools/contracts/SortitionTree.sol";
 import "../RandomBeacon.sol";
 
 // Stub contract used in tests
-contract SortitionPoolStub is ISortitionPool {
+contract SortitionPoolStub is ISortitionPool, SortitionTree {
     mapping(address => bool) public operators;
     mapping(address => bool) public eligibleOperators;
 
+    constructor() SortitionTree() {
+        // Fill operators IDs array so identifiers start with bigger number.
+        uint256 i = 100;
+        while (i > 0) {
+            allocateOperatorID(address(0));
+            i--;
+        }
+    }
+
     function joinPool(address operator) external override {
         operators[operator] = true;
+
+        allocateOperatorID(operator);
     }
 
     function isOperatorInPool(address operator)
@@ -34,5 +46,14 @@ contract SortitionPoolStub is ISortitionPool {
         returns (bool)
     {
         return eligibleOperators[operator];
+    }
+
+    function getIDOperator(uint32 id)
+        public
+        view
+        override(ISortitionPool, SortitionTree)
+        returns (address)
+    {
+        return SortitionTree.getIDOperator(id);
     }
 }
