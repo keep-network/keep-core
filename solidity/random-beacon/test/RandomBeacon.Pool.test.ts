d3a6cfaa57fd86e5344387ac48d1c11bda9b3e30
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-11-03 15:41:22 +0100

    Adjust pool tests after pulling recent changes

--- solidity/random-beacon/test/RandomBeacon.Pool.test.ts
+++ solidity/random-beacon/test/RandomBeacon.Pool.test.ts
@@ -5,15 +5,15 @@ import { expect } from "chai"
 import { Contract, ContractTransaction } from "ethers"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { randomBeaconDeployment } from "./fixtures"
-import type { RandomBeacon, SortitionPoolStub } from "../typechain"
+import type { RandomBeaconStub, SortitionPoolStub } from "../typechain"
 
 const { time } = helpers
 const { increaseTime } = time
 
 describe("RandomBeacon - Pool", () => {
   let operator: SignerWithAddress
-  let randomBeacon: Contract
-  let sortitionPoolStub: Contract
+  let randomBeacon: RandomBeaconStub
+  let sortitionPoolStub: SortitionPoolStub
 
   // prettier-ignore
   before(async () => {
@@ -24,7 +24,7 @@ describe("RandomBeacon - Pool", () => {
     const contracts = await waffle.loadFixture(randomBeaconDeployment)
 
     sortitionPoolStub = contracts.sortitionPoolStub as SortitionPoolStub
-    randomBeacon = contracts.randomBeacon as RandomBeacon
+    randomBeacon = contracts.randomBeacon as RandomBeaconStub
   })
 
   describe("registerOperator", () => {
@@ -44,10 +44,15 @@ describe("RandomBeacon - Pool", () => {
       })
 
       context("when punishment for given operator already elapsed", () => {
+        let operatorID: number
+
         beforeEach(async () => {
+          await randomBeacon.connect(operator).registerOperator()
+          operatorID = await sortitionPoolStub.getOperatorID(operator.address)
+
           const punishmentDuration = 1209600 // 2 weeks
           await randomBeacon.publicPunishOperators(
-            [operator.address],
+            [operatorID],
             punishmentDuration
           )
 
@@ -72,10 +77,15 @@ describe("RandomBeacon - Pool", () => {
       })
 
       context("when there is an active punishment for given operator", () => {
+        let operatorID: number
+
         beforeEach(async () => {
+          await randomBeacon.connect(operator).registerOperator()
+          operatorID = await sortitionPoolStub.getOperatorID(operator.address)
+
           const punishmentDuration = 1209600 // 2 weeks
           await randomBeacon.publicPunishOperators(
-            [operator.address],
+            [operatorID],
             punishmentDuration
           )
 
@@ -105,16 +115,16 @@ describe("RandomBeacon - Pool", () => {
 
   describe("updateOperatorStatus", () => {
     let tx: ContractTransaction
+    let operatorID: number
 
     beforeEach(async () => {
       // Operator is registered and gas deposit is made.
-      await randomBeacon
-        .connect(await ethers.getSigner(operator.address))
-        .registerOperator()
+      await randomBeacon.connect(operator).registerOperator()
+      operatorID = await sortitionPoolStub.getOperatorID(operator.address)
 
       // We simulate the removal during status update directly on the
       // sortition pool stub to leave the gas deposit untouched.
-      await sortitionPoolStub.removeOperator(operator.address)
+      await sortitionPoolStub.removeOperators([operatorID])
 
       tx = await randomBeacon.connect(operator).updateOperatorStatus()
     })
@@ -122,7 +132,7 @@ describe("RandomBeacon - Pool", () => {
     it("should update operator status", async () => {
       await expect(tx)
         .to.emit(sortitionPoolStub, "OperatorStatusUpdated")
-        .withArgs(operator.address)
+        .withArgs(operatorID)
     })
 
     it("should release the gas deposit if operator was removed from pool during the update", async () => {
