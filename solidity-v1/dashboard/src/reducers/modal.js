8f5a54a4f551978c3dfd2b94813501aa8a16bde2
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-11-11 13:54:19 +0100

    Cleanup modal reducer
    Remove unused `isOpen` field.

--- solidity-v1/dashboard/src/reducers/modal.js
+++ solidity-v1/dashboard/src/reducers/modal.js
@@ -1,7 +1,6 @@
 import { CLOSE_MODAL, OPEN_MODAL } from "../actions/modal"
 
 const initialState = {
-  isOpen: false,
   modalProps: {},
   modalType: null,
 }
@@ -10,7 +9,6 @@ const modalReducer = (state = initialState, action) => {
   switch (action.type) {
     case OPEN_MODAL:
       return {
-        isOpen: true,
         modalType: action.payload.modalType,
         modalProps: {
           ...action.payload.modalProps,
@@ -19,7 +17,6 @@ const modalReducer = (state = initialState, action) => {
     case CLOSE_MODAL:
       return {
         ...state,
-        isOpen: false,
         modalType: null,
         modalProps: {},
       }
