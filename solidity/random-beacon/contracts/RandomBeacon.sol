58aa12af7db750763117147c7a7eec8828b46901
Dmitry <dymitr.paremski@keep.network>
2021-11-19 14:47:13 +0100

    Slithering, do not use too many digitswq

--- solidity/random-beacon/contracts/RandomBeacon.sol
+++ solidity/random-beacon/contracts/RandomBeacon.sol
@@ -283,7 +283,7 @@ contract RandomBeacon is Ownable {
         sortitionPoolRewardsBanDuration = 2 weeks;
         relayEntryTimeoutNotificationRewardMultiplier = 5;
         // TODO: Revisit the initial value.
-        minimumStake = 100000;
+        minimumStake = 100e3;
 
         dkg.initSortitionPool(_sortitionPool);
         dkg.setResultChallengePeriodLength(1440); // ~6h assuming 15s block time
