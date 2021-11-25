fa0c7b1b9225bc3a052626e15f6e55f0779da19e
Piotr Dyraga <piotr.dyraga@keep.network>
2021-11-25 11:52:56 +0100

    Improved algorithm for shuffling signers array in Group creation tests
    Instead of just swapping one signer we now use Schwartzian transform to
    shuffle the array. Swapping one signer was not enough on my local machine
    and tests were failing because the signer list was just like expected.

--- solidity/random-beacon/test/RandomBeacon.GroupCreation.test.ts
+++ solidity/random-beacon/test/RandomBeacon.GroupCreation.test.ts
@@ -2090,5 +2090,8 @@ async function assertDkgResultCleanData(randomBeacon: RandomBeaconStub) {
 }
 
 function mixSigners(signers: Operator[]): Operator[] {
-  return [...signers.slice(0, 63), signers[0]]
+  return signers
+    .map((v) => ({ v, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ v }) => v)
 }
