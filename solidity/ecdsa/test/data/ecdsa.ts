4f3de038af5eb5dc2305961adb8e13094ef34528
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-01-14 20:54:34 +0100

    Update ECDSA test data
    The values are taken from keep-ecdsa V1 tests.

--- solidity/ecdsa/test/data/ecdsa.ts
+++ solidity/ecdsa/test/data/ecdsa.ts
@@ -1,20 +1,17 @@
-// FIXME: Provide valid ECDSA test data.
-
 const ecdsaData = {
   // ecdsa private key
   privateKey:
     "0x937ffe93cfc943d1a8fc0cb8bad44a978090a4623da81eefdff5380d0a290b41",
 
   // ecdsa public key
-  groupPubKey:
+  publicKey:
     "0x9a0544440cc47779235ccb76d669590c2cd20c7e431f97e17a1093faf03291c473e661a208a8a565ca1e384059bd2ff7ff6886df081ff1229250099d388c83df",
 
-  // ??? initial beacon entry
-  relayEntry:
-    "0x8bacaa8f02ef807f2f61ae8e00a5bfa4528148e0ae73b2bd54b71b8abe61268e",
+  // digest to sign
+  digest1: "0x8bacaa8f02ef807f2f61ae8e00a5bfa4528148e0ae73b2bd54b71b8abe61268e",
 
-  // ??? group signature over relayEntry
-  groupSignature:
+  // group signature over `digest`
+  signature1:
     "0xedc074a86380cc7e2e4702eaf1bec87843bc0eb7ebd490f5bdd7f024931491703f5005a26eb6f065ea9faea543e5ddb657d13892db2656499a43dfebd6e12efc1c",
 }
 
