e12b5d8ec67494694daa9990ffffd4510ff667e2
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-01-25 16:49:55 +0100

    Remove not used functions from StakingStub

--- solidity/ecdsa/contracts/test/StakingStub.sol
+++ solidity/ecdsa/contracts/test/StakingStub.sol
@@ -9,44 +9,6 @@ import {IWalletStaking} from "../WalletRegistry.sol";
 contract StakingStub is IWalletStaking {
     mapping(address => uint256) public stakedTokens;
 
-    event Slashed(uint256 amount, address[] operators);
-
-    event Seized(
-        uint256 amount,
-        uint256 rewardMultiplier,
-        address notifier,
-        address[] operators
-    );
-
-    function slash(uint256 amount, address[] memory operators) external {
-        if (amount > 0 && operators.length > 0) {
-            emit Slashed(amount, operators);
-        }
-    }
-
-    function seize(
-        uint256 amount,
-        uint256 rewardMultiplier,
-        address notifier,
-        address[] memory operators
-    ) external {
-        if (amount > 0 && operators.length > 0) {
-            emit Seized(amount, rewardMultiplier, notifier, operators);
-        }
-    }
-
-    function rolesOf(address operator)
-        external
-        view
-        returns (
-            address owner,
-            address beneficiary,
-            address authorizer
-        )
-    {
-        return (operator, operator, operator);
-    }
-
     function eligibleStake(
         address operator,
         address // operatorContract
