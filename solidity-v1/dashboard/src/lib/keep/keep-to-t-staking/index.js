d5f40ad71061adf1a7282fc8156b095b2a400d96
michalsmiarowski <michal.smiarowski@thesis.co>
2022-02-02 09:49:49 +0100

    Change operator to stakingProvider
    Change `operator` to `stakingProvider` when getting past `Staked` events by
    stakingProvider from thresholdStakingContract.

--- solidity-v1/dashboard/src/lib/keep/keep-to-t-staking/index.js
+++ solidity-v1/dashboard/src/lib/keep/keep-to-t-staking/index.js
@@ -10,7 +10,7 @@ class KeepToTStaking {
 
   getStakedEventsByOperator = async (operatorAddress) => {
     return await this.thresholdStakingContract.getPastEvents("Staked", {
-      operator: operatorAddress,
+      stakingProvider: operatorAddress,
     })
   }
 }
