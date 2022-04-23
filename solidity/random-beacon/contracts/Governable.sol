d99ab6057242c156d07784147387b973a9cb8fb8
Piotr Dyraga <piotr.dyraga@keep.network>
2022-04-23 21:07:22 +0200

    Governable contract copied from ecdsa to random-beacon
    This contract was originally implemented by @nkuba for ECDSA
    `WalletRegistry` but we can use it also for `RandomBeacon`. First, for
    consistency, second to decrease its size given we may need space for one
    more function and event related to rewards.
    
    Files are copied and not moved so that we can merge into `main`, have a
    new version of `@keep-network/random-beacon` pushed to NPM registry and
    in a separate PR, import these contracts from `ecdsa` module and remove
    them from there.

---
+++ solidity/random-beacon/contracts/Governable.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+//
+// ▓▓▌ ▓▓ ▐▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄
+// ▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//   ▓▓▓▓▓▓    ▓▓▓▓▓▓▓▀    ▐▓▓▓▓▓▓    ▐▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+//   ▓▓▓▓▓▓▄▄▓▓▓▓▓▓▓▀      ▐▓▓▓▓▓▓▄▄▄▄         ▓▓▓▓▓▓▄▄▄▄         ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+//   ▓▓▓▓▓▓▓▓▓▓▓▓▓▀        ▐▓▓▓▓▓▓▓▓▓▓         ▓▓▓▓▓▓▓▓▓▓         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//   ▓▓▓▓▓▓▀▀▓▓▓▓▓▓▄       ▐▓▓▓▓▓▓▀▀▀▀         ▓▓▓▓▓▓▀▀▀▀         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▀
+//   ▓▓▓▓▓▓   ▀▓▓▓▓▓▓▄     ▐▓▓▓▓▓▓     ▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌
+// ▓▓▓▓▓▓▓▓▓▓ █▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+// ▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+//
+//                           Trust math, not hardware.
+
+pragma solidity ^0.8.9;
+
+/// @notice Governable contract.
+/// @dev A constructor is not defined, which makes the contract compatible with
+///      upgradable proxies. This requires calling explicitly `_transferGovernance`
+///      function in a child contract.
+abstract contract Governable {
+    // Governance of the contract
+    address public governance;
+
+    event GovernanceTransferred(address oldGovernance, address newGovernance);
+
+    modifier onlyGovernance() virtual {
+        require(governance == msg.sender, "Caller is not the governance");
+        _;
+    }
+
+    /// @notice Transfers governance of the contract to `newGovernance`.
+    function transferGovernance(address newGovernance)
+        external
+        virtual
+        onlyGovernance
+    {
+        require(
+            newGovernance != address(0),
+            "New governance is the zero address"
+        );
+        _transferGovernance(newGovernance);
+    }
+
+    function _transferGovernance(address newGovernance) internal virtual {
+        address oldGovernance = governance;
+        governance = newGovernance;
+        emit GovernanceTransferred(oldGovernance, newGovernance);
+    }
+}
