f651b9a27841d65c09104d894ab6ba5e4557753e
Piotr Dyraga <piotr.dyraga@keep.network>
2022-04-23 21:18:52 +0200

    Use Governable contract from random-beacon module
    To do not duplicate exactly the same code, Governable is declared next
    to Reimbursable in random-beacon module and we now import it from ecdsa.
    
    This commit has a temporary workaround in package.json to import from
    `../random-beacon` instead of the development tag. This will have to be
    changed back once the PR moving files to random-beacon gets merged and a
    new package gets published. See commit d99ab6057242c156d07784147387b973a9cb8fb8.

--- solidity/ecdsa/contracts/test/GovernableImpl.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.9;
-
-import "../Governable.sol";
-
-contract GovernableImpl is Governable {
-    function _transferGovernanceExposed(address newGovernance) external {
-        _transferGovernance(newGovernance);
-    }
-}
