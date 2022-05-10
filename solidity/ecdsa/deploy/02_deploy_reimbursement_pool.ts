a3f1a5ab8544607d52b3f5bebefc8db567c94dc9
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-05-10 15:37:27 +0200

    Define maxGasPrice without ethers

--- solidity/ecdsa/deploy/02_deploy_reimbursement_pool.ts
+++ solidity/ecdsa/deploy/02_deploy_reimbursement_pool.ts
@@ -2,17 +2,11 @@ import type { HardhatRuntimeEnvironment } from "hardhat/types"
 import type { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { getNamedAccounts, ethers, deployments } = hre
+  const { getNamedAccounts, deployments } = hre
   const { deployer } = await getNamedAccounts()
 
   const staticGas = 40_800 // gas amount consumed by the refund() + tx cost
-  // FIXME: As a workaround for a bug in hardhat-gas-reporter #86 we need to provide
-  // alternative deployment script to obtain a gas report.
-  // #86: https://github.com/cgewecke/hardhat-gas-reporter/issues/86
-  const maxGasPrice =
-    process.env.GAS_REPORTER_BUG_WORKAROUND === "true"
-      ? 500_000_000_000
-      : ethers.utils.parseUnits("500", "gwei")
+  const maxGasPrice = 500_000_000_000 // 500 Gwei
 
   const ReimbursementPool = await deployments.deploy("ReimbursementPool", {
     from: deployer,
