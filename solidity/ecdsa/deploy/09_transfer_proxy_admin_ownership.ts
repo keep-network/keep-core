faf2c31427494a9722227704e4f87a72a38aab37
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-05-16 13:31:01 +0200

    Refactor TransferProxyAdminOwnership script
    Check current owner of proxy admin so we don't execute this again if the
    owner is already as expected.

--- solidity/ecdsa/deploy/09_transfer_proxy_admin_ownership.ts
+++ solidity/ecdsa/deploy/09_transfer_proxy_admin_ownership.ts
@@ -2,19 +2,26 @@ import type { HardhatRuntimeEnvironment } from "hardhat/types"
 import type { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { helpers, getNamedAccounts, upgrades, deployments } = hre
-  const { esdm } = await getNamedAccounts()
-  const { deployer } = await helpers.signers.getNamedSigners()
-
-  deployments.log(`transferring ProxyAdmin ownership to ${esdm}`)
+  const { helpers, upgrades, deployments } = hre
+  const { esdm, deployer } = await helpers.signers.getNamedSigners()
+  const { log } = deployments
 
   // TODO: Once a DAO is established we want to switch to ProxyAdminWithDeputy and
   // use the DAO as the proxy admin owner and ESDM as the deputy. Until then we
   // use ESDM as the owner of ProxyAdmin contract.
-  const newProxyAdminOwner = esdm
+  const newProxyAdminOwner = esdm.address
 
   const proxyAdmin = await upgrades.admin.getInstance()
-  await proxyAdmin.connect(deployer).transferOwnership(newProxyAdminOwner)
+
+  const currentOwner = await proxyAdmin.owner()
+
+  // The `@openzeppelin/hardhat-upgrades` plugin deploys a single ProxyAdmin
+  // per network. We don't want to transfer the ownership if the owner is already
+  // set to the desired address.
+  if (!helpers.address.equal(currentOwner, newProxyAdminOwner)) {
+    log(`transferring ownership of ProxyAdmin to ${newProxyAdminOwner}`)
+    await proxyAdmin.connect(deployer).transferOwnership(newProxyAdminOwner)
+  }
 }
 
 export default func
