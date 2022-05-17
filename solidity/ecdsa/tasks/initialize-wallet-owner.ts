2cc3dd9c59957abb0e64574a5cb55cb8139e465d
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-05-17 16:53:45 +0200

    Expose initializeWalletOwner function
    We wan to expose `initializeWalletOwner` function for external project
    to initialize the wallet owner after deploying an application.

--- solidity/ecdsa/tasks/initialize-wallet-owner.ts
+++ solidity/ecdsa/tasks/initialize-wallet-owner.ts
@@ -1,28 +1,37 @@
 /* eslint-disable no-console */
 import { task } from "hardhat/config"
 
+import type { HardhatRuntimeEnvironment } from "hardhat/types"
+
 task("initialize-wallet-owner", "Initializes Wallet Owner for Wallet Registry")
   .addParam("walletOwnerAddress", "The Wallet Owner's address")
   .setAction(async (args, hre) => {
-    const { getNamedAccounts, ethers, deployments } = hre
-    const { governance } = await getNamedAccounts()
-
     const { walletOwnerAddress } = args
 
-    if (!ethers.utils.isAddress(walletOwnerAddress)) {
-      throw Error(`invalid address: ${walletOwnerAddress}`)
-    }
-
-    const tx = await deployments.execute(
-      "WalletRegistryGovernance",
-      { from: governance },
-      "initializeWalletOwner",
-      walletOwnerAddress
-    )
-
-    console.log(
-      `Initialized Wallet Owner address: ${walletOwnerAddress} in transaction: ${tx.transactionHash}`
-    )
+    await initializeWalletOwner(hre, walletOwnerAddress)
   })
 
-export default {}
+async function initializeWalletOwner(
+  hre: HardhatRuntimeEnvironment,
+  walletOwnerAddress: string
+): Promise<void> {
+  const { getNamedAccounts, ethers, deployments } = hre
+  const { governance } = await getNamedAccounts()
+
+  if (!ethers.utils.isAddress(walletOwnerAddress)) {
+    throw Error(`invalid address: ${walletOwnerAddress}`)
+  }
+
+  const tx = await deployments.execute(
+    "WalletRegistryGovernance",
+    { from: governance },
+    "initializeWalletOwner",
+    walletOwnerAddress
+  )
+
+  console.log(
+    `Initialized Wallet Owner address: ${walletOwnerAddress} in transaction: ${tx.transactionHash}`
+  )
+}
+
+export default initializeWalletOwner
