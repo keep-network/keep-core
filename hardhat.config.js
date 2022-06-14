38b37eccf82d387bb73e7e29a0893a7ac975f1e1
James Campbell <james.campbell@tanti.org.uk>
2022-06-14 15:05:44 +0300

    Add inital github ci for basic devops workflows

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,30 @@
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-waffle')
 require('hardhat-deploy')
+require('solidity-coverage')
+require('hardhat-gas-reporter')
 require('dotenv').config();
 
 module.exports = {
   solidity: '0.8.9',
-  namedAccounts: {
-    deployer: {
-      default: 0,
-      ropsten: process.env.CONTRACT_OWNER_ACCOUNT_PUBLIC_KEY
-    }
-  },
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_API_URL,
-      chainId: 3,
-      accounts: [process.env.CONTRACT_OWNER_ACCOUNT_PRIV_KEY]
-    }
-  },
-  external: {
-    contracts: [
-      {
-        artifacts: 'node_modules/@threshold-network/solidity-contracts/artifacts',
-      }
-    ]
-  }
+  // namedAccounts: {
+  //   deployer: {
+  //     default: 0,
+  //     ropsten: process.env.CONTRACT_OWNER_ACCOUNT_PUBLIC_KEY
+  //   }
+  // },
+  // networks: {
+  //   ropsten: {
+  //     url: process.env.ROPSTEN_API_URL,
+  //     chainId: 3,
+  //     accounts: [process.env.CONTRACT_OWNER_ACCOUNT_PRIV_KEY]
+  //   }
+  // },
+  // external: {
+  //   contracts: [
+  //     {
+  //       artifacts: 'node_modules/@threshold-network/solidity-contracts/artifacts',
+  //     }
+  //   ]
+  // }
 }
