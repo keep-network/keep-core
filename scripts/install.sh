c1ca234be22007735e95af1d36a4b39eb0ad6ddc
Dmitry <dymitr.paremski@keep.network>
2022-06-17 13:03:36 +0200

    Adding initialization of contracts
    - Added execution of the init-contracts.ts script
    - We can reuse the existing deployed contracts as an option

--- scripts/install.sh
+++ scripts/install.sh
@@ -20,7 +20,7 @@ help()
    echo -e "\nUsage: ENV_VAR(S) $0"\
            "--config-dir <path>"\
            "--network <network>"\
-           "--contracts-only"
+           "--reuse-contracts"
    echo -e "\nEnvironment variables:\n"
    echo -e "\tKEEP_ETHEREUM_PASSWORD: The password to unlock local Ethereum accounts to set up delegations."\
            "Required only for 'local' network. Default value is 'password'"
@@ -28,9 +28,9 @@ help()
    echo -e "\nCommand line arguments:\n"
    echo -e "\t--config-dir: Path to keep-core client configuration file(s)"
    echo -e "\t--network: Ethereum network for keep-core client."\
-                        "Available networks and settings are specified in the 'truffle-config.js'"
-   echo -e "\t--contracts-only: Should execute contracts part only."\
-                        "Client installation will not be executed.\n"
+                        "Available networks and settings are specified in the 'hardhat.config.ts'"
+   echo -e "\t--reuse-contracts: The already deployed contracts can be reused to speed up the development. Default is false.\n"\
+   "Client installation will not be executed.\n"
    exit 1 # Exit script after printing help
 }
 
@@ -38,22 +38,24 @@ help()
 for arg in "$@"; do
   shift
   case "$arg" in
-    "--config-dir")     set -- "$@" "-c" ;;
-    "--network")        set -- "$@" "-n" ;;
-    "--contracts-only") set -- "$@" "-m" ;;
-    "--help")           set -- "$@" "-h" ;;
-    *)                  set -- "$@" "$arg"
+    "--config-dir")      set -- "$@" "-c" ;;
+    "--network")         set -- "$@" "-n" ;;
+    "--reuse-contracts") set -- "$@" "-d" ;;
+    "--help")            set -- "$@" "-h" ;;
+    *)                   set -- "$@" "$arg"
   esac
 done
 
+
+
 # Parse short options
 OPTIND=1
-while getopts "c:n:mh" opt
+while getopts "c:n:dh" opt
 do
    case "$opt" in
       c ) config_dir_path="$OPTARG" ;;
       n ) network="$OPTARG" ;;
-      m ) contracts_only=true ;;
+      d ) reuse_contracts=true ;;
       h ) help ;;
       ? ) help ;; # Print help in case parameter is non-existent
    esac
@@ -63,7 +65,9 @@ shift $(expr $OPTIND - 1) # remove options from positional parameters
 # Overwrite default properties
 CONFIG_DIR_PATH=${config_dir_path:-$CONFIG_DIR_PATH_DEFAULT}
 NETWORK=${network:-$NETWORK_DEFAULT}
-CONTRACTS_ONLY=${contracts_only:-false}
+REUSE_CONTRACTS=${reuse_contracts:-false}
+
+echo "reuse contracts::" ${REUSE_CONTRACTS}
 
 # Run script
 printf "${LOG_START}Starting installation...${LOG_END}"
@@ -74,6 +78,7 @@ printf "Network: $NETWORK"
 cd $KEEP_BEACON_SOL_PATH
 
 printf "${LOG_START}Installing YARN dependencies...${LOG_END}"
+# rm -rf node_modules/
 yarn install
 
 if [ "$NETWORK" == "development" ]; then
@@ -82,39 +87,28 @@ if [ "$NETWORK" == "development" ]; then
         npx hardhat run scripts/unlock-eth-accounts.ts --network $NETWORK
 fi
 
-printf "${LOG_START}Deploying contracts...${LOG_END}"
-rm -rf build/
+printf "${LOG_START}Building contracts...${LOG_END}"
+# rm -rf build && rm -rf cache && rm -rf typechain
+
+yarn build
+
+if [ "$REUSE_CONTRACTS" = false ] ; then
+   printf "${LOG_START}Deploying contracts...${LOG_END}"
+   rm -rf deployments/development/
+
+   CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY \
+      npx hardhat deploy --reset --export export.json --network $NETWORK
+fi
+    
+printf "${LOG_START}Initializing contracts...${LOG_END}"
 
 CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY \
-    npx hardhat deploy --reset --export export.json --network $NETWORK
-    
-# TODO: T staking
-#       Increasing authorization on RandomBeacon
-#       Build the client
+    npx hardhat run scripts/init-contracts.ts --network $NETWORK
 
-# printf "${LOG_START}Copying contract artifacts...${LOG_END}"
-# rm -rf artifacts
-# cp -r build/contracts artifacts
-# npm link
+printf "${LOG_START}Building keep-core client...${LOG_END}"
 
-# printf "${LOG_START}Initializing contracts...${LOG_END}"
-
-# CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY \
-#     npx truffle exec scripts/delegate-tokens.js --network $NETWORK
-
-# if [ "$CONTRACTS_ONLY" = false ] ; then
-#   printf "${LOG_START}Updating keep-core client configs...${LOG_END}"
-#   for CONFIG_FILE in $CONFIG_DIR_PATH/*.toml
-#   do
-#       KEEP_CORE_CONFIG_FILE_PATH=$CONFIG_FILE \
-#       CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY=$CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY \
-#           npx truffle exec scripts/lcl-client-config.js --network $NETWORK
-#   done
-
-#   printf "${LOG_START}Building keep-core client...${LOG_END}"
-#   cd $KEEP_CORE_PATH
-#   go generate ./...
-#   go build -a -o keep-core .
-# fi
+cd $KEEP_CORE_PATH
+go generate ./...
+go build -a -o keep-core .
 
 printf "${DONE_START}Installation completed!${DONE_END}"
