d1068848c80f083d6e6dc690fd563253b502fca3
Dmitry <dymitr.paremski@keep.network>
2022-06-21 16:15:46 +0200

    Improving code around staking and authorization
    - Replaced hardcoded timeout with a 'wait()' function
    - Renamed init-contracts.ts to staking.ts and a function
      init()->setup() These changes better reflect what is done in this
      script
    - Used a named account for deployer
    - Console logging error instead of error.error

--- solidity/random-beacon/scripts/staking.ts
+++ solidity/random-beacon/scripts/staking.ts
@@ -1,18 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { ethers, helpers } from "hardhat"
+import { ethers, helpers, getNamedAccounts } from "hardhat"
 
 import type { T, TokenStaking, RandomBeacon } from "../typechain"
 
 const { to1e18 } = helpers.number
-
-const BLOCK_TIME = 5000 // in ms
 
 async function getAccounts() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.NETWORK)
   return provider.listAccounts()
 }
 
-async function init() {
+async function setup() {
   const t: T = await helpers.contracts.getContract<T>("T")
   const staking: TokenStaking =
     await helpers.contracts.getContract<TokenStaking>("TokenStaking")
@@ -20,8 +18,8 @@ async function init() {
     "RandomBeacon"
   )
 
+  const { deployer } = await getNamedAccounts()
   const accounts = await getAccounts()
-  const deployer = accounts[0]
   const stakeOwner = accounts[1]
   const stakingProvider = accounts[2]
   const beneficiary = stakeOwner
@@ -30,23 +28,17 @@ async function init() {
   const stakedAmount = to1e18(1_000_000) // 1MM T
 
   const deployerSigner = await ethers.getSigner(deployer)
-  await t.connect(deployerSigner).mint(stakeOwner, stakedAmount)
+  await (await t.connect(deployerSigner).mint(stakeOwner, stakedAmount)).wait()
   const stakeOwnerSigner = await ethers.getSigner(stakeOwner)
-  await t.connect(stakeOwnerSigner).approve(staking.address, stakedAmount)
-
-  // We need to wait until the block is mined and that transactions in the
-  // mempool were executed.
-  await new Promise((f) => setTimeout(f, BLOCK_TIME))
+  await (await t.connect(stakeOwnerSigner).approve(staking.address, stakedAmount)).wait()
 
   console.log(
     `Staking ${stakedAmount.toString()} to the staking provider ${stakingProvider} ...`
   )
 
-  await staking
+  await (await staking
     .connect(stakeOwnerSigner)
-    .stake(stakingProvider, beneficiary, authorizer, stakedAmount)
-
-  await new Promise((f) => setTimeout(f, BLOCK_TIME))
+    .stake(stakingProvider, beneficiary, authorizer, stakedAmount)).wait()
 
   console.log(
     `T balance of the staking contract: ${(
@@ -61,15 +53,13 @@ async function init() {
     `Increasing min authorization ${minimumAuthorization.toString()} for the Random Beacon ...`
   )
 
-  await staking
+  await (await staking
     .connect(authorizerSigner)
     .increaseAuthorization(
       stakingProvider,
       randomBeacon.address,
       minimumAuthorization
-    )
-
-  await new Promise((f) => setTimeout(f, BLOCK_TIME))
+    )).wait()
 
   const authorizedStaked = await staking.authorizedStake(
     stakingProvider,
@@ -81,9 +71,9 @@ async function init() {
   )
 }
 
-init()
+setup()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error.error)
+    console.error(error)
     process.exit(1)
   })
