ca98c08d7155537d37ccb27dd9e375951cc0b667
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-06-23 14:27:56 +0200

    Register operator in the sortition pool
    The code is based on keep-ecdsa register.go code, with some
    improvements.
    
    We want the operator to join the sortition pool if it's eligible and the
    operator is registered by a staking provider.

---
+++ pkg/beacon/registration.go
@@ -0,0 +1,220 @@
+package beacon
+
+import (
+	"context"
+	"errors"
+	"math/big"
+	"time"
+
+	corechain "github.com/keep-network/keep-core/pkg/chain"
+	"github.com/keep-network/keep-core/pkg/chain/sortition"
+)
+
+// retryDelay defines the delay between retries related to the registration logic
+// that do not have their own specific values (like for example `eligibilityRetryDelay`
+// for sortition pool join eligibility checks).
+const retryDelay = 1 * time.Second
+
+// operatorRegistrationRetryDelay defines the delay between checks whether the operator
+// is registered by a staking provider.
+const operatorRegistrationRetryDelay = 5 * time.Second
+
+// eligibilityRetryDelay defines the delay between checks whether the operator
+// is eligible to join the sortition pool.
+const eligibilityRetryDelay = 20 * time.Minute
+
+// registerAndMonitorStatus checks whether the operator is registered by a staking
+// provider and joins the sortition pool if the operator is eligible.
+func registerAndMonitorStatus(
+	ctx context.Context,
+	blockCounter corechain.BlockCounter,
+	chainSortitionHandle sortition.Handle,
+) {
+	go func() {
+		operatorRegisteredChan := make(chan string)
+		go waitUntilRegistered(ctx, blockCounter, chainSortitionHandle, operatorRegisteredChan)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case stakingProvider := <-operatorRegisteredChan:
+				logger.Infof(
+					"operator is registered for a staking provider [%s]",
+					stakingProvider,
+				)
+
+				isInPool, err := chainSortitionHandle.IsOperatorInPool()
+				if err != nil {
+					logger.Errorf(
+						"failed to verify if the operator is in the sortition pool: [%v]",
+						err,
+					)
+					time.Sleep(retryDelay) // TODO: #413 Replace with backoff.
+					continue
+				}
+
+				if !isInPool {
+					// if the operator is not in the sortition pool, we need to
+					// join the sortition pool
+					joinSortitionPoolWhenEligible(ctx, stakingProvider, blockCounter, chainSortitionHandle)
+				}
+
+				return
+			}
+		}
+	}()
+
+	go func() {
+		joinedPoolChan := make(chan struct{})
+		go waitUntilJoined(ctx, blockCounter, chainSortitionHandle, joinedPoolChan)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-joinedPoolChan:
+				logger.Infof("operator is in the sortition pool; starting monitoring...")
+
+				// TODO: Monitor status
+				// TODO: Rejoin sortition pool if removed
+
+				monitorSignerPoolStatus()
+			}
+		}
+	}()
+}
+
+// joinSortitionPoolWhenEligible checks current operator's eligibility to join
+// the sortition pool and if it is positive, joins the pool.
+// If the operator is not eligible, it executes the check for each new mined
+// block until the operator is finally eligible and can join the pool.
+func joinSortitionPoolWhenEligible(
+	parentCtx context.Context,
+	stakingProvider string,
+	blockCounter corechain.BlockCounter,
+	chainSortitionHandle sortition.Handle,
+) {
+	ctx, cancel := context.WithCancel(parentCtx)
+	defer cancel()
+
+	newBlockChan := blockCounter.WatchBlocks(ctx) // TODO: Check every X blocks
+
+	for {
+		select {
+		case <-newBlockChan:
+			eligibleStake, err := chainSortitionHandle.EligibleStake(stakingProvider)
+			if err != nil {
+				logger.Errorf(
+					"failed to verify if the operator [%s] is eligible to join the sortition pool: [%v]",
+					stakingProvider,
+					err,
+				)
+				time.Sleep(retryDelay) // TODO: #413 Replace with backoff.
+				continue
+			}
+
+			if eligibleStake.Cmp(big.NewInt(0)) == 0 {
+				logger.Warnf("operator is not eligible to join the sortition pool")
+				time.Sleep(eligibilityRetryDelay)
+				continue
+			}
+
+			logger.Infof("joining the sortition pool...")
+
+			if err := chainSortitionHandle.JoinSortitionPool(); err != nil {
+				logger.Errorf("failed to join the sortition pool: [%v]", err)
+				time.Sleep(retryDelay) // TODO: #413 Replace with backoff.
+				continue
+			}
+
+			return
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// waitUntilJoined blocks until the operator is registered by a staking provider.
+func waitUntilRegistered(
+	parentCtx context.Context,
+	blockCounter corechain.BlockCounter,
+	chainSortitionHandle sortition.Handle,
+	operatorRegisteredChan chan string,
+) {
+	ctx, cancel := context.WithCancel(parentCtx)
+	defer cancel()
+
+	newBlockChan := blockCounter.WatchBlocks(ctx) // TODO: Check every X blocks
+
+	for {
+		select {
+		case <-newBlockChan:
+			stakingProvider, err := chainSortitionHandle.OperatorToStakingProvider()
+			if err != nil {
+				if errors.Is(err, sortition.ErrOperatorNotRegistered) {
+					logger.Warn(
+						"operator is not registered; please make sure a staking provider registered the operator",
+					)
+					time.Sleep(operatorRegistrationRetryDelay)
+				} else {
+					logger.Errorf(
+						"failed to check if the operator is registered for a staking provider: [%v]",
+						err,
+					)
+					time.Sleep(retryDelay) // TODO: #413 Replace with backoff.
+				}
+				continue
+			}
+
+			operatorRegisteredChan <- stakingProvider
+			close(operatorRegisteredChan)
+			return
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// waitUntilJoined blocks until the operator joins the sortition pool.
+func waitUntilJoined(
+	parentCtx context.Context,
+	blockCounter corechain.BlockCounter,
+	chainSortitionHandle sortition.Handle,
+	outChan chan struct{},
+) {
+	ctx, cancel := context.WithCancel(parentCtx)
+	defer cancel()
+
+	newBlockChan := blockCounter.WatchBlocks(ctx) // TODO: Check every X blocks
+
+	for {
+		select {
+		case <-newBlockChan:
+			isInPool, err := chainSortitionHandle.IsOperatorInPool()
+			if err != nil {
+				logger.Errorf(
+					"failed to verify if the operator is in the sortition pool: [%w]",
+					err,
+				)
+				time.Sleep(retryDelay) // TODO: #413 Replace with backoff.
+				continue
+			}
+			if !isInPool {
+				logger.Debugf("operator is not yet in the sortition pool, waiting...")
+				continue
+			}
+
+			close(outChan)
+			return
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func monitorSignerPoolStatus() {
+	for {
+		// TODO: Implement
+	}
+}
