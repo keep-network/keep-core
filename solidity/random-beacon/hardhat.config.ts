06e1d60b39546b5aa5cdc3feeed1325f08456f53
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-07-06 13:17:54 +0200

    Add hardhat config for goerli network
    We use goerli as our testnet. We won't deploy to Ropsten.

--- solidity/random-beacon/hardhat.config.ts
+++ solidity/random-beacon/hardhat.config.ts
@@ -91,9 +91,9 @@ const config: HardhatUserConfig = {
       chainId: 1101,
       tags: ["allowStubs"],
     },
-    ropsten: {
+    goerli: {
       url: process.env.CHAIN_API_URL || "",
-      chainId: 3,
+      chainId: 5,
       accounts: process.env.CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY
         ? [process.env.CONTRACT_OWNER_ACCOUNT_PRIVATE_KEY]
         : undefined,
@@ -112,6 +112,7 @@ const config: HardhatUserConfig = {
     },
     governance: {
       default: 1,
+      goerli: 0,
       // mainnet: ""
     },
   },
@@ -124,16 +125,16 @@ const config: HardhatUserConfig = {
           "node_modules/@threshold-network/solidity-contracts/export/deploy",
       },
     ],
-    // deployments: {
-    //   // For hardhat environment we can fork the mainnet, so we need to point it
-    //   // to the contract artifacts.
-    //   hardhat: process.env.FORKING_URL ? ["./external/mainnet"] : [],
-    //   // For development environment we expect the local dependencies to be linked
-    //   // with `yarn link` command.
-    //   development: ["node_modules/@keep-network/keep-core/artifacts"],
-    //   ropsten: ["node_modules/@keep-network/keep-core/artifacts"],
-    //   mainnet: ["./external/mainnet"],
-    // },
+    deployments: {
+      // For hardhat environment we can fork the mainnet, so we need to point it
+      // to the contract artifacts.
+      hardhat: process.env.FORKING_URL ? ["./external/mainnet"] : [],
+      // For development environment we expect the local dependencies to be linked
+      // with `yarn link` command.
+      // development: ["node_modules/@keep-network/keep-core/artifacts"],
+      goerli: ["node_modules/@threshold-network/solidity-contracts/artifacts"],
+      // mainnet: ["./external/mainnet"],
+    },
   },
   dependencyCompiler: {
     paths: [
