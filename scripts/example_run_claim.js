871eac4f5ded51e0615a1cc1df762def371a6a47
Manuel Montenegro <manuel@nucypher.com>
2022-07-06 21:45:50 +0200

    Adapt scripts to new Merkle Distribution contract
    - 'gen_merkle_dist.js': Add a console line output to show that script
      run was succesful.
    - 'verify_proof.js': Adapt to new Merkle Distribution contract:
      addition of beneficiary field.
    - 'claim_example.js': Adapt to new Merkle Distribution contract:
      addition of beneficiary field in claim. Addition of owner and rewards
      holder accounts in contract constructor. Addition of ERC20 approvement before
      claim and general refactoring.

--- scripts/example_run_claim.js
+++ scripts/example_run_claim.js
@@ -1,43 +1,71 @@
 // Script shown as example about how to claim tokens
 
-const { ethers } = require('hardhat')
-const { BN } = require('@openzeppelin/test-helpers')
-const fs = require('fs')
+const { ethers } = require("hardhat")
+const { BN } = require("@openzeppelin/test-helpers")
+const fs = require("fs")
 
 // input JSON file location with merkle root and claims data
-const PROOF_PATH = 'scripts/examples/example_proof_generated.json'
-const DIST_PATH = 'scripts/examples/example_dist_generated.json'
+const PROOF_PATH = "scripts/examples/example_proof_generated_10.json"
+const DIST_PATH = "scripts/examples/example_dist_generated_10.json"
 
-async function main () {
-  // Deploy token and cumulative merkle contracts
-  const Token = await ethers.getContractFactory('TokenMock')
-  const token = await Token.deploy('token', 't')
-  const CumulativeMerkle = await ethers.getContractFactory('CumulativeMerkleDrop')
-  const cumulativeMerkle = await CumulativeMerkle.deploy(token.address)
+async function main() {
+  const [owner, rewardsHolder] = await ethers.getSigners()
 
-  const proofJson = JSON.parse(fs.readFileSync(PROOF_PATH, { encoding: 'utf8' }))
-  if (typeof proofJson !== 'object') throw new Error('Invalid JSON')
-  const distJson = JSON.parse(fs.readFileSync(DIST_PATH, { encoding: 'utf8' }))
-  if (typeof distJson !== 'object') throw new Error('Invalid JSON')
+  // Read Merkle distribution data
+  const proofJson = JSON.parse(
+    fs.readFileSync(PROOF_PATH, { encoding: "utf8" })
+  )
+  if (typeof proofJson !== "object") throw new Error("Invalid JSON")
+  const distJson = JSON.parse(fs.readFileSync(DIST_PATH, { encoding: "utf8" }))
+  if (typeof distJson !== "object") throw new Error("Invalid JSON")
 
   const merkleRoot = proofJson.merkleRoot
   const totalAmount = new BN(distJson.totalAmount.slice(2), 16)
   const account = Object.keys(proofJson.claims)[0]
+  const beneficiary = proofJson.claims[account].beneficiary
   const amount = proofJson.claims[account].amount
   const merkleProof = proofJson.claims[account].proof
 
+  // Deploy token and cumulative merkle contracts
+  const Token = await ethers.getContractFactory("TokenMock")
+  const token = await Token.deploy()
+
+  await token.mint(rewardsHolder.address, totalAmount.toString())
+  console.log(
+    "Mint:",
+    (await token.balanceOf(rewardsHolder.address)).toString(),
+    "tokens minted"
+  )
+
+  // Deploy cumulative merkle contract
+  const CumulativeMerkle = await ethers.getContractFactory(
+    "CumulativeMerkleDrop"
+  )
+  const cumulativeMerkle = await CumulativeMerkle.deploy(
+    token.address,
+    rewardsHolder.address,
+    owner.address
+  )
+
   // Set Merkle Root on contract
   await cumulativeMerkle.setMerkleRoot(merkleRoot)
-
   merkleRootContract = await cumulativeMerkle.merkleRoot()
-  console.log('Merkle Root in contract:', merkleRootContract)
+  console.log("Merkle Root in contract:", merkleRootContract)
 
-  await token.mint(cumulativeMerkle.address, totalAmount.toString())
-  console.log('Mint:', (await token.balanceOf(cumulativeMerkle.address)).toString(), 'tokens minted')
+  await token
+    .connect(rewardsHolder)
+    .approve(cumulativeMerkle.address, totalAmount.toNumber())
 
-  console.log('Balance of account before claim:', (await token.balanceOf(account)).toString())
-  await cumulativeMerkle.claim(account, amount, merkleRoot, merkleProof)
-  console.log('Balance of account after claim:', (await token.balanceOf(account)).toString())
+  console.log(
+    "Balance of beneficiary before claim:",
+    (await token.balanceOf(beneficiary)).toString()
+  )
+
+  await cumulativeMerkle.claim(account, beneficiary, amount, merkleRoot, merkleProof)
+  console.log(
+    "Balance of account after claim:",
+    (await token.balanceOf(account)).toString()
+  )
 }
 
 main()
