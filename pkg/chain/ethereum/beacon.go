8e9602d419682683b3085f2fab95409a50052685
Piotr Dyraga <piotr.dyraga@keep.network>
2022-07-07 11:14:27 +0200

    sortition.Chain implemented by ethereum.BeaconChain
    All functions of sortition.Chain interface are now implemented in the
    RandomBeacon Ethereum chain implementation.

--- pkg/chain/ethereum/beacon.go
+++ pkg/chain/ethereum/beacon.go
@@ -1,9 +1,13 @@
 package ethereum
 
 import (
+	"bytes"
 	"fmt"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/keep-network/keep-common/pkg/chain/ethereum"
+	"github.com/keep-network/keep-core/pkg/chain"
 	"github.com/keep-network/keep-core/pkg/chain/random-beacon/gen/contract"
 )
 
@@ -16,7 +20,8 @@ const (
 type BeaconChain struct {
 	*Chain
 
-	randomBeacon *contract.RandomBeacon
+	randomBeacon  *contract.RandomBeacon
+	sortitionPool *contract.SortitionPool
 }
 
 // newBeaconChain construct a new instance of the beacon-specific Ethereum
@@ -47,14 +52,86 @@ func newBeaconChain(
 		)
 	if err != nil {
 		return nil, fmt.Errorf(
-			"failed to attach to %s contract: [%v]",
-			RandomBeaconContractName,
+			"failed to attach to RandomBeacon contract: [%v]",
+			err,
+		)
+	}
+
+	sortitionPoolAddress, err := randomBeacon.SortitionPool()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to get sortition pool address: [%v]",
+			err,
+		)
+	}
+
+	sortitionPool, err :=
+		contract.NewSortitionPool(
+			sortitionPoolAddress,
+			baseChain.chainID,
+			baseChain.key,
+			baseChain.client,
+			baseChain.nonceManager,
+			baseChain.miningWaiter,
+			baseChain.blockCounter,
+			baseChain.transactionMutex,
+		)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to attach to SortitionPool contract: [%v]",
 			err,
 		)
 	}
 
 	return &BeaconChain{
-		Chain:        baseChain,
-		randomBeacon: randomBeacon,
+		Chain:         baseChain,
+		randomBeacon:  randomBeacon,
+		sortitionPool: sortitionPool,
 	}, nil
+}
+
+func (bc *BeaconChain) OperatorToStakingProvider() (chain.Address, bool, error) {
+	stakingProvider, err := bc.randomBeacon.OperatorToStakingProvider(bc.key.Address)
+	if err != nil {
+		return "", false, fmt.Errorf(
+			"failed to map operator %v to a staking provider: [%v]",
+			bc.key.Address,
+			err,
+		)
+	}
+
+	if bytes.Equal(
+		stakingProvider.Bytes(),
+		bytes.Repeat([]byte{0}, common.AddressLength),
+	) {
+		return "", false, nil
+	}
+
+	return chain.Address(stakingProvider.Hex()), true, nil
+}
+
+func (bc *BeaconChain) EligibleStake(stakingProvider chain.Address) (*big.Int, error) {
+	eligibleStake, err := bc.randomBeacon.EligibleStake(common.HexToAddress(stakingProvider.String()))
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to get eligible stake for staking provider %s: [%w]",
+			stakingProvider,
+			err,
+		)
+	}
+
+	return eligibleStake, nil
+}
+
+func (bc *BeaconChain) IsPoolLocked() (bool, error) {
+	return bc.sortitionPool.IsLocked()
+}
+
+func (bc *BeaconChain) IsOperatorInPool() (bool, error) {
+	return bc.randomBeacon.IsOperatorInPool(bc.key.Address)
+}
+
+func (bc *BeaconChain) JoinSortitionPool() error {
+	_, err := bc.randomBeacon.JoinSortitionPool()
+	return err
 }
