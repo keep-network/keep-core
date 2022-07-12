0872a2d442f53242df0c838dccfb88cf8d32d47b
Manuel Montenegro <manuel@nucypher.com>
2022-07-12 19:54:30 +0200

    Add function to generate merkle distribution

--- src/staking-rewards-calculation/index.js
+++ src/staking-rewards-calculation/index.js
@@ -1,6 +1,8 @@
 import "isomorphic-unfetch"
 import { createClient, gql } from "@urql/core"
 import { ethers } from "ethers"
+import { MerkleTree } from "merkletreejs"
+import keccak256 from "keccak256"
 import BigNumber from "bignumber.js"
 
 const SECONDS_IN_YEAR = 31536000
@@ -431,6 +433,64 @@ function combineMerkleInputs(baseMerkleInput, addedMerkleInput) {
     }
   })
   return combined
+}
+
+/**
+ * Generate a Merkle distribution from Merkle distribution input
+ * @param {Object} merkleInput      Merkle input generated from rewards
+ * @return {Object}                 Merkle distribution
+ */
+function genMerkleDist(merkleInput) {
+  const stakingProviders = Object.keys(merkleInput)
+  const data = Object.values(merkleInput)
+
+  const elements = stakingProviders.map(
+    (stakingProvider, i) =>
+      stakingProvider +
+      data[i].beneficiary.substr(2) +
+      BigNumber(data[i].amount).toString(16).padStart(64, "0")
+  )
+
+  const tree = new MerkleTree(elements, keccak256, {
+    hashLeaves: true,
+    sort: true,
+  })
+
+  const root = tree.getHexRoot()
+  const leaves = tree.getHexLeaves()
+  const proofs = leaves.map(tree.getHexProof, tree)
+
+  const totalAmount = data
+    .map((claim) => BigNumber(claim.amount))
+    .reduce((a, b) => a.plus(b))
+    .toFixed()
+
+  const claims = Object.entries(merkleInput).map(([stakingProvider, data]) => {
+    const leaf = MerkleTree.bufferToHex(
+      keccak256(
+        stakingProvider +
+          data.beneficiary.substr(2) +
+          BigNumber(data.amount).toString(16).padStart(64, "0")
+      )
+    )
+    return {
+      stakingProvider: stakingProvider,
+      beneficiary: data.beneficiary,
+      amount: data.amount,
+      proof: proofs[leaves.indexOf(leaf)]
+    }
+  })
+
+  const dist = {
+    totalAmount: totalAmount,
+    merkleRoot: root,
+    claims: claims.reduce((a, { stakingProvider, beneficiary, amount, proof }) => {
+      a[stakingProvider] = { beneficiary, amount, proof }
+      return a
+    }, {})
+  }
+
+  return dist
 }
 
 /**
