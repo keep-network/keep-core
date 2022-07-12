ef19e7ef883043b0e12cd1197ef4a2c289373536
Tomasz Slabon <tomasz.slabon@keep.network>
2022-07-12 11:43:23 +0200

    Brought back firewall unit tests

--- pkg/firewall/firewall_test.go
+++ pkg/firewall/firewall_test.go
@@ -1,162 +1,162 @@
 package firewall
 
-// import (
-// 	"github.com/keep-network/keep-core/pkg/operator"
-// 	"math/big"
-// 	"testing"
-// 	"time"
+import (
+	"github.com/keep-network/keep-core/pkg/operator"
+	"math/big"
+	"testing"
+	"time"
 
-// 	"github.com/keep-network/keep-common/pkg/cache"
-// 	"github.com/keep-network/keep-core/pkg/chain/local"
-// )
+	"github.com/keep-network/keep-common/pkg/cache"
+	"github.com/keep-network/keep-core/pkg/chain/local"
+)
 
-// var minimumStake = big.NewInt(1000)
-// var cachingPeriod = time.Second
+var minimumStake = big.NewInt(1000)
+var cachingPeriod = time.Second
 
-// func TestHasMinimumStake(t *testing.T) {
-// 	stakeMonitor := local.NewStakeMonitor(minimumStake)
-// 	policy := &minimumStakePolicy{
-// 		stakeMonitor:        stakeMonitor,
-// 		positiveResultCache: cache.NewTimeCache(cachingPeriod),
-// 		negativeResultCache: cache.NewTimeCache(cachingPeriod),
-// 	}
+func TestHasMinimumStake(t *testing.T) {
+	stakeMonitor := local.NewStakeMonitor(minimumStake)
+	policy := &minimumStakePolicy{
+		stakeMonitor:        stakeMonitor,
+		positiveResultCache: cache.NewTimeCache(cachingPeriod),
+		negativeResultCache: cache.NewTimeCache(cachingPeriod),
+	}
 
-// 	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	err = stakeMonitor.StakeTokens(remotePeerOperatorPublicKey)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	err = stakeMonitor.StakeTokens(remotePeerOperatorPublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != nil {
-// 		t.Fatalf("validation should pass: [%v]", err)
-// 	}
-// }
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != nil {
+		t.Fatalf("validation should pass: [%v]", err)
+	}
+}
 
-// func TestHasNoMinimumStake(t *testing.T) {
-// 	stakeMonitor := local.NewStakeMonitor(minimumStake)
-// 	policy := &minimumStakePolicy{
-// 		stakeMonitor:        stakeMonitor,
-// 		positiveResultCache: cache.NewTimeCache(cachingPeriod),
-// 		negativeResultCache: cache.NewTimeCache(cachingPeriod),
-// 	}
+func TestHasNoMinimumStake(t *testing.T) {
+	stakeMonitor := local.NewStakeMonitor(minimumStake)
+	policy := &minimumStakePolicy{
+		stakeMonitor:        stakeMonitor,
+		positiveResultCache: cache.NewTimeCache(cachingPeriod),
+		negativeResultCache: cache.NewTimeCache(cachingPeriod),
+	}
 
-// 	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != errNoMinimumStake {
-// 		t.Fatalf(
-// 			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
-// 			err,
-// 			errNoMinimumStake,
-// 		)
-// 	}
-// }
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != errNoMinimumStake {
+		t.Fatalf(
+			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
+			err,
+			errNoMinimumStake,
+		)
+	}
+}
 
-// func TestCachesHasMinimumStake(t *testing.T) {
-// 	stakeMonitor := local.NewStakeMonitor(minimumStake)
-// 	policy := &minimumStakePolicy{
-// 		stakeMonitor:        stakeMonitor,
-// 		positiveResultCache: cache.NewTimeCache(cachingPeriod),
-// 		negativeResultCache: cache.NewTimeCache(cachingPeriod),
-// 	}
+func TestCachesHasMinimumStake(t *testing.T) {
+	stakeMonitor := local.NewStakeMonitor(minimumStake)
+	policy := &minimumStakePolicy{
+		stakeMonitor:        stakeMonitor,
+		positiveResultCache: cache.NewTimeCache(cachingPeriod),
+		negativeResultCache: cache.NewTimeCache(cachingPeriod),
+	}
 
-// 	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	err = stakeMonitor.StakeTokens(remotePeerOperatorPublicKey)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	err = stakeMonitor.StakeTokens(remotePeerOperatorPublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != nil {
-// 		t.Fatalf("validation should pass: [%v]", err)
-// 	}
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != nil {
+		t.Fatalf("validation should pass: [%v]", err)
+	}
 
-// 	err = stakeMonitor.UnstakeTokens(remotePeerOperatorPublicKey)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	err = stakeMonitor.UnstakeTokens(remotePeerOperatorPublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	// still caching the old result
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != nil {
-// 		t.Fatalf("validation should pass: [%v]", err)
-// 	}
+	// still caching the old result
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != nil {
+		t.Fatalf("validation should pass: [%v]", err)
+	}
 
-// 	time.Sleep(time.Second)
+	time.Sleep(time.Second)
 
-// 	// no longer caches the previous result
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != errNoMinimumStake {
-// 		t.Fatalf(
-// 			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
-// 			err,
-// 			errNoMinimumStake,
-// 		)
-// 	}
-// }
+	// no longer caches the previous result
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != errNoMinimumStake {
+		t.Fatalf(
+			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
+			err,
+			errNoMinimumStake,
+		)
+	}
+}
 
-// func TestCachesHasNoMinimumStake(t *testing.T) {
-// 	stakeMonitor := local.NewStakeMonitor(minimumStake)
-// 	policy := &minimumStakePolicy{
-// 		stakeMonitor:        stakeMonitor,
-// 		positiveResultCache: cache.NewTimeCache(cachingPeriod),
-// 		negativeResultCache: cache.NewTimeCache(cachingPeriod),
-// 	}
+func TestCachesHasNoMinimumStake(t *testing.T) {
+	stakeMonitor := local.NewStakeMonitor(minimumStake)
+	policy := &minimumStakePolicy{
+		stakeMonitor:        stakeMonitor,
+		positiveResultCache: cache.NewTimeCache(cachingPeriod),
+		negativeResultCache: cache.NewTimeCache(cachingPeriod),
+	}
 
-// 	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	_, remotePeerOperatorPublicKey, err := operator.GenerateKeyPair(local.DefaultCurve)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != errNoMinimumStake {
-// 		t.Fatalf(
-// 			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
-// 			err,
-// 			errNoMinimumStake,
-// 		)
-// 	}
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != errNoMinimumStake {
+		t.Fatalf(
+			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
+			err,
+			errNoMinimumStake,
+		)
+	}
 
-// 	err = stakeMonitor.StakeTokens(remotePeerOperatorPublicKey)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	err = stakeMonitor.StakeTokens(remotePeerOperatorPublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	// still caching the old result
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != errNoMinimumStake {
-// 		t.Fatalf(
-// 			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
-// 			err,
-// 			errNoMinimumStake,
-// 		)
-// 	}
+	// still caching the old result
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != errNoMinimumStake {
+		t.Fatalf(
+			"unexpected validation error\nactual:   [%v]\nexpected: [%v]",
+			err,
+			errNoMinimumStake,
+		)
+	}
 
-// 	time.Sleep(time.Second)
+	time.Sleep(time.Second)
 
-// 	// no longer caches the previous result
-// 	if err := policy.Validate(
-// 		remotePeerOperatorPublicKey,
-// 	); err != nil {
-// 		t.Fatalf("validation should pass: [%v]", err)
-// 	}
-// }
+	// no longer caches the previous result
+	if err := policy.Validate(
+		remotePeerOperatorPublicKey,
+	); err != nil {
+		t.Fatalf("validation should pass: [%v]", err)
+	}
+}
