ae0a759730e78a504df39cadf600e691f684a37c
Tomasz Slabon <tomasz.slabon@keep.network>
2022-07-12 10:52:47 +0200

    Brought back caching in firewall

--- pkg/firewall/firewall.go
+++ pkg/firewall/firewall.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/keep-network/keep-common/pkg/cache"
 	"github.com/keep-network/keep-core/pkg/chain"
 	"github.com/keep-network/keep-core/pkg/net"
 	"github.com/keep-network/keep-core/pkg/operator"
@@ -43,13 +44,36 @@ func MinimumStakePolicy(stakeMonitor chain.StakeMonitor) net.Firewall {
 }
 
 type minimumStakePolicy struct {
-	stakeMonitor chain.StakeMonitor
+	stakeMonitor        chain.StakeMonitor
+	positiveResultCache *cache.TimeCache
+	negativeResultCache *cache.TimeCache
 }
 
 func (msp *minimumStakePolicy) Validate(
 	remotePeerPublicKey *operator.PublicKey,
 ) error {
-	// TODO: Should caching results for given operators be used?
+	remotePeerPublicKeyHex := remotePeerPublicKey.String()
+
+	// First, check in the in-memory time caches to minimize hits to ETH client.
+	// If the Keep client with the given chain address is in the positive result
+	// cache it means it has had a minimum stake the last HasMinimumStake was
+	// executed and caching period has not elapsed yet. Similarly, if the client
+	// is in the negative result cache it means it hasn't had a minimum stake
+	// during the last check.
+	//
+	// If the caching period elapsed, cache checks will return false and we
+	// have to ask the chain about the current status.
+	msp.positiveResultCache.Sweep()
+	msp.negativeResultCache.Sweep()
+
+	if msp.positiveResultCache.Has(remotePeerPublicKeyHex) {
+		return nil
+	}
+
+	if msp.negativeResultCache.Has(remotePeerPublicKeyHex) {
+		return errNoMinimumStake
+	}
+
 	hasMinimumStake, err := msp.stakeMonitor.HasMinimumStake(remotePeerPublicKey)
 	if err != nil {
 		return fmt.Errorf(
@@ -59,8 +83,15 @@ func (msp *minimumStakePolicy) Validate(
 	}
 
 	if !hasMinimumStake {
+		// Add this address to the negative result cache.
+		// We'll not hit HasMinimumStake again for the entire caching period.
+		msp.negativeResultCache.Add(remotePeerPublicKeyHex)
 		return errNoMinimumStake
 	}
+
+	// Add this address to the positive result cache.
+	// We'll not hit HasMinimumStake again for the entire caching period.
+	msp.positiveResultCache.Add(remotePeerPublicKeyHex)
 
 	return nil
 }
