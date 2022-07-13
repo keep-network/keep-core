8ff79720308b2e4b247cfd63dbabba0f0af1808a
Dmitry <dymitr.paremski@keep.network>
2022-07-13 15:46:01 +0200

    Renames for random beacon
    This script will handle not only the random beacon installation, but
    also ecdsa/tbtc installations. Here we just rename vars to denote that
    we work with the random beacon.

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,7 @@ DONE_END='\n\n\e[0m'    # new line + reset
 KEEP_CORE_PATH=$PWD
 
 KEEP_BEACON_SOL_PATH="$KEEP_CORE_PATH/solidity/random-beacon"
+ECDSA_SOL_PATH="$KEEP_CORE_PATH/solidity/random-beacon"
 
 # Defaults, can be overwritten by env variables/input parameters
 NETWORK_DEFAULT="development"
@@ -18,16 +19,16 @@ help()
 {
    echo -e "\nUsage: ENV_VAR(S) $0"\
            "--network <network>"\
-           "--skip-deployment"\
-           "--skip-client-build"
+           "--skip-beacon-deployment"\
+           "--skip-beacon-build"
    echo -e "\nEnvironment variables:\n"
    echo -e "\tKEEP_ETHEREUM_PASSWORD: The password to unlock local Ethereum accounts to set up delegations."\
            "Required only for 'local' network. Default value is 'password'"
    echo -e "\nCommand line arguments:\n"
-   echo -e "\t--network: Ethereum network for keep-core client."\
+   echo -e "\t--network: Ethereum network for keep-core client(s)."\
                         "Available networks and settings are specified in the 'hardhat.config.ts'"
-   echo -e "\t--skip-deployment: When set to true the old artifacts from the '/deployments' dir are used. Default is false"
-   echo -e "\t--skip-client-build: Should execute contracts part only. Client installation will not be executed\n"
+   echo -e "\t--skip-beacon-deployment: When set to true the old artifacts from the '/deployments' dir are used. Default is false"
+   echo -e "\t--skip-beacon-build: Should execute contracts part only. Random Beacon client installation will not be executed\n"
    exit 1 # Exit script after printing help
 }
 
@@ -35,11 +36,11 @@ help()
 for arg in "$@"; do
   shift
   case "$arg" in
-    "--network")           set -- "$@" "-n" ;;
-    "--skip-deployment")   set -- "$@" "-d" ;;
-    "--skip-client-build") set -- "$@" "-b" ;;
-    "--help")              set -- "$@" "-h" ;;
-    *)                     set -- "$@" "$arg"
+    "--network")                  set -- "$@" "-n" ;;
+    "--skip-beacon-deployment")   set -- "$@" "-d" ;;
+    "--skip-beacon-build")        set -- "$@" "-b" ;;
+    "--help")                     set -- "$@" "-h" ;;
+    *)                            set -- "$@" "$arg"
   esac
 done
 
@@ -49,8 +50,8 @@ while getopts "n:dbh" opt
 do
    case "$opt" in
       n ) network="$OPTARG" ;;
-      d ) skip_deployment=${OPTARG:-true} ;;
-      b ) skip_client_build=${OPTARG:-true} ;;
+      d ) skip_beacon_deployment=${OPTARG:-true} ;;
+      b ) skip_beacon_build=${OPTARG:-true} ;;
       h ) help ;;
       ? ) help ;; # Print help in case parameter is non-existent
    esac
@@ -59,8 +60,8 @@ shift $(expr $OPTIND - 1) # remove options from positional parameters
 
 # Overwrite default properties
 NETWORK=${network:-$NETWORK_DEFAULT}
-SKIP_DEPLOYMENT=${skip_deployment:-false}
-SKIP_CLIENT_BUILD=${skip_client_build:-false}
+SKIP_BEACON_DEPLOYMENT=${skip_beacon_deployment:-false}
+SKIP_BEACON_BUILD=${skip_beacon_build:-false}
 
 # Run script
 printf "${LOG_START}Starting installation...${LOG_END}"
@@ -78,22 +79,22 @@ if [ "$NETWORK" == "development" ]; then
         npx hardhat unlock-accounts --network $NETWORK
 fi
 
-printf "${LOG_START}Building contracts...${LOG_END}"
+printf "${LOG_START}Building beacon contracts...${LOG_END}"
 yarn clean
 yarn build
 
-if [ "$SKIP_DEPLOYMENT" = false ] ; then
-  printf "${LOG_START}Deploying contracts...${LOG_END}"
+if [ "$SKIP_BEACON_DEPLOYMENT" = false ] ; then
+  printf "${LOG_START}Deploying beacon contracts...${LOG_END}"
 
   npx hardhat deploy --reset --export export.json --network $NETWORK
 fi
     
-if [ "$SKIP_CLIENT_BUILD" = false ] ; then
-   printf "${LOG_START}Building keep-core client...${LOG_END}"
+if [ "$SKIP_BEACON_BUILD" = false ] ; then
+   printf "${LOG_START}Building beacon client...${LOG_END}"
 
    cd $KEEP_CORE_PATH
    go generate ./...
    go build -a -o keep-core .
 fi
 
-printf "${DONE_START}Installation completed!${DONE_END}"
+printf "${DONE_START}Beacon installation completed!${DONE_END}"
