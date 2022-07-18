6d9737140e8892f55911238ebd900700de69175c
Dmitry <dymitr.paremski@keep.network>
2022-07-18 18:07:31 +0200

    Cloning tbtc-v2 to keep-core as a temp dir
    When chosing an option to deploy tbtc it means deploying all the
    contracts beacong contracts, ecdsa, tbtc and dependencies. We have 2
    repos containing tbtc & keep-core projects. In order to deploy tbtc
    contracts from this script we need to get tbtc project here first and
    then deploy the contracts.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -e
 
 LOG_START='\n\e[1;36m' # new line + bold + color
 LOG_END='\n\e[0m' # new line + reset color
@@ -9,7 +9,7 @@ DONE_END='\n\n\e[0m'    # new line + reset
 KEEP_CORE_PATH=$PWD
 
 KEEP_BEACON_SOL_PATH="$KEEP_CORE_PATH/solidity/random-beacon"
-ECDSA_SOL_PATH="$KEEP_CORE_PATH/solidity/random-beacon"
+TBTC_PATH="$KEEP_CORE_PATH/tbtc-v2"
 
 # Defaults, can be overwritten by env variables/input parameters
 NETWORK_DEFAULT="development"
@@ -19,16 +19,21 @@ help()
 {
    echo -e "\nUsage: ENV_VAR(S) $0"\
            "--network <network>"\
+           "--tbtc-path <tbtc-path>"\
            "--skip-beacon-deployment"\
-           "--skip-beacon-build"
+           "--skip-tbtc-deployment"\
+           "--skip-client-build"
    echo -e "\nEnvironment variables:\n"
    echo -e "\tKEEP_ETHEREUM_PASSWORD: The password to unlock local Ethereum accounts to set up delegations."\
            "Required only for 'local' network. Default value is 'password'"
    echo -e "\nCommand line arguments:\n"
    echo -e "\t--network: Ethereum network for keep-core client(s)."\
                         "Available networks and settings are specified in the 'hardhat.config.ts'"
+   echo -e "\t--tbtc-path: 'Local' tbtc project's path. A temporary folder with tbtc is created and removed "\
+                           "upon installation if the path is not provided"
    echo -e "\t--skip-beacon-deployment: When set to true the old artifacts from the '/deployments' dir are used. Default is false"
-   echo -e "\t--skip-beacon-build: Should execute contracts part only. Random Beacon client installation will not be executed\n"
+   echo -e "\t--skip-tbtc-deployment: When set to true the old artifacts from the '/deployments' dir are used. Default is false"
+   echo -e "\t--skip-client-build: Should execute contracts part only. Client installation will not be executed\n"
    exit 1 # Exit script after printing help
 }
 
@@ -36,22 +41,26 @@ help()
 for arg in "$@"; do
   shift
   case "$arg" in
-    "--network")                  set -- "$@" "-n" ;;
-    "--skip-beacon-deployment")   set -- "$@" "-d" ;;
-    "--skip-beacon-build")        set -- "$@" "-b" ;;
-    "--help")                     set -- "$@" "-h" ;;
-    *)                            set -- "$@" "$arg"
+    "--network")                    set -- "$@" "-n" ;;
+    "--tbtc-path")                  set -- "$@" "-t" ;;
+    "--skip-beacon-deployment")     set -- "$@" "-d" ;;
+    "--skip-tbtc-deployment")       set -- "$@" "-e" ;;
+    "--skip-client-build")          set -- "$@" "-b" ;;
+    "--help")                       set -- "$@" "-h" ;;
+    *)                              set -- "$@" "$arg"
   esac
 done
 
 # Parse short options
 OPTIND=1
-while getopts "n:dbh" opt
+while getopts "n:t:debh" opt
 do
    case "$opt" in
       n ) network="$OPTARG" ;;
+      t ) tbtc_path="$OPTARG" ;;
       d ) skip_beacon_deployment=${OPTARG:-true} ;;
-      b ) skip_beacon_build=${OPTARG:-true} ;;
+      e ) skip_tbtc_deployment=${OPTARG:-true} ;;
+      b ) skip_client_build=${OPTARG:-true} ;;
       h ) help ;;
       ? ) help ;; # Print help in case parameter is non-existent
    esac
@@ -60,8 +69,10 @@ shift $(expr $OPTIND - 1) # remove options from positional parameters
 
 # Overwrite default properties
 NETWORK=${network:-$NETWORK_DEFAULT}
+TBTC_PATH=${tbtc_path:-""}
 SKIP_BEACON_DEPLOYMENT=${skip_beacon_deployment:-false}
-SKIP_BEACON_BUILD=${skip_beacon_build:-false}
+SKIP_TBTC_DEPLOYMENT=${skip_tbtc_deployment:-false}
+SKIP_CLIENT_BUILD=${skip_client_build:-false}
 
 # Run script
 printf "${LOG_START}Starting installation...${LOG_END}"
@@ -70,7 +81,7 @@ printf "Network: $NETWORK\n"
 
 cd $KEEP_BEACON_SOL_PATH
 
-printf "${LOG_START}Installing YARN dependencies...${LOG_END}"
+printf "${LOG_START}Installing beacon YARN dependencies...${LOG_END}"
 yarn install
 
 if [ "$NETWORK" == "development" ]; then
@@ -79,17 +90,50 @@ if [ "$NETWORK" == "development" ]; then
         npx hardhat unlock-accounts --network $NETWORK
 fi
 
-printf "${LOG_START}Building beacon contracts...${LOG_END}"
-yarn clean
-yarn build
+# Skip the beacon deployment if none of the skip-* options are passed and
+# deploy TBTC which include the beacon contracts.
+if [ "$SKIP_BEACON_DEPLOYMENT" = false ] && [ "$SKIP_TBTC_DEPLOYMENT" = false ]; then
+  SKIP_BEACON_DEPLOYMENT=true
+fi
 
 if [ "$SKIP_BEACON_DEPLOYMENT" = false ] ; then
-  printf "${LOG_START}Deploying beacon contracts...${LOG_END}"
+  printf "${LOG_START}Building beacon contracts...${LOG_END}"
+  yarn clean
+  yarn build
+
+  printf "${LOG_START}Deploying contracts for beacon...${LOG_END}"
+
+  npx hardhat deploy --reset --export export.json --network $NETWORK
+fi
+
+cd $KEEP_CORE_PATH
+
+printf "${LOG_START}Installing tbtc YARN dependencies...${LOG_END}"
+yarn install
+
+if [ "$SKIP_TBTC_DEPLOYMENT" = false ] ; then
+  if [ "$TBTC_PATH" = "" ] ; then
+    printf "${LOG_START}Cloning tbtc...${LOG_END}"
+    # if dir is present, remove a temporary cloned tbtc project for a fresh installation
+    rm -rf tmp_tbtc && mkdir tmp_tbtc && cd tmp_tbtc
+    # clone project from the repository
+    git clone https://github.com/keep-network/tbtc-v2.git
+    cd "tbtc-v2/solidity"
+  else
+    printf "${LOG_START}Installing tbtc from the local directory...${LOG_END}"
+    cd "$TBTC_PATH/solidity"
+  fi
+
+  printf "${LOG_START}Building tbtc contracts...${LOG_END}"
+  yarn install
+  yarn build
+
+  printf "${LOG_START}Deploying contracts for tbtc...${LOG_END}"
 
   npx hardhat deploy --reset --export export.json --network $NETWORK
 fi
     
-if [ "$SKIP_BEACON_BUILD" = false ] ; then
+if [ "$SKIP_CLIENT_BUILD" = false ] ; then
    printf "${LOG_START}Building beacon client...${LOG_END}"
 
    cd $KEEP_CORE_PATH
@@ -97,4 +141,4 @@ if [ "$SKIP_BEACON_BUILD" = false ] ; then
    go build -a -o keep-core .
 fi
 
-printf "${DONE_START}Beacon installation completed!${DONE_END}"
+printf "${DONE_START}Installation completed!${DONE_END}"
