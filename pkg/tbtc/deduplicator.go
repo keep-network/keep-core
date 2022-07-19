37f3db27f482ecb99ed7340ad2009fcef5c8070b
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-07-19 14:40:14 +0200

    Add DKG trigger in `pkg/tbtc` package

--- pkg/tbtc/deduplicator.go
+++ pkg/tbtc/deduplicator.go
@@ -1,6 +1,16 @@
 package tbtc
 
-import "math/big"
+import (
+	"github.com/keep-network/keep-common/pkg/cache"
+	"math/big"
+	"time"
+)
+
+const (
+	// DKGSeedCachePeriod is the time period the cache maintains
+	// the DKG seed corresponding to a DKG instance.
+	DKGSeedCachePeriod = 7 * 24 * time.Hour
+)
 
 // deduplicator decides whether the given event should be handled by the
 // client or not.
@@ -15,10 +25,13 @@ import "math/big"
 // Those events are supported:
 // - DKG started
 type deduplicator struct {
+	dkgSeedCache *cache.TimeCache
 }
 
 func newDeduplicator() *deduplicator {
-	return &deduplicator{}
+	return &deduplicator{
+		dkgSeedCache: cache.NewTimeCache(DKGSeedCachePeriod),
+	}
 }
 
 // notifyDKGStarted notifies the client wants to start the distributed key
@@ -27,6 +40,18 @@ func newDeduplicator() *deduplicator {
 func (d *deduplicator) notifyDKGStarted(
 	newDKGSeed *big.Int,
 ) bool {
-	// TODO: Implementation.
-	return true
+	d.dkgSeedCache.Sweep()
+
+	// The cache key is the hexadecimal representation of the seed.
+	cacheKey := newDKGSeed.Text(16)
+	// If the key is not in the cache, that means the seed was not handled
+	// yet and the client should proceed with the execution.
+	if !d.dkgSeedCache.Has(cacheKey) {
+		d.dkgSeedCache.Add(cacheKey)
+		return true
+	}
+
+	// Otherwise, the DKG seed is a duplicate and the client should not proceed
+	// with the execution.
+	return false
 }
--- pkg/tbtc/node.go
+++ pkg/tbtc/node.go
@@ -2,8 +2,9 @@ package tbtc
 
 import (
 	"github.com/keep-network/keep-common/pkg/persistence"
-	"github.com/keep-network/keep-core/pkg/ecdsa"
+	"github.com/keep-network/keep-core/pkg/ecdsa/dkg"
 	"github.com/keep-network/keep-core/pkg/net"
+	"github.com/keep-network/keep-core/pkg/protocol/group"
 	"math/big"
 )
 
@@ -11,6 +12,8 @@ import (
 type node struct {
 	chain       Chain
 	netProvider net.Provider
+
+	// TODO: Persistence layer.
 }
 
 func newNode(
@@ -30,6 +33,120 @@ func newNode(
 // and joinDKGIfEligible can block for an extended period of time while it
 // completes the on-chain operation.
 func (n *node) joinDKGIfEligible(seed *big.Int, startBlockNumber uint64) {
-	// TODO: Implementation.
-	ecdsa.StartDKG()
+	logger.Infof(
+		"checking eligibility for DKG with seed [0x%x]",
+		seed,
+	)
+
+	groupMembers, err := n.chain.SelectGroup(seed)
+	if err != nil {
+		logger.Errorf(
+			"failed to select group with seed [0x%x]: [%v]",
+			seed,
+			err,
+		)
+		return
+	}
+
+	chainConfig := n.chain.GetConfig()
+
+	if len(groupMembers) > chainConfig.GroupSize {
+		logger.Errorf(
+			"group size larger than supported: [%v]",
+			len(groupMembers),
+		)
+		return
+	}
+
+	signing := n.chain.Signing()
+
+	_, operatorPublicKey, err := n.chain.OperatorKeyPair()
+	if err != nil {
+		logger.Errorf("failed to get operator public key: [%v]", err)
+		return
+	}
+
+	operatorAddress, err := signing.PublicKeyToAddress(operatorPublicKey)
+	if err != nil {
+		logger.Errorf("failed to get operator address: [%v]", err)
+		return
+	}
+
+	indexes := make([]uint8, 0)
+	for index, groupMember := range groupMembers {
+		// See if we are amongst those chosen
+		if groupMember == operatorAddress {
+			indexes = append(indexes, uint8(index))
+		}
+	}
+
+	// create temporary broadcast channel name for DKG using the
+	// group selection seed
+	channelName := seed.Text(16)
+
+	if len(indexes) > 0 {
+		logger.Infof(
+			"joining DKG with seed [0x%x] and controlling [%v] group members",
+			seed,
+			len(indexes),
+		)
+
+		broadcastChannel, err := n.netProvider.BroadcastChannelFor(channelName)
+		if err != nil {
+			logger.Errorf("failed to get broadcast channel: [%v]", err)
+			return
+		}
+
+		membershipValidator := group.NewOperatorsMembershipValidator(
+			groupMembers,
+			signing,
+		)
+
+		err = broadcastChannel.SetFilter(membershipValidator.IsInGroup)
+		if err != nil {
+			logger.Errorf(
+				"could not set filter for channel [%v]: [%v]",
+				broadcastChannel.Name(),
+				err,
+			)
+		}
+
+		blockCounter, err := n.chain.BlockCounter()
+		if err != nil {
+			logger.Errorf("failed to get block counter: [%v]", err)
+			return
+		}
+
+		for _, index := range indexes {
+			// Capture the player index for the goroutine. The group member
+			// index should be in range [1, groupSize] so we need to add 1.
+			memberIndex := index + 1
+
+			go func() {
+				result, _, err := dkg.Execute(
+					startBlockNumber,
+					memberIndex,
+					chainConfig.GroupSize,
+					chainConfig.DishonestThreshold(),
+					blockCounter,
+					broadcastChannel,
+					membershipValidator,
+				)
+				if err != nil {
+					logger.Errorf("failed to execute dkg: [%v]", err)
+					return
+				}
+
+				// TODO: Use the result to create a signer and persist the
+				//       key material using the persistence layer.
+				logger.Infof(
+					"[member:%v] generated [%v] symmetric keys",
+					memberIndex,
+					len(result.SymmetricKeys),
+				)
+			}()
+		}
+	} else {
+		logger.Infof("not eligible for DKG with seed [0x%x]", seed)
+	}
 }
