ec9b1b7ed5bd55902121c55c6677405696eedde1
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-07-25 16:26:18 +0200

    Split the stake task
    We split the task into multiple tasks to be able to call them
    independently.

--- solidity/random-beacon/tasks/stake.ts
+++ solidity/random-beacon/tasks/stake.ts
@@ -3,12 +3,13 @@ import { task, types } from "hardhat/config"
 import type { BigNumber } from "ethers"
 import type { HardhatRuntimeEnvironment } from "hardhat/types"
 
-// TODO: split to 4 different tasks: mint, stake, increase authorization and
-// register an operator
-task(
-  "stake",
-  "Stakes T tokens, increases authorization and registers the operator "
-)
+const TASK_INITIALIZE = "initialize"
+const TASK_MINT = `${TASK_INITIALIZE}:mint`
+const TASK_STAKE = `${TASK_INITIALIZE}:stake`
+const TASK_AUTHORIZE = `${TASK_INITIALIZE}:authorize`
+const TASK_REGISTER = `${TASK_INITIALIZE}:register`
+
+task(TASK_INITIALIZE, "Initializes staking for an operator")
   .addParam("owner", "Stake Owner address", undefined, types.string)
   .addParam("provider", "Staking Provider", undefined, types.string)
   .addParam("operator", "Staking Operator", undefined, types.string)
@@ -21,11 +22,12 @@ task(
     undefined,
     types.int
   )
+  .addOptionalParam("force", "Force initialization", false, types.boolean)
   .setAction(async (args, hre) => {
-    await setup(hre, args)
+    await initialize(hre, args)
   })
 
-async function setup(
+async function initialize(
   hre: HardhatRuntimeEnvironment,
   args: {
     owner: string
@@ -35,87 +37,249 @@ async function setup(
     authorizer: string
     amount: BigNumber
     authorization: BigNumber
+    force: boolean
+  }
+) {
+  if (await isAlreadyStaked(hre, args.operator)) {
+    console.log(`Operator ${args.operator} is already staked`)
+    return
+  }
+
+  await hre.run(TASK_MINT, args)
+  await hre.run(TASK_STAKE, args)
+  await hre.run(TASK_AUTHORIZE, args)
+  await hre.run(TASK_REGISTER, args)
+}
+
+task(TASK_MINT, "Mints T tokens")
+  .addParam("owner", "Stake Owner address", undefined, types.string)
+  .addOptionalParam("amount", "Stake amount", 1_000_000, types.int)
+  .setAction(async (args, hre) => {
+    await mint(hre, args)
+  })
+
+async function mint(
+  hre: HardhatRuntimeEnvironment,
+  args: {
+    owner: string
+    amount: BigNumber
   }
 ) {
   const { ethers, helpers } = hre
-  const { owner, provider, operator, amount } = args
-  let { beneficiary, authorizer, authorization } = args
+  const { owner, amount } = args
 
   const { deployer } = await helpers.signers.getNamedSigners()
-  const { to1e18 } = helpers.number
+  const { to1e18, from1e18 } = helpers.number
   const t = await helpers.contracts.getContract("T")
   const staking = await helpers.contracts.getContract("TokenStaking")
-  const randomBeacon = await helpers.contracts.getContract("RandomBeacon")
 
-  const stakedAmount = to1e18(amount)
+  const stakeAmount = to1e18(amount)
 
-  await (await t.connect(deployer).mint(owner, stakedAmount)).wait()
-  const stakeOwnerSigner = await ethers.getSigner(owner)
+  // TODO: Check if he already have tokens
+  const tokenContractOwner = await t.owner()
+
+  const currentBalance: BigNumber = await t.balanceOf(owner)
+  if (currentBalance.lt(stakeAmount)) {
+    const mintAmount = stakeAmount.sub(currentBalance)
+
+    console.log(`Minting ${from1e18(mintAmount)} T for ${owner}...`)
+
+    await (
+      await t
+        .connect(await ethers.getSigner(tokenContractOwner))
+        .mint(owner, mintAmount)
+    ).wait()
+  } else {
+    console.log(`Account ${owner} already holds ${from1e18(currentBalance)} T`)
+  }
+
+  console.log(`Approving ${from1e18(stakeAmount)} T for ${staking.address}...`)
   await (
-    await t.connect(stakeOwnerSigner).approve(staking.address, stakedAmount)
+    await t
+      .connect(await ethers.getSigner(owner))
+      .approve(staking.address, stakeAmount)
   ).wait()
+}
+
+task(TASK_STAKE, "Stakes T tokens")
+  .addParam("owner", "Stake Owner address", undefined, types.string)
+  .addParam("provider", "Staking Provider", undefined, types.string)
+  .addOptionalParam("beneficiary", "Stake Beneficiary", undefined, types.string)
+  .addOptionalParam("authorizer", "Stake Authorizer", undefined, types.string)
+  .addOptionalParam("amount", "Stake amount", 1_000_000, types.int)
+  .setAction(async (args, hre) => {
+    await stake(hre, args)
+  })
+
+async function stake(
+  hre: HardhatRuntimeEnvironment,
+  args: {
+    owner: string
+    provider: string
+    beneficiary: string
+    authorizer: string
+    amount: BigNumber
+  }
+) {
+  const { ethers, helpers } = hre
+  const { owner, provider } = args
 
   // Beneficiary can equal to the owner if not set otherwise. This simplification
   // is used for development purposes.
-  if (!beneficiary) {
-    beneficiary = owner
-  }
+  const beneficiary = args.beneficiary ?? owner
 
   // Authorizer can equal to the owner if not set otherwise. This simplification
   // is used for development purposes.
-  if (!authorizer) {
-    authorizer = owner
-  }
+  const authorizer = args.authorizer ?? owner
+
+  const { to1e18, from1e18 } = helpers.number
+  const t = await helpers.contracts.getContract("T")
+  const staking = await helpers.contracts.getContract("TokenStaking")
+
+  const stakedAmount = to1e18(args.amount)
 
   console.log(
-    `Staking ${stakedAmount.toString()} to the staking provider ${provider} ...`
+    `Staking ${from1e18(stakedAmount)} T to the staking provider ${provider}...`
   )
 
   await (
     await staking
-      .connect(stakeOwnerSigner)
+      .connect(await ethers.getSigner(owner))
       .stake(provider, beneficiary, authorizer, stakedAmount)
   ).wait()
+}
 
-  console.log(
-    `T balance of the staking contract: ${(
-      await t.balanceOf(staking.address)
-    ).toString()}`
+task(TASK_AUTHORIZE, "Sets authorization")
+  .addParam("owner", "Stake Owner address", undefined, types.string)
+  .addParam("provider", "Staking Provider", undefined, types.string)
+  .addOptionalParam("beneficiary", "Stake Beneficiary", undefined, types.string)
+  .addOptionalParam(
+    "authorization",
+    "Authorization amount (default: minimumAuthorization)",
+    undefined,
+    types.int
   )
+  .setAction(async (args, hre) => {
+    await authorize(hre, args)
+  })
 
-  if (authorization) {
-    authorization = to1e18(authorization)
-  } else {
-    authorization = await randomBeacon.minimumAuthorization()
+async function authorize(
+  hre: HardhatRuntimeEnvironment,
+  args: {
+    owner: string
+    provider: string
+    authorizer: string
+    authorization: BigNumber
   }
+) {
+  const { ethers, helpers } = hre
+  const { owner, provider } = args
 
-  const authorizerSigner = await ethers.getSigner(authorizer)
+  const randomBeacon = await helpers.contracts.getContract("RandomBeacon")
 
-  console.log(
-    `Increasing authorization ${authorization.toString()} for the Random Beacon ...`
-  )
+  // Authorizer can equal to the owner if not set otherwise. This simplification
+  // is used for development purposes.
+  const authorizer = args.authorizer ?? owner
 
-  await (
-    await staking
-      .connect(authorizerSigner)
-      .increaseAuthorization(provider, randomBeacon.address, authorization)
-  ).wait()
+  const { to1e18, from1e18 } = helpers.number
+  const t = await helpers.contracts.getContract("T")
+  const staking = await helpers.contracts.getContract("TokenStaking")
 
-  const authorizedStaked = await staking.authorizedStake(
+  const authorization = args.authorization
+    ? to1e18(args.authorization)
+    : await randomBeacon.minimumAuthorization()
+
+  const currentAuthorization = await staking.authorizedStake(
     provider,
     randomBeacon.address
   )
 
-  console.log(
-    `Staked authorization ${authorizedStaked.toString()} was increased for the Random Beacon`
-  )
+  if (currentAuthorization.gte(authorization)) {
+    console.log(
+      `Authorized stake for the Random Beacon is ${from1e18(
+        currentAuthorization
+      )} T`
+    )
+    return
+  }
+
+  const increaseAmount = authorization.sub(currentAuthorization)
 
   console.log(
-    `Registering operator ${operator.toString()} for a staking provider ${provider.toString()} ...`
+    `Increasing Random Beacon's authorization by ${from1e18(
+      increaseAmount
+    )} T to ${from1e18(authorization)} T...`
   )
 
-  const stakingProviderSigner = await ethers.getSigner(provider)
   await (
-    await randomBeacon.connect(stakingProviderSigner).registerOperator(operator)
+    await staking
+      .connect(await ethers.getSigner(authorizer))
+      .increaseAuthorization(provider, randomBeacon.address, increaseAmount)
   ).wait()
+}
+
+task(TASK_REGISTER, "Registers an operator for a staking provider")
+  .addParam("provider", "Staking Provider", undefined, types.string)
+  .addParam("operator", "Staking Operator", undefined, types.string)
+  .setAction(async (args, hre) => {
+    await register(hre, args)
+  })
+
+async function register(
+  hre: HardhatRuntimeEnvironment,
+  args: {
+    provider: string
+    operator: string
+  }
+) {
+  const { ethers, helpers } = hre
+  const { provider, operator } = args
+
+  const randomBeacon = await helpers.contracts.getContract("RandomBeacon")
+
+  console.log(
+    `Registering operator ${operator.toString()} for a staking provider ${provider.toString()}...`
+  )
+
+  await (
+    await randomBeacon
+      .connect(await ethers.getSigner(provider))
+      .registerOperator(operator)
+  ).wait()
+}
+
+async function isAlreadyStaked(
+  hre: HardhatRuntimeEnvironment,
+  operator: string
+): Promise<boolean> {
+  const { ethers, helpers } = hre
+  const { from1e18 } = helpers.number
+
+  const randomBeacon = await helpers.contracts.getContract("RandomBeacon")
+  const staking = await helpers.contracts.getContract("TokenStaking")
+
+  const currentStakingProvider =
+    await randomBeacon.callStatic.operatorToStakingProvider(operator)
+  console.log(
+    `Current staking provider for operator ${operator} is ${currentStakingProvider}`
+  )
+
+  if (currentStakingProvider === ethers.constants.AddressZero) {
+    return false
+  }
+
+  const currentAuthorization = await staking.authorizedStake(
+    currentStakingProvider,
+    randomBeacon.address
+  )
+  console.log(
+    `Current authorization for ${
+      randomBeacon.address
+    } application is ${from1e18(currentAuthorization)} T`
+  )
+  if (currentAuthorization === 0) {
+    return false
+  }
+
+  return true
 }
