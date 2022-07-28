f6cb186f477fc0063c5bf43f858a11b238ae43d5
Beau Shinkle <beaushinkle@gmail.com>
2022-07-28 15:40:36 -0400

    Implement `EvaluateRetryParticipantsForKeyGeneration`

--- pkg/ecdsa/retry/retry.go
+++ pkg/ecdsa/retry/retry.go
@@ -83,3 +83,137 @@ func EvaluateRetryParticipantsForSigning(
 	}
 	return seats, nil
 }
+
+// EvaluateRetryParticipantsForKeyGeneration takes in a slice of `groupMembers`
+// and returns a subslice of those same members of length >=
+// `retryParticipantsCount` randomly according to the provided `seed` and
+// `retryCount`.
+//
+// This function is intended to be called during key generation after a failure
+// *not* due to inactivity. Assuming that some of the `groupMembers` are
+// sending corrupted information, either on purpose or accidentally, we keep
+// trying to find a subset of `groupMembers` that is as large as possible by
+// first excluding single operators, then pairs of operators, then triplets of
+// operators.
+//
+// The `seed` param needs to vary on a per-message basis but must be the same
+// seed between all operators for each invocation. This can be the hash of the
+// message since cryptographically secure randomness isn't important.
+//
+// The `retryCount` denotes the number of the given retry, so that should be
+// incremented after each attempt while the `seed` stays consistent on a
+// per-message basis.
+func EvaluateRetryParticipantsForKeyGeneration(
+	groupMembers []chain.Address,
+	seed int64,
+	retryCount uint,
+	retryParticipantsCount uint,
+) ([]chain.Address, error) {
+	originalRetryCount := retryCount
+	if int(retryParticipantsCount) > len(groupMembers) {
+		return nil, fmt.Errorf(
+			"asked for too many seats. [%d] seats were requested, "+
+				"but there are only [%d] available.",
+			retryParticipantsCount,
+			len(groupMembers),
+		)
+	}
+	operatorToSeatCount := calculateSeatCount(groupMembers)
+	rand.Seed(seed)
+
+	operators := make([]chain.Address, 0, len(operatorToSeatCount))
+	for operator := range operatorToSeatCount {
+		if len(groupMembers)-int(operatorToSeatCount[operator]) >= int(retryParticipantsCount) {
+			operators = append(operators, operator)
+		}
+	}
+	sort.Sort(byAddress(operators))
+	rand.Shuffle(len(operators), func(i, j int) {
+		operators[i], operators[j] = operators[j], operators[i]
+	})
+
+	if int(retryCount) < len(operators) {
+		removedOperator := operators[retryCount]
+		usedOperators := make([]chain.Address, 0, len(groupMembers))
+		for _, operator := range groupMembers {
+			if operator != removedOperator {
+				usedOperators = append(usedOperators, operator)
+			}
+		}
+		return usedOperators, nil
+	} else {
+		retryCount -= uint(len(operators))
+		pairIndexes := make([][2]int, 0, len(operators)*len(operators))
+		for i := 0; i < len(operators)-1; i++ {
+			for j := i + 1; j < len(operators); j++ {
+				leftOperator := operators[i]
+				rightOperator := operators[j]
+				count := len(groupMembers) -
+					int(operatorToSeatCount[leftOperator]) -
+					int(operatorToSeatCount[rightOperator])
+
+				if count >= int(retryParticipantsCount) {
+					pairIndexes = append(pairIndexes, [2]int{i, j})
+				}
+			}
+		}
+		if retryCount < uint(len(pairIndexes)) {
+			rand.Shuffle(len(pairIndexes), func(i, j int) {
+				pairIndexes[i], pairIndexes[j] = pairIndexes[j], pairIndexes[i]
+			})
+			pair := pairIndexes[retryCount]
+			leftOperator := operators[pair[0]]
+			rightOperator := operators[pair[1]]
+			usedOperators := make([]chain.Address, 0, len(groupMembers))
+			for _, operator := range groupMembers {
+				if operator != leftOperator && operator != rightOperator {
+					usedOperators = append(usedOperators, operator)
+				}
+			}
+			return usedOperators, nil
+		} else {
+			retryCount -= uint(len(pairIndexes))
+			tripletIndexes := make([][3]int, 0, len(operators)*len(operators)*len(operators))
+			for i := 0; i < len(operators)-2; i++ {
+				for j := i + 1; i < len(operators)-1; j++ {
+					for k := j + 1; j < len(operators); k++ {
+						leftOperator := operators[i]
+						middleOperator := operators[j]
+						rightOperator := operators[j]
+						count := len(groupMembers) -
+							int(operatorToSeatCount[leftOperator]) -
+							int(operatorToSeatCount[middleOperator]) -
+							int(operatorToSeatCount[rightOperator])
+
+						if count >= int(retryParticipantsCount) {
+							tripletIndexes = append(tripletIndexes, [3]int{i, j, k})
+						}
+					}
+				}
+			}
+			if retryCount < uint(len(tripletIndexes)) {
+				rand.Shuffle(len(tripletIndexes), func(i, j int) {
+					tripletIndexes[i], tripletIndexes[j] = tripletIndexes[j], tripletIndexes[i]
+				})
+				triplet := tripletIndexes[retryCount]
+				leftOperator := operators[triplet[0]]
+				middleOperator := operators[triplet[1]]
+				rightOperator := operators[triplet[2]]
+				usedOperators := make([]chain.Address, 0, len(groupMembers))
+				for _, operator := range groupMembers {
+					if operator != leftOperator && operator != middleOperator && operator != rightOperator {
+						usedOperators = append(usedOperators, operator)
+					}
+				}
+				return usedOperators, nil
+			} else {
+				return nil, fmt.Errorf(
+					"the retry count [%d] was too large to handle! "+
+						"Tried every single, pair, and triplet, but still needed [%d] more.",
+					originalRetryCount,
+					retryCount,
+				)
+			}
+		}
+	}
+}
