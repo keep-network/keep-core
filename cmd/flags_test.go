3d311aeae635f5b20fcfae2112ee6cc1a804134f
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-07-28 23:54:21 +0200

    Add unit tests

---
+++ cmd/flags_test.go
@@ -0,0 +1,309 @@
+package cmd
+
+import (
+	"math/big"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/keep-network/keep-core/config"
+	"github.com/keep-network/keep-core/pkg/chain/ethereum"
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
+
+	ethereumBeacon "github.com/keep-network/keep-core/pkg/chain/ethereum/beacon/gen"
+	ethereumEcdsa "github.com/keep-network/keep-core/pkg/chain/ethereum/ecdsa/gen"
+	ethereumThreshold "github.com/keep-network/keep-core/pkg/chain/ethereum/threshold/gen"
+)
+
+var cmdFlagsTests = map[string]struct {
+	readValueFunc         func(*config.Config) interface{}
+	flagName              string
+	flagValue             string
+	expectedValueFromFlag interface{}
+	defaultValue          interface{}
+}{
+	"ethereum.url": {
+		readValueFunc: func(c *config.Config) interface{} { return c.Ethereum.URL },
+		flagName:      "--ethereum.url",
+		flagValue:     "https://eth-provider.com/mainnet",
+		defaultValue:  "",
+	},
+	"ethereum.keyFile": {
+		readValueFunc: func(c *config.Config) interface{} { return c.Ethereum.Account.KeyFile },
+		flagName:      "--ethereum.keyFile",
+		flagValue:     "/tmp/UTC--2018-03-11T01-37-33.202765887Z--c2a56884538778bacd91aa5bf343bf882c5fb18b",
+		defaultValue:  "",
+	},
+	"ethereum.miningCheckInterval": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Ethereum.MiningCheckInterval },
+		flagName:              "--ethereum.miningCheckInterval",
+		flagValue:             "1m45s",
+		expectedValueFromFlag: 105 * time.Second,
+		defaultValue:          60 * time.Second,
+	},
+	"ethereum.requestPerSecondLimit": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Ethereum.RequestsPerSecondLimit },
+		flagName:              "--ethereum.requestPerSecondLimit",
+		flagValue:             "38",
+		expectedValueFromFlag: 38,
+		defaultValue:          150,
+	},
+	"ethereum.concurrencyLimit": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Ethereum.ConcurrencyLimit },
+		flagName:              "--ethereum.concurrencyLimit",
+		flagValue:             "105",
+		expectedValueFromFlag: 105,
+		defaultValue:          30,
+	},
+	"ethereum.maxGasFeeCap": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Ethereum.MaxGasFeeCap.Int },
+		flagName:              "--ethereum.maxGasFeeCap",
+		flagValue:             "65.8 Gwei",
+		expectedValueFromFlag: big.NewInt(65800000000),
+		defaultValue:          big.NewInt(500000000000),
+	},
+	"ethereum.balanceAlertThreshold": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Ethereum.BalanceAlertThreshold.Int },
+		flagName:              "--ethereum.balanceAlertThreshold",
+		flagValue:             "1.25 ether",
+		expectedValueFromFlag: big.NewInt(1250000000000000000),
+		defaultValue:          big.NewInt(500000000000000000),
+	},
+	"developer.randomBeaconAddress": {
+		readValueFunc: func(c *config.Config) interface{} {
+			address, _ := c.Ethereum.ContractAddress(ethereum.RandomBeaconContractName)
+			return address.String()
+		},
+		flagName:              "--developer.randomBeaconAddress",
+		flagValue:             "0x3b292d36468bc7fd481987818ef2e4d28202a0ed",
+		expectedValueFromFlag: "0x3B292D36468bC7fd481987818ef2E4d28202A0eD",
+		defaultValue:          ethereumBeacon.RandomBeaconAddress,
+	},
+	"developer.walletRegistryAddress": {
+		readValueFunc: func(c *config.Config) interface{} {
+			address, _ := c.Ethereum.ContractAddress(ethereum.WalletRegistryContractName)
+			return address.String()
+		},
+		flagName:              "--developer.walletRegistryAddress",
+		flagValue:             "0xb76707515c3f908411b5211863a7581589a1e31f",
+		expectedValueFromFlag: "0xB76707515C3f908411B5211863A7581589a1E31F",
+		defaultValue:          ethereumEcdsa.WalletRegistryAddress,
+	},
+	"developer.tokenStakingAddress": {
+		readValueFunc: func(c *config.Config) interface{} {
+			address, _ := c.Ethereum.ContractAddress(ethereum.TokenStakingContractName)
+			return address.String()
+		},
+		flagName:              "--developer.tokenStakingAddress",
+		flagValue:             "0x861b021462e7864a7413edf0113030b892978617",
+		expectedValueFromFlag: "0x861b021462e7864a7413edF0113030B892978617",
+		defaultValue:          ethereumThreshold.TokenStakingAddress,
+	},
+	"network.port": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.LibP2P.Port },
+		flagName:              "--network.port",
+		flagValue:             "78690",
+		expectedValueFromFlag: 78690,
+		defaultValue:          3919,
+	},
+	"network.peers": {
+		readValueFunc: func(c *config.Config) interface{} { return c.LibP2P.Peers },
+		flagName:      "--network.peers",
+		flagValue:     `"/ip4/127.0.0.1/tcp/5001/ipfs/3w6C4TFVo","/dns4/domain.local/tcp/3819/ipfs/16xtuKXdTd"`,
+		expectedValueFromFlag: []string{
+			"/ip4/127.0.0.1/tcp/5001/ipfs/3w6C4TFVo",
+			"/dns4/domain.local/tcp/3819/ipfs/16xtuKXdTd",
+		},
+		defaultValue: []string{},
+	},
+	"network.announcedAddresses": {
+		readValueFunc: func(c *config.Config) interface{} { return c.LibP2P.AnnouncedAddresses },
+		flagName:      "--network.announcedAddresses",
+		flagValue:     `"/dns4/boar.network/tcp/4200","/ip4/80.70.69.15/tcp/4201"`,
+		expectedValueFromFlag: []string{
+			"/dns4/boar.network/tcp/4200",
+			"/ip4/80.70.69.15/tcp/4201",
+		},
+		defaultValue: []string{},
+	},
+	"network.disseminationTime": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.LibP2P.DisseminationTime },
+		flagName:              "--network.disseminationTime",
+		flagValue:             "486",
+		expectedValueFromFlag: 486,
+		defaultValue:          0,
+	},
+	"storage.dataDir": {
+		readValueFunc: func(c *config.Config) interface{} { return c.Storage.DataDir },
+		flagName:      "--storage.dataDir",
+		flagValue:     "./flagged/location/dude",
+		defaultValue:  "",
+	},
+	"metrics.port": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Metrics.Port },
+		flagName:              "--metrics.port",
+		flagValue:             "9870",
+		expectedValueFromFlag: 9870,
+		defaultValue:          8080,
+	},
+	"metrics.networkMetricsTick": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Metrics.NetworkMetricsTick },
+		flagName:              "--metrics.networkMetricsTick",
+		flagValue:             "189",
+		expectedValueFromFlag: 189,
+		defaultValue:          60,
+	},
+	"metrics.ethereumMetricsTick": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Metrics.EthereumMetricsTick },
+		flagName:              "--metrics.ethereumMetricsTick",
+		flagValue:             "76",
+		expectedValueFromFlag: 76,
+		defaultValue:          60,
+	},
+	"diagnostics.port": {
+		readValueFunc:         func(c *config.Config) interface{} { return c.Diagnostics.Port },
+		flagName:              "--diagnostics.port",
+		flagValue:             "6089",
+		expectedValueFromFlag: 6089,
+		defaultValue:          8081,
+	},
+}
+
+func TestFlags_ReadConfigFromFlags(t *testing.T) {
+	testCommand, testConfig, _ := initTestCommand()
+
+	args := []string{}
+	for _, test := range cmdFlagsTests {
+		args = append(args, []string{test.flagName, test.flagValue}...)
+	}
+	testCommand.SetArgs(args)
+
+	testCommand.Execute()
+
+	for testName, test := range cmdFlagsTests {
+		t.Run(testName, func(t *testing.T) {
+			var expected interface{} = test.flagValue
+
+			if test.expectedValueFromFlag != nil {
+				expected = test.expectedValueFromFlag
+			}
+
+			actual := test.readValueFunc(testConfig)
+
+			if !reflect.DeepEqual(expected, actual) {
+				t.Errorf("\nexpected: %v\nactual:   %v", expected, actual)
+			}
+		})
+	}
+}
+
+func TestFlags_ReadConfigFromFlagsWithDefaults(t *testing.T) {
+	testCommand, loadedConfig, _ := initTestCommand()
+
+	args := []string{
+		cmdFlagsTests["ethereum.url"].flagName, cmdFlagsTests["ethereum.url"].flagValue,
+		cmdFlagsTests["ethereum.keyFile"].flagName, cmdFlagsTests["ethereum.keyFile"].flagValue,
+		cmdFlagsTests["storage.dataDir"].flagName, cmdFlagsTests["storage.dataDir"].flagValue,
+	}
+	testCommand.SetArgs(args)
+
+	testCommand.Execute()
+
+	for testName, test := range cmdFlagsTests {
+		t.Run(testName, func(t *testing.T) {
+			expected := test.defaultValue
+			if slices.Contains(args, test.flagName) {
+				expected = test.flagValue
+			}
+
+			actual := test.readValueFunc(loadedConfig)
+			if !reflect.DeepEqual(expected, actual) {
+				t.Errorf("\nexpected: %s\nactual:   %s", expected, actual)
+			}
+		})
+	}
+}
+
+// In this test we test a combination of properties defined in command-line and flags.
+func TestFlags_Mixed(t *testing.T) {
+	testCommand, testConfig, _ := initTestCommand()
+
+	args := []string{
+		"--config", "../test/config_flags.toml",
+		"--ethereum.url", "https://api.url.com/123eth",
+		"--ethereum.keyFile", "./keyfile-path/from/flag",
+		"--network.port", "7469",
+	}
+	testCommand.SetArgs(args)
+
+	testCommand.Execute()
+
+	tests := map[string]struct {
+		readValueFunc func(*config.Config) interface{}
+		expectedValue interface{}
+	}{
+		// Properties not defined in the config file, but set with flags.
+		"ethereum.url": {
+			readValueFunc: func(c *config.Config) interface{} { return c.Ethereum.URL },
+			expectedValue: "https://api.url.com/123eth",
+		},
+		// Properties provided in the config file and overwritten by the flags.
+		"ethereum.keyFile": {
+			readValueFunc: func(c *config.Config) interface{} { return c.Ethereum.Account.KeyFile },
+			expectedValue: "./keyfile-path/from/flag",
+		},
+		"network.port": {
+			readValueFunc: func(c *config.Config) interface{} { return c.LibP2P.Port },
+			expectedValue: 7469,
+		},
+		// Properties defined in the config file, not set with flags.
+		"metrics.port": {
+			readValueFunc: func(c *config.Config) interface{} { return c.Metrics.Port },
+			expectedValue: 3097,
+		},
+		"storage.dataDir": {
+			readValueFunc: func(c *config.Config) interface{} { return c.Storage.DataDir },
+			expectedValue: "/my/secure/location",
+		},
+		// Properties not provided in the config file nor set with flags. Use defaults.
+		"diagnostics.port": {
+			readValueFunc: func(c *config.Config) interface{} { return c.Diagnostics.Port },
+			expectedValue: 8081,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			expected := test.expectedValue
+			actual := test.readValueFunc(testConfig)
+			if !reflect.DeepEqual(expected, actual) {
+				t.Errorf("\nexpected: %s\nactual:   %s", expected, actual)
+			}
+		})
+	}
+}
+
+func initTestCommand() (*cobra.Command, *config.Config, *string) {
+	if err := os.Setenv(config.EthereumPasswordEnvVariable, "password from env var"); err != nil {
+		panic(err)
+	}
+
+	var testConfigFilePath string
+	var testConfig = &config.Config{}
+
+	testCommand := &cobra.Command{
+		Use: "Test",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			if err := testConfig.ReadConfig(testConfigFilePath, cmd.Flags()); err != nil {
+				logger.Fatalf("error reading config: %v", err)
+			}
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+
+	initFlags(testCommand, allCategories, &testConfigFilePath, testConfig)
+
+	return testCommand, testConfig, &testConfigFilePath
+}
--- config/config_test.go
+++ config/config_test.go
@@ -3,23 +3,20 @@ package config
 import (
 	"math/big"
 	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/keep-network/keep-core/pkg/chain/ethereum"
 )
 
 func TestReadConfigFromFile(t *testing.T) {
-	err := os.Setenv(ethereumPasswordEnvVariable, "not-my-password")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	filepath := "../test/config.toml"
-	cfg, err := ReadConfig(filepath)
-	if err != nil {
-		t.Fatalf(
-			"failed to read test config: [%v]",
-			err,
-		)
+	filePaths := []string{
+		"../test/config.toml",
+		"../test/config.json",
+		"../test/config.yaml",
 	}
 
 	var configReadTests = map[string]struct {
@@ -30,34 +27,192 @@ func TestReadConfigFromFile(t *testing.T) {
 			readValueFunc: func(c *Config) interface{} { return c.Ethereum.URL },
 			expectedValue: "ws://192.168.0.158:8546",
 		},
-		"Ethereum.ContractAddresses": {
+		"Ethereum.KeyFile": {
+			readValueFunc: func(c *Config) interface{} { return c.Ethereum.Account.KeyFile },
+			expectedValue: "/tmp/UTC--2018-03-11T01-37-33.202765887Z--c2a56884538778bacd91aa5bf343bf882c5fb18b",
+		},
+		"Ethereum.KeyFilePassword": {
+			readValueFunc: func(c *Config) interface{} { return c.Ethereum.Account.KeyFilePassword },
+			expectedValue: "THIS IS TEST! Password should be defined in env variable or prompt",
+		},
+		"Ethereum.MiningCheckInterval": {
+			readValueFunc: func(c *Config) interface{} { return c.Ethereum.MiningCheckInterval },
+			expectedValue: 33 * time.Second,
+		},
+		"Ethereum.RequestsPerSecondLimit": {
+			readValueFunc: func(c *Config) interface{} { return c.Ethereum.RequestsPerSecondLimit },
+			expectedValue: 13,
+		},
+		"Ethereum.ConcurrencyLimit": {
+			readValueFunc: func(c *Config) interface{} { return c.Ethereum.ConcurrencyLimit },
+			expectedValue: 56,
+		},
+		"Ethereum.MaxGasFeeCap": {
+			readValueFunc: func(c *Config) interface{} { return c.Ethereum.MaxGasFeeCap.Int },
+			expectedValue: big.NewInt(148000000000),
+		},
+		"Ethereum.BalanceAlertThreshold": {
+			readValueFunc: func(c *Config) interface{} { return c.Ethereum.BalanceAlertThreshold.Int },
+			expectedValue: big.NewInt(2300000000000000000),
+		},
+		"Ethereum.Developer - map": {
 			readValueFunc: func(c *Config) interface{} { return c.Ethereum.ContractAddresses },
 			expectedValue: map[string]string{
-				"randombeacon": "0xcf64c2a367341170cb4e09cf8c0ed137d8473ceb",
+				"randombeacon":   "0xcf64c2a367341170cb4e09cf8c0ed137d8473ceb",
+				"walletregistry": "0x143ba24e66fce8bca22f7d739f9a932c519b1c76",
+				"tokenstaking":   "0xa363a197f1bbb8877f50350234e3f15fb4175457",
 			},
+		},
+		"Developer - RandomBeacon": {
+			readValueFunc: func(c *Config) interface{} {
+				address, _ := c.Ethereum.ContractAddress(ethereum.RandomBeaconContractName)
+				return address.String()
+			},
+			expectedValue: "0xcf64C2A367341170cb4E09cf8C0ED137d8473CEB",
+		},
+		"Developer.WalletRegistryAddress": {
+			readValueFunc: func(c *Config) interface{} {
+				address, _ := c.Ethereum.ContractAddress(ethereum.WalletRegistryContractName)
+				return address.String()
+			},
+			expectedValue: "0x143Ba24e66FCe8bCA22F7D739F9a932c519B1C76",
+		},
+		"Ethereum.Developer - TokenStaking": {
+			readValueFunc: func(c *Config) interface{} {
+				address, _ := c.Ethereum.ContractAddress(ethereum.TokenStakingContractName)
+				return address.String()
+			},
+			expectedValue: "0xa363a197F1BbB8877F50350234e3f15fB4175457",
+		},
+		"Network.Port": {
+			readValueFunc: func(c *Config) interface{} { return c.LibP2P.Port },
+			expectedValue: 27001,
+		},
+		"Network.Peers": {
+			readValueFunc: func(c *Config) interface{} { return c.LibP2P.Peers },
+			expectedValue: []string{
+				"/ip4/127.0.0.1/tcp/3820/ipfs/16Uiu2HAmVZGi9bgF3w6C4TFVo9HjbCDyuecsQbzQnXQJvP5wBjkd",
+				"/ip4/127.0.0.1/tcp/3819/ipfs/16Uiu2HAmVNfJs6t7bB3hYPTxtuKXdTdqxKYn9wKKfUiGwpCDjySM",
+			},
+		},
+		"Network.AnnouncedAddresses": {
+			readValueFunc: func(c *Config) interface{} { return c.LibP2P.AnnouncedAddresses },
+			expectedValue: []string{
+				"/dns4/example.com/tcp/3919",
+				"/ip4/80.70.60.50/tcp/3919",
+			},
+		},
+		"Network.DisseminationTime": {
+			readValueFunc: func(c *Config) interface{} { return c.LibP2P.DisseminationTime },
+			expectedValue: 76,
 		},
 		"Storage.DataDir": {
 			readValueFunc: func(c *Config) interface{} { return c.Storage.DataDir },
 			expectedValue: "/my/secure/location",
 		},
-		"Ethereum.MaxGasPrice": {
-			readValueFunc: func(c *Config) interface{} { return c.Ethereum.MaxGasFeeCap.Int },
-			expectedValue: big.NewInt(140000000000),
+		"Metrics.Port": {
+			readValueFunc: func(c *Config) interface{} { return c.Metrics.Port },
+			expectedValue: 3498,
 		},
-		"Ethereum.BalanceAlertThreshold": {
-			readValueFunc: func(c *Config) interface{} { return c.Ethereum.BalanceAlertThreshold.Int },
-			expectedValue: big.NewInt(2500000000000000000),
+		"Metrics.NetworkMetricsTick": {
+			readValueFunc: func(c *Config) interface{} { return c.Metrics.NetworkMetricsTick },
+			expectedValue: 43,
+		},
+		"Metrics.EthereumMetricsTick": {
+			readValueFunc: func(c *Config) interface{} { return c.Metrics.EthereumMetricsTick },
+			expectedValue: 27,
+		},
+		"Diagnostics.Port": {
+			readValueFunc: func(c *Config) interface{} { return c.Diagnostics.Port },
+			expectedValue: 9723,
+		},
+	}
+
+	for _, filePath := range filePaths {
+		t.Run(strings.TrimPrefix(filepath.Ext(filePath), "."), func(t *testing.T) {
+			cfg := &Config{}
+			if err := cfg.ReadConfig(filePath, nil); err != nil {
+				t.Fatalf("failed to read test config: [%v]", err)
+			}
+
+			for testName, test := range configReadTests {
+				t.Run(testName, func(t *testing.T) {
+					expected := test.expectedValue
+					actual := test.readValueFunc(cfg)
+					if !reflect.DeepEqual(expected, actual) {
+						t.Errorf("\nexpected: %s\nactual:   %s", expected, actual)
+					}
+				})
+			}
+		})
+	}
+}
+
+func TestReadConfig_ReadPassword(t *testing.T) {
+	expectToPrompt := "expect-to-prompt"
+
+	var configReadTests = map[string]struct {
+		configFilePath      string
+		envVariablePassword string
+		expectedPassword    string
+	}{
+		"no password in file; password in environment variable": {
+			configFilePath:      "../test/config_no_password.toml",
+			envVariablePassword: "some-SECRET-phrase-as-ENVIRONMENT-VARIABLE",
+			expectedPassword:    "some-SECRET-phrase-as-ENVIRONMENT-VARIABLE",
+		},
+		"password in file; password in environment variable": {
+			configFilePath:      "../test/config.toml",
+			envVariablePassword: "some-SECRET-phrase-as-ENVIRONMENT-VARIABLE",
+			expectedPassword:    "THIS IS TEST! Password should be defined in env variable or prompt",
+		},
+		"password in file; no password in environment variable": {
+			configFilePath:      "../test/config.toml",
+			envVariablePassword: "",
+			expectedPassword:    "THIS IS TEST! Password should be defined in env variable or prompt",
+		},
+		"no password in file; no password in environment variable": {
+			configFilePath:      "../test/config_no_password.toml",
+			envVariablePassword: "",
+			expectedPassword:    expectToPrompt,
 		},
 	}
 
 	for testName, test := range configReadTests {
 		t.Run(testName, func(t *testing.T) {
-			expected := test.expectedValue
-			actual := test.readValueFunc(cfg)
-			if !reflect.DeepEqual(expected, actual) {
-				t.Errorf("\nexpected: %s\nactual:   %s", expected, actual)
+			if err := os.Setenv(EthereumPasswordEnvVariable, test.envVariablePassword); err != nil {
+				t.Fatal(err)
+			}
+
+			cfg := &Config{}
+			err := cfg.ReadConfig(test.configFilePath, nil)
+
+			if test.expectedPassword != expectToPrompt {
+				if err != nil {
+					t.Fatalf("failed to read test config: [%v]", err)
+				}
+
+				if cfg.Ethereum.Account.KeyFilePassword != test.expectedPassword {
+					t.Errorf(
+						"\nexpected: %s\nactual:   %s",
+						test.expectedPassword,
+						cfg.Ethereum.Account.KeyFilePassword,
+					)
+				}
+			} else {
+				// When password is not set neither in config file nor environment
+				// variable we expect to prompt user to provide the password.
+				// The prompt execution fail in unit tests so we assume that
+				// if specific error is thrown the prompt was issued.
+				expectedError := "unable to read password, error [operation not supported by device]"
+				if expectedError != err.Error() {
+					t.Errorf(
+						"unexpected error\nexpected: %v\nactual:   %v\n",
+						expectedError,
+						err,
+					)
+				}
 			}
 		})
 	}
-
 }
