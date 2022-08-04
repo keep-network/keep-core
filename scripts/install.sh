e8e88c4a1673b6b4172492f987baaa851fd9ac7b
Dmitry <dymitr.paremski@keep.network>
2022-08-04 09:53:32 +0200

    yarn -> yarn install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-LOG_START='\n\e[1;36m' # new line + bold + color
-LOG_END='\n\e[0m' # new line + reset color
+LOG_START='\n\e[1;36m'  # new line + bold + color
+LOG_END='\n\e[0m'       # new line + reset color
 DONE_START='\n\e[1;32m' # new line + bold + green
 DONE_END='\n\n\e[0m'    # new line + reset
 
@@ -16,51 +16,49 @@ TMP="$KEEP_CORE_PATH/tmp"
 NETWORK_DEFAULT="development"
 KEEP_ETHEREUM_PASSWORD=${KEEP_ETHEREUM_PASSWORD:-"password"}
 
-help()
-{
-   echo -e "\nUsage: ENV_VAR(S) $0"\
-           "--network <network>"\
-           "--tbtc-path <tbtc-path>"\
-           "--skip-deployment"\
-           "--skip-client-build"
-   echo -e "\nEnvironment variables:\n"
-   echo -e "\tKEEP_ETHEREUM_PASSWORD: The password to unlock local Ethereum accounts to set up delegations."\
-           "Required only for 'local' network. Default value is 'password'"
-   echo -e "\nCommand line arguments:\n"
-   echo -e "\t--network: Ethereum network for keep-core client(s)."\
-                        "Available networks and settings are specified in the 'hardhat.config.ts'"
-   echo -e "\t--tbtc-path: 'Local' tbtc project's path. 'tbtc' is cloned to a temporary directory"\
-                           "upon installation if the path is not provided"
-   echo -e "\t--skip-deployment: This option skips all the contracts deployment. Default is false"
-   echo -e "\t--skip-client-build: Should execute contracts part only. Client installation will not be executed\n"
-   exit 1 # Exit script after printing help
+help() {
+  echo -e "\nUsage: ENV_VAR(S) $0" \
+    "--network <network>" \
+    "--tbtc-path <tbtc-path>" \
+    "--skip-deployment" \
+    "--skip-client-build"
+  echo -e "\nEnvironment variables:\n"
+  echo -e "\tKEEP_ETHEREUM_PASSWORD: The password to unlock local Ethereum accounts to set up delegations." \
+    "Required only for 'local' network. Default value is 'password'"
+  echo -e "\nCommand line arguments:\n"
+  echo -e "\t--network: Ethereum network for keep-core client(s)." \
+    "Available networks and settings are specified in the 'hardhat.config.ts'"
+  echo -e "\t--tbtc-path: 'Local' tbtc project's path. 'tbtc' is cloned to a temporary directory" \
+    "upon installation if the path is not provided"
+  echo -e "\t--skip-deployment: This option skips all the contracts deployment. Default is false"
+  echo -e "\t--skip-client-build: Should execute contracts part only. Client installation will not be executed\n"
+  exit 1 # Exit script after printing help
 }
 
 # Transform long options to short ones
 for arg in "$@"; do
   shift
   case "$arg" in
-    "--network")           set -- "$@" "-n" ;;
-    "--tbtc-path")         set -- "$@" "-t" ;;
-    "--skip-deployment")   set -- "$@" "-e" ;;
-    "--skip-client-build") set -- "$@" "-b" ;;
-    "--help")              set -- "$@" "-h" ;;
-    *)                     set -- "$@" "$arg"
+  "--network") set -- "$@" "-n" ;;
+  "--tbtc-path") set -- "$@" "-t" ;;
+  "--skip-deployment") set -- "$@" "-e" ;;
+  "--skip-client-build") set -- "$@" "-b" ;;
+  "--help") set -- "$@" "-h" ;;
+  *) set -- "$@" "$arg" ;;
   esac
 done
 
 # Parse short options
 OPTIND=1
-while getopts "n:t:ebh" opt
-do
-   case "$opt" in
-      n ) network="$OPTARG" ;;
-      t ) tbtc_path="$OPTARG" ;;
-      e ) skip_deployment=${OPTARG:-true} ;;
-      b ) skip_client_build=${OPTARG:-true} ;;
-      h ) help ;;
-      ? ) help ;; # Print help in case parameter is non-existent
-   esac
+while getopts "n:t:ebh" opt; do
+  case "$opt" in
+  n) network="$OPTARG" ;;
+  t) tbtc_path="$OPTARG" ;;
+  e) skip_deployment=${OPTARG:-true} ;;
+  b) skip_client_build=${OPTARG:-true} ;;
+  h) help ;;
+  ?) help ;; # Print help in case parameter is non-existent
+  esac
 done
 shift $(expr $OPTIND - 1) # remove options from positional parameters
 
@@ -78,12 +76,12 @@ printf "Network: $NETWORK\n"
 cd $KEEP_BEACON_SOL_PATH
 
 printf "${LOG_START}Installing beacon YARN dependencies...${LOG_END}"
-yarn
+yarn install
 
 if [ "$NETWORK" == "development" ]; then
-    printf "${LOG_START}Unlocking ethereum accounts...${LOG_END}"
-    KEEP_ETHEREUM_PASSWORD=$KEEP_ETHEREUM_PASSWORD \
-        npx hardhat unlock-accounts --network $NETWORK
+  printf "${LOG_START}Unlocking ethereum accounts...${LOG_END}"
+  KEEP_ETHEREUM_PASSWORD=$KEEP_ETHEREUM_PASSWORD \
+    npx hardhat unlock-accounts --network $NETWORK
 fi
 
 if [ "$SKIP_DEPLOYMENT" != true ]; then
@@ -96,7 +94,7 @@ if [ "$SKIP_DEPLOYMENT" != true ]; then
   cd solidity-contracts
 
   printf "${LOG_START}Building solidity-contracts...${LOG_END}"
-  yarn && yarn clean && yarn build
+  yarn install && yarn clean && yarn build
 
   # deploy solidity-contracts
   printf "${LOG_START}Deploying solidity-contracts contracts...${LOG_END}"
@@ -132,12 +130,12 @@ if [ "$SKIP_DEPLOYMENT" != true ]; then
   yarn link @keep-network/random-beacon
 
   printf "${LOG_START}Building ecdsa...${LOG_END}"
-  yarn && yarn clean && yarn build
+  yarn install && yarn clean && yarn build
 
   # deploy ecdsa
   printf "${LOG_START}Deploying ecdsa contracts...${LOG_END}"
   yarn deploy --reset --network $NETWORK
-  
+
   printf "${LOG_START}Creating ecdsa link...${LOG_END}"
   yarn link
   # create export folder
@@ -147,14 +145,14 @@ if [ "$SKIP_DEPLOYMENT" != true ]; then
     cd $TMP
     printf "${LOG_START}Cloning tbtc...${LOG_END}"
     git clone https://github.com/keep-network/tbtc-v2.git
-    
+
     cd "tbtc-v2/solidity"
   else
     printf "${LOG_START}Installing tbtc from the local directory...${LOG_END}"
     cd "$TBTC_PATH/solidity"
   fi
 
-  yarn
+  yarn install
 
   printf "${LOG_START}Linking solidity-contracts...${LOG_END}"
   yarn link @threshold-network/solidity-contracts
@@ -174,10 +172,10 @@ if [ "$SKIP_DEPLOYMENT" != true ]; then
 fi
 
 if [ "$SKIP_CLIENT_BUILD" = false ]; then
-   printf "${LOG_START}Building client...${LOG_END}"
+  printf "${LOG_START}Building client...${LOG_END}"
 
-   cd $KEEP_CORE_PATH
-   make
+  cd $KEEP_CORE_PATH
+  make
 fi
 
 printf "${DONE_START}Installation completed!${DONE_END}"
