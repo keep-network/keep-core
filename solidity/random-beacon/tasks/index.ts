47d759dac96d4573b4c0bec9fa3f6ffab5701ac1
Dmitry <dymitr.paremski@keep.network>
2022-08-04 15:26:19 +0200

    Removing seperate tasks in favor of grouped ones in initialize.ts

--- solidity/random-beacon/tasks/index.ts
+++ solidity/random-beacon/tasks/index.ts
@@ -1,7 +1,4 @@
 // eslint-disable-next-line global-require
 require("./unlock-eth-accounts")
 require("./genesis")
-require("./mint")
-require("./stake")
-require("./increase-authorization")
-require("./register-operator")
+require("./initialize")
