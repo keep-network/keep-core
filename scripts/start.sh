50096ba69af2963ad3fbaad55bfeb269ebc11917
Dmitry <dymitr.paremski@keep.network>
2022-08-11 16:04:53 +0200

    Formatting

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,35 +8,34 @@ CONFIG_DIR_PATH_DEFAULT="$KEEP_CORE_PATH/configs"
 KEEP_ETHEREUM_PASSWORD=${KEEP_ETHEREUM_PASSWORD:-"password"}
 
 help() {
-  echo -e "\nUsage: ENV_VAR(S) $0" \
-    "--config-dir-path <path-to-configuration-files>"
-  echo -e "\nEnvironment variables:\n"
-  echo -e "\tKEEP_ETHEREUM_PASSWORD: Ethereum account password." \
-    "Required only for 'local' network. Default value is 'password'"
-  echo -e "\nCommand line arguments:\n"
-  echo -e "\t--config-dir-path: Path to a client configuration files\n"
-  exit 1 # Exit script after printing help
+    echo -e "\nUsage: ENV_VAR(S) $0" \
+        "--config-dir-path <path-to-configuration-files>"
+    echo -e "\nEnvironment variables:\n"
+    echo -e "\tKEEP_ETHEREUM_PASSWORD: Ethereum account password." \
+        "Required only for 'local' network. Default value is 'password'"
+    echo -e "\nCommand line arguments:\n"
+    echo -e "\t--config-dir-path: Path to a client configuration files\n"
+    exit 1 # Exit script after printing help
 }
 
 # Transform long options to short ones
 for arg in "$@"; do
-  shift
-  case "$arg" in
+    shift
+    case "$arg" in
     "--config-dir-path") set -- "$@" "-c" ;;
-    "--help")            set -- "$@" "-h" ;;
-    *)                   set -- "$@" "$arg"
-  esac
+    "--help") set -- "$@" "-h" ;;
+    *) set -- "$@" "$arg" ;;
+    esac
 done
 
 # Parse short options
 OPTIND=1
-while getopts "c:" opt
-do
-   case "$opt" in
-      c ) config_dir_path="$OPTARG" ;;
-      h) help ;;
-      ?) help ;; # Print help in case parameter is non-existent
-   esac
+while getopts "c:" opt; do
+    case "$opt" in
+    c) config_dir_path="$OPTARG" ;;
+    h) help ;;
+    ?) help ;; # Print help in case parameter is non-existent
+    esac
 done
 shift $(expr $OPTIND - 1) # remove options from positional parameters
 
@@ -44,8 +43,7 @@ CONFIG_DIR_PATH=${config_dir_path:-$CONFIG_DIR_PATH_DEFAULT}
 
 config_files=($CONFIG_DIR_PATH/*.toml)
 config_files_count=${#config_files[@]}
-while :
-do
+while :; do
     printf "\nSelect client config file: \n"
     i=1
     for o in "${config_files[@]}"; do
@@ -55,7 +53,7 @@ do
 
     read reply
     if [ "$reply" -ge 1 ] && [ "$reply" -le $config_files_count ]; then
-        CONFIG_FILE_PATH=${config_files["$reply"-1]}
+        CONFIG_FILE_PATH=${config_files["$reply" - 1]}
         break
     else
         printf "\nInvalid choice. Please choose an existing option number.\n"
@@ -64,33 +62,41 @@ done
 printf "\nClient config file: \"$CONFIG_FILE_PATH\" \n\n"
 
 log_level_options=("info" "debug" "custom...")
-while :
-do
+while :; do
     echo "Select log level [$LOG_LEVEL_DEFAULT]: "
     i=1
-    for o in  "${log_level_options[@]}"; do
+    for o in "${log_level_options[@]}"; do
         echo "$i) $o"
         let i++
     done
 
     read reply
     case $reply in
-        "1"|"${log_level_options[0]}") LOG_LEVEL=${log_level_options[0]}; break;;
-        "2"|"${log_level_options[1]}") LOG_LEVEL=${log_level_options[1]}; break;;
-        "3"|"${log_level_options[2]}") 
-            read -p "Enter custom log level: [$LOG_LEVEL_DEFAULT]" log_level
-            LOG_LEVEL=${log_level:-$LOG_LEVEL_DEFAULT}
-            break
-            ;;
-        "") LOG_LEVEL=$LOG_LEVEL_DEFAULT; break;;
-        *) echo "Invalid choice. Please choose an existing option number.";;
+    "1" | "${log_level_options[0]}")
+        LOG_LEVEL=${log_level_options[0]}
+        break
+        ;;
+    "2" | "${log_level_options[1]}")
+        LOG_LEVEL=${log_level_options[1]}
+        break
+        ;;
+    "3" | "${log_level_options[2]}")
+        read -p "Enter custom log level: [$LOG_LEVEL_DEFAULT]" log_level
+        LOG_LEVEL=${log_level:-$LOG_LEVEL_DEFAULT}
+        break
+        ;;
+    "")
+        LOG_LEVEL=$LOG_LEVEL_DEFAULT
+        break
+        ;;
+    *) echo "Invalid choice. Please choose an existing option number." ;;
     esac
 done
 echo "Log level: \"$LOG_LEVEL\""
 
 # Run script.
 LOG_START='\n\e[1;36m' # new line + bold + color
-LOG_END='\n\e[0m' # new line + reset color
+LOG_END='\n\e[0m'      # new line + reset color
 
 KEEP_CORE_CONFIG_FILE_PATH=$CONFIG_FILE_PATH
 
