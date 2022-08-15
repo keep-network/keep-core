78b8bf89d50b1ba523b4ecbf067a06e2e3817497
Piotr Dyraga <piotr.dyraga@keep.network>
2022-08-15 10:47:51 +0200

    Added documentation to PreParams structure

--- pkg/tecdsa/preparams.go
+++ pkg/tecdsa/preparams.go
@@ -2,10 +2,14 @@ package tecdsa
 
 import "github.com/binance-chain/tss-lib/ecdsa/keygen"
 
+// PreParams represents tECDSA DKG pre-parameters that were not yet consumed
+// by DKG protocol execution.
 type PreParams struct {
 	data keygen.LocalPreParams
 }
 
+// NewPreParams constructs a new instance of tECDSA DKG pre-parameters based on
+// the generated numbers.
 func NewPreParams(data keygen.LocalPreParams) *PreParams {
 	return &PreParams{data}
 }
