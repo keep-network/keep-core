25468fd1814e345cd8636e9b258babfc82261573
Piotr Dyraga <piotr.dyraga@keep.network>
2022-08-17 14:21:32 +0200

    Block miner computations until everyone agrees to resume
    From now on, Miner has an internal latch counting how many times `Stop()`
    function has been called. Miner expects `Resume()` function to be called the
    same number of times. The reasoning behind it is that multiple goroutines can
    call `Stop()` and we do not want the computations to unblock until the last
    goroutine is done.
    
    For example:
    1. Relay entry signing starts, computations are stopped.
    2. ECDSA DKG starts, computations are already stopped.
    3. Relay entry signing completes. Relay entry goroutine asks the computations
       to resume.
    
    Computations should not resume until ECDSA DKG goroutine asks to resume them.
    This approach requires really good hygiene with calls to miner - never forget
    to resume if you are stopping.

--- pkg/miner/miner.go
+++ pkg/miner/miner.go
@@ -26,6 +26,7 @@ const (
 // are needed elsewhere.
 type Miner struct {
 	state   state
+	latch   sync.WaitGroup
 	workers []func()
 	stops   []chan interface{}
 
@@ -49,11 +50,21 @@ func (m *Miner) Mine(miningFn func()) {
 // Stop asks all worker functions to stop their work. Note that there is no
 // guarantee the worker function will stop immediately but the miner will not
 // call the worker function again until the miner's work is resumed.
+// Stop can be called multiple times. Each call increases a count on the
+// internal latch by one. To resume the work, Resume function needs to be as
+// many times as Stop(). This way, several goroutines can stop the computations
+// and they all need to agree on resuming them.
 func (m *Miner) Stop() {
+	m.latch.Add(1)
+
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	logger.Info("stopping computations")
+	if m.state == stopped {
+		return
+	}
+
+	logger.Info("stopping computations\n")
 	m.state = stopped
 
 	for _, stop := range m.stops {
@@ -63,12 +74,21 @@ func (m *Miner) Stop() {
 }
 
 // Resume resumes the work of all worker functions, each in a separate
-// goroutine.
+// goroutine. If Stop() has been called multiple times, Resume() needs to be
+// called the same number of times. Resume() blocks until the work can be
+// resumed.
 func (m *Miner) Resume() {
+	m.latch.Done()
+	m.latch.Wait()
+
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	logger.Info("resuming computations")
+	if m.state == working {
+		return
+	}
+
+	logger.Info("resuming computations\n")
 	m.state = working
 
 	for _, worker := range m.workers {
--- pkg/miner/miner_test.go
+++ pkg/miner/miner_test.go
@@ -105,6 +105,70 @@ func TestMineStopResume(t *testing.T) {
 	)
 }
 
+func TestMineStopResumeReentrant(t *testing.T) {
+	miner := new(Miner)
+	defer miner.Stop()
+
+	number1 := big.NewInt(0)
+	number2 := big.NewInt(0)
+
+	miner.Mine(func() {
+		number1.Add(number1, one)
+	})
+	miner.Mine(func() {
+		number2.Add(number2, one)
+	})
+
+	// send the stop signal and give the miner some time to stop computations
+	miner.Stop()
+	time.Sleep(100 * time.Millisecond)
+
+	// send the stop signal one more time
+	miner.Stop()
+
+	// at this point, all computations should be stopped, capture the current
+	// result
+	intermediateResult1 := new(big.Int).Set(number1)
+	intermediateResult2 := new(big.Int).Set(number2)
+
+	// send the first resume signal and wait some time
+	go miner.Resume()
+	time.Sleep(100 * time.Millisecond)
+
+	// ensure computations have not been resumed - we had only one resume signal
+	testutils.AssertBigIntsEqual(
+		t,
+		"computation results after the first resume signal",
+		intermediateResult1,
+		number1,
+	)
+	testutils.AssertBigIntsEqual(
+		t,
+		"computation results after resume signal",
+		intermediateResult2,
+		number2,
+	)
+
+	// send the second resume signal and give the miner some time to resume
+	// computations
+	go miner.Resume()
+	time.Sleep(100 * time.Millisecond)
+
+	// ensure computations have been resumed
+	testutils.AssertBigIntsNotEqual(
+		t,
+		"computation results after the second resume signal",
+		intermediateResult1,
+		number1,
+	)
+	testutils.AssertBigIntsNotEqual(
+		t,
+		"computation results after resume signal",
+		intermediateResult2,
+		number2,
+	)
+}
+
 // TestMineStopResumeStop tests the situation when two new worker functions are
 // added to a miner in a working state. Then, the miner is stopped, resumed,
 // and stopped again. The test ensures the worker functions are stopped at the
