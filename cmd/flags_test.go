95c7f1d2f0239617482915c66faaffa566d4e08d
Piotr Dyraga <piotr.dyraga@keep.network>
2022-08-19 09:59:17 +0200

    Fixed client config flags unit test
    In the previous commit the default value for a preParamsPoolSize has
    been increased to 3000 but this was not reflected in flags unit tests.
    Doing it now.

--- cmd/flags_test.go
+++ cmd/flags_test.go
@@ -146,7 +146,7 @@ var cmdFlagsTests = map[string]struct {
 		flagName:              "--tbtc.preParamsPoolSize",
 		flagValue:             "75",
 		expectedValueFromFlag: 75,
-		defaultValue:          50,
+		defaultValue:          3000,
 	},
 	"tbtc.preParamsGenerationTimeout": {
 		readValueFunc:         func(c *config.Config) interface{} { return c.Tbtc.PreParamsGenerationTimeout },
