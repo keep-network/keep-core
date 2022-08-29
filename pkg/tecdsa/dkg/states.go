13677bb13a6bde798e896a21784b6c3b6868b6e6
Piotr Dyraga <piotr.dyraga@keep.network>
2022-08-29 14:27:42 +0200

    Increased ECDSA tssRoundTwoStateActiveBlocks to 85
    This change has been lost during the merge. Bringing it back.
    
    Based on the benchmarks, a single DLN proof verification takes about 0.35sec
    on Apple M1 Max. For 10-core dev machine we need 100*99*2*0.35/10 = 693s
    (~12 minutes) for the full verification for a group size of 100.
    
    Let's say that the absolute worst case is when a node receives 20 members on
    mainnet. Here, assuming there are two cores available and 20 members are on
    the node, we need 20*99*2*0.35/2 = 693s for the verification. It is the same
    result as for the 10-core dev box.
    
    Assuming 15 sec average block time, the verification should take about 46
    blocks but there is other logic that needs to be executed in round 2.
    
    The time needed to execute TestTssRoundTwo is 1039s (~17 minutes).
    Assuming 15 sec average block time, this takes about 70 blocks.
    
    Setting DLN proof verification to 85 blocks should then be more than enough
    for every reasonable machine to finish the computations.

--- pkg/tecdsa/dkg/states.go
+++ pkg/tecdsa/dkg/states.go
@@ -21,7 +21,7 @@ const (
 	tssRoundOneStateActiveBlocks = 5
 
 	tssRoundTwoStateDelayBlocks  = 1
-	tssRoundTwoStateActiveBlocks = 10
+	tssRoundTwoStateActiveBlocks = 85
 
 	tssRoundThreeStateDelayBlocks  = 1
 	tssRoundThreeStateActiveBlocks = 5
