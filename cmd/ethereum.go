6b35a03fdbdcf897ab449a1917ca6bf05ee71d86
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-08-30 14:40:58 +0200

    Define the root command in the cmd package
    With this change we keep all command related stuff in the cmd package.
    The main package will just initialize the command and execute it.
    
    We also added global flags to the root command.

--- cmd/ethereum.go
+++ cmd/ethereum.go
@@ -13,7 +13,21 @@ import (
 
 // EthereumCommand contains the definition of the ethereum command-line
 // subcommand and its own subcommands.
-var EthereumCommand *cobra.Command
+var EthereumCommand = &cobra.Command{
+	Use:   "ethereum",
+	Short: `Provides access to Keep network Ethereum contracts.`,
+	Long:  ethereumDescription,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if err := clientConfig.ReadConfig(configFilePath, cmd.Flags(), config.General, config.Ethereum); err != nil {
+			logger.Fatalf("error reading config: %v", err)
+		}
+
+		beaconcmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
+		ecdsacmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
+		tbtccmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
+		thresholdcmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
+	},
+}
 
 const ethereumDescription = `The ethereum command allows interacting with Keep's Ethereum
 contracts directly. Each subcommand corresponds to one contract, and has subcommands 
@@ -23,22 +37,6 @@ based on the contract method's parameters.
 See the subcommand help for additional details.`
 
 func init() {
-	EthereumCommand = &cobra.Command{
-		Use:   "ethereum",
-		Short: `Provides access to Keep network Ethereum contracts.`,
-		Long:  ethereumDescription,
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			if err := clientConfig.ReadConfig(configFilePath, cmd.Flags(), config.General, config.Ethereum); err != nil {
-				logger.Fatalf("error reading config: %v", err)
-			}
-
-			beaconcmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
-			ecdsacmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
-			tbtccmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
-			thresholdcmd.ModuleCommand.SetConfig(&clientConfig.Ethereum)
-		},
-	}
-
 	initFlags(EthereumCommand, &configFilePath, clientConfig, config.General, config.Ethereum)
 
 	EthereumCommand.AddCommand(&beaconcmd.ModuleCommand.Command)
