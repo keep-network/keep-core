fe560ccc870fed50b37c59b32f12b66651506874
Dmitry <dymitr.paremski@keep.network>
2022-09-15 17:14:27 +0200

    Saving peers.json to a file for further gcloud upload
    Peers information is written to a JSON file and stored under
    /diagnostics dir. Later this file will be uplodated to GCP by a seperate
    script.

--- scripts/diagnostics.sh
+++ scripts/diagnostics.sh
@@ -3,23 +3,22 @@ set -eou pipefail
 
 LOG_START='\n\e[1;36m'           # new line + bold + color
 LOG_END='\n\e[0m'                # new line + reset color
-LOG_WARNING_START='\n\e\033[33m' # new line + bold + warning color
-LOG_WARNING_END='\n\e\033[0m'    # new line + reset
 DONE_START='\n\e[1;32m'          # new line + bold + green
 DONE_END='\n\n\e[0m'             # new line + reset
 
-IP_ADDRESS_DEFAULT="127.0.0.1"
-PORT_DEFAULT="8081"
-PEER_PORTS_DEFAULT=("8081" "8082" "8083")
-ENDPOINT="diagnostics"
+IP_ADDRESS_DEFAULT="127.0.0.1" # bootstrap node
+PORT_DEFAULT="8081" # bootstrap port
+PEER_PORTS_DEFAULT=("9701") # a range of ports can be provided
+ENDPOINT="diagnostics" # diagnostics api endpoint
+DIAGNOSTICS_DIR="diagnostics" # tmp dir for saving peers info
 
 help() {
   echo -e "\nUsage: $0" \
-    "--ip-address <bootstrap ip address>" \
+    "--address <bootstrap address>" \
     "--port <bootstrap port>" \
     "--peer-ports <range of the eligible peer ports>" \
   echo -e "\n\nOptional command line arguments:\n"
-  echo -e "\t--ip-address: IP address of the bootstrap node\n"
+  echo -e "\t--address: Address of the bootstrap node\n"
   echo -e "\t--port: Port of the bootstrap node\n"
   echo -e "\t--peer-ports: Range of the eligible peer ports\n"
   exit 1 # Exit script after printing help
@@ -29,7 +28,7 @@ help() {
 for arg in "$@"; do
   shift
   case "$arg" in
-  "--ip-address") set -- "$@" "-i" ;;
+  "--address") set -- "$@" "-a" ;;
   "--port") set -- "$@" "-p" ;;
   "--peer-ports") set -- "$@" "-s" ;;
   "--help") set -- "$@" "-h" ;;
@@ -39,9 +38,9 @@ done
 
 # Parse short options
 OPTIND=1
-while getopts "i:p:s:h" opt; do
+while getopts "a:p:s:h" opt; do
   case "$opt" in
-  i) ip_address="$OPTARG" ;;
+  a) address="$OPTARG" ;;
   p) port="$OPTARG" ;;
   s) peer_ports="$OPTARG" ;;
   h) help ;;
@@ -51,34 +50,39 @@ done
 shift $(expr $OPTIND - 1) # remove options from positional parameters
 
 # Overwrite default properties
-IP_ADDRESS=${ip_address:-$IP_ADDRESS_DEFAULT}
+ADDRESS=${address:-$IP_ADDRESS_DEFAULT}
 PORT=${port:-$PORT_DEFAULT}
 PEER_PORTS=${peer_ports:-$PEER_PORTS_DEFAULT}
 
 # Run script
 printf "${LOG_START}Starting bootstrap diagnostics...${LOG_END}"
 
-ips=$(curl ${IP_ADDRESS}:${PORT}/${ENDPOINT} | jq -r '.connected_peers[].ip')
+# Clean up for fresh data
+rm -rf ${DIAGNOSTICS_DIR}
+mkdir ${DIAGNOSTICS_DIR}
+
+peerAddresses=$(curl ${ADDRESS}:${PORT}/${ENDPOINT} | jq -r '.connected_peers[].address')
 
 peers=()
 # Iterate over the connected peers using their IP addresses
-for ip in ${ips[@]}; do
+for peerAddress in ${peerAddresses[@]}; do
   # Iterate over the eligible peer ports. First port that returns no error breaks
   # the loop and assign data to peer[] array.
   for port in ${PEER_PORTS[@]}; do
-    if peer=$(curl ${ip}:${port}/${ENDPOINT}); then
-      peerExtractedData=$(jq -r '{version: .client_info.version, preParamPoolSize: .tbtc.preParamsPoolSize}' <<<"${peer}")
+    if peer=$(curl ${peerAddress}:${port}/${ENDPOINT}); then
+      peerExtractedData=$(jq -r '{chain_address: .client_info.chain_address, version: .client_info.version, preParamPoolSize: .tbtc.preParamsPoolSize}' <<< "${peer}")
       peers+=(${peerExtractedData})
       break
     else
-      echo "${ip}:${port}/${ENDPOINT} is not a valid endpoint"
+      echo "${peerAddress}:${port}/${ENDPOINT} is not a valid endpoint"
     fi
   done
 done
 
-peersJsonArray=$(jq -s <<<"${peers[@]}")
+peersJsonArray=$(jq -s <<< "${peers[@]}")
 peersJson=$(jq --null-input -r --argjson peersJsonArray "${peersJsonArray}" '{peers: $peersJsonArray}')
 
-echo $peersJson
+printf "${LOG_START}Saving diagnostics to a file...${LOG_END}"
+echo $peersJson > ${DIAGNOSTICS_DIR}/peers.json
 
 printf "${DONE_START}Bootstrap diagnostics completed!${DONE_END}"
