2d7efa02827ab1d5cc15a392b38a6a1b07cdefab
Dmitry <dymitr.paremski@keep.network>
2022-10-05 18:11:34 +0200

    fetching tags for the given rewards interval

--- beta-operator-rewards/rewards-requirements.ts
+++ beta-operator-rewards/rewards-requirements.ts
@@ -12,7 +12,7 @@ import {
 } from "@keep-network/ecdsa/artifacts/WalletRegistry.json";
 import axios from "axios";
 
-export async function calculateBetaOperatorFactors() {
+export async function calculateRewardsFactors() {
   program
     .version("0.0.1")
     .requiredOption(
@@ -24,33 +24,29 @@ export async function calculateBetaOperatorFactors() {
       "ending time for rewards calculation"
     )
     .requiredOption("-i, --interval <timestamp>", "scrape interval") // IMPORTANT, it has to be the same as in prometheous config
+    .requiredOption("-a, --api <prometheus api>", "prometheus API")
+    .requiredOption("-j, --job <prometheus job>", "prometheus job")
     .requiredOption(
-      "-a, --api <prometheus api>",
-      "prometheus API"
+      "-v, --versions <client version(s) in a rewards interval>",
+      "client version(s) in a rewards interval"
     )
-    .requiredOption(
-      "-j, --job <prometheus job>",
-      "prometheus job"
-    )
-    .requiredOption(
-      "-o, --output <file>",
-      "output JSON file"
-    )
+    .requiredOption("-o, --output <file>", "output JSON file")
     .parse(process.argv);
 
   const options = program.opts();
 
   const queryStep = 600; // 10min in sec
   const prometheous_job = options.job;
-  const prometheusAPI = options.api
+  const prometheusAPI = options.api;
   const startRewardsTimestamp = parseInt(options.start);
   const endRewardsTimestamp = parseInt(options.end);
   const scrapeInterval = parseInt(options.interval);
+  const clientVersions = options.versions;
   const rewardsInterval = endRewardsTimestamp - startRewardsTimestamp;
   const requiredUptime = 96; // percent
-  const preParamsAvgInterval = 24 // hours
-  const preParamsResolution = 24 // hours
-  const minPreParams = 500 // min requirement for pre params daily avg
+  const preParamsAvgInterval = 24; // hours
+  const preParamsResolution = 24; // hours
+  const minPreParams = 500; // min requirement for pre params daily avg
   const factors = {
     isBeaconAuthorized: "isBeaconAuthorized",
     isTbtcAuthorized: "isTbtcAuthorized",
@@ -58,8 +54,8 @@ export async function calculateBetaOperatorFactors() {
     version: "verion",
     preParams: "preParams",
   };
-  const upTimeRewardsCoefficient = "upTimeRewardsCoefficient"
-  const peersDataFile = options.output
+  const upTimeRewardsCoefficient = "upTimeRewardsCoefficient";
+  const peersDataFile = options.output;
   const defaultProvider = "goerli";
 
   // Query for bootstrap data that has peer instances
@@ -71,7 +67,10 @@ export async function calculateBetaOperatorFactors() {
     step: queryStep,
   };
 
-  const bootstrapData = await queryPrometheus(queryBootstrapData, paramsBootstrapData);
+  const bootstrapData = await queryPrometheus(
+    queryBootstrapData,
+    paramsBootstrapData
+  );
 
   let peersData = new Map<string, Map<string, number>>(); // peer address -> {component name: factor}
 
@@ -88,15 +87,14 @@ export async function calculateBetaOperatorFactors() {
     provider
   );
 
-  if ((Date.now() / 1000) < endRewardsTimestamp) {
+  if (Date.now() / 1000 < endRewardsTimestamp) {
     return "End time interval must be in the past";
   }
 
-  let counter = 0
-  for (let i = 0; i < await bootstrapData.data.result.length; i++) {
-    const peer = bootstrapData.data.result[i]
+  for (let i = 0; i < (await bootstrapData.data.result.length); i++) {
+    const peer = bootstrapData.data.result[i];
     let peerData = new Map<string, any>(); // Map<factor_name: value> value is in the range from 0 to 1
-    peerData.set("address", peer.metric.chain_address)
+    peerData.set("address", peer.metric.chain_address);
 
     /// Random Beacon application authorization requirement
 
@@ -146,12 +144,14 @@ export async function calculateBetaOperatorFactors() {
     const resultUptimePercent = resultUptime.data.result[0].value[1] * 100;
     const upFactor = resultUptimePercent < requiredUptime ? 0 : 1;
     peerData.set(factors.upTime, upFactor);
-    const upFactorCoefficient = upFactor ? uptimeSearchRange/rewardsInterval : 0
+    const upFactorCoefficient = upFactor
+      ? uptimeSearchRange / rewardsInterval
+      : 0;
     // Rewards should be adjusted by the upFactorCoefficient for a given peer if
     // that peer joins the network later relative to the rewards interval start.
     // Ex. if a peer joins mid month and all other factors are satisfied, then
     // the rewards are devided by half.
-    peerData.set(upTimeRewardsCoefficient, upFactorCoefficient)
+    peerData.set(upTimeRewardsCoefficient, upFactorCoefficient);
 
     /// Pre-param requirement
 
@@ -161,28 +161,33 @@ export async function calculateBetaOperatorFactors() {
       query: `avg_over_time(tbtc_pre_params_count{instance='${peer.metric.instance}', job='${prometheous_job}'}[${preParamsAvgInterval}h] offset ${offset}s)[${rewardsInterval}s:${preParamsResolution}h]`,
     };
 
-    const resultPreParams = await queryPrometheus(queryPreParams, paramsPreParams)
+    const resultPreParams = await queryPrometheus(
+      queryPreParams,
+      paramsPreParams
+    );
     peerData.set(factors.preParams, 1);
     if (resultPreParams.data.result.length == 0) {
       peerData.set(factors.preParams, 0);
     } else {
-      resultPreParams.data.result[0].values.forEach(function (peerPreParams: any) {
+      resultPreParams.data.result[0].values.forEach(function (
+        peerPreParams: any
+      ) {
         if (Number(peerPreParams[1]) < minPreParams) {
           peerData.set(factors.preParams, 0);
         }
-      })
+      });
     }
 
     /// Version requirement (One-week delay in updates to the most recent version)
 
     // TODO: implement
-    // latest tag: git describe --tags $(git rev-list --tags --max-count=1)
-    // unix timestamp: git show -s --format=%ct v1.3.1-6464-g43ea52158
+    // - parse clientVersions by "|" and extract the tag timestamp by "_"
+    // - start checking the version req from the latest to newest from the parsed
+    //   timestamps above and add 7 days (delay) to each timestamp
+    // - check against Prometheus. If they match.. good, if not.. no rewards
 
-
-    peersData.set(peer.metric.instance, peerData)
-    console.log("peersDataFactors", peersData)
-    console.log("counter: ", counter++)
+    peersData.set(peer.metric.instance, peerData);
+    console.log("peersDataFactors", peersData);
   }
 
   // TODO: calculate rewards for a given address
@@ -194,20 +199,17 @@ export async function calculateBetaOperatorFactors() {
 
   const jsonObject = await convertToJSON(peersData);
   // Save to file
-  fs.writeFileSync(
-    peersDataFile,
-    JSON.stringify(jsonObject, null, 2)
-  )
+  fs.writeFileSync(peersDataFile, JSON.stringify(jsonObject, null, 2));
 }
 
 async function convertToJSON(map: Map<string, Map<string, any>>) {
-  var json: {[k:string]: any} = {};
+  var json: { [k: string]: any } = {};
   map.forEach((value: Map<string, any>, key: string) => {
     const result = Object.fromEntries(value);
-    json[key] = result
+    json[key] = result;
   });
-  
-  return json
+
+  return json;
 }
 
 async function queryPrometheus(url: string, params: any): Promise<any> {
@@ -226,4 +228,4 @@ async function queryPrometheus(url: string, params: any): Promise<any> {
   }
 }
 
-calculateBetaOperatorFactors();
+calculateRewardsFactors();
--- beta-operator-rewards/rewards.sh
+++ beta-operator-rewards/rewards.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -eou pipefail
 
-
-LOG_START='\n\e[1;36m'           # new line + bold + color
-LOG_END='\n\e[0m'                # new line + reset color
-DONE_START='\n\e[1;32m'          # new line + bold + green
-DONE_END='\n\n\e[0m'             # new line + reset
+LOG_START='\n\e[1;36m'  # new line + bold + color
+LOG_END='\n\e[0m'       # new line + reset color
+DONE_START='\n\e[1;32m' # new line + bold + green
+DONE_END='\n\n\e[0m'    # new line + reset
 
 PROMETHEUS_API_DEFAULT="http://prometheus.monitoring.svc.cluster.local:9090/api/v1"
 PROMETHEOUS_JOB_DEFAULT="keep-discovered-nodes"
 REWARDS_END_DATE_DEFAULT=$(date +"%Y-%m-%d")
 OUTPUT_JSON_FILE="peersData.json"
 BUCKET_NAME_DEFAULT="diagnostics_test"
+CLIENT_UPGRADE_DELAY_ACCEPTANCE=604800 # 7days in sec
 
 help() {
   echo -e "\nUsage: $0" \
@@ -75,6 +75,37 @@ rewardsEndDate=$(date -j -f "%Y-%m-%d" ${REWARDS_END_DATE} "+%s")
 printf "${LOG_START}Installing yarn dependencies...${LOG_END}"
 yarn install
 
+# latestTags=$(git tag --sort=version:refname --list 'v[0-9]*') # sorted oldest -> latest
+latestTags=$(git tag --sort=-version:refname --list 'v[0-9]*') # sorted latest -> oldest
+# latestTags=$(git tag --sort=version:refname --list 'v[0-9]*.*-m[0-9]*') # sorted oldest -> latest
+tagsInRewardInterval=()
+for tag in $latestTags; do
+  timestamp=$(git show -s --format=%ct $tag^{commit})
+  # Getting the client tags that were released 7 days prior to the rewards interval
+  # start time and 7 days prior to the interval end. If a new client is released
+  # ex. 3 days before the end of the interval and a peer didn't upgrade the client
+  # it is not taken into account for the current rewards interval. It will be
+  # measured only in the next reward interval. In short, the rewards interval is
+  # offset by 7 days.
+  rewardsStartOffset=$(($rewardsStartDate - ${CLIENT_UPGRADE_DELAY_ACCEPTANCE}))
+  rewardsEndOffset=$(($rewardsEndDate - ${CLIENT_UPGRADE_DELAY_ACCEPTANCE}))
+  if [ "$rewardsStartOffset" -lt "$timestamp" ] && [ "$timestamp" -le "$rewardsEndOffset" ]; then
+    tagTimestamp="${tag}_$(git show -s --format=%ct $tag^{commit})"
+    tagsInRewardInterval+=($tagTimestamp)
+  elif [ "$timestamp" -le "$rewardsStartOffset" ]; then
+    # current tag (in case no new tags in a given rewards interval)
+    # or one before the latest to check what version was before the newest tag
+    tagTimestamp="${tag}_$(git show -s --format=%ct $tag^{commit})"
+    tagsInRewardInterval+=($tagTimestamp)
+    break
+  fi
+done
+
+# Converting array to string so we can pass to the rewards-requirements.ts
+printf -v tags '|%s' "${tagsInRewardInterval[@]}"
+
+echo $tags
+
 # Run script
 printf "${LOG_START}Fetching peers data...${LOG_END}"
 
@@ -84,6 +115,7 @@ yarn rewards-requirements \
   --start $rewardsStartDate \
   --end $rewardsEndDate \
   --interval 5 \
+  --versions $tags \
   --output ${OUTPUT_JSON_FILE}
 
 # TODO: do we want to upload the output file to a GCP bucket?
