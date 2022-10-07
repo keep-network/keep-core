615fd09af3ef69c52bd20ff3ab19f1fab46f0238
Beau Shinkle <beaushinkle@gmail.com>
2022-10-07 16:02:01 -0400

    Move constants related to rewards to their own file

---
+++ beta-operator-rewards/rewards-constants.ts
@@ -0,0 +1,10 @@
+export const queryStep = 600; // 10min in sec
+export const allowedUpgradeDelay = 1209600; // 2 weeks in sec. TODO: add as an option.param
+export const requiredUptime = 96; // percent
+export const preParamsAvgInterval = "24h"; // hours
+export const preParamsResolution = "24h"; // hours
+export const minPreParams = 500; // min requirement for pre params daily avg
+export const upTimeRewardsCoefficient = "upTimeRewardsCoefficient";
+export const defaultProvider = "goerli";
+export const clientVersionIndex = 0;
+export const clientTimestampIndex = 1;
