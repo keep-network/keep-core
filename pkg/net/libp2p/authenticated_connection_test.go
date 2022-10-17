1267bd5b9dacfd702e9e99af6c03389d94eba968
Tomasz Slabon <tomasz.slabon@keep.network>
2022-10-17 12:53:30 +0200

    Lint fix: removed Fatal assertion from goroutine

--- pkg/net/libp2p/authenticated_connection_test.go
+++ pkg/net/libp2p/authenticated_connection_test.go
@@ -153,17 +153,17 @@ func TestHandshake(t *testing.T) {
 	msg := []byte("brown fox blue tail")
 	go func(authnOutboundConn *authenticatedConnection, msg []byte) {
 		if _, err := authnOutboundConn.Write(msg); err != nil {
-			t.Fatal(err)
+			t.Error(err)
 		}
 	}(authnOutboundConn, msg)
 
 	msgContainer := make([]byte, len(msg))
 	if _, err := io.ReadFull(authnInboundConn.Conn, msgContainer); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	if string(msgContainer) != string(msg) {
-		t.Fatalf("message mismatch got %v, want %v", string(msgContainer), string(msg))
+		t.Errorf("message mismatch got %v, want %v", string(msgContainer), string(msg))
 	}
 }
 
