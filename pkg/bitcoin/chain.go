f4d4f4dbd2f1f584aa8a37f88c1178cf2803150d
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-10-26 13:29:16 +0200

    Define `bitcoin.Chain` interface
    Here we declare a minimal interface required to interact with the Bitcoin
    chain.

---
+++ pkg/bitcoin/chain.go
@@ -0,0 +1,31 @@
+package bitcoin
+
+// Chain defines an interface meant to be used for interaction with the
+// Bitcoin chain.
+type Chain interface {
+	// GetTransaction gets the transaction with the given transaction hash.
+	// If the transaction with the given hash was not found on the chain,
+	// this function returns an error.
+	GetTransaction(transactionHash Hash) (*Transaction, error)
+
+	// GetTransactionConfirmations gets the number of confirmations for the
+	// transaction with the given transaction hash. If the transaction with the
+	// given hash was not found on the chain, this function returns an error.
+	GetTransactionConfirmations(transactionHash Hash) (uint, error)
+
+	// BroadcastTransaction broadcasts the given transaction over the
+	// network of the Bitcoin chain nodes. If the broadcast action could not be
+	// done, this function returns an error. This function does not give any
+	// guarantees regarding transaction mining. The transaction may be mined or
+	// rejected eventually.
+	BroadcastTransaction(transactionHash Hash) error
+
+	// GetCurrentBlockNumber gets the number of the current block. If the
+	// current block was not determined, this function returns an error.
+	GetCurrentBlockNumber() (uint, error)
+
+	// GetBlockHeader gets the block header for the given block number. If the
+	// block with the given number was not found on the chain, this function
+	// returns an error.
+	GetBlockHeader(blockNumber uint) (*BlockHeader, error)
+}
