075a122f82933f2cb0652e3c4ba6d7e43184a5ee
Tomasz Slabon <tomasz.slabon@keep.network>
2022-11-04 13:15:37 +0100

    Added the relay maintainer loop

--- pkg/maintainer/relay.go
+++ pkg/maintainer/relay.go
@@ -10,20 +10,32 @@ import (
 	"github.com/keep-network/keep-core/pkg/chain"
 )
 
-// Back-off time which should be applied when the relay is restarted.
-// It helps to avoid being flooded with error logs in case of a permanent error
-// in the relay.
-const restartBackoffTime = 10 * time.Second
+const (
+	// Back-off time which should be applied when the relay is restarted.
+	// It helps to avoid being flooded with error logs in case of a permanent error
+	// in the relay.
+	restartBackoffTime = 10 * time.Second
+
+	// Back-off time applied when the relay has not submitted headers for
+	// retargetting, i.e. it is up to date with the Bitcoin blockchain.
+	// It helps to slow down the relay so that it does not overload the on-chain
+	// client by asking it for its state too often.
+	hardRetargetSleepTime = 5 * time.Minute
+
+	// Back-off time applied when the relay has just submitted a new retarget.
+	// It helps to speed up the relay when it may be behind the Bitcoin
+	// blockchain.
+	softRetargetSleepTime = 60 * time.Second
+
+	// The number of blocks in a Bitcoin epoch.
+	bitcoinEpochLength = 2016
+)
 
 var logger = log.Logger("keep-maintainer-relay")
 
 // RelayChain is an interface that provides the ability to communicate with the
 // relay on-chain contract.
 type RelayChain interface {
-	// Retarget adds a new epoch to the relay by providing a proof
-	// of the difficulty before and after the retarget.
-	Retarget(headers []bitcoin.BlockHeader) error
-
 	// Ready checks whether the relay is active (i.e. genesis has been
 	// performed).
 	Ready() (bool, error)
@@ -38,6 +50,21 @@ type RelayChain interface {
 
 	// Signing returns the signing associated with the chain.
 	Signing() chain.Signing
+
+	// Retarget adds a new epoch to the relay by providing a proof
+	// of the difficulty before and after the retarget.
+	Retarget(headers []*bitcoin.BlockHeader) error
+
+	// CurrentEpoch returns the number of the latest epoch whose difficulty is
+	// proven to the relay. If the genesis epoch's number is set correctly, and
+	// retargets along the way have been legitimate, the current epoch equals
+	// the height of the block starting the most recent epoch, divided by 2016.
+	CurrentEpoch() (uint64, error)
+
+	// ProofLength returns the number of blocks required for each side of a
+	// retarget proof: a retarget must provide `proofLength` blocks before
+	// the retarget and `proofLength` blocks after it.
+	ProofLength() (uint64, error)
 }
 
 func newRelay(
@@ -62,7 +89,7 @@ type Relay struct {
 	chain    RelayChain
 }
 
-// startControlLoop launches the loop responsible for controlling the relay.
+// startControlLoop starts the loop responsible for controlling the relay.
 func (r *Relay) startControlLoop(ctx context.Context) {
 	logger.Info("starting headers relay")
 
@@ -85,7 +112,7 @@ func (r *Relay) startControlLoop(ctx context.Context) {
 	}
 }
 
-//lint:ignore U1000 will be used soon
+// submitHeaders submits block headers for retargets.
 func (r *Relay) submitHeaders(ctx context.Context) error {
 	if err := r.verifySubmissionEligibility(); err != nil {
 		return fmt.Errorf(
@@ -94,11 +121,68 @@ func (r *Relay) submitHeaders(ctx context.Context) error {
 		)
 	}
 
-	// TODO: Implement a loop that keeps submitting retargets.
+	for {
+		currentBlockNumber, err := r.btcChain.GetCurrentBlockNumber()
+		if err != nil {
+			return fmt.Errorf(
+				"failed to get current block number [%v]",
+				err,
+			)
+		}
 
-	return nil
+		currentEpoch, err := r.chain.CurrentEpoch()
+		if err != nil {
+			return fmt.Errorf(
+				"failed to get current epoch [%v]",
+				err,
+			)
+		}
+
+		proofLength, err := r.chain.ProofLength()
+		if err != nil {
+			return fmt.Errorf(
+				"failed to get proof length [%v]",
+				err,
+			)
+		}
+
+		nextEpoch := currentEpoch + 1
+		nextEpochHeight := nextEpoch * bitcoinEpochLength
+
+		firstBlockHeaderHeight := uint(nextEpochHeight - proofLength)
+		lastBlockHeaderHeight := uint(nextEpochHeight + proofLength - 1)
+
+		retargetSleepTime := hardRetargetSleepTime
+
+		if currentBlockNumber >= lastBlockHeaderHeight {
+			headers, err := r.getBlockHeaders(
+				firstBlockHeaderHeight,
+				lastBlockHeaderHeight,
+			)
+			if err != nil {
+				return fmt.Errorf(
+					"failed to get block headers for retarget [%v]",
+					err,
+				)
+			}
+
+			if err := r.chain.Retarget(headers); err != nil {
+				return fmt.Errorf(
+					"failed to submit block headers [%v] for a retarget [%v]",
+					headers,
+					err,
+				)
+			}
+
+			retargetSleepTime = softRetargetSleepTime
+		}
+
+		time.Sleep(retargetSleepTime)
+	}
 }
 
+// verifySubmissionEligibility verifies whether a relay maintainer is eligible
+// to submit block headers for retargets.
 func (r *Relay) verifySubmissionEligibility() error {
 	isReady, err := r.chain.Ready()
 	if err != nil {
@@ -143,4 +227,26 @@ func (r *Relay) verifySubmissionEligibility() error {
 	}
 
 	return nil
+}
+
+// getBlockHeaders returns block headers from the given range.
+func (r *Relay) getBlockHeaders(firstHeaderHeight, lastHeaderHeight uint) (
+	[]*bitcoin.BlockHeader, error,
+) {
+	var headers []*bitcoin.BlockHeader
+
+	for height := firstHeaderHeight; height <= lastHeaderHeight; height++ {
+		header, err := r.btcChain.GetBlockHeader(height)
+		if err != nil {
+			return []*bitcoin.BlockHeader{}, fmt.Errorf(
+				"failed to get block header at height [%v]: [%v]",
+				height,
+				err,
+			)
+		}
+
+		headers = append(headers, header)
+	}
+
+	return headers, nil
 }
