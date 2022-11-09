3e33e22dbe1a9b5412a9484e72dab6526a9089e2
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-11-09 13:48:08 +0100

    Receive messages in tECDSA DKG's `symmetricKeyGenerationState`
    This state does not produce any message but should still receive incoming
    messages from other states and store them in the message history. Otherwise,
    there is a gap when messages can be lost.

--- pkg/tecdsa/dkg/states.go
+++ pkg/tecdsa/dkg/states.go
@@ -86,7 +86,16 @@ func (skgs *symmetricKeyGenerationState) Initiate(ctx context.Context) error {
 	)
 }
 
-func (skgs *symmetricKeyGenerationState) Receive(msg net.Message) error {
+func (skgs *symmetricKeyGenerationState) Receive(netMessage net.Message) error {
+	if protocolMessage, ok := netMessage.Payload().(message); ok {
+		if skgs.member.shouldAcceptMessage(
+			protocolMessage.SenderID(),
+			netMessage.SenderPublicKey(),
+		) && skgs.member.sessionID == protocolMessage.SessionID() {
+			skgs.ReceiveToHistory(netMessage)
+		}
+	}
+
 	return nil
 }
 
