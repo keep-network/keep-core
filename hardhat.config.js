f0e924e485cd50656394e10b163d9fd0d69a0a98
Manuel Montenegro <manuel@nucypher.com>
2022-11-09 17:27:31 +0100

    Change Rospten testnet for Goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,21 +21,21 @@ module.exports = {
   },
   namedAccounts: {
     deployer: {
-      "ropsten": 0,
+      "goerli": 0,
       "mainnet": 0,
       "mainnet_test": 0
     },
     rewardsHolder: {
-      "ropsten": "0xCe692F6fA86319Af43050fB7F09FDC43319F7612",
+      "goerli": "0xCe692F6fA86319Af43050fB7F09FDC43319F7612",
       "mainnet": "0x9F6e831c8F8939DC0C830C6e492e7cEf4f9C2F5f",
       "mainnet_test": 0
     },
     tokenContract: {
-      "ropsten": "0x8562d01c9C0F1A8173360E48F50F6b9879c98Dc6",
+      "goerli": "0x3f16380656cAE45D3f80D8833682d2b606eD094A",
       "mainnet": "0xCdF7028ceAB81fA0C6971208e83fa7872994beE5"
     },
     owner: {
-      "ropsten": 0,
+      "goerli": 0,
       "mainnet": "0x9F6e831c8F8939DC0C830C6e492e7cEf4f9C2F5f",
       "mainnet_test": 0
     }
--- hardhat.networks.js
+++ hardhat.networks.js
@@ -18,7 +18,7 @@ function register (name, deploy, chainId, url, privateKey, etherscanNetworkName,
 
 register('mainnet', ['deploy'], 1, process.env.MAINNET_RPC_URL, process.env.MAINNET_PRIVATE_KEY, 'mainnet', process.env.MAINNET_ETHERSCAN_KEY);
 register('mainnet_test', ['deploy_test'], 1, process.env.MAINNET_RPC_URL, process.env.MAINNET_PRIVATE_KEY, 'mainnet', process.env.MAINNET_ETHERSCAN_KEY);
-register('ropsten', ['deploy'], 3, process.env.ROPSTEN_RPC_URL, process.env.ROPSTEN_PRIVATE_KEY, 'ropsten', process.env.MAINNET_ETHERSCAN_KEY);
+register('goerli', ['deploy'], 3, process.env.GOERLI_RPC_URL, process.env.GOERLI_PRIVATE_KEY, 'goerli', process.env.MAINNET_ETHERSCAN_KEY);
 
 module.exports = {
     networks,
