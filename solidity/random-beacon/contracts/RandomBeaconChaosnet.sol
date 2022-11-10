065583e0c3b8432e819dcc239da457d423477559
Dmitry <dymitr.paremski@keep.network>
2022-11-10 18:37:56 +0100

    Adding RandomBeaconChaosnet contract
    This is a stub contract that will replace the original RandomBeacon
    contract until we have off-chain clients ready. It will be used only for
    the initial choasnet phases. Request of a relay entry will set the
    callback contract and also exectute a callback function with an
    arbitrary number instead of a "random beacon".

---
+++ solidity/random-beacon/contracts/RandomBeaconChaosnet.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// ▓▓▌ ▓▓ ▐▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄
+// ▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//   ▓▓▓▓▓▓    ▓▓▓▓▓▓▓▀    ▐▓▓▓▓▓▓    ▐▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+//   ▓▓▓▓▓▓▄▄▓▓▓▓▓▓▓▀      ▐▓▓▓▓▓▓▄▄▄▄         ▓▓▓▓▓▓▄▄▄▄         ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+//   ▓▓▓▓▓▓▓▓▓▓▓▓▓▀        ▐▓▓▓▓▓▓▓▓▓▓         ▓▓▓▓▓▓▓▓▓▓         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//   ▓▓▓▓▓▓▀▀▓▓▓▓▓▓▄       ▐▓▓▓▓▓▓▀▀▀▀         ▓▓▓▓▓▓▀▀▀▀         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▀
+//   ▓▓▓▓▓▓   ▀▓▓▓▓▓▓▄     ▐▓▓▓▓▓▓     ▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌
+// ▓▓▓▓▓▓▓▓▓▓ █▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+// ▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+//
+//                           Trust math, not hardware.
+
+pragma solidity 0.8.17;
+
+import "./api/IRandomBeacon.sol";
+import "./libraries/Callback.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title Keep Random Beacon Stub
+/// @notice Keep Random Beacon stub contract that will be used temporarly until
+///         the real-world random beacon client implementation is ready.
+/// @dev Used for testing purposes only.
+contract RandomBeaconChaosnet is IRandomBeacon, Ownable {
+    using Callback for Callback.Data;
+
+    /// @notice Arbitrary relay entry - Euler's number
+    uint256 public entry = 271828182845904523536028747135266249;
+
+    /// @notice Relay entry callback gas limit.
+    uint256 public _callbackGasLimit = 64_000;
+
+    /// @notice Authorized addresses that can request a relay entry.
+    mapping(address => bool) public authorizedRequesters;
+
+    Callback.Data internal callback;
+
+    event RequesterAuthorizationUpdated(
+        address indexed requester,
+        bool isAuthorized
+    );
+
+    /// @notice Request relay entry stub function sets a callback contract
+    ///         and executes a callback with an arbitrary relay entry number.
+    /// @param callbackContract Beacon consumer callback contract - Wallet Registry
+    /// @dev    Despite being a stub function, a requester still needs to be
+    ///         authorized by the owner.
+    function requestRelayEntry(IRandomBeaconConsumer callbackContract)
+        external
+    {
+        require(
+            authorizedRequesters[msg.sender],
+            "Requester must be authorized"
+        );
+
+        callback.setCallbackContract(callbackContract);
+
+        callback.executeCallback(entry, _callbackGasLimit);
+    }
+
+    /// @notice Authorizes a requester of the relay entry.
+    function setRequesterAuthorization(address requester, bool isAuthorized)
+        external
+        onlyOwner
+    {
+        authorizedRequesters[requester] = isAuthorized;
+
+        emit RequesterAuthorizationUpdated(requester, isAuthorized);
+    }
+}
