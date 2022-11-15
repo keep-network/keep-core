3a9548cd7744393527b0962012b2602ef373cc4f
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-11-15 23:22:33 +0100

    Define Protocol enumeration
    Electrum client supports TCP and SSL connections to the Electrum server.
    This enumeration will be used to handle the protocol types.

---
+++ pkg/bitcoin/electrum/protocol.go
@@ -0,0 +1,30 @@
+package electrum
+
+import "strings"
+
+// Protocol is a type used for electrum protocol enumeration.
+type Protocol int
+
+// Electrum protocols enumeration.
+const (
+	Unknown Protocol = iota
+	TCP
+	SSL
+)
+
+var (
+	protocolMap = map[string]Protocol{
+		"tcp": TCP,
+		"ssl": SSL,
+	}
+)
+
+func (n Protocol) String() string {
+	return []string{"unknown", "tcp", "ssl"}[n]
+}
+
+// ParseProtocol parses string
+func ParseProtocol(str string) (Protocol, bool) {
+	c, ok := protocolMap[strings.ToLower(str)]
+	return c, ok
+}
