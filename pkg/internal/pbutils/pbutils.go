4f82b96d13b41a4d25bf0eaa39ec41c8f6abb12e
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-11-18 16:08:27 +0100

    Unit test for `signingDoneMessage` marshaling code

--- pkg/internal/pbutils/pbutils.go
+++ pkg/internal/pbutils/pbutils.go
@@ -4,6 +4,7 @@ package pbutils
 
 import (
 	"crypto/ecdsa"
+	"github.com/keep-network/keep-core/pkg/tecdsa"
 	"math/big"
 
 	"github.com/btcsuite/btcd/btcec"
@@ -58,6 +59,7 @@ func FuzzFuncs() []interface{} {
 		fuzzEphemeralPrivateKey(),
 		fuzzG1(),
 		fuzzG2(),
+		fuzzTecdsaSignature(),
 	}
 }
 
@@ -121,5 +123,21 @@ func fuzzG2() func(*bn256.G2, fuzz.Continue) {
 		}
 
 		g2.ScalarBaseMult(&k)
+	}
+}
+
+func fuzzTecdsaSignature() func(*tecdsa.Signature, fuzz.Continue) {
+	return func(signature *tecdsa.Signature, c fuzz.Continue) {
+		var r big.Int
+		var s big.Int
+		var recoveryID int
+
+		c.Fuzz(&r)
+		c.Fuzz(&s)
+		c.Fuzz(&recoveryID)
+
+		signature.S = &s
+		signature.R = &r
+		signature.RecoveryID = recoveryID
 	}
 }
--- pkg/tbtc/marshaling_test.go
+++ pkg/tbtc/marshaling_test.go
@@ -3,12 +3,14 @@ package tbtc
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	fuzz "github.com/google/gofuzz"
 	"github.com/keep-network/keep-core/pkg/chain"
 	"github.com/keep-network/keep-core/pkg/internal/pbutils"
 	"github.com/keep-network/keep-core/pkg/internal/tecdsatest"
 	"github.com/keep-network/keep-core/pkg/internal/testutils"
 	"github.com/keep-network/keep-core/pkg/protocol/group"
 	"github.com/keep-network/keep-core/pkg/tecdsa"
+	"math/big"
 	"reflect"
 	"testing"
 )
@@ -41,6 +43,66 @@ func TestSignerMarshalling_NonTECDSAKey(t *testing.T) {
 	_, err := signer.Marshal()
 
 	testutils.AssertErrorsSame(t, errIncompatiblePublicKey, err)
+}
+
+func TestSigningDoneMessage_MarshalingRoundtrip(t *testing.T) {
+	msg := &signingDoneMessage{
+		senderID:      group.MemberIndex(10),
+		message:       big.NewInt(100),
+		attemptNumber: 2,
+		signature: &tecdsa.Signature{
+			R:          big.NewInt(200),
+			S:          big.NewInt(300),
+			RecoveryID: 3,
+		},
+		endBlock: 4500,
+	}
+	unmarshaled := &signingDoneMessage{}
+
+	err := pbutils.RoundTrip(msg, unmarshaled)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(msg, unmarshaled) {
+		t.Fatalf("unexpected content of unmarshaled message")
+	}
+}
+
+func TestFuzzSigningDoneMessage_MarshalingRoundtrip(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		var (
+			senderID      group.MemberIndex
+			message       big.Int
+			attemptNumber uint
+			signature     tecdsa.Signature
+			endBlock      uint64
+		)
+
+		f := fuzz.New().NilChance(0.1).
+			NumElements(0, 512).
+			Funcs(pbutils.FuzzFuncs()...)
+
+		f.Fuzz(&senderID)
+		f.Fuzz(&message)
+		f.Fuzz(&attemptNumber)
+		f.Fuzz(&signature)
+		f.Fuzz(&endBlock)
+
+		doneMessage := &signingDoneMessage{
+			senderID:      senderID,
+			message:       &message,
+			attemptNumber: attemptNumber,
+			signature:     &signature,
+			endBlock:      endBlock,
+		}
+
+		_ = pbutils.RoundTrip(doneMessage, &signingDoneMessage{})
+	}
+}
+
+func TestFuzzSigningDoneMessage_Unmarshaler(t *testing.T) {
+	pbutils.FuzzUnmarshaler(&signingDoneMessage{})
 }
 
 func sampleSigner(t *testing.T) *signer {
