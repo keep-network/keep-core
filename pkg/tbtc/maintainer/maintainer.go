c87ccffd4c948c25b629641e022c91451826225a
Tomasz Slabon <tomasz.slabon@keep.network>
2022-11-22 13:41:36 +0100

    Enabled launching all maintainers if no option specified to the maintainer command

--- pkg/tbtc/maintainer/maintainer.go
+++ pkg/tbtc/maintainer/maintainer.go
@@ -12,13 +12,16 @@ func Initialize(
 	btcChain bitcoin.Chain,
 	chain BitcoinDifficultyChain,
 ) {
-	if config.BitcoinDifficulty {
-		go newBitcoinDifficultyMaintainer(ctx, btcChain, chain)
+	// If non of the maintainers was specified in the config (i.e. no option was
+	// provided to the `maintainer` command), all maintainers should be launched.
+	launchAll := !config.BitcoinDifficulty
+
+	if config.BitcoinDifficulty || launchAll {
+		newBitcoinDifficultyMaintainer(ctx, btcChain, chain)
 	}
 
 	// TODO: Allow for launching multiple maintainers here. Every flag
 	//       indicating a maintainer task should launch a separate maintainer.
-	//       If there are no flags specified, all the maintainers should be launch.
 	//       Notice that panic on one maintainer goroutine will crush the whole
 	//       program. Consider cancelling all maintainers if one maintainer
 	//       cannot ba launched due to a configuration error.
