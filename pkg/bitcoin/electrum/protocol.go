2319f2ad4cf1d74163cd12e613ef3467a76771db
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-12-02 15:00:26 +0100

    Add command flags for electrum config properties
    The flags reflect all the configuration properties for electrum.
    
    We introduced a custom flag for Protocol enum.

--- pkg/bitcoin/electrum/protocol.go
+++ pkg/bitcoin/electrum/protocol.go
@@ -1,6 +1,9 @@
 package electrum
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Protocol is a type used for electrum protocol enumeration.
 type Protocol int
@@ -19,12 +22,24 @@ var (
 	}
 )
 
-func (n Protocol) String() string {
-	return []string{"unknown", "tcp", "ssl"}[n]
+func (p Protocol) String() string {
+	return []string{"unknown", "tcp", "ssl"}[p]
 }
 
 // ParseProtocol parses string.
 func ParseProtocol(str string) (Protocol, bool) {
 	c, ok := protocolMap[strings.ToLower(str)]
 	return c, ok
+}
+
+// UnmarshalText deserializes bytes to a Protocol.
+func (p *Protocol) UnmarshalText(text []byte) error {
+	protocol, ok := ParseProtocol(string(text))
+	if !ok {
+		return fmt.Errorf("failed to parse protocol string %s", text)
+	}
+
+	*p = protocol
+
+	return nil
 }
