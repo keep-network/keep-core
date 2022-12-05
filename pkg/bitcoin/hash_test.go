1fdbbd0ea1fcb589fd5590f37878e8057f99ea0b
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-12-05 15:44:19 +0100

    Unit tests for `bitcoin.ComputeHash`

--- pkg/bitcoin/hash_test.go
+++ pkg/bitcoin/hash_test.go
@@ -60,3 +60,26 @@ func TestHashConversions(t *testing.T) {
 		)
 	}
 }
+
+func TestComputeHash(t *testing.T) {
+	testutils.AssertStringsEqual(
+		t,
+		"hash one",
+		"a26baf5a9a07d9eb7ba10f43924dcdf3f75f0abf066cd9f0c76f983121302e01",
+		ComputeHash([]byte{0x01, 0x02, 0x03, 0x04, 0x05}).String(InternalByteOrder),
+	)
+
+	testutils.AssertStringsEqual(
+		t,
+		"hash two",
+		"648f1115f075ad7c3998a0827fb20c61f37308252ad1778a0ae0b9f3268d4b92",
+		ComputeHash([]byte{0xAA, 0xBB, 0xCC, 0xDD, 0xEE}).String(InternalByteOrder),
+	)
+
+	testutils.AssertStringsEqual(
+		t,
+		"hash three",
+		"1397459010d34bc0a14dd4a5cf9e091cf6b4ebc6a1b21d315135af4139a73b3a",
+		ComputeHash([]byte{0x01, 0xAA, 0x02, 0xBB, 0x03}).String(InternalByteOrder),
+	)
+}
