7a58312bace2a2866facfa57c1fd350b30dc8663
Tomasz Slabon <tomasz.slabon@keep.network>
2022-12-09 17:06:20 +0100

    Updated the btcdiff.Chain interface

--- pkg/btcdiff/chain.go
+++ pkg/btcdiff/chain.go
@@ -11,16 +11,19 @@ type Chain interface {
 	// Ready checks whether the relay is active (i.e. genesis has been performed).
 	// Note that if the relay is used by querying the current and previous epoch
 	// difficulty, at least one retarget needs to be provided after genesis;
-	// otherwise the prevEpochDifficulty will be uninitialised and zero.
+	// otherwise the prevEpochDifficulty will be uninitialized and zero.
 	Ready() (bool, error)
 
 	// AuthorizationRequired checks whether the relay requires the address
 	// submitting a retarget to be authorised in advance by governance.
 	AuthorizationRequired() (bool, error)
 
-	// IsAuthorized checks whether the given address has been authorised by
+	// IsAuthorized checks whether the given address has been authorized by
 	// governance to submit a retarget.
 	IsAuthorized(address chain.Address) (bool, error)
+
+	// Signing returns the signing associated with the chain.
+	Signing() chain.Signing
 
 	// Retarget adds a new epoch to the relay by providing a proof
 	// of the difficulty before and after the retarget.
