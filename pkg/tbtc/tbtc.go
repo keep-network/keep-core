0e55626fdbaa16f5d5e29d74511dc1a9a28f67d2
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-12-16 09:58:12 +0100

    Add a TODO about actions that should be done after a DKG result is challenged

--- pkg/tbtc/tbtc.go
+++ pkg/tbtc/tbtc.go
@@ -165,6 +165,9 @@ func Initialize(
 		}()
 	})
 
+	// TODO: Repeat DKG in case of the result challenge and archive the
+	//       signers stored for the invalid result.
+
 	_ = chain.OnHeartbeatRequested(func(event *HeartbeatRequestedEvent) {
 		go func() {
 			// There is no need to deduplicate. Test loop events are unique.
