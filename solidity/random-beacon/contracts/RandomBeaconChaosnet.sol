d94f367691dd62952eb60be3a8fea90af5f5efb4
Tomasz Slabon <tomasz.slabon@keep.network>
2023-01-03 11:32:18 +0100

    Simplified RandomBeaconChaosnet

--- solidity/random-beacon/contracts/RandomBeaconChaosnet.sol
+++ solidity/random-beacon/contracts/RandomBeaconChaosnet.sol
@@ -15,27 +15,18 @@
 pragma solidity 0.8.17;
 
 import "./api/IRandomBeacon.sol";
-import "./libraries/Callback.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Keep Random Beacon Chaosnet Stub
 /// @notice Keep Random Beacon stub contract that will be used temporarily until
 ///         the real-world random beacon client implementation is ready.
-/// @dev Used for testing purposes only.
 contract RandomBeaconChaosnet is IRandomBeacon, Ownable {
-    using Callback for Callback.Data;
-
-    /// @notice Relay entry callback gas limit.
-    uint256 public constant _callbackGasLimit = 64_000;
-
     /// @notice Authorized addresses that can request a relay entry.
     mapping(address => bool) public authorizedRequesters;
 
     /// @notice Arbitrary relay entry. Initially set to the Euler's number.
     ///         It's updated after each relay entry request.
     uint256 internal entry = 271828182845904523536028747135266249;
-
-    Callback.Data internal callback;
 
     event RequesterAuthorizationUpdated(
         address indexed requester,
@@ -55,13 +46,10 @@ contract RandomBeaconChaosnet is IRandomBeacon, Ownable {
             "Requester must be authorized"
         );
 
-        callback.setCallbackContract(callbackContract);
-
         // Update the entry so that a different group of wallet operators is
         // selected in `WalletRegistry` on each request.
         entry = uint256(keccak256(abi.encodePacked(entry)));
-
-        callback.executeCallback(entry, _callbackGasLimit);
+        callbackContract.__beaconCallback(entry, block.number);
     }
 
     /// @notice Authorizes a requester of the relay entry.
--- solidity/random-beacon/contracts/test/RandomBeaconChaosnetStub.sol
+++ solidity/random-beacon/contracts/test/RandomBeaconChaosnetStub.sol
@@ -4,7 +4,7 @@ import "../RandomBeaconChaosnet.sol";
 import "../libraries/Callback.sol";
 
 contract RandomBeaconChaosnetStub is RandomBeaconChaosnet {
-    function getCallbackContract() external view returns (address) {
-        return address(callback.callbackContract);
-    }
+    // function getCallbackContract() external view returns (address) {
+    //     return address(callback.callbackContract);
+    // }
 }
