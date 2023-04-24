0bfa45aa476975a97abbc0e5793bfa5c6402e692
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-04-24 12:37:01 +0200

    Comment about nil `Vault` in `DepositRevealedEvent` and `DepositChainRequest`

--- pkg/tbtc/chain.go
+++ pkg/tbtc/chain.go
@@ -216,6 +216,7 @@ type HeartbeatRequestedEvent struct {
 }
 
 // DepositRevealedEvent represents a deposit reveal event.
+// The Vault field is nil if the deposit does not target any vault on-chain.
 type DepositRevealedEvent struct {
 	FundingTxHash       bitcoin.Hash
 	FundingOutputIndex  uint32
@@ -256,6 +257,7 @@ type DepositRevealedEventFilter struct {
 }
 
 // DepositChainRequest represents a deposit request stored on-chain.
+// The Vault field is nil if the deposit does not target any vault on-chain.
 type DepositChainRequest struct {
 	Depositor   chain.Address
 	Amount      uint64
