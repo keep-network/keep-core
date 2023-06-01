d0cb5bb91ed37a2ca3d2c97d4456bd0547d20211
Tomasz Slabon <tomasz.slabon@keep.network>
2023-06-01 15:35:32 +0200

    Refactored unit tests

--- pkg/maintainer/proof_test.go
+++ pkg/maintainer/proof_test.go
@@ -8,104 +8,56 @@ import (
 	"github.com/keep-network/keep-core/pkg/bitcoin"
 )
 
-func TestAssembleTransactionProof_SingleInputTransaction(t *testing.T) {
-	// TODO: Rewrite to parametrized test once more cases are added.
-	transactionHash := testData.AssembleProof["single input"].BitcoinChainData.TransactionHash
-	transaction := testData.AssembleProof["single input"].BitcoinChainData.Transaction
-	accumulatedConfirmations := testData.AssembleProof["single input"].BitcoinChainData.AccumulatedTxConfirmations
-	requiredConfirmations := testData.AssembleProof["single input"].RequiredConfirmations
-	blockHeaders := testData.AssembleProof["single input"].BitcoinChainData.HeadersChain
-	transactionMerkleProof := testData.AssembleProof["single input"].BitcoinChainData.TransactionMerkleProof
-	expectedProof := testData.AssembleProof["single input"].ExpectedProof
-	expectedTx := &transaction
+func TestAssembleTransactionProof(t *testing.T) {
+	for testName, test := range testData.AssembleProof {
+		t.Run(testName, func(t *testing.T) {
+			requiredConfirmations := test.RequiredConfirmations
+			transactionHash := test.BitcoinChainData.TransactionHash
+			transaction := test.BitcoinChainData.Transaction
+			accumulatedConfirmations := test.BitcoinChainData.AccumulatedTxConfirmations
+			blockHeaders := test.BitcoinChainData.HeadersChain
+			transactionMerkleProof := test.BitcoinChainData.TransactionMerkleProof
+			expectedProof := test.ExpectedProof
+			expectedTx := &transaction
 
-	bitcoinChain := connectLocalBitcoinChain()
+			bitcoinChain := connectLocalBitcoinChain()
 
-	var transactions = map[bitcoin.Hash]*bitcoin.Transaction{
-		transactionHash: &transaction,
-	}
-	bitcoinChain.SetTransactions(transactions)
+			var transactions = map[bitcoin.Hash]*bitcoin.Transaction{
+				transactionHash: &transaction,
+			}
+			bitcoinChain.SetTransactions(transactions)
 
-	var transactionConfirmations = map[bitcoin.Hash]uint{
-		transactionHash: accumulatedConfirmations,
-	}
-	bitcoinChain.SetTransactionConfirmations(transactionConfirmations)
+			var transactionConfirmations = map[bitcoin.Hash]uint{
+				transactionHash: accumulatedConfirmations,
+			}
+			bitcoinChain.SetTransactionConfirmations(transactionConfirmations)
 
-	bitcoinChain.SetBlockHeaders(blockHeaders)
-	bitcoinChain.SetTransactionMerkleProof(transactionMerkleProof)
+			bitcoinChain.SetBlockHeaders(blockHeaders)
+			bitcoinChain.SetTransactionMerkleProof(transactionMerkleProof)
 
-	tx, proof, err := AssembleTransactionProof(
-		transactionHash,
-		requiredConfirmations,
-		bitcoinChain,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
+			tx, proof, err := AssembleTransactionProof(
+				transactionHash,
+				requiredConfirmations,
+				bitcoinChain,
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if !reflect.DeepEqual(expectedProof, proof) {
-		t.Errorf(
-			"unexpected proof\nexpected: %v\nactual:   %v\n",
-			expectedProof,
-			proof,
-		)
-	}
-	if !reflect.DeepEqual(expectedTx, tx) {
-		t.Errorf(
-			"unexpected transaction\nexpected: %v\nactual:   %v\n",
-			expectedTx,
-			tx,
-		)
-	}
-}
-
-func TestAssembleTransactionProof_MultipleInputsTransaction(t *testing.T) {
-	// TODO: Rewrite to parametrized test once more cases are added.
-	transactionHash := testData.AssembleProof["multiple inputs"].BitcoinChainData.TransactionHash
-	transaction := testData.AssembleProof["multiple inputs"].BitcoinChainData.Transaction
-	accumulatedConfirmations := testData.AssembleProof["multiple inputs"].BitcoinChainData.AccumulatedTxConfirmations
-	requiredConfirmations := testData.AssembleProof["multiple inputs"].RequiredConfirmations
-	blockHeaders := testData.AssembleProof["multiple inputs"].BitcoinChainData.HeadersChain
-	transactionMerkleProof := testData.AssembleProof["multiple inputs"].BitcoinChainData.TransactionMerkleProof
-	expectedProof := testData.AssembleProof["multiple inputs"].ExpectedProof
-	expectedTx := &transaction
-
-	bitcoinChain := connectLocalBitcoinChain()
-
-	var transactions = map[bitcoin.Hash]*bitcoin.Transaction{
-		transactionHash: &transaction,
-	}
-	bitcoinChain.SetTransactions(transactions)
-
-	var transactionConfirmations = map[bitcoin.Hash]uint{
-		transactionHash: accumulatedConfirmations,
-	}
-	bitcoinChain.SetTransactionConfirmations(transactionConfirmations)
-
-	bitcoinChain.SetBlockHeaders(blockHeaders)
-	bitcoinChain.SetTransactionMerkleProof(transactionMerkleProof)
-
-	tx, proof, err := AssembleTransactionProof(
-		transactionHash,
-		requiredConfirmations,
-		bitcoinChain,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !reflect.DeepEqual(expectedProof, proof) {
-		t.Errorf(
-			"unexpected proof\nexpected: %v\nactual:   %v\n",
-			expectedProof,
-			proof,
-		)
-	}
-	if !reflect.DeepEqual(expectedTx, tx) {
-		t.Errorf(
-			"unexpected transaction\nexpected: %v\nactual:   %v\n",
-			expectedTx,
-			tx,
-		)
+			if !reflect.DeepEqual(expectedProof, proof) {
+				t.Errorf(
+					"unexpected proof\nexpected: %v\nactual:   %v\n",
+					expectedProof,
+					proof,
+				)
+			}
+			if !reflect.DeepEqual(expectedTx, tx) {
+				t.Errorf(
+					"unexpected transaction\nexpected: %v\nactual:   %v\n",
+					expectedTx,
+					tx,
+				)
+			}
+		})
 	}
 }
