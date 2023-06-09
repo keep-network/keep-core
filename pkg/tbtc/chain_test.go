fb80bdffa9e409eb768e946b8d50f8f2ab40a9b7
Jakub Nowakowski <jakub.nowakowski@keep.network>
2023-06-09 16:16:50 +0200

    Implement local chain for PastNewWalletRegisteredEvents

--- pkg/tbtc/chain_test.go
+++ pkg/tbtc/chain_test.go
@@ -56,6 +56,9 @@ type localChain struct {
 	pastDepositRevealedEventsMutex sync.Mutex
 	pastDepositRevealedEvents      map[[32]byte][]*DepositRevealedEvent
 
+	pastNewWalletRegisteredEventsMutex  sync.Mutex
+	pastNewWalletRegisteredEventsEvents map[[32]byte][]*NewWalletRegisteredEvent
+
 	depositSweepProposalValidationsMutex sync.Mutex
 	depositSweepProposalValidations      map[[32]byte]bool
 
@@ -552,6 +555,51 @@ func (lc *localChain) setDepositRequest(
 	requestKey := buildDepositRequestKey(fundingTxHash, fundingOutputIndex)
 
 	lc.depositRequests[requestKey] = request
+}
+
+func buildPastNewWalletRegisteredEventsKey(
+	filter *NewWalletRegisteredEventFilter,
+) ([32]byte, error) {
+	var buffer bytes.Buffer
+
+	startBlock := make([]byte, 8)
+	binary.BigEndian.PutUint64(startBlock, filter.StartBlock)
+	buffer.Write(startBlock)
+
+	if filter.EndBlock != nil {
+		endBlock := make([]byte, 8)
+		binary.BigEndian.PutUint64(startBlock, *filter.EndBlock)
+		buffer.Write(endBlock)
+	}
+
+	for _, ecdsaWalletID := range filter.EcdsaWalletID {
+		buffer.Write(ecdsaWalletID[:])
+	}
+
+	for _, walletPublicKeyHash := range filter.WalletPublicKeyHash {
+		buffer.Write(walletPublicKeyHash[:])
+	}
+
+	return sha256.Sum256(buffer.Bytes()), nil
+}
+
+func (lc *localChain) PastNewWalletRegisteredEvents(
+	filter *NewWalletRegisteredEventFilter,
+) ([]*NewWalletRegisteredEvent, error) {
+	lc.pastNewWalletRegisteredEventsMutex.Lock()
+	defer lc.pastNewWalletRegisteredEventsMutex.Unlock()
+
+	eventsKey, err := buildPastNewWalletRegisteredEventsKey(filter)
+	if err != nil {
+		return nil, err
+	}
+
+	events, ok := lc.pastNewWalletRegisteredEventsEvents[eventsKey]
+	if !ok {
+		return nil, fmt.Errorf("no events for given filter")
+	}
+
+	return events, nil
 }
 
 func (lc *localChain) BuildDepositKey(
