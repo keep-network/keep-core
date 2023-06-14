936fffe5118034bb5e0a8e9e832389bab11064c5
Jakub Nowakowski <jakub.nowakowski@keep.network>
2023-06-14 10:55:26 +0200

    Define more cases to prefixed and unprefixed hex strings

--- cmd/coordinator_test.go
+++ cmd/coordinator_test.go
@@ -17,9 +17,12 @@ var walletPublicKeyHashTests = []struct {
 	{input: ``, wantErr: fmt.Errorf("empty hex string")},
 	{input: `01`, wantErr: fmt.Errorf("invalid bytes length: [1], expected: [20]")},
 	{input: `0x01`, wantErr: fmt.Errorf("invalid bytes length: [1], expected: [20]")},
-	{input: `0x5bee2805df9fcea4691c442fe4c1a33f7288e2`, wantErr: fmt.Errorf("invalid bytes length: [19], expected: [20]")},
+	{input: `5bee2805df9fcea4691c442fe4c1a33f7288e2`, wantErr: fmt.Errorf("invalid bytes length: [19], expected: [20]")},
 	{input: `000f4224b6858eee7f8999e6299c056c6405bbede0`, wantErr: fmt.Errorf("invalid bytes length: [21], expected: [20]")},
+	{input: `0x5bee2805df9fcea4691c442fe4c1a33f7288e2`, wantErr: fmt.Errorf("invalid bytes length: [19], expected: [20]")},
+	{input: `0x000f4224b6858eee7f8999e6299c056c6405bbede0`, wantErr: fmt.Errorf("invalid bytes length: [21], expected: [20]")},
 	// valid
+	{input: `48b88e1074c33c7a934f781220e1a4523f1768c0`, expectedResult: [20]byte{72, 184, 142, 16, 116, 195, 60, 122, 147, 79, 120, 18, 32, 225, 164, 82, 63, 23, 104, 192}},
 	{input: `0x48b88e1074c33c7a934f781220e1a4523f1768c0`, expectedResult: [20]byte{72, 184, 142, 16, 116, 195, 60, 122, 147, 79, 120, 18, 32, 225, 164, 82, 63, 23, 104, 192}},
 	{input: `0x00008e1074c33c7a934f781220e1a4523f1768c0`, expectedResult: [20]byte{00, 00, 142, 16, 116, 195, 60, 122, 147, 79, 120, 18, 32, 225, 164, 82, 63, 23, 104, 192}},
 	{input: `0x48b88e1074c33c7a934f781220e1a4523f000000`, expectedResult: [20]byte{72, 184, 142, 16, 116, 195, 60, 122, 147, 79, 120, 18, 32, 225, 164, 82, 63, 00, 00, 00}},
