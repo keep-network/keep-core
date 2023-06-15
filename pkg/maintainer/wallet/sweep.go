901de2ff516cc63c5a14ac15e7c1c116f5f5d322
Jakub Nowakowski <jakub.nowakowski@keep.network>
2023-06-15 11:21:20 +0200

    Extract runOnceWalletUnlocked function
    The function can be reused by other actions or used in unit tests.

--- pkg/maintainer/wallet/sweep.go
+++ pkg/maintainer/wallet/sweep.go
@@ -3,9 +3,7 @@ package wallet
 import (
 	"context"
 	"fmt"
-	"time"
 
-	"github.com/keep-network/keep-core/internal/hexutils"
 	"github.com/keep-network/keep-core/pkg/coordinator"
 )
 
@@ -25,36 +23,18 @@ func (wm *walletMaintainer) runSweepTask(ctx context.Context) error {
 		return fmt.Errorf("failed to prepare deposits sweep proposal: %v", err)
 	}
 
-	lockExpiration, walletAction, err := wm.chain.GetWalletLock(walletPublicKeyHash)
-	if err != nil {
-		return fmt.Errorf(
-			"failed to get wallet locks for wallet public key hash[%s]: [%w]",
-			hexutils.Encode(walletPublicKeyHash[:]),
-			err,
-		)
-	}
-
-	if lockExpiration.Unix() > 0 {
-		logger.Infof(
-			"wallet is locked due to [%s] action until [%s]; waiting...",
-			walletAction.String(),
-			lockExpiration.String(),
-		)
-
-		select {
-		case <-ctx.Done():
-			return nil
-		case <-time.After(time.Until(lockExpiration)):
-			break
-		}
-	}
-
-	return coordinator.ProposeDepositsSweep(
-		wm.chain,
-		wm.btcChain,
+	return wm.runOnceWalletUnlocked(
+		ctx,
 		walletPublicKeyHash,
-		0,
-		deposits,
-		true, // TODO: Change dry run argument to false
+		func() error {
+			return coordinator.ProposeDepositsSweep(
+				wm.chain,
+				wm.btcChain,
+				walletPublicKeyHash,
+				0,
+				deposits,
+				true, // TODO: Change dry run argument to false
+			)
+		},
 	)
 }
--- pkg/maintainer/wallet/wallet.go
+++ pkg/maintainer/wallet/wallet.go
@@ -2,11 +2,13 @@ package wallet
 
 import (
 	"context"
+	"fmt"
 
 	"time"
 
 	"github.com/ipfs/go-log/v2"
 
+	"github.com/keep-network/keep-core/internal/hexutils"
 	"github.com/keep-network/keep-core/pkg/bitcoin"
 )
 
@@ -82,4 +84,36 @@ func (wm *walletMaintainer) startControlLoop(ctx context.Context) {
 			sweepTicker.Reset(wm.config.SweepInterval)
 		}
 	}
+}
+
+func (wm *walletMaintainer) runOnceWalletUnlocked(
+	ctx context.Context,
+	walletPublicKeyHash [20]byte,
+	runFunc func() error,
+) error {
+	lockExpiration, walletAction, err := wm.chain.GetWalletLock(walletPublicKeyHash)
+	if err != nil {
+		return fmt.Errorf(
+			"failed to get wallet lock for wallet public key hash [%s]: [%w]",
+			hexutils.Encode(walletPublicKeyHash[:]),
+			err,
+		)
+	}
+
+	if lockExpiration.Unix() > 0 {
+		logger.Infof(
+			"wallet is locked due to [%s] action until [%s]; waiting...",
+			walletAction.String(),
+			lockExpiration.String(),
+		)
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.After(time.Until(lockExpiration)):
+			break
+		}
+	}
+
+	return runFunc()
 }
