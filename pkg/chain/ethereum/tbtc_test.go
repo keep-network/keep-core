a449416f31b41fd6633d9b43c80815628cb19db7
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-06-21 15:49:23 +0200

    Unit tests for `buildRedemptionKey` function

--- pkg/chain/ethereum/tbtc_test.go
+++ pkg/chain/ethereum/tbtc_test.go
@@ -360,3 +360,32 @@ func TestBuildDepositKey(t *testing.T) {
 		depositKey.Text(16),
 	)
 }
+
+func TestBuildRedemptionKey(t *testing.T) {
+	fromHex := func(hexString string) []byte {
+		b, err := hex.DecodeString(hexString)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return b
+	}
+
+	walletPublicKeyHashBytes := fromHex("8db50eb52063ea9d98b3eac91489a90f738986f6")
+	var walletPublicKeyHash [20]byte
+	copy(walletPublicKeyHash[:], walletPublicKeyHashBytes)
+
+	redeemerOutputScript := fromHex("76a9144130879211c54df460e484ddf9aac009cb38ee7488ac")
+
+	redemptionKey, err := buildRedemptionKey(walletPublicKeyHash, redeemerOutputScript)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedRedemptionKey := "cb493004c645792101cfa4cc5da4c16aa3148065034371a6f1478b7df4b92d39"
+	testutils.AssertStringsEqual(
+		t,
+		"redemption key",
+		expectedRedemptionKey,
+		redemptionKey.Text(16),
+	)
+}
