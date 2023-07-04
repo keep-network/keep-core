374c839fbcf6d8f731a5de0b60bab0cdb28c5067
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-07-04 17:10:13 +0200

    Unit tests machinery for `FindPendingRedemptions` functions

--- pkg/maintainer/wallet/redemptions_test.go
+++ pkg/maintainer/wallet/redemptions_test.go
@@ -2,9 +2,12 @@ package wallet_test
 
 import (
 	"encoding/hex"
+	"github.com/go-test/deep"
 	"github.com/keep-network/keep-core/internal/testutils"
 	"github.com/keep-network/keep-core/pkg/bitcoin"
 	walletmtr "github.com/keep-network/keep-core/pkg/maintainer/wallet"
+	"github.com/keep-network/keep-core/pkg/maintainer/wallet/internal/test"
+	"github.com/keep-network/keep-core/pkg/tbtc"
 	"testing"
 )
 
@@ -36,4 +39,101 @@ func TestEstimateRedemptionFee(t *testing.T) {
 
 	expectedFee := 4000 // transactionVirtualSize * satPerVByteFee = 250 * 16 = 4000
 	testutils.AssertIntsEqual(t, "fee", expectedFee, int(actualFee))
+}
+
+func TestFindPendingRedemptions(t *testing.T) {
+	scenarios, err := test.LoadFindPendingRedemptionsTestScenario()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.Title, func(t *testing.T) {
+			tbtcChain := walletmtr.NewLocalChain()
+
+			// Set the average block time enforced by the scenario.
+			tbtcChain.SetAverageBlockTime(scenario.ChainParameters.AverageBlockTime)
+
+			// Set the scenario's current block using a mock block counter.
+			// This is needed to build a proper filter for the
+			// `PastRedemptionRequestedEvents` call.
+			blockCounter := walletmtr.NewMockBlockCounter()
+			blockCounter.SetCurrentBlock(scenario.ChainParameters.CurrentBlock)
+			tbtcChain.SetBlockCounter(blockCounter)
+
+			// Set relevant governable parameters based on values provided by
+			// the scenario.
+			tbtcChain.SetRedemptionParameters(
+				0,
+				0,
+				0,
+				0,
+				scenario.ChainParameters.RequestTimeout,
+				nil,
+				0,
+			)
+			tbtcChain.SetRedemptionRequestMinAge(scenario.ChainParameters.RequestMinAge)
+
+			requestTimeoutBlocks := uint64(scenario.ChainParameters.RequestTimeout) /
+				uint64(scenario.ChainParameters.AverageBlockTime.Seconds())
+
+			// Record scenario wallets to the local chain.
+			for _, wallet := range scenario.Wallets {
+				err := tbtcChain.AddPastNewWalletRegisteredEvent(
+					nil,
+					&tbtc.NewWalletRegisteredEvent{
+						WalletPublicKeyHash: wallet.WalletPublicKeyHash,
+						BlockNumber:         wallet.RegistrationBlockNumber,
+					},
+				)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			// Record scenario pending redemptions to the local chain.
+			for _, pendingRedemption := range scenario.PendingRedemptions {
+				// Record the corresponding event. Set only relevant fields.
+				err = tbtcChain.AddPastRedemptionRequestedEvent(
+					&tbtc.RedemptionRequestedEventFilter{
+						// Remember about including the constant factor
+						// of 1000 blocks.
+						StartBlock:          scenario.ChainParameters.CurrentBlock - requestTimeoutBlocks - 1000,
+						WalletPublicKeyHash: [][20]byte{pendingRedemption.WalletPublicKeyHash},
+					},
+					&tbtc.RedemptionRequestedEvent{
+						WalletPublicKeyHash:  pendingRedemption.WalletPublicKeyHash,
+						RedeemerOutputScript: pendingRedemption.RedeemerOutputScript,
+						RequestedAmount:      pendingRedemption.RequestedAmount,
+						BlockNumber:          pendingRedemption.RequestBlock,
+					},
+				)
+
+				// Record the corresponding request object. Set only relevant fields.
+				tbtcChain.SetPendingRedemptionRequest(
+					pendingRedemption.WalletPublicKeyHash,
+					&tbtc.RedemptionRequest{
+						RedeemerOutputScript: pendingRedemption.RedeemerOutputScript,
+						RequestedAmount:      pendingRedemption.RequestedAmount,
+						RequestedAt:          pendingRedemption.RequestedAt,
+					},
+				)
+			}
+
+			walletsPendingRedemptions, err := walletmtr.FindPendingRedemptions(
+				tbtcChain,
+				scenario.Filter,
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := deep.Equal(
+				scenario.ExpectedWalletsPendingRedemptions,
+				walletsPendingRedemptions,
+			); diff != nil {
+				t.Errorf("invalid wallets pending redemptions: %v", diff)
+			}
+		})
+	}
 }
