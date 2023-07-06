0d6c8ac805cce3fc9c067ab5b7a621d25c7f1446
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-07-06 13:43:26 +0200

    Add `spv.localBitcoinChain` stub

---
+++ pkg/maintainer/spv/bitcoin_chain_test.go
@@ -0,0 +1,74 @@
+package spv
+
+import (
+	"github.com/keep-network/keep-core/pkg/bitcoin"
+	"sync"
+)
+
+type localBitcoinChain struct {
+	mutex sync.Mutex
+}
+
+func newLocalBitcoinChain() *localBitcoinChain {
+	return &localBitcoinChain{}
+}
+
+func (lbc *localBitcoinChain) GetTransaction(transactionHash bitcoin.Hash) (
+	*bitcoin.Transaction,
+	error,
+) {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) GetTransactionConfirmations(transactionHash bitcoin.Hash) (
+	uint,
+	error,
+) {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) BroadcastTransaction(transaction *bitcoin.Transaction) error {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) GetLatestBlockHeight() (uint, error) {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) GetBlockHeader(blockHeight uint) (
+	*bitcoin.BlockHeader,
+	error,
+) {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) GetTransactionMerkleProof(
+	transactionHash bitcoin.Hash,
+	blockHeight uint,
+) (*bitcoin.TransactionMerkleProof, error) {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) GetTransactionsForPublicKeyHash(
+	publicKeyHash [20]byte,
+	limit int,
+) ([]*bitcoin.Transaction, error) {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) GetMempoolForPublicKeyHash(publicKeyHash [20]byte) (
+	[]*bitcoin.Transaction,
+	error,
+) {
+	panic("unsupported")
+}
+
+func (lbc *localBitcoinChain) EstimateSatPerVByteFee(blocks uint32) (
+	int64,
+	error,
+) {
+	panic("unsupported")
+}
+
+
+
