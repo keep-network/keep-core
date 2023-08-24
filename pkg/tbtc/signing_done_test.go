874c32033121796f7ee74d321449ef5faa204baa
Tomasz Slabon <tomasz.slabon@keep.network>
2023-08-24 17:40:07 +0200

    Updated signing done check related unit tests

--- pkg/tbtc/signing_done_test.go
+++ pkg/tbtc/signing_done_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/keep-network/keep-core/internal/testutils"
 	"github.com/keep-network/keep-core/pkg/chain"
 	"github.com/keep-network/keep-core/pkg/chain/local_v1"
+	"github.com/keep-network/keep-core/pkg/net"
 	"github.com/keep-network/keep-core/pkg/net/local"
 	"github.com/keep-network/keep-core/pkg/operator"
 	"github.com/keep-network/keep-core/pkg/protocol/group"
@@ -325,6 +326,10 @@ func setupSigningDoneCheck(
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	broadcastChannel.SetUnmarshaler(func() net.TaggedUnmarshaler {
+		return &signingDoneMessage{}
+	})
 
 	membershipValidator := group.NewMembershipValidator(
 		&testutils.MockLogger{},
