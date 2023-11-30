2ab464ded7403b78d6efb0cc7ea8a529d3863ebc
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-11-30 16:42:21 +0100

    Implement proposal generator core
    Here we implement the core logic of the proposal generator. Specific
    proposal tasks will be added in the follow-up changesets.

--- pkg/tbtcpg/tbtcpg.go
+++ pkg/tbtcpg/tbtcpg.go
@@ -1,7 +1,106 @@
 package tbtcpg
 
 import (
+	"fmt"
 	"github.com/ipfs/go-log/v2"
+	"github.com/keep-network/keep-core/pkg/bitcoin"
+	"github.com/keep-network/keep-core/pkg/tbtc"
+	"go.uber.org/zap"
+	"golang.org/x/exp/slices"
 )
 
 var logger = log.Logger("keep-tbtcpg")
+
+// proposalTask encapsulates logic used to generate an action proposal
+// of the given type.
+type proposalTask interface {
+	// run executes the task and returns a proposal, a boolean flag indicating
+	// whether the proposal was generated and an error if any.
+	run(walletPublicKeyHash [20]byte) (tbtc.CoordinationProposal, bool, error)
+	// actionType returns the type of the action proposal.
+	actionType() tbtc.WalletActionType
+}
+
+// ProposalGenerator is a component responsible for generating coordination
+// proposals for tbtc wallets.
+type ProposalGenerator struct {
+	tasks []proposalTask
+}
+
+// NewProposalGenerator returns a new proposal generator.
+func NewProposalGenerator(
+	chain Chain,
+	btcChain bitcoin.Chain,
+) *ProposalGenerator {
+	tasks := []proposalTask{
+		// TODO: Fill with implementations.
+	}
+
+	return &ProposalGenerator{
+		tasks: tasks,
+	}
+}
+
+// Generate generates a coordination proposal based on the given checklist
+// of possible wallet actions. The checklist is a list of actions that
+// should be checked for the given coordination window. This function returns
+// a proposal for the first action from the checklist that is valid for the
+// given wallet's state. If none of the actions are valid, the generator
+// returns a no-op proposal.
+func (pg *ProposalGenerator) Generate(
+	walletPublicKeyHash [20]byte,
+	actionsChecklist []tbtc.WalletActionType,
+) (tbtc.CoordinationProposal, error) {
+	walletLogger := logger.With(
+		zap.String("walletPKH", fmt.Sprintf("0x%x", walletPublicKeyHash)),
+	)
+
+	walletLogger.Info(
+		"starting proposal generation with tasks checklist [%v]",
+		actionsChecklist,
+	)
+
+	for _, action := range actionsChecklist {
+		walletLogger.Infof("starting proposal task [%s]", action)
+
+		taskIndex := slices.IndexFunc(pg.tasks, func(task proposalTask) bool {
+			return task.actionType() == action
+		})
+
+		if taskIndex < 0 {
+			walletLogger.Warnf("proposal task [%s] is not supported", action)
+			continue
+		}
+
+		proposal, ok, err := pg.tasks[taskIndex].run(walletPublicKeyHash)
+		if err != nil {
+			return nil, fmt.Errorf(
+				"error while running proposal task [%s]: [%v]",
+				action,
+				err,
+			)
+		}
+
+		if !ok {
+			walletLogger.Infof(
+				"proposal task [%s] completed without result",
+				action,
+			)
+			continue
+		}
+
+		walletLogger.Infof(
+			"proposal task [%s] completed with a result",
+			action,
+		)
+
+		return proposal, nil
+	}
+
+	walletLogger.Infof(
+		"all proposal tasks completed without result; " +
+			"returning no-op proposal",
+	)
+
+	return &tbtc.NoopProposal{}, nil
+}
---
+++ pkg/tbtcpg/tbtcpg_test.go
@@ -0,0 +1,214 @@
+package tbtcpg
+
+import (
+	"fmt"
+	"github.com/keep-network/keep-core/pkg/tbtc"
+	"reflect"
+	"testing"
+)
+
+func TestProposalGenerator_Generate(t *testing.T) {
+	walletPublicKeyHash := [20]byte{1, 2, 3}
+
+	tests := map[string]struct {
+		tasks            []proposalTask
+		actionsChecklist []tbtc.WalletActionType
+		expectedProposal tbtc.CoordinationProposal
+		expectedErr      error
+	}{
+		"first task generates a proposal": {
+			tasks: []proposalTask{
+				&mockProposalTask{
+					action: tbtc.ActionRedemption,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultProposal,
+					},
+				},
+				&mockProposalTask{
+					action: tbtc.ActionDepositSweep,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultProposal,
+					},
+				},
+			},
+			actionsChecklist: []tbtc.WalletActionType{
+				tbtc.ActionRedemption,
+				tbtc.ActionDepositSweep,
+			},
+			expectedProposal: &mockCoordinationProposal{tbtc.ActionRedemption},
+		},
+		"subsequent task generates a proposal": {
+			tasks: []proposalTask{
+				&mockProposalTask{
+					action: tbtc.ActionRedemption,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultEmpty,
+					},
+				},
+				&mockProposalTask{
+					action: tbtc.ActionDepositSweep,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultProposal,
+					},
+				},
+			},
+			actionsChecklist: []tbtc.WalletActionType{
+				tbtc.ActionRedemption,
+				tbtc.ActionDepositSweep,
+			},
+			expectedProposal: &mockCoordinationProposal{tbtc.ActionDepositSweep},
+		},
+		"first task returns error": {
+			tasks: []proposalTask{
+				&mockProposalTask{
+					action: tbtc.ActionRedemption,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultError,
+					},
+				},
+				&mockProposalTask{
+					action: tbtc.ActionDepositSweep,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultProposal,
+					},
+				},
+			},
+			actionsChecklist: []tbtc.WalletActionType{
+				tbtc.ActionRedemption,
+				tbtc.ActionDepositSweep,
+			},
+			expectedProposal: nil,
+			expectedErr:      fmt.Errorf("error while running proposal task [Redemption]: [proposal task error]"),
+		},
+		"first task is unsupported": {
+			tasks: []proposalTask{
+				&mockProposalTask{
+					action: tbtc.ActionDepositSweep,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultProposal,
+					},
+				},
+			},
+			actionsChecklist: []tbtc.WalletActionType{
+				tbtc.ActionRedemption,
+				tbtc.ActionDepositSweep,
+			},
+			expectedProposal: &mockCoordinationProposal{tbtc.ActionDepositSweep},
+		},
+		"all tasks complete without result": {
+			tasks: []proposalTask{
+				&mockProposalTask{
+					action: tbtc.ActionRedemption,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultEmpty,
+					},
+				},
+				&mockProposalTask{
+					action: tbtc.ActionDepositSweep,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultEmpty,
+					},
+				},
+				&mockProposalTask{
+					action: tbtc.ActionHeartbeat,
+					results: map[[20]byte]mockProposalTaskResult{
+						walletPublicKeyHash: resultProposal,
+					},
+				},
+			},
+			actionsChecklist: []tbtc.WalletActionType{
+				tbtc.ActionRedemption,
+				tbtc.ActionDepositSweep,
+			},
+			expectedProposal: &tbtc.NoopProposal{},
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			generator := &ProposalGenerator{
+				tasks: test.tasks,
+			}
+
+			proposal, err := generator.Generate(
+				walletPublicKeyHash,
+				test.actionsChecklist,
+			)
+
+			if !reflect.DeepEqual(test.expectedErr, err) {
+				t.Errorf(
+					"unexpected error\nexpected: %v\nactual:   %v",
+					test.expectedErr,
+					err,
+				)
+			}
+
+			if !reflect.DeepEqual(test.expectedProposal, proposal) {
+				t.Errorf(
+					"unexpected proposal\nexpected: %v\nactual:   %v",
+					test.expectedProposal,
+					proposal,
+				)
+			}
+		})
+	}
+}
+
+type mockProposalTaskResult uint8
+
+const (
+	resultProposal mockProposalTaskResult = iota
+	resultEmpty
+	resultError
+)
+
+type mockProposalTask struct {
+	action  tbtc.WalletActionType
+	results map[[20]byte]mockProposalTaskResult
+}
+
+func (mpt *mockProposalTask) run(walletPublicKeyHash [20]byte) (
+	tbtc.CoordinationProposal,
+	bool,
+	error,
+) {
+	result, ok := mpt.results[walletPublicKeyHash]
+	if !ok {
+		panic("unexpected wallet public key hash")
+	}
+
+	switch result {
+	case resultProposal:
+		return &mockCoordinationProposal{mpt.action}, true, nil
+	case resultEmpty:
+		return nil, false, nil
+	case resultError:
+		return nil, false, fmt.Errorf("proposal task error")
+	default:
+		panic("unexpected result")
+	}
+}
+
+func (mpt *mockProposalTask) actionType() tbtc.WalletActionType {
+	return mpt.action
+}
+
+type mockCoordinationProposal struct {
+	action tbtc.WalletActionType
+}
+
+func (mcp *mockCoordinationProposal) ActionType() tbtc.WalletActionType {
+	return mcp.action
+}
+
+func (mcp *mockCoordinationProposal) ValidityBlocks() uint64 {
+	panic("unsupported")
+}
+
+func (mcp *mockCoordinationProposal) Marshal() ([]byte, error) {
+	panic("unsupported")
+}
+
+func (mcp *mockCoordinationProposal) Unmarshal(bytes []byte) error {
+	panic("unsupported")
+}
