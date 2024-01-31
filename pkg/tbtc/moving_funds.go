0cd714e08be30a76927e740885a4f317d35c633c
Tomasz Slabon <tomasz.slabon@keep.network>
2024-01-31 18:54:10 +0100

    Added Bitcoin moving funds transaction assembling

--- pkg/tbtc/moving_funds.go
+++ pkg/tbtc/moving_funds.go
@@ -1,8 +1,10 @@
 package tbtc
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/ipfs/go-log/v2"
 
@@ -12,8 +14,11 @@ import (
 )
 
 const (
-	// TODO: Determine what the value should be.
-	movingFundsProposalValidityBlocks = 600
+	// TODO: Determine what the values should be.
+	movingFundsProposalValidityBlocks           = 600
+	movingFundsSigningTimeoutSafetyMarginBlocks = 300
+	movingFundsBroadcastTimeout                 = 15 * time.Minute
+	movingFundsBroadcastCheckDelay              = 1 * time.Minute
 )
 
 // MovingFundsProposal represents a moving funds proposal issued by a wallet's
@@ -44,6 +49,10 @@ type movingFundsAction struct {
 	proposal                     *MovingFundsProposal
 	proposalProcessingStartBlock uint64
 	proposalExpiryBlock          uint64
+
+	signingTimeoutSafetyMarginBlocks uint64
+	broadcastTimeout                 time.Duration
+	broadcastCheckDelay              time.Duration
 }
 
 func newMovingFundsAction(
@@ -65,18 +74,25 @@ func newMovingFundsAction(
 	)
 
 	return &movingFundsAction{
-		logger:                       logger,
-		chain:                        chain,
-		btcChain:                     btcChain,
-		movingFundsWallet:            movingFundsWallet,
-		transactionExecutor:          transactionExecutor,
-		proposal:                     proposal,
-		proposalProcessingStartBlock: proposalProcessingStartBlock,
-		proposalExpiryBlock:          proposalExpiryBlock,
+		logger:                           logger,
+		chain:                            chain,
+		btcChain:                         btcChain,
+		movingFundsWallet:                movingFundsWallet,
+		transactionExecutor:              transactionExecutor,
+		proposal:                         proposal,
+		proposalProcessingStartBlock:     proposalProcessingStartBlock,
+		proposalExpiryBlock:              proposalExpiryBlock,
+		signingTimeoutSafetyMarginBlocks: movingFundsSigningTimeoutSafetyMarginBlocks,
+		broadcastTimeout:                 depositSweepBroadcastTimeout,
+		broadcastCheckDelay:              depositSweepBroadcastCheckDelay,
 	}
 }
 
 func (mfa *movingFundsAction) execute() error {
+	validateProposalLogger := mfa.logger.With(
+		zap.String("step", "validateProposal"),
+	)
+
 	walletPublicKeyHash := bitcoin.PublicKeyHash(mfa.wallet().publicKey)
 
 	// Make sure the moving funds commitment transaction has entered the
@@ -89,7 +105,94 @@ func (mfa *movingFundsAction) execute() error {
 		)
 	}
 
-	// TODO: Continue with the implementation.
+	walletMainUtxo, err := DetermineWalletMainUtxo(
+		walletPublicKeyHash,
+		mfa.chain,
+		mfa.btcChain,
+	)
+	if err != nil {
+		return fmt.Errorf(
+			"error while determining wallet's main UTXO: [%v]",
+			err,
+		)
+	}
+
+	targetWallets, err := ValidateMovingFundsProposal(
+		validateProposalLogger,
+		walletPublicKeyHash,
+		walletMainUtxo,
+		mfa.proposal,
+		mfa.chain,
+	)
+	if err != nil {
+		return fmt.Errorf("validate proposal step failed: [%v]", err)
+	}
+
+	err = EnsureWalletSyncedBetweenChains(
+		walletPublicKeyHash,
+		walletMainUtxo,
+		mfa.chain,
+		mfa.btcChain,
+	)
+	if err != nil {
+		return fmt.Errorf(
+			"error while ensuring wallet state is synced between "+
+				"BTC and host chain: [%v]",
+			err,
+		)
+	}
+
+	unsignedMovingFundsTx, err := assembleMovingFundsTransaction(
+		mfa.btcChain,
+		mfa.wallet().publicKey,
+		walletMainUtxo,
+		targetWallets,
+		mfa.proposal.MovingFundsTxFee.Int64(),
+	)
+	if err != nil {
+		return fmt.Errorf(
+			"error while assembling moving funds transaction: [%v]",
+			err,
+		)
+	}
+
+	signTxLogger := mfa.logger.With(
+		zap.String("step", "signTransaction"),
+	)
+
+	// Just in case. This should never happen.
+	if mfa.proposalExpiryBlock < mfa.signingTimeoutSafetyMarginBlocks {
+		return fmt.Errorf("invalid proposal expiry block")
+	}
+
+	movingFundsTx, err := mfa.transactionExecutor.signTransaction(
+		signTxLogger,
+		unsignedMovingFundsTx,
+		mfa.proposalProcessingStartBlock,
+		mfa.proposalExpiryBlock-mfa.signingTimeoutSafetyMarginBlocks,
+	)
+	if err != nil {
+		return fmt.Errorf("sign transaction step failed: [%v]", err)
+	}
+
+	broadcastTxLogger := mfa.logger.With(
+		zap.String("step", "broadcastTransaction"),
+		zap.String(
+			"movingFundsTxHash",
+			movingFundsTx.Hash().Hex(bitcoin.ReversedByteOrder),
+		),
+	)
+
+	err = mfa.transactionExecutor.broadcastTransaction(
+		broadcastTxLogger,
+		movingFundsTx,
+		mfa.broadcastTimeout,
+		mfa.broadcastCheckDelay,
+	)
+	if err != nil {
+		return fmt.Errorf("broadcast transaction step failed: [%v]", err)
+	}
+
 	return nil
 }
 
@@ -180,4 +283,65 @@ func (mfa *movingFundsAction) wallet() wallet {
 
 func (mfa *movingFundsAction) actionType() WalletActionType {
 	return ActionMovingFunds
+}
+
+func assembleMovingFundsTransaction(
+	bitcoinChain bitcoin.Chain,
+	walletPublicKey *ecdsa.PublicKey,
+	walletMainUtxo *bitcoin.UnspentTransactionOutput,
+	targetWallets [][20]byte,
+	fee int64,
+) (*bitcoin.TransactionBuilder, error) {
+	if len(targetWallets) < 1 {
+		return nil, fmt.Errorf("at least one target wallet is required")
+	}
+
+	if walletMainUtxo == nil {
+		return nil, fmt.Errorf("wallet main UTXO is required")
+	}
+
+	builder := bitcoin.NewTransactionBuilder(bitcoinChain)
+	err := builder.AddPublicKeyHashInput(walletMainUtxo)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"cannot add input pointing to wallet main UTXO: [%v]",
+			err,
+		)
+	}
+
+	// The sum of all the outputs equal to the input value minus fee.
+	totalOutputValue := walletMainUtxo.Value - fee
+
+	// The amount of Bitcoin allocated to each output. Notice that the last
+	// output may have a slightly higher amount. It happens if the total output
+	// amount is not divisible by the number of target wallets. The amount for
+	// the last output will be increased by the remainder left from distributing
+	// the total output amount across all the outputs. The value of this
+	// remainder is at most `len(targetWallets) - 1` satoshis.
+	singleOutputValue := totalOutputValue / int64(len(targetWallets))
+
+	for i, targetWalletPublicKeyHash := range targetWallets {
+		outputScript, err := bitcoin.PayToWitnessPublicKeyHash(
+			targetWalletPublicKeyHash,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("cannot compute output script: [%v]", err)
+		}
+
+		outputValue := singleOutputValue
+		if i == len(targetWallets)-1 {
+			// If we are at the last output, increase its value by the remaining
+			// satoshis. If the total output amount is divisible by the number
+			// of target wallets, the increase will be `0`.
+			outputValue += (totalOutputValue % int64(len(targetWallets)))
+		}
+
+		builder.AddOutput(&bitcoin.TransactionOutput{
+			Value:           outputValue,
+			PublicKeyScript: outputScript,
+		})
+
+	}
+
+	return builder, nil
 }
