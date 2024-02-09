95cca99d69db75279d6accbe9227c1fc5c135b84
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-02-09 10:20:07 +0100

    Set pubsub log to `warn` by default
    The libp2p pubsub implementation produces a huge amount of duplicated
    `info` logs if one of the participants goes offline during messaging.
    This increases costs of logs storage and makes debugging harder.
    
    To overcome that problem, we are setting pubsub logs to the `warn` level
    by default and provide a way to customize this setting through the
    `PUBSUB_LOG_LEVEL` env variable. We will also add some app-level
    logging that will help detecting offline members.
    
    (cherry picked from commit b725e1c1650c57336355630a8803ac163c435304)

--- main.go
+++ main.go
@@ -18,14 +18,34 @@ import (
 var logger = log.Logger("keep-main")
 
 func main() {
-	err := logging.Configure(os.Getenv(config.LogLevelEnvVariable))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to configure logging: [%v]\n", err)
-	}
+	configureLogging()
 
 	rootCmd := cmd.Initialize(build.Version, build.Revision)
 
 	if err := rootCmd.Execute(); err != nil {
 		logger.Fatal(err)
+	}
+}
+
+func configureLogging() {
+	logLevel := "info"
+	if env := os.Getenv(config.LogLevelEnvVariable); len(env) > 0 {
+		logLevel = env
+	}
+
+	pubsubLogLevel := "warn"
+	if env := os.Getenv(config.PubsubLogLevelEnvVariable); len(env) > 0 {
+		pubsubLogLevel = env
+	}
+
+	levelDirective := fmt.Sprintf(
+		"%s pubsub=%s",
+		logLevel,
+		pubsubLogLevel,
+	)
+
+	err := logging.Configure(levelDirective)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to configure logging: [%v]\n", err)
 	}
 }
