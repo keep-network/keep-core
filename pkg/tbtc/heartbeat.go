acd49ee1ec5326a0134c1554ea6c3f16be04dd9c
Tomasz Slabon <tomasz.slabon@keep.network>
2024-05-07 11:54:23 +0200

    Moved heartbeat failure counter

--- pkg/tbtc/heartbeat.go
+++ pkg/tbtc/heartbeat.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/big"
+	"sync"
 
 	"github.com/ipfs/go-log/v2"
 	"github.com/keep-network/keep-core/pkg/bitcoin"
@@ -219,4 +220,40 @@ func (ha *heartbeatAction) wallet() wallet {
 
 func (ha *heartbeatAction) actionType() WalletActionType {
 	return ActionHeartbeat
+}
+
+// heartbeatFailureCounter holds counters keeping track of consecutive
+// heartbeat failures. Each wallet has a separate counter. The key used in
+// the map is the uncompressed public key (with 04 prefix) of the wallet.
+type heartbeatFailureCounter struct {
+	mutex    sync.Mutex
+	counters map[string]uint
+}
+
+func newHeartbeatFailureCounter() *heartbeatFailureCounter {
+	return &heartbeatFailureCounter{
+		counters: make(map[string]uint),
+	}
+}
+
+func (hfc *heartbeatFailureCounter) increment(walletPublicKey string) {
+	hfc.mutex.Lock()
+	defer hfc.mutex.Unlock()
+
+	hfc.counters[walletPublicKey]++
+
+}
+
+func (hfc *heartbeatFailureCounter) reset(walletPublicKey string) {
+	hfc.mutex.Lock()
+	defer hfc.mutex.Unlock()
+
+	hfc.counters[walletPublicKey] = 0
+}
+
+func (hfc *heartbeatFailureCounter) get(walletPublicKey string) uint {
+	hfc.mutex.Lock()
+	defer hfc.mutex.Unlock()
+
+	return hfc.counters[walletPublicKey]
 }
--- pkg/tbtc/heartbeat_test.go
+++ pkg/tbtc/heartbeat_test.go
@@ -2,6 +2,7 @@ package tbtc
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
 	"math/big"
@@ -137,6 +138,125 @@ func TestHeartbeatAction_SigningError(t *testing.T) {
 	// 	"cannot sign heartbeat message: [oofta]",
 	// 	err.Error(),
 	// )
+}
+
+func TestHeartbeatFailureCounter_Increment(t *testing.T) {
+	walletPublicKey := createMockSigner(t).wallet.publicKey
+	walletPublicKeyBytes, err := marshalPublicKey(walletPublicKey)
+	if err != nil {
+		t.Fatal(t)
+	}
+
+	heartbeatFailureCounter := newHeartbeatFailureCounter()
+
+	counterKey := hex.EncodeToString(walletPublicKeyBytes)
+
+	// Check first increment.
+	heartbeatFailureCounter.increment(counterKey)
+	count := heartbeatFailureCounter.get(counterKey)
+	testutils.AssertUintsEqual(
+		t,
+		"counter value",
+		1,
+		uint64(count),
+	)
+
+	// Check second increment.
+	heartbeatFailureCounter.increment(counterKey)
+	count = heartbeatFailureCounter.get(counterKey)
+	testutils.AssertUintsEqual(
+		t,
+		"counter value",
+		2,
+		uint64(count),
+	)
+}
+
+func TestHeartbeatFailureCounter_Reset(t *testing.T) {
+	walletPublicKey := createMockSigner(t).wallet.publicKey
+	walletPublicKeyBytes, err := marshalPublicKey(walletPublicKey)
+	if err != nil {
+		t.Fatal(t)
+	}
+
+	heartbeatFailureCounter := newHeartbeatFailureCounter()
+
+	counterKey := hex.EncodeToString(walletPublicKeyBytes)
+
+	// Check reset works as the first operation.
+	heartbeatFailureCounter.reset(counterKey)
+	count := heartbeatFailureCounter.get(counterKey)
+	testutils.AssertUintsEqual(
+		t,
+		"counter value",
+		0,
+		uint64(count),
+	)
+
+	// Check reset works after an increment.
+	heartbeatFailureCounter.increment(counterKey)
+	heartbeatFailureCounter.reset(counterKey)
+
+	count = heartbeatFailureCounter.get(counterKey)
+	testutils.AssertUintsEqual(
+		t,
+		"counter value",
+		0,
+		uint64(count),
+	)
+}
+
+func TestHeartbeatFailureCounter_Get(t *testing.T) {
+	walletPublicKey := createMockSigner(t).wallet.publicKey
+	walletPublicKeyBytes, err := marshalPublicKey(walletPublicKey)
+	if err != nil {
+		t.Fatal(t)
+	}
+
+	heartbeatFailureCounter := newHeartbeatFailureCounter()
+
+	counterKey := hex.EncodeToString(walletPublicKeyBytes)
+
+	// Check get works as the first operation.
+	count := heartbeatFailureCounter.get(counterKey)
+	testutils.AssertUintsEqual(
+		t,
+		"counter value",
+		0,
+		uint64(count),
+	)
+
+	// Check get works after an increment.
+	heartbeatFailureCounter.increment(counterKey)
+	count = heartbeatFailureCounter.get(counterKey)
+	testutils.AssertUintsEqual(
+		t,
+		"counter value",
+		1,
+		uint64(count),
+	)
+
+	// Construct an arbitrary public key representing a different wallet.
+	x, y := walletPublicKey.Curve.Double(walletPublicKey.X, walletPublicKey.Y)
+	anotherWalletPublicKey := &ecdsa.PublicKey{
+		Curve: walletPublicKey.Curve,
+		X:     x,
+		Y:     y,
+	}
+	anotherWalletPublicKeyBytes, err := marshalPublicKey(anotherWalletPublicKey)
+	if err != nil {
+		t.Fatal(t)
+	}
+	anotherCounterKey := hex.EncodeToString(anotherWalletPublicKeyBytes)
+
+	// Check get works on another wallet.
+	count = heartbeatFailureCounter.get(anotherCounterKey)
+	testutils.AssertUintsEqual(
+		t,
+		"counter value",
+		0,
+		uint64(count),
+	)
 }
 
 type mockHeartbeatSigningExecutor struct {
